8cec5a74623e0ff1ec1039a255dfc3c1476e5b2c
mittwald <>
2024-04-09 06:35:10 +0000

    Update from https://github.com/mittwald/flow/commit/7670cbdf38c64143b4df1c183375a6bb7095a7db

--- docs/add-skeleton-to-components/_next/static/zdw2I3wNbfGqZvidgypdT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a60193fdd3549bab.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
