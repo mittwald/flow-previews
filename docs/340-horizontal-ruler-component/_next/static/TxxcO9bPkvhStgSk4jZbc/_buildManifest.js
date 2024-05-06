00822f064395045fd14bf8c6a29f2699776d1799
mittwald <>
2024-05-06 12:01:37 +0000

    Update from https://github.com/mittwald/flow/commit/b7305b0b78c4121b82bbb37c24628b7df5c65298

--- docs/340-horizontal-ruler-component/_next/static/TxxcO9bPkvhStgSk4jZbc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-8233628826cc4e81.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
