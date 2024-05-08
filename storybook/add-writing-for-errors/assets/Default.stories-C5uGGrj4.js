7c2896b464023da33fd319eb4de36a075742502d
mittwald <>
2024-05-08 04:06:10 +0000

    Update from https://github.com/mittwald/flow/commit/e92988a019cdc925d43f174e60da221c58117ada

---
+++ storybook/add-writing-for-errors/assets/Default.stories-C5uGGrj4.js
@@ -0,0 +1,58 @@
+import{R as e}from"./index-BwDkhjyp.js";import"./index-DzE_Eh0U.js";import"./index-CLxSJn0c.js";import"./index-C0kgWbNG.js";import{T as h}from"./index-nsRPfGrU.js";import"./index-CNAQugcr.js";import"./index-CbGrRnEJ.js";import"./index-7pHJL0pz.js";import{M as s}from"./index-DWBidwvD.js";import{u as H,A as c}from"./index-JfntgTh2.js";import{O as S}from"./index-1s5RXayl.js";import{H as p}from"./Heading-DsFi8idn.js";import{C as i}from"./Content-DvzNxvwK.js";import{T as u}from"./Text-B0HvyfcO.js";import{L as G}from"./Label-d-PIMz94.js";import{B as d}from"./ButtonGroup-CBhz8FCj.js";import{B as r}from"./Button-D7-eBhv0.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DsbLNCMP.js";import"./FormField.module-DiWtHsUh.js";import"./clsx-B-dksMZM.js";import"./flowComponent-B3-LTTWn.js";import"./index-B8XB3FuZ.js";import"./IconApp-D-kcT16p.js";import"./IconWarning-Cw7drCLR.js";import"./Form-BGMe3SSh.js";import"./useFocusable-Ddi47NB9.js";import"./utils-D7InqjV7.js";import"./useLabel-lOoP3iJB.js";import"./useLabels-DFbr5jK-.js";import"./Text-nCCH5eH7.js";import"./Input-D6Py-UK9.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFormReset-C6n9I88d.js";import"./useControlledState-C0C-dzuH.js";import"./Label-Ce3BVkqc.js";import"./useSelector-3rXxgsPD.js";import"./Dialog-CeEUt42U.js";import"./RSPContexts-Bc9pl35m.js";import"./OverlayArrow-BBzZ3ne4.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-BaJnhnaM.js";import"./usePress-BZaguEpt.js";import"./FocusScope-4iKrC6rw.js";import"./VisuallyHidden-DV81ojJ5.js";import"./Wrap-DQq6jo70.js";import"./index-dhKWstfL.js";import"./dynamic-DKDa4OpU.js";import"./LoadingSpinner-D3fzZb_z.js";import"./Button-tv1ba3Dw.js";const he={title:"Overlays/Modal",component:s,parameters:{controls:{exclude:["controller","offCanvas"]}},argTypes:{size:{control:"inline-radio",options:["s","m","l"]}},args:{size:"s"},render:t=>e.createElement(s,{...t,controller:H({defaultOpen:!0})},e.createElement(p,null,"New Customer"),e.createElement(i,null,e.createElement(u,null,"Create a new customer to manage your projects, members and payments."),e.createElement(h,null,e.createElement(G,null,"Customer name"))),e.createElement(d,null,e.createElement(c,{closeOverlay:!0},e.createElement(r,{color:"accent"},"Create customer"),e.createElement(r,{color:"secondary",variant:"soft"},"Abort"))))},o={},n={render:t=>{const E=H();return e.createElement(e.Fragment,null,e.createElement(r,{color:"primary",onPress:E.open},"Create customer"),e.createElement(s,{...t,controller:E},e.createElement(p,null,"New Customer"),e.createElement(i,null,e.createElement(u,null,"Create a new customer to manage your projects, members and payments."),e.createElement(h,null,e.createElement(G,null,"Customer name"))),e.createElement(d,null,e.createElement(c,{closeOverlay:!0},e.createElement(r,{color:"accent"},"Create customer"),e.createElement(r,{color:"secondary",variant:"soft"},"Abort")))))}},a={render:t=>e.createElement(S,null,e.createElement(r,{color:"danger"},"Delete project"),e.createElement(s,{...t},e.createElement(p,null,"Delete project"),e.createElement(i,null,e.createElement(u,null,"Are you sure you want to delete this project?")),e.createElement(d,null,e.createElement(c,{closeOverlay:!0},e.createElement(r,{color:"danger"},"Delete project"),e.createElement(r,{variant:"soft",color:"secondary"},"Abort")))))},l={parameters:{viewport:{defaultViewport:"mobile1"}}},m={args:{offCanvas:!0}};var f,g,y;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:"{}",...(y=(g=o.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var C,v,B;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController();
+    return <>
+        <Button color="primary" onPress={controller.open}>
+          Create customer
+        </Button>
+        <Modal {...props} controller={controller}>
+          <Heading>New Customer</Heading>
+          <Content>
+            <Text>
+              Create a new customer to manage your projects, members and
+              payments.
+            </Text>
+            <TextField>
+              <Label>Customer name</Label>
+            </TextField>
+          </Content>
+          <ButtonGroup>
+            <Action closeOverlay>
+              <Button color="accent">Create customer</Button>
+              <Button color="secondary" variant="soft">
+                Abort
+              </Button>
+            </Action>
+          </ButtonGroup>
+        </Modal>
+      </>;
+  }
+}`,...(B=(v=n.parameters)==null?void 0:v.docs)==null?void 0:B.source}}};var O,T,b;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: props => <OverlayTrigger>
+      <Button color="danger">Delete project</Button>
+      <Modal {...props}>
+        <Heading>Delete project</Heading>
+        <Content>
+          <Text>Are you sure you want to delete this project?</Text>
+        </Content>
+        <ButtonGroup>
+          <Action closeOverlay>
+            <Button color="danger">Delete project</Button>
+            <Button variant="soft" color="secondary">
+              Abort
+            </Button>
+          </Action>
+        </ButtonGroup>
+      </Modal>
+    </OverlayTrigger>
+}`,...(b=(T=a.parameters)==null?void 0:T.docs)==null?void 0:b.source}}};var A,w,j;l.parameters={...l.parameters,docs:{...(A=l.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(j=(w=l.parameters)==null?void 0:w.docs)==null?void 0:j.source}}};var x,M,D;m.parameters={...m.parameters,docs:{...(x=m.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    offCanvas: true
+  }
+}`,...(D=(M=m.parameters)==null?void 0:M.docs)==null?void 0:D.source}}};const He=["Default","WithController","WithTrigger","Mobile","OffCanvas"];export{o as Default,l as Mobile,m as OffCanvas,n as WithController,a as WithTrigger,He as __namedExportsOrder,he as default};
