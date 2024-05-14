39afc5df9a37e9b610455a63e1be5bd43458f4d6
mittwald <>
2024-05-14 13:12:33 +0000

    Update from https://github.com/mittwald/flow/commit/160053b4b17e6a4090d94f80a83287f1a6f07724

---
+++ storybook/311-add-listitem-content-and-compact-version/assets/EdgeCases.stories-D-a693bN.js
@@ -0,0 +1,7 @@
+import{m as n,B as d}from"./Default.stories-ECdtbsRz.js";import{R as e}from"./index-BwDkhjyp.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import"./index-DKLr_WF0.js";import{L as t}from"./Link-b3M-L3Xb.js";import"./clsx-B-dksMZM.js";import"./IconApp-D-kcT16p.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-B3-LTTWn.js";import"./index-B8XB3FuZ.js";import"./IconWarning-D9rAmEP3.js";import"./Collection-D_WRBQDn.js";import"./utils-D7InqjV7.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-BZaguEpt.js";import"./useFocusable-Ddi47NB9.js";const _={...n,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>e.createElement(d,null,e.createElement(t,null,r.medium),e.createElement(t,null,r.medium),e.createElement(t,null,r.medium))};var o,i,a;m.parameters={...m.parameters,docs:{...(o=m.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+    </Breadcrumb>
+}`,...(a=(i=m.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const v=["LongTexts"];export{m as LongTexts,v as __namedExportsOrder,_ as default};
