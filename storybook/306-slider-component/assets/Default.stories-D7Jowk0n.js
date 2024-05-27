51f16ec3c721e913d9d8f87aa7aee63c92694e9f
mittwald <>
2024-05-27 12:12:25 +0000

    Update from https://github.com/mittwald/flow/commit/001e8f68f62c34559b98e6fa941aaf36d986f327

---
+++ storybook/306-slider-component/assets/Default.stories-D7Jowk0n.js
@@ -0,0 +1,12 @@
+import{R as o}from"./index-uCp2LrAq.js";import{S as i}from"./Slider-zDZShsUY.js";import{L as f}from"./Label-BAdgD445.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-B3aOilil.js";import"./PropsContextProvider-v92cY4my.js";import"./index-CFQDndEx.js";import"./clsx-B-dksMZM.js";import"./index-BOkhicXD.js";import"./Button-RSy4MY88.js";import"./IconApp-DEshkfZj.js";import"./IconWarning-Ba_VjyzM.js";import"./Wrap-DQq6jo70.js";import"./Text-JJiRTb5U.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Text-D6Zjnbf0.js";import"./utils-DZgOXw50.js";import"./LoadingSpinner-Dx1NoUG_.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Button-D4VcI4Uu.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-DpEA8Hsr.js";import"./usePress-CaJD4Kpr.js";import"./Label-BQk6lJ-q.js";import"./useNumberFormatter-DTr98jAo.js";import"./useControlledState-B8AA3oOY.js";import"./number-nHrFdSb-.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./useFormReset-Bt7ImmB-.js";import"./VisuallyHidden-DWE0U9Im.js";const P={title:"Form Controls/Slider",component:i,render:a=>o.createElement(i,{...a},o.createElement(f,null,"Amount"))},r={},e={render:a=>o.createElement(i,{...a,formatOptions:{style:"unit",unit:"gigabyte"},minValue:20,maxValue:2e3,defaultValue:200,step:20},o.createElement(f,null,"Storage"))},t={args:{isDisabled:!0}};var m,p,s;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(s=(p=r.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};var n,l,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <Slider {...props} formatOptions={{
+    style: "unit",
+    unit: "gigabyte"
+  }} minValue={20} maxValue={2000} defaultValue={200} step={20}>
+      <Label>Storage</Label>
+    </Slider>
+}`,...(c=(l=e.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var u,d,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const Q=["Default","WithUnit","Disabled"];export{r as Default,t as Disabled,e as WithUnit,Q as __namedExportsOrder,P as default};
