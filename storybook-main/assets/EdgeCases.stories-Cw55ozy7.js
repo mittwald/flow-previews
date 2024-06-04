6430b80898b3dedf58e099679628f4f92f669926
mittwald <>
2024-06-04 15:11:19 +0000

    Update from https://github.com/mittwald/flow/commit/f698e257fbf2f150fd60d274173e3b502ee7cebf

---
+++ storybook-main/assets/EdgeCases.stories-Cw55ozy7.js
@@ -0,0 +1,20 @@
+import{R as e}from"./index-uCp2LrAq.js";import{T as u}from"./Text-BPR0_XVn.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import x from"./Default.stories-VphXbvP2.js";import{I as d}from"./IllustratedMessage-Ct927CJY.js";import{H as c}from"./Heading-2hsWwErZ.js";import{B as g}from"./Button-BacIsH5Q.js";import{I as f}from"./IconApp-Cxjf7gPB.js";import"./IconWarning-CR1QMAEc.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-Bes6Wf8F.js";import"./index-CFQDndEx.js";import"./clsx-B-dksMZM.js";import"./flowComponent-wUaQYyEI.js";import"./index-BOkhicXD.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./utils-DZgOXw50.js";import"./RSPContexts-CJnKmcd7.js";import"./LoadingSpinner-DaVdL8uE.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Button-Dy16a6a4.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-rZ2Ly5zk.js";import"./usePress-B-dVD-xw.js";const F={...x,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,a.medium),e.createElement(u,null,a.long),e.createElement(g,null,"Create app"))},r={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,"No apps installed"),e.createElement(u,null,"Create your first app to start working on your website."),e.createElement(g,null,"Create app")),parameters:{viewport:{defaultViewport:"mobile1"}}};var p,n,m;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var s,l,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(i=(l=r.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const G=["LongTexts","SmallSpace"];export{t as LongTexts,r as SmallSpace,G as __namedExportsOrder,F as default};
--- storybook-main/assets/EdgeCases.stories-kAgfvdX0.js
+++
@@ -1,7 +0,0 @@
-import{m as n,B as d}from"./Default.stories-CDJdAdyo.js";import{R as m}from"./index-uCp2LrAq.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-MuKyuGJk.js";import"./clsx-B-dksMZM.js";import"./IconApp-cgHI8b4v.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-CtgwYsrm.js";import"./index-CFQDndEx.js";import"./flowComponent-CBWHhPfJ.js";import"./index-BOkhicXD.js";import"./IconWarning-Dlq6rs17.js";import"./Collection-B_ssJImm.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-B-dVD-xw.js";import"./useFocusable-rZ2Ly5zk.js";const C={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(d,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const N=["LongTexts"];export{e as LongTexts,N as __namedExportsOrder,C as default};
