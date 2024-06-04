275adf0279e29ceec2c89621c1f890a531333904
mittwald <>
2024-06-04 13:30:28 +0000

    Update from https://github.com/mittwald/flow/commit/4987db567bfe6dcb12f4f8d54e31ce1147c647dc

---
+++ storybook/add-notification-component/assets/EdgeCases.stories-C-OBNS38.js
@@ -0,0 +1,19 @@
+import{m as p,O as l,a as c}from"./Default.stories-U7NY9t9T.js";import{R as t}from"./index-uCp2LrAq.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import{B as s}from"./Button-0tthOWjT.js";import"./IconApp-CRTG2YXc.js";import{j as u}from"./IconWarning-BslSAFTD.js";import{H as g}from"./Heading-B8lltjno.js";import{S as o}from"./Section-Bvu1gTFi.js";import"./clsx-B-dksMZM.js";import"./Action-CifBrJkk.js";import"./flowComponent-l_xGG0uE.js";import"./PropsContextProvider-CtgwYsrm.js";import"./index-CFQDndEx.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Text-BbXb_6M7.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./utils-DZgOXw50.js";import"./ModalOverlay-Bnh1QVNf.js";import"./OverlayTrigger-BMXKKoby.js";import"./RSPContexts-CJnKmcd7.js";import"./OverlayArrow-BDeolAmD.js";import"./useFocusable-rZ2Ly5zk.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./useControlledState-B8AA3oOY.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-B-dVD-xw.js";import"./FocusScope-DHvs1HAk.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-POLqFEYE.js";import"./VisuallyHidden-dBh8p4gE.js";import"./Header-CdM7Oh_9.js";import"./Link-DGWPcVdH.js";import"./NavigationGroup-CQn6uAEa.js";import"./deepFindOfType-CMLjW25q.js";import"./react-children-utilities-BZCIIV1H.js";import"./Accordion-DRUN6AHp.js";import"./dynamic-DKDa4OpU.js";import"./Activity-Ck-RMSDT.js";import"./Content-CzMbiR60.js";import"./LoadingSpinner-miEzjrnf.js";import"./Button-Dy16a6a4.js";const mt={...p,title:"Overlays/OffCanvas/Edge Cases"},n={render:a=>t.createElement(l,null,t.createElement(s,null,t.createElement(u,null)),t.createElement(c,{...a},t.createElement(g,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long)))};var r,m,i;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: props => {
+    return <OffCanvasTrigger>
+        <Button>
+          <IconMenu />
+        </Button>
+        <OffCanvas {...props}>
+          <Heading>{dummyText.long}</Heading>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+        </OffCanvas>
+      </OffCanvasTrigger>;
+  }
+}`,...(i=(m=n.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const it=["LongTexts"];export{n as LongTexts,it as __namedExportsOrder,mt as default};
---
+++ storybook/add-notification-component/assets/EdgeCases.stories-CHRMY-ed.js
@@ -0,0 +1,13 @@
+import{L as d}from"./LabeledValue-D6rJQrU2.js";import C from"./Default.stories-DIHjUcK8.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{L as u}from"./Label-CY9yrom_.js";import{C as c}from"./Content-CzMbiR60.js";import{R as e}from"./index-uCp2LrAq.js";import{C as L}from"./CopyButton-BaN3G5XB.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CtgwYsrm.js";import"./index-CFQDndEx.js";import"./InlineCode-DRKxWtwe.js";import"./Link-DGWPcVdH.js";import"./flowComponent-l_xGG0uE.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-B-dVD-xw.js";import"./useFocusable-rZ2Ly5zk.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Label-BQk6lJ-q.js";import"./Wrap-DQq6jo70.js";import"./Activity-Ck-RMSDT.js";import"./Button-0tthOWjT.js";import"./IconApp-CRTG2YXc.js";import"./IconWarning-BslSAFTD.js";import"./Text-BbXb_6M7.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Text-D6Zjnbf0.js";import"./LoadingSpinner-miEzjrnf.js";import"./Button-Dy16a6a4.js";import"./TooltipTrigger-DekzKp7U.js";import"./OverlayArrow-BDeolAmD.js";import"./number-nHrFdSb-.js";import"./useControlledState-B8AA3oOY.js";import"./react-children-utilities-BZCIIV1H.js";import"./Action-CifBrJkk.js";const Z={title:"Content/LabeledValue/Edge Cases",...C},r={render:m=>e.createElement(d,{...m},e.createElement(u,null,t.medium),e.createElement(c,null,t.short),e.createElement(L,{text:t.short}))},o={render:m=>e.createElement(d,{...m},e.createElement(u,null,t.medium),e.createElement(c,null,t.long),e.createElement(L,{text:t.long}))};var n,a,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>{dummyText.medium}</Label>
+      <Content>{dummyText.short}</Content>
+      <CopyButton text={dummyText.short} />
+    </LabeledValue>
+}`,...(p=(a=r.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var i,l,s;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>{dummyText.medium}</Label>
+      <Content>{dummyText.long}</Content>
+      <CopyButton text={dummyText.long} />
+    </LabeledValue>
+}`,...(s=(l=o.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const ee=["LongLabel","LongContent"];export{o as LongContent,r as LongLabel,ee as __namedExportsOrder,Z as default};
