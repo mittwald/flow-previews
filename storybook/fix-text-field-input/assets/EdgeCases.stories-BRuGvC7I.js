7d580c38fd74330ed46a3527166a8b18e79556af
mittwald <>
2024-06-06 07:08:00 +0000

    Update from https://github.com/mittwald/flow/commit/17a2ac7aab942b528626cde6249893cea7be6396

---
+++ storybook/fix-text-field-input/assets/EdgeCases.stories-BRuGvC7I.js
@@ -0,0 +1,30 @@
+import{S as c}from"./Section-m7O-9q4L.js";import{R as e}from"./index-uCp2LrAq.js";import{H as u}from"./Heading-DFwFiMSa.js";import{T as d}from"./Text-DaXThI_-.js";import{S as l}from"./Switch-DSRSrESi.js";import S from"./Default.stories-C_Iimmhu.js";import{d as g}from"./dummyText-CX_I_Wpl.js";import{H as b}from"./Header-D8BSJimy.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Ca66243O.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Activity-Ck-RMSDT.js";import"./RSPContexts-CJnKmcd7.js";import"./utils-DZgOXw50.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./IconApp-BaqjkNZn.js";import"./IconWarning-DH7tlXwU.js";import"./Label-iTt8mwRi.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Label-BQk6lJ-q.js";import"./filterDOMProps-CeZl_uWj.js";import"./useToggleState-aJf0ifXc.js";import"./useFormReset-Bt7ImmB-.js";import"./useFocusable-DpEA8Hsr.js";import"./usePress-CaJD4Kpr.js";import"./useControlledState-B8AA3oOY.js";import"./VisuallyHidden-DWE0U9Im.js";import"./TextField-Dl2ei6Kd.js";import"./TextFieldBase-CMp-ToFU.js";import"./FormField.module-DLIQX5lR.js";import"./Form-BOwpWbru.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./FieldDescription-DDL45TyN.js";import"./useMessageFormatter-ByXXGcme.js";import"./tslib.es6-pJfR_DrR.js";import"./Input-AN0YuBj3.js";import"./Link-BSBGklkh.js";import"./StatusBadge-vGObVqup.js";import"./StatusIcon-CrOojEzx.js";const pe={...S,title:"Structure/Section/Edge Cases"},t={render:o=>e.createElement(c,{...o},e.createElement(b,null,e.createElement(u,null,g.medium),e.createElement(l,null,"Subscribed")),e.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."))},r={render:o=>e.createElement(c,{...o},e.createElement(b,null,e.createElement(u,null,"Newsletter Subscription"),e.createElement(l,null,"Subscribed")),e.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date.")),parameters:{viewport:{defaultViewport:"mobile1"}}};var i,n,m;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>{dummyText.medium}</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var a,p,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter Subscription</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(s=(p=r.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const se=["WithLongHeading","SmallSpace"];export{r as SmallSpace,t as WithLongHeading,se as __namedExportsOrder,pe as default};
