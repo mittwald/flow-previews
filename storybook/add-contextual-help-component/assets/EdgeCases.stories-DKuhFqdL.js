7da8afeb1797a05c2ae571aaea43aecd07760200
mittwald <>
2024-06-07 06:07:15 +0000

    Update from https://github.com/mittwald/flow/commit/7599dba4730a3eae23efdde4773e56ed4dd93833

---
+++ storybook/add-contextual-help-component/assets/EdgeCases.stories-DKuhFqdL.js
@@ -0,0 +1,32 @@
+import{M as c}from"./ModalTrigger-CRRuhBUx.js";import{R as t}from"./index-uCp2LrAq.js";import{B as m}from"./Button-CDZHoT_0.js";import s from"./Default.stories-BAoqNYWx.js";import{C as u}from"./Content-Bt4iOjf4.js";import{S as o}from"./Section-m7O-9q4L.js";import{T as r}from"./Text-DaXThI_-.js";import{A as d}from"./ActionGroup-t5TiWM0F.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import{H as x}from"./Heading-DFwFiMSa.js";import{A as E}from"./Action-C4y8uw1U.js";import{u as T}from"./useOverlayController-uhA2g3Cs.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Ca66243O.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ModalOverlay-CTu05M5H.js";import"./utils-DZgOXw50.js";import"./OverlayTrigger-DvDjXdfn.js";import"./RSPContexts-CJnKmcd7.js";import"./OverlayArrow-BCqBDIej.js";import"./useFocusable-DpEA8Hsr.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./useControlledState-B8AA3oOY.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-CaJD4Kpr.js";import"./FocusScope-Bpk85l6Y.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-POLqFEYE.js";import"./VisuallyHidden-DWE0U9Im.js";import"./IconApp-BaqjkNZn.js";import"./IconWarning-DH7tlXwU.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-BAybvwIX.js";import"./Button-D4VcI4Uu.js";import"./TextField-CGetB3aK.js";import"./TextFieldBase-BKURgZm-.js";import"./FormField.module-DLIQX5lR.js";import"./Form-BOwpWbru.js";import"./useLabel-D9TuMkUI.js";import"./Text-D6Zjnbf0.js";import"./FieldDescription-DDL45TyN.js";import"./useMessageFormatter-ByXXGcme.js";import"./tslib.es6-pJfR_DrR.js";import"./Input-AN0YuBj3.js";import"./useFormReset-Bt7ImmB-.js";import"./Label-BQk6lJ-q.js";import"./Label-iTt8mwRi.js";import"./lib-kUgEtMyh.js";import"./chunk-MZXVCX43-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Activity-Ck-RMSDT.js";import"./getActionGroupSlot-CgY4eby_.js";import"./dynamic-DKDa4OpU.js";const Ct={...s,title:"Overlays/Modal/Edge Cases"},n={render:a=>t.createElement(c,{...a,controller:T({defaultOpen:!0})},t.createElement(x,null,e.short),t.createElement(u,null,t.createElement(o,null,t.createElement(r,null,e.long)),t.createElement(o,null,t.createElement(r,null,e.long)),t.createElement(o,null,t.createElement(r,null,e.long)),t.createElement(o,null,t.createElement(r,null,e.long)),t.createElement(o,null,t.createElement(r,null,e.long))),t.createElement(d,null,t.createElement(E,{closeOverlay:!0},t.createElement(m,{color:"accent"},"Create customer"),t.createElement(m,{variant:"soft",color:"secondary"},"Abort"))))};var i,l,p;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController({
+    defaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay>
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(p=(l=n.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const At=["LongContent"];export{n as LongContent,At as __namedExportsOrder,Ct as default};
--- storybook/add-contextual-help-component/assets/EdgeCases.stories-mv2XX2Lt.js
+++
@@ -1,7 +0,0 @@
-import{m as n,B as d}from"./Default.stories-q8ljx-HP.js";import{R as m}from"./index-uCp2LrAq.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-Cn5QLMbf.js";import"./clsx-B-dksMZM.js";import"./IconApp-BaqjkNZn.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Ca66243O.js";import"./index-BOkhicXD.js";import"./IconWarning-Bsd3dSOq.js";import"./Collection-B_ssJImm.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-9lmrtZep.js";import"./useFocusable-78usWDLt.js";const C={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(d,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const N=["LongTexts"];export{e as LongTexts,N as __namedExportsOrder,C as default};
