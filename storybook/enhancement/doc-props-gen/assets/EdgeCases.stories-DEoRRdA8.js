ac1f230e4cbc3cbb20bdf2b0ba637994b42eeb4a
mittwald <>
2024-06-10 11:41:31 +0000

    Update from https://github.com/mittwald/flow/commit/32f8246361693ca212c26f1916737d0505789c3a

---
+++ storybook/enhancement/doc-props-gen/assets/EdgeCases.stories-DEoRRdA8.js
@@ -0,0 +1,13 @@
+import{I as o}from"./InlineCode-Bi2pjHJE.js";import{R as m}from"./index-uCp2LrAq.js";import{T as p}from"./Text-D2IU3xaq.js";import a from"./Default.stories-4pBCj44f.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import{S as l}from"./Section-CCAXujHi.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-ICaMEJBy.js";import"./index-BOkhicXD.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./utils-DZgOXw50.js";import"./Activity-Ck-RMSDT.js";const $={...a,title:"Content/InlineCode/Edge Cases"},t={render:r=>m.createElement(l,null,m.createElement(p,null,e.medium," ",m.createElement(o,{...r},e.medium.replaceAll(" ",""))," ",e.medium,m.createElement(o,{...r},e.medium)," ",e.medium))};var n,i,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        {dummyText.medium}{" "}
+        <InlineCode {...props}>
+          {dummyText.medium.replaceAll(" ", "")}
+        </InlineCode>{" "}
+        {dummyText.medium}
+        <InlineCode {...props}>{dummyText.medium}</InlineCode>{" "}
+        {dummyText.medium}
+      </Text>
+    </Section>
+}`,...(d=(i=t.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};const b=["LongText"];export{t as LongText,b as __namedExportsOrder,$ as default};
