27809d7217b90fae54e4468c32769f65b073d4f4
mittwald <>
2024-06-11 12:44:50 +0000

    Update from https://github.com/mittwald/flow/commit/3a7566764e2cceedd3e2bdaf93a4d9307a85adbf

---
+++ storybook/form-docs/assets/Variants.stories-B9gqpoWF.js
@@ -0,0 +1,31 @@
+import{R as e}from"./index-uCp2LrAq.js";import{H as s}from"./Heading-yRmJ4OM8.js";import T from"./Default.stories-Dg4mnr46.js";import{N as i}from"./Notification-BnXavptI.js";import{T as c}from"./Text-D2IU3xaq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-ICaMEJBy.js";import"./index-BOkhicXD.js";import"./RSPContexts-CJnKmcd7.js";import"./utils-DZgOXw50.js";import"./chunk-MZXVCX43-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./StatusIcon-BSViOm-4.js";import"./IconApp-CJl9NOSQ.js";import"./IconWarning-BVHxHP_W.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Link-prW6TUuX.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-CaJD4Kpr.js";import"./useFocusable-DpEA8Hsr.js";import"./Button-C1KOH12u.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-D9VRk0oy.js";import"./Button-D4VcI4Uu.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Text-D6Zjnbf0.js";const V={...T,title:"Status/Notifications/Notification/Status"},t={},o={args:{status:"warning"},render:r=>e.createElement(i,{...r},e.createElement(s,null,"Storage almost exceeded"),e.createElement(c,null,"The storage in your project ",e.createElement("b",null,"My Project")," is over 80% utilized."))},a={args:{status:"danger"},render:r=>e.createElement(i,{...r},e.createElement(s,null,"No SSL certificate"),e.createElement(c,null,"No SSL certificate could be issued for ",e.createElement("b",null,"example.de"),"."))},n={args:{status:"success"},render:r=>e.createElement(i,{...r},e.createElement(s,null,"App installation finished"),e.createElement(c,null,"Your app ",e.createElement("b",null,"My WordPress")," has been installed."))};var p,m,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(l=(m=t.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var d,u,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "warning"
+  },
+  render: props => <Notification {...props}>
+      <Heading>Storage almost exceeded</Heading>
+      <Text>
+        The storage in your project <b>My Project</b> is over 80% utilized.
+      </Text>
+    </Notification>
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var f,S,N;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    status: "danger"
+  },
+  render: props => <Notification {...props}>
+      <Heading>No SSL certificate</Heading>
+      <Text>
+        No SSL certificate could be issued for <b>example.de</b>.
+      </Text>
+    </Notification>
+}`,...(N=(S=a.parameters)==null?void 0:S.docs)==null?void 0:N.source}}};var b,x,E;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  },
+  render: props => <Notification {...props}>
+      <Heading>App installation finished</Heading>
+      <Text>
+        Your app <b>My WordPress</b> has been installed.
+      </Text>
+    </Notification>
+}`,...(E=(x=n.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};const X=["Info","Warning","Danger","Success"];export{a as Danger,t as Info,n as Success,o as Warning,X as __namedExportsOrder,V as default};
