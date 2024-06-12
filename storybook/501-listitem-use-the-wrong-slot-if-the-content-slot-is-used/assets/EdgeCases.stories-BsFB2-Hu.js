148978f6cdec306ecf9e28d438e61db3b64bba35
mittwald <>
2024-06-12 08:44:42 +0000

    Update from https://github.com/mittwald/flow/commit/3269afb0479e54fad32b907a64d3126309b3ec36

---
+++ storybook/501-listitem-use-the-wrong-slot-if-the-content-slot-is-used/assets/EdgeCases.stories-BsFB2-Hu.js
@@ -0,0 +1,19 @@
+import{m as a,O as l,a as c}from"./Default.stories-Coi0lupG.js";import{R as t}from"./index-uCp2LrAq.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import{B as s}from"./Button-C1KOH12u.js";import"./IconApp-CJl9NOSQ.js";import{j as u}from"./IconWarning-BVHxHP_W.js";import{H as g}from"./Heading-yRmJ4OM8.js";import{S as o}from"./Section-CCAXujHi.js";import"./clsx-B-dksMZM.js";import"./Action-9wZk6VsF.js";import"./useOverlayController-BEtHDaPX.js";import"./flowComponent-ICaMEJBy.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useSelector-D8Q7WzRc.js";import"./Text-D2IU3xaq.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./utils-DZgOXw50.js";import"./dynamic-DKDa4OpU.js";import"./ModalOverlay-CAownBLP.js";import"./OverlayTrigger-YwK9sEbW.js";import"./RSPContexts-CJnKmcd7.js";import"./OverlayArrow-BCqBDIej.js";import"./useFocusable-DpEA8Hsr.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./useControlledState-B8AA3oOY.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-CaJD4Kpr.js";import"./FocusScope-Bpk85l6Y.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-POLqFEYE.js";import"./VisuallyHidden-DWE0U9Im.js";import"./Header-6qkqz8Hl.js";import"./Link-prW6TUuX.js";import"./NavigationGroup-DovKFfBA.js";import"./deepFindOfType-CMLjW25q.js";import"./react-children-utilities-BZCIIV1H.js";import"./Accordion-CvkIcx_l.js";import"./Activity-Ck-RMSDT.js";import"./Content-DTlQYa-T.js";import"./LoadingSpinner-D9VRk0oy.js";import"./Button-D4VcI4Uu.js";const pt={...a,title:"Overlays/OffCanvas/Edge Cases"},n={render:p=>t.createElement(l,null,t.createElement(s,null,t.createElement(u,null)),t.createElement(c,{...p},t.createElement(g,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long),t.createElement(o,null,e.long)))};var r,m,i;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: props => {
+    return <OffCanvasTrigger>
+        <Button>
+          <IconMenu />
+        </Button>
+        <OffCanvas {...props}>
+          <Heading>{dummyText.long}</Heading>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+          <Section>{dummyText.long}</Section>
+        </OffCanvas>
+      </OffCanvasTrigger>;
+  }
+}`,...(i=(m=n.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const at=["LongTexts"];export{n as LongTexts,at as __namedExportsOrder,pt as default};
