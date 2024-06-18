e658bc66c0a052c26f78e5154b6663f5447b696c
mittwald <>
2024-06-18 07:52:58 +0000

    Update from https://github.com/mittwald/flow/commit/ec3ba2c40a3366f27506dd1a00900ca8fa0667f6

--- storybook-main/assets/EdgeCases.stories-s1ma4wAd.js
+++
@@ -1,6 +0,0 @@
-import{L as a}from"./Link-D11O8mXd.js";import{R as t}from"./index-uCp2LrAq.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import p from"./Default.stories-n_LzUC1L.js";import"./IconApp-BsJtYAdy.js";import{e as s}from"./IconWarning-DuBxq7yL.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./clsx-B-dksMZM.js";import"./flowComponent-ClMeDTWi.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-CaJD4Kpr.js";import"./useFocusable-DpEA8Hsr.js";import"./chunk-MZXVCX43-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./Text-DFRbGu6E.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./storyBackgrounds-BHxp8d-8.js";const j={...p,title:"Navigation/Link/Edge Cases"},r={render:i=>t.createElement(a,{...i},n.long,t.createElement(s,{"aria-label":"external link"}))};var o,e,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const q=["LongText"];export{r as LongText,q as __namedExportsOrder,j as default};
