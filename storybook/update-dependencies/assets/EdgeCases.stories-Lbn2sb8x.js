c17e062950b799076d22b5714d67de942ebf4f9c
mittwald <>
2024-06-18 07:51:51 +0000

    Cleanup merged Storybook previews

--- storybook/update-dependencies/assets/EdgeCases.stories-Lbn2sb8x.js
+++
@@ -1,6 +0,0 @@
-import{H as n}from"./Heading-DHtPm7Q6.js";import p from"./Default.stories-B9u5mmFu.js";import"./IconApp-CqdsO7mM.js";import{R as r}from"./index-uCp2LrAq.js";import{b as i}from"./IconWarning-BSZUxli_.js";import{d as s}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-dvxR-0IS.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./RSPContexts-CJnKmcd7.js";import"./utils-DZgOXw50.js";import"./storyBackgrounds-BHxp8d-8.js";const M={...p,title:"Content/Heading/Edge Cases",component:n},e={render:a=>r.createElement(n,{...a},r.createElement(i,null),s.medium)};var o,t,m;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: props => <Heading {...props}>
-      <IconMember />
-      {dummyText.medium}
-    </Heading>
-}`,...(m=(t=e.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const R=["LongText"];export{e as LongText,R as __namedExportsOrder,M as default};
