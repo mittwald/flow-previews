4ea96d215569db7f2d3d837b47f7bb496ac5ff37
mittwald <>
2024-06-27 10:30:49 +0000

    Update from https://github.com/mittwald/flow/commit/93ad200dfe9f3c5c2585693a34a75f4ef1174ef2

---
+++ storybook/558-tray-view-of-offcanvas-looks-broken/assets/EdgeCases.stories-D-J7l3W3.js
@@ -0,0 +1,63 @@
+import{M as x}from"./ModalTrigger-Qi1SlLr8.js";import{R as e}from"./index-uCp2LrAq.js";import{B as m}from"./Button-b1cVdHRY.js";import S from"./Default.stories-PCw9s5lJ.js";import{C as E}from"./Content-ByAjhsSv.js";import{S as n}from"./Section-DzNnpsuO.js";import{T as o}from"./Text-DICJV5XT.js";import{A as T}from"./ActionGroup-D1PL9lnQ.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{H as g}from"./Heading-BHRpEcyE.js";import{A as f}from"./Action-Dtbgxw-g.js";import{u as y}from"./OverlayTrigger-DzHiGsoK.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Bwx-eWaq.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./OverlayArrow-BCqBDIej.js";import"./useFocusable-DpEA8Hsr.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./useControlledState-B8AA3oOY.js";import"./Header-Cry1ipkq.js";import"./IconApp-Cw4q-ANY.js";import"./IconWarning-BKpEgIOz.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-DyqbfneA.js";import"./Button-D4VcI4Uu.js";import"./usePress-CaJD4Kpr.js";import"./TextField-BixtR3uA.js";import"./TextFieldBase-65YOoAkS.js";import"./FormField.module-CLpgzBkd.js";import"./Form-BOwpWbru.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./Text-D6Zjnbf0.js";import"./FieldDescription-hUMarsck.js";import"./Input-AN0YuBj3.js";import"./useFormReset-Bt7ImmB-.js";import"./Label-BQk6lJ-q.js";import"./Label-4LeiabC0.js";import"./lib-B76Zrq0u.js";import"./chunk-MZXVCX43-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Activity-Ck-RMSDT.js";import"./RSPContexts-CJnKmcd7.js";import"./FocusScope-Bpk85l6Y.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-DWE0U9Im.js";import"./context-4EJujaZN.js";import"./useSelector-C7-C3jKq.js";import"./dynamic-DKDa4OpU.js";import"./Collection-BZQFS83W.js";import"./Header-CL3U4zra.js";const Oe={...S,title:"Overlays/Modal/Edge Cases"},r={render:a=>e.createElement(x,{...a,controller:y("Modal",{defaultOpen:!0})},e.createElement(g,null,t.short),e.createElement(E,null,e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long))),e.createElement(T,null,e.createElement(f,{closeOverlay:"Modal"},e.createElement(m,{color:"accent"},"Create customer"),e.createElement(m,{variant:"soft",color:"secondary"},"Abort"))))},l={render:a=>e.createElement(x,{offCanvas:!0,...a,controller:y("Modal",{defaultOpen:!0})},e.createElement(g,null,t.short),e.createElement(E,null,e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long)),e.createElement(n,null,e.createElement(o,null,t.long))),e.createElement(T,null,e.createElement(f,{closeOverlay:"Modal"},e.createElement(m,{color:"accent"},"Create customer"),e.createElement(m,{variant:"soft",color:"secondary"},"Abort"))))};var c,i,p;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController("Modal", {
+    defaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,s,d;l.parameters={...l.parameters,docs:{...(u=l.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <Modal offCanvas {...props} controller={useOverlayController("Modal", {
+    defaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(d=(s=l.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};const Be=["LongContent","LongContentOffCanvas"];export{r as LongContent,l as LongContentOffCanvas,Be as __namedExportsOrder,Oe as default};
