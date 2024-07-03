3c7cbc0d704511bc333eb1e2755a3f4f098b6379
mittwald <>
2024-07-03 09:44:54 +0000

    Update from https://github.com/mittwald/flow/commit/7d842255ccb5e071cbfab7b62d4e67a0f66c6054

---
+++ storybook/update-style-guide-heading-sizes/assets/Default.stories-BGGVshNX.js
@@ -0,0 +1,35 @@
+import{C as o}from"./ColumnLayout-B78AueES.js";import{R as e}from"./index-uCp2LrAq.js";import{T as t}from"./TextField-BixtR3uA.js";import{L as l}from"./Label-4LeiabC0.js";import{I as i}from"./InlineAlert-CiP6m2WN.js";import{H as u}from"./Heading-BHRpEcyE.js";import{S as s}from"./Section-CYxcFJfL.js";import"./clsx-B-dksMZM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TextFieldBase-65YOoAkS.js";import"./FormField.module-CLpgzBkd.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./IconApp-Cw4q-ANY.js";import"./flowComponent-Bwx-eWaq.js";import"./index-BOkhicXD.js";import"./IconWarning-BKpEgIOz.js";import"./Form-BOwpWbru.js";import"./useFocusable-DpEA8Hsr.js";import"./utils-DZgOXw50.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./Text-D6Zjnbf0.js";import"./FieldDescription-hUMarsck.js";import"./Text-DICJV5XT.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Input-AN0YuBj3.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFormReset-Bt7ImmB-.js";import"./useControlledState-B8AA3oOY.js";import"./Label-BQk6lJ-q.js";import"./StatusIcon-CaYRvD1u.js";import"./RSPContexts-CJnKmcd7.js";import"./Activity-Ck-RMSDT.js";import"./Collection-BZQFS83W.js";import"./Header-CL3U4zra.js";const ne={title:"Structure/ColumnLayout",component:o,parameters:{controls:{exclude:["s","m","l","className"]}},render:r=>e.createElement(s,null,e.createElement(i,null,e.createElement(u,null,"s: [1] (default), m: [1, 1] (default), l: [1, 1, 1] (default)")),e.createElement(o,{...r},e.createElement(t,null,e.createElement(l,null,"First name")),e.createElement(t,null,e.createElement(l,null,"Last name")),e.createElement(t,null,e.createElement(l,null,"Street")),e.createElement(t,null,e.createElement(l,null,"House number"))))},n={},m={render:r=>e.createElement(s,null,e.createElement(i,null,e.createElement(u,null,"s: [1] (default), m: [2, 1], l: [2, 1] (inherited)")),e.createElement(o,{...r,m:[2,1]},e.createElement(t,null,e.createElement(l,null,"Street")),e.createElement(t,null,e.createElement(l,null,"House number"))))},a={render:r=>e.createElement(s,null,e.createElement(i,null,e.createElement(u,null,"s: [2, 1], m: [3, 2, 1], l: [4, 3, 2, 1]")),e.createElement(o,{...r,s:[2,1],m:[3,2,1],l:[4,3,2,1]},e.createElement(t,null,e.createElement(l,null,"First name")),e.createElement(t,null,e.createElement(l,null,"Last name")),e.createElement(t,null,e.createElement(l,null,"Street")),e.createElement(t,null,e.createElement(l,null,"House number"))))};var c,p,d;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(p=n.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var E,L,b;m.parameters={...m.parameters,docs:{...(E=m.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <Section>
+      <InlineAlert>
+        <Heading>s: [1] (default), m: [2, 1], l: [2, 1] (inherited)</Heading>
+      </InlineAlert>
+      <ColumnLayout {...props} m={[2, 1]}>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(b=(L=m.parameters)==null?void 0:L.docs)==null?void 0:b.source}}};var x,F,f;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <Section>
+      <InlineAlert>
+        <Heading>s: [2, 1], m: [3, 2, 1], l: [4, 3, 2, 1]</Heading>
+      </InlineAlert>
+      <ColumnLayout {...props} s={[2, 1]} m={[3, 2, 1]} l={[4, 3, 2, 1]}>
+        <TextField>
+          <Label>First name</Label>
+        </TextField>
+        <TextField>
+          <Label>Last name</Label>
+        </TextField>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(f=(F=a.parameters)==null?void 0:F.docs)==null?void 0:f.source}}};const me=["Default","CustomValueM","AllCustomValues"];export{a as AllCustomValues,m as CustomValueM,n as Default,me as __namedExportsOrder,ne as default};
---
+++ storybook/update-style-guide-heading-sizes/assets/EdgeCases.stories-lKrPFLPx.js
@@ -0,0 +1,30 @@
+import{S as c}from"./Section-CYxcFJfL.js";import{R as e}from"./index-uCp2LrAq.js";import{H as u}from"./Heading-BHRpEcyE.js";import{T as d}from"./Text-DICJV5XT.js";import{S as l}from"./Switch-Br6wX_ei.js";import S from"./Default.stories-CDC6m0AX.js";import{d as g}from"./dummyText-CX_I_Wpl.js";import{H as b}from"./Header-Cry1ipkq.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Bwx-eWaq.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Activity-Ck-RMSDT.js";import"./Collection-BZQFS83W.js";import"./Header-CL3U4zra.js";import"./utils-DZgOXw50.js";import"./RSPContexts-CJnKmcd7.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./IconApp-Cw4q-ANY.js";import"./IconWarning-BKpEgIOz.js";import"./Label-4LeiabC0.js";import"./useLocalizedStringFormatter-CHReNXSI.js";import"./Label-BQk6lJ-q.js";import"./filterDOMProps-CeZl_uWj.js";import"./useToggleState-aJf0ifXc.js";import"./useFormReset-Bt7ImmB-.js";import"./useFocusable-DpEA8Hsr.js";import"./usePress-CaJD4Kpr.js";import"./useControlledState-B8AA3oOY.js";import"./VisuallyHidden-DWE0U9Im.js";import"./TextField-BixtR3uA.js";import"./TextFieldBase-65YOoAkS.js";import"./FormField.module-CLpgzBkd.js";import"./Form-BOwpWbru.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./FieldDescription-hUMarsck.js";import"./Input-AN0YuBj3.js";import"./Link-CYcBNNLK.js";import"./StatusBadge-C2ibyuGO.js";import"./StatusIcon-CaYRvD1u.js";const pe={...S,title:"Structure/Section/Edge Cases"},t={render:o=>e.createElement(c,{...o},e.createElement(b,null,e.createElement(u,null,g.medium),e.createElement(l,null,"Subscribed")),e.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."))},r={render:o=>e.createElement(c,{...o},e.createElement(b,null,e.createElement(u,null,"Newsletter Subscription"),e.createElement(l,null,"Subscribed")),e.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date.")),parameters:{viewport:{defaultViewport:"mobile1"}}};var i,n,m;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>{dummyText.medium}</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var a,p,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter Subscription</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(s=(p=r.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const se=["WithLongHeading","SmallSpace"];export{r as SmallSpace,t as WithLongHeading,se as __namedExportsOrder,pe as default};
