ac9fa72ffafc3706f9b310fc4b64aa7e9a6f294a
mittwald <>
2024-07-03 09:46:10 +0000

    Update from https://github.com/mittwald/flow/commit/9d2427acaf6e33173444c4cb90d42a1740df41fd

---
+++ storybook/update-style-guide-heading-sizes/assets/Default.stories-S1t7pcxH.js
@@ -0,0 +1,15 @@
+import{T as t}from"./Text-D755SsIi.js";import{R as o}from"./index-uCp2LrAq.js";import{S as k}from"./Section-CYxcFJfL.js";import{s as y,a as f}from"./storyBackgrounds-BHxp8d-8.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./clsx-B-dksMZM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-Bwx-eWaq.js";import"./index-BOkhicXD.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-D6Zjnbf0.js";import"./utils-DZgOXw50.js";import"./Activity-Ck-RMSDT.js";import"./Collection-BZQFS83W.js";import"./Header-CL3U4zra.js";const w={title:"Content/Text",component:t,argTypes:{elementType:{control:"inline-radio",options:["span","p"],defaultValue:"span"}},args:{elementType:"span"},render:g=>o.createElement(k,null,o.createElement(t,{...g},"Text is an unstyled component that can be used to display texts. By default it renders a span."))},r={},e={args:{color:"dark"},parameters:{backgrounds:y}},a={args:{color:"light"},parameters:{backgrounds:f}};var s,n,p;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};var m,c,i;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var d,l,u;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(u=(l=a.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const z=["Default","Dark","Light"];export{e as Dark,r as Default,a as Light,z as __namedExportsOrder,w as default};
