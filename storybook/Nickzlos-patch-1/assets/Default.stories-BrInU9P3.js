cf2a618daea303f4955c762cb4e8dfc0e7c320df
mittwald <>
2024-07-05 08:58:38 +0000

    Update from https://github.com/mittwald/flow/commit/07d9f208886d75ee92a5544ef27f7b74c9cd7f68

---
+++ storybook/Nickzlos-patch-1/assets/Default.stories-BrInU9P3.js
@@ -0,0 +1,14 @@
+import{I as n}from"./InlineAlert-ClKOw7F2.js";import{R as e}from"./index-BwDkhjyp.js";import{H as l}from"./Heading-rdQzPY0C.js";import{C as c}from"./Content-BQ3ZqW64.js";import{B as u}from"./Button-DWl6SRuO.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./clsx-B-dksMZM.js";import"./StatusIcon-DLWdn967.js";import"./IconApp-DGeSE4ad.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-CbDEtdyo.js";import"./index-B8XB3FuZ.js";import"./IconWarning-DyJIeaqs.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./RSPContexts-Bc9pl35m.js";import"./utils-B-mS0hjG.js";import"./Wrap-DQq6jo70.js";import"./Activity-Cov3f-FH.js";import"./Text-Bxhza4fE.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./Text-C_suByGF.js";import"./LoadingSpinner-BeDxB4g7.js";import"./Button-BTMfZQLF.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-CKMlalor.js";import"./usePress-YR3pmEr9.js";const G={title:"Status/InlineAlert",component:n,argTypes:{status:{control:"inline-radio",options:["info","success","warning","danger"]}},args:{status:"info"}},r={render:a=>e.createElement(n,{...a},e.createElement(l,null,"Email address has been archived"))},t={render:a=>e.createElement(n,{...a},e.createElement(l,null,"Email address has been archived"),e.createElement(c,null,"As your domain has been deleted, this email address has been archived. To be able to send and receive emails, you must rename the address.",e.createElement(u,null,"Update email address")))};var o,s,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: props => <InlineAlert {...props}>
+      <Heading>Email address has been archived</Heading>
+    </InlineAlert>
+}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var m,d,p;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: props => <InlineAlert {...props}>
+      <Heading>Email address has been archived</Heading>
+      <Content>
+        As your domain has been deleted, this email address has been archived.
+        To be able to send and receive emails, you must rename the address.
+        <Button>Update email address</Button>
+      </Content>
+    </InlineAlert>
+}`,...(p=(d=t.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const J=["Default","WithContent"];export{r as Default,t as WithContent,J as __namedExportsOrder,G as default};
---
+++ storybook/Nickzlos-patch-1/assets/Default.stories-DwrtBvzo.js
@@ -0,0 +1,32 @@
+import{R as e}from"./index-BwDkhjyp.js";import{T as B,$ as M}from"./TextFieldBase-B4ElcLkc.js";import{C as O}from"./PropsContextProvider-DwTel3aB.js";import{f as j}from"./flowComponent-CbDEtdyo.js";import{L as t}from"./Label-nQ0y4l8x.js";import{a as G}from"./chunk-MZXVCX43-CM0pFb8Z.js";import{F as H}from"./FieldDescription-DyYHFM_e.js";import{F as J}from"./FormField.module-CNlZpOkK.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./index-DVe9vSxq.js";import"./utils-B-mS0hjG.js";import"./index-B8XB3FuZ.js";import"./Form-Dmkgi1d-.js";import"./useFocusable-CKMlalor.js";import"./useLabel-BaXqGmeE.js";import"./useLabels-kg7v6Afk.js";import"./Text-C_suByGF.js";import"./Input-DNo_oYh5.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFormReset-CfqvoD0r.js";import"./useControlledState-C0C-dzuH.js";import"./Label-DwL17Jrp.js";import"./v4-CQkTLCs1.js";import"./Text-Bxhza4fE.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./Wrap-DQq6jo70.js";import"./IconApp-DGeSE4ad.js";import"./IconWarning-DyJIeaqs.js";const Q="flow--text-area",U={textArea:Q},a=j("TextArea",r=>{const{children:q,placeholder:_,rows:$=5,refProp:k,...z}=r,N=e.createElement(M,{rows:$,placeholder:_,className:U.textArea,ref:k});return e.createElement(O,null,e.createElement(B,{...z,input:N},q))});a.__docgenInfo={description:"",methods:[],displayName:"TextArea"};const Pe={title:"Form Controls/TextArea",component:a,render:r=>e.createElement(a,{onChange:G("onChange"),...r},e.createElement(t,null,"Message"))},s={},o={args:{isDisabled:!0}},n={args:{isRequired:!0}},l={render:r=>e.createElement(a,{...r},e.createElement(t,null,"Public Key"),e.createElement(H,null,"Starts with ssh-rsa"))},i={render:r=>e.createElement(a,{...r,defaultValue:"ssh-rsa asdfkehbahrgvszuagfdashdfgsaghdfiuadsf"},e.createElement(t,null,"Public Key"))},c={render:r=>e.createElement(a,{...r,placeholder:"ssh-rsa..."},e.createElement(t,null,"Public Key"))},p={render:r=>e.createElement(a,{...r,isInvalid:!0,defaultValue:"hello"},e.createElement(t,null,"Public Key"),e.createElement(J,null,"Invalid Key"))},m={args:{showCharacterCount:!0,maxLength:100}};var d,u,h;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(h=(u=s.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var f,g,b;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(b=(g=o.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var x,E,A;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(A=(E=n.parameters)==null?void 0:E.docs)==null?void 0:A.source}}};var C,F,T;l.parameters={...l.parameters,docs:{...(C=l.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <TextArea {...props}>
+      <Label>Public Key</Label>
+      <FieldDescription>Starts with ssh-rsa</FieldDescription>
+    </TextArea>
+}`,...(T=(F=l.parameters)==null?void 0:F.docs)==null?void 0:T.source}}};var D,y,L;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} defaultValue="ssh-rsa asdfkehbahrgvszuagfdashdfgsaghdfiuadsf">
+      <Label>Public Key</Label>
+    </TextArea>
+}`,...(L=(y=i.parameters)==null?void 0:y.docs)==null?void 0:L.source}}};var P,S,K;c.parameters={...c.parameters,docs:{...(P=c.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} placeholder="ssh-rsa...">
+      <Label>Public Key</Label>
+    </TextArea>
+}`,...(K=(S=c.parameters)==null?void 0:S.docs)==null?void 0:K.source}}};var w,W,v;p.parameters={...p.parameters,docs:{...(w=p.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} isInvalid defaultValue="hello">
+      <Label>Public Key</Label>
+      <FieldError>Invalid Key</FieldError>
+    </TextArea>
+}`,...(v=(W=p.parameters)==null?void 0:W.docs)==null?void 0:v.source}}};var R,V,I;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    showCharacterCount: true,
+    maxLength: 100
+  }
+}`,...(I=(V=m.parameters)==null?void 0:V.docs)==null?void 0:I.source}}};const Se=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithPlaceholder","WithFieldError","ShowCharacterCount"];export{s as Default,o as Disabled,n as Required,m as ShowCharacterCount,i as WithDefaultValue,l as WithFieldDescription,p as WithFieldError,c as WithPlaceholder,Se as __namedExportsOrder,Pe as default};
