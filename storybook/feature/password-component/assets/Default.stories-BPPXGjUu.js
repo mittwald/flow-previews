7a7918fc8aca448e93074b6ca7ce50926f5791f0
mittwald <>
2024-07-09 12:20:52 +0000

    Update from https://github.com/mittwald/flow/commit/0642a5214154374340097b32971cae94e8438628

---
+++ storybook/feature/password-component/assets/Default.stories-BPPXGjUu.js
@@ -0,0 +1,49 @@
+import{S as r}from"./Section-DgGPYImf.js";import{R as e}from"./index-uCp2LrAq.js";import{H as n}from"./Heading-BCjHoBHi.js";import{T as m}from"./Text-Dgrf8S-7.js";import{T as l}from"./TextField-Cdx6ZZCR.js";import{L as p}from"./Label-BTedSu1I.js";import{L as H}from"./Link-BLisCSKB.js";import{S as L}from"./Switch-B_X_he5l.js";import{S as F}from"./StatusBadge-Bm83Pais.js";import{H as T}from"./Header-CkoFYiSi.js";import"./IconApp-DWya8WHB.js";import{b as N}from"./IconWarning-CVIujYNc.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Cll4pGCY.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Activity-Ck-RMSDT.js";import"./ContextMenu.module-CAjBRoKS.js";import"./Collection-BZQFS83W.js";import"./Header-CL3U4zra.js";import"./utils-DZgOXw50.js";import"./RSPContexts-CJnKmcd7.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./Text-3FUNhTNc.js";import"./TextFieldBase-CVygZSdA.js";import"./FormField.module-BYarEUJD.js";import"./Form-pLdcVy8a.js";import"./useFocusable-B1CfUGG-.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./FieldDescription-DwIkflUx.js";import"./useLocalizedStringFormatter-Bpx1Gtyq.js";import"./TextField-BGn1ZlWC.js";import"./Input-CK4H1fha.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFormReset-Bt7ImmB-.js";import"./useControlledState-B8AA3oOY.js";import"./Label-BzjXWGX7.js";import"./usePress-Bc78XfGJ.js";import"./useToggleState-D5LCi4E-.js";import"./VisuallyHidden-CVANJuSe.js";import"./StatusIcon-D62bjPgK.js";const he={title:"Structure/Section",component:r,render:t=>e.createElement(r,{...t},e.createElement(n,null,"Newsletter"),e.createElement(m,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."),e.createElement(H,{href:"#"},"Subscribe"))},o={},a={render:t=>e.createElement(e.Fragment,null,e.createElement(r,{...t},e.createElement(n,null,e.createElement(N,null),"Personal Information"),e.createElement(l,{isRequired:!0,defaultValue:"John"},e.createElement(p,null,"First name")),e.createElement(l,{isRequired:!0,defaultValue:"Doe"},e.createElement(p,null,"Last name"))),e.createElement(r,{...t},e.createElement(n,null,"Newsletter"),e.createElement(m,null,"Upcoming releases, new features and tips about your hosting - we bring you the most important information in your inbox. Subscribe to our newsletter and stay up to date."),e.createElement(H,{href:"#"},"Subscribe")))},i={render:t=>e.createElement(r,{...t},e.createElement(T,null,e.createElement(n,null,"Newsletter"),e.createElement(L,null,"Subscription")),e.createElement(m,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."))},s={render:t=>e.createElement(r,{...t},e.createElement(T,null,e.createElement(n,null,"Newsletter"),e.createElement(F,null,"Subscribed")),e.createElement(m,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."))};var u,c,d;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(d=(c=o.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var b,S,g;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props}>
+        <Heading>
+          <IconMember />
+          Personal Information
+        </Heading>
+        <TextField isRequired defaultValue="John">
+          <Label>First name</Label>
+        </TextField>
+        <TextField isRequired defaultValue="Doe">
+          <Label>Last name</Label>
+        </TextField>
+      </Section>
+      <Section {...props}>
+        <Heading>Newsletter</Heading>
+        <Text>
+          Upcoming releases, new features and tips about your hosting - we bring
+          you the most important information in your inbox. Subscribe to our
+          newsletter and stay up to date.
+        </Text>
+        <Link href="#">Subscribe</Link>
+      </Section>
+    </>
+}`,...(g=(S=a.parameters)==null?void 0:S.docs)==null?void 0:g.source}}};var f,w,h;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter</Heading>
+        <Switch>Subscription</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(h=(w=i.parameters)==null?void 0:w.docs)==null?void 0:h.source}}};var E,x,y;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter</Heading>
+        <StatusBadge>Subscribed</StatusBadge>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(y=(x=s.parameters)==null?void 0:x.docs)==null?void 0:y.source}}};const Ee=["Default","MultipleSections","WithSwitch","WithStatusBadge"];export{o as Default,a as MultipleSections,s as WithStatusBadge,i as WithSwitch,Ee as __namedExportsOrder,he as default};
---
+++ storybook/feature/password-component/assets/Default.stories-DU74z6mp.js
@@ -0,0 +1,35 @@
+import{C as o}from"./ColumnLayout-B78AueES.js";import{R as e}from"./index-uCp2LrAq.js";import{T as t}from"./TextField-Cdx6ZZCR.js";import{L as l}from"./Label-BTedSu1I.js";import{I as i}from"./InlineAlert-Bv-P7TOp.js";import{H as u}from"./Heading-BCjHoBHi.js";import{S as s}from"./Section-DgGPYImf.js";import"./clsx-B-dksMZM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TextFieldBase-CVygZSdA.js";import"./FormField.module-BYarEUJD.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./IconApp-DWya8WHB.js";import"./flowComponent-Cll4pGCY.js";import"./index-BOkhicXD.js";import"./IconWarning-CVIujYNc.js";import"./Form-pLdcVy8a.js";import"./useFocusable-B1CfUGG-.js";import"./utils-DZgOXw50.js";import"./useLabel-D9TuMkUI.js";import"./useLabels-POLqFEYE.js";import"./Text-3FUNhTNc.js";import"./FieldDescription-DwIkflUx.js";import"./Text-Dgrf8S-7.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Wrap-DQq6jo70.js";import"./useLocalizedStringFormatter-Bpx1Gtyq.js";import"./TextField-BGn1ZlWC.js";import"./Input-CK4H1fha.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFormReset-Bt7ImmB-.js";import"./useControlledState-B8AA3oOY.js";import"./Label-BzjXWGX7.js";import"./StatusIcon-D62bjPgK.js";import"./RSPContexts-CJnKmcd7.js";import"./Activity-Ck-RMSDT.js";import"./ContextMenu.module-CAjBRoKS.js";import"./Collection-BZQFS83W.js";import"./Header-CL3U4zra.js";const ae={title:"Structure/ColumnLayout",component:o,parameters:{controls:{exclude:["s","m","l","className"]}},render:r=>e.createElement(s,null,e.createElement(i,null,e.createElement(u,null,"s: [1] (default), m: [1, 1] (default), l: [1, 1, 1] (default)")),e.createElement(o,{...r},e.createElement(t,null,e.createElement(l,null,"First name")),e.createElement(t,null,e.createElement(l,null,"Last name")),e.createElement(t,null,e.createElement(l,null,"Street")),e.createElement(t,null,e.createElement(l,null,"House number"))))},n={},m={render:r=>e.createElement(s,null,e.createElement(i,null,e.createElement(u,null,"s: [1] (default), m: [2, 1], l: [2, 1] (inherited)")),e.createElement(o,{...r,m:[2,1]},e.createElement(t,null,e.createElement(l,null,"Street")),e.createElement(t,null,e.createElement(l,null,"House number"))))},a={render:r=>e.createElement(s,null,e.createElement(i,null,e.createElement(u,null,"s: [2, 1], m: [3, 2, 1], l: [4, 3, 2, 1]")),e.createElement(o,{...r,s:[2,1],m:[3,2,1],l:[4,3,2,1]},e.createElement(t,null,e.createElement(l,null,"First name")),e.createElement(t,null,e.createElement(l,null,"Last name")),e.createElement(t,null,e.createElement(l,null,"Street")),e.createElement(t,null,e.createElement(l,null,"House number"))))};var c,p,d;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(p=n.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var E,L,b;m.parameters={...m.parameters,docs:{...(E=m.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <Section>
+      <InlineAlert>
+        <Heading>s: [1] (default), m: [2, 1], l: [2, 1] (inherited)</Heading>
+      </InlineAlert>
+      <ColumnLayout {...props} m={[2, 1]}>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(b=(L=m.parameters)==null?void 0:L.docs)==null?void 0:b.source}}};var x,F,f;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <Section>
+      <InlineAlert>
+        <Heading>s: [2, 1], m: [3, 2, 1], l: [4, 3, 2, 1]</Heading>
+      </InlineAlert>
+      <ColumnLayout {...props} s={[2, 1]} m={[3, 2, 1]} l={[4, 3, 2, 1]}>
+        <TextField>
+          <Label>First name</Label>
+        </TextField>
+        <TextField>
+          <Label>Last name</Label>
+        </TextField>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(f=(F=a.parameters)==null?void 0:F.docs)==null?void 0:f.source}}};const oe=["Default","CustomValueM","AllCustomValues"];export{a as AllCustomValues,m as CustomValueM,n as Default,oe as __namedExportsOrder,ae as default};
