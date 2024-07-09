87193239ed6edc9d67b4ab0b12d6332465b2f40c
mittwald <>
2024-07-09 09:13:03 +0000

    Update from https://github.com/mittwald/flow/commit/2856aa471ecbf560da423b0fb95eebb9b627f253

---
+++ storybook/feature/password-component/assets/EdgeCases.stories-CkCVqNAV.js
@@ -0,0 +1,7 @@
+import{m as n,B as d}from"./Default.stories-DvYn9J2i.js";import{R as m}from"./index-uCp2LrAq.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-BLisCSKB.js";import"./clsx-B-dksMZM.js";import"./IconApp-DWya8WHB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./flowComponent-Cll4pGCY.js";import"./index-BOkhicXD.js";import"./IconWarning-CVIujYNc.js";import"./Collection-BZQFS83W.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-Bc78XfGJ.js";import"./useFocusable-B1CfUGG-.js";const C={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(d,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+    </Breadcrumb>
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const N=["LongTexts"];export{e as LongTexts,N as __namedExportsOrder,C as default};
---
+++ storybook/feature/password-component/assets/EdgeCases.stories-DoCcTQR3.js
@@ -0,0 +1,13 @@
+import{L as d}from"./LabeledValue-Dm6AysED.js";import C from"./Default.stories-UUZ1stoq.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{L as u}from"./Label-SpZFRJ_k.js";import{C as c}from"./Content-CeaKmh6s.js";import{R as e}from"./index-uCp2LrAq.js";import{C as L}from"./CopyButton-DWwUMfSl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-RdMO5ekw.js";import"./index-CFQDndEx.js";import"./InlineCode-Bi2pjHJE.js";import"./Link-BLisCSKB.js";import"./flowComponent-Cll4pGCY.js";import"./index-BOkhicXD.js";import"./_commonjsHelpers-BosuxZz1.js";import"./utils-DZgOXw50.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-Bc78XfGJ.js";import"./useFocusable-B1CfUGG-.js";import"./useLocalizedStringFormatter-Bpx1Gtyq.js";import"./Label-BzjXWGX7.js";import"./Wrap-DQq6jo70.js";import"./Activity-Ck-RMSDT.js";import"./Button-D6GiwIW_.js";import"./IconApp-DWya8WHB.js";import"./IconWarning-CVIujYNc.js";import"./Text-Dgrf8S-7.js";import"./EmulatedBoldText-DzNCZpDp.js";import"./Text-3FUNhTNc.js";import"./LoadingSpinner-xMX5UQ9s.js";import"./Button-DvIckO9Z.js";import"./TooltipTrigger-B0K2wUO5.js";import"./OverlayArrow-B_2Pd_cl.js";import"./number-nHrFdSb-.js";import"./useControlledState-B8AA3oOY.js";import"./react-children-utilities-BZCIIV1H.js";import"./Action-CAcZ0_fm.js";import"./context-CSQJgabl.js";import"./useSelector-DpPpxyR7.js";import"./dynamic-DKDa4OpU.js";const re={title:"Content/LabeledValue/Edge Cases",...C},r={render:m=>e.createElement(d,{...m},e.createElement(u,null,t.medium),e.createElement(c,null,t.short),e.createElement(L,{text:t.short}))},o={render:m=>e.createElement(d,{...m},e.createElement(u,null,t.medium),e.createElement(c,null,t.long),e.createElement(L,{text:t.long}))};var n,p,a;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>{dummyText.medium}</Label>
+      <Content>{dummyText.short}</Content>
+      <CopyButton text={dummyText.short} />
+    </LabeledValue>
+}`,...(a=(p=r.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};var i,l,s;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>{dummyText.medium}</Label>
+      <Content>{dummyText.long}</Content>
+      <CopyButton text={dummyText.long} />
+    </LabeledValue>
+}`,...(s=(l=o.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const oe=["LongLabel","LongContent"];export{o as LongContent,r as LongLabel,oe as __namedExportsOrder,re as default};
