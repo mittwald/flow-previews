93784c143021b481d5ea7769e23c82c1364edd19
mittwald <>
2024-07-09 07:54:34 +0000

    Update from https://github.com/mittwald/flow/commit/6c692011cc915cab8500d04e81af5e74ede9be62

---
+++ storybook/update-field-description/assets/EdgeCases.stories-CJIqonJd.js
@@ -0,0 +1,11 @@
+import{N as t}from"./NumberField-DhhphHNz.js";import{R as o}from"./index-BwDkhjyp.js";import{L as c}from"./Label-nQ0y4l8x.js";import{a as u}from"./chunk-MZXVCX43-CM0pFb8Z.js";import"./FormField.module-B3UvHS1K.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./IconApp-BCpbdHgg.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-CbDEtdyo.js";import"./index-B8XB3FuZ.js";import"./IconWarning-zYrPFMer.js";import"./Form-Dmkgi1d-.js";import"./useFocusable-CKMlalor.js";import"./utils-B-mS0hjG.js";import"./useLabel-BaXqGmeE.js";import"./useLabels-kg7v6Afk.js";import"./Text-C_suByGF.js";import"./Button-BTNOdvWu.js";import"./Wrap-DQq6jo70.js";import"./Text-Bxhza4fE.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./LoadingSpinner-CdrecLs9.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./Button-BTMfZQLF.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-YR3pmEr9.js";import"./Input-DNo_oYh5.js";import"./useFormReset-CfqvoD0r.js";import"./useControlledState-C0C-dzuH.js";import"./Label-DwL17Jrp.js";import"./number-nHrFdSb-.js";import"./useNumberFormatter-DEpQvdBY.js";import"./useEvent-m5sATXdH.js";import"./LiveAnnouncer-AIgN_avV.js";import"./v4-CQkTLCs1.js";const X={title:"Form Controls/NumberField/Edge Cases",component:t,render:l=>o.createElement(t,{onChange:u("onChange"),...l},o.createElement(c,null,"Age"))},r={args:{minValue:5,defaultValue:5}},e={args:{maxValue:5,defaultValue:5}};var m,a,i;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    minValue: 5,
+    defaultValue: 5
+  }
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var p,s,n;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    maxValue: 5,
+    defaultValue: 5
+  }
+}`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const Y=["WithDisabledDecrement","WithDisabledIncrement"];export{r as WithDisabledDecrement,e as WithDisabledIncrement,Y as __namedExportsOrder,X as default};
