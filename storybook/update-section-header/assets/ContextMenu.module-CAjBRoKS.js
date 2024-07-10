86869408428ea6d2bfa11b5166b1c8499e1760f3
mittwald <>
2024-07-10 11:34:52 +0000

    Update from https://github.com/mittwald/flow/commit/d9af5ee3e627e307fc1d515f17dcae439a44e5ea

---
+++ storybook/update-section-header/assets/ContextMenu.module-CAjBRoKS.js
@@ -0,0 +1,1 @@
+const o="flow--context-menu",t="flow--context-menu--section",e="flow--context-menu--popover",n={contextMenu:o,section:t,popover:e};export{n as s};
---
+++ storybook/update-section-header/assets/EdgeCases.stories-dLP04g_B.js
@@ -0,0 +1,10 @@
+import{R as r}from"./index-BwDkhjyp.js";import{P as i}from"./PopoverTrigger-BmMoymuk.js";import{B as a}from"./Button-CeJV07Fm.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import s from"./Default.stories-BGY_NU4x.js";import{$ as g}from"./OverlayTrigger-DBKZn7pn.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./flowComponent-CbDEtdyo.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./index-B8XB3FuZ.js";import"./OverlayArrow-74RQSP_w.js";import"./useFocusable-CKMlalor.js";import"./utils-B-mS0hjG.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./useControlledState-C0C-dzuH.js";import"./IconApp-BCpbdHgg.js";import"./IconWarning-BU9QN_UO.js";import"./Wrap-DQq6jo70.js";import"./Text-Bxhza4fE.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./Text-C_suByGF.js";import"./LoadingSpinner-CwasJxbm.js";import"./Button-BTMfZQLF.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-YR3pmEr9.js";import"./RSPContexts-Bc9pl35m.js";import"./FocusScope-DW-lXgh3.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-kg7v6Afk.js";import"./VisuallyHidden-BSWHffzG.js";import"./context-Bn5j6nR1.js";import"./useSelector-J4AjFox4.js";const N={...s,title:"Overlays/Popover/Edge Cases",component:i},o={render:n=>r.createElement(g,null,r.createElement(a,null,"Trigger popover"),r.createElement(i,{...n,placement:"bottom right"},t.long,t.long,t.long))};var m,e,p;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: props => <Aria.DialogTrigger>
+      <Button>Trigger popover</Button>
+      <Popover {...props} placement="bottom right">
+        {dummyText.long}
+        {dummyText.long}
+        {dummyText.long}
+      </Popover>
+    </Aria.DialogTrigger>
+}`,...(p=(e=o.parameters)==null?void 0:e.docs)==null?void 0:p.source}}};const Q=["LongContent"];export{o as LongContent,Q as __namedExportsOrder,N as default};
