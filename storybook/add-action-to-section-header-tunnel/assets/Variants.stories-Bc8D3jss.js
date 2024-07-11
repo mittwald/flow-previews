ea21f801feda4dd0fdf8527b68c82f81ce69b891
mittwald <>
2024-07-11 06:26:41 +0000

    Update from https://github.com/mittwald/flow/commit/2b1a2ce5a1ff91f68bbdd8f82cd5b52f361d1588

---
+++ storybook/add-action-to-section-header-tunnel/assets/Variants.stories-Bc8D3jss.js
@@ -0,0 +1,16 @@
+import{S as o}from"./StatusBadge-DXxUSh5R.js";import E from"./Default.stories-CeRxR6ai.js";import{R as n}from"./index-BwDkhjyp.js";import"./clsx-B-dksMZM.js";import"./StatusIcon-CghxaSSn.js";import"./IconApp-D9YGKrkS.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./flowComponent-L8FZPC_o.js";import"./index-B8XB3FuZ.js";import"./IconWarning-B77fLmlz.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./Text-CKjZYOWy.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./Wrap-DQq6jo70.js";import"./Text-C_suByGF.js";import"./utils-B-mS0hjG.js";const F={...E,title:"Status/StatusBadge/Status"},e={},s={render:r=>n.createElement(o,{...r},"Success"),args:{status:"success"}},a={render:r=>n.createElement(o,{...r},"Warning"),args:{status:"warning"}},t={render:r=>n.createElement(o,{...r},"Danger"),args:{status:"danger"}};var p,c,m;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var u,i,d;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <StatusBadge {...props}>Success</StatusBadge>,
+  args: {
+    status: "success"
+  }
+}`,...(d=(i=s.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,S,l;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <StatusBadge {...props}>Warning</StatusBadge>,
+  args: {
+    status: "warning"
+  }
+}`,...(l=(S=a.parameters)==null?void 0:S.docs)==null?void 0:l.source}}};var B,f,D;t.parameters={...t.parameters,docs:{...(B=t.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <StatusBadge {...props}>Danger</StatusBadge>,
+  args: {
+    status: "danger"
+  }
+}`,...(D=(f=t.parameters)==null?void 0:f.docs)==null?void 0:D.source}}};const G=["Info","Success","Warning","Danger"];export{t as Danger,e as Info,s as Success,a as Warning,G as __namedExportsOrder,F as default};
