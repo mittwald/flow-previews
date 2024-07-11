da217c8f6b2a56e5a51ba5e5e684def2f8121ce8
mittwald <>
2024-07-11 13:48:59 +0000

    Update from https://github.com/mittwald/flow/commit/164bea9098ac88e79e0a08ce534a4c73c29764a1

---
+++ storybook/list-fixes/assets/EdgeCases.stories-C7nD9pAK.js
@@ -0,0 +1,20 @@
+import{R as e}from"./index-BwDkhjyp.js";import{T as u}from"./Text-CKjZYOWy.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import x from"./Default.stories-sAlCcuh4.js";import{I as d}from"./IllustratedMessage-CWybKTzD.js";import{H as c}from"./Heading-lKANVE1O.js";import{B as g}from"./Button-CE1BCqF-.js";import{I as f}from"./IconApp-D9YGKrkS.js";import"./IconWarning-B77fLmlz.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./clsx-B-dksMZM.js";import"./flowComponent-L8FZPC_o.js";import"./index-B8XB3FuZ.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./Wrap-DQq6jo70.js";import"./Text-C_suByGF.js";import"./utils-B-mS0hjG.js";import"./storyBackgrounds-BHxp8d-8.js";import"./RSPContexts-Bc9pl35m.js";import"./LoadingSpinner-WO6qy2zn.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./Button-BTMfZQLF.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-CKMlalor.js";import"./usePress-YR3pmEr9.js";const G={...x,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,a.medium),e.createElement(u,null,a.long),e.createElement(g,null,"Create app"))},r={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,"No apps installed"),e.createElement(u,null,"Create your first app to start working on your website."),e.createElement(g,null,"Create app")),parameters:{viewport:{defaultViewport:"mobile1"}}};var p,n,m;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var s,l,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(i=(l=r.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const J=["LongTexts","SmallSpace"];export{t as LongTexts,r as SmallSpace,J as __namedExportsOrder,G as default};
---
+++ storybook/list-fixes/assets/EdgeCases.stories-CnWsecKW.js
@@ -0,0 +1,7 @@
+import{m as n,B as d}from"./Default.stories-EgDLRvdm.js";import{R as m}from"./index-BwDkhjyp.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-Ay8P_6g2.js";import"./clsx-B-dksMZM.js";import"./IconApp-D9YGKrkS.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./flowComponent-L8FZPC_o.js";import"./index-B8XB3FuZ.js";import"./IconWarning-B77fLmlz.js";import"./Collection-Dx1XK91-.js";import"./utils-B-mS0hjG.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-YR3pmEr9.js";import"./useFocusable-CKMlalor.js";const C={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(d,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+    </Breadcrumb>
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const N=["LongTexts"];export{e as LongTexts,N as __namedExportsOrder,C as default};
