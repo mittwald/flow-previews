1466726a67c1982d6dcd643b8fcc637af66949d0
mittwald <>
2024-07-17 06:33:51 +0000

    Update from https://github.com/mittwald/flow/commit/c86d161f67f4344aaf9d7efcefdbfb236a294d44

---
+++ storybook-main/assets/EdgeCases.stories-BElo8XTq.js
@@ -0,0 +1,30 @@
+import{S as c}from"./Section-BeFlFaPG.js";import{R as t}from"./index-BwDkhjyp.js";import{H as u}from"./Heading-DVg7wytp.js";import{T as d}from"./Text-5DZcOrRy.js";import{S as l}from"./Switch-DTpt999t.js";import S from"./Default.stories-BGR2Cj9y.js";import{d as g}from"./dummyText-CX_I_Wpl.js";import{H as b}from"./Header-7p3tVUi5.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./flowComponent-DpPOHS38.js";import"./index-B8XB3FuZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Activity-Cov3f-FH.js";import"./ContextMenu.module-68N5lvfm.js";import"./Collection-wxk3VkXl.js";import"./Header-tof3fAA-.js";import"./utils-CcQIssJO.js";import"./RSPContexts-Bc9pl35m.js";import"./EmulatedBoldText-B1t6hs5E.js";import"./Wrap-DQq6jo70.js";import"./Text-DqgvIX4k.js";import"./IconApp-ZB_mDMmk.js";import"./IconWarning-BtAGI7Yn.js";import"./Label-BD5VUJI0.js";import"./useLocalizedStringFormatter-BNeRQeib.js";import"./Label-J_0igrPF.js";import"./filterDOMProps-CeZl_uWj.js";import"./useToggleState-ioH5SgDc.js";import"./useFormReset-CfqvoD0r.js";import"./useFocusable-C-qOUFhv.js";import"./usePress-Bw-L1sLn.js";import"./useControlledState-C0C-dzuH.js";import"./VisuallyHidden-BopbO3Wg.js";import"./TextField-DHcMigna.js";import"./TextFieldBase-C-l-YCDi.js";import"./FormField.module-DQthEUsq.js";import"./Form-CuH0gdtx.js";import"./useLabel-BaXqGmeE.js";import"./useLabels-kg7v6Afk.js";import"./FieldDescription-BSEJ-1b-.js";import"./Input-CmsFj7Gh.js";import"./Link-BjQYOPjl.js";import"./StatusBadge-BYe4E20c.js";import"./StatusIcon-Bf46Ku1G.js";import"./Button-DbqhdQOB.js";import"./LoadingSpinner-CsAgL5A_.js";import"./Button-CoDApBTL.js";import"./Action-D4UJ51Z2.js";import"./context-D0eOVtdN.js";import"./useSelector-43iio57k.js";import"./dynamic-DKDa4OpU.js";import"./ModalTrigger-g-Cxq6re.js";import"./OverlayTrigger-C-T-XlBI.js";import"./OverlayArrow-CscfTk_S.js";import"./number-nHrFdSb-.js";import"./FocusScope-DBQ4F6TM.js";import"./getScrollParent-CcmJp979.js";const Et={...S,title:"Structure/Section/Edge Cases"},e={render:o=>t.createElement(c,{...o},t.createElement(b,null,t.createElement(u,null,g.medium),t.createElement(l,null,"Subscribed")),t.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."))},r={render:o=>t.createElement(c,{...o},t.createElement(b,null,t.createElement(u,null,"Newsletter Subscription"),t.createElement(l,null,"Subscribed")),t.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date.")),parameters:{viewport:{defaultViewport:"mobile1"}}};var i,n,m;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>{dummyText.medium}</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(m=(n=e.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var p,a,s;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter Subscription</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const yt=["WithLongHeading","SmallSpace"];export{r as SmallSpace,e as WithLongHeading,yt as __namedExportsOrder,Et as default};
