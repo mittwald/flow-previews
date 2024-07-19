c9f9c0644e5c2f7b70ab9f628c02e75ba9ce27c3
mittwald <>
2024-07-19 07:22:51 +0000

    Cleanup merged Storybook previews

--- storybook/fix/skeleton/assets/Default.stories-C3a3kL-l.js
+++
@@ -1,7 +0,0 @@
-import{S as o}from"./SkeletonText-DS94kYFE.js";import{R as k}from"./index-BwDkhjyp.js";import{a as g}from"./storyBackgrounds-BHxp8d-8.js";import"./_commonjsHelpers-BosuxZz1.js";const D={title:"Content/SkeletonText",component:o,render:t=>k.createElement(o,{...t}),parameters:{controls:{exclude:["className"]}}},r={},e={parameters:{backgrounds:g}},a={render:t=>k.createElement(o,{...t,width:"200px"})};var s,c,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(c=r.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var m,p,d;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  parameters: {
-    backgrounds: storyBackgroundDark
-  }
-}`,...(d=(p=e.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var u,i,l;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
-  render: props => <SkeletonText {...props} width="200px" />
-}`,...(l=(i=a.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const B=["Default","OnDarkBackground","WithWidth"];export{r as Default,e as OnDarkBackground,a as WithWidth,B as __namedExportsOrder,D as default};
--- storybook/fix/skeleton/assets/EdgeCases.stories-DGmsX59b.js
+++
@@ -1,7 +0,0 @@
-import{m as n,B as d}from"./Default.stories-BnO3eDSM.js";import{R as m}from"./index-BwDkhjyp.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-BjQYOPjl.js";import"./clsx-B-dksMZM.js";import"./IconApp-ZB_mDMmk.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-DwTel3aB.js";import"./index-DVe9vSxq.js";import"./flowComponent-DpPOHS38.js";import"./index-B8XB3FuZ.js";import"./IconWarning-BtAGI7Yn.js";import"./Collection-wxk3VkXl.js";import"./utils-CcQIssJO.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-Bw-L1sLn.js";import"./useFocusable-C-qOUFhv.js";const C={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(d,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const N=["LongTexts"];export{e as LongTexts,N as __namedExportsOrder,C as default};
