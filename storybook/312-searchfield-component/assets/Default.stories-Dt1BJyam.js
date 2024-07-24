1bbbbf1df6d43e80a53858eb46063fc639e4848f
mittwald <>
2024-07-24 11:03:08 +0000

    Update from https://github.com/mittwald/flow/commit/e47d835a3af23f46e78c411c95949383d8e3d810

---
+++ storybook/312-searchfield-component/assets/Default.stories-Dt1BJyam.js
@@ -0,0 +1,17 @@
+import{S as t}from"./SearchField-Bub4IkXp.js";import{R as e}from"./index-Cs7sjTYM.js";import{a as L}from"./chunk-454WOBUV-CM0pFb8Z.js";import{L as W}from"./Label-BiPcLPDW.js";import{F as I}from"./FieldDescription-W_Hg9AsC.js";import{F as C}from"./FormField.module-BSGa-iGQ.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-58HgA8rU.js";import"./index-C8LjR2Bd.js";import"./flowComponent-BePwBSVK.js";import"./index-BMdlDBjA.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Button-D-KqNHp8.js";import"./IconApp-4m04zbQ2.js";import"./IconWarning-CTWky_Q0.js";import"./Wrap-DQq6jo70.js";import"./Text-bxh09eJ5.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bu-3W4UG.js";import"./utils-Ca6JWPXc.js";import"./LoadingSpinner-BHLzaSO_.js";import"./useLocalizedStringFormatter-J1nK3MOF.js";import"./Button-DB69cHYY.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-OJvPCLGe.js";import"./usePress-DiyYDbeg.js";import"./Form-CIOW7CQK.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./Group-CssRjSsF.js";import"./Input-Bzotem6v.js";import"./useFormReset-DlrFI1sj.js";import"./useControlledState-B7K2eYzO.js";import"./Label-D7wSVlkX.js";import"./v4-CQkTLCs1.js";const mr={title:"Form Controls/SearchField",component:t,render:r=>e.createElement(t,{onChange:L("onChange"),...r})},o={},a={args:{isDisabled:!0}},i={render:r=>e.createElement(t,{...r},e.createElement(I,null,"Press enter to search"))},s={render:r=>e.createElement(t,{...r},e.createElement(W,null,"Suche"))},p={render:r=>e.createElement(t,{...r,defaultValue:"test",isInvalid:!0},e.createElement(C,null,"Invalid search value"))};var m,c,n;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(n=(c=o.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var l,d,u;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(u=(d=a.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var h,F,S;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <SearchField {...props}>
+      <FieldDescription>Press enter to search</FieldDescription>
+    </SearchField>
+}`,...(S=(F=i.parameters)==null?void 0:F.docs)==null?void 0:S.source}}};var E,f,D;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <SearchField {...props}>
+      <Label>Suche</Label>
+    </SearchField>
+}`,...(D=(f=s.parameters)==null?void 0:f.docs)==null?void 0:D.source}}};var b,g,v;p.parameters={...p.parameters,docs:{...(b=p.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <SearchField {...props} defaultValue="test" isInvalid>
+      <FieldError>Invalid search value</FieldError>
+    </SearchField>
+}`,...(v=(g=p.parameters)==null?void 0:g.docs)==null?void 0:v.source}}};const cr=["Default","Disabled","WithFieldDescription","WithLabel","WithFieldError"];export{o as Default,a as Disabled,i as WithFieldDescription,p as WithFieldError,s as WithLabel,cr as __namedExportsOrder,mr as default};
