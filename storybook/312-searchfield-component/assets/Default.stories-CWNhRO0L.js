d7dc101380a6fd9f78fae0b5a5d1c31cd220b40c
mittwald <>
2024-07-25 04:14:12 +0000

    Cleanup merged Storybook previews

--- storybook/312-searchfield-component/assets/Default.stories-CWNhRO0L.js
+++
@@ -1,5 +0,0 @@
-import{L as t}from"./Label-BiPcLPDW.js";import{R as c}from"./index-Cs7sjTYM.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-58HgA8rU.js";import"./index-C8LjR2Bd.js";import"./flowComponent-BePwBSVK.js";import"./index-BMdlDBjA.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useLocalizedStringFormatter-J1nK3MOF.js";import"./Label-D7wSVlkX.js";import"./utils-Ca6JWPXc.js";const R={title:"Content/Label",component:t,render:i=>c.createElement(t,{...i},"Label")},r={},e={args:{optional:!0}};var o,a,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var p,m,n;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
-  args: {
-    optional: true
-  }
-}`,...(n=(m=e.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const S=["Default","Optional"];export{r as Default,e as Optional,S as __namedExportsOrder,R as default};
