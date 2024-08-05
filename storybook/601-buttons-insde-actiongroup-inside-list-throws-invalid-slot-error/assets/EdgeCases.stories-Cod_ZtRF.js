53a99a220f8b5d02a017711df898b26025919cdf
mittwald <>
2024-08-05 08:14:44 +0000

    Update from https://github.com/mittwald/flow/commit/492b757aa46a929bae37edd5d4a0ee7d70666fcb

---
+++ storybook/601-buttons-insde-actiongroup-inside-list-throws-invalid-slot-error/assets/EdgeCases.stories-Cod_ZtRF.js
@@ -0,0 +1,20 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{T as u}from"./Text-LHdvXnr0.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import x from"./Default.stories-yMKZDjKd.js";import{I as d}from"./IllustratedMessage-C4d3enDJ.js";import{H as c}from"./Heading-BCPLx9jC.js";import{B as g}from"./Button-B8U8VEt3.js";import{I as f}from"./IconApp-4m04zbQ2.js";import"./IconWarning-CTWky_Q0.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-58HgA8rU.js";import"./index-C8LjR2Bd.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BePwBSVK.js";import"./index-BMdlDBjA.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-Bu-3W4UG.js";import"./utils-Ca6JWPXc.js";import"./storyBackgrounds-BHxp8d-8.js";import"./RSPContexts-BseA8goh.js";import"./LoadingSpinner-BHLzaSO_.js";import"./useLocalizedStringFormatter-J1nK3MOF.js";import"./Button-DB69cHYY.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-OJvPCLGe.js";import"./usePress-DiyYDbeg.js";const G={...x,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,a.medium),e.createElement(u,null,a.long),e.createElement(g,null,"Create app"))},r={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,"No apps installed"),e.createElement(u,null,"Create your first app to start working on your website."),e.createElement(g,null,"Create app")),parameters:{viewport:{defaultViewport:"mobile1"}}};var p,n,m;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var s,l,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(i=(l=r.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const J=["LongTexts","SmallSpace"];export{t as LongTexts,r as SmallSpace,J as __namedExportsOrder,G as default};
