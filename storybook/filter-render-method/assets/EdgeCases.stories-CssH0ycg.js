605b86e1ff41f9e4bd0a74d14ea8e308142e4669
mittwald <>
2024-08-05 13:47:57 +0000

    Update from https://github.com/mittwald/flow/commit/8b53ae3ba740eb69dd08b0c891d570a04f10658d

---
+++ storybook/filter-render-method/assets/EdgeCases.stories-CssH0ycg.js
@@ -0,0 +1,6 @@
+import{H as n}from"./Heading-B3jEV0Oz.js";import p from"./Default.stories-lPNsG58r.js";import"./IconApp-kHJN8BdN.js";import{R as r}from"./index-Cs7sjTYM.js";import{b as i}from"./IconWarning-mV0QuYPn.js";import{d as s}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-58HgA8rU.js";import"./index-C8LjR2Bd.js";import"./flowComponent-DhdNqvi-.js";import"./index-BU4L-DQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./RSPContexts-BseA8goh.js";import"./utils-CgpzwoFP.js";import"./storyBackgrounds-BHxp8d-8.js";const M={...p,title:"Content/Heading/Edge Cases",component:n},e={render:a=>r.createElement(n,{...a},r.createElement(i,null),s.medium)};var o,t,m;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      {dummyText.medium}
+    </Heading>
+}`,...(m=(t=e.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const R=["LongText"];export{e as LongText,R as __namedExportsOrder,M as default};
