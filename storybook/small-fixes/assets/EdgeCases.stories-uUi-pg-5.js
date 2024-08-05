ef55da6a213ad8a4579c7dd0a4404829c8ffbb7e
mittwald <>
2024-08-05 07:50:04 +0000

    Cleanup merged Storybook previews

--- storybook/small-fixes/assets/EdgeCases.stories-uUi-pg-5.js
+++
@@ -1,6 +0,0 @@
-import{L as a}from"./Link-6PEKY5W-.js";import{R as t}from"./index-Cs7sjTYM.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import p from"./Default.stories-D4RYcTKd.js";import"./IconApp-Cq2nF_H2.js";import{e as s}from"./IconWarning-CT4MKmtH.js";import"./PropsContextProvider-58HgA8rU.js";import"./index-C8LjR2Bd.js";import"./clsx-B-dksMZM.js";import"./flowComponent-CmFvo3FE.js";import"./index-BMdlDBjA.js";import"./_commonjsHelpers-BosuxZz1.js";import"./utils-Ca6JWPXc.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-BgSaJexe.js";import"./useFocusable-CMUHH7RC.js";import"./chunk-454WOBUV-CM0pFb8Z.js";import"./v4-CQkTLCs1.js";import"./Text-CKRVNL4F.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-Bu-3W4UG.js";import"./storyBackgrounds-BHxp8d-8.js";const j={...p,title:"Navigation/Link/Edge Cases"},r={render:i=>t.createElement(a,{...i},n.long,t.createElement(s,{"aria-label":"external link"}))};var o,e,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const q=["LongText"];export{r as LongText,q as __namedExportsOrder,j as default};
