743ea571e0d275df100ad69582cc292a168fbe32
mittwald <>
2024-08-07 08:45:55 +0000

    Update from https://github.com/mittwald/flow/commit/a17c4d33be796136b7b30c6f4ed818cf4c100bc9

---
+++ storybook/remove-button-tunnel-to-use-form-in-modal/assets/EdgeCases.stories-QhVs2oxl.js
@@ -0,0 +1,7 @@
+import{m as n,B as p}from"./Default.stories-DwfGQEsd.js";import{R as m}from"./index-Cs7sjTYM.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-DgMD5g1f.js";import"./clsx-B-dksMZM.js";import"./IconApp-C6Ekwtz9.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-8D1yScr8.js";import"./index-BU4L-DQy.js";import"./IconWarning-BmHOAWu0.js";import"./Collection-Ds0ZPgJZ.js";import"./CollectionBuilder-DEbmrEtK.js";import"./Hidden-BsXGu--s.js";import"./utils-CgpzwoFP.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-e9a-mRh7.js";import"./useFocusable-BkYgQ3nW.js";const O={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(p,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+    </Breadcrumb>
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const S=["LongTexts"];export{e as LongTexts,S as __namedExportsOrder,O as default};
