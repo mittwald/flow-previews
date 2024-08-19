9cc233e897650d3d09dc8e86d512b4df589000de
mittwald <>
2024-08-19 04:24:39 +0000

    Update from https://github.com/mittwald/flow/commit/323aea650d04d80da7b1395a2c853921bcebd915

---
+++ storybook/support-action-group-in-list-item/assets/Default.stories-DAiOd998.js
@@ -0,0 +1,29 @@
+import{B as o}from"./Button-szKgIl8e.js";import{R as e}from"./index-Cs7sjTYM.js";import"./IconApp-C6Ekwtz9.js";import{a as b,I as D}from"./IconWarning-Bx6ZD98J.js";import{a as y}from"./chunk-454WOBUV-CM0pFb8Z.js";import{T as P}from"./Text-F433P2hn.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./Wrap-DQq6jo70.js";import"./flowComponent-8D1yScr8.js";import"./index-BU4L-DQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./LoadingSpinner-BlbZWXJk.js";import"./useLocalizedStringFormatter-J1nK3MOF.js";import"./Button-inQNMO4w.js";import"./utils-CgpzwoFP.js";import"./Hidden-BsXGu--s.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-BkYgQ3nW.js";import"./usePress-e9a-mRh7.js";import"./v4-CQkTLCs1.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-BgIWd_-0.js";const re={title:"Actions/Button",component:o,args:{onPress:y("onPress"),variant:"solid",color:"primary",size:"m",isDisabled:!1},argTypes:{color:{control:"inline-radio",options:["primary","accent","secondary","danger","dark","light"]},variant:{control:"inline-radio",options:["plain","solid","soft"]},size:{control:"inline-radio",options:["m","s"]},isDisabled:{control:"boolean"}},parameters:{controls:{exclude:["onPress"]}},render:r=>e.createElement(o,{...r},"Continue/Action")},t={},a={args:{size:"s"}},s={render:r=>e.createElement(o,{...r,"aria-label":"Add to favorites"},e.createElement(b,null))},n={render:r=>e.createElement(o,{...r,"aria-label":"Add to favorites"},e.createElement(b,null)),args:{size:"s"}},i={render:r=>e.createElement(o,{...r},e.createElement(P,null,"Add email address"),e.createElement(D,null))},c={render:r=>e.createElement(o,{...r},e.createElement(P,null,"Add email address"),e.createElement(D,null)),args:{size:"s"}};var m,l,p;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var d,u,g;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "s"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var I,f,A;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <Button {...props} aria-label="Add to favorites">
+      <IconPlus />
+    </Button>
+}`,...(A=(f=s.parameters)==null?void 0:f.docs)==null?void 0:A.source}}};var h,x,E;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <Button {...props} aria-label="Add to favorites">
+      <IconPlus />
+    </Button>,
+  args: {
+    size: "s"
+  }
+}`,...(E=(x=n.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};var S,B,T;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <Button {...props}>
+      <Text>Add email address</Text>
+      <IconChevronDown />
+    </Button>
+}`,...(T=(B=i.parameters)==null?void 0:B.docs)==null?void 0:T.source}}};var v,z,W;c.parameters={...c.parameters,docs:{...(v=c.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <Button {...props}>
+      <Text>Add email address</Text>
+      <IconChevronDown />
+    </Button>,
+  args: {
+    size: "s"
+  }
+}`,...(W=(z=c.parameters)==null?void 0:z.docs)==null?void 0:W.source}}};const oe=["Default","Small","WithIcon","SmallWithIcon","WithTextAndIcon","SmallWithTextAndIcon"];export{t as Default,a as Small,n as SmallWithIcon,c as SmallWithTextAndIcon,s as WithIcon,i as WithTextAndIcon,oe as __namedExportsOrder,re as default};
