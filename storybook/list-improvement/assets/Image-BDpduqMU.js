6d2ee7583cfc78f872097e1e2451757f58fb144d
mittwald <>
2024-09-02 13:25:22 +0000

    Cleanup merged Storybook previews

--- storybook/list-improvement/assets/Image-BDpduqMU.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./index-Cs7sjTYM.js";import{C as m}from"./PropsContextProvider-BmJWDS_p.js";import{f as a}from"./flowComponent-B5Zp4U0D.js";const n=a("Image",o=>{const{refProp:r,...t}=o;return e.createElement(m,null,e.createElement("img",{...t,ref:r}))});n.__docgenInfo={description:"",methods:[],displayName:"Image"};export{n as I};
