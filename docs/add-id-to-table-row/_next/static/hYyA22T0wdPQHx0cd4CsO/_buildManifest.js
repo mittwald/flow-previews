c7998de476b16ceafff2d716e768b74dd0fd0de0
mittwald <>
2024-09-12 06:08:06 +0000

    Cleanup merged docs previews

--- docs/add-id-to-table-row/_next/static/hYyA22T0wdPQHx0cd4CsO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-8469a62d3dc4913c.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
