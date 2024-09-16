3872ee998b32714ceb54925e7a86805d669aead7
mittwald <>
2024-09-16 10:05:42 +0000

    Update from https://github.com/mittwald/flow/commit/4c7774e682e94563c7b61210e2d47bb4bf907533

---
+++ storybook/save-list-filters/assets/Default.stories-BwyEPqAe.js
@@ -0,0 +1,23 @@
+import{C as m}from"./CheckboxButton-D0cf-Rxc.js";import{R as n}from"./index-Cs7sjTYM.js";import{a as B}from"./chunk-454WOBUV-CM0pFb8Z.js";import{C as R}from"./Content-BU4cnPsD.js";import{T as W}from"./Text-DJF2XnfV.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./Checkbox-BI_J0btz.js";import"./IconApp-lopuvbZk.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-vONTWrQR.js";import"./index-BU4L-DQy.js";import"./IconWarning-DhKn4xeP.js";import"./RSPContexts-BseA8goh.js";import"./utils-CgpzwoFP.js";import"./Form-DAtDAkn6.js";import"./useFocusable-DdFCD-YN.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./Text-BgIWd_-0.js";import"./Label-BRZ8BO2K.js";import"./Hidden-BsXGu--s.js";import"./filterDOMProps-CeZl_uWj.js";import"./useControlledState-B7K2eYzO.js";import"./useToggleState-BuCL2c8C.js";import"./useFormReset-DlrFI1sj.js";import"./usePress-C9_maMb0.js";import"./VisuallyHidden-Bx6BLr_2.js";import"./v4-CQkTLCs1.js";import"./Wrap-DQq6jo70.js";import"./Activity-CPnWONG0.js";import"./EmulatedBoldText-Dmze-Iwg.js";const pe={title:"Form Controls/CheckboxButton",component:m,args:{onChange:B("onChange")},parameters:{controls:{exclude:["onChange"]}},render:i=>n.createElement(m,{...i},"Consent to terms and conditions")},e={},r={args:{isDisabled:!0}},t={args:{isIndeterminate:!0}},o={args:{isDisabled:!0,isSelected:!0}},s={render:i=>n.createElement(m,{...i},n.createElement(W,null,"Terms and Conditions"),n.createElement(R,null,"Consent to terms and conditions"))},a={args:{isInvalid:!0}};var p,c,d;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(d=(c=e.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,l,C;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(C=(l=r.parameters)==null?void 0:l.docs)==null?void 0:C.source}}};var g,b,x;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isIndeterminate: true
+  }
+}`,...(x=(b=t.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var D,S,h;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    isSelected: true
+  }
+}`,...(h=(S=o.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};var f,I,T;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <CheckboxButton {...props}>
+      <Text>Terms and Conditions</Text>
+      <Content>Consent to terms and conditions</Content>
+    </CheckboxButton>
+}`,...(T=(I=s.parameters)==null?void 0:I.docs)==null?void 0:T.source}}};var E,k,v;a.parameters={...a.parameters,docs:{...(E=a.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    isInvalid: true
+  }
+}`,...(v=(k=a.parameters)==null?void 0:k.docs)==null?void 0:v.source}}};const ce=["Default","Disabled","Indeterminate","DisabledSelected","WithContent","Invalid"];export{e as Default,r as Disabled,o as DisabledSelected,t as Indeterminate,a as Invalid,s as WithContent,ce as __namedExportsOrder,pe as default};
---
+++ storybook/save-list-filters/assets/Default.stories-eJJrbwwy.js
@@ -0,0 +1,78 @@
+import{S as o}from"./Section-t4bW1llv.js";import{R as e}from"./index-Cs7sjTYM.js";import{H as n}from"./Heading-DPteghjl.js";import{T as a}from"./Text-DJF2XnfV.js";import{T as c}from"./TextField-BhO7Xur3.js";import{L as u}from"./Label-lL6r_vY4.js";import{L as T}from"./Link-Dqk-L9wG.js";import{S as B}from"./Switch-COH3Bx-4.js";import{S as d}from"./StatusBadge-DDZ_032_.js";import{H as s}from"./Header-C40UChvd.js";import"./IconApp-lopuvbZk.js";import{b as v,i as C}from"./IconWarning-DhKn4xeP.js";import{B as r}from"./Button-GE8-fgN-.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import{A as f}from"./Action-BXMJuGo7.js";import{M as g}from"./ModalTrigger-9nYuENec.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-vONTWrQR.js";import"./index-BU4L-DQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Activity-CPnWONG0.js";import"./ContextMenu.module-68N5lvfm.js";import"./Collection-D5YsYnk7.js";import"./CollectionBuilder-bH4u-dJi.js";import"./Hidden-BsXGu--s.js";import"./Header-8TqMsGve.js";import"./utils-CgpzwoFP.js";import"./Heading-2-efPYe9.js";import"./RSPContexts-BseA8goh.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-BgIWd_-0.js";import"./TextFieldBase-CFxq25lk.js";import"./FormField.module-UIiSkr8K.js";import"./Form-DAtDAkn6.js";import"./useFocusable-DdFCD-YN.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./FieldDescription-4n_2qPAu.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./Input-BPUwJsxH.js";import"./Label-BRZ8BO2K.js";import"./filterDOMProps-CeZl_uWj.js";import"./useTextField-8-06sg4o.js";import"./useFormReset-DlrFI1sj.js";import"./useControlledState-B7K2eYzO.js";import"./usePress-C9_maMb0.js";import"./useToggleState-BuCL2c8C.js";import"./VisuallyHidden-Bx6BLr_2.js";import"./StatusIcon-DbaJuoem.js";import"./LoadingSpinner-kEEe5Erh.js";import"./Button-B_pBO4HW.js";import"./context-C-ovOQEn.js";import"./useSelector-BTkA6qJI.js";import"./dynamic-DKDa4OpU.js";import"./OverlayTrigger-C6xLtrSv.js";import"./OverlayArrow-B9aGSeR2.js";import"./number-nHrFdSb-.js";import"./FocusScope-jwB0dEBj.js";import"./getScrollParent-CcmJp979.js";const Ue={title:"Structure/Section",component:o,render:t=>e.createElement(o,{...t},e.createElement(n,null,"Newsletter"),e.createElement(a,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."),e.createElement(T,{href:"#"},"Subscribe"))},i={},m={render:t=>e.createElement(e.Fragment,null,e.createElement(o,{...t},e.createElement(n,null,e.createElement(v,null),"Personal Information"),e.createElement(c,{isRequired:!0,defaultValue:"John"},e.createElement(u,null,"First name")),e.createElement(c,{isRequired:!0,defaultValue:"Doe"},e.createElement(u,null,"Last name"))),e.createElement(o,{...t},e.createElement(n,null,"Newsletter"),e.createElement(a,null,"Upcoming releases, new features and tips about your hosting - we bring you the most important information in your inbox. Subscribe to our newsletter and stay up to date."),e.createElement(T,{href:"#"},"Subscribe")))},l={render:t=>e.createElement(e.Fragment,null,e.createElement(o,{...t},e.createElement(s,null,e.createElement(n,null,"Newsletter ",e.createElement(d,null,"Subscribed")),e.createElement(B,{defaultSelected:!0},"Subscription"),e.createElement(r,{variant:"soft",color:"secondary"},"Start database migration")),e.createElement(a,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date.")),e.createElement(o,{...t},e.createElement(s,null,e.createElement(n,null,"My Project with a looooooooooong name",e.createElement(d,{status:"danger"},"Deactivated")),e.createElement(f,null,e.createElement(g,{slot:"actionConfirm"},e.createElement(n,null,"Confirmation Modal")),e.createElement(r,{variant:"soft",color:"secondary"},"Start database migration")),e.createElement(f,null,e.createElement(g,{slot:"actionConfirm"},e.createElement(n,null,"Confirmation Modal")),e.createElement(r,{color:"accent"},"Activate"))),e.createElement(a,null,p.medium)),e.createElement(o,{...t},e.createElement(s,null,e.createElement(n,null,"My Project"),e.createElement(r,{variant:"soft",color:"secondary"},e.createElement(C,null)),e.createElement(r,{color:"danger"},"Deactivate")),e.createElement(a,null,p.medium)))};var b,S,E;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:"{}",...(E=(S=i.parameters)==null?void 0:S.docs)==null?void 0:E.source}}};var y,x,H;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props}>
+        <Heading>
+          <IconMember />
+          Personal Information
+        </Heading>
+        <TextField isRequired defaultValue="John">
+          <Label>First name</Label>
+        </TextField>
+        <TextField isRequired defaultValue="Doe">
+          <Label>Last name</Label>
+        </TextField>
+      </Section>
+      <Section {...props}>
+        <Heading>Newsletter</Heading>
+        <Text>
+          Upcoming releases, new features and tips about your hosting - we bring
+          you the most important information in your inbox. Subscribe to our
+          newsletter and stay up to date.
+        </Text>
+        <Link href="#">Subscribe</Link>
+      </Section>
+    </>
+}`,...(H=(x=m.parameters)==null?void 0:x.docs)==null?void 0:H.source}}};var w,h,M;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props}>
+        <Header>
+          <Heading>
+            Newsletter <StatusBadge>Subscribed</StatusBadge>
+          </Heading>
+
+          <Switch defaultSelected>Subscription</Switch>
+          <Button variant="soft" color="secondary">
+            Start database migration
+          </Button>
+        </Header>
+        <Text>
+          Upcoming releases, new features and tips about your hosting - we bring
+          the most important information to inbox. Subscribe to our newsletter
+          and stay up to date.
+        </Text>
+      </Section>
+      <Section {...props}>
+        <Header>
+          <Heading>
+            My Project with a looooooooooong name
+            <StatusBadge status="danger">Deactivated</StatusBadge>
+          </Heading>
+          <Action>
+            <Modal slot="actionConfirm">
+              <Heading>Confirmation Modal</Heading>
+            </Modal>
+            <Button variant="soft" color="secondary">
+              Start database migration
+            </Button>
+          </Action>
+          <Action>
+            <Modal slot="actionConfirm">
+              <Heading>Confirmation Modal</Heading>
+            </Modal>
+            <Button color="accent">Activate</Button>
+          </Action>
+        </Header>
+        <Text>{dummyText.medium}</Text>
+      </Section>
+      <Section {...props}>
+        <Header>
+          <Heading>My Project</Heading>
+          <Button variant="soft" color="secondary">
+            <IconContextMenu />
+          </Button>
+          <Button color="danger">Deactivate</Button>
+        </Header>
+        <Text>{dummyText.medium}</Text>
+      </Section>
+    </>
+}`,...(M=(h=l.parameters)==null?void 0:h.docs)==null?void 0:M.source}}};const je=["Default","MultipleSections","WithHeaderContent"];export{i as Default,m as MultipleSections,l as WithHeaderContent,je as __namedExportsOrder,Ue as default};
