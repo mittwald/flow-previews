a2f7f618ff8aaf512e03ea6a4a74400245e46fad
mittwald <>
2024-09-19 15:21:24 +0000

    Update from https://github.com/mittwald/flow/commit/e99fcc08a6dea64cf83752958bd88b5d6435bfb3

---
+++ storybook/fix/out-of-memory-bug-of-tabs/assets/Default.stories-BjmojC8t.js
@@ -0,0 +1,23 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{D as s}from"./DateRangePicker-Dm-hi0YE.js";import{L as m}from"./Label-CkNEJYRB.js";import{F as k}from"./FieldDescription-wuh1vDL7.js";import{$ as E,a as L,b as p}from"./CalendarHeader-DNKxPB50.js";import"./_commonjsHelpers-BosuxZz1.js";import"./FormField.module-BJIQTW9h.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./IconApp-C8WE169t.js";import"./flowComponent-CvwRENnB.js";import"./index-BU4L-DQy.js";import"./IconWarning-3CE2c5rz.js";import"./Form-DAtDAkn6.js";import"./useFocusable-DdFCD-YN.js";import"./utils-CgpzwoFP.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./Text-BgIWd_-0.js";import"./PopoverTrigger-DiQdNu3C.js";import"./OverlayTrigger-DD33fYz0.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-B9aGSeR2.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./useControlledState-B7K2eYzO.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-C9_maMb0.js";import"./FocusScope-jwB0dEBj.js";import"./Hidden-BsXGu--s.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-Bx6BLr_2.js";import"./context-CXlEaGVI.js";import"./useSelector-CJePwlsu.js";import"./Button-BIheqNcJ.js";import"./Wrap-DQq6jo70.js";import"./Text-OA-i8KWP.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./LoadingSpinner-DOyTA3Hv.js";import"./Button-B_pBO4HW.js";import"./Group-yf_Q-t6C.js";import"./Label-BRZ8BO2K.js";import"./LiveAnnouncer-AIgN_avV.js";import"./useUpdateEffect-CBT5oQVW.js";import"./useEvent-Be9f7BQw.js";import"./useCollator-BmGy_8lF.js";import"./Input-BPUwJsxH.js";import"./useFormReset-DlrFI1sj.js";import"./useSpinButton-Bb28Ki-W.js";import"./NumberFormatter-DNR9MAW-.js";import"./Heading-2-efPYe9.js";const Le={title:"Form Controls/DateRangePicker",component:s,render:r=>e.createElement(s,{...r,isRequired:!0},e.createElement(m,null,"Date")),parameters:{controls:{exclude:["errorMessage"]}}},t={},a={args:{isDisabled:!0}},o={args:{minValue:E(L())},render:r=>e.createElement(s,{isRequired:!0,defaultValue:{start:p("2012-07-03"),end:p("2012-07-04")},...r,isInvalid:!0,errorMessage:"Date is in the past"},e.createElement(m,null,"Future Date"))},i={args:{minValue:E(L())},render:r=>e.createElement(s,{isRequired:!0,...r},e.createElement(m,null,"Future Date"),e.createElement(k,null,"Select a future date"))};var n,c,l;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,d,D;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(D=(d=a.parameters)==null?void 0:d.docs)==null?void 0:D.source}}};var f,g,b;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DateRangePicker isRequired defaultValue={{
+    start: parseDate("2012-07-03"),
+    end: parseDate("2012-07-04")
+  }} {...props} isInvalid errorMessage="Date is in the past">
+      <Label>Future Date</Label>
+    </DateRangePicker>
+}`,...(b=(g=o.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var R,F,$;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DateRangePicker isRequired {...props}>
+      <Label>Future Date</Label>
+      <FieldDescription>Select a future date</FieldDescription>
+    </DateRangePicker>
+}`,...($=(F=i.parameters)==null?void 0:F.docs)==null?void 0:$.source}}};const ke=["Default","Disabled","Invalid","FutureDatesOnly"];export{t as Default,a as Disabled,i as FutureDatesOnly,o as Invalid,ke as __namedExportsOrder,Le as default};
