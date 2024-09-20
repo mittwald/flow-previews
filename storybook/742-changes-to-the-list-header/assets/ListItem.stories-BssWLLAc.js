46f7247eeaa67db94b789c6006517391c7b86382
mittwald <>
2024-09-20 09:22:17 +0000

    Update from https://github.com/mittwald/flow/commit/226cd51d9e0dcdc1ad9d641e37e71d5a7c9352ba

---
+++ storybook/742-changes-to-the-list-header/assets/ListItem.stories-BssWLLAc.js
@@ -0,0 +1,87 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{H as r}from"./Heading-Ct12fHX3.js";import{T as o}from"./Text-OA-i8KWP.js";import{C as a,M as l}from"./ContextMenuTrigger-DDPbhzxf.js";import{m as W,V as m}from"./Default.stories-BVDwOpq7.js";import{A as i}from"./Avatar-Cr4Mko9X.js";import{d as c}from"./dummyText-CX_I_Wpl.js";import{I as p}from"./Image-DmDA31eF.js";import{C as t}from"./Content-DCYQwudB.js";import{S as k}from"./StatusBadge-enXwypK4.js";import{A as R}from"./ActionGroup-rDLUao4V.js";import{B as z}from"./Button-crZof4i3.js";import"./IconApp-C8WE169t.js";import{h as L}from"./IconWarning-CjtEH0ZC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-CvwRENnB.js";import"./index-BU4L-DQy.js";import"./Heading-2-efPYe9.js";import"./RSPContexts-BseA8goh.js";import"./utils-CgpzwoFP.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-BgIWd_-0.js";import"./ContextMenu.module-68N5lvfm.js";import"./PopoverTrigger-DiQdNu3C.js";import"./OverlayTrigger-DD33fYz0.js";import"./OverlayArrow-B9aGSeR2.js";import"./useFocusable-DdFCD-YN.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./useControlledState-B7K2eYzO.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-C9_maMb0.js";import"./FocusScope-jwB0dEBj.js";import"./Hidden-BsXGu--s.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-DMhdkBu1.js";import"./VisuallyHidden-Bx6BLr_2.js";import"./context-CXlEaGVI.js";import"./useSelector-CJePwlsu.js";import"./Action-DLPAjbQp.js";import"./dynamic-DKDa4OpU.js";import"./deepFindOfType-BylCc5Xi.js";import"./react-children-utilities-CdZndyK3.js";import"./Collection-D5YsYnk7.js";import"./CollectionBuilder-bH4u-dJi.js";import"./Header-8TqMsGve.js";import"./Separator-BGQky6UT.js";import"./useMenuTrigger-BOwmIx5X.js";import"./SelectionManager-DV2VTso8.js";import"./useEvent-Be9f7BQw.js";import"./useCollator-BmGy_8lF.js";import"./TooltipTrigger-6kBqAEYW.js";import"./SearchField-CCamYJGd.js";import"./FormField.module-CZy45to7.js";import"./Form-DAtDAkn6.js";import"./useLabel-D8UVq-XF.js";import"./Button-B_pBO4HW.js";import"./Group-yf_Q-t6C.js";import"./Input-BPUwJsxH.js";import"./Label-BRZ8BO2K.js";import"./useTextField-8-06sg4o.js";import"./useFormReset-DlrFI1sj.js";import"./Section-BEJcH2_B.js";import"./Activity-CPnWONG0.js";import"./getContainerBreakpointSizeClassName-BiU6pQS0.js";import"./datetime-nQE3q1jc.js";import"./SkeletonText-CPbQ-PYc.js";import"./DragAndDrop-Bssnb-st.js";import"./ListBox-BV_2eL2f.js";import"./useListState-BEdF7Q_Y.js";import"./TableRow-n3J8sXmh.js";import"./LiveAnnouncer-AIgN_avV.js";import"./useUpdateEffect-CBT5oQVW.js";import"./useHasTabbableChild-EvJDk9Y7.js";import"./IllustratedMessage-ClPY7BHS.js";import"./Skeleton-815pubIr.js";import"./StatusIcon-DpHuJbur.js";import"./LoadingSpinner-DIu9Kgi6.js";const n=g=>e.createElement("div",{style:{display:"flex",width:"100%",height:"100%",border:"1px dashed purple",color:"purple",alignItems:"center",justifyContent:"center",borderRadius:"4px",padding:"8px"}},e.createElement(o,null,g.children)),ht={...W,title:"Structure/List/ListItem",decorators:[g=>e.createElement("div",null,g())],render:()=>e.createElement(m,null,e.createElement(i,null,e.createElement(p,{alt:"John Doe",src:c.imageSrc})),e.createElement(r,null,"John Doe ",e.createElement(k,{status:"danger"},"Gesperrt")),e.createElement(o,null,"Mittwald"),e.createElement(a,null,e.createElement(l,null,"Show details")))},s={},u={render:()=>e.createElement(m,null,e.createElement(i,null,e.createElement(p,{alt:"John Doe",src:c.imageSrc})),e.createElement(r,null,"John Doe"),e.createElement(o,null,"Mittwald"),e.createElement(t,null,e.createElement(n,null,"Top content")),e.createElement(a,null,e.createElement(l,null,"Show details")))},d={render:()=>e.createElement(m,null,e.createElement(i,null,e.createElement(p,{alt:"John Doe",src:c.imageSrc})),e.createElement(r,null,"John Doe"),e.createElement(o,null,"Mittwald"),e.createElement(t,{slot:"top"},e.createElement(n,null,"Top content")),e.createElement(t,{slot:"bottom"},e.createElement(n,null,"Bottom content")),e.createElement(a,null,e.createElement(l,null,"Show details")))},E={render:()=>e.createElement(m,null,e.createElement(i,null,e.createElement(p,{alt:"John Doe",src:c.imageSrc})),e.createElement(r,null,"John Doe"),e.createElement(o,null,"Mittwald"),e.createElement(t,{slot:"top"},e.createElement(n,null,"Top content")),e.createElement(t,{slot:"bottom"},e.createElement(n,null,"Bottom content")),e.createElement(a,null,e.createElement(l,null,"Show details"))),parameters:{viewport:{defaultViewport:"mobile1"}}},h={render:()=>e.createElement(m,{containerBreakpointSize:"s"},e.createElement(i,null,e.createElement(p,{alt:"John Doe",src:c.imageSrc})),e.createElement(r,null,"John Doe"),e.createElement(o,null,"Mittwald"),e.createElement(t,{slot:"top"},e.createElement(n,null,"Top content")),e.createElement(t,{slot:"bottom"},e.createElement(n,null,"Bottom content")),e.createElement(a,null,e.createElement(l,null,"Show details")))},C={render:()=>e.createElement(m,null,e.createElement(i,null,e.createElement(p,{alt:"John Doe",src:c.imageSrc})),e.createElement(r,null,"John Doe"),e.createElement(o,null,"Mittwald"),e.createElement(R,null,e.createElement(z,{color:"secondary",variant:"soft",slot:"secondary"},e.createElement(L,null)),e.createElement(a,null,e.createElement(l,null,"Show details"))))};var S,w,x;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:"{}",...(x=(w=s.parameters)==null?void 0:w.docs)==null?void 0:x.source}}};var M,T,D;u.parameters={...u.parameters,docs:{...(M=u.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: () => <View>
+      <Avatar>
+        <Image alt="John Doe" src={dummyText.imageSrc} />
+      </Avatar>
+      <Heading>John Doe</Heading>
+      <Text>Mittwald</Text>
+      <Content>
+        <ContentPlaceholder>Top content</ContentPlaceholder>
+      </Content>
+      <ContextMenu>
+        <MenuItem>Show details</MenuItem>
+      </ContextMenu>
+    </View>
+}`,...(D=(T=u.parameters)==null?void 0:T.docs)==null?void 0:D.source}}};var f,I,J;d.parameters={...d.parameters,docs:{...(f=d.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: () => <View>
+      <Avatar>
+        <Image alt="John Doe" src={dummyText.imageSrc} />
+      </Avatar>
+      <Heading>John Doe</Heading>
+      <Text>Mittwald</Text>
+      <Content slot="top">
+        <ContentPlaceholder>Top content</ContentPlaceholder>
+      </Content>
+      <Content slot="bottom">
+        <ContentPlaceholder>Bottom content</ContentPlaceholder>
+      </Content>
+      <ContextMenu>
+        <MenuItem>Show details</MenuItem>
+      </ContextMenu>
+    </View>
+}`,...(J=(I=d.parameters)==null?void 0:I.docs)==null?void 0:J.source}}};var A,v,B;E.parameters={...E.parameters,docs:{...(A=E.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => <View>
+      <Avatar>
+        <Image alt="John Doe" src={dummyText.imageSrc} />
+      </Avatar>
+      <Heading>John Doe</Heading>
+      <Text>Mittwald</Text>
+      <Content slot="top">
+        <ContentPlaceholder>Top content</ContentPlaceholder>
+      </Content>
+      <Content slot="bottom">
+        <ContentPlaceholder>Bottom content</ContentPlaceholder>
+      </Content>
+      <ContextMenu>
+        <MenuItem>Show details</MenuItem>
+      </ContextMenu>
+    </View>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(B=(v=E.parameters)==null?void 0:v.docs)==null?void 0:B.source}}};var P,V,y;h.parameters={...h.parameters,docs:{...(P=h.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: () => <View containerBreakpointSize="s">
+      <Avatar>
+        <Image alt="John Doe" src={dummyText.imageSrc} />
+      </Avatar>
+      <Heading>John Doe</Heading>
+      <Text>Mittwald</Text>
+      <Content slot="top">
+        <ContentPlaceholder>Top content</ContentPlaceholder>
+      </Content>
+      <Content slot="bottom">
+        <ContentPlaceholder>Bottom content</ContentPlaceholder>
+      </Content>
+      <ContextMenu>
+        <MenuItem>Show details</MenuItem>
+      </ContextMenu>
+    </View>
+}`,...(y=(V=h.parameters)==null?void 0:V.docs)==null?void 0:y.source}}};var H,b,G;C.parameters={...C.parameters,docs:{...(H=C.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: () => <View>
+      <Avatar>
+        <Image alt="John Doe" src={dummyText.imageSrc} />
+      </Avatar>
+      <Heading>John Doe</Heading>
+      <Text>Mittwald</Text>
+      <ActionGroup>
+        <Button color="secondary" variant="soft" slot="secondary">
+          <IconStar />
+        </Button>
+        <ContextMenu>
+          <MenuItem>Show details</MenuItem>
+        </ContextMenu>
+      </ActionGroup>
+    </View>
+}`,...(G=(b=C.parameters)==null?void 0:b.docs)==null?void 0:G.source}}};const Ct=["Default","WithTopContent","WithContent","SmallSpace","CustomContainerBreakpoint","WithActionGroup"];export{h as CustomContainerBreakpoint,s as Default,E as SmallSpace,C as WithActionGroup,d as WithContent,u as WithTopContent,Ct as __namedExportsOrder,ht as default};
