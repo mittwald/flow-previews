3e5e3578286ad386374e82b57429945f99566173
mittwald <>
2024-09-20 09:06:59 +0000

    Update from https://github.com/mittwald/flow/commit/1aa63363fdd181b62df09ffe7917d26c237043d9

--- storybook-main/assets/Default.stories-CcE_-pxX.js
+++
@@ -1,9 +0,0 @@
-import{S as t}from"./Switch-G_8-Zqqa.js";import{R as u}from"./index-Cs7sjTYM.js";import{a as b}from"./chunk-D5ZWXAHU-CGElDDNX.js";import"./clsx-B-dksMZM.js";import"./IconApp-BxFtnrXv.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-DcwGE90j.js";import"./index-Cf-03bMR.js";import"./IconWarning-D_3gEtfh.js";import"./Label-CEl6Xbkq.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./Label-D5u8zl72.js";import"./utils-DI5tH0Ou.js";import"./Hidden-DMUPAT4b.js";import"./filterDOMProps-CeZl_uWj.js";import"./useToggleState-pg2EuAp2.js";import"./useFormReset-DlrFI1sj.js";import"./useFocusable-BZYwbtlG.js";import"./usePress-DMpbNI7r.js";import"./useControlledState-B7K2eYzO.js";import"./VisuallyHidden-D3JK2BHS.js";import"./v4-CQkTLCs1.js";const H={title:"Form Controls/Switch",component:t,args:{onChange:b("onChange"),labelPosition:"trailing"},parameters:{controls:{exclude:["onChange"]}},argTypes:{labelPosition:{control:"inline-radio",options:["trailing","leading"]}},render:g=>u.createElement(t,{...g},"Autoresponder")},r={},o={args:{labelPosition:"leading"}},e={args:{isDisabled:!0}};var a,i,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(s=(i=r.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var n,m,p;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  args: {
-    labelPosition: "leading"
-  }
-}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var c,l,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    isDisabled: true
-  }
-}`,...(d=(l=e.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const I=["Default","LabelLeading","Disabled"];export{r as Default,e as Disabled,o as LabelLeading,I as __namedExportsOrder,H as default};
--- storybook-main/assets/EdgeCases.stories-DuCi5AWK.js
+++
@@ -1,7 +0,0 @@
-import{m as n,B as p}from"./Default.stories-r5yO_x48.js";import{R as m}from"./index-Cs7sjTYM.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-TA4WAiD3.js";import"./clsx-B-dksMZM.js";import"./IconApp-BxFtnrXv.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-DcwGE90j.js";import"./index-Cf-03bMR.js";import"./IconWarning-D_3gEtfh.js";import"./Collection-BSVICx61.js";import"./CollectionBuilder-C1pJ_vaJ.js";import"./Hidden-DMUPAT4b.js";import"./utils-DI5tH0Ou.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-DMpbNI7r.js";import"./useFocusable-BZYwbtlG.js";const O={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(p,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const S=["LongTexts"];export{e as LongTexts,S as __namedExportsOrder,O as default};
