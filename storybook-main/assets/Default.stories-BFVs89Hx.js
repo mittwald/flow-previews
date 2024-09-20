b2f801106ad6f108a4f2ef3037cf26eab18b3cf6
mittwald <>
2024-09-20 06:48:50 +0000

    Update from https://github.com/mittwald/flow/commit/1a9d7f0ee7943068d2fa1dcf43a60f31fc30461a

---
+++ storybook-main/assets/Default.stories-BFVs89Hx.js
@@ -0,0 +1,21 @@
+import{L as n}from"./Label-CkNEJYRB.js";import{R as e}from"./index-Cs7sjTYM.js";import{B as f}from"./Button-BIheqNcJ.js";import{a as L,C as E}from"./ContextualHelpTrigger-Dd-YTDVt.js";import{H}from"./Heading-Ct12fHX3.js";import{T as i}from"./Text-OA-i8KWP.js";import{L as T}from"./Link-7vHvCDau.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-CvwRENnB.js";import"./index-BU4L-DQy.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./Label-BRZ8BO2K.js";import"./utils-CgpzwoFP.js";import"./Hidden-BsXGu--s.js";import"./IconApp-C8WE169t.js";import"./IconWarning-3CE2c5rz.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-DOyTA3Hv.js";import"./Button-B_pBO4HW.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-DdFCD-YN.js";import"./usePress-C9_maMb0.js";import"./PopoverTrigger-DiQdNu3C.js";import"./OverlayTrigger-DD33fYz0.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-B9aGSeR2.js";import"./number-nHrFdSb-.js";import"./useControlledState-B7K2eYzO.js";import"./FocusScope-jwB0dEBj.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-DMhdkBu1.js";import"./VisuallyHidden-Bx6BLr_2.js";import"./context-CXlEaGVI.js";import"./useSelector-CJePwlsu.js";import"./Heading-2-efPYe9.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-BgIWd_-0.js";const me={title:"Content/Label",component:n,render:r=>e.createElement(n,{...r},"Label")},t={},o={args:{optional:!0}},a={render:r=>e.createElement(n,{...r},e.createElement(i,null,"Label"),e.createElement(L,null,e.createElement(f,null),e.createElement(E,{...r},e.createElement(H,null,"Rights & roles"),e.createElement(i,null,"Each user profile is assigned a role in mStudio for each project and/or organization. This allows you to work in a completely new and modern way."),e.createElement(T,null,"Learn more"))))};var m,p,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var s,c,u;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    optional: true
+  }
+}`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var d,g,x;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <Label {...props}>
+      <Text>Label</Text>
+      <ContextualHelpTrigger>
+        <Button />
+        <ContextualHelp {...props}>
+          <Heading>Rights & roles</Heading>
+          <Text>
+            Each user profile is assigned a role in mStudio for each project
+            and/or organization. This allows you to work in a completely new and
+            modern way.
+          </Text>
+          <Link>Learn more</Link>
+        </ContextualHelp>
+      </ContextualHelpTrigger>
+    </Label>
+}`,...(x=(g=a.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const pe=["Default","Optional","WithContextualHelp"];export{t as Default,o as Optional,a as WithContextualHelp,pe as __namedExportsOrder,me as default};
