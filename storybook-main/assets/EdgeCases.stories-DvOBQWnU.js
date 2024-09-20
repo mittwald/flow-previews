3449b92bf92a4f053d36fcb74dbec85bd54821bb
mittwald <>
2024-09-20 08:48:55 +0000

    Update from https://github.com/mittwald/flow/commit/7a555e32ac8c35e71b770d809a568a0326bcf1f2

--- storybook-main/assets/EdgeCases.stories-DvOBQWnU.js
+++
@@ -1,6 +0,0 @@
-import{L as a}from"./Link-BmdpRAhu.js";import{R as t}from"./index-Cs7sjTYM.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import p from"./Default.stories-BflWipKQ.js";import"./IconApp-FFcOAhSU.js";import{g as s}from"./IconWarning-DVQwOeH0.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./clsx-B-dksMZM.js";import"./flowComponent-B2GRt226.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./utils-DI5tH0Ou.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-DMpbNI7r.js";import"./useFocusable-BZYwbtlG.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./Text-CvyZKomL.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text--Nm9EVqj.js";import"./storyBackgrounds-BHxp8d-8.js";const j={...p,title:"Navigation/Link/Edge Cases"},r={render:i=>t.createElement(a,{...i},n.long,t.createElement(s,{"aria-label":"external link"}))};var o,e,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const q=["LongText"];export{r as LongText,q as __namedExportsOrder,j as default};
---
+++ storybook-main/assets/EdgeCases.stories-KPlMbx-L.js
@@ -0,0 +1,7 @@
+import{m as n,B as p}from"./Default.stories-C2epTC1R.js";import{R as m}from"./index-Cs7sjTYM.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{L as t}from"./Link-CSopS7hG.js";import"./clsx-B-dksMZM.js";import"./IconApp-FFcOAhSU.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-B2GRt226.js";import"./index-Cf-03bMR.js";import"./IconWarning-DVQwOeH0.js";import"./Collection-BSVICx61.js";import"./CollectionBuilder-C1pJ_vaJ.js";import"./Hidden-DMUPAT4b.js";import"./utils-DI5tH0Ou.js";import"./filterDOMProps-CeZl_uWj.js";import"./storyBackgrounds-BHxp8d-8.js";import"./usePress-DMpbNI7r.js";import"./useFocusable-BZYwbtlG.js";const O={...n,title:"Navigation/Breadcrumb/Edge Cases"},e={render:()=>m.createElement(p,null,m.createElement(t,null,r.medium),m.createElement(t,null,r.medium),m.createElement(t,null,r.medium))};var o,i,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Breadcrumb>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+      <Link>{dummyText.medium}</Link>
+    </Breadcrumb>
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const S=["LongTexts"];export{e as LongTexts,S as __namedExportsOrder,O as default};
