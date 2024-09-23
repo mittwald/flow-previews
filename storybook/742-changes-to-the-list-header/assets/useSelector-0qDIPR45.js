3ce55a89b8172ec092fcec476a813b95e45fb80f
mittwald <>
2024-09-23 06:53:27 +0000

    Cleanup merged Storybook previews

--- storybook/742-changes-to-the-list-header/assets/useSelector-0qDIPR45.js
+++
@@ -1,1 +0,0 @@
-import{a as u}from"./flowComponent-D7ioKQ_2.js";import{r as t}from"./index-Cs7sjTYM.js";const n=(e,r=[])=>{const[o,s]=t.useState(e());return t.useEffect(()=>u(()=>{s(e())}),r),o};export{n as u};
