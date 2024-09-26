fdafca62b811170483e766a510c765915f4f28c9
mittwald <>
2024-09-26 09:48:07 +0000

    Update from https://github.com/mittwald/flow/commit/6db09cf5b67abe0d7f35058dd13f72b27eb14ad7

---
+++ storybook/add-autogrow-to-text-area/assets/Default.stories-D7JsHpYz.js
@@ -0,0 +1,37 @@
+import{T as a}from"./TextArea-CFfsPkzR.js";import{R as e}from"./index-Cs7sjTYM.js";import{L as s}from"./Label-Cb_5Fm_J.js";import{a as M}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{F as O}from"./FieldDescription-5MbVX17f.js";import{F as j}from"./FormField.module-B8yqGsc9.js";import"./TextFieldBase-BRwWE7jh.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./utils-BBsc42SX.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Form-BJmzrAR9.js";import"./useFocusRing-CRCkdtud.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./Text-C0gOxGaJ.js";import"./Input-BRW8j_n9.js";import"./Hidden-DMUPAT4b.js";import"./Label-DgAfL5vK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useTextField-BfWoRbPu.js";import"./useFormReset-DlrFI1sj.js";import"./useControlledState-B7K2eYzO.js";import"./useFocusable-D6sODRzj.js";import"./flowComponent-D7ioKQ_2.js";import"./v4-CQkTLCs1.js";import"./Text-DYnmzK5R.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./IconApp-BOae6dD3.js";import"./IconWarning-DMnbQijb.js";const Le={title:"Form Controls/TextArea",component:a,render:r=>e.createElement(a,{onChange:M("onChange"),...r},e.createElement(s,null,"Message"))},t={},o={args:{isDisabled:!0}},i={args:{isRequired:!0}},n={render:r=>e.createElement(a,{...r},e.createElement(s,null,"Public Key"),e.createElement(O,null,"Starts with ssh-rsa"))},c={render:r=>e.createElement(a,{...r,defaultValue:"ssh-rsa asdfkehbahrgvszuagfdashdfgsaghdfiuadsf"},e.createElement(s,null,"Public Key"))},l={render:r=>e.createElement(a,{...r,placeholder:"ssh-rsa..."},e.createElement(s,null,"Public Key"))},m={render:r=>e.createElement(a,{...r,isInvalid:!0,defaultValue:"hello"},e.createElement(s,null,"Public Key"),e.createElement(j,null,"Invalid Key"))},p={args:{showCharacterCount:!0,maxLength:100}},d={args:{rows:1,maxRows:5}};var u,h,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(g=(h=t.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var b,f,E;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(E=(f=o.parameters)==null?void 0:f.docs)==null?void 0:E.source}}};var x,D,F;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(F=(D=i.parameters)==null?void 0:D.docs)==null?void 0:F.source}}};var S,L,C;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <TextArea {...props}>
+      <Label>Public Key</Label>
+      <FieldDescription>Starts with ssh-rsa</FieldDescription>
+    </TextArea>
+}`,...(C=(L=n.parameters)==null?void 0:L.docs)==null?void 0:C.source}}};var T,w,y;c.parameters={...c.parameters,docs:{...(T=c.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} defaultValue="ssh-rsa asdfkehbahrgvszuagfdashdfgsaghdfiuadsf">
+      <Label>Public Key</Label>
+    </TextArea>
+}`,...(y=(w=c.parameters)==null?void 0:w.docs)==null?void 0:y.source}}};var A,K,P;l.parameters={...l.parameters,docs:{...(A=l.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} placeholder="ssh-rsa...">
+      <Label>Public Key</Label>
+    </TextArea>
+}`,...(P=(K=l.parameters)==null?void 0:K.docs)==null?void 0:P.source}}};var R,W,v;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} isInvalid defaultValue="hello">
+      <Label>Public Key</Label>
+      <FieldError>Invalid Key</FieldError>
+    </TextArea>
+}`,...(v=(W=m.parameters)==null?void 0:W.docs)==null?void 0:v.source}}};var V,q,z;p.parameters={...p.parameters,docs:{...(V=p.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    showCharacterCount: true,
+    maxLength: 100
+  }
+}`,...(z=(q=p.parameters)==null?void 0:q.docs)==null?void 0:z.source}}};var I,k,_;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    rows: 1,
+    maxRows: 5
+  }
+}`,...(_=(k=d.parameters)==null?void 0:k.docs)==null?void 0:_.source}}};const Ce=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithPlaceholder","WithFieldError","ShowCharacterCount","Resizeable"];export{t as Default,o as Disabled,i as Required,d as Resizeable,p as ShowCharacterCount,c as WithDefaultValue,n as WithFieldDescription,m as WithFieldError,l as WithPlaceholder,Ce as __namedExportsOrder,Le as default};
