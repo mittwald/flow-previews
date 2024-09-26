07d37f5eb82f721c274578100337c82257eab7f7
mittwald <>
2024-09-26 08:24:22 +0000

    Update from https://github.com/mittwald/flow/commit/3f06402f814df88e04459ffaa204a7703b50d3f2

---
+++ storybook/clean-up-tokens/assets/EdgeCases.stories-DqEwvvhg.js
@@ -0,0 +1,13 @@
+import{I as o}from"./InlineCode-CU6XHDNo.js";import{R as m}from"./index-Cs7sjTYM.js";import{T as d}from"./Text-D9clVBM1.js";import a from"./Default.stories-SQnQNHq8.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import{S as l}from"./Section-DcwKg0kW.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-D7ioKQ_2.js";import"./index-Cf-03bMR.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-KzcdCwWD.js";import"./utils-Bcr9fq-m.js";import"./ContextMenu.module-68N5lvfm.js";import"./Collection-BSVICx61.js";import"./CollectionBuilder-C1pJ_vaJ.js";import"./Hidden-DMUPAT4b.js";import"./Header-R7RwfTAD.js";const k={...a,title:"Content/InlineCode/Edge Cases"},t={render:r=>m.createElement(l,null,m.createElement(d,null,e.medium," ",m.createElement(o,{...r},e.medium.replaceAll(" ",""))," ",e.medium,m.createElement(o,{...r},e.medium)," ",e.medium))};var n,i,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        {dummyText.medium}{" "}
+        <InlineCode {...props}>
+          {dummyText.medium.replaceAll(" ", "")}
+        </InlineCode>{" "}
+        {dummyText.medium}
+        <InlineCode {...props}>{dummyText.medium}</InlineCode>{" "}
+        {dummyText.medium}
+      </Text>
+    </Section>
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const q=["LongText"];export{t as LongText,q as __namedExportsOrder,k as default};
