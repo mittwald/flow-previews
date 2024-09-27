4a7665810098514af7835b938dc28ef53e7ccbb9
mittwald <>
2024-09-27 09:52:43 +0000

    Update from https://github.com/mittwald/flow/commit/47a7ee312f36d73550643bc2e1cc5d91bfb5b5d2

---
+++ storybook/675-centered-table-content/assets/Default.stories-C7JM5sIk.js
@@ -0,0 +1,36 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{T as c,a as E,b as t,c as d,d as n,e as l}from"./TableRow-BxrpQ7PZ.js";import{I as a}from"./InlineCode-CU6XHDNo.js";import{d as C}from"./dummyText-CX_I_Wpl.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./SkeletonText-CPbQ-PYc.js";import"./Button-BknGA9dM.js";import"./utils-BBsc42SX.js";import"./index-C8LjR2Bd.js";import"./index-Cf-03bMR.js";import"./Hidden-DMUPAT4b.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-D6sODRzj.js";import"./useFocusRing-CRCkdtud.js";import"./usePress-Do5SRlXR.js";import"./RSPContexts-BseA8goh.js";import"./Collection-BSVICx61.js";import"./CollectionBuilder-C1pJ_vaJ.js";import"./DragAndDrop-Bssnb-st.js";import"./SelectionManager-D54GM_Uc.js";import"./FocusScope-CGKzeA_Y.js";import"./useEvent-Be9f7BQw.js";import"./useCollator-BmGy_8lF.js";import"./getScrollParent-CcmJp979.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./useControlledState-B7K2eYzO.js";import"./VisuallyHidden-D4oQIDY4.js";import"./LiveAnnouncer-AIgN_avV.js";import"./useUpdateEffect-CBT5oQVW.js";import"./useHasTabbableChild-DAzEVVSA.js";import"./PropsContextProvider-BmJWDS_p.js";const X={title:"Structure/Table",component:c,render:m=>e.createElement(c,{"aria-label":"Button Props",...m},e.createElement(E,null,e.createElement(t,null,"Name"),e.createElement(t,null,"Type"),e.createElement(t,null,"Default"),e.createElement(t,null,"Description")),e.createElement(d,null,e.createElement(n,null,e.createElement(l,null,e.createElement(a,null,"color")),e.createElement(l,null,"primary | accent | secondary | danger"),e.createElement(l,null,"primary"),e.createElement(l,null,"The color of the button")),e.createElement(n,null,e.createElement(l,null,e.createElement(a,null,"variant")),e.createElement(l,null,"plain | solid | soft"),e.createElement(l,null,"solid"),e.createElement(l,null,"The variant of the button")),e.createElement(n,null,e.createElement(l,null,e.createElement(a,null,"size")),e.createElement(l,null,"m | s"),e.createElement(l,null,"m"),e.createElement(l,null,"The size of the button"))))},r={},o={render:m=>e.createElement(c,{"aria-label":"Button Props",...m,verticalAlign:"middle"},e.createElement(E,null,e.createElement(t,null,"Name"),e.createElement(t,null,"Type"),e.createElement(t,null,"Default"),e.createElement(t,null,"Description")),e.createElement(d,null,e.createElement(n,null,e.createElement(l,null,e.createElement(a,null,"color")),e.createElement(l,null,"primary | accent | secondary | danger"),e.createElement(l,null,"primary"),e.createElement(l,null,C.long)),e.createElement(n,null,e.createElement(l,null,e.createElement(a,null,"variant")),e.createElement(l,null,"plain | solid | soft"),e.createElement(l,null,"solid"),e.createElement(l,null,"The variant of the button")),e.createElement(n,null,e.createElement(l,null,e.createElement(a,null,"size")),e.createElement(l,null,"m | s"),e.createElement(l,null,"m"),e.createElement(l,null,"The size of the button"))))};var i,u,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(p=(u=r.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var s,T,b;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <Table aria-label="Button Props" {...props} verticalAlign="middle">
+      <TableHeader>
+        <TableColumn>Name</TableColumn>
+        <TableColumn>Type</TableColumn>
+        <TableColumn>Default</TableColumn>
+        <TableColumn>Description</TableColumn>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <InlineCode>color</InlineCode>
+          </TableCell>
+          <TableCell>primary | accent | secondary | danger</TableCell>
+          <TableCell>primary</TableCell>
+          <TableCell>{dummyText.long}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <InlineCode>variant</InlineCode>
+          </TableCell>
+          <TableCell>plain | solid | soft</TableCell>
+          <TableCell>solid</TableCell>
+          <TableCell>The variant of the button</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <InlineCode>size</InlineCode>
+          </TableCell>
+          <TableCell>m | s</TableCell>
+          <TableCell>m</TableCell>
+          <TableCell>The size of the button</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+}`,...(b=(T=o.parameters)==null?void 0:T.docs)==null?void 0:b.source}}};const Y=["Default","VerticallyCenteredRows"];export{r as Default,o as VerticallyCenteredRows,Y as __namedExportsOrder,X as default};
