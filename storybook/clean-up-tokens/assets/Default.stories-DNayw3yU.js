f5980e0bfb8e8631cbd12aedd71076ca36cb6e83
mittwald <>
2024-09-27 03:53:21 +0000

    Update from https://github.com/mittwald/flow/commit/b86e51059948e0572b4524a863e2cff29b217796

---
+++ storybook/clean-up-tokens/assets/Default.stories-DNayw3yU.js
@@ -0,0 +1,20 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{D as m}from"./DatePicker-C8JVo7WC.js";import{L as s}from"./Label-B95_EtR8.js";import{F as R}from"./FieldDescription-NGzPjDZr.js";import{$ as E,a as L,b as k}from"./DateField-BsPMc41l.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./Button-IhKxSqjT.js";import"./IconApp-BOae6dD3.js";import"./flowComponent-D7ioKQ_2.js";import"./index-Cf-03bMR.js";import"./IconWarning-DMnbQijb.js";import"./Wrap-DQq6jo70.js";import"./Text-D9clVBM1.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-KzcdCwWD.js";import"./utils-Bcr9fq-m.js";import"./LoadingSpinner-BLH-7Wh6.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./Button-BCnEwtbK.js";import"./Hidden-DMUPAT4b.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-B6dIjavV.js";import"./useFocusRing-CRCkdtud.js";import"./usePress-Do5SRlXR.js";import"./Group-KXcft_HN.js";import"./FormField.module-DKr-bq5L.js";import"./Form-Ds7Vg32t.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./PopoverTrigger-DA37s1jF.js";import"./OverlayTrigger-oVkQhLAu.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-ZcRxrH45.js";import"./number-nHrFdSb-.js";import"./useControlledState-B7K2eYzO.js";import"./FocusScope-CGKzeA_Y.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-D4oQIDY4.js";import"./context-Dd6hPQBJ.js";import"./useSelector-0qDIPR45.js";import"./RangeCalendar-DJHI5lxU.js";import"./Label-D_v1lLm2.js";import"./useCollator-BmGy_8lF.js";import"./Heading-CS0RTEE1.js";import"./LiveAnnouncer-AIgN_avV.js";import"./useUpdateEffect-CBT5oQVW.js";import"./useEvent-Be9f7BQw.js";import"./Input-DCO5aFG7.js";import"./useFormReset-DlrFI1sj.js";import"./useSpinButton-DksKbOre.js";import"./NumberFormatter-DNR9MAW-.js";const xe={title:"Form Controls/DatePicker",component:m,render:r=>e.createElement(m,{...r,isRequired:!0},e.createElement(s,null,"Date")),parameters:{controls:{exclude:["errorMessage"]}}},t={},a={args:{isDisabled:!0}},o={args:{minValue:E(L())},render:r=>e.createElement(m,{isRequired:!0,defaultValue:k("2012-07-03"),...r,isInvalid:!0,errorMessage:"Date is in the past"},e.createElement(s,null,"Future Date"))},i={args:{minValue:E(L())},render:r=>e.createElement(m,{isRequired:!0,...r},e.createElement(s,null,"Future Date"),e.createElement(R,null,"Select a future date"))};var p,n,c;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,u,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var D,f,g;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DatePicker isRequired defaultValue={parseDate("2012-07-03")} {...props} isInvalid errorMessage="Date is in the past">
+      <Label>Future Date</Label>
+    </DatePicker>
+}`,...(g=(f=o.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var b,F,$;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DatePicker isRequired {...props}>
+      <Label>Future Date</Label>
+      <FieldDescription>Select a future date</FieldDescription>
+    </DatePicker>
+}`,...($=(F=i.parameters)==null?void 0:F.docs)==null?void 0:$.source}}};const Pe=["Default","Disabled","Invalid","FutureDatesOnly"];export{t as Default,a as Disabled,i as FutureDatesOnly,o as Invalid,Pe as __namedExportsOrder,xe as default};
