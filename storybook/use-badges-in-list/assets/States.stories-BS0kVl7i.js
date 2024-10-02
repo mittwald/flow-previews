18a0f137d71682f87670e9a0c445d9a02937820c
mittwald <>
2024-10-02 04:20:06 +0000

    Update from https://github.com/mittwald/flow/commit/011d9ff0bab740f7fd85951c3c139dc0daa97354

---
+++ storybook/use-badges-in-list/assets/States.stories-BS0kVl7i.js
@@ -0,0 +1,36 @@
+import{B as W}from"./Button-7bMiLlhs.js";import _ from"./Default.stories-DxXCUEKy.js";import"./IconApp-BOae6dD3.js";import{R as r}from"./index-Cs7sjTYM.js";import{I as w,a as C}from"./IconWarning-DMnbQijb.js";import{T as O}from"./Text-DYnmzK5R.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./Wrap-DQq6jo70.js";import"./flowComponent-D7ioKQ_2.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./LoadingSpinner-BLH-7Wh6.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./Button-BknGA9dM.js";import"./utils-BBsc42SX.js";import"./Hidden-DMUPAT4b.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-D6sODRzj.js";import"./useFocusRing-CRCkdtud.js";import"./usePress-Do5SRlXR.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-C0gOxGaJ.js";const or={..._,title:"Actions/Button/States",render:d=>r.createElement(W,{...d},r.createElement(O,null,"Add email address"),r.createElement(w,null))},e={args:{isDisabled:!0}},a={args:{isDisabled:!0,variant:"plain"}},s={args:{isPending:!0}},n={args:{isPending:!0,variant:"plain"}},t={args:{isPending:!0,variant:"plain"},render:d=>r.createElement(W,{...d,"aria-label":"Add to favorites"},r.createElement(C,null))},i={args:{isSucceeded:!0}},o={args:{isFailed:!0,variant:"plain"}};var c,m,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var l,u,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    variant: "plain"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var P,S,v;s.parameters={...s.parameters,docs:{...(P=s.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    isPending: true
+  }
+}`,...(v=(S=s.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var b,D,f;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  }
+}`,...(f=(D=n.parameters)==null?void 0:D.docs)==null?void 0:f.source}}};var E,I,B;t.parameters={...t.parameters,docs:{...(E=t.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  },
+  render: props => <Button {...props} aria-label="Add to favorites">
+      <IconPlus />
+    </Button>
+}`,...(B=(I=t.parameters)==null?void 0:I.docs)==null?void 0:B.source}}};var A,F,h;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    isSucceeded: true
+  }
+}`,...(h=(F=i.parameters)==null?void 0:F.docs)==null?void 0:h.source}}};var x,R,T;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    isFailed: true,
+    variant: "plain"
+  }
+}`,...(T=(R=o.parameters)==null?void 0:R.docs)==null?void 0:T.source}}};const dr=["DisabledSolid","DisabledPlain","PendingSolid","PendingPlain","PendingPlainWithIcon","SucceededSolid","FailedPlain"];export{a as DisabledPlain,e as DisabledSolid,o as FailedPlain,n as PendingPlain,t as PendingPlainWithIcon,s as PendingSolid,i as SucceededSolid,dr as __namedExportsOrder,or as default};
