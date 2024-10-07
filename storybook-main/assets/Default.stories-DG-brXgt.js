6e70a24a9564a2a2789956edad2ed44365d9b423
mittwald <>
2024-10-07 06:05:52 +0000

    Update from https://github.com/mittwald/flow/commit/c18e937b6d157bad508faa9534e97896c2753977

---
+++ storybook-main/assets/Default.stories-DG-brXgt.js
@@ -0,0 +1,41 @@
+import{S as o,O as t}from"./Option-CmwY1gvR.js";import{R as e}from"./index-Cs7sjTYM.js";import{L as s}from"./Label-CV7xN_BM.js";import{F as Y}from"./FieldDescription-CpxHb4F0.js";import{F as L}from"./FormField.module-ExQut69c.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./clsx-B-dksMZM.js";import"./IconApp-OI763F0y.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-mVHdDF_7.js";import"./index-Cf-03bMR.js";import"./IconWarning-DnIf0GKe.js";import"./PopoverTrigger-BJQ8sjBJ.js";import"./OverlayTrigger-C837Kg63.js";import"./utils-BBsc42SX.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-B8rEgeMV.js";import"./useFocusRing-CRCkdtud.js";import"./number-nHrFdSb-.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./useControlledState-B7K2eYzO.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-Do5SRlXR.js";import"./FocusScope-CGKzeA_Y.js";import"./Hidden-DMUPAT4b.js";import"./getScrollParent-CcmJp979.js";import"./useLabels-DMhdkBu1.js";import"./VisuallyHidden-D4oQIDY4.js";import"./context-C-sXsD2V.js";import"./useSelector-CuvloarS.js";import"./ListBox-CC1D_2dq.js";import"./Collection-BSVICx61.js";import"./CollectionBuilder-C1pJ_vaJ.js";import"./DragAndDrop-Bssnb-st.js";import"./Header-BikxQE8J.js";import"./Separator-C6jiSXXg.js";import"./Text-C0gOxGaJ.js";import"./useMenuTrigger-Bs7oJgqd.js";import"./SelectionManager-D54GM_Uc.js";import"./useEvent-Be9f7BQw.js";import"./useCollator-BmGy_8lF.js";import"./useLabel-D8UVq-XF.js";import"./useListState-D5Ew4QeR.js";import"./Button-BknGA9dM.js";import"./useFocusable-D6sODRzj.js";import"./Form-BJmzrAR9.js";import"./Label-DgAfL5vK.js";import"./useSingleSelectListState-B3Jus2RD.js";import"./useFormReset-DlrFI1sj.js";import"./Text-CTCoi9MV.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";const Le={title:"Form Controls/Select",component:o,render:r=>e.createElement(o,{...r},e.createElement(s,null,"App"),e.createElement(t,null,"WordPress"),e.createElement(t,null,"TYPO3"),e.createElement(t,null,"Contao"),e.createElement(t,null,"Drupal"),e.createElement(t,null,"Joomla!"),e.createElement(t,null,"Matomo"))},n={},a={args:{isDisabled:!0}},l={args:{isRequired:!0}},p={render:r=>e.createElement(o,{...r},e.createElement(s,null,"App"),e.createElement(t,null,"WordPress"),e.createElement(t,null,"TYPO3"),e.createElement(t,null,"Contao"),e.createElement(t,null,"Drupal"),e.createElement(t,null,"Joomla!"),e.createElement(t,null,"Matomo"),e.createElement(Y,null,"Select an app"))},i={render:r=>e.createElement(o,{...r,defaultSelectedKey:"WordPress"},e.createElement(s,null,"App"),e.createElement(t,{value:"WordPress"},"WordPress"),e.createElement(t,{value:"TYPO3"},"TYPO3"),e.createElement(t,{value:"Contao"},"Contao"),e.createElement(t,{value:"Drupal"},"Drupal"),e.createElement(t,{value:"Joomla!"},"Joomla!"),e.createElement(t,{value:"Matomo"},"Matomo"))},m={render:r=>e.createElement(o,{...r,isInvalid:!0,isRequired:!0},e.createElement(s,null,"App"),e.createElement(t,null,"WordPress"),e.createElement(t,null,"TYPO3"),e.createElement(t,null,"Contao"),e.createElement(t,null,"Drupal"),e.createElement(t,null,"Joomla!"),e.createElement(t,null,"Matomo"),e.createElement(L,null,"Select an app to continue"))};var c,u,d;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(u=n.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var O,E,D;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(D=(E=a.parameters)==null?void 0:E.docs)==null?void 0:D.source}}};var S,P,W;l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(W=(P=l.parameters)==null?void 0:P.docs)==null?void 0:W.source}}};var v,F,f;p.parameters={...p.parameters,docs:{...(v=p.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <Select {...props}>
+      <Label>App</Label>
+      <Option>WordPress</Option>
+      <Option>TYPO3</Option>
+      <Option>Contao</Option>
+      <Option>Drupal</Option>
+      <Option>Joomla!</Option>
+      <Option>Matomo</Option>
+      <FieldDescription>Select an app</FieldDescription>
+    </Select>
+}`,...(f=(F=p.parameters)==null?void 0:F.docs)==null?void 0:f.source}}};var b,g,C;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Select {...props} defaultSelectedKey="WordPress">
+      <Label>App</Label>
+      <Option value="WordPress">WordPress</Option>
+      <Option value="TYPO3">TYPO3</Option>
+      <Option value="Contao">Contao</Option>
+      <Option value="Drupal">Drupal</Option>
+      <Option value="Joomla!">Joomla!</Option>
+      <Option value="Matomo">Matomo</Option>
+    </Select>
+}`,...(C=(g=i.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var J,M,T;m.parameters={...m.parameters,docs:{...(J=m.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  render: props => <Select {...props} isInvalid isRequired>
+      <Label>App</Label>
+      <Option>WordPress</Option>
+      <Option>TYPO3</Option>
+      <Option>Contao</Option>
+      <Option>Drupal</Option>
+      <Option>Joomla!</Option>
+      <Option>Matomo</Option>
+      <FieldError>Select an app to continue</FieldError>
+    </Select>
+}`,...(T=(M=m.parameters)==null?void 0:M.docs)==null?void 0:T.source}}};const Re=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithFieldError"];export{n as Default,a as Disabled,l as Required,i as WithDefaultValue,p as WithFieldDescription,m as WithFieldError,Re as __namedExportsOrder,Le as default};
---
+++ storybook-main/assets/EdgeCases.stories-D8Dl44gS.js
@@ -0,0 +1,25 @@
+import{R as p,a as l,b as g}from"./Radio-Cu2MNtN4.js";import{R as e}from"./index-Cs7sjTYM.js";import{d as o}from"./dummyText-CX_I_Wpl.js";import y from"./Default.stories-BWw0XW9Z.js";import{T as E}from"./Text-CTCoi9MV.js";import{C as T}from"./Content-CiWjIu6H.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./dynamic-DKDa4OpU.js";import"./FormField.module-ExQut69c.js";import"./IconApp-OI763F0y.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-mVHdDF_7.js";import"./index-Cf-03bMR.js";import"./IconWarning-DnIf0GKe.js";import"./Form-BJmzrAR9.js";import"./useFocusRing-CRCkdtud.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./utils-BBsc42SX.js";import"./Text-C0gOxGaJ.js";import"./ColumnLayout-BGHswle4.js";import"./deepFindOfType-BylCc5Xi.js";import"./react-children-utilities-CdZndyK3.js";import"./getContainerBreakpointSizeClassName-BiU6pQS0.js";import"./Label-DgAfL5vK.js";import"./Hidden-DMUPAT4b.js";import"./filterDOMProps-CeZl_uWj.js";import"./useControlledState-B7K2eYzO.js";import"./FocusScope-CGKzeA_Y.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./useFormReset-DlrFI1sj.js";import"./useFocusable-D6sODRzj.js";import"./usePress-Do5SRlXR.js";import"./VisuallyHidden-D4oQIDY4.js";import"./Label-CV7xN_BM.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";const ue={title:"Form Controls/RadioGroup/Edge Cases",...y},t={render:a=>e.createElement(p,{...a,defaultValue:"a","aria-label":"Label"},e.createElement(l,{value:"a"},o.long," "),e.createElement(l,{value:"b"},o.long," "),e.createElement(l,{value:"c"},o.long))},m={render:a=>e.createElement(p,{...a,defaultValue:"0","aria-label":"Label",l:[1,1,1,1]},Array(6).fill("").map((v,r)=>e.createElement(g,{value:r.toString(),key:r},e.createElement(E,null,o.medium),e.createElement(T,null,o.medium))))},i={render:a=>e.createElement(p,{...a,defaultValue:"0","aria-label":"Label"},Array(3).fill("").map((v,r)=>e.createElement(g,{value:r.toString(),key:r},"Option ",r+1))),parameters:{viewport:{defaultViewport:"mobile1"}}};var n,u,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="a" aria-label="Label">
+      <Radio value="a">{dummyText.long} </Radio>
+      <Radio value="b">{dummyText.long} </Radio>
+      <Radio value="c">{dummyText.long}</Radio>
+    </RadioGroup>
+}`,...(d=(u=t.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var s,c,R;m.parameters={...m.parameters,docs:{...(s=m.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="0" aria-label="Label" l={[1, 1, 1, 1]}>
+      {Array(6).fill("").map((value, index) => <RadioButton value={index.toString()} key={index}>
+            <Text>{dummyText.medium}</Text>
+            <Content>{dummyText.medium}</Content>
+          </RadioButton>)}
+    </RadioGroup>
+}`,...(R=(c=m.parameters)==null?void 0:c.docs)==null?void 0:R.source}}};var f,b,x;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="0" aria-label="Label">
+      {Array(3).fill("").map((value, index) => <RadioButton value={index.toString()} key={index}>
+            Option {index + 1}
+          </RadioButton>)}
+    </RadioGroup>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(x=(b=i.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};const de=["LongTexts","MultipleRadioButtons","SmallSpace"];export{t as LongTexts,m as MultipleRadioButtons,i as SmallSpace,de as __namedExportsOrder,ue as default};
