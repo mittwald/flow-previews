67b09411d83ce28806f82d83103e0e7bd28d7655
mittwald <>
2024-10-07 06:30:02 +0000

    Update from https://github.com/mittwald/flow/commit/dd33e1bb9fc02da5fbffd8a20e2606faab848153

---
+++ storybook/792-improvements-to-notification-and-inlinealert/assets/Default.stories-DQcfO57M.js
@@ -0,0 +1,26 @@
+import{C as t}from"./CheckboxGroup-YjGw6bXv.js";import{R as e}from"./index-Cs7sjTYM.js";import{L as c}from"./Label-CV7xN_BM.js";import{a as W}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{F as L}from"./FormField.module-ExQut69c.js";import{C as o}from"./Checkbox-NA1TY8sO.js";import{C as p}from"./CheckboxButton-uf0ksURh.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BmJWDS_p.js";import"./index-C8LjR2Bd.js";import"./flowComponent-mVHdDF_7.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ColumnLayout-BGHswle4.js";import"./deepFindOfType-BylCc5Xi.js";import"./react-children-utilities-CdZndyK3.js";import"./useLocalizedStringFormatter-BSNlGq_b.js";import"./Label-DgAfL5vK.js";import"./utils-BBsc42SX.js";import"./Hidden-DMUPAT4b.js";import"./v4-CQkTLCs1.js";import"./IconApp-OI763F0y.js";import"./IconWarning-DnIf0GKe.js";import"./Form-BJmzrAR9.js";import"./useFocusRing-CRCkdtud.js";import"./useLabel-D8UVq-XF.js";import"./useLabels-DMhdkBu1.js";import"./Text-C0gOxGaJ.js";import"./RSPContexts-BseA8goh.js";import"./filterDOMProps-CeZl_uWj.js";import"./useControlledState-B7K2eYzO.js";import"./useToggleState-BRhxJd93.js";import"./useFormReset-DlrFI1sj.js";import"./useFocusable-D6sODRzj.js";import"./usePress-Do5SRlXR.js";import"./VisuallyHidden-D4oQIDY4.js";const pe={title:"Form Controls/CheckboxGroup",component:t,args:{onChange:W("onChange")},parameters:{controls:{exclude:["onChange"]}},render:r=>e.createElement(t,{...r},e.createElement(c,null,"Permissions"),e.createElement(o,{value:"read"},"Read"),e.createElement(o,{value:"write"},"Write"))},a={},s={args:{isDisabled:!0}},i={render:r=>e.createElement(t,{...r},e.createElement(c,null,"Permissions"),e.createElement(o,{value:"read",isDisabled:!0},"Read"),e.createElement(o,{value:"write"},"Write"))},n={render:r=>e.createElement(t,{...r},e.createElement(c,null,"Permissions"),e.createElement(p,{value:"read"},"Read"),e.createElement(p,{value:"write"},"Write"))},m={render:r=>e.createElement(t,{...r,isInvalid:!0,isRequired:!0},e.createElement(c,null,"Permissions"),e.createElement(o,{value:"read"},"Read"),e.createElement(o,{value:"write"},"Write"),e.createElement(L,null,"Select at least one to continue"))};var l,u,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var b,C,h;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(h=(C=s.parameters)==null?void 0:C.docs)==null?void 0:h.source}}};var x,k,E;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <CheckboxGroup {...props}>
+      <Label>Permissions</Label>
+      <Checkbox value="read" isDisabled>
+        Read
+      </Checkbox>
+      <Checkbox value="write">Write</Checkbox>
+    </CheckboxGroup>
+}`,...(E=(k=i.parameters)==null?void 0:k.docs)==null?void 0:E.source}}};var v,g,R;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <CheckboxGroup {...props}>
+      <Label>Permissions</Label>
+      <CheckboxButton value="read">Read</CheckboxButton>
+      <CheckboxButton value="write">Write</CheckboxButton>
+    </CheckboxGroup>
+}`,...(R=(g=n.parameters)==null?void 0:g.docs)==null?void 0:R.source}}};var f,D,G;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <CheckboxGroup {...props} isInvalid isRequired>
+      <Label>Permissions</Label>
+      <Checkbox value="read">Read</Checkbox>
+      <Checkbox value="write">Write</Checkbox>
+      <FieldError>Select at least one to continue</FieldError>
+    </CheckboxGroup>
+}`,...(G=(D=m.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};const le=["Default","CheckboxGroupDisabled","CheckboxDisabled","CheckboxButtons","WithFieldError"];export{n as CheckboxButtons,i as CheckboxDisabled,s as CheckboxGroupDisabled,a as Default,m as WithFieldError,le as __namedExportsOrder,pe as default};
