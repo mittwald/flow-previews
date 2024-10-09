81600699b43d929d3e82d35bd6afd1f260aeb9d8
mittwald <>
2024-10-09 09:08:14 +0000

    Update from https://github.com/mittwald/flow/commit/c904f78486c992b7da88c2ee356c4f84a2d230ed

---
+++ docs/add-table-footer/_next/static/CXQiJVMtkjKQLzr-ZRDmv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-485ac7b0de725be5.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
