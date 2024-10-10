5f6e487ba905714610a14481765f0b9d35bbd6d4
mittwald <>
2024-10-10 09:37:18 +0000

    Update from https://github.com/mittwald/flow/commit/d20f4c3e795878593fe75251401cbb76958d3858

---
+++ storybook/fix-icon-size/assets/Default.stories-CVB77asM.js
@@ -0,0 +1,26 @@
+import{C as t}from"./CheckboxGroup-Dzf1cP4c.js";import{R as e}from"./index-Cs7sjTYM.js";import{L as m}from"./Label-CT5Ir9KL.js";import{a as W}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{F as L}from"./FormField.module-Ddl-p44B.js";import{C as o}from"./Checkbox-B09aOcAs.js";import{C as p}from"./CheckboxButton-BUvh7v6S.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DXo7lD_U.js";import"./index-j_f0pOtZ.js";import"./flowComponent-DewkrGXe.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ColumnLayout-BGHswle4.js";import"./deepFindOfType-BylCc5Xi.js";import"./react-children-utilities-CdZndyK3.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./Label-C-iBdzH7.js";import"./utils-Cd8r7pc9.js";import"./Hidden-C2kgQquV.js";import"./v4-CQkTLCs1.js";import"./IconApp-CdASQQtj.js";import"./IconWarning-BNLeXliD.js";import"./Form-CDVAwZBo.js";import"./useFocusRing-DdBFev_0.js";import"./useLabel-BA8WH6BO.js";import"./Text-Bnjm0IRl.js";import"./filterDOMProps-CeZl_uWj.js";import"./RSPContexts-BseA8goh.js";import"./useControlledState-B7K2eYzO.js";import"./useToggleState-BlySn88k.js";import"./useFormReset-BmVKDtZT.js";import"./useFocusable-GQF4xS8_.js";import"./usePress-Cn8oVN-C.js";import"./VisuallyHidden-BVqa5uLw.js";const me={title:"Form Controls/CheckboxGroup",component:t,args:{onChange:W("onChange")},parameters:{controls:{exclude:["onChange"]}},render:r=>e.createElement(t,{...r},e.createElement(m,null,"Permissions"),e.createElement(o,{value:"read"},"Read"),e.createElement(o,{value:"write"},"Write"))},a={},s={args:{isDisabled:!0}},i={render:r=>e.createElement(t,{...r},e.createElement(m,null,"Permissions"),e.createElement(o,{value:"read",isDisabled:!0},"Read"),e.createElement(o,{value:"write"},"Write"))},n={render:r=>e.createElement(t,{...r},e.createElement(m,null,"Permissions"),e.createElement(p,{value:"read"},"Read"),e.createElement(p,{value:"write"},"Write"))},c={render:r=>e.createElement(t,{...r,isInvalid:!0,isRequired:!0},e.createElement(m,null,"Permissions"),e.createElement(o,{value:"read"},"Read"),e.createElement(o,{value:"write"},"Write"),e.createElement(L,null,"Select at least one to continue"))};var l,u,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var b,C,h;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(h=(C=s.parameters)==null?void 0:C.docs)==null?void 0:h.source}}};var x,k,E;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <CheckboxGroup {...props}>
+      <Label>Permissions</Label>
+      <Checkbox value="read" isDisabled>
+        Read
+      </Checkbox>
+      <Checkbox value="write">Write</Checkbox>
+    </CheckboxGroup>
+}`,...(E=(k=i.parameters)==null?void 0:k.docs)==null?void 0:E.source}}};var v,g,R;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <CheckboxGroup {...props}>
+      <Label>Permissions</Label>
+      <CheckboxButton value="read">Read</CheckboxButton>
+      <CheckboxButton value="write">Write</CheckboxButton>
+    </CheckboxGroup>
+}`,...(R=(g=n.parameters)==null?void 0:g.docs)==null?void 0:R.source}}};var f,D,G;c.parameters={...c.parameters,docs:{...(f=c.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <CheckboxGroup {...props} isInvalid isRequired>
+      <Label>Permissions</Label>
+      <Checkbox value="read">Read</Checkbox>
+      <Checkbox value="write">Write</Checkbox>
+      <FieldError>Select at least one to continue</FieldError>
+    </CheckboxGroup>
+}`,...(G=(D=c.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};const pe=["Default","CheckboxGroupDisabled","CheckboxDisabled","CheckboxButtons","WithFieldError"];export{n as CheckboxButtons,i as CheckboxDisabled,s as CheckboxGroupDisabled,a as Default,c as WithFieldError,pe as __namedExportsOrder,me as default};
---
+++ storybook/fix-icon-size/assets/EdgeCases.stories-C7KfIFzd.js
@@ -0,0 +1,54 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{m as E,T as p,a as m,b as o}from"./Default.stories-C_FEHrJ8.js";import{S as r}from"./Section-DXXAOylU.js";import{T as n}from"./Text-CCODsZR3.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./flowComponent-DewkrGXe.js";import"./PropsContextProvider-DXo7lD_U.js";import"./index-j_f0pOtZ.js";import"./index-Cf-03bMR.js";import"./ContextMenuTrigger-2hvxRipM.js";import"./ContextMenu.module-68N5lvfm.js";import"./PopoverTrigger-D3n0-DEu.js";import"./OverlayTrigger-hNSPMKkP.js";import"./utils-Cd8r7pc9.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-Bm5GlnqX.js";import"./useFocusRing-DdBFev_0.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-B7K2eYzO.js";import"./usePress-Cn8oVN-C.js";import"./FocusScope-DMuRQrVO.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-BVqa5uLw.js";import"./context-Be0nDoLg.js";import"./useSelector-BAs3VJFZ.js";import"./Action-BrzbUalv.js";import"./dynamic-DKDa4OpU.js";import"./IconApp-CdASQQtj.js";import"./IconWarning-BNLeXliD.js";import"./deepFindOfType-BylCc5Xi.js";import"./react-children-utilities-CdZndyK3.js";import"./Wrap-DQq6jo70.js";import"./Collection-BdcdgcY_.js";import"./CollectionBuilder-B4BE-ZOx.js";import"./Header-3zDVqz72.js";import"./Separator-ZOqStrt8.js";import"./Text-Bnjm0IRl.js";import"./useMenuTrigger-BI8VnLTz.js";import"./SelectionManager-CLd9IAHF.js";import"./useEvent-BjqsPY1I.js";import"./useCollator-tPR4bsZ_.js";import"./useFocusable-GQF4xS8_.js";import"./Button-B7B5Ghno.js";import"./LoadingSpinner-CLv9VLUj.js";import"./Button-B89OlhFY.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useHasTabbableChild-Cd40yaDl.js";import"./useSingleSelectListState-DQfa2Mvd.js";import"./useListState-CL5yIiDJ.js";import"./Activity-Cr-Aqo1o.js";import"./Heading-dmduvgvo.js";import"./Heading-CG14-WVD.js";import"./TextField-CddEa8sD.js";import"./TextFieldBase-BryOYv2t.js";import"./FormField.module-Ddl-p44B.js";import"./Form-CDVAwZBo.js";import"./FieldDescription-e9vLXI_Z.js";import"./Input-zQ88cEWx.js";import"./useTextField-BFt9_4PS.js";import"./useFormReset-BmVKDtZT.js";import"./Label-CT5Ir9KL.js";import"./LabeledValue-DY3Z86ut.js";import"./Switch-zd9HXwlR.js";import"./useToggleState-BlySn88k.js";import"./Header-DzAPUuik.js";import"./AlertIcon-CCULBB4a.js";import"./EmulatedBoldText-Dmze-Iwg.js";const we={title:"Navigation/Tabs/Edge Cases",...E},a={render:i=>e.createElement(p,{...i},e.createElement(m,{id:"a"},e.createElement(o,null,"Tab A"),e.createElement(r,null,e.createElement(n,null,t.long))),e.createElement(m,{id:"b"},e.createElement(o,null,"Tab B"),e.createElement(r,null,e.createElement(n,null,t.long)),e.createElement(r,null,e.createElement(n,null,t.long)),e.createElement(r,null,e.createElement(n,null,t.long)),e.createElement(r,null,e.createElement(n,null,t.long))))},l={render:i=>e.createElement(p,{...i},e.createElement(m,{id:"a"},e.createElement(o,null,"Tab A"),e.createElement(r,null,e.createElement(n,null,t.short))),e.createElement(m,{id:"b"},e.createElement(o,null,"Tab B"),e.createElement(r,null,e.createElement(n,null,t.short),e.createElement(p,{...i,disabledKeys:["spam"]},e.createElement(m,{id:"subA"},e.createElement(o,null,"Sub tab A"),e.createElement(r,null,e.createElement(n,null,t.short))),e.createElement(m,{id:"subB"},e.createElement(o,null,"Sub tab B"),e.createElement(r,null,e.createElement(n,null,t.short)))))))};var T,c,s;a.parameters={...a.parameters,docs:{...(T=a.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <Tabs {...props}>
+      <Tab id="a">
+        <TabTitle>Tab A</TabTitle>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Tab>
+      <Tab id="b">
+        <TabTitle>Tab B</TabTitle>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Tab>
+    </Tabs>
+}`,...(s=(c=a.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};var b,u,d;l.parameters={...l.parameters,docs:{...(b=l.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Tabs {...props}>
+      <Tab id="a">
+        <TabTitle>Tab A</TabTitle>
+        <Section>
+          <Text>{dummyText.short}</Text>
+        </Section>
+      </Tab>
+      <Tab id="b">
+        <TabTitle>Tab B</TabTitle>
+        <Section>
+          <Text>{dummyText.short}</Text>
+          <Tabs {...props} disabledKeys={["spam"]}>
+            <Tab id="subA">
+              <TabTitle>Sub tab A</TabTitle>
+              <Section>
+                <Text>{dummyText.short}</Text>
+              </Section>
+            </Tab>
+            <Tab id="subB">
+              <TabTitle>Sub tab B</TabTitle>
+              <Section>
+                <Text>{dummyText.short}</Text>
+              </Section>
+            </Tab>
+          </Tabs>
+        </Section>
+      </Tab>
+    </Tabs>
+}`,...(d=(u=l.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const ze=["DifferentHeight","Nested"];export{a as DifferentHeight,l as Nested,ze as __namedExportsOrder,we as default};
