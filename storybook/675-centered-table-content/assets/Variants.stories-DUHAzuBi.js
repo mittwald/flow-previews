4ea0d8a1e8b640b700bef4452006eaa1c661ceca
mittwald <>
2024-10-10 08:21:20 +0000

    Update from https://github.com/mittwald/flow/commit/fc1e15b13fd2eaaade95978f7e3e72fa8e11df7b

---
+++ storybook/675-centered-table-content/assets/Variants.stories-DUHAzuBi.js
@@ -0,0 +1,16 @@
+import{A as o}from"./AlertBadge-DkPmgocm.js";import D from"./Default.stories-f5re76MO.js";import{R as n}from"./index-Cs7sjTYM.js";import"./clsx-B-dksMZM.js";import"./AlertIcon-obMAXfZy.js";import"./IconApp-CdASQQtj.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PropsContextProvider-DXo7lD_U.js";import"./index-j_f0pOtZ.js";import"./flowComponent-DewkrGXe.js";import"./index-Cf-03bMR.js";import"./IconWarning-CPyFcIuI.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./Text-CCODsZR3.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-Bnjm0IRl.js";import"./utils-Cd8r7pc9.js";const F={...D,title:"Status/AlertBadge/Status"},e={},s={render:r=>n.createElement(o,{...r},"Success"),args:{status:"success"}},a={render:r=>n.createElement(o,{...r},"Warning"),args:{status:"warning"}},t={render:r=>n.createElement(o,{...r},"Danger"),args:{status:"danger"}};var p,c,m;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var i,d,g;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Success</AlertBadge>,
+  args: {
+    status: "success"
+  }
+}`,...(g=(d=s.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,l,S;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Warning</AlertBadge>,
+  args: {
+    status: "warning"
+  }
+}`,...(S=(l=a.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};var A,B,f;t.parameters={...t.parameters,docs:{...(A=t.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Danger</AlertBadge>,
+  args: {
+    status: "danger"
+  }
+}`,...(f=(B=t.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};const G=["Info","Success","Warning","Danger"];export{t as Danger,e as Info,s as Success,a as Warning,G as __namedExportsOrder,F as default};
