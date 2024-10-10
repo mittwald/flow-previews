171cd21a4364692908b32d94ef6ee4f208ec131b
mittwald <>
2024-10-10 11:05:42 +0000

    Cleanup merged Storybook previews

--- storybook/add-list-header-slot/assets/EdgeCases.stories-BKZAE4Xw.js
+++
@@ -1,6 +0,0 @@
-import{H as n}from"./Heading-Bk4isR8H.js";import a from"./Default.stories-DW3LkCw6.js";import"./IconApp-CKfGrMYq.js";import{R as r}from"./index-Cs7sjTYM.js";import{b as i}from"./IconWarning-CfieAnFV.js";import{d as s}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DXo7lD_U.js";import"./index-j_f0pOtZ.js";import"./flowComponent-Bg40pdZw.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Heading-CG14-WVD.js";import"./RSPContexts-BseA8goh.js";import"./utils-Cd8r7pc9.js";import"./storyBackgrounds-GFSAi-dI.js";const R={...a,title:"Content/Heading/Edge Cases",component:n},e={render:p=>r.createElement(n,{...p},r.createElement(i,null),s.medium)};var o,t,m;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: props => <Heading {...props}>
-      <IconMember />
-      {dummyText.medium}
-    </Heading>
-}`,...(m=(t=e.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const _=["LongText"];export{e as LongText,_ as __namedExportsOrder,R as default};
