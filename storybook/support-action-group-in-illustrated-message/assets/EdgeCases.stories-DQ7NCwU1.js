61c9e9e237c143b0128c4d6191e04f3cd25a9983
mittwald <>
2024-10-24 05:05:39 +0000

    Update from https://github.com/mittwald/flow/commit/4e9675d6e8d3488db9e79f07d43e043aaf7ae75b

---
+++ storybook/support-action-group-in-illustrated-message/assets/EdgeCases.stories-DQ7NCwU1.js
@@ -0,0 +1,54 @@
+import{R as e}from"./index-Cs7sjTYM.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{m as E,T as p,a as m,b as o}from"./Default.stories-C_pM7IS5.js";import{S as r}from"./Section-DsrNCVfI.js";import{T as n}from"./Text-DbnY-2dT.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./flowComponent-Bg40pdZw.js";import"./PropsContextProvider-DXo7lD_U.js";import"./index-j_f0pOtZ.js";import"./index-Cf-03bMR.js";import"./ContextMenuTrigger-DU0wU4h8.js";import"./ContextMenu.module-68N5lvfm.js";import"./Popover-V756F-P3.js";import"./OverlayTrigger-bWOm86mb.js";import"./utils-Cd8r7pc9.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-Bm5GlnqX.js";import"./useFocusRing-DdBFev_0.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-B7K2eYzO.js";import"./usePress-BiUpVy3m.js";import"./FocusScope-DMuRQrVO.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-BVqa5uLw.js";import"./context-BnMUo41h.js";import"./useSelector-DiJHxpbu.js";import"./Action-D4n2WPP_.js";import"./getActionGroupSlot-DLuT6VQZ.js";import"./dynamic-DKDa4OpU.js";import"./IconApp-CKfGrMYq.js";import"./IconWarning-DoqSRZU1.js";import"./deepFindOfType-BylCc5Xi.js";import"./react-children-utilities-CdZndyK3.js";import"./Wrap-DQq6jo70.js";import"./Collection-BdcdgcY_.js";import"./CollectionBuilder-B4BE-ZOx.js";import"./Header-3zDVqz72.js";import"./Separator-ZOqStrt8.js";import"./Text-Bnjm0IRl.js";import"./useMenuTrigger-VovAUH_O.js";import"./SelectionManager-CstbXOPk.js";import"./useEvent-BjqsPY1I.js";import"./useCollator-tPR4bsZ_.js";import"./useFocusable-GQF4xS8_.js";import"./Button-P6k_BIBP.js";import"./LoadingSpinner-9k8jsvZY.js";import"./Button-CROQF3YN.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useHasTabbableChild-Cd40yaDl.js";import"./useSingleSelectListState-CMWjPCF8.js";import"./useListState-_MSxqlzF.js";import"./Activity-Cr-Aqo1o.js";import"./Heading-CuXfARmX.js";import"./Heading-CG14-WVD.js";import"./TextField-9YUZ50D-.js";import"./TextFieldBase-CANxoHIE.js";import"./FormField.module-BQ0wLCGA.js";import"./Form-CDVAwZBo.js";import"./FieldDescription-D5E2i6qp.js";import"./Input-zQ88cEWx.js";import"./useTextField-BFt9_4PS.js";import"./useFormReset-BmVKDtZT.js";import"./Label-Ce3Da7vQ.js";import"./LabeledValue-DY3Z86ut.js";import"./Switch-BX83I3a0.js";import"./useToggleState-BrWHT7eX.js";import"./Header-dj3Po25X.js";import"./AlertIcon-t7ZrfOSf.js";import"./EmulatedBoldText-Dmze-Iwg.js";const ze={title:"Navigation/Tabs/Edge Cases",...E},a={render:i=>e.createElement(p,{...i},e.createElement(m,{id:"a"},e.createElement(o,null,"Tab A"),e.createElement(r,null,e.createElement(n,null,t.long))),e.createElement(m,{id:"b"},e.createElement(o,null,"Tab B"),e.createElement(r,null,e.createElement(n,null,t.long)),e.createElement(r,null,e.createElement(n,null,t.long)),e.createElement(r,null,e.createElement(n,null,t.long)),e.createElement(r,null,e.createElement(n,null,t.long))))},l={render:i=>e.createElement(p,{...i},e.createElement(m,{id:"a"},e.createElement(o,null,"Tab A"),e.createElement(r,null,e.createElement(n,null,t.short))),e.createElement(m,{id:"b"},e.createElement(o,null,"Tab B"),e.createElement(r,null,e.createElement(n,null,t.short),e.createElement(p,{...i,disabledKeys:["spam"]},e.createElement(m,{id:"subA"},e.createElement(o,null,"Sub tab A"),e.createElement(r,null,e.createElement(n,null,t.short))),e.createElement(m,{id:"subB"},e.createElement(o,null,"Sub tab B"),e.createElement(r,null,e.createElement(n,null,t.short)))))))};var T,c,s;a.parameters={...a.parameters,docs:{...(T=a.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <Tabs {...props}>
+      <Tab id="a">
+        <TabTitle>Tab A</TabTitle>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Tab>
+      <Tab id="b">
+        <TabTitle>Tab B</TabTitle>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Tab>
+    </Tabs>
+}`,...(s=(c=a.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};var b,u,d;l.parameters={...l.parameters,docs:{...(b=l.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Tabs {...props}>
+      <Tab id="a">
+        <TabTitle>Tab A</TabTitle>
+        <Section>
+          <Text>{dummyText.short}</Text>
+        </Section>
+      </Tab>
+      <Tab id="b">
+        <TabTitle>Tab B</TabTitle>
+        <Section>
+          <Text>{dummyText.short}</Text>
+          <Tabs {...props} disabledKeys={["spam"]}>
+            <Tab id="subA">
+              <TabTitle>Sub tab A</TabTitle>
+              <Section>
+                <Text>{dummyText.short}</Text>
+              </Section>
+            </Tab>
+            <Tab id="subB">
+              <TabTitle>Sub tab B</TabTitle>
+              <Section>
+                <Text>{dummyText.short}</Text>
+              </Section>
+            </Tab>
+          </Tabs>
+        </Section>
+      </Tab>
+    </Tabs>
+}`,...(d=(u=l.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const Fe=["DifferentHeight","Nested"];export{a as DifferentHeight,l as Nested,Fe as __namedExportsOrder,ze as default};
