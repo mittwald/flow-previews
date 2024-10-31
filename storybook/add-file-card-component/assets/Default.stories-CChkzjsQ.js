89703756baef43337ffc0cbd82214ccd971d981d
mittwald <>
2024-10-31 06:53:29 +0000

    Update from https://github.com/mittwald/flow/commit/3981d2b7af6586a54278e8f70cfc98d23c30d068

---
+++ storybook/add-file-card-component/assets/Default.stories-CChkzjsQ.js
@@ -0,0 +1,18 @@
+import{P as m,a as R}from"./Popover-MTzL8RQA.js";import{R as r}from"./index-Cs7sjTYM.js";import{B as P}from"./Button-D9ARpkqB.js";import{u as T}from"./OverlayTrigger-BtrVDkuc.js";import"./flowComponent-C7dqiiMm.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./clsx-B-dksMZM.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./OverlayArrow-BrbzJDKz.js";import"./useFocusRing-CqFdZJbR.js";import"./ProgressBar-K2upHjuk.js";import"./utils-Cd8r7pc9.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-B7K2eYzO.js";import"./IconApp-CO8EIbbc.js";import"./IconWarning-DS4KJnIA.js";import"./Wrap-DQq6jo70.js";import"./Text-BL-QAQsJ.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bnjm0IRl.js";import"./LoadingSpinner-BDWH73hL.js";import"./Button-DKDQv6qd.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CKybZYMg.js";import"./usePress-Dcn7RY69.js";import"./RSPContexts-BseA8goh.js";import"./FocusScope-DxaEUeh1.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-BOk7_Rfy.js";import"./context-CT50f05J.js";import"./useSelector-3ucM3joq.js";const pr={title:"Overlays/Popover",component:m,render:p=>r.createElement(R,null,r.createElement(P,null,"Trigger popover"),r.createElement(m,{...p,placement:"bottom right"},"I am a popover."))},e={},o={args:{withTip:!0}},t={render:p=>{const n=T("Popover"),i=r.useRef(null);return r.createElement(r.Fragment,null,r.createElement(P,{ref:i,onPress:n.open},"Trigger popover"),r.createElement(m,{triggerRef:i,...p,controller:n},"I am a popover."))}};var a,s,l;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var c,u,g;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    withTip: true
+  }
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var f,v,d;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController("Popover");
+    const triggerRef = React.useRef(null);
+    return <>
+        <Button ref={triggerRef} onPress={controller.open}>
+          Trigger popover
+        </Button>
+        <Popover triggerRef={triggerRef} {...props} controller={controller}>
+          I am a popover.
+        </Popover>
+      </>;
+  }
+}`,...(d=(v=t.parameters)==null?void 0:v.docs)==null?void 0:d.source}}};const mr=["Default","WithTip","WithController"];export{e as Default,t as WithController,o as WithTip,mr as __namedExportsOrder,pr as default};
---
+++ storybook/add-file-card-component/assets/EdgeCases.stories-xGWieq2M.js
@@ -0,0 +1,23 @@
+import{H as a}from"./Heading-C51F131U.js";import B from"./Default.stories-C_6KUAOe.js";import"./IconApp-CO8EIbbc.js";import{R as e}from"./index-Cs7sjTYM.js";import{b as H}from"./IconWarning-DS4KJnIA.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import{A as T}from"./AlertBadge-Ccde9mXY.js";import{a as E,C as f}from"./ContextualHelpTrigger-IV1U8hpZ.js";import{B as C}from"./Button-D9ARpkqB.js";import{T as A}from"./Text-BL-QAQsJ.js";import{B as y}from"./Badge-B1tyAAtK.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./flowComponent-C7dqiiMm.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Heading-CG14-WVD.js";import"./RSPContexts-BseA8goh.js";import"./utils-Cd8r7pc9.js";import"./storyBackgrounds-GFSAi-dI.js";import"./AlertIcon-BusKF1FH.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./Popover-MTzL8RQA.js";import"./OverlayTrigger-BtrVDkuc.js";import"./OverlayArrow-BrbzJDKz.js";import"./useFocusRing-CqFdZJbR.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-B7K2eYzO.js";import"./usePress-Dcn7RY69.js";import"./FocusScope-DxaEUeh1.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-BOk7_Rfy.js";import"./context-CT50f05J.js";import"./useSelector-3ucM3joq.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-BDWH73hL.js";import"./Button-DKDQv6qd.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CKybZYMg.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bnjm0IRl.js";const Be={...B,title:"Content/Heading/Edge Cases",component:a},t={render:r=>e.createElement(a,{...r},e.createElement(H,null),n.medium)},o={render:r=>e.createElement(a,{...r},e.createElement(H,null),n.long,e.createElement(T,null,"Alert"),e.createElement(y,null,"Badge"))},m={render:r=>e.createElement(a,{...r},n.long,e.createElement(E,null,e.createElement(C,null),e.createElement(f,{...r},e.createElement(A,null,n.medium))))};var p,i,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      {dummyText.medium}
+    </Heading>
+}`,...(l=(i=t.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var s,d,c;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      {dummyText.long}
+      <AlertBadge>Alert</AlertBadge>
+      <Badge>Badge</Badge>
+    </Heading>
+}`,...(c=(d=o.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var u,g,x;m.parameters={...m.parameters,docs:{...(u=m.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      {dummyText.long}
+      <ContextualHelpTrigger>
+        <Button />
+        <ContextualHelp {...props}>
+          <Text>{dummyText.medium}</Text>
+        </ContextualHelp>
+      </ContextualHelpTrigger>
+    </Heading>
+}`,...(x=(g=m.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const Te=["LongText","WithAlertBadge","WithContextualHelp"];export{t as LongText,o as WithAlertBadge,m as WithContextualHelp,Te as __namedExportsOrder,Be as default};
