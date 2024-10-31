0d5fbd7253b5ff9f56c7e6cc8e86f0019fc7d3f8
mittwald <>
2024-10-31 10:09:34 +0000

    Update from https://github.com/mittwald/flow/commit/4a584722cb5a664652250a54d21754b33b514ad5

---
+++ storybook-main/assets/Sizes.stories-7dRp_eYC.js
@@ -0,0 +1,17 @@
+import l from"./Default.stories-D3qOIYBv.js";import"./Avatar-BJvRINaC.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./react-children-utilities-CdZndyK3.js";import"./flowComponent-aK1NJH1f.js";import"./index-Cf-03bMR.js";import"./Initials-C-VdwCt3.js";import"./Image-CdV7uWK9.js";import"./dummyText-CX_I_Wpl.js";import"./IconApp-Cq7w81Gv.js";const j={...l,title:"Content/Avatar/Sizes"},r={args:{size:"xs"}},e={args:{size:"s"}},s={args:{size:"m"}},o={args:{size:"l"}};var a,t,i;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    size: "xs"
+  }
+}`,...(i=(t=r.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};var m,p,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: "s"
+  }
+}`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var n,z,S;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: "m"
+  }
+}`,...(S=(z=s.parameters)==null?void 0:z.docs)==null?void 0:S.source}}};var d,u,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "l"
+  }
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};const k=["SizeXS","SizeS","SizeM","SizeL"];export{o as SizeL,s as SizeM,e as SizeS,r as SizeXS,k as __namedExportsOrder,j as default};
