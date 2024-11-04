37362fe85049242134928e91460e904482d87412
mittwald <>
2024-11-04 13:04:46 +0000

    Cleanup merged docs previews

--- docs/chore/remove-optional-in-properties-table/_next/static/JE9L7aOcCdy5k919Fuffh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-31a953618235c18e.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
