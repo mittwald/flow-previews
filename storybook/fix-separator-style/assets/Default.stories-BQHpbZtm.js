de576a3175273e3e97a7aa96a9cc4c8eeed4b832
mittwald <>
2024-11-06 09:34:23 +0000

    Update from https://github.com/mittwald/flow/commit/1fa455c479c4767b5cd79f3d47512fec6e8633d4

---
+++ storybook/fix-separator-style/assets/Default.stories-BQHpbZtm.js
@@ -0,0 +1,78 @@
+import{R as e,r as R}from"./index-Cs7sjTYM.js";import"./IconApp-C4Wlkpqb.js";import{B,c as H,e as $}from"./IconWarning-9UTZL34K.js";import{T as b}from"./Text-nt2VY5KZ.js";import{L as a}from"./Link-eTjaLkoV.js";import{c as O}from"./clsx-B-dksMZM.js";import{P as h}from"./PropsContextProvider-CRusYdr1.js";import{P as N,O as k}from"./flowComponent-C7dqiiMm.js";import{A as z}from"./Accordion-dGFv0CUj.js";import{C as F}from"./Content-CGhwX46t.js";import{W as J}from"./Wrap-DQq6jo70.js";import{d as K}from"./deepHas-C3achdHB.js";import{L as o}from"./Label-D_Dp2ikF.js";import{S as Q}from"./Separator-DjnnN6s2.js";import"./_commonjsHelpers-BosuxZz1.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bnjm0IRl.js";import"./utils-Cd8r7pc9.js";import"./index-j_f0pOtZ.js";import"./index-Cf-03bMR.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-Dcn7RY69.js";import"./useFocusRing-CqFdZJbR.js";import"./useFocusable-CKybZYMg.js";import"./dynamic-DKDa4OpU.js";import"./Button-BifBWCXZ.js";import"./LoadingSpinner-Cwk6VGUN.js";import"./Button-DKDQv6qd.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./Activity-uBW5QVzV.js";import"./react-children-utilities-CdZndyK3.js";import"./Separator-ZOqStrt8.js";import"./CollectionBuilder-B4BE-ZOx.js";const U="flow--navigation",V="flow--navigation--item",X="flow--navigation--icon",Y="flow--navigation--text",Z="flow--navigation--control-icon",ee="flow--navigation--avatar",m={navigation:U,item:V,icon:X,text:Y,controlIcon:Z,avatar:ee},t=n=>{const{className:i,children:l,...E}=n,L=O(m.navigation,i),s=K(l,r),c={Link:{wrapWith:e.createElement("li",null),className:m.item,unstyled:!0,Text:{className:m.text},Icon:{className:m.icon}}};return e.createElement("nav",{className:L,role:"navigation",...E},e.createElement(h,{props:c,mergeInParentContext:!0},e.createElement(J,{if:!s},e.createElement("ul",null,l))))};t.__docgenInfo={description:"",methods:[],displayName:"Navigation",props:{className:{required:!1,tsType:{name:"string"},description:""}},composes:["PropsWithChildren"]};const ae="flow--navigation--navigation-group",ne="flow--navigation--navigation-group--label",w={navigationGroup:ae,label:ne},r=n=>{const{children:i,className:l,collapsable:E,...L}=n,s=O(w.navigationGroup,l),c=R.useId(),v={Label:{id:c,className:w.label,"aria-hidden":!0},Link:{tunnelId:"links"}};return E?e.createElement(h,{mergeInParentContext:!0,props:v},e.createElement(N,null,e.createElement(z,{defaultExpanded:!0,className:s},i,e.createElement(F,{clearPropsContext:!1},e.createElement("ul",null,e.createElement(k,{id:"links"})))))):e.createElement(h,{mergeInParentContext:!0,props:v},e.createElement(N,null,e.createElement("section",{"aria-labelledby":c,className:s,...L},e.createElement(h,{mergeInParentContext:!0,props:v},i,e.createElement(k,{id:"Label"}),e.createElement("ul",null,e.createElement(k,{id:"links"}))))))};r.__docgenInfo={description:"",methods:[],displayName:"NavigationGroup",props:{collapsable:{required:!1,tsType:{name:"boolean"},description:""}},composes:["PropsWithChildren"]};const He={title:"Navigation/Navigation",component:t,parameters:{controls:{exclude:["className"]}}},p={render:n=>e.createElement(t,{"aria-label":"Companies",...n},e.createElement(a,{href:"https://www.apple.com"},"Apple"),e.createElement(a,{href:"https://www.mittwald.de","aria-current":"page"},"mittwald"),e.createElement(a,{href:"https://www.adobe.com"},"Adobe"),e.createElement(a,{href:"https://www.google.com"},"Google"))},u={render:n=>e.createElement(t,{"aria-label":"Main menu",...n},e.createElement(a,null,e.createElement(B,null),e.createElement(b,null,"Customer")),e.createElement(a,{"aria-current":"page"},e.createElement(H,null),e.createElement(b,null,"Server")),e.createElement(a,null,e.createElement($,null),e.createElement(b,null,"Project")))},g={render:n=>e.createElement(t,{"aria-label":"Main navigation",...n},e.createElement(r,null,e.createElement(o,null,"General"),e.createElement(a,{href:"#"},"Dashboard"),e.createElement(a,{href:"#","aria-current":"page"},"Performance")),e.createElement(r,null,e.createElement(o,null,"Components"),e.createElement(a,{href:"#"},"Apps"),e.createElement(a,{href:"#"},"Databases"),e.createElement(a,{href:"#"},"Domains")))},d={render:n=>e.createElement(t,{"aria-label":"Main navigation",...n},e.createElement(r,null,e.createElement(o,null,"General"),e.createElement(a,{href:"#"},"Dashboard"),e.createElement(a,{href:"#","aria-current":"page"},"Performance")),e.createElement(Q,null),e.createElement(r,null,e.createElement(o,null,"Components"),e.createElement(a,{href:"#"},"Apps"),e.createElement(a,{href:"#"},"Databases"),e.createElement(a,{href:"#"},"Domains")))},f={render:n=>e.createElement(t,{"aria-label":"Main navigation",...n},e.createElement(r,{collapsable:!0},e.createElement(o,null,"General"),e.createElement(a,{href:"#"},"Dashboard"),e.createElement(a,{href:"#","aria-current":"page"},"Performance")),e.createElement(r,{collapsable:!0},e.createElement(o,null,"Components"),e.createElement(a,{href:"#"},"Apps"),e.createElement(a,{href:"#"},"Databases"),e.createElement(a,{href:"#"},"Domains")))};var G,C,x;p.parameters={...p.parameters,docs:{...(G=p.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: props => <Navigation aria-label="Companies" {...props}>
+      <Link href="https://www.apple.com">Apple</Link>
+      <Link href="https://www.mittwald.de" aria-current="page">
+        mittwald
+      </Link>
+      <Link href="https://www.adobe.com">Adobe</Link>
+      <Link href="https://www.google.com">Google</Link>
+    </Navigation>
+}`,...(x=(C=p.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var P,D,I;u.parameters={...u.parameters,docs:{...(P=u.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: props => <Navigation aria-label="Main menu" {...props}>
+      <Link>
+        <IconCustomer />
+        <Text>Customer</Text>
+      </Link>
+      <Link aria-current="page">
+        <IconServer />
+        <Text>Server</Text>
+      </Link>
+      <Link>
+        <IconProject />
+        <Text>Project</Text>
+      </Link>
+    </Navigation>
+}`,...(I=(D=u.parameters)==null?void 0:D.docs)==null?void 0:I.source}}};var S,W,A;g.parameters={...g.parameters,docs:{...(S=g.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <Navigation aria-label="Main navigation" {...props}>
+      <NavigationGroup>
+        <Label>General</Label>
+        <Link href="#">Dashboard</Link>
+        <Link href="#" aria-current="page">
+          Performance
+        </Link>
+      </NavigationGroup>
+
+      <NavigationGroup>
+        <Label>Components</Label>
+        <Link href="#">Apps</Link>
+        <Link href="#">Databases</Link>
+        <Link href="#">Domains</Link>
+      </NavigationGroup>
+    </Navigation>
+}`,...(A=(W=g.parameters)==null?void 0:W.docs)==null?void 0:A.source}}};var T,y,M;d.parameters={...d.parameters,docs:{...(T=d.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <Navigation aria-label="Main navigation" {...props}>
+      <NavigationGroup>
+        <Label>General</Label>
+        <Link href="#">Dashboard</Link>
+        <Link href="#" aria-current="page">
+          Performance
+        </Link>
+      </NavigationGroup>
+
+      <Separator />
+
+      <NavigationGroup>
+        <Label>Components</Label>
+        <Link href="#">Apps</Link>
+        <Link href="#">Databases</Link>
+        <Link href="#">Domains</Link>
+      </NavigationGroup>
+    </Navigation>
+}`,...(M=(y=d.parameters)==null?void 0:y.docs)==null?void 0:M.source}}};var _,j,q;f.parameters={...f.parameters,docs:{...(_=f.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: props => <Navigation aria-label="Main navigation" {...props}>
+      <NavigationGroup collapsable>
+        <Label>General</Label>
+        <Link href="#">Dashboard</Link>
+        <Link href="#" aria-current="page">
+          Performance
+        </Link>
+      </NavigationGroup>
+
+      <NavigationGroup collapsable>
+        <Label>Components</Label>
+        <Link href="#">Apps</Link>
+        <Link href="#">Databases</Link>
+        <Link href="#">Domains</Link>
+      </NavigationGroup>
+    </Navigation>
+}`,...(q=(j=f.parameters)==null?void 0:j.docs)==null?void 0:q.source}}};const $e=["Default","WithIcons","WithGroups","WithSeparator","WithCollapsableGroups"];export{p as Default,f as WithCollapsableGroups,g as WithGroups,u as WithIcons,d as WithSeparator,$e as __namedExportsOrder,He as default};
