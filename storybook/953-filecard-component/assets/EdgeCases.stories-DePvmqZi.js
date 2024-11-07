b2d63082784e083db3e68f43717ba39e8f2b442d
mittwald <>
2024-11-07 08:05:12 +0000

    Update from https://github.com/mittwald/flow/commit/b3ac7fa3cde3fe2a1fa7fe5be08f847cd7d686db

---
+++ storybook/953-filecard-component/assets/EdgeCases.stories-DePvmqZi.js
@@ -0,0 +1,18 @@
+import a from"./Default.stories-C7oNIVHh.js";import{F as d}from"./FileCard-DJqK1WDf.js";import{d as s}from"./dummyText-CX_I_Wpl.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Avatar-Da21hVDL.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./react-children-utilities-CdZndyK3.js";import"./flowComponent-C7dqiiMm.js";import"./index-Cf-03bMR.js";import"./Text-nt2VY5KZ.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-Bnjm0IRl.js";import"./utils-Cd8r7pc9.js";import"./IconApp-C4Wlkpqb.js";import"./IconWarning-8RCo9PDY.js";import"./Link-BJ6dGsRk.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-Dcn7RY69.js";import"./useFocusRing-CqFdZJbR.js";import"./useFocusable-CKybZYMg.js";import"./Button-DUFiFWGa.js";import"./LoadingSpinner-C9n5moKL.js";import"./Button-DKDQv6qd.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";const M={...a,title:"Upload/FileCard/Edge Cases",component:d},o={args:{name:s.long,sizeInBytes:47500,onDelete:()=>{console.log("deleted")}}},e={args:{name:s.long,sizeInBytes:47500,onDelete:()=>{console.log("deleted")},href:"#"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      console.log("deleted");
+    }
+  }
+}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};var n,i,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      console.log("deleted");
+    },
+    href: "#"
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const P=["LongName","LongNameAndLink"];export{o as LongName,e as LongNameAndLink,P as __namedExportsOrder,M as default};
