9926039d4cb005369570c7e44965b81424b68de2
mittwald <>
2024-11-08 07:20:12 +0000

    Update from https://github.com/mittwald/flow/commit/710a1d17eb5db9bed63236773cb2dc904a816bb2

---
+++ storybook/add-white-background-to-file-card/assets/Default.stories-R_JgkSdq.js
@@ -0,0 +1,30 @@
+import{R as C}from"./index-Cs7sjTYM.js";import{F as i}from"./FileCard-C9DgvVCk.js";import{d as E}from"./dummyText-CX_I_Wpl.js";import{a as A}from"./chunk-D5ZWXAHU-CGElDDNX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./IconWarning-OhTjZCw4.js";import"./IconApp-C4Wlkpqb.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./flowComponent-C7dqiiMm.js";import"./index-Cf-03bMR.js";import"./Avatar-Da21hVDL.js";import"./react-children-utilities-CdZndyK3.js";import"./Image-C5NcMbe4.js";import"./Text-nt2VY5KZ.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-Bnjm0IRl.js";import"./utils-Cd8r7pc9.js";import"./Link-B1v0oZMI.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-Dcn7RY69.js";import"./useFocusRing-CqFdZJbR.js";import"./useFocusable-CKybZYMg.js";import"./Button-CLj7M7A6.js";import"./LoadingSpinner-e8Pq0YvL.js";import"./Button-DKDQv6qd.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./v4-CQkTLCs1.js";const he={title:"Upload/FileCard",component:i,args:{name:"image.jpg"},render:B=>C.createElement(i,{...B})},e={},r={args:{type:"image/png"}},t={args:{sizeInBytes:47500}},o={args:{onDelete:()=>{A("onDelete")}}},a={args:{href:"#"}},s={args:{href:"#",onDelete:()=>{A("onDelete")}}},m={args:{imageSrc:E.imageSrc}};var n,p,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,g,l;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    type: "image/png"
+  }
+}`,...(l=(g=r.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var u,h,D;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    sizeInBytes: 47500
+  }
+}`,...(D=(h=t.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};var S,W,f;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(f=(W=o.parameters)==null?void 0:W.docs)==null?void 0:f.source}}};var y,O,k;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    href: "#"
+  }
+}`,...(k=(O=a.parameters)==null?void 0:O.docs)==null?void 0:k.source}}};var x,z,I;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    href: "#",
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(I=(z=s.parameters)==null?void 0:z.docs)==null?void 0:I.source}}};var L,T,F;m.parameters={...m.parameters,docs:{...(L=m.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    imageSrc: dummyText.imageSrc
+  }
+}`,...(F=(T=m.parameters)==null?void 0:T.docs)==null?void 0:F.source}}};const De=["Default","WithType","WithSize","WithOnDelete","WithLink","WithLinkAndOnDelete","WithImage"];export{e as Default,m as WithImage,a as WithLink,s as WithLinkAndOnDelete,o as WithOnDelete,t as WithSize,r as WithType,De as __namedExportsOrder,he as default};
