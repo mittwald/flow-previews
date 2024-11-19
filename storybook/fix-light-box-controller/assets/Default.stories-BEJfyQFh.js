a089ab65349aae297cfedcd5dc8d2cddafd8d9f6
mittwald <>
2024-11-19 08:03:50 +0000

    Update from https://github.com/mittwald/flow/commit/f3e84af39e6d927f1bcdd4f0e9d513ff4e2a658b

---
+++ storybook/fix-light-box-controller/assets/Default.stories-BEJfyQFh.js
@@ -0,0 +1,19 @@
+import{L as r}from"./LabeledValue-C8O8zkjk.js";import{R as e}from"./index-Cs7sjTYM.js";import{L as m}from"./Label-BjTAT_L8.js";import{C as x}from"./Content-BaT5-SuQ.js";import{C as i}from"./CopyButton-CYAFjtR5.js";import{I as S}from"./InlineCode-Dxoiemgo.js";import{L as I}from"./Link-jn5gxHTR.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-ioG1zo33.js";import"./index-Cf-03bMR.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./Label-C-iBdzH7.js";import"./utils-Cd8r7pc9.js";import"./Hidden-C2kgQquV.js";import"./Wrap-DQq6jo70.js";import"./Button-BWPqBSre.js";import"./IconApp-DqdQf8xk.js";import"./IconWarning-Bz8aDAH7.js";import"./Text-Kq1Uppf8.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bnjm0IRl.js";import"./LoadingSpinner-CEUYaNew.js";import"./Button-DKDQv6qd.js";import"./ProgressBar-K2upHjuk.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CKybZYMg.js";import"./useFocusRing-CqFdZJbR.js";import"./usePress-Dcn7RY69.js";import"./Tooltip-CoEnvxI3.js";import"./OverlayArrow-BrbzJDKz.js";import"./useControlledState-B7K2eYzO.js";import"./react-children-utilities-CdZndyK3.js";import"./Action-Dto-FCwm.js";import"./context-Cfdsb8D2.js";import"./useSelector-CFdl5cbA.js";import"./getActionGroupSlot-B_XxGEdc.js";import"./dynamic-DKDa4OpU.js";const ue={title:"Content/LabeledValue",component:r,parameters:{controls:{exclude:["className"]}},render:t=>e.createElement(r,{...t},e.createElement(m,null,"Project"),e.createElement(x,null,"My proSpace"))},o={},a={render:t=>e.createElement(r,{...t},e.createElement(m,null,"Project"),e.createElement(x,null,"My proSpace"),e.createElement(i,{text:"My proSpace"}))},p={render:t=>e.createElement(r,{...t},e.createElement(m,null,"IP address"),e.createElement(S,null,"127.0.0.1"),e.createElement(i,{text:"127.0.0.1"}))},n={render:t=>e.createElement(r,{...t},e.createElement(m,null,"Domain"),e.createElement(I,null,"https://mittwald.de"),e.createElement(i,{text:"https://mittwald.de"}))};var l,s,c;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(c=(s=o.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var d,u,L;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Project</Label>
+      <Content>My proSpace</Content>
+      <CopyButton text="My proSpace" />
+    </LabeledValue>
+}`,...(L=(u=a.parameters)==null?void 0:u.docs)==null?void 0:L.source}}};var C,E,b;p.parameters={...p.parameters,docs:{...(C=p.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>IP address</Label>
+      <InlineCode>127.0.0.1</InlineCode>
+      <CopyButton text="127.0.0.1" />
+    </LabeledValue>
+}`,...(b=(E=p.parameters)==null?void 0:E.docs)==null?void 0:b.source}}};var y,f,h;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Domain</Label>
+      <Link>https://mittwald.de</Link>
+      <CopyButton text="https://mittwald.de" />
+    </LabeledValue>
+}`,...(h=(f=n.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};const Le=["Default","WithCopyButton","WithInlineCode","WithLink"];export{o as Default,a as WithCopyButton,p as WithInlineCode,n as WithLink,Le as __namedExportsOrder,ue as default};
---
+++ storybook/fix-light-box-controller/assets/EdgeCases.stories-Dy84MMDI.js
@@ -0,0 +1,25 @@
+import{R as p,a as l,b as g}from"./Radio-DQ1rARaU.js";import{R as e}from"./index-Cs7sjTYM.js";import{d as o}from"./dummyText-CX_I_Wpl.js";import y from"./Default.stories-C9eA9MYK.js";import{T as E}from"./Text-Kq1Uppf8.js";import{C as T}from"./Content-BaT5-SuQ.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./FormField.module-CsYi_t4Q.js";import"./IconApp-DqdQf8xk.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-ioG1zo33.js";import"./index-Cf-03bMR.js";import"./IconWarning-Bz8aDAH7.js";import"./FieldError-C27s8X8S.js";import"./utils-Cd8r7pc9.js";import"./Text-Bnjm0IRl.js";import"./filterDOMProps-CeZl_uWj.js";import"./ColumnLayout-BAUBZrLX.js";import"./deepHas-C3achdHB.js";import"./react-children-utilities-CdZndyK3.js";import"./RadioGroup-DL06ub0D.js";import"./Form-CN49erGr.js";import"./useFocusRing-CqFdZJbR.js";import"./useLabel-BA8WH6BO.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./useControlledState-B7K2eYzO.js";import"./FocusScope-DxaEUeh1.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./useFormReset-BmVKDtZT.js";import"./useFocusable-CKybZYMg.js";import"./usePress-Dcn7RY69.js";import"./VisuallyHidden-BOk7_Rfy.js";import"./Label-BjTAT_L8.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";const ne={title:"Form Controls/RadioGroup/Edge Cases",...y},t={render:a=>e.createElement(p,{...a,defaultValue:"a","aria-label":"Label"},e.createElement(l,{value:"a"},o.long," "),e.createElement(l,{value:"b"},o.long," "),e.createElement(l,{value:"c"},o.long))},m={render:a=>e.createElement(p,{...a,defaultValue:"0","aria-label":"Label",l:[1,1,1,1]},Array(6).fill("").map((v,r)=>e.createElement(g,{value:r.toString(),key:r},e.createElement(E,null,o.medium),e.createElement(T,null,o.medium))))},i={render:a=>e.createElement(p,{...a,defaultValue:"0","aria-label":"Label"},Array(3).fill("").map((v,r)=>e.createElement(g,{value:r.toString(),key:r},"Option ",r+1))),parameters:{viewport:{defaultViewport:"mobile1"}}};var n,u,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="a" aria-label="Label">
+      <Radio value="a">{dummyText.long} </Radio>
+      <Radio value="b">{dummyText.long} </Radio>
+      <Radio value="c">{dummyText.long}</Radio>
+    </RadioGroup>
+}`,...(d=(u=t.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var s,c,R;m.parameters={...m.parameters,docs:{...(s=m.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="0" aria-label="Label" l={[1, 1, 1, 1]}>
+      {Array(6).fill("").map((value, index) => <RadioButton value={index.toString()} key={index}>
+            <Text>{dummyText.medium}</Text>
+            <Content>{dummyText.medium}</Content>
+          </RadioButton>)}
+    </RadioGroup>
+}`,...(R=(c=m.parameters)==null?void 0:c.docs)==null?void 0:R.source}}};var f,b,x;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="0" aria-label="Label">
+      {Array(3).fill("").map((value, index) => <RadioButton value={index.toString()} key={index}>
+            Option {index + 1}
+          </RadioButton>)}
+    </RadioGroup>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(x=(b=i.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};const ue=["LongTexts","MultipleRadioButtons","SmallSpace"];export{t as LongTexts,m as MultipleRadioButtons,i as SmallSpace,ue as __namedExportsOrder,ne as default};
