90c708deda28be4cc6a62aebdff9d74d106867c0
mittwald <>
2024-11-25 14:31:37 +0000

    Update from https://github.com/mittwald/flow/commit/22b6068f250aa54cc6d92d4ca1e1b3b25f78b856

---
+++ storybook/add-suspense-to-notification/assets/Default.stories-Bk1ipzwk.js
@@ -0,0 +1,49 @@
+import{R as e}from"./index-Cs7sjTYM.js";import"./IconApp-DqdQf8xk.js";import{u as T,v as w,s as D,w as P,x as W}from"./IconWarning-Bz8aDAH7.js";import{L as p}from"./Link-jn5gxHTR.js";import{P as A}from"./PropsContextProvider-CRusYdr1.js";import{d as _}from"./dynamic-DKDa4OpU.js";import{c as G}from"./clsx-B-dksMZM.js";import{T as s}from"./Text-Kq1Uppf8.js";import{B as r}from"./Button-BWPqBSre.js";import{I as q}from"./Image-B1DXJdfx.js";import{d as R}from"./dummyText-CX_I_Wpl.js";import{A as O}from"./Avatar-C3nnJ_Ko.js";import{a as j,C as z,M as u}from"./ContextMenuTrigger-D3Q-NWPN.js";import{a as F,s as J}from"./storyBackgrounds-GFSAi-dI.js";import{a as K,M as Q}from"./ModalTrigger-H-D_YtgS.js";import{H as U}from"./Heading-DZV50dxJ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./flowComponent-ioG1zo33.js";import"./index-j_f0pOtZ.js";import"./index-Cf-03bMR.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./utils-Cd8r7pc9.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-Dcn7RY69.js";import"./useFocusRing-CqFdZJbR.js";import"./useFocusable-CKybZYMg.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Wrap-DQq6jo70.js";import"./Text-Bnjm0IRl.js";import"./LoadingSpinner-CEUYaNew.js";import"./Button-DKDQv6qd.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./react-children-utilities-CdZndyK3.js";import"./ContextMenu.module-68N5lvfm.js";import"./Popover-DkK9p4JA.js";import"./OverlayTrigger-BpSxnOsX.js";import"./RSPContexts-BseA8goh.js";import"./OverlayArrow-BrbzJDKz.js";import"./useControlledState-B7K2eYzO.js";import"./FocusScope-DxaEUeh1.js";import"./getScrollParent-CcmJp979.js";import"./VisuallyHidden-BOk7_Rfy.js";import"./context-Cfdsb8D2.js";import"./useSelector-CFdl5cbA.js";import"./Action-Dto-FCwm.js";import"./getActionGroupSlot-B_XxGEdc.js";import"./deepHas-C3achdHB.js";import"./Collection-BdcdgcY_.js";import"./CollectionBuilder-B4BE-ZOx.js";import"./Header-3zDVqz72.js";import"./Separator-ZOqStrt8.js";import"./useMenuTrigger-C6m_oYu1.js";import"./SelectionManager-BkcLa_O0.js";import"./useEvent-BjqsPY1I.js";import"./useCollator-tPR4bsZ_.js";import"./Overlay-DTawgEf6.js";import"./Header-B2_C_gbn.js";import"./Heading-CG14-WVD.js";const V="flow--header-navigation",X="flow--header-navigation--link",Y="flow--header-navigation--button",Z="flow--header-navigation--light",$="flow--header-navigation--dark",a={headerNavigation:V,link:X,button:Y,light:Z,dark:$},l=t=>{const{children:B,className:C,color:c="primary",...H}=t,S=G(a.headerNavigation,a[c],C),b={Link:{wrapWith:e.createElement("li",null),className:a.link,unstyled:!0,children:_(L=>e.createElement(s,{emulateBoldWidth:!0},L.children))},Button:{className:a.button,color:c,variant:"plain",wrapWith:e.createElement("li",null)}};return e.createElement("nav",{className:S,role:"navigation",...H},e.createElement("ul",null,e.createElement(A,{props:b},B)))};l.__docgenInfo={description:"",methods:[],displayName:"HeaderNavigation",props:{className:{required:!1,tsType:{name:"string"},description:""},color:{required:!1,tsType:{name:"union",raw:'"primary" | "dark" | "light"',elements:[{name:"literal",value:'"primary"'},{name:"literal",value:'"dark"'},{name:"literal",value:'"light"'}]},description:'@default "primary"'}},composes:["PropsWithChildren"]};const st={title:"Navigation/HeaderNavigation",component:l,parameters:{controls:{exclude:["className"]}},render:t=>e.createElement(l,{"aria-label":"Header navigation",...t},e.createElement(p,{href:"#"},"Getting startet"),e.createElement(p,{href:"#","aria-current":"page"},"Components"),e.createElement(r,null,e.createElement(T,null))),argTypes:{color:{control:"inline-radio",options:["primary","dark","light"]}}},o={},n={render:t=>e.createElement(l,{"aria-label":"Header navigation",...t},e.createElement(r,null,e.createElement(T,null)),e.createElement(r,null,e.createElement(w,null)),e.createElement(K,null,e.createElement(r,null,e.createElement(D,null)),e.createElement(Q,null,e.createElement(U,null,"Notifications"))),e.createElement(j,null,e.createElement(r,null,e.createElement(O,null,e.createElement(q,{alt:"Gopher",src:R.imageSrc}))),e.createElement(z,null,e.createElement(u,null,e.createElement(P,null),e.createElement(s,null,"Profile")),e.createElement(u,null,e.createElement(W,null),e.createElement(s,null,"Logout")))))},i={args:{color:"dark"},parameters:{backgrounds:F}},m={args:{color:"light"},parameters:{backgrounds:J}};var d,g,h;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(h=(g=o.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var f,E,v;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <HeaderNavigation aria-label="Header navigation" {...props}>
+      <Button>
+        <IconSearch />
+      </Button>
+      <Button>
+        <IconSupport />
+      </Button>
+      <ModalTrigger>
+        <Button>
+          <IconNotification />
+        </Button>
+        <Modal>
+          <Heading>Notifications</Heading>
+        </Modal>
+      </ModalTrigger>
+      <ContextMenuTrigger>
+        <Button>
+          <Avatar>
+            <Image alt="Gopher" src={dummyText.imageSrc} />
+          </Avatar>
+        </Button>
+        <ContextMenu>
+          <MenuItem>
+            <IconSettings />
+            <Text>Profile</Text>
+          </MenuItem>
+          <MenuItem>
+            <IconLogout />
+            <Text>Logout</Text>
+          </MenuItem>
+        </ContextMenu>
+      </ContextMenuTrigger>
+    </HeaderNavigation>
+}`,...(v=(E=n.parameters)==null?void 0:E.docs)==null?void 0:v.source}}};var k,x,M;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(M=(x=i.parameters)==null?void 0:x.docs)==null?void 0:M.source}}};var N,I,y;m.parameters={...m.parameters,docs:{...(N=m.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(y=(I=m.parameters)==null?void 0:I.docs)==null?void 0:y.source}}};const ct=["Default","WithContextMenu","Dark","Light"];export{i as Dark,o as Default,m as Light,n as WithContextMenu,ct as __namedExportsOrder,st as default};
---
+++ storybook/add-suspense-to-notification/assets/States.stories-39Sh2ws1.js
@@ -0,0 +1,36 @@
+import{B as W}from"./Button-BWPqBSre.js";import _ from"./Default.stories-DBpUqg9k.js";import"./IconApp-DqdQf8xk.js";import{R as r}from"./index-Cs7sjTYM.js";import{a as w,I as C}from"./IconWarning-Bz8aDAH7.js";import{T as O}from"./Text-Kq1Uppf8.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./Wrap-DQq6jo70.js";import"./flowComponent-ioG1zo33.js";import"./index-Cf-03bMR.js";import"./_commonjsHelpers-BosuxZz1.js";import"./LoadingSpinner-CEUYaNew.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./Button-DKDQv6qd.js";import"./utils-Cd8r7pc9.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CKybZYMg.js";import"./useFocusRing-CqFdZJbR.js";import"./usePress-Dcn7RY69.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./Avatar-C3nnJ_Ko.js";import"./react-children-utilities-CdZndyK3.js";import"./Tooltip-CoEnvxI3.js";import"./OverlayArrow-BrbzJDKz.js";import"./useControlledState-B7K2eYzO.js";import"./dummyText-CX_I_Wpl.js";import"./Image-B1DXJdfx.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bnjm0IRl.js";const Dr={..._,title:"Actions/Button/States",render:m=>r.createElement(W,{...m},r.createElement(O,null,"Add email address"),r.createElement(w,null))},e={args:{isDisabled:!0}},a={args:{isDisabled:!0,variant:"plain"}},t={args:{isPending:!0}},i={args:{isPending:!0,variant:"plain"}},o={args:{isPending:!0,variant:"plain"},render:m=>r.createElement(W,{...m,"aria-label":"Add to favorites"},r.createElement(C,null))},s={args:{isSucceeded:!0}},n={args:{isFailed:!0,variant:"plain"}};var p,d,c;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(c=(d=e.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var l,u,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    variant: "plain"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var P,S,v;t.parameters={...t.parameters,docs:{...(P=t.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    isPending: true
+  }
+}`,...(v=(S=t.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var b,D,f;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  }
+}`,...(f=(D=i.parameters)==null?void 0:D.docs)==null?void 0:f.source}}};var E,I,B;o.parameters={...o.parameters,docs:{...(E=o.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  },
+  render: props => <Button {...props} aria-label="Add to favorites">
+      <IconPlus />
+    </Button>
+}`,...(B=(I=o.parameters)==null?void 0:I.docs)==null?void 0:B.source}}};var A,F,h;s.parameters={...s.parameters,docs:{...(A=s.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    isSucceeded: true
+  }
+}`,...(h=(F=s.parameters)==null?void 0:F.docs)==null?void 0:h.source}}};var x,R,T;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    isFailed: true,
+    variant: "plain"
+  }
+}`,...(T=(R=n.parameters)==null?void 0:R.docs)==null?void 0:T.source}}};const fr=["DisabledSolid","DisabledPlain","PendingSolid","PendingPlain","PendingPlainWithIcon","SucceededSolid","FailedPlain"];export{a as DisabledPlain,e as DisabledSolid,n as FailedPlain,i as PendingPlain,o as PendingPlainWithIcon,t as PendingSolid,s as SucceededSolid,fr as __namedExportsOrder,Dr as default};
