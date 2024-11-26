9fe20ffeca9a5e1cb2271d925760e9c3232b5141
mittwald <>
2024-11-26 06:46:01 +0000

    Update from https://github.com/mittwald/flow/commit/56561f13fc9a8750f40f5e95f5b69ca2f6902f5b

---
+++ storybook/add-context-menu-user-section/assets/Default.stories-D01sal4g.js
@@ -0,0 +1,28 @@
+import{R as C}from"./index-Cs7sjTYM.js";import{C as s}from"./CodeBlock-Wt2jURwu.js";import{a as D,s as S}from"./storyBackgrounds-GFSAi-dI.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./CopyButton-A5iglgOn.js";import"./Button-BIP8WbnA.js";import"./PropsContextProvider-CRusYdr1.js";import"./index-j_f0pOtZ.js";import"./IconApp-Dl7D1yMd.js";import"./flowComponent-ioG1zo33.js";import"./index-Cf-03bMR.js";import"./IconWarning-D0l2tnhs.js";import"./Wrap-DQq6jo70.js";import"./Text-Kq1Uppf8.js";import"./EmulatedBoldText-Dmze-Iwg.js";import"./Text-Bnjm0IRl.js";import"./utils-Cd8r7pc9.js";import"./LoadingSpinner-OWwH84Wh.js";import"./useLocalizedStringFormatter-TZkCr3Qr.js";import"./Button-DKDQv6qd.js";import"./ProgressBar-K2upHjuk.js";import"./Label-C-iBdzH7.js";import"./Hidden-C2kgQquV.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BA8WH6BO.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CKybZYMg.js";import"./useFocusRing-CqFdZJbR.js";import"./usePress-Dcn7RY69.js";import"./Tooltip-CoEnvxI3.js";import"./OverlayArrow-BrbzJDKz.js";import"./useControlledState-B7K2eYzO.js";import"./react-children-utilities-CdZndyK3.js";import"./Action-Dto-FCwm.js";import"./context-Cfdsb8D2.js";import"./useSelector-CFdl5cbA.js";import"./getActionGroupSlot-B_XxGEdc.js";import"./dynamic-DKDa4OpU.js";const ur={title:"Content/CodeBlock",component:s,render:B=>C.createElement(s,{...B,code:`{
+    "projectId": "b3a96db5-ba8f-40dd-9100-bab43ac1f698",
+    "name": "My Project"
+}`}),args:{language:"json"}},r={},o={args:{showLineNumbers:!0}},e={args:{copyable:!0}},t={args:{color:"dark",copyable:!0},parameters:{backgrounds:D}},a={args:{color:"light",copyable:!0},parameters:{backgrounds:S}};var m,p,c;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(c=(p=r.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var i,n,u;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    showLineNumbers: true
+  }
+}`,...(u=(n=o.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};var d,g,l;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    copyable: true
+  }
+}`,...(l=(g=e.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var b,k,y;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: "dark",
+    copyable: true
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(y=(k=t.parameters)==null?void 0:k.docs)==null?void 0:y.source}}};var h,f,L;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    color: "light",
+    copyable: true
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(L=(f=a.parameters)==null?void 0:f.docs)==null?void 0:L.source}}};const dr=["Default","WithLineNumbers","Copyable","Dark","Light"];export{e as Copyable,t as Dark,r as Default,a as Light,o as WithLineNumbers,dr as __namedExportsOrder,ur as default};
