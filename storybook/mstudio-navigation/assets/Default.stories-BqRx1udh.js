b97495c465ae661228af4cfeb2fb904e2b7a38e9
mittwald <>
2024-12-04 06:57:11 +0000

    Update from https://github.com/mittwald/flow/commit/c223a13ba59f286ed63d297e3677075cb92f55c0

---
+++ storybook/mstudio-navigation/assets/Default.stories-BqRx1udh.js
@@ -0,0 +1,19 @@
+import{L as r}from"./LabeledValue-BhOSQRj6.js";import{R as e}from"./index-B-o1Wr-g.js";import{L as m}from"./Label-DmbcA-Bx.js";import{C as x}from"./Content-DnB3Z0p2.js";import{C as i}from"./CopyButton-D8JVM0PD.js";import{I as S}from"./InlineCode-B3WUYwD5.js";import{L as I}from"./Link-DV8ZjRND.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DWb7RnwM.js";import"./index-ynQ6n1pc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./flowComponent-D6-TVbjo.js";import"./index-DolzVqEf.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Label-BFvRcNt5.js";import"./utils-CCebJm36.js";import"./Hidden-CRdWNeH8.js";import"./Wrap-DQq6jo70.js";import"./Button-CEPV1PYe.js";import"./IconApp-BTi5eST8.js";import"./IconWarning-C5JDBu26.js";import"./Text-B5qFq-QV.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Text-CByKVUtS.js";import"./LoadingSpinner-DIqKivrC.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CjESOWAx.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./Tooltip-zyFT3NFh.js";import"./OverlayArrow-BMY-498z.js";import"./useControlledState-BgxdDqm0.js";import"./react-children-utilities-D14ImiYi.js";import"./Action-B-qRylWW.js";import"./context-Rc-flGIq.js";import"./useSelector-D83Ddg6S.js";import"./getActionGroupSlot-KvD6pyGF.js";import"./dynamic-DKDa4OpU.js";const ue={title:"Content/LabeledValue",component:r,parameters:{controls:{exclude:["className"]}},render:t=>e.createElement(r,{...t},e.createElement(m,null,"Project"),e.createElement(x,null,"My proSpace"))},o={},a={render:t=>e.createElement(r,{...t},e.createElement(m,null,"Project"),e.createElement(x,null,"My proSpace"),e.createElement(i,{text:"My proSpace"}))},p={render:t=>e.createElement(r,{...t},e.createElement(m,null,"IP address"),e.createElement(S,null,"127.0.0.1"),e.createElement(i,{text:"127.0.0.1"}))},n={render:t=>e.createElement(r,{...t},e.createElement(m,null,"Domain"),e.createElement(I,null,"https://mittwald.de"),e.createElement(i,{text:"https://mittwald.de"}))};var l,s,c;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(c=(s=o.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var d,u,L;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Project</Label>
+      <Content>My proSpace</Content>
+      <CopyButton text="My proSpace" />
+    </LabeledValue>
+}`,...(L=(u=a.parameters)==null?void 0:u.docs)==null?void 0:L.source}}};var C,E,b;p.parameters={...p.parameters,docs:{...(C=p.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>IP address</Label>
+      <InlineCode>127.0.0.1</InlineCode>
+      <CopyButton text="127.0.0.1" />
+    </LabeledValue>
+}`,...(b=(E=p.parameters)==null?void 0:E.docs)==null?void 0:b.source}}};var y,f,h;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Domain</Label>
+      <Link>https://mittwald.de</Link>
+      <CopyButton text="https://mittwald.de" />
+    </LabeledValue>
+}`,...(h=(f=n.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};const Le=["Default","WithCopyButton","WithInlineCode","WithLink"];export{o as Default,a as WithCopyButton,p as WithInlineCode,n as WithLink,Le as __namedExportsOrder,ue as default};
