fd954ba268a0a29f50b94f436ad0b5b0e08ee6a1
mittwald <>
2024-12-05 13:15:32 +0000

    Update from https://github.com/mittwald/flow/commit/317ac7e9ae2d7b9ef80e414640aaf49a7ac19544

---
+++ storybook/add-forward-icon/assets/EdgeCases.stories-CoBythdD.js
@@ -0,0 +1,30 @@
+import{S as c}from"./Section-DAn1xlX5.js";import{R as t}from"./index-B-o1Wr-g.js";import{H as u}from"./Heading-Bn-Oekdv.js";import{T as d}from"./Text-B5qFq-QV.js";import{S as l}from"./Switch-CbnX0cPw.js";import S from"./Default.stories-BV0-BbCM.js";import{d as g}from"./dummyText-CX_I_Wpl.js";import{H as b}from"./Header-CEYghI-s.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DWb7RnwM.js";import"./index-ynQ6n1pc.js";import"./flowComponent-D6-TVbjo.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./ContextMenu.module-37Y7DEq9.js";import"./Collection-DyVww_gO.js";import"./CollectionBuilder-CRjClK3P.js";import"./Hidden-CRdWNeH8.js";import"./Header-Bk-D7IzI.js";import"./utils-CCebJm36.js";import"./Heading--STTcIhS.js";import"./RSPContexts-BT50PPhj.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-CByKVUtS.js";import"./IconApp-BTi5eST8.js";import"./IconWarning-fFpik5u2.js";import"./Label-DmbcA-Bx.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Label-BFvRcNt5.js";import"./filterDOMProps-CeZl_uWj.js";import"./useToggleState-93Gd9adu.js";import"./useFormReset-DMREOE0P.js";import"./useFocusable-CjESOWAx.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./useControlledState-BgxdDqm0.js";import"./VisuallyHidden-lWpahT0S.js";import"./TextField-Dzkn0yTq.js";import"./TextFieldBase-DBEn_Dww.js";import"./FieldError-BglEnUNZ.js";import"./FieldError-CjNivrvv.js";import"./FieldDescription-DUHOTkLn.js";import"./Form-B_VAMaUE.js";import"./useLabel-DrlXsFf-.js";import"./Input-IHjXEmTc.js";import"./useTextField-EYrZGk_G.js";import"./Link-C-2PiW_y.js";import"./AlertBadge-Dd-A0SaW.js";import"./AlertIcon-1C6j6emi.js";import"./Button-C7_4lgO6.js";import"./LoadingSpinner-CMpmsqdX.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./Action-B-qRylWW.js";import"./context-Rc-flGIq.js";import"./useSelector-D83Ddg6S.js";import"./getActionGroupSlot-KvD6pyGF.js";import"./dynamic-DKDa4OpU.js";import"./ModalTrigger-C60e16O2.js";import"./Overlay-DDQPS4Hh.js";import"./OverlayTrigger-CbJR2aUU.js";import"./OverlayArrow-BMY-498z.js";import"./Separator-BAc0WD8W.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./ContextualHelpTrigger-BYiKOZyL.js";import"./Popover-CW9-jQa3.js";import"./ContextMenuTrigger-BCT96JXh.js";import"./deepHas-qlU7dqTi.js";import"./react-children-utilities-D14ImiYi.js";import"./Avatar-otkdn0iw.js";const zt={...S,title:"Structure/Section/Edge Cases"},r={render:o=>t.createElement(c,{...o},t.createElement(b,null,t.createElement(u,null,g.medium),t.createElement(l,null,"Subscribed")),t.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."))},e={render:o=>t.createElement(c,{...o},t.createElement(b,null,t.createElement(u,null,"Newsletter Subscription"),t.createElement(l,null,"Subscribed")),t.createElement(d,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date.")),parameters:{viewport:{defaultViewport:"mobile1"}}};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>{dummyText.medium}</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,a,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter Subscription</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const At=["WithLongHeading","SmallSpace"];export{e as SmallSpace,r as WithLongHeading,At as __namedExportsOrder,zt as default};
---
+++ storybook/add-forward-icon/assets/InputButton.stories-CDqoLc6Y.js
@@ -0,0 +1,29 @@
+import{R as e}from"./index-B-o1Wr-g.js";import{A as r}from"./Align-DLwV5EOX.js";import{T}from"./TextField-Dzkn0yTq.js";import{L as l}from"./Label-DmbcA-Bx.js";import{B as o}from"./Button-C7_4lgO6.js";import h from"./Default.stories-Cz-jEkzV.js";import{N}from"./NumberField-Qd6FWjgk.js";import{T as W}from"./TextArea-Dt6X1t7m.js";import{S as F,O as s}from"./Option-D0JxTnwQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DWb7RnwM.js";import"./index-ynQ6n1pc.js";import"./flowComponent-D6-TVbjo.js";import"./index-DolzVqEf.js";import"./TextFieldBase-DBEn_Dww.js";import"./FieldError-BglEnUNZ.js";import"./IconApp-BTi5eST8.js";import"./IconWarning-fFpik5u2.js";import"./FieldError-CjNivrvv.js";import"./useFocusRing-CTDVvBmm.js";import"./utils-CCebJm36.js";import"./Text-CByKVUtS.js";import"./filterDOMProps-CeZl_uWj.js";import"./FieldDescription-DUHOTkLn.js";import"./Text-B5qFq-QV.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Form-B_VAMaUE.js";import"./useLabel-DrlXsFf-.js";import"./Input-IHjXEmTc.js";import"./Hidden-CRdWNeH8.js";import"./Label-BFvRcNt5.js";import"./useTextField-EYrZGk_G.js";import"./useFormReset-DMREOE0P.js";import"./useControlledState-BgxdDqm0.js";import"./useFocusable-CjESOWAx.js";import"./LoadingSpinner-CMpmsqdX.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-C3EQNDer.js";import"./Avatar-otkdn0iw.js";import"./react-children-utilities-D14ImiYi.js";import"./Initials-Hu0TJYpG.js";import"./Group-CFRZOSwE.js";import"./useSpinButton-BbqLYcl6.js";import"./useEvent-CmPtILZD.js";import"./Popover-CW9-jQa3.js";import"./OverlayTrigger-CbJR2aUU.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-BMY-498z.js";import"./Collection-DyVww_gO.js";import"./CollectionBuilder-CRjClK3P.js";import"./Header-Bk-D7IzI.js";import"./Separator-BAc0WD8W.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./context-Rc-flGIq.js";import"./useSelector-D83Ddg6S.js";import"./ListBox-CqxyM-24.js";import"./DragAndDrop-1_tMLeAb.js";import"./useListState-DEKbwa90.js";import"./useSingleSelectListState-DL4uThJu.js";const Je={...h,title:"Structure/Align/Input + Button",component:r,render:t=>e.createElement(r,{...t},e.createElement(T,null,e.createElement(l,null,"Mail address")),e.createElement(o,null,"Hinzufügen"))},n={},m={render:t=>e.createElement(r,{...t},e.createElement(N,null,e.createElement(l,null,"Number")),e.createElement(o,null,"Hinzufügen"))},i={render:t=>e.createElement(r,{...t},e.createElement(W,null,e.createElement(l,null,"Message")),e.createElement(o,null,"Hinzufügen"))},p={render:t=>e.createElement(r,{...t},e.createElement(F,null,e.createElement(l,null,"Options"),e.createElement(s,null,"Option 1"),e.createElement(s,null,"Option 2")),e.createElement(o,null,"Hinzufügen"))},a={render:t=>e.createElement(r,{...t},e.createElement(T,null),e.createElement(o,null,"Hinzufügen"))};var c,u,d;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(u=n.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,E,f;m.parameters={...m.parameters,docs:{...(g=m.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <NumberField>
+        <Label>Number</Label>
+      </NumberField>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(f=(E=m.parameters)==null?void 0:E.docs)==null?void 0:f.source}}};var b,A,O;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextArea>
+        <Label>Message</Label>
+      </TextArea>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(O=(A=i.parameters)==null?void 0:A.docs)==null?void 0:O.source}}};var S,B,L;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <Select>
+        <Label>Options</Label>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+      </Select>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(L=(B=p.parameters)==null?void 0:B.docs)==null?void 0:L.source}}};var x,z,H;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextField />
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(H=(z=a.parameters)==null?void 0:z.docs)==null?void 0:H.source}}};const Ke=["Default","WithNumberField","WithTextArea","WithSelect","WithoutLabel"];export{n as Default,m as WithNumberField,p as WithSelect,i as WithTextArea,a as WithoutLabel,Ke as __namedExportsOrder,Je as default};
