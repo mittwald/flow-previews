b085ab09ff1aaddd9b6ce51e129da1e10cae8dcb
mittwald <>
2024-12-06 07:41:09 +0000

    Update from https://github.com/mittwald/flow/commit/ca823296a9b23ccafc6a6fa724290d0d3de4cfa7

---
+++ storybook/add-autoresponder-icon/assets/Sizes.stories-DJT-INwA.js
@@ -0,0 +1,13 @@
+import{R as g}from"./index-B-o1Wr-g.js";import u from"./Default.stories-DmhBlu_g.js";import"./IconApp-BTi5eST8.js";import{g as z}from"./IconWarning-fFpik5u2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DWb7RnwM.js";import"./index-ynQ6n1pc.js";import"./flowComponent-D6-TVbjo.js";import"./index-DolzVqEf.js";const h={...u,title:"Content/Icon/Sizes",args:{"aria-label":"home"},render:l=>g.createElement(z,{...l})},r={args:{size:"s"}},e={args:{size:"m"}},s={args:{size:"l"}};var a,o,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    size: "s"
+  }
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var m,c,i;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: "m"
+  }
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var n,p,d;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: "l"
+  }
+}`,...(d=(p=s.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const C=["Small","Medium","Large"];export{s as Large,e as Medium,r as Small,C as __namedExportsOrder,h as default};
