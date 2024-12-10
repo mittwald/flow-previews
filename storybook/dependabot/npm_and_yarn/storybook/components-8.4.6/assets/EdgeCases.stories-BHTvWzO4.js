3c5c90e960c5a095f0a95047870508a676d6b2c3
mittwald <>
2024-12-10 07:51:58 +0000

    Update from https://github.com/mittwald/flow/commit/11dd40112a65caa36ffde712f3f221f89b111fbf

---
+++ storybook/dependabot/npm_and_yarn/storybook/components-8.4.6/assets/EdgeCases.stories-BHTvWzO4.js
@@ -0,0 +1,8 @@
+import{m as s,P as i}from"./Default.stories-CUDRa-5w.js";import{L as p}from"./Label-Bz1FhDHf.js";import{R as e}from"./index-B-o1Wr-g.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./ProgressBar-C7LzY9et.js";import"./utils-CCebJm36.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Label-BFvRcNt5.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./flowComponent-DtNydOjY.js";const w={...s,title:"Status/ProgressBar/Edge Cases"},r={render:m=>e.createElement(i,{value:500,maxValue:1e3,minValue:0,showMaxValue:!0,formatOptions:{style:"unit",unit:"gigabyte"},...m},e.createElement(p,null,n.medium))};var t,o,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: props => <ProgressBar value={500} maxValue={1000} minValue={0} showMaxValue formatOptions={{
+    style: "unit",
+    unit: "gigabyte"
+  }} {...props}>
+      <Label>{dummyText.medium}</Label>
+    </ProgressBar>
+}`,...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const M=["LongText"];export{r as LongText,M as __namedExportsOrder,w as default};
---
+++ storybook/dependabot/npm_and_yarn/storybook/components-8.4.6/assets/Variants.stories-BAn1ZrjM.js
@@ -0,0 +1,16 @@
+import{A as o}from"./AlertBadge-kMwwa3Xn.js";import D from"./Default.stories-D7mU3sC6.js";import{R as n}from"./index-B-o1Wr-g.js";import"./clsx-B-dksMZM.js";import"./AlertIcon-CILOdubV.js";import"./IconApp-Ch-QazpE.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-DtNydOjY.js";import"./index-DolzVqEf.js";import"./IconWarning-BZUIPdJF.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Text-f9zdzMsn.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-CByKVUtS.js";import"./utils-CCebJm36.js";const F={...D,title:"Status/AlertBadge/Status"},e={},s={render:r=>n.createElement(o,{...r},"Success"),args:{status:"success"}},a={render:r=>n.createElement(o,{...r},"Warning"),args:{status:"warning"}},t={render:r=>n.createElement(o,{...r},"Danger"),args:{status:"danger"}};var p,c,m;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var i,d,g;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Success</AlertBadge>,
+  args: {
+    status: "success"
+  }
+}`,...(g=(d=s.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,l,S;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Warning</AlertBadge>,
+  args: {
+    status: "warning"
+  }
+}`,...(S=(l=a.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};var A,B,f;t.parameters={...t.parameters,docs:{...(A=t.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Danger</AlertBadge>,
+  args: {
+    status: "danger"
+  }
+}`,...(f=(B=t.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};const G=["Info","Success","Warning","Danger"];export{t as Danger,e as Info,s as Success,a as Warning,G as __namedExportsOrder,F as default};
