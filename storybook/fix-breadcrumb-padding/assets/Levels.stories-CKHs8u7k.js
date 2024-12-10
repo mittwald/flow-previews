116c69acc688b661229d588878b7c6571215a4bb
mittwald <>
2024-12-10 15:05:59 +0000

    Update from https://github.com/mittwald/flow/commit/1301a7fcb85627dc4137cd53837791bff9a66161

---
+++ storybook/fix-breadcrumb-padding/assets/Levels.stories-CKHs8u7k.js
@@ -0,0 +1,25 @@
+import{H as O}from"./Heading-DQ0-u3p0.js";import $ from"./Default.stories-1R_tA_7j.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-SXQnFwcy.js";import"./index-DolzVqEf.js";import"./Heading--STTcIhS.js";import"./RSPContexts-BT50PPhj.js";import"./utils-CCebJm36.js";import"./IconApp--rto3B0B.js";import"./IconWarning--ZE28x2f.js";import"./storyBackgrounds-GFSAi-dI.js";const K={...$,title:"Content/Heading/Levels",component:O},r={args:{level:1}},e={args:{level:2}},s={args:{level:3}},a={args:{level:4}},o={args:{level:5}},t={args:{level:6}};var c,m,n;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    level: 1
+  }
+}`,...(n=(m=r.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var p,l,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    level: 2
+  }
+}`,...(i=(l=e.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};var d,g,u;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    level: 3
+  }
+}`,...(u=(g=s.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var H,v,S;a.parameters={...a.parameters,docs:{...(H=a.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    level: 4
+  }
+}`,...(S=(v=a.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var f,x,_;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    level: 5
+  }
+}`,...(_=(x=o.parameters)==null?void 0:x.docs)==null?void 0:_.source}}};var C,E,L;t.parameters={...t.parameters,docs:{...(C=t.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    level: 6
+  }
+}`,...(L=(E=t.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};const M=["H1","H2","H3","H4","H5","H6"];export{r as H1,e as H2,s as H3,a as H4,o as H5,t as H6,M as __namedExportsOrder,K as default};
