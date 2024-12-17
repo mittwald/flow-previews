128e27d50c2b309964d863c96a9f16be7cfbbe7b
mittwald <>
2024-12-17 07:04:27 +0000

    Cleanup merged Storybook previews

--- storybook/1070-tausende-icon-chunks/assets/Default.stories-DengEPrF.js
+++
@@ -1,9 +0,0 @@
-import{S as t}from"./Skeleton-DAM5ch2x.js";import{R as k}from"./index-B-o1Wr-g.js";import{s as g}from"./storyBackgrounds-GFSAi-dI.js";import"./clsx-B-dksMZM.js";import"./_commonjsHelpers-Cpj98o6Y.js";const y={title:"Content/Skeleton",component:t,render:o=>k.createElement(t,{...o}),parameters:{controls:{exclude:["className"]}}},e={},r={parameters:{backgrounds:g}},a={render:o=>k.createElement(t,{...o,style:{height:"200px"}})};var s,n,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(c=(n=e.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var m,p,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  parameters: {
-    backgrounds: storyBackgroundDark
-  }
-}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var u,i,l;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
-  render: props => <Skeleton {...props} style={{
-    height: "200px"
-  }} />
-}`,...(l=(i=a.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const B=["Default","OnDarkBackground","WithCustomSize"];export{e as Default,r as OnDarkBackground,a as WithCustomSize,B as __namedExportsOrder,y as default};
