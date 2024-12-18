7fb3892f9792c53e19a4b3afdb09c0798c7d2503
mittwald <>
2024-12-18 12:53:30 +0000

    Update from https://github.com/mittwald/flow/commit/50d0fadc695e6e69722fc9bef25eab9b674b688a

---
+++ storybook/fix-avatar-color/assets/Default.stories-1Y-gIvlf.js
@@ -0,0 +1,23 @@
+import{R as e}from"./index-B-o1Wr-g.js";import{D as m}from"./DateRangePicker-Cp47hhFj.js";import{L as p}from"./Label-BFoBZBqy.js";import{F as k}from"./FieldDescription-C1Olr54J.js";import{$ as E,a as L,b as s}from"./DateField-B55wiFcA.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./FieldError-BWRuv_hr.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./IconApp--rto3B0B.js";import"./flowComponent-SXQnFwcy.js";import"./index-DolzVqEf.js";import"./IconWarning-C7RtCnUV.js";import"./FieldError-CjNivrvv.js";import"./useFocusRing-CTDVvBmm.js";import"./utils-CCebJm36.js";import"./Text-CByKVUtS.js";import"./filterDOMProps-CeZl_uWj.js";import"./Popover-DAQfSrhS.js";import"./OverlayTrigger-DBMCsseW.js";import"./context-HWij4CCA.js";import"./useSelector-iQqgW7gO.js";import"./Dialog-EQBJGkcZ.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./Label-BFvRcNt5.js";import"./Hidden-CRdWNeH8.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CjESOWAx.js";import"./usePress-C3EQNDer.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-BMY-498z.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-D4kl22nw.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./RangeCalendar-BQ3KmWKR.js";import"./Form-B_VAMaUE.js";import"./Group-CFRZOSwE.js";import"./Button-TH90Unv1.js";import"./Wrap-DQq6jo70.js";import"./Text-DPEzzey4.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./LoadingSpinner-D3THsmJZ.js";import"./Heading--STTcIhS.js";import"./useUpdateEffect-D5ZsYDSi.js";import"./Input-IHjXEmTc.js";import"./useFormReset-DMREOE0P.js";import"./useSpinButton-BbqLYcl6.js";const ve={title:"Form Controls/DateRangePicker",component:m,render:r=>e.createElement(m,{...r,isRequired:!0},e.createElement(p,null,"Date")),parameters:{controls:{exclude:["errorMessage"]}}},t={},a={args:{isDisabled:!0}},o={args:{minValue:E(L())},render:r=>e.createElement(m,{isRequired:!0,defaultValue:{start:s("2012-07-03"),end:s("2012-07-04")},...r,isInvalid:!0,errorMessage:"Date is in the past"},e.createElement(p,null,"Future Date"))},i={args:{minValue:E(L())},render:r=>e.createElement(m,{isRequired:!0,...r},e.createElement(p,null,"Future Date"),e.createElement(k,null,"Select a future date"))};var n,c,l;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,d,D;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(D=(d=a.parameters)==null?void 0:d.docs)==null?void 0:D.source}}};var f,g,b;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DateRangePicker isRequired defaultValue={{
+    start: parseDate("2012-07-03"),
+    end: parseDate("2012-07-04")
+  }} {...props} isInvalid errorMessage="Date is in the past">
+      <Label>Future Date</Label>
+    </DateRangePicker>
+}`,...(b=(g=o.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var R,F,$;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DateRangePicker isRequired {...props}>
+      <Label>Future Date</Label>
+      <FieldDescription>Select a future date</FieldDescription>
+    </DateRangePicker>
+}`,...($=(F=i.parameters)==null?void 0:F.docs)==null?void 0:$.source}}};const ye=["Default","Disabled","Invalid","FutureDatesOnly"];export{t as Default,a as Disabled,i as FutureDatesOnly,o as Invalid,ye as __namedExportsOrder,ve as default};
---
+++ storybook/fix-avatar-color/assets/InputButton.stories-DKhMNBmq.js
@@ -0,0 +1,29 @@
+import{R as e}from"./index-B-o1Wr-g.js";import{A as r}from"./Align-7zumIWot.js";import{T}from"./TextField-7a5bPdUH.js";import{L as l}from"./Label-BFoBZBqy.js";import{B as o}from"./Button-TH90Unv1.js";import h from"./Default.stories-DZGqZLzY.js";import{N}from"./NumberField-Do1H8xmo.js";import{T as W}from"./TextArea-DuwdnJ-R.js";import{S as F,O as s}from"./CountryOptions-Cup3gW8c.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-SXQnFwcy.js";import"./index-DolzVqEf.js";import"./TextFieldBase-DjKEWBs4.js";import"./FieldError-BWRuv_hr.js";import"./IconApp--rto3B0B.js";import"./IconWarning-C7RtCnUV.js";import"./FieldError-CjNivrvv.js";import"./useFocusRing-CTDVvBmm.js";import"./utils-CCebJm36.js";import"./Text-CByKVUtS.js";import"./filterDOMProps-CeZl_uWj.js";import"./FieldDescription-C1Olr54J.js";import"./Text-DPEzzey4.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Form-B_VAMaUE.js";import"./useLabel-DrlXsFf-.js";import"./Input-IHjXEmTc.js";import"./Hidden-CRdWNeH8.js";import"./Label-BFvRcNt5.js";import"./useTextField-EYrZGk_G.js";import"./useFormReset-DMREOE0P.js";import"./useControlledState-BgxdDqm0.js";import"./useFocusable-CjESOWAx.js";import"./LoadingSpinner-D3THsmJZ.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-C3EQNDer.js";import"./Avatar-D_KFb-03.js";import"./react-children-utilities-D14ImiYi.js";import"./deepHas-qlU7dqTi.js";import"./Initials-DV3qKvQK.js";import"./Group-CFRZOSwE.js";import"./useSpinButton-BbqLYcl6.js";import"./useEvent-CmPtILZD.js";import"./Popover-DAQfSrhS.js";import"./OverlayTrigger-DBMCsseW.js";import"./context-HWij4CCA.js";import"./useSelector-iQqgW7gO.js";import"./Dialog-EQBJGkcZ.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-BMY-498z.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-D4kl22nw.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./ListBox-DYVV_XWq.js";import"./DragAndDrop-1_tMLeAb.js";import"./useListState-DEKbwa90.js";import"./useSingleSelectListState-DL4uThJu.js";const Pe={...h,title:"Structure/Align/Input + Button",component:r,render:t=>e.createElement(r,{...t},e.createElement(T,null,e.createElement(l,null,"Mail address")),e.createElement(o,null,"Hinzufügen"))},n={},m={render:t=>e.createElement(r,{...t},e.createElement(N,null,e.createElement(l,null,"Number")),e.createElement(o,null,"Hinzufügen"))},i={render:t=>e.createElement(r,{...t},e.createElement(W,null,e.createElement(l,null,"Message")),e.createElement(o,null,"Hinzufügen"))},p={render:t=>e.createElement(r,{...t},e.createElement(F,null,e.createElement(l,null,"Options"),e.createElement(s,null,"Option 1"),e.createElement(s,null,"Option 2")),e.createElement(o,null,"Hinzufügen"))},a={render:t=>e.createElement(r,{...t},e.createElement(T,null),e.createElement(o,null,"Hinzufügen"))};var c,u,d;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(u=n.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,E,f;m.parameters={...m.parameters,docs:{...(g=m.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <NumberField>
+        <Label>Number</Label>
+      </NumberField>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(f=(E=m.parameters)==null?void 0:E.docs)==null?void 0:f.source}}};var b,A,O;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextArea>
+        <Label>Message</Label>
+      </TextArea>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(O=(A=i.parameters)==null?void 0:A.docs)==null?void 0:O.source}}};var S,B,L;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <Select>
+        <Label>Options</Label>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+      </Select>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(L=(B=p.parameters)==null?void 0:B.docs)==null?void 0:L.source}}};var x,z,H;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextField />
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(H=(z=a.parameters)==null?void 0:z.docs)==null?void 0:H.source}}};const Qe=["Default","WithNumberField","WithTextArea","WithSelect","WithoutLabel"];export{n as Default,m as WithNumberField,p as WithSelect,i as WithTextArea,a as WithoutLabel,Qe as __namedExportsOrder,Pe as default};
