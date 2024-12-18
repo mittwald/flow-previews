3dc5215830f06487521e3af95125b148163bd4ad
mittwald <>
2024-12-18 09:24:12 +0000

    Update from https://github.com/mittwald/flow/commit/67b4f67a29eba4372c5f8e374a5d573bf4a5fe57

---
+++ storybook/support-switch-in-menu-item/assets/EdgeCases.stories-Ao0Y1vkh.js
@@ -0,0 +1,13 @@
+import{I as o}from"./InlineCode-tVMCqBe_.js";import{R as t}from"./index-B-o1Wr-g.js";import{T as d}from"./Text-DPEzzey4.js";import a from"./Default.stories-BdSZx0l1.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import{S as l}from"./Section-DN-JIAqL.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./browser-B3hGj0u-.js";import"./flowComponent-SXQnFwcy.js";import"./index-DolzVqEf.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-CByKVUtS.js";import"./utils-CCebJm36.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ContextMenuSection-DgABpKHZ.js";import"./Action-nwH1LYxm.js";import"./context-HWij4CCA.js";import"./useSelector-iQqgW7gO.js";import"./getActionGroupSlot-Ctir2FTR.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-EQBJGkcZ.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./Label-BFvRcNt5.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CjESOWAx.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-BMY-498z.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-D4kl22nw.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";const im={...a,title:"Content/InlineCode/Edge Cases"},e={render:r=>t.createElement(l,null,t.createElement(d,null,m.medium," ",t.createElement(o,{...r},m.medium.replaceAll(" ",""))," ",m.medium,t.createElement(o,{...r},m.medium)," ",m.medium))};var i,p,n;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        {dummyText.medium}{" "}
+        <InlineCode {...props}>
+          {dummyText.medium.replaceAll(" ", "")}
+        </InlineCode>{" "}
+        {dummyText.medium}
+        <InlineCode {...props}>{dummyText.medium}</InlineCode>{" "}
+        {dummyText.medium}
+      </Text>
+    </Section>
+}`,...(n=(p=e.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const pm=["LongText"];export{e as LongText,pm as __namedExportsOrder,im as default};
