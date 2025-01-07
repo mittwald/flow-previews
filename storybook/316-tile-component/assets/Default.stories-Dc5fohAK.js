42100371e3d0bcfc3c27a566dd21f0c8686398cc
mittwald <>
2025-01-07 12:20:16 +0000

    Update from https://github.com/mittwald/flow/commit/e66a3de08c30a37eb6045d413eb07f54c1e743e7

---
+++ storybook/316-tile-component/assets/Default.stories-Dc5fohAK.js
@@ -0,0 +1,21 @@
+import{L as i}from"./Label-BFoBZBqy.js";import{R as e}from"./index-B-o1Wr-g.js";import{B as f}from"./Button-4Wboahs7.js";import{a as L,C as E}from"./ContextualHelpTrigger-Ch_43yJW.js";import{H}from"./Heading-DQ0-u3p0.js";import{T as n}from"./Text-DPEzzey4.js";import{L as T}from"./Link-DADEdaoW.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-SXQnFwcy.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Label-BFvRcNt5.js";import"./utils-CCebJm36.js";import"./Hidden-CRdWNeH8.js";import"./IconApp--rto3B0B.js";import"./IconWarning-BYoQf-GA.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-Dg0Lfff-.js";import"./Button-DpaYcQnV.js";import"./ProgressBar-C7LzY9et.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CjESOWAx.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./Popover-DAQfSrhS.js";import"./OverlayTrigger-DBMCsseW.js";import"./context-HWij4CCA.js";import"./useSelector-iQqgW7gO.js";import"./Dialog-EQBJGkcZ.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-BMY-498z.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-D4kl22nw.js";import"./Text-CByKVUtS.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./Heading--STTcIhS.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";const Le={title:"Content/Label",component:i,render:r=>e.createElement(i,{...r},"Label")},t={},o={args:{optional:!0}},a={render:r=>e.createElement(i,{...r},e.createElement(n,null,"Label"),e.createElement(L,null,e.createElement(f,null),e.createElement(E,{...r},e.createElement(H,null,"Rights & roles"),e.createElement(n,null,"Each user profile is assigned a role in mStudio for each project and/or organization. This allows you to work in a completely new and modern way."),e.createElement(T,null,"Learn more"))))};var m,p,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var s,c,u;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    optional: true
+  }
+}`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var d,g,x;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <Label {...props}>
+      <Text>Label</Text>
+      <ContextualHelpTrigger>
+        <Button />
+        <ContextualHelp {...props}>
+          <Heading>Rights & roles</Heading>
+          <Text>
+            Each user profile is assigned a role in mStudio for each project
+            and/or organization. This allows you to work in a completely new and
+            modern way.
+          </Text>
+          <Link>Learn more</Link>
+        </ContextualHelp>
+      </ContextualHelpTrigger>
+    </Label>
+}`,...(x=(g=a.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const Ee=["Default","Optional","WithContextualHelp"];export{t as Default,o as Optional,a as WithContextualHelp,Ee as __namedExportsOrder,Le as default};
