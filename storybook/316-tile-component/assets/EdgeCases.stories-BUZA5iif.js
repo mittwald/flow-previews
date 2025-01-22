4695071a8b5307f17971346c23785d6505dacf88
mittwald <>
2025-01-22 09:19:30 +0000

    Update from https://github.com/mittwald/flow/commit/d64962a1c8276c0a95e7f23832225a28c8f7aa23

--- storybook/316-tile-component/assets/EdgeCases.stories-BUZA5iif.js
+++
@@ -1,8 +0,0 @@
-import{P as s}from"./ProgressBar-Bx09YZQc.js";import i from"./Default.stories-CS3jTiYY.js";import{L as p}from"./Label-BFoBZBqy.js";import{R as e}from"./index-B-o1Wr-g.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./ProgressBar-C7LzY9et.js";import"./utils-CCebJm36.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Label-BFvRcNt5.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./flowComponent-SXQnFwcy.js";const M={...i,title:"Status/ProgressBar/Edge Cases"},r={render:a=>e.createElement(s,{value:500,maxValue:1e3,minValue:0,showMaxValue:!0,formatOptions:{style:"unit",unit:"gigabyte"},...a},e.createElement(p,null,n.medium))};var t,o,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: props => <ProgressBar value={500} maxValue={1000} minValue={0} showMaxValue formatOptions={{
-    style: "unit",
-    unit: "gigabyte"
-  }} {...props}>
-      <Label>{dummyText.medium}</Label>
-    </ProgressBar>
-}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const R=["LongText"];export{r as LongText,R as __namedExportsOrder,M as default};
