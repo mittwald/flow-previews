556dca98133b979c5cf3637324eab517ea3f406b
mittwald <>
2025-01-24 07:24:01 +0000

    Update from https://github.com/mittwald/flow/commit/99d26d1bb72cb5b83615715d8eed30ed7ab63d8c

---
+++ storybook/315-combobox-component/assets/Default.stories-DAqbPyrW.js
@@ -0,0 +1,18 @@
+import{P as m,a as R}from"./Popover-COWctmtn.js";import{R as r}from"./index-B-o1Wr-g.js";import{B as P}from"./Button-DfqfA7xf.js";import{u as h}from"./OverlayTrigger-K_SGs4A-.js";import"./flowComponent-Bz6Io0hO.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./clsx-B-dksMZM.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Dialog-4DVrVcAA.js";import"./Button-CFfO75lD.js";import"./utils-7gTBvjqH.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./Text-R-rrVGxo.js";import"./SelectionManager-D5qS5Dhh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./IconApp-3W3XzCjg.js";import"./IconWarning-CxL-YAPF.js";import"./Wrap-DQq6jo70.js";import"./Text-9mML8Z29.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./LoadingSpinner-BQNky5o5.js";import"./context-CtwQP4Yz.js";import"./useSelector-Cb4sc2b-.js";const cr={title:"Overlays/Popover",component:m,render:p=>r.createElement(R,null,r.createElement(P,null,"Trigger popover"),r.createElement(m,{...p,placement:"bottom right"},"I am a popover."))},o={},e={args:{width:800}},t={render:p=>{const i=h("Popover"),n=r.useRef(null);return r.createElement(r.Fragment,null,r.createElement(P,{ref:n,onPress:i.open},"Trigger popover"),r.createElement(m,{triggerRef:n,...p,controller:i},"I am a popover."))}};var a,s,l;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(l=(s=o.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var c,u,g;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    width: 800
+  }
+}`,...(g=(u=e.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,f,v;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController("Popover");
+    const triggerRef = React.useRef(null);
+    return <>
+        <Button ref={triggerRef} onPress={controller.open}>
+          Trigger popover
+        </Button>
+        <Popover triggerRef={triggerRef} {...props} controller={controller}>
+          I am a popover.
+        </Popover>
+      </>;
+  }
+}`,...(v=(f=t.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};const ur=["Default","CustomWidth","WithController"];export{e as CustomWidth,o as Default,t as WithController,ur as __namedExportsOrder,cr as default};
---
+++ storybook/315-combobox-component/assets/EdgeCases.stories-eKMnk55x.js
@@ -0,0 +1,59 @@
+import{R as e}from"./index-B-o1Wr-g.js";import{H as T}from"./Heading-BDRONVjZ.js";import{T as E}from"./Text-9mML8Z29.js";import{m as g,R as c,u as L,V as C,t as u}from"./Default.stories-BuU92l-c.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-Bz6Io0hO.js";import"./index-DolzVqEf.js";import"./Heading-CrY40Q5a.js";import"./RSPContexts-BT50PPhj.js";import"./utils-7gTBvjqH.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-R-rrVGxo.js";import"./ContextMenuTrigger-D0jUjj8S.js";import"./ContextMenuSection-BlI2bv0B.js";import"./Action-v-b0G_7k.js";import"./context-CtwQP4Yz.js";import"./useSelector-Cb4sc2b-.js";import"./getActionGroupSlot-DVGp_tC1.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-4DVrVcAA.js";import"./Button-CFfO75lD.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./SelectionManager-D5qS5Dhh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./Popover-COWctmtn.js";import"./OverlayTrigger-K_SGs4A-.js";import"./IconApp-3W3XzCjg.js";import"./IconWarning-CxL-YAPF.js";import"./Switch-N5TfLgZz.js";import"./Label-D1gQZv67.js";import"./useToggleState-C-A--gcY.js";import"./useFormReset-DMREOE0P.js";import"./Button-DfqfA7xf.js";import"./LoadingSpinner-BQNky5o5.js";import"./Tooltip-BTyBnpIw.js";import"./Badge-9kXVzeKz.js";import"./SearchField-se3no5CV.js";import"./FieldError-Chn1-6sB.js";import"./FieldError-Dc7wTCBj.js";import"./Form-CS2x0aS5.js";import"./Group-BaWKqaXG.js";import"./Input-In3k7nSs.js";import"./useTextField-BGTL6xBV.js";import"./Section-CrvhOWtx.js";import"./Avatar-Bl9Q2YEB.js";import"./react-children-utilities-xgYgah_I.js";import"./datetime-nQE3q1jc.js";import"./SkeletonText-Cbdt4GA-.js";import"./Skeleton-DK4V2SVs.js";import"./DragAndDrop-1_tMLeAb.js";import"./ListBox-Cf_-0Lu9.js";import"./useListState-DxMClvEm.js";import"./Table-CYXAU7Dp.js";import"./useUpdateEffect-D5ZsYDSi.js";import"./useHasTabbableChild-Cm5gK5Hj.js";import"./IllustratedMessage-BBTX4c47.js";import"./ActionGroup-CriEXt9Q.js";import"./AlertBadge-BFOIRhZC.js";import"./AlertIcon-R_HGj2Pw.js";import"./Content-Cg8Hfq9T.js";const Wt={...g,title:"Structure/List/Edge Cases"},w=()=>new Promise(t=>window.setTimeout(t,2e3)),d=async t=>(await w(),`${t}@info.de`),r={render:()=>{const t=u();return e.createElement(t.List,{batchSize:5,"aria-label":"List"},e.createElement(t.StaticData,{data:[{name:"John"},{name:"Max"}]}),e.createElement(t.Item,{textValue:i=>i.name},i=>e.createElement(c,null,()=>{const b=L(d,[i.name]);return e.createElement(C,null,e.createElement(T,null,i.name),e.createElement(E,null,b))})))}},m={render:()=>{const t=u();return e.createElement(t.List,{batchSize:5,"aria-label":"List",defaultViewMode:"table"},e.createElement(t.StaticData,{data:[{name:"John"},{name:"Max"}]}),e.createElement(t.Table,null,e.createElement(t.TableHeader,null,e.createElement(t.TableColumn,null,"Name"),e.createElement(t.TableColumn,null,"Email")),e.createElement(t.TableBody,null,e.createElement(t.TableRow,null,e.createElement(t.TableCell,null,i=>i.name),e.createElement(t.TableCell,null,i=>e.createElement(c,null,()=>L(d,[i.name])))))))}};var a,n,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List batchSize={5} aria-label="List">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Item textValue={item => item.name}>
+          {item => <Render>
+              {() => {
+            const email = usePromise(getEmail, [item.name]);
+            return <ListItemView>
+                    <Heading>{item.name}</Heading>
+                    <Text>{email}</Text>
+                  </ListItemView>;
+          }}
+            </Render>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(o=(n=r.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var l,s,p;m.parameters={...m.parameters,docs:{...(l=m.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List batchSize={5} aria-label="List" defaultViewMode="table">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Table>
+          <List.TableHeader>
+            <List.TableColumn>Name</List.TableColumn>
+            <List.TableColumn>Email</List.TableColumn>
+          </List.TableHeader>
+
+          <List.TableBody>
+            <List.TableRow>
+              <List.TableCell>{item => item.name}</List.TableCell>
+              <List.TableCell>
+                {item => <Render>
+                    {() => {
+                  return usePromise(getEmail, [item.name]);
+                }}
+                  </Render>}
+              </List.TableCell>
+            </List.TableRow>
+          </List.TableBody>
+        </List.Table>
+      </List.List>;
+  }
+}`,...(p=(s=m.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const Xt=["LoadingListItem","LoadingTableCell"];export{r as LoadingListItem,m as LoadingTableCell,Xt as __namedExportsOrder,Wt as default};
