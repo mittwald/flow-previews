8a2a86f2f6da404d81d3c3f974437693b852292a
mittwald <>
2025-01-24 06:38:37 +0000

    Update from https://github.com/mittwald/flow/commit/3da4f7889f50b49607b888932c16675a1719145e

---
+++ storybook/export-useFormContext/assets/Default.stories-OoLjdaGL.js
@@ -0,0 +1,135 @@
+import{R as e}from"./index-B-o1Wr-g.js";import{C as r,a,M as t}from"./ContextMenuTrigger-BlwjqOjh.js";import{C as m}from"./ContextMenuSection-fuBh6GFp.js";import{B as l}from"./Button-DdH-eFOg.js";import{S as x}from"./Separator-Dyd9PV28.js";import{H as f}from"./Heading-BCzqphXr.js";import"./IconApp--rto3B0B.js";import{c as E,b as Y,d as Z}from"./IconWarning-s47gvt4o.js";import{T as o}from"./Text-BziEbUGv.js";import{A as $}from"./Avatar-DvJSpMY9.js";import{I as ee}from"./Initials-BjhGT3zq.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./flowComponent-SXQnFwcy.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./clsx-B-dksMZM.js";import"./index-DolzVqEf.js";import"./Popover-DJI0CRYC.js";import"./OverlayTrigger-DBMCsseW.js";import"./context-HWij4CCA.js";import"./useSelector-iQqgW7gO.js";import"./Dialog-ugMEkBXT.js";import"./Button-CFfO75lD.js";import"./utils-7gTBvjqH.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./Text-R-rrVGxo.js";import"./SelectionManager-Dnw_ATjh.js";import"./FocusScope-Deam_7e8.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./Action-nwH1LYxm.js";import"./browser-B3hGj0u-.js";import"./getActionGroupSlot-Ctir2FTR.js";import"./dynamic-DKDa4OpU.js";import"./Switch-BrQWV4ot.js";import"./Label-_TQx26eL.js";import"./useToggleState-C-A--gcY.js";import"./useFormReset-DMREOE0P.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-DV7QM5bq.js";import"./Heading-CrY40Q5a.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./react-children-utilities-xgYgah_I.js";const mt={title:"Actions/ContextMenu",component:r,render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{...n},e.createElement(t,{id:"item1"},"Item 1"),e.createElement(t,{id:"item2"},"Item 2"),e.createElement(t,{id:"item3"},"Item 3"))),parameters:{controls:{exclude:["defaultOpen","selectionMode","defaultSelectedKeys"]}}},i={},s={args:{defaultOpen:!0,selectionMode:"single",defaultSelectedKeys:["item2"]}},c={args:{defaultOpen:!0,selectionMode:"multiple",defaultSelectedKeys:["item2","item3"]}},u={render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{defaultSelectedKeys:["https://www.mittwald.de"],selectionMode:"navigation",...n},e.createElement(t,{href:"https://www.mittwald.de",id:"https://www.mittwald.de"},"www.mittwald.de"),e.createElement(t,{href:"https://www.google.de"},"www.google.de"),e.createElement(t,{href:"https://www.adobe.com"},"www.adobe.com"))),args:{defaultOpen:!0}},d={render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{defaultSelectedKeys:["https://www.mittwald.de"],selectionMode:"navigation",...n},e.createElement(t,{href:"https://www.mittwald.de",id:"https://www.mittwald.de"},"www.mittwald.de"),e.createElement(x,null),e.createElement(t,{href:"https://www.google.de"},"www.google.de"),e.createElement(t,{href:"https://www.adobe.com"},"www.adobe.com"))),args:{defaultOpen:!0}},w={render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{selectionMode:"navigation",...n},e.createElement(m,null,e.createElement(f,null,"Websites"),e.createElement(t,{href:"https://www.mittwald.de",id:"https://www.mittwald.de"},"www.mittwald.de"),e.createElement(t,{href:"https://www.google.de"},"www.google.de"),e.createElement(t,{href:"https://www.adobe.com"},"www.adobe.com")))),args:{defaultOpen:!0}},p={render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{selectionMode:"navigation",...n},e.createElement(m,null,e.createElement(f,null,"Websites"),e.createElement(t,{href:"https://www.mittwald.de",id:"https://www.mittwald.de"},e.createElement(E,null),e.createElement(o,null,"www.mittwald.de")),e.createElement(t,{href:"https://www.google.de"},e.createElement(E,null),e.createElement(o,null,"www.google.de")),e.createElement(t,{isDisabled:!0,href:"https://www.adobe.com"},e.createElement(E,null),e.createElement(o,null,"www.adobe.com"))))),args:{defaultOpen:!0}},g={args:{width:400}},M={render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{selectionMode:"navigation",...n},e.createElement(m,null,e.createElement(t,null,e.createElement($,null,e.createElement(ee,null,"Max Mustermann")),e.createElement(Y,null)),e.createElement(f,null,"Max Mustermann")),e.createElement(x,null),e.createElement(m,null,e.createElement(t,null,e.createElement(o,null,"Settings")),e.createElement(t,null,e.createElement(o,null,"Logout"))))),args:{defaultOpen:!0}},h={render:n=>e.createElement(a,null,e.createElement(l,null,"Trigger"),e.createElement(r,{...n},e.createElement(m,{selectionMode:"switch"},e.createElement(t,null,e.createElement(o,null,"Spam protection"))),e.createElement(x,null),e.createElement(m,{selectionMode:"navigation"},e.createElement(t,null,e.createElement(Z,null),e.createElement(o,null,"Update email address"))))),args:{defaultOpen:!0}};var S,I,C;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:"{}",...(C=(I=i.parameters)==null?void 0:I.docs)==null?void 0:C.source}}};var T,b,v;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    defaultOpen: true,
+    selectionMode: "single",
+    defaultSelectedKeys: ["item2"]
+  }
+}`,...(v=(b=s.parameters)==null?void 0:b.docs)==null?void 0:v.source}}};var O,W,B;c.parameters={...c.parameters,docs:{...(O=c.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    defaultOpen: true,
+    selectionMode: "multiple",
+    defaultSelectedKeys: ["item2", "item3"]
+  }
+}`,...(B=(W=c.parameters)==null?void 0:W.docs)==null?void 0:B.source}}};var y,K,H;u.parameters={...u.parameters,docs:{...(y=u.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu defaultSelectedKeys={["https://www.mittwald.de"]} selectionMode="navigation" {...props}>
+        <MenuItem href="https://www.mittwald.de" id="https://www.mittwald.de">
+          www.mittwald.de
+        </MenuItem>
+        <MenuItem href="https://www.google.de">www.google.de</MenuItem>
+        <MenuItem href="https://www.adobe.com">www.adobe.com</MenuItem>
+      </ContextMenu>
+    </ContextMenuTrigger>,
+  args: {
+    defaultOpen: true
+  }
+}`,...(H=(K=u.parameters)==null?void 0:K.docs)==null?void 0:H.source}}};var A,D,L;d.parameters={...d.parameters,docs:{...(A=d.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu defaultSelectedKeys={["https://www.mittwald.de"]} selectionMode="navigation" {...props}>
+        <MenuItem href="https://www.mittwald.de" id="https://www.mittwald.de">
+          www.mittwald.de
+        </MenuItem>
+        <Separator />
+        <MenuItem href="https://www.google.de">www.google.de</MenuItem>
+        <MenuItem href="https://www.adobe.com">www.adobe.com</MenuItem>
+      </ContextMenu>
+    </ContextMenuTrigger>,
+  args: {
+    defaultOpen: true
+  }
+}`,...(L=(D=d.parameters)==null?void 0:D.docs)==null?void 0:L.source}}};var k,R,U;w.parameters={...w.parameters,docs:{...(k=w.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu selectionMode="navigation" {...props}>
+        <ContextMenuSection>
+          <Heading>Websites</Heading>
+          <MenuItem href="https://www.mittwald.de" id="https://www.mittwald.de">
+            www.mittwald.de
+          </MenuItem>
+          <MenuItem href="https://www.google.de">www.google.de</MenuItem>
+          <MenuItem href="https://www.adobe.com">www.adobe.com</MenuItem>
+        </ContextMenuSection>
+      </ContextMenu>
+    </ContextMenuTrigger>,
+  args: {
+    defaultOpen: true
+  }
+}`,...(U=(R=w.parameters)==null?void 0:R.docs)==null?void 0:U.source}}};var _,j,q;p.parameters={...p.parameters,docs:{...(_=p.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu selectionMode="navigation" {...props}>
+        <ContextMenuSection>
+          <Heading>Websites</Heading>
+          <MenuItem href="https://www.mittwald.de" id="https://www.mittwald.de">
+            <IconServer />
+            <Text>www.mittwald.de</Text>
+          </MenuItem>
+          <MenuItem href="https://www.google.de">
+            <IconServer />
+            <Text>www.google.de</Text>
+          </MenuItem>
+          <MenuItem isDisabled={true} href="https://www.adobe.com">
+            <IconServer />
+            <Text>www.adobe.com</Text>
+          </MenuItem>
+        </ContextMenuSection>
+      </ContextMenu>
+    </ContextMenuTrigger>,
+  args: {
+    defaultOpen: true
+  }
+}`,...(q=(j=p.parameters)==null?void 0:j.docs)==null?void 0:q.source}}};var z,F,G;g.parameters={...g.parameters,docs:{...(z=g.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    width: 400
+  }
+}`,...(G=(F=g.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var J,N,P;M.parameters={...M.parameters,docs:{...(J=M.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu selectionMode="navigation" {...props}>
+        <ContextMenuSection>
+          <MenuItem>
+            <Avatar>
+              <Initials>Max Mustermann</Initials>
+            </Avatar>
+            <IconCamera />
+          </MenuItem>
+          <Heading>Max Mustermann</Heading>
+        </ContextMenuSection>
+        <Separator />
+        <ContextMenuSection>
+          <MenuItem>
+            <Text>Settings</Text>
+          </MenuItem>
+          <MenuItem>
+            <Text>Logout</Text>
+          </MenuItem>
+        </ContextMenuSection>
+      </ContextMenu>
+    </ContextMenuTrigger>,
+  args: {
+    defaultOpen: true
+  }
+}`,...(P=(N=M.parameters)==null?void 0:N.docs)==null?void 0:P.source}}};var Q,V,X;h.parameters={...h.parameters,docs:{...(Q=h.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu {...props}>
+        <ContextMenuSection selectionMode="switch">
+          <MenuItem>
+            <Text>Spam protection</Text>
+          </MenuItem>
+        </ContextMenuSection>
+        <Separator />
+        <ContextMenuSection selectionMode="navigation">
+          <MenuItem>
+            <IconEmail />
+            <Text>Update email address</Text>
+          </MenuItem>
+        </ContextMenuSection>
+      </ContextMenu>
+    </ContextMenuTrigger>,
+  args: {
+    defaultOpen: true
+  }
+}`,...(X=(V=h.parameters)==null?void 0:V.docs)==null?void 0:X.source}}};const it=["Default","SingleSelection","MultipleSelection","WithLinks","WithSeparator","WithContextMenuSection","WithIcon","CustomWidth","WithAvatar","WithSectionSelectionMode"];export{g as CustomWidth,i as Default,c as MultipleSelection,s as SingleSelection,M as WithAvatar,w as WithContextMenuSection,p as WithIcon,u as WithLinks,h as WithSectionSelectionMode,d as WithSeparator,it as __namedExportsOrder,mt as default};
