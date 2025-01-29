b3460ceadb3129ffdd0bae563146895543e3f86c
mittwald <>
2025-01-29 08:22:36 +0000

    Update from https://github.com/mittwald/flow/commit/2cacf7c821382d92ff3043230faed06b3d338712

---
+++ storybook/support-link-in-action-group/assets/Default.stories-BVPnBpiy.js
@@ -0,0 +1,148 @@
+import{S as r}from"./Section-CT9dD0YZ.js";import{R as e}from"./index-B-o1Wr-g.js";import{H as n}from"./Heading-BDRONVjZ.js";import{T as o}from"./Text-9mML8Z29.js";import{T as g}from"./TextField-dUEbLjWr.js";import{L as E}from"./Label-D1gQZv67.js";import{L as b}from"./Link-CQOfPOLX.js";import{S as W}from"./Switch-CdI-Z2Vd.js";import{A as S}from"./AlertBadge-DnqGei0J.js";import{H as l}from"./Header-C7AUAiBL.js";import"./IconApp-3W3XzCjg.js";import{e as U,j}from"./IconWarning-BoAQPcSM.js";import{B as a}from"./Button-CAPBBAH6.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import{A as f}from"./Action-v-b0G_7k.js";import{M as H}from"./ModalTrigger-BuU-R2bA.js";import{a as k,C as J}from"./ContextualHelpTrigger-rrsjDyn7.js";import{a as _,C as O,M as x}from"./ContextMenuTrigger-wosNPsm8.js";import"./ContextMenuSection-B0aP5-y_.js";import{F as z}from"./FileField-DyQkILmo.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-Bz6Io0hO.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Heading-CrY40Q5a.js";import"./RSPContexts-BT50PPhj.js";import"./utils-7gTBvjqH.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-R-rrVGxo.js";import"./TextFieldBase-D2gguQdb.js";import"./FieldError-zIiVOJJb.js";import"./FieldError-Dc7wTCBj.js";import"./useFocusRing-CTDVvBmm.js";import"./filterDOMProps-CeZl_uWj.js";import"./FieldDescription-DL_UboA3.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Form-CS2x0aS5.js";import"./useLabel-DrlXsFf-.js";import"./Input-In3k7nSs.js";import"./Hidden-CRdWNeH8.js";import"./Label-CqgmsInd.js";import"./useTextField-BGTL6xBV.js";import"./useFormReset-DMREOE0P.js";import"./useControlledState-BgxdDqm0.js";import"./useFocusable-BXVt7Q_3.js";import"./usePress-C3EQNDer.js";import"./useToggleState-C-A--gcY.js";import"./VisuallyHidden-lWpahT0S.js";import"./AlertIcon-nPq7a9IA.js";import"./LoadingSpinner-DNhPsDbg.js";import"./Button-CPCoFgob.js";import"./ProgressBar-BQAJCbMD.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./context-CtwQP4Yz.js";import"./useSelector-Cb4sc2b-.js";import"./getActionGroupSlot-DVGp_tC1.js";import"./dynamic-DKDa4OpU.js";import"./Overlay-vWsNacJy.js";import"./OverlayTrigger-K_SGs4A-.js";import"./Dialog-CbvlC6bd.js";import"./OverlayArrow-CuXMKzfT.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./SelectionManager-ubMUrr_P.js";import"./FocusScope-DvafcTTH.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./Popover-sRC00zfV.js";const pt={title:"Structure/Section",component:r,render:t=>e.createElement(r,{...t},e.createElement(n,null,"Newsletter"),e.createElement(o,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."),e.createElement(b,{href:"#"},"Subscribe"))},m={},u={render:t=>e.createElement(e.Fragment,null,e.createElement(r,{...t},e.createElement(n,null,e.createElement(U,null),"Personal Information"),e.createElement(g,{isRequired:!0,defaultValue:"John"},e.createElement(E,null,"First name")),e.createElement(g,{isRequired:!0,defaultValue:"Doe"},e.createElement(E,null,"Last name"))),e.createElement(r,{...t},e.createElement(n,null,"Newsletter"),e.createElement(o,null,"Upcoming releases, new features and tips about your hosting - we bring you the most important information in your inbox. Subscribe to our newsletter and stay up to date."),e.createElement(b,{href:"#"},"Subscribe")))},c={render:t=>e.createElement(e.Fragment,null,e.createElement(r,{...t},e.createElement(l,null,e.createElement(n,null,"Newsletter ",e.createElement(S,null,"Subscribed")),e.createElement(W,{defaultSelected:!0},"Subscription"),e.createElement(a,{variant:"soft",color:"secondary"},"Start database migration"),e.createElement(k,null,e.createElement(a,null),e.createElement(J,null,"Additional information about database migration"))),e.createElement(o,null,"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date.")),e.createElement(r,{...t},e.createElement(l,null,e.createElement(n,null,"My Project with a looooooooooong name",e.createElement(S,{status:"danger"},"Deactivated")),e.createElement(f,null,e.createElement(H,{slot:"actionConfirm"},e.createElement(n,null,"Confirmation Modal")),e.createElement(a,{variant:"soft",color:"secondary"},"Start database migration")),e.createElement(f,null,e.createElement(H,{slot:"actionConfirm"},e.createElement(n,null,"Confirmation Modal")),e.createElement(a,{color:"accent"},"Activate"))),e.createElement(o,null,i.medium)),e.createElement(r,{...t},e.createElement(l,null,e.createElement(n,null,"My Project"),e.createElement(_,null,e.createElement(a,{variant:"soft",color:"secondary"},e.createElement(j,null)),e.createElement(O,null,e.createElement(x,null,"Item 1"),e.createElement(x,null,"Item 2"))),e.createElement(a,{color:"danger"},"Deactivate")),e.createElement(o,null,i.medium)))},s={render:t=>e.createElement(e.Fragment,null,e.createElement(r,{...t},e.createElement(n,null,"Heading"),e.createElement(o,null,i.long),e.createElement(n,{level:3},"Sub-Heading"),e.createElement(o,null,i.long)),e.createElement(r,{...t},e.createElement(l,null,e.createElement(n,null,"Heading"),e.createElement(a,null,"Button")),e.createElement(o,null,i.long),e.createElement(l,null,e.createElement(n,{level:3},"Sub-Heading"),e.createElement(a,null,"Button")),e.createElement(o,null,i.long)))},d={render:t=>e.createElement(e.Fragment,null,e.createElement(r,{...t,hideSeparator:!0},e.createElement(n,null,e.createElement(U,null),"Personal Information"),e.createElement(g,{isRequired:!0,defaultValue:"John"},e.createElement(E,null,"First name")),e.createElement(g,{isRequired:!0,defaultValue:"Doe"},e.createElement(E,null,"Last name"))),e.createElement(r,{...t},e.createElement(n,null,"Newsletter"),e.createElement(o,null,"Upcoming releases, new features and tips about your hosting - we bring you the most important information in your inbox. Subscribe to our newsletter and stay up to date."),e.createElement(b,{href:"#"},"Subscribe")))},p={render:t=>e.createElement(e.Fragment,null,e.createElement(r,{...t,hideSeparator:!0},e.createElement(l,null,e.createElement(n,null,"Domains"),e.createElement(z,null,e.createElement(a,null,"Import CSV")),e.createElement(a,null,"Add")),e.createElement(o,null,"Add at least one domain.")))};var T,y,h;m.parameters={...m.parameters,docs:{...(T=m.parameters)==null?void 0:T.docs,source:{originalSource:"{}",...(h=(y=m.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var w,M,B;u.parameters={...u.parameters,docs:{...(w=u.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props}>
+        <Heading>
+          <IconMember />
+          Personal Information
+        </Heading>
+        <TextField isRequired defaultValue="John">
+          <Label>First name</Label>
+        </TextField>
+        <TextField isRequired defaultValue="Doe">
+          <Label>Last name</Label>
+        </TextField>
+      </Section>
+      <Section {...props}>
+        <Heading>Newsletter</Heading>
+        <Text>
+          Upcoming releases, new features and tips about your hosting - we bring
+          you the most important information in your inbox. Subscribe to our
+          newsletter and stay up to date.
+        </Text>
+        <Link href="#">Subscribe</Link>
+      </Section>
+    </>
+}`,...(B=(M=u.parameters)==null?void 0:M.docs)==null?void 0:B.source}}};var F,C,A;c.parameters={...c.parameters,docs:{...(F=c.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props}>
+        <Header>
+          <Heading>
+            Newsletter <AlertBadge>Subscribed</AlertBadge>
+          </Heading>
+
+          <Switch defaultSelected>Subscription</Switch>
+          <Button variant="soft" color="secondary">
+            Start database migration
+          </Button>
+          <ContextualHelpTrigger>
+            <Button />
+            <ContextualHelp>
+              Additional information about database migration
+            </ContextualHelp>
+          </ContextualHelpTrigger>
+        </Header>
+        <Text>
+          Upcoming releases, new features and tips about your hosting - we bring
+          the most important information to inbox. Subscribe to our newsletter
+          and stay up to date.
+        </Text>
+      </Section>
+      <Section {...props}>
+        <Header>
+          <Heading>
+            My Project with a looooooooooong name
+            <AlertBadge status="danger">Deactivated</AlertBadge>
+          </Heading>
+          <Action>
+            <Modal slot="actionConfirm">
+              <Heading>Confirmation Modal</Heading>
+            </Modal>
+            <Button variant="soft" color="secondary">
+              Start database migration
+            </Button>
+          </Action>
+          <Action>
+            <Modal slot="actionConfirm">
+              <Heading>Confirmation Modal</Heading>
+            </Modal>
+            <Button color="accent">Activate</Button>
+          </Action>
+        </Header>
+        <Text>{dummyText.medium}</Text>
+      </Section>
+      <Section {...props}>
+        <Header>
+          <Heading>My Project</Heading>
+          <ContextMenuTrigger>
+            <Button variant="soft" color="secondary">
+              <IconContextMenu />
+            </Button>
+            <ContextMenu>
+              <MenuItem>Item 1</MenuItem>
+              <MenuItem>Item 2</MenuItem>
+            </ContextMenu>
+          </ContextMenuTrigger>
+          <Button color="danger">Deactivate</Button>
+        </Header>
+        <Text>{dummyText.medium}</Text>
+      </Section>
+    </>
+}`,...(A=(C=c.parameters)==null?void 0:C.docs)==null?void 0:A.source}}};var I,L,v;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props}>
+        <Heading>Heading</Heading>
+        <Text>{dummyText.long}</Text>
+        <Heading level={3}>Sub-Heading</Heading>
+        <Text>{dummyText.long}</Text>
+      </Section>
+      <Section {...props}>
+        <Header>
+          <Heading>Heading</Heading>
+          <Button>Button</Button>
+        </Header>
+        <Text>{dummyText.long}</Text>
+        <Header>
+          <Heading level={3}>Sub-Heading</Heading>
+          <Button>Button</Button>
+        </Header>
+        <Text>{dummyText.long}</Text>
+      </Section>
+    </>
+}`,...(v=(L=s.parameters)==null?void 0:L.docs)==null?void 0:v.source}}};var D,R,V;d.parameters={...d.parameters,docs:{...(D=d.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props} hideSeparator>
+        <Heading>
+          <IconMember />
+          Personal Information
+        </Heading>
+        <TextField isRequired defaultValue="John">
+          <Label>First name</Label>
+        </TextField>
+        <TextField isRequired defaultValue="Doe">
+          <Label>Last name</Label>
+        </TextField>
+      </Section>
+      <Section {...props}>
+        <Heading>Newsletter</Heading>
+        <Text>
+          Upcoming releases, new features and tips about your hosting - we bring
+          you the most important information in your inbox. Subscribe to our
+          newsletter and stay up to date.
+        </Text>
+        <Link href="#">Subscribe</Link>
+      </Section>
+    </>
+}`,...(V=(R=d.parameters)==null?void 0:R.docs)==null?void 0:V.source}}};var q,P,N;p.parameters={...p.parameters,docs:{...(q=p.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: props => <>
+      <Section {...props} hideSeparator>
+        <Header>
+          <Heading>Domains</Heading>
+          <FileField>
+            <Button>Import CSV</Button>
+          </FileField>
+          <Button>Add</Button>
+        </Header>
+        <Text>Add at least one domain.</Text>
+      </Section>
+    </>
+}`,...(N=(P=p.parameters)==null?void 0:P.docs)==null?void 0:N.source}}};const gt=["Default","MultipleSections","WithHeaderContent","WithSubHeadings","HideSeperator","WithFileField"];export{m as Default,d as HideSeperator,u as MultipleSections,p as WithFileField,c as WithHeaderContent,s as WithSubHeadings,gt as __namedExportsOrder,pt as default};
---
+++ storybook/support-link-in-action-group/assets/EdgeCases.stories-CcAGNQVQ.js
@@ -0,0 +1,20 @@
+import{R as e}from"./index-B-o1Wr-g.js";import{T as u}from"./Text-9mML8Z29.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import x from"./Default.stories-DK5bMyyd.js";import{I as d}from"./IllustratedMessage-BBTX4c47.js";import{H as c}from"./Heading-BDRONVjZ.js";import{B as g}from"./Button-CAPBBAH6.js";import{I as f}from"./IconApp-3W3XzCjg.js";import"./IconWarning-BoAQPcSM.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./clsx-B-dksMZM.js";import"./browser-B3hGj0u-.js";import"./flowComponent-Bz6Io0hO.js";import"./index-DolzVqEf.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-R-rrVGxo.js";import"./utils-7gTBvjqH.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ActionGroup-DJwZ8gTJ.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-DVGp_tC1.js";import"./useSelector-Cb4sc2b-.js";import"./Heading-CrY40Q5a.js";import"./RSPContexts-BT50PPhj.js";import"./LoadingSpinner-DNhPsDbg.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Button-CPCoFgob.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";const oe={...x,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,p.medium),e.createElement(u,null,p.long),e.createElement(g,null,"Create app"))},r={render:o=>e.createElement(d,{...o},e.createElement(f,null),e.createElement(c,null,"No apps installed"),e.createElement(u,null,"Create your first app to start working on your website."),e.createElement(g,null,"Create app")),parameters:{viewport:{defaultViewport:"mobile1"}}};var a,m,n;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(n=(m=t.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var s,i,l;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(l=(i=r.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const pe=["LongTexts","SmallSpace"];export{t as LongTexts,r as SmallSpace,pe as __namedExportsOrder,oe as default};
