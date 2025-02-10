85007f901d7e42797d24507e23cb26c3a42ce36d
mittwald <>
2025-02-10 11:06:54 +0000

    Update from https://github.com/mittwald/flow/commit/5c0353e391694c6465a53f8ff63acab2b22059a8

---
+++ storybook/fix-missing-entries/assets/Default.stories-y8khJZnI.js
@@ -0,0 +1,76 @@
+import{B as t,b as G}from"./Badge-St5QXtuk.js";import{R as e}from"./index-B-o1Wr-g.js";import{T as o}from"./Text-CZmkQV3T.js";import{L as n}from"./Label-C0pbfKh0.js";import{C as H,M as S}from"./ContextMenuTrigger-B6JLhCy5.js";import"./ContextMenuSection-CcEb0PeQ.js";import{u as J}from"./OverlayTrigger-ChQW1Rsg.js";import{s as K,a as Q}from"./storyBackgrounds-GFSAi-dI.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./flowComponent-D5geu_h3.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-DE5z48KK.js";import"./IconApp-CVCpMqGp.js";import"./IconWarning-BAQE0NeC.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-DZNyHsLk.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Button-CPCoFgob.js";import"./utils-7gTBvjqH.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Text-R-rrVGxo.js";import"./Popover-D-aqpmzx.js";import"./Dialog-CbvlC6bd.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./SelectionManager-ubMUrr_P.js";import"./FocusScope-DvafcTTH.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";import"./Action-Cq0UywbC.js";import"./context-Dp-H5xwt.js";import"./useStatic-Dv1ygyV5.js";import"./getActionGroupSlot-DREz4eBl.js";import"./dynamic-DKDa4OpU.js";import"./Switch-Dc14AonT.js";import"./useToggleState-C-A--gcY.js";import"./useFormReset-DMREOE0P.js";const Ye={title:"Status/Badge",component:t,parameters:{controls:{exclude:["className"]}},args:{color:"neutral",isDisabled:!1},argTypes:{color:{control:"inline-radio",options:G}},render:r=>e.createElement(t,{...r},"Value")},a={},s={render:r=>e.createElement(t,{...r},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value"))},l={render:r=>e.createElement(t,{...r,onPress:()=>{alert("pressed!")}},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value"))},p={render:r=>e.createElement(t,{...r,onClose:()=>{alert("closed!")}},"Value")},c={render:r=>e.createElement(t,{...r,onClose:()=>{alert("closed!")}},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value"))},m={render:r=>e.createElement(t,{...r,onClose:()=>{alert("closed!")},onPress:()=>{alert("pressed!")}},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value"))},i={render:r=>{const g=J("ContextMenu"),E=e.useRef(null);return e.createElement(e.Fragment,null,e.createElement(t,{ref:E,onPress:g.open,...r},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value")),e.createElement(H,{controller:g,triggerRef:E},e.createElement(S,null,"Item 1"),e.createElement(S,null,"Item 2")))}},d={args:{color:"light"},render:r=>e.createElement(t,{...r,onPress:()=>{alert("pressed!")}},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value")),parameters:{backgrounds:K}},u={args:{color:"dark"},render:r=>e.createElement(t,{...r,onPress:()=>{alert("pressed!")}},e.createElement(n,null,"Scope"),e.createElement(o,null,"Value")),parameters:{backgrounds:Q}};var x,B,C;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:"{}",...(C=(B=a.parameters)==null?void 0:B.docs)==null?void 0:C.source}}};var b,f,L;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Badge {...props}>
+      <Label>Scope</Label>
+      <Text>Value</Text>
+    </Badge>
+}`,...(L=(f=s.parameters)==null?void 0:f.docs)==null?void 0:L.source}}};var h,T,V;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <Badge {...props} onPress={() => {
+    alert("pressed!");
+  }}>
+      <Label>Scope</Label>
+      <Text>Value</Text>
+    </Badge>
+}`,...(V=(T=l.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};var k,P,M;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: props => <Badge {...props} onClose={() => {
+    alert("closed!");
+  }}>
+      Value
+    </Badge>
+}`,...(M=(P=p.parameters)==null?void 0:P.docs)==null?void 0:M.source}}};var O,W,R;c.parameters={...c.parameters,docs:{...(O=c.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: props => <Badge {...props} onClose={() => {
+    alert("closed!");
+  }}>
+      <Label>Scope</Label>
+      <Text>Value</Text>
+    </Badge>
+}`,...(R=(W=c.parameters)==null?void 0:W.docs)==null?void 0:R.source}}};var I,y,D;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <Badge {...props} onClose={() => {
+    alert("closed!");
+  }} onPress={() => {
+    alert("pressed!");
+  }}>
+      <Label>Scope</Label>
+      <Text>Value</Text>
+    </Badge>
+}`,...(D=(y=m.parameters)==null?void 0:y.docs)==null?void 0:D.source}}};var A,v,_;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController("ContextMenu");
+    const triggerRef = React.useRef(null);
+    return <>
+        <Badge ref={triggerRef} onPress={controller.open} {...props}>
+          <Label>Scope</Label>
+          <Text>Value</Text>
+        </Badge>
+        <ContextMenu controller={controller} triggerRef={triggerRef}>
+          <MenuItem>Item 1</MenuItem>
+          <MenuItem>Item 2</MenuItem>
+        </ContextMenu>
+      </>;
+  }
+}`,...(_=(v=i.parameters)==null?void 0:v.docs)==null?void 0:_.source}}};var F,N,j;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  render: props => <Badge {...props} onPress={() => {
+    alert("pressed!");
+  }}>
+      <Label>Scope</Label>
+      <Text>Value</Text>
+    </Badge>,
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(j=(N=d.parameters)==null?void 0:N.docs)==null?void 0:j.source}}};var q,w,z;u.parameters={...u.parameters,docs:{...(q=u.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  render: props => <Badge {...props} onPress={() => {
+    alert("pressed!");
+  }}>
+      <Label>Scope</Label>
+      <Text>Value</Text>
+    </Badge>,
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(z=(w=u.parameters)==null?void 0:w.docs)==null?void 0:z.source}}};const Ze=["Default","WithScope","WithOnPress","WithOnClose","WithScopeAndOnClose","WithOnPressAndOnClose","WithContextMenu","Light","Dark"];export{u as Dark,a as Default,d as Light,i as WithContextMenu,p as WithOnClose,l as WithOnPress,m as WithOnPressAndOnClose,s as WithScope,c as WithScopeAndOnClose,Ze as __namedExportsOrder,Ye as default};
---
+++ storybook/fix-missing-entries/assets/EdgeCases.stories-BGqDesB-.js
@@ -0,0 +1,13 @@
+import{I as o}from"./InlineCode-tVMCqBe_.js";import{R as t}from"./index-B-o1Wr-g.js";import{T as d}from"./Text-CZmkQV3T.js";import a from"./Default.stories-CAs5ymYa.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import{S as l}from"./Section-3HwNGTS6.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHq6Fnml.js";import"./index-ynQ6n1pc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./browser-B3hGj0u-.js";import"./flowComponent-D5geu_h3.js";import"./index-DolzVqEf.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-R-rrVGxo.js";import"./utils-7gTBvjqH.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ContextMenuSection-CcEb0PeQ.js";import"./Action-Cq0UywbC.js";import"./context-Dp-H5xwt.js";import"./useStatic-Dv1ygyV5.js";import"./getActionGroupSlot-DREz4eBl.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-CbvlC6bd.js";import"./Button-CPCoFgob.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./SelectionManager-ubMUrr_P.js";import"./FocusScope-DvafcTTH.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";const im={...a,title:"Content/InlineCode/Edge Cases"},e={render:r=>t.createElement(l,null,t.createElement(d,null,m.medium," ",t.createElement(o,{...r},m.medium.replaceAll(" ",""))," ",m.medium,t.createElement(o,{...r},m.medium)," ",m.medium))};var i,p,n;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        {dummyText.medium}{" "}
+        <InlineCode {...props}>
+          {dummyText.medium.replaceAll(" ", "")}
+        </InlineCode>{" "}
+        {dummyText.medium}
+        <InlineCode {...props}>{dummyText.medium}</InlineCode>{" "}
+        {dummyText.medium}
+      </Text>
+    </Section>
+}`,...(n=(p=e.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const pm=["LongText"];export{e as LongText,pm as __namedExportsOrder,im as default};
