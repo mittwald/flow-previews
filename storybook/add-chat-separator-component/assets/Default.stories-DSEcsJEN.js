a3d89a1abf71d3146ef81aba16f83068a1c20458
mittwald <>
2025-02-14 10:31:01 +0000

    Update from https://github.com/mittwald/flow/commit/ad147e801f02f27463f0d9f3f3eace87ad7bbc43

---
+++ storybook/add-chat-separator-component/assets/Default.stories-DSEcsJEN.js
@@ -0,0 +1,5 @@
+import{I as t}from"./Image-DabeQpm5.js";import{R as n}from"./index-B-o1Wr-g.js";import{d}from"./dummyText-CX_I_Wpl.js";import"./flowComponent-Bi8R7MU8.js";import"./index-ynQ6n1pc.js";import"./clsx-B-dksMZM.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";const S={title:"Content/Image",component:t,render:i=>n.createElement(t,{...i,alt:"Gopher",src:d.imageSrc})},r={},e={args:{withBorder:!0}};var o,a,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var m,c,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    withBorder: true
+  }
+}`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const w=["Default","WithBorder"];export{r as Default,e as WithBorder,w as __namedExportsOrder,S as default};
---
+++ storybook/add-chat-separator-component/assets/useStatic-DVVlOpvb.js
@@ -0,0 +1,1 @@
+import{b as a}from"./flowComponent-Bi8R7MU8.js";import{r as e}from"./index-B-o1Wr-g.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
