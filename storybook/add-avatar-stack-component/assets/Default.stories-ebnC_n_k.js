31e9c5aa8c2d511788ee574da077cee5efdd76a2
mittwald <>
2025-02-17 09:12:26 +0000

    Update from https://github.com/mittwald/flow/commit/06b35ed7ab0aac0c181f7704a0107db785675a31

---
+++ storybook/add-avatar-stack-component/assets/Default.stories-ebnC_n_k.js
@@ -0,0 +1,12 @@
+import{A as s,a as f}from"./Avatar-ZUKAtG7G.js";import{R as r}from"./index-B-o1Wr-g.js";import{I as g}from"./Initials-BmlCWGKS.js";import{I as A}from"./Image-DabeQpm5.js";import{d as v}from"./dummyText-CX_I_Wpl.js";import{I as E}from"./IconApp-B0VM5pA-.js";import"./clsx-B-dksMZM.js";import"./flowComponent-Bi8R7MU8.js";import"./index-ynQ6n1pc.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./react-children-utilities-xgYgah_I.js";const _={title:"Content/Avatar",component:s,render:e=>r.createElement(s,{...e},r.createElement(A,{alt:"Gopher",src:v.imageSrc})),parameters:{controls:{exclude:["className"]}},args:{size:"m"},argTypes:{size:{control:"inline-radio",options:["s","m","l"]},color:{control:"inline-radio",options:f}}},a={},t={render:e=>r.createElement(s,{...e},r.createElement(g,null,"Max Mustermann"))},o={render:e=>r.createElement(s,{...e},r.createElement(E,null)),args:{color:"blue"}};var n,m,c;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=a.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var i,p,l;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <Avatar {...props}>
+      <Initials>Max Mustermann</Initials>
+    </Avatar>
+}`,...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var d,u,I;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <Avatar {...props}>
+      <IconApp />
+    </Avatar>,
+  args: {
+    color: "blue"
+  }
+}`,...(I=(u=o.parameters)==null?void 0:u.docs)==null?void 0:I.source}}};const G=["Default","WithInitials","WithIcon"];export{a as Default,o as WithIcon,t as WithInitials,G as __namedExportsOrder,_ as default};
---
+++ storybook/add-avatar-stack-component/assets/States.stories-DIMccdO-.js
@@ -0,0 +1,36 @@
+import{B as W}from"./Button-DxJ53Gh-.js";import _ from"./Default.stories-COC6Qcqn.js";import"./IconApp-B0VM5pA-.js";import{R as r}from"./index-B-o1Wr-g.js";import{b as w,a as C}from"./IconWarning-CeoFRZGA.js";import{T as O}from"./Text-Cxo14Dks.js";import"./clsx-B-dksMZM.js";import"./flowComponent-Bi8R7MU8.js";import"./index-ynQ6n1pc.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-BIc094zz.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./Button-CPCoFgob.js";import"./utils-7gTBvjqH.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./Avatar-ZUKAtG7G.js";import"./react-children-utilities-xgYgah_I.js";import"./Tooltip-BTyBnpIw.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./dummyText-CX_I_Wpl.js";import"./Image-DabeQpm5.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Text-R-rrVGxo.js";const Dr={..._,title:"Actions/Button/States",render:m=>r.createElement(W,{...m},r.createElement(O,null,"Add email address"),r.createElement(w,null))},e={args:{isDisabled:!0}},a={args:{isDisabled:!0,variant:"plain"}},t={args:{isPending:!0}},i={args:{isPending:!0,variant:"plain"}},o={args:{isPending:!0,variant:"plain"},render:m=>r.createElement(W,{...m,"aria-label":"Add to favorites"},r.createElement(C,null))},s={args:{isSucceeded:!0}},n={args:{isFailed:!0,variant:"plain"}};var p,d,c;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(c=(d=e.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var l,u,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    variant: "plain"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var P,S,b;t.parameters={...t.parameters,docs:{...(P=t.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    isPending: true
+  }
+}`,...(b=(S=t.parameters)==null?void 0:S.docs)==null?void 0:b.source}}};var v,D,f;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  }
+}`,...(f=(D=i.parameters)==null?void 0:D.docs)==null?void 0:f.source}}};var E,B,I;o.parameters={...o.parameters,docs:{...(E=o.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  },
+  render: props => <Button {...props} aria-label="Add to favorites">
+      <IconPlus />
+    </Button>
+}`,...(I=(B=o.parameters)==null?void 0:B.docs)==null?void 0:I.source}}};var A,F,h;s.parameters={...s.parameters,docs:{...(A=s.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    isSucceeded: true
+  }
+}`,...(h=(F=s.parameters)==null?void 0:F.docs)==null?void 0:h.source}}};var x,R,T;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    isFailed: true,
+    variant: "plain"
+  }
+}`,...(T=(R=n.parameters)==null?void 0:R.docs)==null?void 0:T.source}}};const fr=["DisabledSolid","DisabledPlain","PendingSolid","PendingPlain","PendingPlainWithIcon","SucceededSolid","FailedPlain"];export{a as DisabledPlain,e as DisabledSolid,n as FailedPlain,i as PendingPlain,o as PendingPlainWithIcon,t as PendingSolid,s as SucceededSolid,fr as __namedExportsOrder,Dr as default};
