eeb643960a2d2c879d075affc0afedaad2c20c98
mittwald <>
2025-02-18 07:53:31 +0000

    Update from https://github.com/mittwald/flow/commit/2c02954e935169e27fbb063f406b61759533a815

---
+++ storybook/Nickzlos-patch-1/assets/Default.stories-Bc_mS6qT.js
@@ -0,0 +1,43 @@
+import{T as t}from"./Text-DGGzSEam.js";import{R as e}from"./index-B-o1Wr-g.js";import{S as V}from"./Section-Cv9LY6rU.js";import{a as j,s as q}from"./storyBackgrounds-GFSAi-dI.js";import"./flowComponent-Bi8R7MU8.js";import"./index-ynQ6n1pc.js";import"./clsx-B-dksMZM.js";import"./index-DolzVqEf.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./browser-B3hGj0u-.js";import"./EmulatedBoldText-1x_0FOGF.js";import"./Wrap-DQq6jo70.js";import"./Text-R-rrVGxo.js";import"./utils-7gTBvjqH.js";import"./ContextMenuSection-VhEUIc7O.js";import"./Action-_v7HSv4S.js";import"./context-tra5QY3B.js";import"./useStatic-DVVlOpvb.js";import"./getActionGroupSlot-7U_pQ_UO.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-CbvlC6bd.js";import"./Button-CPCoFgob.js";import"./ProgressBar-BQAJCbMD.js";import"./Label-CqgmsInd.js";import"./Hidden-CRdWNeH8.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DrlXsFf-.js";import"./useLocalizedStringFormatter-BQr1rA-c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-BXVt7Q_3.js";import"./useFocusRing-CTDVvBmm.js";import"./usePress-C3EQNDer.js";import"./RSPContexts-BT50PPhj.js";import"./OverlayArrow-CuXMKzfT.js";import"./useControlledState-BgxdDqm0.js";import"./Collection-CYXS91v-.js";import"./CollectionBuilder-cPXIJfS-.js";import"./Separator-C4vObYq_.js";import"./SelectionManager-ubMUrr_P.js";import"./FocusScope-DvafcTTH.js";import"./useEvent-CmPtILZD.js";import"./useCollator-DiB6vpBo.js";import"./VisuallyHidden-lWpahT0S.js";const Be={title:"Content/Text",component:t,argTypes:{elementType:{control:"inline-radio",options:["span","p"],defaultValue:"span"}},args:{elementType:"span"},render:r=>e.createElement(V,null,e.createElement(t,{...r},"Text is an unstyled component that can be used to display texts. By default it renders a span."))},o={},a={render:r=>e.createElement(t,{...r},"Text without styling parent components")},n={render:r=>e.createElement(t,{...r},"Text with unordered list:",e.createElement("ul",null,e.createElement("li",null,"Item"),e.createElement("li",null,"Item"),e.createElement("li",null,"Item")))},s={render:r=>e.createElement(t,{...r},"Text with ordered list:",e.createElement("ol",null,e.createElement("li",null,"Item"),e.createElement("li",null,"Item"),e.createElement("li",null,"Item")))},m={render:r=>e.createElement(t,{...r},e.createElement("small",null,"Small Text"))},p={args:{color:"dark"},parameters:{backgrounds:j}},i={args:{color:"light"},parameters:{backgrounds:q}},l={args:{align:"end"}};var c,d,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,T,x;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Text {...props}>Text without styling parent components</Text>
+}`,...(x=(T=a.parameters)==null?void 0:T.docs)==null?void 0:x.source}}};var E,k,S;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with unordered list:
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    </Text>
+}`,...(S=(k=n.parameters)==null?void 0:k.docs)==null?void 0:S.source}}};var h,y,I;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with ordered list:
+      <ol>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ol>
+    </Text>
+}`,...(I=(y=s.parameters)==null?void 0:y.docs)==null?void 0:I.source}}};var f,w,L;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <small>Small Text</small>
+    </Text>
+}`,...(L=(w=m.parameters)==null?void 0:w.docs)==null?void 0:L.source}}};var D,b,B;p.parameters={...p.parameters,docs:{...(D=p.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(B=(b=p.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var R,O,A;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(A=(O=i.parameters)==null?void 0:O.docs)==null?void 0:A.source}}};var U,_,C;l.parameters={...l.parameters,docs:{...(U=l.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    align: "end"
+  }
+}`,...(C=(_=l.parameters)==null?void 0:_.docs)==null?void 0:C.source}}};const Re=["Default","RawText","UnorderedList","OrderedList","SmallText","Dark","Light","AlignEnd"];export{l as AlignEnd,p as Dark,o as Default,i as Light,s as OrderedList,a as RawText,m as SmallText,n as UnorderedList,Re as __namedExportsOrder,Be as default};
