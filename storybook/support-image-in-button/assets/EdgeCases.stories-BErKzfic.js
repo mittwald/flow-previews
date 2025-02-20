b6191d4089a5468369dca7e6824f2f1a13a3ca20
mittwald <>
2025-02-20 09:59:11 +0000

    Update from https://github.com/mittwald/flow/commit/57b3f5a768640fe13e533e57c588d3949ad6e829

---
+++ storybook/support-image-in-button/assets/EdgeCases.stories-BErKzfic.js
@@ -0,0 +1,63 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{M as u}from"./ModalTrigger-BkQ58Wfe.js";import"./index-BZISi7jw.js";import{B as m}from"./Button-Bwq2LB8B.js";import y from"./Default.stories-DPgGAneg.js";import{C as h}from"./Content-Dcl2F0_Z.js";import{S as r}from"./Section-_hmbPKgP.js";import{T as n}from"./Text-CuR7qYwD.js";import{A as j}from"./ActionGroup-CE92BbTj.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{H as T}from"./Heading-DM8gHvTp.js";import{A as g}from"./Action-CBGMaTwy.js";import{u as f}from"./MenuTrigger-YQWMfric.js";import"./clsx-B-dksMZM.js";import"./flowComponent-B-ghCXY_.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./OverlayContent-DQPWguzJ.js";import"./Dialog-B_fESGfq.js";import"./Button-BBWo3kGM.js";import"./utils-CzVyYWZn.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-3nCN3lnL.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./Text-PTaPc_Ci.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./IconApp-vAs-LrT2.js";import"./IconWarning-DIwIHW1V.js";import"./Header-BEO744sA.js";import"./ButtonView-BRZGva8G.js";import"./DialogTriggerView-BZmxzAcU.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-D9XbF_eS.js";import"./TextField-C_aJGins.js";import"./TextFieldBase-CJU6WI_U.js";import"./FieldError-DQ-SI-Q3.js";import"./FieldError-DoW3KOQ8.js";import"./FieldDescription-D2Kld19z.js";import"./Form-D162gIjQ.js";import"./Input-DaA3cxmW.js";import"./useTextField-uM6U_XJE.js";import"./useFormReset-CilIRCFg.js";import"./Label-_wcwLnA8.js";import"./lib-rGAx4MC5.js";import"./chunk-D5ZWXAHU-Dm3eDOzv.js";import"./v4-CtRu48qb.js";import"./Form-CmGvRP0_.js";import"./dynamic-DKDa4OpU.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Heading-BCoG4c8D.js";import"./context-CYJytrr2.js";import"./useStatic-CJjI45yZ.js";import"./getActionGroupSlot-Bf7lgP6T.js";import"./ContextMenuSection-Cfu5tWcF.js";const Jo={...y,title:"Overlays/Modal/Edge Cases"},e={render:s=>o.jsxs(u,{...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})},i={render:s=>o.jsxs(u,{offCanvas:!0,...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})};var c,l,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(p=(l=e.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var a,d,x;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Modal offCanvas {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(x=(d=i.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const Ko=["LongContent","LongContentOffCanvas"];export{e as LongContent,i as LongContentOffCanvas,Ko as __namedExportsOrder,Jo as default};
