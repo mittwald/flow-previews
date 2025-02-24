9e24426f4aea33ea481ff8c39fe2608b99be82e0
mittwald <>
2025-02-24 07:30:22 +0000

    Update from https://github.com/mittwald/flow/commit/b02d0113c279f7b0696ed4d542fee8110aa40678

---
+++ storybook/add-chat-separator-component/assets/Default.stories-OL-YzrRz.js
@@ -0,0 +1,69 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r as H}from"./index-BZISi7jw.js";import{L as s}from"./Label-Cn-E9sJO.js";import{F as o}from"./FileField-Dp7UKpty.js";import{B as t}from"./Button-BDlP74xq.js";import{S as P}from"./Section-3nLNVgY1.js";import{F as R}from"./FieldError-VFT6O44b.js";import{F as k}from"./FieldDescription-DVdM9Jga.js";import{c as G,a as q}from"./IconApp-B-TnySLn.js";import{u as w,F as U,t as _}from"./Form-BPymN5xw.js";import{a as M}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import{A as O}from"./ActionGroup-BTtZWeGd.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-C6bHOsmH.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./Label-RYyxMwZH.js";import"./utils-CzVyYWZn.js";import"./Hidden-DBxI3g91.js";import"./FieldError-DoW3KOQ8.js";import"./useFocusRing-DzGSygZM.js";import"./Text-PTaPc_Ci.js";import"./filterDOMProps-CeZl_uWj.js";import"./Input-DaA3cxmW.js";import"./IconWarning-BB1u60Fs.js";import"./Wrap-DQq6jo70.js";import"./Text-C9p5wZii.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./LoadingSpinner-DU9wsl3-.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./usePress-DZCrAIzv.js";import"./ContextMenuSection-CZkT5tzv.js";import"./Action-BrQvp_5i.js";import"./context-Z26u4tY9.js";import"./useStatic-BHWj4WSG.js";import"./getActionGroupSlot-BQuzfC4b.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BV8uMRwe.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./v4-CtRu48qb.js";/**
+ * @license @tabler/icons-react v3.30.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var z=G("outline","paperclip","IconPaperclip",[["path",{d:"M15 7l-6.5 6.5a1.5 1.5 0 0 0 3 3l6.5 -6.5a3 3 0 0 0 -6 -6l-6.5 6.5a4.5 4.5 0 0 0 9 9l6.5 -6.5",key:"svg-0"}]]);const Xe={title:"Form Controls/FileField",component:o,render:r=>e.jsxs(o,{...r,children:[e.jsx(s,{children:"Certificate"}),e.jsx(t,{variant:"outline",color:"secondary",children:"Select"})]})},J=M("submit"),l={},c={render:r=>e.jsxs(o,{...r,children:[e.jsx(s,{children:"Certificate"}),e.jsx(t,{variant:"outline",color:"secondary",children:"Select"}),e.jsx(k,{children:"Supported formats: .pem, .pfx"})]})},p={render:r=>e.jsxs(o,{...r,isInvalid:!0,children:[e.jsx(s,{children:"Certificate"}),e.jsx(t,{variant:"outline",color:"secondary",children:"Select"}),e.jsx(R,{children:"File too large"})]})},m={render:r=>e.jsx(o,{...r,children:e.jsx(t,{variant:"soft","aria-label":"Select certificate",children:e.jsx(q,{children:e.jsx(z,{})})})})},d={render:r=>{const[n,a]=H.useState(null),i=n==null?void 0:n[0];return e.jsxs(P,{children:[e.jsxs(o,{...r,isInvalid:!0,onChange:a,children:[e.jsx(s,{children:"Certificate"}),e.jsx(t,{variant:"outline",color:"secondary",children:"Select"})]}),i==null?void 0:i.name]})}},u={render:()=>{var a,i;const r=w(),n=_();return e.jsx(U,{form:r,onSubmit:J,children:e.jsxs(P,{children:[e.jsx(n,{name:"file",rules:{required:"Please choose a file"},children:e.jsxs(o,{multiple:!0,children:[e.jsx(s,{children:"Certificate"}),e.jsx(t,{variant:"outline",color:"secondary",children:"Select"})]})}),(i=(a=r.watch("file"))==null?void 0:a[0])==null?void 0:i.name,e.jsx(O,{children:e.jsx(t,{type:"submit",children:"Upload"})})]})})}};var F,f,h;l.parameters={...l.parameters,docs:{...(F=l.parameters)==null?void 0:F.docs,source:{originalSource:"{}",...(h=(f=l.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var x,j,S;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <FileField {...props}>
+      <Label>Certificate</Label>
+      <Button variant="outline" color="secondary">
+        Select
+      </Button>
+      <FieldDescription>Supported formats: .pem, .pfx</FieldDescription>
+    </FileField>
+}`,...(S=(j=c.parameters)==null?void 0:j.docs)==null?void 0:S.source}}};var b,v,B;p.parameters={...p.parameters,docs:{...(b=p.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <FileField {...props} isInvalid>
+      <Label>Certificate</Label>
+      <Button variant="outline" color="secondary">
+        Select
+      </Button>
+      <FieldError>File too large</FieldError>
+    </FileField>
+}`,...(B=(v=p.parameters)==null?void 0:v.docs)==null?void 0:B.source}}};var y,C,I;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <FileField {...props}>
+      <Button variant="soft" aria-label="Select certificate">
+        <Icon>
+          <IconPaperclip />
+        </Icon>
+      </Button>
+    </FileField>
+}`,...(I=(C=m.parameters)==null?void 0:C.docs)==null?void 0:I.source}}};var L,g,E;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: props => {
+    const [files, setFiles] = useState<FileList | null>(null);
+    const file = files?.[0];
+    return <Section>
+        <FileField {...props} isInvalid onChange={setFiles}>
+          <Label>Certificate</Label>
+          <Button variant="outline" color="secondary">
+            Select
+          </Button>
+        </FileField>
+        {file?.name}
+      </Section>;
+  }
+}`,...(E=(g=d.parameters)==null?void 0:g.docs)==null?void 0:E.source}}};var W,D,A;u.parameters={...u.parameters,docs:{...(W=u.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: () => {
+    const form = useForm<{
+      file: FileList | null;
+    }>();
+    const Field = typedField(form);
+    return <Form form={form} onSubmit={submitAction}>
+        <Section>
+          <Field name="file" rules={{
+          required: "Please choose a file"
+        }}>
+            <FileField multiple>
+              <Label>Certificate</Label>
+              <Button variant="outline" color="secondary">
+                Select
+              </Button>
+            </FileField>
+          </Field>
+          {form.watch("file")?.[0]?.name}
+          <ActionGroup>
+            <Button type="submit">Upload</Button>
+          </ActionGroup>
+        </Section>
+      </Form>;
+  }
+}`,...(A=(D=u.parameters)==null?void 0:D.docs)==null?void 0:A.source}}};const Ye=["Default","WithDescription","WithError","IconButton","WithHandler","WithReactHookForm"];export{l as Default,m as IconButton,c as WithDescription,p as WithError,d as WithHandler,u as WithReactHookForm,Ye as __namedExportsOrder,Xe as default};
