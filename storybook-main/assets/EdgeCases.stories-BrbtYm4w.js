2f51b2438d854146fec4311d2ae4c8ee97ab5ab3
mittwald <>
2025-02-25 07:38:14 +0000

    Update from https://github.com/mittwald/flow/commit/34995e55cb01dab2efe366c67385f8206825887a

---
+++ storybook-main/assets/EdgeCases.stories-BrbtYm4w.js
@@ -0,0 +1,54 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import{m as h,T as a,a as o,b as n}from"./Default.stories-DsgWi9p3.js";import{S as r}from"./Section-BsXEPCGH.js";import{T as e}from"./Text-Cf2tf5FC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./clsx-B-dksMZM.js";import"./ContextMenuContent-CBbCSPDV.js";import"./ContextMenuSection-Cr5cVYnI.js";import"./Action-BSwnzS5s.js";import"./context-C8ohvFo-.js";import"./useStatic-DnpCrHZX.js";import"./browser-B1A6F7LW.js";import"./getActionGroupSlot-THtyb9Nj.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BV8uMRwe.js";import"./Button-BBWo3kGM.js";import"./utils-CzVyYWZn.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./Text-PTaPc_Ci.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./Popover-CR86BZCA.js";import"./MenuTrigger-CEhyUIft.js";import"./DialogTriggerView-PpJDYL3B.js";import"./IconApp-2NmtClNa.js";import"./IconWarning-BmnfB8Vv.js";import"./Switch-D01BezOK.js";import"./Label-DyyPFqCS.js";import"./useToggleState-B7W1plhv.js";import"./useFormReset-CilIRCFg.js";import"./Button-Bcoi9wQu.js";import"./Wrap-DQq6jo70.js";import"./LoadingSpinner-Cbr7OqZd.js";import"./useHasTabbableChild-5OHQREjG.js";import"./useSingleSelectListState-HCetLLf6.js";import"./useListState-YiQIo-bF.js";import"./Activity-DrXcoKbv.js";import"./Heading-C443cwOq.js";import"./Heading-BCoG4c8D.js";import"./TextField-DmgLZ27n.js";import"./TextFieldBase-TxoyKGR2.js";import"./FieldError-D2mTtwek.js";import"./FieldError-DoW3KOQ8.js";import"./FieldDescription-BR4uF6lQ.js";import"./Form-D162gIjQ.js";import"./Input-DaA3cxmW.js";import"./useTextField-uM6U_XJE.js";import"./LabeledValue-DKhEUmXV.js";import"./Header-DPaXAUXM.js";import"./AlertIcon-B1tqC-yI.js";import"./EmulatedBoldText-BfYT5ZHF.js";const zt={title:"Navigation/Tabs/Edge Cases",...h},s={render:m=>t.jsxs(a,{...m,children:[t.jsxs(o,{id:"a",children:[t.jsx(n,{children:"Tab A"}),t.jsx(r,{children:t.jsx(e,{children:i.long})})]}),t.jsxs(o,{id:"b",children:[t.jsx(n,{children:"Tab B"}),t.jsx(r,{children:t.jsx(e,{children:i.long})}),t.jsx(r,{children:t.jsx(e,{children:i.long})}),t.jsx(r,{children:t.jsx(e,{children:i.long})}),t.jsx(r,{children:t.jsx(e,{children:i.long})})]})]})},p={render:m=>t.jsxs(a,{...m,children:[t.jsxs(o,{id:"a",children:[t.jsx(n,{children:"Tab A"}),t.jsx(r,{children:t.jsx(e,{children:i.short})})]}),t.jsxs(o,{id:"b",children:[t.jsx(n,{children:"Tab B"}),t.jsxs(r,{children:[t.jsx(e,{children:i.short}),t.jsxs(a,{...m,disabledKeys:["spam"],children:[t.jsxs(o,{id:"subA",children:[t.jsx(n,{children:"Sub tab A"}),t.jsx(r,{children:t.jsx(e,{children:i.short})})]}),t.jsxs(o,{id:"subB",children:[t.jsx(n,{children:"Sub tab B"}),t.jsx(r,{children:t.jsx(e,{children:i.short})})]})]})]})]})]})};var T,d,c;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <Tabs {...props}>
+      <Tab id="a">
+        <TabTitle>Tab A</TabTitle>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Tab>
+      <Tab id="b">
+        <TabTitle>Tab B</TabTitle>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Tab>
+    </Tabs>
+}`,...(c=(d=s.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var x,l,b;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <Tabs {...props}>
+      <Tab id="a">
+        <TabTitle>Tab A</TabTitle>
+        <Section>
+          <Text>{dummyText.short}</Text>
+        </Section>
+      </Tab>
+      <Tab id="b">
+        <TabTitle>Tab B</TabTitle>
+        <Section>
+          <Text>{dummyText.short}</Text>
+          <Tabs {...props} disabledKeys={["spam"]}>
+            <Tab id="subA">
+              <TabTitle>Sub tab A</TabTitle>
+              <Section>
+                <Text>{dummyText.short}</Text>
+              </Section>
+            </Tab>
+            <Tab id="subB">
+              <TabTitle>Sub tab B</TabTitle>
+              <Section>
+                <Text>{dummyText.short}</Text>
+              </Section>
+            </Tab>
+          </Tabs>
+        </Section>
+      </Tab>
+    </Tabs>
+}`,...(b=(l=p.parameters)==null?void 0:l.docs)==null?void 0:b.source}}};const Ft=["DifferentHeight","Nested"];export{s as DifferentHeight,p as Nested,Ft as __namedExportsOrder,zt as default};
---
+++ storybook-main/assets/EdgeCases.stories-D8e8xNA3.js
@@ -0,0 +1,92 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{H as B}from"./Heading-C443cwOq.js";import{T as x}from"./Text-Cf2tf5FC.js";import{m as j,t as r,R as g,u as h,L as C}from"./Default.stories-B8ZXaLqi.js";import{A as I}from"./Avatar-DoEfqei1.js";import{I as w}from"./Initials-CqJ9vt_y.js";import{C as a}from"./Content-B1QEbK3k.js";import{C as V,M}from"./ContextMenuContent-CBbCSPDV.js";import"./ContextMenuSection-Cr5cVYnI.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./Heading-BCoG4c8D.js";import"./RSPContexts-BEYwq1Zw.js";import"./utils-CzVyYWZn.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./Text-PTaPc_Ci.js";import"./IconApp-2NmtClNa.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./ButtonView-DKbiKsIa.js";import"./Button-Bcoi9wQu.js";import"./IconWarning-BmnfB8Vv.js";import"./LoadingSpinner-Cbr7OqZd.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./Tooltip-D1bGwq0W.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Badge-D_kliszS.js";import"./context-C8ohvFo-.js";import"./useStatic-DnpCrHZX.js";import"./SearchField-B0jytSC-.js";import"./FieldError-D2mTtwek.js";import"./FieldError-DoW3KOQ8.js";import"./Form-D162gIjQ.js";import"./Group-Df3SyVuJ.js";import"./Input-DaA3cxmW.js";import"./useTextField-uM6U_XJE.js";import"./useFormReset-CilIRCFg.js";import"./IllustratedMessage-CItdvToX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./DragAndDrop-CvXJIw9w.js";import"./ListBox-CC2Hg4MK.js";import"./Dialog-BV8uMRwe.js";import"./Separator-DNY_h6GP.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./useListState-YiQIo-bF.js";import"./Table-Dh2XaqIS.js";import"./SkeletonText-iV6GVzrR.js";import"./useUpdateEffect-Bw-e1fiu.js";import"./useHasTabbableChild-5OHQREjG.js";import"./dynamic-DKDa4OpU.js";import"./Section-BsXEPCGH.js";import"./datetime-CJSQP4eG.js";import"./Skeleton-DC0hmluT.js";import"./react-children-utilities-fm2cgFnI.js";import"./ActionGroup-BQ8LoA2z.js";import"./getActionGroupSlot-THtyb9Nj.js";import"./AlertBadge-C01mK4wq.js";import"./AlertIcon-B1tqC-yI.js";import"./onlyText-DBswRDk6.js";import"./Popover-CR86BZCA.js";import"./MenuTrigger-CEhyUIft.js";import"./DialogTriggerView-PpJDYL3B.js";import"./Action-BSwnzS5s.js";import"./Switch-D01BezOK.js";import"./Label-DyyPFqCS.js";import"./useToggleState-B7W1plhv.js";const lo={...j,title:"Structure/List/Edge Cases"},f=()=>new Promise(o=>window.setTimeout(o,2e3)),S=async o=>(await f(),`${o}@info.de`),i={render:()=>{const o=r();return t.jsxs(o.List,{batchSize:5,"aria-label":"List",children:[t.jsx(o.StaticData,{data:[{name:"John"},{name:"Max"}]}),t.jsx(o.Item,{textValue:e=>e.name,children:e=>t.jsx(g,{children:()=>{const H=h(S,[e.name]);return t.jsxs(C,{children:[t.jsx(B,{children:e.name}),t.jsx(x,{children:H})]})}})})]})}},n={render:()=>{const o=r();return t.jsxs(o.List,{batchSize:5,"aria-label":"List",defaultViewMode:"table",children:[t.jsx(o.StaticData,{data:[{name:"John"},{name:"Max"}]}),t.jsxs(o.Table,{children:[t.jsxs(o.TableHeader,{children:[t.jsx(o.TableColumn,{children:"Name"}),t.jsx(o.TableColumn,{children:"Email"})]}),t.jsx(o.TableBody,{children:t.jsxs(o.TableRow,{children:[t.jsx(o.TableCell,{children:e=>e.name}),t.jsx(o.TableCell,{children:e=>t.jsx(g,{children:()=>h(S,[e.name])})})]})})]})]})}},m={render:()=>{const o=r();return t.jsxs(o.List,{children:[t.jsx(o.StaticData,{data:[{name:"John Doe"}]}),t.jsx(o.Item,{showTiles:!0,textValue:e=>e.name,children:e=>t.jsxs(o.ItemView,{children:[t.jsx(I,{children:t.jsx(w,{children:e.name})}),t.jsx(B,{children:"HeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeading"}),t.jsx(x,{children:"SubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitle"}),t.jsx(a,{slot:"top",children:"TopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTop"}),t.jsx(a,{slot:"bottom",children:"BottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottom"}),t.jsx(V,{children:t.jsx(M,{children:"Show details"})})]})})]})}};var p,s,l;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List batchSize={5} aria-label="List">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Item textValue={item => item.name}>
+          {item => <Render>
+              {() => {
+            const email = usePromise(getEmail, [item.name]);
+            return <ListItemView>
+                    <Heading>{item.name}</Heading>
+                    <Text>{email}</Text>
+                  </ListItemView>;
+          }}
+            </Render>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(l=(s=i.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var T,d,u;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List batchSize={5} aria-label="List" defaultViewMode="table">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Table>
+          <List.TableHeader>
+            <List.TableColumn>Name</List.TableColumn>
+            <List.TableColumn>Email</List.TableColumn>
+          </List.TableHeader>
+
+          <List.TableBody>
+            <List.TableRow>
+              <List.TableCell>{item => item.name}</List.TableCell>
+              <List.TableCell>
+                {item => <Render>
+                    {() => {
+                  return usePromise(getEmail, [item.name]);
+                }}
+                  </Render>}
+              </List.TableCell>
+            </List.TableRow>
+          </List.TableBody>
+        </List.Table>
+      </List.List>;
+  }
+}`,...(u=(d=n.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var c,L,b;m.parameters={...m.parameters,docs:{...(c=m.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List>
+        <List.StaticData data={[{
+        name: "John Doe"
+      }]} />
+        <List.Item showTiles textValue={user => user.name}>
+          {user => <List.ItemView>
+              <Avatar>
+                <Initials>{user.name}</Initials>
+              </Avatar>
+              <Heading>
+                HeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeading
+              </Heading>
+              <Text>
+                SubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitle
+              </Text>
+              <Content slot="top">
+                TopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTop
+              </Content>
+              <Content slot="bottom">
+                BottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottom
+              </Content>
+              <ContextMenu>
+                <MenuItem>Show details</MenuItem>
+              </ContextMenu>
+            </List.ItemView>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(b=(L=m.parameters)==null?void 0:L.docs)==null?void 0:b.source}}};const To=["LoadingListItem","LoadingTableCell","VeryLongWords"];export{i as LoadingListItem,n as LoadingTableCell,m as VeryLongWords,To as __namedExportsOrder,lo as default};
