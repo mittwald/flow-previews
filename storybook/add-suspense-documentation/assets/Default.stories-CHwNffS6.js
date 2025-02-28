6f1d956483aba63f44667a209683e562af24ea43
mittwald <>
2025-02-28 07:57:35 +0000

    Update from https://github.com/mittwald/flow/commit/3922fdda3f4f831f87ca25161f924bc160db3c8b

---
+++ storybook/add-suspense-documentation/assets/Default.stories-CHwNffS6.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{S as o}from"./SearchField-k_61rp4N.js";import"./index-BZISi7jw.js";import{a as E}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import{L as v}from"./Label-DyyPFqCS.js";import{F as L}from"./FieldDescription-BR4uF6lQ.js";import{F as W}from"./FieldError-SHLXg733.js";import"./clsx-B-dksMZM.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./Button-D1dLS-jR.js";import"./IconApp-2NmtClNa.js";import"./IconWarning-D17V2fWY.js";import"./Wrap-DQq6jo70.js";import"./Text-Cf2tf5FC.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Text-PTaPc_Ci.js";import"./utils-CzVyYWZn.js";import"./LoadingSpinner-CBBIEtuR.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./FieldError-DoW3KOQ8.js";import"./Form-D162gIjQ.js";import"./Group-Df3SyVuJ.js";import"./Input-DaA3cxmW.js";import"./useControlledState-4nfZ-XMX.js";import"./useTextField-uM6U_XJE.js";import"./useFormReset-CilIRCFg.js";import"./v4-CtRu48qb.js";const Sr={title:"Form Controls/SearchField",component:o,render:r=>e.jsx(o,{onChange:E("onChange"),...r})},i={},t={args:{isDisabled:!0}},s={render:r=>e.jsx(o,{...r,children:e.jsx(L,{children:"Press enter to search"})})},a={render:r=>e.jsx(o,{...r,children:e.jsx(v,{children:"Suche"})})},p={render:r=>e.jsx(o,{...r,defaultValue:"test",isInvalid:!0,children:e.jsx(W,{children:"Invalid search value"})})};var m,c,n;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(n=(c=i.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var d,l,u;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(u=(l=t.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var h,F,S;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <SearchField {...props}>
+      <FieldDescription>Press enter to search</FieldDescription>
+    </SearchField>
+}`,...(S=(F=s.parameters)==null?void 0:F.docs)==null?void 0:S.source}}};var f,x,D;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <SearchField {...props}>
+      <Label>Suche</Label>
+    </SearchField>
+}`,...(D=(x=a.parameters)==null?void 0:x.docs)==null?void 0:D.source}}};var b,g,j;p.parameters={...p.parameters,docs:{...(b=p.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <SearchField {...props} defaultValue="test" isInvalid>
+      <FieldError>Invalid search value</FieldError>
+    </SearchField>
+}`,...(j=(g=p.parameters)==null?void 0:g.docs)==null?void 0:j.source}}};const fr=["Default","Disabled","WithFieldDescription","WithLabel","WithFieldError"];export{i as Default,t as Disabled,s as WithFieldDescription,p as WithFieldError,a as WithLabel,fr as __namedExportsOrder,Sr as default};
---
+++ storybook/add-suspense-documentation/assets/Default.stories-Lyvrmk9r.js
@@ -0,0 +1,14 @@
+import{j as C}from"./jsx-runtime-D_zvdyIk.js";import{C as s}from"./Checkbox-Dkxvuz1i.js";import"./index-BZISi7jw.js";import{a as S}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import"./IconApp-2NmtClNa.js";import"./clsx-B-dksMZM.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./IconWarning-D17V2fWY.js";import"./RSPContexts-BEYwq1Zw.js";import"./utils-CzVyYWZn.js";import"./FieldError-DoW3KOQ8.js";import"./useFocusRing-DzGSygZM.js";import"./Text-PTaPc_Ci.js";import"./filterDOMProps-CeZl_uWj.js";import"./Form-D162gIjQ.js";import"./useLabel-Hju3pN3c.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./usePress-DZCrAIzv.js";import"./useToggleState-B7W1plhv.js";import"./useFormReset-CilIRCFg.js";import"./useFocusable-CdO2YIL_.js";import"./useControlledState-4nfZ-XMX.js";import"./VisuallyHidden-DJvehTyr.js";import"./v4-CtRu48qb.js";const W={title:"Form Controls/Checkbox",component:s,args:{onChange:S("onChange")},parameters:{controls:{exclude:["onChange"]}},render:x=>C.jsx(s,{...x,children:"Consent to terms and conditions"})},r={},e={args:{isDisabled:!0}},t={args:{isIndeterminate:!0}},o={args:{isDisabled:!0,isSelected:!0}};var a,i,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var n,p,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,u,l;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    isIndeterminate: true
+  }
+}`,...(l=(u=t.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var g,b,D;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    isSelected: true
+  }
+}`,...(D=(b=o.parameters)==null?void 0:b.docs)==null?void 0:D.source}}};const X=["Default","Disabled","Indeterminate","DisabledSelected"];export{r as Default,e as Disabled,o as DisabledSelected,t as Indeterminate,X as __namedExportsOrder,W as default};
