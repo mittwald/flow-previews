6f59956bf656eed01cb038a7a3bcae82bc3abe12
mittwald <>
2025-03-03 14:47:02 +0000

    Update from https://github.com/mittwald/flow/commit/e1baee2bf804e33e638609f5f1dcbe2624305a0c

---
+++ storybook/add/missingPropsContextExport/assets/Default.stories-mZbz0JxW.js
@@ -0,0 +1,39 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{L as e}from"./Link-DBSJhW7Z.js";import{a as A}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import"./index-BZISi7jw.js";import{T as N}from"./Text-Cf2tf5FC.js";import{a as O,s as R}from"./storyBackgrounds-GFSAi-dI.js";import"./IconApp-2NmtClNa.js";import{r as q}from"./IconWarning-eLy06F5O.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./clsx-B-dksMZM.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./utils-CzVyYWZn.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-DZCrAIzv.js";import"./useFocusRing-DzGSygZM.js";import"./useFocusable-CdO2YIL_.js";import"./v4-CtRu48qb.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./Text-PTaPc_Ci.js";const mr={title:"Navigation/Link",component:e,args:{onPress:A("onPress"),isDisabled:!1},render:r=>o.jsx(e,{...r,children:"Adjust project"}),parameters:{controls:{exclude:["onPress"]}},argTypes:{color:{control:"inline-radio",options:["primary","dark","light"]},isDisabled:{control:"boolean"}}},a={},s={render:r=>o.jsxs(N,{children:["Check our ",o.jsx(e,{...r,children:"onboarding"})," for more information."]})},n={args:{isDisabled:!0}},t={render:r=>o.jsxs(e,{...r,href:"https://mittwald.de",children:["Download invoice",o.jsx(q,{"aria-label":"download"})]})},i={args:{color:"dark"},parameters:{backgrounds:O}},c={args:{color:"light"},parameters:{backgrounds:R}},d={args:{target:"_blank"},render:r=>o.jsx(e,{...r,children:"mittwald.de"})},p={args:{target:"_blank",download:!0},render:r=>o.jsx(e,{...r,children:"Download file"})};var m,l,u;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(u=(l=a.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var g,k,h;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Text>
+      Check our <Link {...props}>onboarding</Link> for more information.
+    </Text>
+}`,...(h=(k=s.parameters)==null?void 0:k.docs)==null?void 0:h.source}}};var D,b,f;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(f=(b=n.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var x,L,w;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <Link {...props} href="https://mittwald.de">
+      Download invoice
+      <IconDownload aria-label="download" />
+    </Link>
+}`,...(w=(L=t.parameters)==null?void 0:L.docs)==null?void 0:w.source}}};var j,S,y;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(y=(S=i.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};var I,_,T;c.parameters={...c.parameters,docs:{...(I=c.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(T=(_=c.parameters)==null?void 0:_.docs)==null?void 0:T.source}}};var B,E,v;d.parameters={...d.parameters,docs:{...(B=d.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    target: "_blank"
+  },
+  render: props => <Link {...props}>mittwald.de</Link>
+}`,...(v=(E=d.parameters)==null?void 0:E.docs)==null?void 0:v.source}}};var P,C,W;p.parameters={...p.parameters,docs:{...(P=p.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    target: "_blank",
+    download: true
+  },
+  render: props => <Link {...props}>Download file</Link>
+}`,...(W=(C=p.parameters)==null?void 0:C.docs)==null?void 0:W.source}}};const lr=["Default","Inline","Disabled","WithIcon","Dark","Light","ExternalLink","Download"];export{i as Dark,a as Default,n as Disabled,p as Download,d as ExternalLink,s as Inline,c as Light,t as WithIcon,lr as __namedExportsOrder,mr as default};
