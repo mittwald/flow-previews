d7ed1998b4e829a34619d0a280c0c279c5647d59
mittwald <>
2025-03-04 10:54:46 +0000

    Update from https://github.com/mittwald/flow/commit/a4cb397b5da40341c26a9f89e0d7e7a6a961d796

---
+++ storybook/add-flex-component/assets/Default.stories-itMwmilU.js
@@ -0,0 +1,28 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{F as p}from"./Flex-CO6xmqZO.js";import{L as x}from"./LayoutCard-B0oqJmem.js";import{S as u}from"./Section-BsXEPCGH.js";import{T as h}from"./Text-Cf2tf5FC.js";import{d as r}from"./dummyText-CX_I_Wpl.js";import{B as f}from"./Button-DMvMpHNz.js";import"./browser-B1A6F7LW.js";import{t as L}from"./typedList-C1eq68Nn.js";import{A as j}from"./Avatar-DoEfqei1.js";import{I as g}from"./Image-DGzLV-KN.js";import{H as w}from"./Heading-C443cwOq.js";import{C as y}from"./Content-B1QEbK3k.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./ContextMenuSection-Cr5cVYnI.js";import"./Action-BSwnzS5s.js";import"./context-C8ohvFo-.js";import"./useStatic-DnpCrHZX.js";import"./getActionGroupSlot-THtyb9Nj.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BV8uMRwe.js";import"./Button-BBWo3kGM.js";import"./utils-CzVyYWZn.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./Text-PTaPc_Ci.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./IconApp-2NmtClNa.js";import"./IconWarning-eLy06F5O.js";import"./LoadingSpinner-BUdWX6OM.js";import"./ContextMenuContent-C9TXG0sK.js";import"./Popover-CR86BZCA.js";import"./MenuTrigger-CEhyUIft.js";import"./DialogTriggerView-PpJDYL3B.js";import"./Switch-B0PRXbfZ.js";import"./Label-DyyPFqCS.js";import"./useToggleState-B7W1plhv.js";import"./useFormReset-CilIRCFg.js";import"./ButtonView-m4E__OTL.js";import"./Tooltip-D1bGwq0W.js";import"./Badge-CXg-3L40.js";import"./SearchField-BtyMNGKL.js";import"./FieldError-Cl07oYpm.js";import"./FieldError-DoW3KOQ8.js";import"./Form-D162gIjQ.js";import"./Group-Df3SyVuJ.js";import"./Input-DaA3cxmW.js";import"./useTextField-uM6U_XJE.js";import"./IllustratedMessage-CItdvToX.js";import"./DragAndDrop-CvXJIw9w.js";import"./ListBox-CC2Hg4MK.js";import"./useListState-YiQIo-bF.js";import"./Table-Dh2XaqIS.js";import"./SkeletonText-iV6GVzrR.js";import"./useUpdateEffect-Bw-e1fiu.js";import"./useHasTabbableChild-5OHQREjG.js";import"./datetime-CJSQP4eG.js";import"./Skeleton-DC0hmluT.js";import"./react-children-utilities-fm2cgFnI.js";import"./ActionGroup-BQ8LoA2z.js";import"./Heading-BCoG4c8D.js";const ir={title:"Structure/Flex",component:p,render:()=>t.jsxs(x,{style:{height:300},children:[t.jsx(u,{children:t.jsx(h,{children:r.medium})}),t.jsx(p,{justify:"end",align:"end",grow:!0,children:t.jsx(f,{color:"accent",children:"Install"})})]})},o={},m={render:()=>{const i=L();return t.jsxs(i.List,{defaultViewMode:"tiles",children:[t.jsx(i.StaticData,{data:[{text:r.short},{text:`${r.short} ${r.short}`}]}),t.jsx(i.Item,{showTiles:!0,showList:!1,textValue:e=>e.text,children:e=>t.jsxs(i.ItemView,{children:[t.jsx(j,{children:t.jsx(g,{alt:"",src:r.imageSrc})}),t.jsx(w,{children:e.text}),t.jsx(y,{children:t.jsx(p,{justify:"end",align:"end",grow:!0,children:"Free"})})]})})]})}};var s,n,a;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(n=o.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var d,c,l;m.parameters={...m.parameters,docs:{...(d=m.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      text: string;
+    }>();
+    return <List.List defaultViewMode="tiles">
+        <List.StaticData data={[{
+        text: dummyText.short
+      }, {
+        text: \`\${dummyText.short} \${dummyText.short}\`
+      }]} />
+        <List.Item showTiles showList={false} textValue={i => i.text}>
+          {i => <List.ItemView>
+              <Avatar>
+                <Image alt="" src={dummyText.imageSrc} />
+              </Avatar>
+              <Heading>{i.text}</Heading>
+
+              <Content>
+                <Flex justify="end" align="end" grow>
+                  Free
+                </Flex>
+              </Content>
+            </List.ItemView>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(l=(c=m.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const or=["Default","ListItemContent"];export{o as Default,m as ListItemContent,or as __namedExportsOrder,ir as default};
