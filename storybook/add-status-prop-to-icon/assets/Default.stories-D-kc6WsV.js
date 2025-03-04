a599cffd4e1410c16dfe4dee61d9e22a168c2cf9
mittwald <>
2025-03-04 14:26:48 +0000

    Update from https://github.com/mittwald/flow/commit/7709f7944baa5c84e8c31406bc3198051627ae91

---
+++ storybook/add-status-prop-to-icon/assets/Default.stories-D-kc6WsV.js
@@ -0,0 +1,32 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{B as o}from"./Button-CiQsPemY.js";import{A as T}from"./ActionGroup-BQ8LoA2z.js";import{L as s,a as y}from"./LightBoxTrigger-BAdG7lg-.js";import{I as m}from"./Image-DGzLV-KN.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import"./IconApp-DGVUqOEb.js";import{r as G,s as O}from"./IconWarning-6AwrC8gW.js";import{u as w}from"./MenuTrigger-CEhyUIft.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-QaLaNUaQ.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./Wrap-DQq6jo70.js";import"./Text-Cf2tf5FC.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Text-PTaPc_Ci.js";import"./utils-CzVyYWZn.js";import"./LoadingSpinner-DnMtsiV2.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-THtyb9Nj.js";import"./useStatic-DnpCrHZX.js";import"./OverlayContent-HC9_xAJi.js";import"./Dialog-BV8uMRwe.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./context-C8ohvFo-.js";const Or={title:"Overlays/LightBox",component:s,parameters:{controls:{exclude:["controller"]}},render:a=>r.jsxs(y,{children:[r.jsx(o,{children:"Open LightBox"}),r.jsxs(s,{...a,children:[r.jsx(m,{src:p.imageSrc}),r.jsxs(T,{children:[r.jsx(o,{children:r.jsx(G,{})}),r.jsx(o,{children:r.jsx(O,{})})]})]})]})},t={},e={args:{fitScreen:!1}},i={render:a=>{const c=w("LightBox");return r.jsxs(r.Fragment,{children:[r.jsx(o,{onPress:c.open,children:"Open LightBox"}),r.jsxs(s,{...a,controller:c,children:[r.jsx(m,{src:p.imageSrc}),r.jsxs(T,{children:[r.jsx(o,{children:r.jsx(G,{})}),r.jsx(o,{children:r.jsx(O,{})})]})]})]})}},n={render:()=>r.jsxs(y,{children:[r.jsx(o,{children:r.jsx(m,{width:100,withBorder:!0,alt:"Gopher",src:p.imageSrc})}),r.jsx(s,{children:r.jsx(m,{alt:"Gopher",src:p.imageSrc})})]})};var l,d,x;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(x=(d=t.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};var g,h,u;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    fitScreen: false
+  }
+}`,...(u=(h=e.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var B,j,f;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController("LightBox");
+    return <>
+        <Button onPress={controller.open}>Open LightBox</Button>
+        <LightBox {...props} controller={controller}>
+          <Image src={dummyText.imageSrc} />
+          <ActionGroup>
+            <Button>
+              <IconDownload />
+            </Button>
+            <Button>
+              <IconDelete />
+            </Button>
+          </ActionGroup>
+        </LightBox>
+      </>;
+  }
+}`,...(f=(j=i.parameters)==null?void 0:j.docs)==null?void 0:f.source}}};var L,S,I;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: () => <LightBoxTrigger>
+      <Button>
+        <Image width={100} withBorder alt="Gopher" src={dummyText.imageSrc} />
+      </Button>
+      <LightBox>
+        <Image alt="Gopher" src={dummyText.imageSrc} />
+      </LightBox>
+    </LightBoxTrigger>
+}`,...(I=(S=n.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};const wr=["Default","WithoutFitScreen","WithController","WithImageTrigger"];export{t as Default,i as WithController,n as WithImageTrigger,e as WithoutFitScreen,wr as __namedExportsOrder,Or as default};
