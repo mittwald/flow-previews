2710012ced15b4ce73caa57feeef52f5be92ae3a
mittwald <>
2025-03-07 07:57:43 +0000

    Update from https://github.com/mittwald/flow/commit/9e010ecbed94773a77e8e9afb3670863e1d3d71a

---
+++ storybook/add-placeholder-to-combo-box/assets/Default.stories-3ECG_nJK.js
@@ -0,0 +1,15 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{I as i}from"./InlineCode-CNOGBbns.js";import"./index-BZISi7jw.js";import{T as k}from"./Text-D9M1FVhy.js";import{S as h}from"./Section-VaUdaP2P.js";import{a as f,s as x}from"./storyBackgrounds-GFSAi-dI.js";import"./flowComponent-BYduNA4_.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./clsx-B-dksMZM.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./browser-B1A6F7LW.js";import"./Text-PTaPc_Ci.js";import"./utils-CzVyYWZn.js";import"./ContextMenuSection-COnqG3mz.js";import"./Action-Dnj71fzz.js";import"./context-T6wCf2v9.js";import"./useStatic-79kqjvSG.js";import"./getActionGroupSlot-CjUGCB-i.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BV8uMRwe.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";const dr={title:"Content/InlineCode",component:i,render:r=>m.jsx(h,{children:m.jsxs(k,{color:r.color==="dark"||r.color==="light"?r.color:void 0,children:["Enter ",m.jsx(i,{...r,children:"yarn start"})," to start your app."]})})},o={},t={args:{color:"dark"},parameters:{backgrounds:f}},a={args:{color:"light"},parameters:{backgrounds:x}};var e,p,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(s=(p=o.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var l,g,u;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(u=(g=a.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};const lr=["Default","Dark","Light"];export{t as Dark,o as Default,a as Light,lr as __namedExportsOrder,dr as default};
