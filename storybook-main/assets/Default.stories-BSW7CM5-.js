6026e80b8cca66382a06b84d7a95bc1ed936afe8
mittwald <>
2025-03-10 12:49:52 +0000

    Update from https://github.com/mittwald/flow/commit/9eec3f973c8297cf5d7734346cf1a30bacb13e76

---
+++ storybook-main/assets/Default.stories-BSW7CM5-.js
@@ -0,0 +1,61 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{R as i,a,b as r}from"./RadioButton-Ccwfd_rq.js";import"./index-BZISi7jw.js";import{L as n}from"./Label-DZ85i_Qc.js";import{a as W}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import{F as k}from"./FieldError-tUrg5-HN.js";import{T as p}from"./Text-CEm0ouz8.js";import{C as R}from"./Content-gCQPdKfb.js";import"./clsx-B-dksMZM.js";import"./flowComponent-DH4ScC2R.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./ColumnLayout-DloEpPIH.js";import"./RadioGroup-dsBcmkhX.js";import"./utils-CzVyYWZn.js";import"./FieldError-DoW3KOQ8.js";import"./useFocusRing-DzGSygZM.js";import"./Text-PTaPc_Ci.js";import"./filterDOMProps-CeZl_uWj.js";import"./Form-D162gIjQ.js";import"./useLabel-Hju3pN3c.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./useFormReset-CilIRCFg.js";import"./useFocusable-CdO2YIL_.js";import"./usePress-DZCrAIzv.js";import"./FocusScope-BnLtY9jQ.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./VisuallyHidden-DJvehTyr.js";import"./useControlledState-4nfZ-XMX.js";import"./IconApp-CsW6VVPl.js";import"./IconWarning-DAj50GX8.js";import"./v4-CtRu48qb.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";const Ae={title:"Form Controls/RadioGroup",component:i,args:{onChange:W("onChange")},parameters:{controls:{exclude:["onChange"]}},render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(a,{value:"admin",children:"Admin"}),e.jsx(a,{value:"member",children:"Member"}),e.jsx(a,{value:"accountant",children:"Accountant"})]})},t={},d={args:{isDisabled:!0}},s={render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(a,{value:"admin",children:"Admin"}),e.jsx(a,{value:"member",isDisabled:!0,children:"Member"}),e.jsx(a,{value:"accountant",children:"Accountant"})]})},u={render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(r,{value:"admin",children:"Admin"}),e.jsx(r,{value:"member",children:"Member"}),e.jsx(r,{value:"accountant",children:"Accountant"})]})},m={render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(r,{value:"admin",children:"Admin"}),e.jsx(r,{value:"member",isDisabled:!0,children:"Member"}),e.jsx(r,{value:"accountant",children:"Accountant"})]})},l={render:o=>e.jsxs(i,{...o,defaultValue:"domain","aria-label":"Domain",children:[e.jsxs(r,{value:"domain",children:[e.jsx(p,{children:"Book domain"}),e.jsx(R,{children:"Do you have a desired domain? No problem, we'll help you find the right domain for you."})]}),e.jsxs(r,{value:"virtualHost",children:[e.jsx(p,{children:"Add virtual host"}),e.jsx(R,{children:"The domain remains with your previous provider, but you can use it for your website in our mStudio."})]}),e.jsxs(r,{value:"subdomain",children:[e.jsx(p,{children:"Add subdomain"}),e.jsx(R,{children:"Create a subdomain from an existing domain to use for your project."})]})]})},c={render:o=>e.jsxs(i,{...o,isInvalid:!0,isRequired:!0,children:[e.jsx(n,{children:"Role"}),e.jsx(a,{value:"admin",children:"Admin"}),e.jsx(a,{value:"member",children:"Member"}),e.jsx(a,{value:"accountant",children:"Accountant"}),e.jsx(k,{children:"Select a role to continue"})]})};var h,b,x;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:"{}",...(x=(b=t.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var v,j,f;d.parameters={...d.parameters,docs:{...(v=d.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(f=(j=d.parameters)==null?void 0:j.docs)==null?void 0:f.source}}};var B,A,D;s.parameters={...s.parameters,docs:{...(B=s.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="admin">
+      <Label>Role</Label>
+      <Radio value="admin">Admin</Radio>
+      <Radio value="member" isDisabled>
+        Member
+      </Radio>
+      <Radio value="accountant">Accountant</Radio>
+    </RadioGroup>
+}`,...(D=(A=s.parameters)==null?void 0:A.docs)==null?void 0:D.source}}};var g,C,y;u.parameters={...u.parameters,docs:{...(g=u.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="admin">
+      <Label>Role</Label>
+      <RadioButton value="admin">Admin</RadioButton>
+      <RadioButton value="member">Member</RadioButton>
+      <RadioButton value="accountant">Accountant</RadioButton>
+    </RadioGroup>
+}`,...(y=(C=u.parameters)==null?void 0:C.docs)==null?void 0:y.source}}};var G,S,L;m.parameters={...m.parameters,docs:{...(G=m.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="admin">
+      <Label>Role</Label>
+      <RadioButton value="admin">Admin</RadioButton>
+      <RadioButton value="member" isDisabled>
+        Member
+      </RadioButton>
+      <RadioButton value="accountant">Accountant</RadioButton>
+    </RadioGroup>
+}`,...(L=(S=m.parameters)==null?void 0:S.docs)==null?void 0:L.source}}};var T,M,V;l.parameters={...l.parameters,docs:{...(T=l.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="domain" aria-label="Domain">
+      <RadioButton value="domain">
+        <Text>Book domain</Text>
+        <Content>
+          Do you have a desired domain? No problem, we'll help you find the
+          right domain for you.
+        </Content>
+      </RadioButton>
+      <RadioButton value="virtualHost">
+        <Text>Add virtual host</Text>
+        <Content>
+          The domain remains with your previous provider, but you can use it for
+          your website in our mStudio.
+        </Content>
+      </RadioButton>
+      <RadioButton value="subdomain">
+        <Text>Add subdomain</Text>
+        <Content>
+          Create a subdomain from an existing domain to use for your project.
+        </Content>
+      </RadioButton>
+    </RadioGroup>
+}`,...(V=(M=l.parameters)==null?void 0:M.docs)==null?void 0:V.source}}};var E,F,w;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} isInvalid isRequired>
+      <Label>Role</Label>
+      <Radio value="admin">Admin</Radio>
+      <Radio value="member">Member</Radio>
+      <Radio value="accountant">Accountant</Radio>
+      <FieldError>Select a role to continue</FieldError>
+    </RadioGroup>
+}`,...(w=(F=c.parameters)==null?void 0:F.docs)==null?void 0:w.source}}};const De=["Default","RadioGroupDisabled","RadioDisabled","RadioButtons","RadioButtonDisabled","RadioButtonsWithContent","WithFieldError"];export{t as Default,m as RadioButtonDisabled,u as RadioButtons,l as RadioButtonsWithContent,s as RadioDisabled,d as RadioGroupDisabled,c as WithFieldError,De as __namedExportsOrder,Ae as default};
---
+++ storybook-main/assets/EdgeCases.stories-D7p-K3ck.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{T as l}from"./Text-CEm0ouz8.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import f from"./Default.stories-D1pz6v2d.js";import{I as u}from"./IllustratedMessage-BSblEsB3.js";import{H as c}from"./Heading-DvV74Kd2.js";import{B as x}from"./Button-Cx852dC8.js";import{I as g}from"./IconApp-CsW6VVPl.js";import"./IconWarning-DAj50GX8.js";import"./_commonjsHelpers-CqkleIqs.js";import"./flowComponent-DH4ScC2R.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./clsx-B-dksMZM.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./Text-PTaPc_Ci.js";import"./utils-CzVyYWZn.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ActionGroup-COkRDdJZ.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-DBhwSc7c.js";import"./useStatic-Dg6j0jAA.js";import"./Heading-BCoG4c8D.js";import"./RSPContexts-BEYwq1Zw.js";import"./LoadingSpinner-Dhg1Wy_7.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";const pr={...f,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>r.jsxs(u,{...o,children:[r.jsx(g,{}),r.jsx(c,{children:p.medium}),r.jsx(l,{children:p.long}),r.jsx(x,{children:"Create app"})]})},e={render:o=>r.jsxs(u,{...o,children:[r.jsx(g,{}),r.jsx(c,{children:"No apps installed"}),r.jsx(l,{children:"Create your first app to start working on your website."}),r.jsx(x,{children:"Create app"})]}),parameters:{viewport:{defaultViewport:"mobile1"}}};var s,m,a;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var i,n,d;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(d=(n=e.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};const sr=["LongTexts","SmallSpace"];export{t as LongTexts,e as SmallSpace,sr as __namedExportsOrder,pr as default};
