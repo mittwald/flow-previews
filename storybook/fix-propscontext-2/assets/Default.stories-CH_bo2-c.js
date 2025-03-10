38d9121be04d594ab8701605c4c0756e53d7b37d
mittwald <>
2025-03-10 10:12:53 +0000

    Update from https://github.com/mittwald/flow/commit/a7f3084123e29652a86ea6b2572d001f806868db

---
+++ storybook/fix-propscontext-2/assets/Default.stories-CH_bo2-c.js
@@ -0,0 +1,23 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{D as s}from"./DateRangePicker-ESZtSSFz.js";import{L as p}from"./Label-DOV1pvGR.js";import{F as j}from"./FieldDescription-DUNiJQSB.js";import{$,a as m,b as h}from"./DateField-D66nZa9M.js";import"./_commonjsHelpers-CqkleIqs.js";import"./FieldError-CT3TC-hN.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BmZ-J2KH.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./IconApp-C7Y3bW-A.js";import"./IconWarning-BnftdKnW.js";import"./FieldError-DoW3KOQ8.js";import"./useFocusRing-DzGSygZM.js";import"./utils-CzVyYWZn.js";import"./Text-PTaPc_Ci.js";import"./filterDOMProps-CeZl_uWj.js";import"./Popover-B-Jv56Df.js";import"./MenuTrigger-BU8DgDqg.js";import"./context-BhrHGJWf.js";import"./useStatic-D_81B-Xg.js";import"./Dialog-BV8uMRwe.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./useLabel-Hju3pN3c.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./usePress-DZCrAIzv.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./DialogTriggerView-DsKzdvn4.js";import"./RangeCalendar-CS0zfiMM.js";import"./Form-D162gIjQ.js";import"./Group-Df3SyVuJ.js";import"./Button-D5HoY52J.js";import"./Text-CgLlZl6i.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./LoadingSpinner-BQDrjPXc.js";import"./Heading-BCoG4c8D.js";import"./useUpdateEffect-Bw-e1fiu.js";import"./Input-DaA3cxmW.js";import"./useFormReset-CilIRCFg.js";import"./useSpinButton-DuLNe-aw.js";import"./useFilter-ByIzJMED.js";const yr={title:"Form Controls/DateRangePicker",component:s,render:e=>r.jsx(s,{...e,isRequired:!0,children:r.jsx(p,{children:"Date"})}),parameters:{controls:{exclude:["errorMessage"]}}},t={},a={args:{isDisabled:!0}},o={args:{minValue:$(h())},render:e=>r.jsx(s,{isRequired:!0,defaultValue:{start:m("2012-07-03"),end:m("2012-07-04")},...e,isInvalid:!0,errorMessage:"Date is in the past",children:r.jsx(p,{children:"Future Date"})})},i={args:{minValue:$(h())},render:e=>r.jsxs(s,{isRequired:!0,...e,children:[r.jsx(p,{children:"Future Date"}),r.jsx(j,{children:"Select a future date"})]})};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,l,D;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(D=(l=a.parameters)==null?void 0:l.docs)==null?void 0:D.source}}};var f,g,x;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DateRangePicker isRequired defaultValue={{
+    start: parseDate("2012-07-03"),
+    end: parseDate("2012-07-04")
+  }} {...props} isInvalid errorMessage="Date is in the past">
+      <Label>Future Date</Label>
+    </DateRangePicker>
+}`,...(x=(g=o.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var b,R,F;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DateRangePicker isRequired {...props}>
+      <Label>Future Date</Label>
+      <FieldDescription>Select a future date</FieldDescription>
+    </DateRangePicker>
+}`,...(F=(R=i.parameters)==null?void 0:R.docs)==null?void 0:F.source}}};const Ir=["Default","Disabled","Invalid","FutureDatesOnly"];export{t as Default,a as Disabled,i as FutureDatesOnly,o as Invalid,Ir as __namedExportsOrder,yr as default};
---
+++ storybook/fix-propscontext-2/assets/EdgeCases.stories-D59FxDBd.js
@@ -0,0 +1,63 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{M as u}from"./ModalTrigger-CC561USj.js";import"./index-BZISi7jw.js";import{B as m}from"./Button-D5HoY52J.js";import y from"./Default.stories-DBg0iBPa.js";import{C as h}from"./Content-BPqk4r7u.js";import{S as r}from"./Section-DXvKP_sH.js";import{T as n}from"./Text-CgLlZl6i.js";import{A as j}from"./ActionGroup-BUTEAwov.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{H as T}from"./Heading-D-tkdnj4.js";import{A as g}from"./Action-PclcM_g-.js";import{u as f}from"./MenuTrigger-BU8DgDqg.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BmZ-J2KH.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./Overlay-BV5CAXUv.js";import"./utils-CzVyYWZn.js";import"./Dialog-BV8uMRwe.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-CByfAkhe.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-DxADqCjP.js";import"./CollectionBuilder-D73R-Ru4.js";import"./Separator-DNY_h6GP.js";import"./Text-PTaPc_Ci.js";import"./SelectionManager-D_sMwGle.js";import"./useEvent-CPQp9FCP.js";import"./useCollator-C0JtIjTO.js";import"./FocusScope-BnLtY9jQ.js";import"./VisuallyHidden-DJvehTyr.js";import"./IconApp-C7Y3bW-A.js";import"./IconWarning-BnftdKnW.js";import"./Header-CmN0kA0S.js";import"./ButtonView-Dmj6LMp9.js";import"./DialogTriggerView-DsKzdvn4.js";import"./LoadingSpinner-BQDrjPXc.js";import"./TextField-DiuYG3tx.js";import"./TextFieldBase-BfGxgykR.js";import"./FieldError-CT3TC-hN.js";import"./FieldError-DoW3KOQ8.js";import"./FieldDescription-DUNiJQSB.js";import"./Form-D162gIjQ.js";import"./Input-DaA3cxmW.js";import"./useTextField-uM6U_XJE.js";import"./useFormReset-CilIRCFg.js";import"./Label-DOV1pvGR.js";import"./lib-B-OeWZmh.js";import"./chunk-D5ZWXAHU-Dm3eDOzv.js";import"./v4-CtRu48qb.js";import"./Form-DGX4LpXj.js";import"./dynamic-DKDa4OpU.js";import"./browser-B1A6F7LW.js";import"./Align-Ct7OICEm.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Heading-BCoG4c8D.js";import"./context-BhrHGJWf.js";import"./useStatic-D_81B-Xg.js";import"./getActionGroupSlot-BvlOMvkG.js";import"./ContextMenuSection-CG6xIWhE.js";const Jo={...y,title:"Overlays/Modal/Edge Cases"},e={render:s=>o.jsxs(u,{...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})},i={render:s=>o.jsxs(u,{offCanvas:!0,...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})};var c,l,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(p=(l=e.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var a,d,x;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Modal offCanvas {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(x=(d=i.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const Ko=["LongContent","LongContentOffCanvas"];export{e as LongContent,i as LongContentOffCanvas,Ko as __namedExportsOrder,Jo as default};
