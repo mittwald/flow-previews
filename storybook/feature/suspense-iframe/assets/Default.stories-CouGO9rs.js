f47d9d3540280f53d4997588f78757586a1eacc7
mittwald <>
2025-03-11 10:09:11 +0000

    Update from https://github.com/mittwald/flow/commit/73f9ae8aeea6c044db692aed718d31f9ab57842e

---
+++ storybook/feature/suspense-iframe/assets/Default.stories-CouGO9rs.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{A as n}from"./Accordion-CrmrccaI.js";import{H as L}from"./Heading-DvV74Kd2.js";import{C as A}from"./Content-gCQPdKfb.js";import{L as b}from"./Label-DZ85i_Qc.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-DH4ScC2R.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./dynamic-DKDa4OpU.js";import"./Button-Cx852dC8.js";import"./IconApp-CsW6VVPl.js";import"./IconWarning-DAj50GX8.js";import"./Wrap-DQq6jo70.js";import"./Text-CEm0ouz8.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Text-PTaPc_Ci.js";import"./utils-CzVyYWZn.js";import"./LoadingSpinner-Dhg1Wy_7.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFocusable-CdO2YIL_.js";import"./useFocusRing-DzGSygZM.js";import"./usePress-DZCrAIzv.js";import"./Activity-DrXcoKbv.js";import"./Heading-BCoG4c8D.js";import"./RSPContexts-BEYwq1Zw.js";const nr={title:"Structure/Accordion",component:n,render:i=>r.jsxs(n,{...i,children:[r.jsx(L,{children:"Heading"}),r.jsx(A,{children:"Accordion Content"})]}),parameters:{controls:{exclude:["defaultExpanded"]}}},o={},t={args:{defaultExpanded:!0}},e={args:{variant:"outline"}},a={render:i=>r.jsxs(n,{...i,children:[r.jsx(b,{children:"Label"}),r.jsx(A,{children:"Accordion Content"})]})};var p,s,m;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(m=(s=o.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var c,d,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    defaultExpanded: true
+  }
+}`,...(l=(d=t.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var u,x,f;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: "outline"
+  }
+}`,...(f=(x=e.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};var g,h,j;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Accordion {...props}>
+      <Label>Label</Label>
+      <Content>Accordion Content</Content>
+    </Accordion>
+}`,...(j=(h=a.parameters)==null?void 0:h.docs)==null?void 0:j.source}}};const pr=["Default","DefaultExpanded","OutlineVariant","WithLabel"];export{o as Default,t as DefaultExpanded,e as OutlineVariant,a as WithLabel,pr as __namedExportsOrder,nr as default};
