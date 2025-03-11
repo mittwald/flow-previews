b368bb9273809ada8e96f8761a4b0ed772ec72b2
mittwald <>
2025-03-11 09:15:49 +0000

    Update from https://github.com/mittwald/flow/commit/02c490f898d71235c57a8210d63d731fa2adf5a7

---
+++ storybook-main/assets/Default.stories-C7Y66Jgm.js
@@ -0,0 +1,55 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{A as e}from"./ActionGroup-FZzJR_vi.js";import"./index-BZISi7jw.js";import{B as o}from"./Button-CnylmBwY.js";import{A as l,s as _}from"./Action-Y9vUQ_E8.js";import{S as C}from"./Switch-C-KlLhsA.js";import{L as R}from"./Link-iCB64Jmn.js";import"./flowComponent-BPJHi05H.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-DmntCNXg.js";import"./clsx-B-dksMZM.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-CX7tjRC3.js";import"./useStatic-Boalhu8h.js";import"./IconApp-W8-S6AAB.js";import"./IconWarning-BUA863Ku.js";import"./Wrap-DQq6jo70.js";import"./Text-CiS2CEB-.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Text-CUmgXOYU.js";import"./utils-B8p6n-_S.js";import"./LoadingSpinner-Cpk2RUSc.js";import"./useLocalizedStringFormatter-z2l0_8m0.js";import"./Button-DlOLKXkK.js";import"./ProgressBar-CMGFgh_N.js";import"./Label-CRtDyyLG.js";import"./Hidden-kqRI_mPI.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DjubqfyV.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-D_abas1X.js";import"./useFocus-0lzzAyJ_.js";import"./useFocusRing-C08ZnYSv.js";import"./useFocusable-dJayk3b7.js";import"./context-Cn4G8t_B.js";import"./Label-CzYjGm4V.js";import"./useToggleState-DbTFgthm.js";import"./useFormReset-m57alayF.js";import"./useControlledState-4nfZ-XMX.js";import"./VisuallyHidden-DCnFJsId.js";const Dr={title:"Actions/ActionGroup",component:e,render:t=>r.jsxs(e,{...t,children:[r.jsx(l,{action:()=>_(1500),children:r.jsx(o,{color:"accent",children:"Create customer"})}),r.jsx(l,{children:r.jsx(o,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})})]})},n={},s={render:t=>r.jsxs(e,{...t,children:[r.jsx(o,{color:"danger",children:"Delete project"}),r.jsx(o,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})]})},a={render:t=>r.jsx(e,{...t,children:r.jsx(o,{children:"Ok"})})},c={render:t=>r.jsxs(e,{...t,children:[r.jsx(o,{slot:"primary",color:"accent",children:"Add email address"}),r.jsx(o,{slot:"secondary",variant:"soft",color:"secondary",children:"Save and add more"}),r.jsx(o,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})]})},i={render:t=>r.jsxs(e,{...t,children:[r.jsx(o,{slot:"primary",color:"accent",children:"Add email address"}),r.jsx(o,{slot:"secondary",variant:"soft",color:"secondary",children:"Save and add more"}),r.jsx(o,{slot:"secondary",variant:"soft",color:"secondary",children:"Save as new"}),r.jsx(o,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})]})},p={parameters:{viewport:{defaultViewport:"mobile1"}}},d={render:t=>r.jsxs(e,{...t,children:[r.jsx(o,{slot:"secondary",children:"Edit"}),r.jsx(C,{slot:"primary",children:"Activate"})]})},m={render:t=>r.jsxs(e,{...t,children:[r.jsx(R,{slot:"abort",children:"Forgot password?"}),r.jsx(o,{color:"accent",children:"Login"})]})};var u,h,A;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(A=(h=n.parameters)==null?void 0:h.docs)==null?void 0:A.source}}};var x,y,j;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button color="danger">Delete project</Button>
+      <Button slot="abort" variant="soft" color="secondary">
+        Abort
+      </Button>
+    </ActionGroup>
+}`,...(j=(y=s.parameters)==null?void 0:y.docs)==null?void 0:j.source}}};var f,B,S;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button>Ok</Button>
+    </ActionGroup>
+}`,...(S=(B=a.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};var v,b,g;c.parameters={...c.parameters,docs:{...(v=c.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button slot="primary" color="accent">
+        Add email address
+      </Button>
+      <Button slot="secondary" variant="soft" color="secondary">
+        Save and add more
+      </Button>
+      <Button slot="abort" variant="soft" color="secondary">
+        Abort
+      </Button>
+    </ActionGroup>
+}`,...(g=(b=c.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var G,w,L;i.parameters={...i.parameters,docs:{...(G=i.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button slot="primary" color="accent">
+        Add email address
+      </Button>
+      <Button slot="secondary" variant="soft" color="secondary">
+        Save and add more
+      </Button>
+      <Button slot="secondary" variant="soft" color="secondary">
+        Save as new
+      </Button>
+      <Button slot="abort" variant="soft" color="secondary">
+        Abort
+      </Button>
+    </ActionGroup>
+}`,...(L=(w=i.parameters)==null?void 0:w.docs)==null?void 0:L.source}}};var W,k,D;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(D=(k=p.parameters)==null?void 0:k.docs)==null?void 0:D.source}}};var E,O,F;d.parameters={...d.parameters,docs:{...(E=d.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button slot="secondary">Edit</Button>
+      <Switch slot="primary">Activate</Switch>
+    </ActionGroup>
+}`,...(F=(O=d.parameters)==null?void 0:O.docs)==null?void 0:F.source}}};var I,M,V;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Link slot="abort">Forgot password?</Link>
+      <Button color="accent">Login</Button>
+    </ActionGroup>
+}`,...(V=(M=m.parameters)==null?void 0:M.docs)==null?void 0:V.source}}};const Er=["Default","Danger","Info","WithSecondaryAction","WithExtraSecondaryAction","Mobile","WithSwitch","WithLink"];export{s as Danger,n as Default,a as Info,p as Mobile,i as WithExtraSecondaryAction,m as WithLink,c as WithSecondaryAction,d as WithSwitch,Er as __namedExportsOrder,Dr as default};
