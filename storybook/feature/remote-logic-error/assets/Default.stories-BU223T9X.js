816e098b436ab47e2f94752eedd279b702cf8f15
mittwald <>
2025-03-14 14:36:30 +0000

    Update from https://github.com/mittwald/flow/commit/a6bcdbc31db6b4df939e2c547937cd42e67d85f3

---
+++ storybook/feature/remote-logic-error/assets/Default.stories-BU223T9X.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{D as s}from"./DatePicker-BBcNF0UK.js";import{L as p}from"./Label-CzYjGm4V.js";import{F as j}from"./FieldDescription-BV4XyTGq.js";import{$,a as L,b as h}from"./DateField-B_3Ei6A7.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BPJHi05H.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-DmntCNXg.js";import"./Button-CF4ZysTX.js";import"./IconApp-W8-S6AAB.js";import"./IconWarning-BUA863Ku.js";import"./Wrap-DQq6jo70.js";import"./Text-DKc2hFom.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Text-CUmgXOYU.js";import"./utils-B8p6n-_S.js";import"./LoadingSpinner-Cpk2RUSc.js";import"./useLocalizedStringFormatter-z2l0_8m0.js";import"./Button-DlOLKXkK.js";import"./ProgressBar-CMGFgh_N.js";import"./Label-CRtDyyLG.js";import"./Hidden-kqRI_mPI.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DjubqfyV.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-D_abas1X.js";import"./useFocus-0lzzAyJ_.js";import"./useFocusRing-C08ZnYSv.js";import"./useFocusable-dJayk3b7.js";import"./Group-DxLqPfCe.js";import"./FieldError-DbNWEBso.js";import"./FieldError-CFxmzwuR.js";import"./Popover-Bo6e0GAV.js";import"./MenuTrigger-4stFWxuJ.js";import"./context-Cn4G8t_B.js";import"./useStatic-Boalhu8h.js";import"./Dialog-TWm-lwKH.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-BxXqfwgF.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-B-y1oEx2.js";import"./CollectionBuilder-cVXcHFND.js";import"./Separator-D0eYhWhZ.js";import"./SelectionManager-BB9PAfr3.js";import"./useEvent-BxpP-RFo.js";import"./useCollator-CB8NJtcR.js";import"./FocusScope-2Ar0PVcM.js";import"./VisuallyHidden-DCnFJsId.js";import"./DialogTriggerView-BetAvd-7.js";import"./RangeCalendar-Dwe_oNaw.js";import"./Form-CQcw_pbg.js";import"./Heading-Cc6WIW0_.js";import"./useUpdateEffect-DmEbVEsi.js";import"./Input-DtximQ_K.js";import"./useFormReset-m57alayF.js";import"./useSpinButton-D_VWpY4-.js";import"./useFilter-CnzKAAR2.js";const Mr={title:"Form Controls/DatePicker",component:s,render:e=>r.jsx(s,{...e,isRequired:!0,children:r.jsx(p,{children:"Date"})}),parameters:{controls:{exclude:["errorMessage"]}}},t={},o={args:{isDisabled:!0}},i={args:{minValue:$(h())},render:e=>r.jsx(s,{isRequired:!0,defaultValue:L("2012-07-03"),...e,isInvalid:!0,errorMessage:"Date is in the past",children:r.jsx(p,{children:"Future Date"})})},a={args:{minValue:$(h())},render:e=>r.jsxs(s,{isRequired:!0,...e,children:[r.jsx(p,{children:"Future Date"}),r.jsx(j,{children:"Select a future date"})]})};var m,n,c;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var d,u,l;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(l=(u=o.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var D,f,g;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DatePicker isRequired defaultValue={parseDate("2012-07-03")} {...props} isInvalid errorMessage="Date is in the past">
+      <Label>Future Date</Label>
+    </DatePicker>
+}`,...(g=(f=i.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var x,b,F;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DatePicker isRequired {...props}>
+      <Label>Future Date</Label>
+      <FieldDescription>Select a future date</FieldDescription>
+    </DatePicker>
+}`,...(F=(b=a.parameters)==null?void 0:b.docs)==null?void 0:F.source}}};const Or=["Default","Disabled","Invalid","FutureDatesOnly"];export{t as Default,o as Disabled,a as FutureDatesOnly,i as Invalid,Or as __namedExportsOrder,Mr as default};
---
+++ storybook/feature/remote-logic-error/assets/Variants.stories-DX31G8r1.js
@@ -0,0 +1,19 @@
+import f from"./Default.stories-Bhl-VtKB.js";import"./jsx-runtime-D_zvdyIk.js";import"./ProgressBar-MiBekyO3.js";import"./index-BZISi7jw.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BPJHi05H.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-DmntCNXg.js";import"./useLocalizedStringFormatter-z2l0_8m0.js";import"./ProgressBar-CMGFgh_N.js";import"./utils-B8p6n-_S.js";import"./Label-CRtDyyLG.js";import"./Hidden-kqRI_mPI.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DjubqfyV.js";import"./NumberFormatter-DNR9MAW-.js";import"./Label-CzYjGm4V.js";const z={...f,title:"Status/ProgressBar/Variants"},r={args:{status:"info"}},s={args:{status:"success"}},a={args:{status:"warning",value:800}},t={args:{status:"danger",value:950}};var e,o,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    status: "info"
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var m,c,p;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  }
+}`,...(p=(c=s.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var i,u,g;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    status: "warning",
+    value: 800
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,l,S;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "danger",
+    value: 950
+  }
+}`,...(S=(l=t.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};const A=["Info","Success","Warning","Danger"];export{t as Danger,r as Info,s as Success,a as Warning,A as __namedExportsOrder,z as default};
