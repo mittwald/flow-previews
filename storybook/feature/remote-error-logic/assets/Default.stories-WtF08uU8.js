49dc4f880c317651f9f897db43498964ac661749
mittwald <>
2025-03-14 14:03:06 +0000

    Update from https://github.com/mittwald/flow/commit/00f5b53ee1e57efb078f573539a06bf590723cfd

---
+++ storybook/feature/remote-error-logic/assets/Default.stories-WtF08uU8.js
@@ -0,0 +1,32 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{L as d}from"./Label-DZ85i_Qc.js";import{a as I}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import{F as _}from"./FieldDescription-BC0snYdZ.js";import{F as O}from"./FieldError-tUrg5-HN.js";import{T as i}from"./TimeField-Bw-qOU3y.js";import{c as l}from"./DateField-D66nZa9M.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-DH4ScC2R.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./Label-RYyxMwZH.js";import"./utils-CzVyYWZn.js";import"./Hidden-DBxI3g91.js";import"./v4-CtRu48qb.js";import"./Text-CEm0ouz8.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./Text-PTaPc_Ci.js";import"./IconApp-CsW6VVPl.js";import"./IconWarning-DAj50GX8.js";import"./FieldError-DoW3KOQ8.js";import"./useFocusRing-DzGSygZM.js";import"./filterDOMProps-CeZl_uWj.js";import"./Form-D162gIjQ.js";import"./useLabel-Hju3pN3c.js";import"./Group-Df3SyVuJ.js";import"./Input-DaA3cxmW.js";import"./usePress-DZCrAIzv.js";import"./FocusScope-BnLtY9jQ.js";import"./useCollator-C0JtIjTO.js";import"./useFormReset-CilIRCFg.js";import"./useEvent-CPQp9FCP.js";import"./useSpinButton-DuLNe-aw.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useFilter-ByIzJMED.js";import"./useControlledState-4nfZ-XMX.js";const wr={title:"Form Controls/TimeField",component:i,render:e=>r.jsx(i,{onChange:I("onChange"),...e,children:r.jsx(d,{children:"Time"})})},o={},a={args:{isDisabled:!0}},s={args:{isRequired:!0}},t={render:e=>r.jsxs(i,{...e,children:[r.jsx(d,{children:"Time"}),r.jsx(_,{children:"Enter a time"})]})},m={render:e=>r.jsx(i,{...e,defaultValue:new l(11,45),children:r.jsx(d,{children:"Time"})})},n={render:e=>r.jsxs(i,{...e,isInvalid:!0,isRequired:!0,children:[r.jsx(d,{children:"Time"}),r.jsx(O,{children:"Time is required"})]})},p={args:{granularity:"hour"}},c={args:{minValue:new l(8,0),maxValue:new l(16,0)}};var u,F,T;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(T=(F=o.parameters)==null?void 0:F.docs)==null?void 0:T.source}}};var g,h,x;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(x=(h=a.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var f,b,D;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(D=(b=s.parameters)==null?void 0:b.docs)==null?void 0:D.source}}};var j,V,E;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: props => <TimeField {...props}>
+      <Label>Time</Label>
+      <FieldDescription>Enter a time</FieldDescription>
+    </TimeField>
+}`,...(E=(V=t.parameters)==null?void 0:V.docs)==null?void 0:E.source}}};var q,L,S;m.parameters={...m.parameters,docs:{...(q=m.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: props => <TimeField {...props} defaultValue={new Time(11, 45)}>
+      <Label>Time</Label>
+    </TimeField>
+}`,...(S=(L=m.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var R,w,W;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: props => <TimeField {...props} isInvalid isRequired>
+      <Label>Time</Label>
+      <FieldError>Time is required</FieldError>
+    </TimeField>
+}`,...(W=(w=n.parameters)==null?void 0:w.docs)==null?void 0:W.source}}};var y,M,C;p.parameters={...p.parameters,docs:{...(y=p.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    granularity: "hour"
+  }
+}`,...(C=(M=p.parameters)==null?void 0:M.docs)==null?void 0:C.source}}};var $,v,G;c.parameters={...c.parameters,docs:{...($=c.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    minValue: new Time(8, 0),
+    maxValue: new Time(16, 0)
+  }
+}`,...(G=(v=c.parameters)==null?void 0:v.docs)==null?void 0:G.source}}};const Wr=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithFieldError","Granularity","MinMaxValue"];export{o as Default,a as Disabled,p as Granularity,c as MinMaxValue,s as Required,m as WithDefaultValue,t as WithFieldDescription,n as WithFieldError,Wr as __namedExportsOrder,wr as default};
---
+++ storybook/feature/remote-error-logic/assets/EdgeCases.stories-B8w1-fee.js
@@ -0,0 +1,18 @@
+import c from"./Default.stories-B9qdec42.js";import{F as d}from"./FileCard-BhJ3479X.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import{a as s}from"./chunk-D5ZWXAHU-Dm3eDOzv.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import"./_commonjsHelpers-CqkleIqs.js";import"./IconApp-CsW6VVPl.js";import"./clsx-B-dksMZM.js";import"./flowComponent-DH4ScC2R.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-IBhSrjl6.js";import"./IconWarning-DAj50GX8.js";import"./Avatar-BA_eSWpI.js";import"./react-children-utilities-fm2cgFnI.js";import"./Image-BF-H7TZN.js";import"./Text-CEm0ouz8.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./Text-PTaPc_Ci.js";import"./utils-CzVyYWZn.js";import"./Link-BTnLQo5W.js";import"./useLocalizedStringFormatter-D6AjEcek.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-DZCrAIzv.js";import"./useFocusRing-DzGSygZM.js";import"./useFocusable-CdO2YIL_.js";import"./Button-Cx852dC8.js";import"./LoadingSpinner-Dhg1Wy_7.js";import"./Button-BBWo3kGM.js";import"./ProgressBar-B3uNJn7L.js";import"./Label-RYyxMwZH.js";import"./Hidden-DBxI3g91.js";import"./useLabel-Hju3pN3c.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./v4-CtRu48qb.js";const X={...c,title:"Upload/FileCard/Edge Cases",component:d},o={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")}}},e={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")},href:"#"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};var n,i,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    },
+    href: "#"
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const Y=["LongName","LongNameAndLink"];export{o as LongName,e as LongNameAndLink,Y as __namedExportsOrder,X as default};
