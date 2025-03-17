2322b2fdc7e6ea475626d31014e0884c58358ba8
mittwald <>
2025-03-17 14:14:17 +0000

    Update from https://github.com/mittwald/flow/commit/72617ed9c18fa99ef7f34e68dcb957af60fd907f

---
+++ storybook/fix-default-exports/assets/Default.stories-CLQBmxGQ.js
@@ -0,0 +1,18 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{P as i,a as R}from"./Popover-Bo6e0GAV.js";import{R as x}from"./index-BZISi7jw.js";import{B as h}from"./Button-CnylmBwY.js";import{u as P}from"./MenuTrigger-4stFWxuJ.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BPJHi05H.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-DmntCNXg.js";import"./DialogTriggerView-BetAvd-7.js";import"./Dialog-TWm-lwKH.js";import"./Button-DlOLKXkK.js";import"./utils-B8p6n-_S.js";import"./ProgressBar-CMGFgh_N.js";import"./Label-CRtDyyLG.js";import"./Hidden-kqRI_mPI.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DjubqfyV.js";import"./useLocalizedStringFormatter-z2l0_8m0.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-D_abas1X.js";import"./useFocus-0lzzAyJ_.js";import"./useFocusRing-C08ZnYSv.js";import"./useFocusable-dJayk3b7.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-BxXqfwgF.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-B-y1oEx2.js";import"./CollectionBuilder-cVXcHFND.js";import"./Separator-D0eYhWhZ.js";import"./Text-CUmgXOYU.js";import"./SelectionManager-BB9PAfr3.js";import"./useEvent-BxpP-RFo.js";import"./useCollator-CB8NJtcR.js";import"./FocusScope-2Ar0PVcM.js";import"./VisuallyHidden-DCnFJsId.js";import"./IconApp-W8-S6AAB.js";import"./IconWarning-BUA863Ku.js";import"./Wrap-DQq6jo70.js";import"./Text-CiS2CEB-.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./LoadingSpinner-Cpk2RUSc.js";import"./context-Cn4G8t_B.js";import"./useStatic-Boalhu8h.js";const fr={title:"Overlays/Popover",component:i,render:p=>r.jsxs(R,{children:[r.jsx(h,{children:"Trigger popover"}),r.jsx(i,{...p,placement:"bottom right",children:"I am a popover."})]})},o={},t={args:{width:800}},e={render:p=>{const m=P("Popover"),s=x.useRef(null);return r.jsxs(r.Fragment,{children:[r.jsx(h,{ref:s,onPress:m.open,children:"Trigger popover"}),r.jsx(i,{triggerRef:s,...p,controller:m,children:"I am a popover."})]})}};var n,a,c;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(a=o.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};var l,u,d;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    width: 800
+  }
+}`,...(d=(u=t.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,f,v;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController("Popover");
+    const triggerRef = React.useRef(null);
+    return <>
+        <Button ref={triggerRef} onPress={controller.open}>
+          Trigger popover
+        </Button>
+        <Popover triggerRef={triggerRef} {...props} controller={controller}>
+          I am a popover.
+        </Popover>
+      </>;
+  }
+}`,...(v=(f=e.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};const vr=["Default","CustomWidth","WithController"];export{t as CustomWidth,o as Default,e as WithController,vr as __namedExportsOrder,fr as default};
---
+++ storybook/fix-default-exports/assets/Default.stories-WQdd4yjg.js
@@ -0,0 +1,15 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{I as i}from"./InlineCode-Dw_V_bw8.js";import"./index-BZISi7jw.js";import{T as k}from"./Text-CiS2CEB-.js";import{S as h}from"./Section-CkgEY5ZA.js";import{a as f,s as x}from"./storyBackgrounds-GFSAi-dI.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BPJHi05H.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-DmntCNXg.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./Text-CUmgXOYU.js";import"./utils-B8p6n-_S.js";import"./ContextMenuSection-C2hykenk.js";import"./Action-Y9vUQ_E8.js";import"./context-Cn4G8t_B.js";import"./useStatic-Boalhu8h.js";import"./getActionGroupSlot-CX7tjRC3.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-TWm-lwKH.js";import"./Button-DlOLKXkK.js";import"./ProgressBar-CMGFgh_N.js";import"./Label-CRtDyyLG.js";import"./Hidden-kqRI_mPI.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DjubqfyV.js";import"./useLocalizedStringFormatter-z2l0_8m0.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-D_abas1X.js";import"./useFocus-0lzzAyJ_.js";import"./useFocusRing-C08ZnYSv.js";import"./useFocusable-dJayk3b7.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-BxXqfwgF.js";import"./useControlledState-4nfZ-XMX.js";import"./Collection-B-y1oEx2.js";import"./CollectionBuilder-cVXcHFND.js";import"./Separator-D0eYhWhZ.js";import"./SelectionManager-BB9PAfr3.js";import"./useEvent-BxpP-RFo.js";import"./useCollator-CB8NJtcR.js";import"./FocusScope-2Ar0PVcM.js";import"./VisuallyHidden-DCnFJsId.js";const gr={title:"Content/InlineCode",component:i,render:r=>a.jsx(h,{children:a.jsxs(k,{color:r.color==="dark"||r.color==="light"?r.color:void 0,children:["Enter ",a.jsx(i,{...r,children:"yarn start"})," to start your app."]})})},o={},t={args:{color:"dark"},parameters:{backgrounds:f}},m={args:{color:"light"},parameters:{backgrounds:x}};var e,p,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(s=(p=o.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var l,g,u;m.parameters={...m.parameters,docs:{...(l=m.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(u=(g=m.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};const ur=["Default","Dark","Light"];export{t as Dark,o as Default,m as Light,ur as __namedExportsOrder,gr as default};
