6cdaf54a7d5b808bc59e8e036243fe77797e4ed2
mittwald <>
2025-03-18 08:16:51 +0000

    Cleanup merged Storybook previews

--- storybook/1237-introduction-of-the-accentbox/assets/Heading-DmITIAYD.js
+++
@@ -1,1 +0,0 @@
-import{$ as c}from"./RSPContexts-BEYwq1Zw.js";import{$ as f}from"./utils-BAtcVSHo.js";import{r as m,R as s}from"./index-BZISi7jw.js";const x=m.forwardRef(function(e,a){[e,a]=f(e,a,c);let{children:t,level:l=3,className:r,...o}=e,$=`h${l}`;return s.createElement($,{...o,ref:a,className:r??"react-aria-Heading"},t)});export{x as $};
