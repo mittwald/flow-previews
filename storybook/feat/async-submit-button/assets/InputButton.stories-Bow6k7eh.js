a84ab353d1b63d68cccfa759833700150b1350b4
mittwald <>
2025-03-20 13:45:02 +0000

    Update from https://github.com/mittwald/flow/commit/acf685530d3633a1c678fe3f76b0b3e53086975b

---
+++ storybook/feat/async-submit-button/assets/InputButton.stories-Bow6k7eh.js
@@ -0,0 +1,45 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BZISi7jw.js";import{A as o}from"./Align-BrW_juzT.js";import{T as c}from"./TextField-D3otY94u.js";import{L as i}from"./Label-Awo8DmgK.js";import{B as t}from"./Button-wBfRci_n.js";import y from"./Default.stories-DoDdme5g.js";import{N as w}from"./NumberField-8jGO_FcM.js";import{T as D}from"./TextArea-BVigfj6l.js";import{S as E}from"./CountryOptions-CAAvflbF.js";import{O as d}from"./Option-5AeAD13Y.js";import{u as _,F as I}from"./Form-BDW7wiMZ.js";import"./Field-BwJtRSZ6.js";import{s as R}from"./Action-DdkAMDFz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BQKp_5Ng.js";import"./index-BBXbSWyk.js";import"./index-C0qn59OO.js";import"./index-Dfd2pZHs.js";import"./TextFieldBase-CMM9jdlj.js";import"./FieldError-CbHf54AP.js";import"./IconApp-DZf0STiH.js";import"./IconWarning-BqX4qer7.js";import"./FieldError-DhbXwyXA.js";import"./useFocus-B7LXpZ-i.js";import"./utils-BAtcVSHo.js";import"./Text-CxIdO6ok.js";import"./filterDOMProps-CeZl_uWj.js";import"./FieldDescription-DtEGO0XD.js";import"./Text-BPAvSiAh.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Wrap-DQq6jo70.js";import"./useLocalizedStringFormatter-CZn_2Om1.js";import"./Form-qm5cnMB7.js";import"./useLabel-Bh1EGhcd.js";import"./Input-CxXkjS8B.js";import"./Hidden-kqRI_mPI.js";import"./useFocusRing-CYqHz5Aw.js";import"./Label--FbMCZJS.js";import"./useTextField-Cu3zxanp.js";import"./useFormReset-4n_yD9ws.js";import"./useControlledState-4nfZ-XMX.js";import"./useFocusable-XxrpiuTs.js";import"./LoadingSpinner-_BP_ZOWb.js";import"./Button-DAivm4ks.js";import"./ProgressBar-B6tGqqoL.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-ChNHSMR7.js";import"./Avatar-DABVibzv.js";import"./react-children-utilities-fm2cgFnI.js";import"./Initials-wVNBvhu1.js";import"./onlyText-DBswRDk6.js";import"./Group-DK5WmvqU.js";import"./useSpinButton-BEMuQPPk.js";import"./useEvent-0eO3yNkM.js";import"./OverlayTrigger-BUDTS6yc.js";import"./context-BWt75kQX.js";import"./useStatic-CZ_o1reA.js";import"./Dialog-BXcqMr2E.js";import"./RSPContexts-BEYwq1Zw.js";import"./OverlayArrow-PYb9PIt4.js";import"./Collection-BNxR9rC7.js";import"./CollectionBuilder-BlZkV0U0.js";import"./Separator-xQlHCyu8.js";import"./SelectionManager-B6hW899S.js";import"./useCollator-CDl7pGf-.js";import"./FocusScope-BXsjqcwe.js";import"./VisuallyHidden-CfzjMNmM.js";import"./intlStrings-ZMTSFH_M.js";import"./ListBox-BbAUOZlF.js";import"./DragAndDrop-CvXJIw9w.js";import"./useListState-DMUPFU3B.js";import"./useSingleSelectListState-BxbGsrK1.js";import"./Popover-CcIgeXBJ.js";import"./DialogTriggerView-D4zJWcQb.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-CeuQjEkT.js";const ce={...y,title:"Structure/Align/Input + Button",component:o,render:e=>r.jsxs(o,{...e,children:[r.jsx(c,{children:r.jsx(i,{children:"Mail address"})}),r.jsx(t,{children:"Hinzufügen"})]})},n={},s={render:e=>r.jsxs(o,{...e,children:[r.jsx(w,{children:r.jsx(i,{children:"Number"})}),r.jsx(t,{children:"Hinzufügen"})]})},m={render:e=>r.jsxs(o,{...e,children:[r.jsx(D,{children:r.jsx(i,{children:"Message"})}),r.jsx(t,{children:"Hinzufügen"})]})},p={render:e=>r.jsxs(o,{...e,children:[r.jsxs(E,{children:[r.jsx(i,{children:"Options"}),r.jsx(d,{children:"Option 1"}),r.jsx(d,{children:"Option 2"})]}),r.jsx(t,{children:"Hinzufügen"})]})},a={render:e=>r.jsxs(o,{...e,children:[r.jsx(c,{}),r.jsx(t,{children:"Hinzufügen"})]})},l={render:e=>{const M=_();return r.jsx(I,{form:M,onSubmit:async()=>{console.log("submitting"),await R(2e3),console.log("submitted")},children:r.jsxs(o,{children:[r.jsx(c,{children:r.jsx(i,{children:"Mail address"})}),r.jsx(t,{type:"submit",children:"Hinzufügen"})]})})}};var u,x,g;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(g=(x=n.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};var h,f,j;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <NumberField>
+        <Label>Number</Label>
+      </NumberField>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(j=(f=s.parameters)==null?void 0:f.docs)==null?void 0:j.source}}};var b,A,F;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextArea>
+        <Label>Message</Label>
+      </TextArea>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(F=(A=m.parameters)==null?void 0:A.docs)==null?void 0:F.source}}};var S,B,O;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <Select>
+        <Label>Options</Label>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+      </Select>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(O=(B=p.parameters)==null?void 0:B.docs)==null?void 0:O.source}}};var L,z,H;a.parameters={...a.parameters,docs:{...(L=a.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextField />
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(H=(z=a.parameters)==null?void 0:z.docs)==null?void 0:H.source}}};var T,W,N;l.parameters={...l.parameters,docs:{...(T=l.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Form form={form} onSubmit={async () => {
+      console.log("submitting");
+      await sleep(2000);
+      console.log("submitted");
+    }}>
+        <Align>
+          <TextField>
+            <Label>Mail address</Label>
+          </TextField>
+          <Button type="submit">Hinzufügen</Button>
+        </Align>
+      </Form>;
+  }
+}`,...(N=(W=l.parameters)==null?void 0:W.docs)==null?void 0:N.source}}};const de=["Default","WithNumberField","WithTextArea","WithSelect","WithoutLabel","WithForm"];export{n as Default,l as WithForm,s as WithNumberField,p as WithSelect,m as WithTextArea,a as WithoutLabel,de as __namedExportsOrder,ce as default};
