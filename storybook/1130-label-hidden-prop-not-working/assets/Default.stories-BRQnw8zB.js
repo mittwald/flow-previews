31d0a69dde9ea2b849378c507b45d7976f6ebc54
mittwald <>
2025-03-21 10:24:21 +0000

    Update from https://github.com/mittwald/flow/commit/b422c13d2bbbafca1cfae36e1e2b6789e73abd6e

---
+++ storybook/1130-label-hidden-prop-not-working/assets/Default.stories-BRQnw8zB.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{L as t}from"./LabeledValue-Cd56Iz2z.js";import"./index-BZISi7jw.js";import{L as o}from"./Label-Awo8DmgK.js";import{C as I}from"./Content-fE7CgtG0.js";import{C as m}from"./CopyButton-mz8NDl3f.js";import{B as W}from"./Button-GjZOW2uO.js";import{I as g}from"./InlineCode-XpZawjOy.js";import{L as k}from"./Link-mZ-kuTN2.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BQKp_5Ng.js";import"./index-BBXbSWyk.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-C0qn59OO.js";import"./index-Dfd2pZHs.js";import"./useLocalizedStringFormatter-CZn_2Om1.js";import"./Label--FbMCZJS.js";import"./utils-BAtcVSHo.js";import"./Hidden-kqRI_mPI.js";import"./Wrap-DQq6jo70.js";import"./IconApp-DZf0STiH.js";import"./IconWarning-BqX4qer7.js";import"./Tooltip-50klWZnn.js";import"./OverlayArrow-B-bZ71PV.js";import"./useFocus-CGq_xkJe.js";import"./ProgressBar-B6tGqqoL.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Bh1EGhcd.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-4nfZ-XMX.js";import"./useFocusable-D8BATkaF.js";import"./useFocusRing-D6MLu59I.js";import"./react-children-utilities-fm2cgFnI.js";import"./Action-DdkAMDFz.js";import"./context-BWt75kQX.js";import"./useStatic-CZ_o1reA.js";import"./browser-B1A6F7LW.js";import"./getActionGroupSlot-CeuQjEkT.js";import"./dynamic-DKDa4OpU.js";import"./onlyText-DBswRDk6.js";import"./Text-BPAvSiAh.js";import"./EmulatedBoldText-BfYT5ZHF.js";import"./Text-CxIdO6ok.js";import"./LoadingSpinner-_BP_ZOWb.js";import"./Button-Bd7GnYjT.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-AhNAqARA.js";const Be={title:"Content/LabeledValue",component:t,parameters:{controls:{exclude:["className"]}},render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Project"}),e.jsx(I,{children:"My proSpace"})]})},p={},a={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Project"}),e.jsx(I,{children:"My proSpace"}),e.jsx(m,{text:"My proSpace"})]})},s={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"IP address"}),e.jsx(g,{children:"127.0.0.1"}),e.jsx(m,{text:"127.0.0.1"})]})},n={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Domain"}),e.jsx(k,{children:"https://mittwald.de"}),e.jsx(m,{text:"https://mittwald.de"})]})},i={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Support pin"}),e.jsx(W,{variant:"soft",color:"secondary",children:"Generate"})]})};var d,c,l;p.parameters={...p.parameters,docs:{...(d=p.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(l=(c=p.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,x,h;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Project</Label>
+      <Content>My proSpace</Content>
+      <CopyButton text="My proSpace" />
+    </LabeledValue>
+}`,...(h=(x=a.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var L,j,b;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>IP address</Label>
+      <InlineCode>127.0.0.1</InlineCode>
+      <CopyButton text="127.0.0.1" />
+    </LabeledValue>
+}`,...(b=(j=s.parameters)==null?void 0:j.docs)==null?void 0:b.source}}};var C,f,y;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Domain</Label>
+      <Link>https://mittwald.de</Link>
+      <CopyButton text="https://mittwald.de" />
+    </LabeledValue>
+}`,...(y=(f=n.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var B,S,V;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Support pin</Label>
+      <Button variant="soft" color="secondary">
+        Generate
+      </Button>
+    </LabeledValue>
+}`,...(V=(S=i.parameters)==null?void 0:S.docs)==null?void 0:V.source}}};const Se=["Default","WithCopyButton","WithInlineCode","WithLink","WithButton"];export{p as Default,i as WithButton,a as WithCopyButton,s as WithInlineCode,n as WithLink,Se as __namedExportsOrder,Be as default};
