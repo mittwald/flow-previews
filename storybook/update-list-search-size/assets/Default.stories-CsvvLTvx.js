9384befab63cb605f5301207507951c6ffc38453
mittwald <>
2025-04-02 12:23:08 +0000

    Update from https://github.com/mittwald/flow/commit/fde6bb24cc2595c27c2d2e53be3e29b0f771cb58

---
+++ storybook/update-list-search-size/assets/Default.stories-CsvvLTvx.js
@@ -0,0 +1,9 @@
+import{j as l}from"./jsx-runtime-D_zvdyIk.js";import{I as o}from"./Image-CR5wxurj.js";import"./index-CtQTiInQ.js";import{d as h}from"./dummyText-CX_I_Wpl.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./clsx-B-dksMZM.js";import"./index-DTllGi7h.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Cdp0xV4M.js";const D={title:"Content/Image",component:o,render:g=>l.jsx(o,{...g,alt:"Gopher",src:h.imageSrc})},r={},e={args:{withBorder:!0}},t={args:{aspectRatio:16/9}};var a,s,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(m=(s=r.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var p,c,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    withBorder: true
+  }
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var n,d,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    aspectRatio: 16 / 9
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const E=["Default","WithBorder","WithAspectRation"];export{r as Default,t as WithAspectRation,e as WithBorder,E as __namedExportsOrder,D as default};
---
+++ storybook/update-list-search-size/assets/Default.stories-Pg1NWR36.js
@@ -0,0 +1,64 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{r as P}from"./index-CtQTiInQ.js";import{L as s}from"./Label-C3GsNgl3.js";import{F as o}from"./FileField-JW0L6ZM8.js";import{B as t}from"./Button-hygsiG4S.js";import{S as H}from"./Section-B3bS0dJT.js";import{F as k}from"./FieldError-snFLArLq.js";import{F as G}from"./FieldDescription-CCuMWzFR.js";import{a as R}from"./IconApp-ZwiVKkvj.js";import{u as q,F as w,t as U}from"./Form-v9R1h0gz.js";import{a as _}from"./index-B-lxVbXh.js";import{A as O}from"./ActionGroup-btqjAAND.js";import{k as z}from"./IconWarning-ZndYkHab.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./index-Cdp0xV4M.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./Label-BZrcB42p.js";import"./utils-D0CTRpvX.js";import"./Hidden-Dd0lSYNZ.js";import"./FormField.module-B9AzUSUD.js";import"./useFormValidation-CI5EZsou.js";import"./useFocus-HZduNndM.js";import"./FieldError-DHw7a52T.js";import"./Text-oMr6ueMZ.js";import"./filterDOMProps-CeZl_uWj.js";import"./Input-DIVKzZcg.js";import"./useFocusRing-HsOmCAqc.js";import"./Text-DbOmKCPv.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-D1jUzRoU.js";import"./LoadingSpinner-B1RDGRlO.js";import"./Button-BGa7Lue7.js";import"./ProgressBar-TBeTHm5P.js";import"./useLabel-BhTOAlCQ.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-CEYeyB88.js";import"./useFocusable-43R3EC9q.js";import"./ContextMenuSection-CKwYExBR.js";import"./Action-BzpotNjO.js";import"./context-CTL7IXlD.js";import"./useStatic-6gmUMcuL.js";import"./getActionGroupSlot-OAZ-j8kn.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-Ds9EASAq.js";import"./RSPContexts-8O1fXkhd.js";import"./OverlayArrow-Cidi8nN7.js";import"./Collection-mdal2UB1.js";import"./CollectionBuilder-Bzd5BBwK.js";import"./Separator-CJeFikfz.js";import"./useOverlayTriggerState-Zz3oHLvd.js";import"./useControlledState-BKUqB07S.js";import"./SelectionManager-BAB-BMSj.js";import"./useEvent-BQK_0uZ_.js";import"./useCollator-B4aWBoUu.js";import"./FocusScope-Cx-5Cw_n.js";import"./VisuallyHidden-p5Z0Rn92.js";import"./v4-CtRu48qb.js";const Zr={title:"Form Controls/FileField",component:o,render:e=>r.jsxs(o,{...e,children:[r.jsx(s,{children:"Certificate"}),r.jsx(t,{variant:"outline",color:"secondary",children:"Select"})]})},J=_("submit"),l={},c={render:e=>r.jsxs(o,{...e,children:[r.jsx(s,{children:"Certificate"}),r.jsx(t,{variant:"outline",color:"secondary",children:"Select"}),r.jsx(G,{children:"Supported formats: .pem, .pfx"})]})},m={render:e=>r.jsxs(o,{...e,isInvalid:!0,children:[r.jsx(s,{children:"Certificate"}),r.jsx(t,{variant:"outline",color:"secondary",children:"Select"}),r.jsx(k,{children:"File too large"})]})},p={render:e=>r.jsx(o,{...e,children:r.jsx(t,{variant:"soft","aria-label":"Select certificate",children:r.jsx(R,{children:r.jsx(z,{})})})})},d={render:e=>{const[n,a]=P.useState(null),i=n==null?void 0:n[0];return r.jsxs(H,{children:[r.jsxs(o,{...e,isInvalid:!0,onChange:a,children:[r.jsx(s,{children:"Certificate"}),r.jsx(t,{variant:"outline",color:"secondary",children:"Select"})]}),i==null?void 0:i.name]})}},u={render:()=>{var a,i;const e=q(),n=U();return r.jsx(w,{form:e,onSubmit:J,children:r.jsxs(H,{children:[r.jsx(n,{name:"file",rules:{required:"Please choose a file"},children:r.jsxs(o,{multiple:!0,children:[r.jsx(s,{children:"Certificate"}),r.jsx(t,{variant:"outline",color:"secondary",children:"Select"})]})}),(i=(a=e.watch("file"))==null?void 0:a[0])==null?void 0:i.name,r.jsx(O,{children:r.jsx(t,{type:"submit",children:"Upload"})})]})})}};var F,f,h;l.parameters={...l.parameters,docs:{...(F=l.parameters)==null?void 0:F.docs,source:{originalSource:"{}",...(h=(f=l.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var x,j,S;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: props => <FileField {...props}>
+      <Label>Certificate</Label>
+      <Button variant="outline" color="secondary">
+        Select
+      </Button>
+      <FieldDescription>Supported formats: .pem, .pfx</FieldDescription>
+    </FileField>
+}`,...(S=(j=c.parameters)==null?void 0:j.docs)==null?void 0:S.source}}};var b,B,v;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <FileField {...props} isInvalid>
+      <Label>Certificate</Label>
+      <Button variant="outline" color="secondary">
+        Select
+      </Button>
+      <FieldError>File too large</FieldError>
+    </FileField>
+}`,...(v=(B=m.parameters)==null?void 0:B.docs)==null?void 0:v.source}}};var y,C,L;p.parameters={...p.parameters,docs:{...(y=p.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <FileField {...props}>
+      <Button variant="soft" aria-label="Select certificate">
+        <Icon>
+          <IconPaperclip />
+        </Icon>
+      </Button>
+    </FileField>
+}`,...(L=(C=p.parameters)==null?void 0:C.docs)==null?void 0:L.source}}};var I,g,E;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => {
+    const [files, setFiles] = useState<FileList | null>(null);
+    const file = files?.[0];
+    return <Section>
+        <FileField {...props} isInvalid onChange={setFiles}>
+          <Label>Certificate</Label>
+          <Button variant="outline" color="secondary">
+            Select
+          </Button>
+        </FileField>
+        {file?.name}
+      </Section>;
+  }
+}`,...(E=(g=d.parameters)==null?void 0:g.docs)==null?void 0:E.source}}};var W,D,A;u.parameters={...u.parameters,docs:{...(W=u.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: () => {
+    const form = useForm<{
+      file: FileList | null;
+    }>();
+    const Field = typedField(form);
+    return <Form form={form} onSubmit={submitAction}>
+        <Section>
+          <Field name="file" rules={{
+          required: "Please choose a file"
+        }}>
+            <FileField multiple>
+              <Label>Certificate</Label>
+              <Button variant="outline" color="secondary">
+                Select
+              </Button>
+            </FileField>
+          </Field>
+          {form.watch("file")?.[0]?.name}
+          <ActionGroup>
+            <Button type="submit">Upload</Button>
+          </ActionGroup>
+        </Section>
+      </Form>;
+  }
+}`,...(A=(D=u.parameters)==null?void 0:D.docs)==null?void 0:A.source}}};const $r=["Default","WithDescription","WithError","IconButton","WithHandler","WithReactHookForm"];export{l as Default,p as IconButton,c as WithDescription,m as WithError,d as WithHandler,u as WithReactHookForm,$r as __namedExportsOrder,Zr as default};
