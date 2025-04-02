4c236571d73964d56c17b572f9e386031907d398
mittwald <>
2025-04-02 12:59:00 +0000

    Update from https://github.com/mittwald/flow/commit/c80f5340be86f3811c13b5be587ea4a0ee50bce7

---
+++ storybook/enhancement/package/assets/DialogTriggerView--BsqquDh.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r as i}from"./index-CtQTiInQ.js";import{D as t}from"./OverlayTrigger-CeSY8tTU.js";import{v as s}from"./PropsContextProvider-CcavWj25.js";const g=o=>{const r=i.useContext(s).DialogTrigger??t;return e.jsx(r,{...o})};g.__docgenInfo={description:"",methods:[],displayName:"DialogTriggerView"};export{g as D};
