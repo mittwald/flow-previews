6032c9e091e1016b79550f3a9f95577b84efe57e
mittwald <>
2025-04-08 14:01:37 +0000

    Update from https://github.com/mittwald/flow/commit/46dbd91a65b272dc6eeff4696a490e8f5193d88f

--- storybook/1288-progressbar-improvements/assets/EdgeCases.stories-CHgKCMNl.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{P as i}from"./ProgressBar-CTFotANl.js";import s from"./Default.stories-BXvNVjH-.js";import{L as p}from"./Label-C3GsNgl3.js";import"./index-CtQTiInQ.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Cdp0xV4M.js";import"./ProgressBar-DPPT7aTs.js";import"./utils-D0CTRpvX.js";import"./Label-BZrcB42p.js";import"./Hidden-Dd0lSYNZ.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BhTOAlCQ.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./NumberFormatter-DNR9MAW-.js";import"./Legend-BMHzhgUH.js";import"./Text-VF_oP7nB.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-D1jUzRoU.js";import"./Text-oMr6ueMZ.js";const $={...s,title:"Status/ProgressBar/Edge Cases"},r={render:a=>t.jsx(i,{value:500,maxValue:1e3,minValue:0,showMaxValue:!0,formatOptions:{style:"unit",unit:"gigabyte"},...a,children:t.jsx(p,{children:n.medium})})};var o,e,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: props => <ProgressBar value={500} maxValue={1000} minValue={0} showMaxValue formatOptions={{
-    style: "unit",
-    unit: "gigabyte"
-  }} {...props}>
-      <Label>{dummyText.medium}</Label>
-    </ProgressBar>
-}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const k=["LongText"];export{r as LongText,k as __namedExportsOrder,$ as default};
