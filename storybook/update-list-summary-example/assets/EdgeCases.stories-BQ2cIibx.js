0dd735f926fc32e1498e6c06c71f9415f8307cad
mittwald <>
2025-04-08 08:06:05 +0000

    Cleanup merged Storybook previews

--- storybook/update-list-summary-example/assets/EdgeCases.stories-BQ2cIibx.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{P as s}from"./ProgressBar-DgzAt6bg.js";import i from"./Default.stories-C1BOlJLL.js";import{L as p}from"./Label-C3GsNgl3.js";import"./index-CtQTiInQ.js";import{d as n}from"./dummyText-CX_I_Wpl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Cdp0xV4M.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./ProgressBar-TBeTHm5P.js";import"./utils-D0CTRpvX.js";import"./Label-BZrcB42p.js";import"./Hidden-Dd0lSYNZ.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BhTOAlCQ.js";import"./NumberFormatter-DNR9MAW-.js";const M={...i,title:"Status/ProgressBar/Edge Cases"},r={render:a=>t.jsx(s,{value:500,maxValue:1e3,minValue:0,showMaxValue:!0,formatOptions:{style:"unit",unit:"gigabyte"},...a,children:t.jsx(p,{children:n.medium})})};var e,o,m;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  render: props => <ProgressBar value={500} maxValue={1000} minValue={0} showMaxValue formatOptions={{
-    style: "unit",
-    unit: "gigabyte"
-  }} {...props}>
-      <Label>{dummyText.medium}</Label>
-    </ProgressBar>
-}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const S=["LongText"];export{r as LongText,S as __namedExportsOrder,M as default};
