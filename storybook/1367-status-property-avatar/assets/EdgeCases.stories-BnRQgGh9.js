1f075269ee51d8b282f7fd3b87ea7123df4a3ecf
mittwald <>
2025-04-11 06:49:32 +0000

    Update from https://github.com/mittwald/flow/commit/fc43b368a8e661c22a41b9dd002f2a92fb950107

---
+++ storybook/1367-status-property-avatar/assets/EdgeCases.stories-BnRQgGh9.js
@@ -0,0 +1,25 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{R as l,b as p,a as g}from"./RadioButton-BsG4NRCT.js";import"./index-CtQTiInQ.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import j from"./Default.stories-BJtIGjD0.js";import{T as y}from"./Text-VF_oP7nB.js";import{C as T}from"./Content-hpcy-Uta.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Cdp0xV4M.js";import"./FieldError-D1V0yKtv.js";import"./IconWarning-QXks_Kv-.js";import"./FieldError-DHw7a52T.js";import"./utils-D0CTRpvX.js";import"./Text-oMr6ueMZ.js";import"./filterDOMProps-CeZl_uWj.js";import"./ColumnLayout-V6_UbH1_.js";import"./FormField.module-B9AzUSUD.js";import"./RadioGroup-GzxUY-Vm.js";import"./Form-DrDCrztH.js";import"./useLabel-BhTOAlCQ.js";import"./Label-BZrcB42p.js";import"./Hidden-Dd0lSYNZ.js";import"./useFormReset-Dt6BcYvw.js";import"./usePress-CEYeyB88.js";import"./useFocus-HZduNndM.js";import"./useFocusRing-HsOmCAqc.js";import"./useFocusable-43R3EC9q.js";import"./useFormValidation-CI5EZsou.js";import"./FocusScope-Cx-5Cw_n.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./VisuallyHidden-p5Z0Rn92.js";import"./useControlledState-BKUqB07S.js";import"./Label-C3GsNgl3.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-D1jUzRoU.js";const dr={title:"Form Controls/RadioGroup/Edge Cases",...j},t={render:e=>r.jsxs(l,{...e,defaultValue:"a","aria-label":"Label",children:[r.jsxs(p,{value:"a",children:[a.long," "]}),r.jsxs(p,{value:"b",children:[a.long," "]}),r.jsx(p,{value:"c",children:a.long})]})},i={render:e=>r.jsx(l,{...e,defaultValue:"0","aria-label":"Label",l:[1,1,1,1],children:Array(6).fill("").map((v,o)=>r.jsxs(g,{value:o.toString(),children:[r.jsx(y,{children:a.medium}),r.jsx(T,{children:a.medium})]},o))})},m={render:e=>r.jsx(l,{...e,defaultValue:"0","aria-label":"Label",children:Array(3).fill("").map((v,o)=>r.jsxs(g,{value:o.toString(),children:["Option ",o+1]},o))}),parameters:{viewport:{defaultViewport:"mobile1"}}};var n,d,s;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="a" aria-label="Label">
+      <Radio value="a">{dummyText.long} </Radio>
+      <Radio value="b">{dummyText.long} </Radio>
+      <Radio value="c">{dummyText.long}</Radio>
+    </RadioGroup>
+}`,...(s=(d=t.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};var u,c,x;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="0" aria-label="Label" l={[1, 1, 1, 1]}>
+      {Array(6).fill("").map((value, index) => <RadioButton value={index.toString()} key={index}>
+            <Text>{dummyText.medium}</Text>
+            <Content>{dummyText.medium}</Content>
+          </RadioButton>)}
+    </RadioGroup>
+}`,...(x=(c=i.parameters)==null?void 0:c.docs)==null?void 0:x.source}}};var R,f,b;m.parameters={...m.parameters,docs:{...(R=m.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="0" aria-label="Label">
+      {Array(3).fill("").map((value, index) => <RadioButton value={index.toString()} key={index}>
+            Option {index + 1}
+          </RadioButton>)}
+    </RadioGroup>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(b=(f=m.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};const sr=["LongTexts","MultipleRadioButtons","SmallSpace"];export{t as LongTexts,i as MultipleRadioButtons,m as SmallSpace,sr as __namedExportsOrder,dr as default};
---
+++ storybook/1367-status-property-avatar/assets/Variants.stories-C9PiSS6U.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as n}from"./Alert-BWOi_Awu.js";import"./index-CtQTiInQ.js";import{H as i}from"./Heading-C5oNFXhr.js";import{C as d}from"./Content-hpcy-Uta.js";import v from"./Default.stories-DK9BoJ4w.js";import"./AlertIcon-tK-r7TkC.js";import"./IconWarning-QXks_Kv-.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Cdp0xV4M.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./dynamic-DKDa4OpU.js";import"./Heading-BzdgKMPb.js";import"./RSPContexts-8O1fXkhd.js";import"./utils-D0CTRpvX.js";import"./Button-rH72YzgV.js";import"./Text-VF_oP7nB.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-D1jUzRoU.js";import"./Text-oMr6ueMZ.js";import"./LoadingSpinner-Bpd2bwIk.js";import"./Button-BEBevqbU.js";import"./ProgressBar-DPPT7aTs.js";import"./Label-BZrcB42p.js";import"./Hidden-Dd0lSYNZ.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BhTOAlCQ.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-CEYeyB88.js";import"./useFocus-HZduNndM.js";import"./useFocusRing-HsOmCAqc.js";import"./useFocusable-43R3EC9q.js";const ae={...v,title:"Status/Alert/Status"},s={render:r=>e.jsxs(n,{...r,children:[e.jsx(i,{children:"Email address has been archived"}),e.jsx(d,{children:"As your domain has been deleted, this email address has been archived. To be able to send and receive emails, you must rename the address."})]})},t={args:{status:"warning"},render:r=>e.jsxs(n,{...r,children:[e.jsx(i,{children:"Storage is almost exceeded"}),e.jsx(d,{children:"Your storage space is over 80% utilized. We recommend that you upgrade the storage space to avoid disruptions during backups."})]})},o={args:{status:"danger"},render:r=>e.jsxs(n,{...r,children:[e.jsx(i,{children:"No SSL certificate could be issued"}),e.jsx(d,{children:"No SSL certificate could be issued for this domain because the domain IP does not point to your server IP."})]})},a={args:{status:"success"},render:r=>e.jsxs(n,{...r,children:[e.jsx(i,{children:"Your app is up to date"}),e.jsx(d,{children:"Your app has been updated to the current version."})]})};var p,m,c;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: props => <Alert {...props}>
+      <Heading>Email address has been archived</Heading>
+      <Content>
+        As your domain has been deleted, this email address has been archived.
+        To be able to send and receive emails, you must rename the address.
+      </Content>
+    </Alert>
+}`,...(c=(m=s.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var u,l,h;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    status: "warning"
+  },
+  render: props => <Alert {...props}>
+      <Heading>Storage is almost exceeded</Heading>
+      <Content>
+        Your storage space is over 80% utilized. We recommend that you upgrade
+        the storage space to avoid disruptions during backups.
+      </Content>
+    </Alert>
+}`,...(h=(l=t.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var g,b,x;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    status: "danger"
+  },
+  render: props => <Alert {...props}>
+      <Heading>No SSL certificate could be issued</Heading>
+      <Content>
+        No SSL certificate could be issued for this domain because the domain IP
+        does not point to your server IP.
+      </Content>
+    </Alert>
+}`,...(x=(b=o.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var S,f,j;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  },
+  render: props => <Alert {...props}>
+      <Heading>Your app is up to date</Heading>
+      <Content>Your app has been updated to the current version.</Content>
+    </Alert>
+}`,...(j=(f=a.parameters)==null?void 0:f.docs)==null?void 0:j.source}}};const ne=["Info","Warning","Danger","Success"];export{o as Danger,s as Info,a as Success,t as Warning,ne as __namedExportsOrder,ae as default};
