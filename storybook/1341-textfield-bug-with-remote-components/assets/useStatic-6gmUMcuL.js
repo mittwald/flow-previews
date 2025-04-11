061888939a99dc288b4575bff1b77f69fbb6893e
mittwald <>
2025-04-11 12:31:54 +0000

    Cleanup merged Storybook previews

--- storybook/1341-textfield-bug-with-remote-components/assets/useStatic-6gmUMcuL.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./PropsContextProvider-CcavWj25.js";import{r as e}from"./index-CtQTiInQ.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
