64d40b4a54440af8006af1321fabfc4cd35b82f4
mittwald <>
2025-04-14 09:46:39 +0000

    Update from https://github.com/mittwald/flow/commit/9f021c5f88a4597c2ca9550777c110e485bd1690

---
+++ storybook-main/assets/Default.stories-BdgjHGyb.js
@@ -0,0 +1,56 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-CtQTiInQ.js";import{H as o}from"./Heading-C5oNFXhr.js";import{T as i}from"./Text-VF_oP7nB.js";import{L as t}from"./Link-Bib_098N.js";import{A as n}from"./AccentBox-mRCof9DO.js";import{d as s}from"./dummyText-CX_I_Wpl.js";import{S as l}from"./Section-C2M3YcEF.js";import{F as g}from"./Flex-FxZ6wrmm.js";import{I as x}from"./IconWarning-QXks_Kv-.js";import{L as C}from"./LayoutCard-DFDHmcIx.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./index-Cdp0xV4M.js";import"./Heading-BzdgKMPb.js";import"./RSPContexts-8O1fXkhd.js";import"./utils-D0CTRpvX.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-D1jUzRoU.js";import"./Text-oMr6ueMZ.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-CEYeyB88.js";import"./useFocus-HZduNndM.js";import"./useFocusRing-HsOmCAqc.js";import"./useFocusable-43R3EC9q.js";import"./ContextMenuSection-CazUyE0l.js";import"./Action-BzpotNjO.js";import"./context-CTL7IXlD.js";import"./useStatic-6gmUMcuL.js";import"./getActionGroupSlot-OAZ-j8kn.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-D2bUbTtQ.js";import"./Button-BEBevqbU.js";import"./ProgressBar-DPPT7aTs.js";import"./Label-BZrcB42p.js";import"./Hidden-Dd0lSYNZ.js";import"./useLabel-BhTOAlCQ.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./OverlayArrow-Cidi8nN7.js";import"./Collection-mdal2UB1.js";import"./CollectionBuilder-Bzd5BBwK.js";import"./Separator-CJeFikfz.js";import"./useOverlayTriggerState-BZ_Sugy1.js";import"./useControlledState-BKUqB07S.js";import"./SelectionManager-BAB-BMSj.js";import"./useEvent-BQK_0uZ_.js";import"./useCollator-B4aWBoUu.js";import"./FocusScope-Cx-5Cw_n.js";import"./VisuallyHidden-p5Z0Rn92.js";const Gr={title:"Structure/AccentBox",component:n,args:{color:"blue"},render:e=>r.jsx(n,{...e,children:r.jsxs(l,{children:[r.jsx(o,{children:"Heading"}),r.jsx(i,{children:s.medium}),r.jsx(t,{children:"Link"})]})})},c={},a={args:{color:"gradient"},render:e=>r.jsx(n,{...e,children:r.jsxs(g,{align:"center",children:[r.jsxs(g,{direction:"column",grow:!0,children:[r.jsx(o,{size:"l",children:"Heading"}),r.jsx(i,{color:"dark",children:r.jsx("b",{children:s.short})})]}),r.jsx(t,{target:"_blank",href:"#",color:"dark",children:"Link"})]})})},m={args:{color:"blue"},render:e=>r.jsxs(n,{...e,children:[r.jsx(x,{}),r.jsxs(l,{children:[r.jsx(o,{children:"Heading"}),r.jsx(i,{children:s.long}),r.jsx(t,{children:"Link"})]})]})},d={args:{color:"green"},render:e=>r.jsxs(n,{...e,children:[r.jsx(x,{}),r.jsxs(l,{children:[r.jsx(o,{children:"Heading"}),r.jsx(i,{children:s.long}),r.jsx(t,{children:"Link"})]})]})},p={args:{color:"blue"},render:e=>r.jsx(C,{children:r.jsxs(n,{...e,children:[r.jsx(x,{}),r.jsxs(l,{children:[r.jsx(o,{children:"Heading"}),r.jsx(i,{children:s.long}),r.jsx(t,{children:"Link"})]})]})})};var u,h,j;c.parameters={...c.parameters,docs:{...(u=c.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(j=(h=c.parameters)==null?void 0:h.docs)==null?void 0:j.source}}};var L,k,H;a.parameters={...a.parameters,docs:{...(L=a.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    color: "gradient"
+  },
+  render: props => <AccentBox {...props}>
+      <Flex align="center">
+        <Flex direction="column" grow>
+          <Heading size="l">Heading</Heading>
+          <Text color="dark">
+            <b>{dummyText.short}</b>
+          </Text>
+        </Flex>
+        <Link target="_blank" href="#" color="dark">
+          Link
+        </Link>
+      </Flex>
+    </AccentBox>
+}`,...(H=(k=a.parameters)==null?void 0:k.docs)==null?void 0:H.source}}};var S,f,T;m.parameters={...m.parameters,docs:{...(S=m.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    color: "blue"
+  },
+  render: props => <AccentBox {...props}>
+      <IconStar />
+      <Section>
+        <Heading>Heading</Heading>
+        <Text>{dummyText.long}</Text>
+        <Link>Link</Link>
+      </Section>
+    </AccentBox>
+}`,...(T=(f=m.parameters)==null?void 0:f.docs)==null?void 0:T.source}}};var A,b,y;d.parameters={...d.parameters,docs:{...(A=d.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    color: "green"
+  },
+  render: props => <AccentBox {...props}>
+      <IconStar />
+      <Section>
+        <Heading>Heading</Heading>
+        <Text>{dummyText.long}</Text>
+        <Link>Link</Link>
+      </Section>
+    </AccentBox>
+}`,...(y=(b=d.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var B,I,F;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    color: "blue"
+  },
+  render: props => <LayoutCard>
+      <AccentBox {...props}>
+        <IconStar />
+        <Section>
+          <Heading>Heading</Heading>
+          <Text>{dummyText.long}</Text>
+          <Link>Link</Link>
+        </Section>
+      </AccentBox>
+    </LayoutCard>
+}`,...(F=(I=p.parameters)==null?void 0:I.docs)==null?void 0:F.source}}};const _r=["Default","Gradient","WithIcon","Green","InLayoutCard"];export{c as Default,a as Gradient,d as Green,p as InLayoutCard,m as WithIcon,_r as __namedExportsOrder,Gr as default};
---
+++ storybook-main/assets/Variants.stories-Bex3LrOG.js
@@ -0,0 +1,19 @@
+import f from"./Default.stories-Bn9XpMUI.js";import"./jsx-runtime-D_zvdyIk.js";import"./ProgressBar-Do0pFLau.js";import"./index-CtQTiInQ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CcavWj25.js";import"./mergeProps-BKLeaOo7.js";import"./index-DTllGi7h.js";import"./index-Cdp0xV4M.js";import"./ProgressBar-DPPT7aTs.js";import"./utils-D0CTRpvX.js";import"./Label-BZrcB42p.js";import"./Hidden-Dd0lSYNZ.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-BhTOAlCQ.js";import"./useLocalizedStringFormatter-DpLhROVm.js";import"./NumberFormatter-DNR9MAW-.js";import"./Legend-B_OX09bK.js";import"./Text-VF_oP7nB.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-D1jUzRoU.js";import"./Text-oMr6ueMZ.js";import"./Label-YolQAcjQ.js";const H={...f,title:"Status/ProgressBar/Variants"},r={args:{status:"info"}},s={args:{status:"success"}},a={args:{status:"warning",value:800}},t={args:{status:"danger",value:950}};var o,e,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    status: "info"
+  }
+}`,...(n=(e=r.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};var m,p,i;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  }
+}`,...(i=(p=s.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var c,u,g;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    status: "warning",
+    value: 800
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,l,S;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "danger",
+    value: 950
+  }
+}`,...(S=(l=t.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};const J=["Info","Success","Warning","Danger"];export{t as Danger,r as Info,s as Success,a as Warning,J as __namedExportsOrder,H as default};
