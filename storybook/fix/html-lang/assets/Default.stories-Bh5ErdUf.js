a6b01b31dc2f92f85977c2eb3ce4e1abace70d9b
mittwald <>
2025-04-28 11:08:27 +0000

    Update from https://github.com/mittwald/flow/commit/4eebf3f1eac199e740fddd171190df057cf232a2

---
+++ storybook/fix/html-lang/assets/Default.stories-Bh5ErdUf.js
@@ -0,0 +1,44 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import"./index-F2Fta7eo.js";import{a as d,c as u,b as E,s as R}from"./lib-D0yQtt8v.js";import{M as q}from"./Modal-DiJTdmg2.js";import{A as z}from"./ActionGroup-Dz1xxQId.js";import{B as p}from"./Button-ChyokZsX.js";import{H as I}from"./Heading-CN-8kQfF.js";import{C as J}from"./Content-DTmqmws-.js";import{A as r}from"./Action-BTcdCt_m.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./OverlayTrigger-DZPROePs.js";import"./context-MJhbzvKT.js";import"./PropsContextProvider-Dzc2yKZi.js";import"./mergeRefs-12BfVTtv.js";import"./clsx-B-dksMZM.js";import"./index-GHXRqlzO.js";import"./index-FKzx4Va7.js";import"./useStatic-D_3dHl9P.js";import"./Dialog-BBdTn5Uc.js";import"./Button-DCaCfXLp.js";import"./utils-D0arg4qg.js";import"./ProgressBar-bhmU_ZvQ.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./RSPContexts-DhZDYkwS.js";import"./OverlayArrow-ULot_6Ir.js";import"./Collection-CutN93xp.js";import"./CollectionBuilder-D5vZPvsR.js";import"./Separator-YWiDMMN1.js";import"./Text-AN1gvCNp.js";import"./useOverlayTriggerState-Bt-3O924.js";import"./useControlledState-15AFXVxl.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";import"./DialogTriggerView-BAm4hvCk.js";import"./Overlay-DRwMoKWr.js";import"./IconWarning-qggAu6gT.js";import"./Header-DBr1P6G6.js";import"./ButtonView-DHUWjE7v.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-COvCfpC2.js";import"./Text-DZJIxYzy.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./LoadingSpinner-_3JM3lT5.js";import"./Heading-AP9WPpJD.js";const Yo={title:"Actions/Action",component:r,render:_=>o.jsx(r,{..._,children:o.jsx(p,{children:"Button"})}),parameters:{controls:{exclude:["children","action","closeOverlay","openOverlay","toggleOverlay","showFeedback","break","skip"]}}},t={},n={args:{action:d}},e={args:{action:u}},s={args:{action:d,showFeedback:!0}},a={args:{action:u,showFeedback:!0}},c={args:{children:o.jsx(r,{action:R,children:E})}},i={args:{action:d,children:o.jsx(r,{action:d,children:E})}},m={render:()=>o.jsxs(r,{action:u,children:[o.jsxs(q,{slot:"actionConfirm",children:[o.jsx(I,{children:"Delete customer"}),o.jsx(J,{children:"Do you really want to delete the customer?"}),o.jsxs(z,{children:[o.jsx(p,{color:"danger",children:"Confirm"}),o.jsx(p,{color:"secondary",variant:"soft",children:"Abort"})]})]}),o.jsx(p,{color:"secondary",variant:"soft",children:"Delete customer"})]})};var l,g,y;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(y=(g=t.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var h,A,f;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    action: asyncFunction
+  }
+}`,...(f=(A=n.parameters)==null?void 0:A.docs)==null?void 0:f.source}}};var F,x,b;e.parameters={...e.parameters,docs:{...(F=e.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    action: asyncLongFunction
+  }
+}`,...(b=(x=e.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var j,k,C;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    action: asyncFunction,
+    showFeedback: true
+  }
+}`,...(C=(k=s.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};var B,D,L;a.parameters={...a.parameters,docs:{...(B=a.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    action: asyncLongFunction,
+    showFeedback: true
+  }
+}`,...(L=(D=a.parameters)==null?void 0:D.docs)==null?void 0:L.source}}};var S,v,w;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    children: <Action action={syncFunction}>{button}</Action>
+  }
+}`,...(w=(v=c.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};var M,W,H;i.parameters={...i.parameters,docs:{...(M=i.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    action: asyncFunction,
+    children: <Action action={asyncFunction}>{button}</Action>
+  }
+}`,...(H=(W=i.parameters)==null?void 0:W.docs)==null?void 0:H.source}}};var N,O,G;m.parameters={...m.parameters,docs:{...(N=m.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: () => <Action action={asyncLongFunction}>
+      <Modal slot="actionConfirm">
+        <Heading>Delete customer</Heading>
+        <Content>Do you really want to delete the customer?</Content>
+        <ActionGroup>
+          <Button color="danger">Confirm</Button>
+          <Button color="secondary" variant="soft">
+            Abort
+          </Button>
+        </ActionGroup>
+      </Modal>
+      <Button color="secondary" variant="soft">
+        Delete customer
+      </Button>
+    </Action>
+}`,...(G=(O=m.parameters)==null?void 0:O.docs)==null?void 0:G.source}}};const Zo=["Default","Async","AsyncLong","AsyncWithFeedback","AsyncLongWithFeedback","Nested","NestedAsync","WithConfirmationModal"];export{n as Async,e as AsyncLong,a as AsyncLongWithFeedback,s as AsyncWithFeedback,t as Default,c as Nested,i as NestedAsync,m as WithConfirmationModal,Zo as __namedExportsOrder,Yo as default};
