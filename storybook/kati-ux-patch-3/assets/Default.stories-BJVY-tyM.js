de8f0b805bfdc69060a5935811189727e97a0b59
mittwald <>
2025-05-02 12:08:34 +0000

    Update from https://github.com/mittwald/flow/commit/a5ff97ddfd918e4151b734b6ff2aaceac435b637

---
+++ storybook/kati-ux-patch-3/assets/Default.stories-BJVY-tyM.js
@@ -0,0 +1,15 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{I as i}from"./InlineCode-YVj4xVXC.js";import"./index-F2Fta7eo.js";import{T as k}from"./Text-DD6vurGQ.js";import{S as h}from"./Section-CRPz-V0i.js";import{a as f,s as x}from"./storyBackgrounds-GFSAi-dI.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-FieVuPWg.js";import"./mergeRefs-12BfVTtv.js";import"./index-GHXRqlzO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-FKzx4Va7.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./Text-AN1gvCNp.js";import"./utils-D0arg4qg.js";import"./ContextMenuSection-CjOnKstb.js";import"./Action-BEEG2BI4.js";import"./context-Cfwpplei.js";import"./useStatic-C1947tFT.js";import"./getActionGroupSlot-CFRykICz.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BBdTn5Uc.js";import"./Button-DCaCfXLp.js";import"./ProgressBar-bhmU_ZvQ.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./RSPContexts-DhZDYkwS.js";import"./OverlayArrow-ULot_6Ir.js";import"./Collection-CutN93xp.js";import"./CollectionBuilder-D5vZPvsR.js";import"./Separator-YWiDMMN1.js";import"./useOverlayTriggerState-Bt-3O924.js";import"./useControlledState-15AFXVxl.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";const gr={title:"Content/InlineCode",component:i,render:r=>a.jsx(h,{children:a.jsxs(k,{color:r.color==="dark"||r.color==="light"?r.color:void 0,children:["Enter ",a.jsx(i,{...r,children:"yarn start"})," to start your app."]})})},o={},t={args:{color:"dark"},parameters:{backgrounds:f}},m={args:{color:"light"},parameters:{backgrounds:x}};var e,p,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(s=(p=o.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var l,g,u;m.parameters={...m.parameters,docs:{...(l=m.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(u=(g=m.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};const ur=["Default","Dark","Light"];export{t as Dark,o as Default,m as Light,ur as __namedExportsOrder,gr as default};
