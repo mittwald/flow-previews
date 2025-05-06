20fb83d248b26ea95065959e61853957c07e473a
mittwald <>
2025-05-06 15:09:45 +0000

    Cleanup merged Storybook previews

--- storybook/backward-compatibility/assets/useStatic-C1947tFT.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./PropsContextProvider-FieVuPWg.js";import{r as e}from"./index-F2Fta7eo.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
