91a988af6270ce0a07c8d390442c7c1fe89b36c8
mittwald <>
2025-05-08 09:04:43 +0000

    Update from https://github.com/mittwald/flow/commit/787fdf30223aa6225aeffbeef95c54464375446b

---
+++ storybook/Nickzlos-patch-1/assets/Default.stories-BlJhGa-7.js
@@ -0,0 +1,75 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{S as t,C as w}from"./CountryOptions-BpUbl5tm.js";import"./index-F2Fta7eo.js";import{L as n}from"./Label-BBMGsxZ-.js";import{F as G}from"./FieldDescription-BoTpW2_I.js";import{F as Q}from"./FieldError-BHK6dEuK.js";import{O as e}from"./Option-NWai4CU4.js";import{C as U,a as X}from"./ContextualHelpTrigger-DIuv_Qx0.js";import{d as Z}from"./dummyText-CX_I_Wpl.js";import{B as $}from"./Button-BbQUsVuD.js";import{T as rr}from"./Text-C0wqZrY5.js";import"./PropsContextProvider-CLv7rqut.js";import"./mergeRefs-12BfVTtv.js";import"./clsx-B-dksMZM.js";import"./index-GHXRqlzO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-FKzx4Va7.js";import"./FormField.module-B9AzUSUD.js";import"./IconWarning-DxEBU1uy.js";import"./OverlayTrigger-D5TqBCil.js";import"./context-BvCJA7hU.js";import"./useStatic-DRfNlbL5.js";import"./Dialog-BBdTn5Uc.js";import"./Button-DCaCfXLp.js";import"./utils-D0arg4qg.js";import"./ProgressBar-bhmU_ZvQ.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./RSPContexts-DhZDYkwS.js";import"./OverlayArrow-ULot_6Ir.js";import"./Collection-CutN93xp.js";import"./CollectionBuilder-D5vZPvsR.js";import"./Separator-YWiDMMN1.js";import"./Text-AN1gvCNp.js";import"./useOverlayTriggerState-Bt-3O924.js";import"./useControlledState-15AFXVxl.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";import"./FieldError-Cy41ow6l.js";import"./Form-YK_aY0K2.js";import"./intlStrings-ZMTSFH_M.js";import"./ListBox-ClWI2WO3.js";import"./DragAndDrop-DtFh3aNA.js";import"./useListState-CddJDIHp.js";import"./useFormValidation-C9irvmUd.js";import"./useSingleSelectListState-Di6pIa41.js";import"./useFormReset-hDk0Ijlo.js";import"./Popover-DNib6Uko.js";import"./Popover.module-BJC3BRN9.js";import"./DialogTriggerView-CPTffblo.js";import"./LoadingSpinner-CiN3VJkM.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";const x=["DE","AT","CH"],er=(o,z)=>x.includes(z.code)?1:0-(x.includes(o.code)?1:0),ue={title:"Form Controls/Select",component:t,render:o=>r.jsxs(t,{...o,children:[r.jsx(n,{children:"App"}),r.jsx(e,{children:"WordPress"}),r.jsx(e,{children:"TYPO3"}),r.jsx(e,{children:"Contao"}),r.jsx(e,{children:"Drupal"}),r.jsx(e,{children:"Joomla!"}),r.jsx(e,{children:"Matomo"})]})},i={},s={args:{isDisabled:!0}},p={args:{isRequired:!0}},a={render:o=>r.jsxs(t,{...o,children:[r.jsx(n,{children:"App"}),r.jsx(e,{children:"WordPress"}),r.jsx(e,{children:"TYPO3"}),r.jsx(e,{children:"Contao"}),r.jsx(e,{children:"Drupal"}),r.jsx(e,{children:"Joomla!"}),r.jsx(e,{children:"Matomo"}),r.jsx(G,{children:"Select an app"})]})},l={render:o=>r.jsxs(t,{...o,defaultSelectedKey:"WordPress",children:[r.jsx(n,{children:"App"}),r.jsx(e,{value:"WordPress",children:"WordPress"}),r.jsx(e,{value:"TYPO3",children:"TYPO3"}),r.jsx(e,{value:"Contao",children:"Contao"}),r.jsx(e,{value:"Drupal",children:"Drupal"}),r.jsx(e,{value:"Joomla!",children:"Joomla!"}),r.jsx(e,{value:"Matomo",children:"Matomo"})]})},c={render:o=>r.jsxs(t,{...o,isInvalid:!0,isRequired:!0,children:[r.jsx(n,{children:"App"}),r.jsx(e,{children:"WordPress"}),r.jsx(e,{children:"TYPO3"}),r.jsx(e,{children:"Contao"}),r.jsx(e,{children:"Drupal"}),r.jsx(e,{children:"Joomla!"}),r.jsx(e,{children:"Matomo"}),r.jsx(Q,{children:"Select an app to continue"})]})},m={render:o=>r.jsxs(t,{...o,children:[r.jsx(n,{children:"Number"}),r.jsx(e,{value:1,children:"1"}),r.jsx(e,{value:2,children:"2"}),r.jsx(e,{value:3,children:"3"}),r.jsx(e,{value:4,children:"4"})]})},d={render:o=>r.jsx(t,{...o,children:r.jsx(w,{})})},u={render:o=>r.jsx(t,{...o,children:r.jsx(w,{sortBy:er})})},O={render:o=>r.jsxs(t,{...o,children:[r.jsxs(n,{children:["App",r.jsxs(U,{children:[r.jsx($,{}),r.jsx(X,{children:r.jsx(rr,{children:Z.short})})]})]}),r.jsx(e,{children:"WordPress"}),r.jsx(e,{children:"TYPO3"}),r.jsx(e,{children:"Contao"}),r.jsx(e,{children:"Drupal"}),r.jsx(e,{children:"Joomla!"}),r.jsx(e,{children:"Matomo"})]})};var h,j,S;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:"{}",...(S=(j=i.parameters)==null?void 0:j.docs)==null?void 0:S.source}}};var C,D,W;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(W=(D=s.parameters)==null?void 0:D.docs)==null?void 0:W.source}}};var P,v,T;p.parameters={...p.parameters,docs:{...(P=p.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(T=(v=p.parameters)==null?void 0:v.docs)==null?void 0:T.source}}};var g,b,f;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Select {...props}>
+      <Label>App</Label>
+      <Option>WordPress</Option>
+      <Option>TYPO3</Option>
+      <Option>Contao</Option>
+      <Option>Drupal</Option>
+      <Option>Joomla!</Option>
+      <Option>Matomo</Option>
+      <FieldDescription>Select an app</FieldDescription>
+    </Select>
+}`,...(f=(b=a.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var y,F,A;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <Select {...props} defaultSelectedKey="WordPress">
+      <Label>App</Label>
+      <Option value="WordPress">WordPress</Option>
+      <Option value="TYPO3">TYPO3</Option>
+      <Option value="Contao">Contao</Option>
+      <Option value="Drupal">Drupal</Option>
+      <Option value="Joomla!">Joomla!</Option>
+      <Option value="Matomo">Matomo</Option>
+    </Select>
+}`,...(A=(F=l.parameters)==null?void 0:F.docs)==null?void 0:A.source}}};var L,J,M;c.parameters={...c.parameters,docs:{...(L=c.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: props => <Select {...props} isInvalid isRequired>
+      <Label>App</Label>
+      <Option>WordPress</Option>
+      <Option>TYPO3</Option>
+      <Option>Contao</Option>
+      <Option>Drupal</Option>
+      <Option>Joomla!</Option>
+      <Option>Matomo</Option>
+      <FieldError>Select an app to continue</FieldError>
+    </Select>
+}`,...(M=(J=c.parameters)==null?void 0:J.docs)==null?void 0:M.source}}};var Y,H,E;m.parameters={...m.parameters,docs:{...(Y=m.parameters)==null?void 0:Y.docs,source:{originalSource:`{
+  render: props => <Select {...props}>
+      <Label>Number</Label>
+      <Option value={1}>1</Option>
+      <Option value={2}>2</Option>
+      <Option value={3}>3</Option>
+      <Option value={4}>4</Option>
+    </Select>
+}`,...(E=(H=m.parameters)==null?void 0:H.docs)==null?void 0:E.source}}};var B,R,q;d.parameters={...d.parameters,docs:{...(B=d.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <Select {...props}>
+      <CountryOptions />
+    </Select>
+}`,...(q=(R=d.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};var N,I,K;u.parameters={...u.parameters,docs:{...(N=u.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: props => <Select {...props}>
+      <CountryOptions sortBy={sortByDachFirst} />
+    </Select>
+}`,...(K=(I=u.parameters)==null?void 0:I.docs)==null?void 0:K.source}}};var V,_,k;O.parameters={...O.parameters,docs:{...(V=O.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: props => <Select {...props}>
+      <Label>
+        App
+        <ContextualHelpTrigger>
+          <Button />
+          <ContextualHelp>
+            <Text>{dummyText.short}</Text>
+          </ContextualHelp>
+        </ContextualHelpTrigger>
+      </Label>
+      <Option>WordPress</Option>
+      <Option>TYPO3</Option>
+      <Option>Contao</Option>
+      <Option>Drupal</Option>
+      <Option>Joomla!</Option>
+      <Option>Matomo</Option>
+    </Select>
+}`,...(k=(_=O.parameters)==null?void 0:_.docs)==null?void 0:k.source}}};const Oe=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithFieldError","WithNumbers","WithCountryOptions","WithCountryOptionsAndCustomSort","WithContextualHelp"];export{i as Default,s as Disabled,p as Required,O as WithContextualHelp,d as WithCountryOptions,u as WithCountryOptionsAndCustomSort,l as WithDefaultValue,a as WithFieldDescription,c as WithFieldError,m as WithNumbers,Oe as __namedExportsOrder,ue as default};
