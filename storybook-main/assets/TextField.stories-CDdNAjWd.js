e935985f526205dd47f636d86d34466800b80af3
mittwald <>
2025-05-12 05:56:35 +0000

    Update from https://github.com/mittwald/flow/commit/912cd2a82766f90bffbfe3cf60ba087b9d395818

---
+++ storybook-main/assets/TextField.stories-CDdNAjWd.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as x}from"./ActionGroup-i7KgXf1Z.js";import{B as f}from"./Button-BR8g0h2X.js";import{L as m}from"./Label-BBMGsxZ-.js";import{S as j}from"./Section-Bfv1eGXQ.js";import{T as n}from"./TextField-BBpZlWtk.js";import{a as g,u as b,F,t as S}from"./Form-Bkgi4z4l.js";import{s as y}from"./Action-VzGiUi9p.js";import{a as L}from"./index-B-lxVbXh.js";import"./index-F2Fta7eo.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./PropsContextProvider-CLv7rqut.js";import"./mergeRefs-12BfVTtv.js";import"./clsx-B-dksMZM.js";import"./index-GHXRqlzO.js";import"./index-FKzx4Va7.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-DjaHqBmJ.js";import"./useStatic-DRfNlbL5.js";import"./IconWarning-DxEBU1uy.js";import"./Text-D1T0pLmS.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./Text-AN1gvCNp.js";import"./utils-D0arg4qg.js";import"./LoadingSpinner-CiN3VJkM.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./Button-DCaCfXLp.js";import"./ProgressBar-bhmU_ZvQ.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./ContextMenuSection-BTorvOQF.js";import"./Dialog-BBdTn5Uc.js";import"./RSPContexts-DhZDYkwS.js";import"./OverlayArrow-ULot_6Ir.js";import"./Collection-CutN93xp.js";import"./CollectionBuilder-D5vZPvsR.js";import"./Separator-YWiDMMN1.js";import"./useOverlayTriggerState-Bt-3O924.js";import"./useControlledState-15AFXVxl.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";import"./TextFieldBase-DvNvrPPn.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-BHK6dEuK.js";import"./FieldError-Cy41ow6l.js";import"./FieldDescription-CiJ9FH2f.js";import"./Form-YK_aY0K2.js";import"./Input-BJzWkL0O.js";import"./useTextField-lWgcofAo.js";import"./useFormReset-hDk0Ijlo.js";import"./useFormValidation-C9irvmUd.js";import"./context-BvCJA7hU.js";import"./v4-CtRu48qb.js";const V=L("submit"),Ge={title:"Integrations/React Hook Form/TextField",component:g,render:()=>{const s=async o=>{await y(1500),V(o)},t=b({defaultValues:{name:"",nameDefaultValue:"John",nameRequired:"",nameMaxLength:""}}),r=S();return e.jsx(F,{form:t,onSubmit:s,children:e.jsxs(j,{children:[e.jsx(r,{name:"name",children:e.jsx(n,{children:e.jsx(m,{children:"Name"})})}),e.jsx(r,{name:"nameDefaultValue",children:e.jsx(n,{children:e.jsx(m,{children:"Name"})})}),e.jsx(r,{name:"nameRequired",rules:{required:"Please enter your name"},children:e.jsx(n,{children:e.jsx(m,{children:"Name"})})}),e.jsx(r,{name:"nameMaxLength",children:e.jsx(n,{maxLength:10,showCharacterCount:!0,children:e.jsx(m,{children:"Name"})})}),e.jsx(x,{children:e.jsx(f,{type:"submit",children:"Submit"})})]})})}},i={},a={render:()=>{const s=o=>{V(o)},t=b({defaultValues:{name:""}}),r=S();return e.jsx(F,{form:t,onSubmit:s,children:e.jsxs(j,{children:[e.jsx(r,{name:"name",children:e.jsx(n,{value:t.watch("name"),onChange:o=>{t.setValue("name",o.toUpperCase())},children:e.jsx(m,{children:"Name"})})}),e.jsx(x,{children:e.jsx(f,{type:"submit",children:"Submit"})})]})})}};var p,l,u;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(u=(l=i.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var c,d,h;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    interface Values {
+      name: string;
+    }
+    const handleOnSubmit = (values: Values) => {
+      submitAction(values);
+    };
+    const form = useForm<Values>({
+      defaultValues: {
+        name: ""
+      }
+    });
+    const Field = typedField(form);
+    return <Form form={form} onSubmit={handleOnSubmit}>
+        <Section>
+          <Field name="name">
+            <TextField value={form.watch("name")} onChange={val => {
+            form.setValue("name", val.toUpperCase());
+          }}>
+              <Label>Name</Label>
+            </TextField>
+          </Field>
+          <ActionGroup>
+            <Button type="submit">Submit</Button>
+          </ActionGroup>
+        </Section>
+      </Form>;
+  }
+}`,...(h=(d=a.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};const Ee=["Default","WithTransformedValue"];export{i as Default,a as WithTransformedValue,Ee as __namedExportsOrder,Ge as default};
