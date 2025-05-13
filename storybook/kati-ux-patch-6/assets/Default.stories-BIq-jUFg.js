5689646faa08aaf905bb4f07b12b79617df5f0e7
mittwald <>
2025-05-13 09:42:56 +0000

    Update from https://github.com/mittwald/flow/commit/978892a65ff1f0319c408b8dc29f6dcf94830204

---
+++ storybook/kati-ux-patch-6/assets/Default.stories-BIq-jUFg.js
@@ -0,0 +1,28 @@
+import{j as g}from"./jsx-runtime-D_zvdyIk.js";import"./index-F2Fta7eo.js";import{M as i}from"./Markdown-CblwgmII.js";import{a as k,s as b}from"./storyBackgrounds-GFSAi-dI.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./CodeBlock-BGwZYdwN.js";import"./clsx-B-dksMZM.js";import"./CopyButton-GWL5RdRd.js";import"./Button-BR8g0h2X.js";import"./PropsContextProvider-CLv7rqut.js";import"./mergeRefs-12BfVTtv.js";import"./index-GHXRqlzO.js";import"./index-FKzx4Va7.js";import"./IconWarning-DxEBU1uy.js";import"./Text-D1T0pLmS.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./Text-AN1gvCNp.js";import"./utils-D0arg4qg.js";import"./LoadingSpinner-CiN3VJkM.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./Button-DCaCfXLp.js";import"./ProgressBar-bhmU_ZvQ.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./Tooltip-Bn5mu-yh.js";import"./OverlayArrow-ULot_6Ir.js";import"./useOverlayTriggerState-Bt-3O924.js";import"./useControlledState-15AFXVxl.js";import"./react-children-utilities-OrI0BXp-.js";import"./Action-VzGiUi9p.js";import"./context-BvCJA7hU.js";import"./useStatic-DRfNlbL5.js";import"./getActionGroupSlot-DjaHqBmJ.js";import"./dynamic-DKDa4OpU.js";import"./onlyText-DT0buBi1.js";import"./Heading-BA94TxFr.js";import"./Heading-AP9WPpJD.js";import"./RSPContexts-DhZDYkwS.js";import"./InlineCode-fKVjoyB7.js";import"./Link-DLAQ92o0.js";import"./Separator-CE26_tOD.js";import"./Separator-YWiDMMN1.js";import"./CollectionBuilder-D5vZPvsR.js";const kr={title:"Content/Markdown",component:i,render:l=>g.jsx(i,{...l,children:`# Heading
+Lorem ipsum dolor sit amet **consectetur adipisicing** elit. Cumque eius \`quam quas vel voluptas\` ullam aliquid fugit.
+\`\`\`json
+{
+    "projectId": "b3a96db5-ba8f-40dd-9100-bab43ac1f698",
+    "name": "My Project"
+}
+\`\`\`
+[link](#)
+> Block quote
+ - Unordered list item 1 
+ - Unordered list item 2 
+ 1. Ordered list item 1 
+ 2. Ordered list item 2`})},r={},o={args:{color:"dark"},parameters:{backgrounds:k}},t={args:{color:"light"},parameters:{backgrounds:b}};var m,e,a;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var p,s,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var c,d,u;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const br=["Default","Dark","Light"];export{o as Dark,r as Default,t as Light,br as __namedExportsOrder,kr as default};
