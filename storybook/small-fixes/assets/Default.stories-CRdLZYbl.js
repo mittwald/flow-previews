aa11befd07b50d69ea3f9ea95f79aff2e45a595b
mittwald <>
2025-05-14 05:25:59 +0000

    Update from https://github.com/mittwald/flow/commit/dd1e21062f7fe37b11ff10cb4a5aba14603390a9

---
+++ storybook/small-fixes/assets/Default.stories-CRdLZYbl.js
@@ -0,0 +1,74 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{T as t}from"./Text-CaWyuoTz.js";import"./index-F2Fta7eo.js";import{S as M}from"./Section-Bfv1eGXQ.js";import{a as N,s as P}from"./storyBackgrounds-GFSAi-dI.js";import{I as u}from"./InlineCode-fKVjoyB7.js";import{I as h}from"./IconWarning-DxEBU1uy.js";import{d as x}from"./dummyText-CX_I_Wpl.js";import"./PropsContextProvider-CLv7rqut.js";import"./mergeRefs-12BfVTtv.js";import"./clsx-B-dksMZM.js";import"./index-GHXRqlzO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-FKzx4Va7.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./Text-AN1gvCNp.js";import"./utils-D0arg4qg.js";import"./ContextMenuSection-BTorvOQF.js";import"./Action-VzGiUi9p.js";import"./context-BvCJA7hU.js";import"./useStatic-DRfNlbL5.js";import"./getActionGroupSlot-DjaHqBmJ.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BBdTn5Uc.js";import"./Button-DCaCfXLp.js";import"./ProgressBar-bhmU_ZvQ.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./RSPContexts-DhZDYkwS.js";import"./OverlayArrow-ULot_6Ir.js";import"./Collection-CutN93xp.js";import"./CollectionBuilder-D5vZPvsR.js";import"./Separator-YWiDMMN1.js";import"./useOverlayTriggerState-Bt-3O924.js";import"./useControlledState-15AFXVxl.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";const Kr={title:"Content/Text",component:t,argTypes:{elementType:{control:"inline-radio",options:["span","p"],defaultValue:"span"}},args:{elementType:"span"},render:e=>r.jsx(M,{children:r.jsx(t,{...e,children:"Text is an unstyled component that can be used to display texts. By default it renders a span."})})},s={},o={render:e=>r.jsx(t,{...e,children:"Text without styling parent components"})},n={render:e=>r.jsxs(t,{...e,children:["Text with unordered list:",r.jsxs("ul",{children:[r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"})]})]})},a={render:e=>r.jsxs(t,{...e,children:["Text with ordered list:",r.jsxs("ol",{children:[r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"})]})]})},i={render:e=>r.jsx(t,{...e,children:r.jsx("small",{children:"Small Text"})})},p={args:{color:"dark"},parameters:{backgrounds:N}},m={args:{color:"light"},parameters:{backgrounds:P}},c={args:{align:"end"}},l={render:e=>r.jsxs(M,{children:[r.jsx(t,{children:r.jsxs("strong",{children:["Without specified ",r.jsx(u,{children:"wrap"}),":"]})}),r.jsx(t,{...e,align:"center",children:"This is a centered text in a small container so the text is wrapped."}),r.jsx(t,{children:r.jsxs("strong",{children:["With ",r.jsx(u,{children:"wrap: balance"}),":"]})}),r.jsx(t,{...e,align:"center",wrap:"balance",children:"This is a centered text in a small container so the text is wrapped."})]}),parameters:{viewport:{defaultViewport:"mobile2"}}},d={render:e=>r.jsxs(t,{...e,children:[r.jsx(h,{})," ",x.medium,r.jsxs("small",{children:[x.short," ",r.jsx(h,{})," ",x.short]})]})};var g,T,j;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(j=(T=s.parameters)==null?void 0:T.docs)==null?void 0:j.source}}};var I,w,S;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <Text {...props}>Text without styling parent components</Text>
+}`,...(S=(w=o.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};var f,y,k;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with unordered list:
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    </Text>
+}`,...(k=(y=n.parameters)==null?void 0:y.docs)==null?void 0:k.source}}};var b,L,W;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with ordered list:
+      <ol>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ol>
+    </Text>
+}`,...(W=(L=a.parameters)==null?void 0:L.docs)==null?void 0:W.source}}};var B,C,D;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <small>Small Text</small>
+    </Text>
+}`,...(D=(C=i.parameters)==null?void 0:C.docs)==null?void 0:D.source}}};var E,O,R;p.parameters={...p.parameters,docs:{...(E=p.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(R=(O=p.parameters)==null?void 0:O.docs)==null?void 0:R.source}}};var V,v,A;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(A=(v=m.parameters)==null?void 0:v.docs)==null?void 0:A.source}}};var U,_,q;c.parameters={...c.parameters,docs:{...(U=c.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    align: "end"
+  }
+}`,...(q=(_=c.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var z,F,G;l.parameters={...l.parameters,docs:{...(z=l.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        <strong>
+          Without specified <InlineCode>wrap</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+      <Text>
+        <strong>
+          With <InlineCode>wrap: balance</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center" wrap="balance">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2"
+    }
+  }
+}`,...(G=(F=l.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var H,J,K;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <IconStar /> {dummyText.medium}
+      <small>
+        {dummyText.short} <IconStar /> {dummyText.short}
+      </small>
+    </Text>
+}`,...(K=(J=d.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const Mr=["Default","RawText","UnorderedList","OrderedList","SmallText","Dark","Light","AlignEnd","WrapBalance","WithIcon"];export{c as AlignEnd,p as Dark,s as Default,m as Light,a as OrderedList,o as RawText,i as SmallText,n as UnorderedList,d as WithIcon,l as WrapBalance,Mr as __namedExportsOrder,Kr as default};
---
+++ storybook/small-fixes/assets/Default.stories-D1f6RM4A.js
@@ -0,0 +1,61 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{R as i,b as a,a as r}from"./RadioButton-CA2gyZ4B.js";import"./index-F2Fta7eo.js";import{L as n}from"./Label-BBMGsxZ-.js";import{a as W}from"./index-B-lxVbXh.js";import{F as k}from"./FieldError-BHK6dEuK.js";import{T as p}from"./Text-CaWyuoTz.js";import{C as R}from"./Content-BYRUKBcO.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CLv7rqut.js";import"./mergeRefs-12BfVTtv.js";import"./index-GHXRqlzO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-FKzx4Va7.js";import"./ColumnLayout-DZVPw614.js";import"./FormField.module-B9AzUSUD.js";import"./RadioGroup-DttNE3Rf.js";import"./utils-D0arg4qg.js";import"./FieldError-Cy41ow6l.js";import"./Text-AN1gvCNp.js";import"./filterDOMProps-CeZl_uWj.js";import"./Form-YK_aY0K2.js";import"./useLabel-DB0A2tZF.js";import"./Label-wLZ3I4V9.js";import"./Hidden-aaDrlwOM.js";import"./useFormReset-hDk0Ijlo.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-Tcmfb9SA.js";import"./useFormValidation-C9irvmUd.js";import"./FocusScope-BlLq5c4F.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./VisuallyHidden-LoYkyM-T.js";import"./useControlledState-15AFXVxl.js";import"./IconWarning-DxEBU1uy.js";import"./v4-CtRu48qb.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";const De={title:"Form Controls/RadioGroup",component:i,args:{onChange:W("onChange")},parameters:{controls:{exclude:["onChange"]}},render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(a,{value:"admin",children:"Admin"}),e.jsx(a,{value:"member",children:"Member"}),e.jsx(a,{value:"accountant",children:"Accountant"})]})},t={},d={args:{isDisabled:!0}},s={render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(a,{value:"admin",children:"Admin"}),e.jsx(a,{value:"member",isDisabled:!0,children:"Member"}),e.jsx(a,{value:"accountant",children:"Accountant"})]})},u={render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(r,{value:"admin",children:"Admin"}),e.jsx(r,{value:"member",children:"Member"}),e.jsx(r,{value:"accountant",children:"Accountant"})]})},m={render:o=>e.jsxs(i,{...o,defaultValue:"admin",children:[e.jsx(n,{children:"Role"}),e.jsx(r,{value:"admin",children:"Admin"}),e.jsx(r,{value:"member",isDisabled:!0,children:"Member"}),e.jsx(r,{value:"accountant",children:"Accountant"})]})},l={render:o=>e.jsxs(i,{...o,defaultValue:"domain","aria-label":"Domain",children:[e.jsxs(r,{value:"domain",children:[e.jsx(p,{children:"Book domain"}),e.jsx(R,{children:"Do you have a desired domain? No problem, we'll help you find the right domain for you."})]}),e.jsxs(r,{value:"virtualHost",children:[e.jsx(p,{children:"Add virtual host"}),e.jsx(R,{children:"The domain remains with your previous provider, but you can use it for your website in our mStudio."})]}),e.jsxs(r,{value:"subdomain",children:[e.jsx(p,{children:"Add subdomain"}),e.jsx(R,{children:"Create a subdomain from an existing domain to use for your project."})]})]})},c={render:o=>e.jsxs(i,{...o,isInvalid:!0,isRequired:!0,children:[e.jsx(n,{children:"Role"}),e.jsx(a,{value:"admin",children:"Admin"}),e.jsx(a,{value:"member",children:"Member"}),e.jsx(a,{value:"accountant",children:"Accountant"}),e.jsx(k,{children:"Select a role to continue"})]})};var h,b,x;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:"{}",...(x=(b=t.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var v,j,f;d.parameters={...d.parameters,docs:{...(v=d.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(f=(j=d.parameters)==null?void 0:j.docs)==null?void 0:f.source}}};var B,A,D;s.parameters={...s.parameters,docs:{...(B=s.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="admin">
+      <Label>Role</Label>
+      <Radio value="admin">Admin</Radio>
+      <Radio value="member" isDisabled>
+        Member
+      </Radio>
+      <Radio value="accountant">Accountant</Radio>
+    </RadioGroup>
+}`,...(D=(A=s.parameters)==null?void 0:A.docs)==null?void 0:D.source}}};var g,C,y;u.parameters={...u.parameters,docs:{...(g=u.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="admin">
+      <Label>Role</Label>
+      <RadioButton value="admin">Admin</RadioButton>
+      <RadioButton value="member">Member</RadioButton>
+      <RadioButton value="accountant">Accountant</RadioButton>
+    </RadioGroup>
+}`,...(y=(C=u.parameters)==null?void 0:C.docs)==null?void 0:y.source}}};var G,S,L;m.parameters={...m.parameters,docs:{...(G=m.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="admin">
+      <Label>Role</Label>
+      <RadioButton value="admin">Admin</RadioButton>
+      <RadioButton value="member" isDisabled>
+        Member
+      </RadioButton>
+      <RadioButton value="accountant">Accountant</RadioButton>
+    </RadioGroup>
+}`,...(L=(S=m.parameters)==null?void 0:S.docs)==null?void 0:L.source}}};var T,M,V;l.parameters={...l.parameters,docs:{...(T=l.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} defaultValue="domain" aria-label="Domain">
+      <RadioButton value="domain">
+        <Text>Book domain</Text>
+        <Content>
+          Do you have a desired domain? No problem, we'll help you find the
+          right domain for you.
+        </Content>
+      </RadioButton>
+      <RadioButton value="virtualHost">
+        <Text>Add virtual host</Text>
+        <Content>
+          The domain remains with your previous provider, but you can use it for
+          your website in our mStudio.
+        </Content>
+      </RadioButton>
+      <RadioButton value="subdomain">
+        <Text>Add subdomain</Text>
+        <Content>
+          Create a subdomain from an existing domain to use for your project.
+        </Content>
+      </RadioButton>
+    </RadioGroup>
+}`,...(V=(M=l.parameters)==null?void 0:M.docs)==null?void 0:V.source}}};var E,F,w;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <RadioGroup {...props} isInvalid isRequired>
+      <Label>Role</Label>
+      <Radio value="admin">Admin</Radio>
+      <Radio value="member">Member</Radio>
+      <Radio value="accountant">Accountant</Radio>
+      <FieldError>Select a role to continue</FieldError>
+    </RadioGroup>
+}`,...(w=(F=c.parameters)==null?void 0:F.docs)==null?void 0:w.source}}};const ge=["Default","RadioGroupDisabled","RadioDisabled","RadioButtons","RadioButtonDisabled","RadioButtonsWithContent","WithFieldError"];export{t as Default,m as RadioButtonDisabled,u as RadioButtons,l as RadioButtonsWithContent,s as RadioDisabled,d as RadioGroupDisabled,c as WithFieldError,ge as __namedExportsOrder,De as default};
