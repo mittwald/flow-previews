881715d5615d9e5c4c0bf69148ae9f464353df7f
mittwald <>
2025-05-19 11:33:11 +0000

    Update from https://github.com/mittwald/flow/commit/c669588facbdea7c00b4d873072fe4a18770bf55

---
+++ storybook/Jan-Eimertenbrink-patch-1/assets/Default.stories-DKfdlmAZ.js
@@ -0,0 +1,14 @@
+import{j as C}from"./jsx-runtime-D_zvdyIk.js";import{C as s}from"./Checkbox-Cny6_SCJ.js";import"./index-CgfFrydU.js";import{a as S}from"./index-B-lxVbXh.js";import"./IconWarning-C_dXgkkz.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Ijz6GgOq.js";import"./mergeRefs-78-678FT.js";import"./index-sY8i7rw0.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-B2KKL2uI.js";import"./RSPContexts-9nj2DFX_.js";import"./utils-CEW6KzsL.js";import"./FieldError-CSoyHznZ.js";import"./Text-H2FrnJWx.js";import"./filterDOMProps-CeZl_uWj.js";import"./Form-Ce06eXuO.js";import"./useLabel-Bwhd-XGR.js";import"./Label-i46snhnj.js";import"./Hidden-DwT0szmK.js";import"./useFormValidation-Ck8Kc0sM.js";import"./useFocus-DQVi-kHM.js";import"./usePress-CcZGRUOg.js";import"./useFocusRing-B_JVXUf7.js";import"./useToggleState-DZsZloGE.js";import"./useFormReset-Cri5RZCQ.js";import"./useFocusable-D2JRVW0s.js";import"./useControlledState-DjePt-lg.js";import"./VisuallyHidden-Drr3U20F.js";import"./v4-CtRu48qb.js";const X={title:"Form Controls/Checkbox",component:s,args:{onChange:S("onChange")},parameters:{controls:{exclude:["onChange"]}},render:x=>C.jsx(s,{...x,children:"Consent to terms and conditions"})},r={},e={args:{isDisabled:!0}},t={args:{isIndeterminate:!0}},o={args:{isDisabled:!0,isSelected:!0}};var a,i,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var n,p,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,u,l;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    isIndeterminate: true
+  }
+}`,...(l=(u=t.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var g,b,D;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    isSelected: true
+  }
+}`,...(D=(b=o.parameters)==null?void 0:b.docs)==null?void 0:D.source}}};const Y=["Default","Disabled","Indeterminate","DisabledSelected"];export{r as Default,e as Disabled,o as DisabledSelected,t as Indeterminate,Y as __namedExportsOrder,X as default};
