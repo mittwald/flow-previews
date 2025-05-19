a330fde634def7fe288e97e6c08b8d7fa4821ba7
mittwald <>
2025-05-19 08:38:40 +0000

    Update from https://github.com/mittwald/flow/commit/3d45419452ba4b052b52a66598abeb4ae3f12091

---
+++ storybook/support-uncontrolled-fields/assets/Default.stories-DPtzTxeg.js
@@ -0,0 +1,103 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{C as i}from"./ColumnLayout-I8_MRzzX.js";import"./index-F2Fta7eo.js";import{T as r}from"./TextField-DjHFfmzf.js";import{L as n}from"./Label-bOVntHUy.js";import{A as l}from"./Alert-Cyr8PoLj.js";import{H as s}from"./Heading-BP-DEnDA.js";import{S as x}from"./Section-D1p7ntAU.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CGP4rIIM.js";import"./mergeRefs-12BfVTtv.js";import"./index-GHXRqlzO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-FKzx4Va7.js";import"./TextFieldBase-fQaefIQS.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-B7WE-msJ.js";import"./IconWarning-JHNVKD2U.js";import"./FieldError-BIIkw2ft.js";import"./utils-DCmqzjri.js";import"./Text-B6xPbVJ8.js";import"./filterDOMProps-CeZl_uWj.js";import"./FieldDescription-ujFz9pWL.js";import"./Text-C0sWdp1H.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./Form-YK_aY0K2.js";import"./useLabel-DB0A2tZF.js";import"./Input-Cy89Xo-s.js";import"./Hidden-aaDrlwOM.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocus-CPhYqd-d.js";import"./Label-DKaTUQz-.js";import"./useTextField-BbIZEqTu.js";import"./useFormReset-hDk0Ijlo.js";import"./useControlledState-15AFXVxl.js";import"./useFocusable-BlNB76NP.js";import"./useFormValidation-C9irvmUd.js";import"./ReactAriaControlledValueFix-2mXEZviY.js";import"./AlertIcon-C29Z4cTI.js";import"./dynamic-DKDa4OpU.js";import"./Heading-CugmPflL.js";import"./RSPContexts-DhZDYkwS.js";import"./ContextMenuSection-Cu2bLHsG.js";import"./Action-CXU1vJUm.js";import"./context-Df4xkzhD.js";import"./useStatic-CeLwg7Tb.js";import"./getActionGroupSlot-Cwkd5BAj.js";import"./Dialog-7H2cTJka.js";import"./Button-CEdwHtJW.js";import"./ProgressBar-BxwrPIRf.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./OverlayArrow-NIVDv4-Z.js";import"./Collection-CW2dxbCi.js";import"./CollectionBuilder-DuMx_c7J.js";import"./Separator-DdlpIzj5.js";import"./useOverlayTriggerState-D0DLvnhT.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";const qe={title:"Structure/ColumnLayout",component:i,parameters:{controls:{exclude:["s","m","l","className"]}},render:t=>e.jsxs(x,{children:[e.jsx(l,{children:e.jsx(s,{children:"s: [1] (default), m: [1, 1] (default), l: [1, 1, 1] (default)"})}),e.jsxs(i,{...t,children:[e.jsx(r,{children:e.jsx(n,{children:"First name"})}),e.jsx(r,{children:e.jsx(n,{children:"Last name"})}),e.jsx(r,{children:e.jsx(n,{children:"Street"})}),e.jsx(r,{children:e.jsx(n,{children:"House number"})})]})]})},a={},o={render:t=>e.jsxs(x,{children:[e.jsx(l,{children:e.jsx(s,{children:"s: [1] (default), m: [2, 1], l: [2, 1] (inherited)"})}),e.jsxs(i,{...t,m:[2,1],children:[e.jsx(r,{children:e.jsx(n,{children:"Street"})}),e.jsx(r,{children:e.jsx(n,{children:"House number"})})]})]})},d={render:t=>e.jsxs(x,{children:[e.jsx(l,{children:e.jsx(s,{children:"s: [2, 1], m: [3, 2, 1], l: [4, 3, 2, 1]"})}),e.jsxs(i,{...t,s:[2,1],m:[3,2,1],l:[4,3,2,1],children:[e.jsx(r,{children:e.jsx(n,{children:"First name"})}),e.jsx(r,{children:e.jsx(n,{children:"Last name"})}),e.jsx(r,{children:e.jsx(n,{children:"Street"})})]})]})},m={render:t=>e.jsxs(x,{children:[e.jsx(l,{children:e.jsx(s,{children:'Gap size: "s"'})}),e.jsxs(i,{...t,gap:"s",children:[e.jsx(r,{children:e.jsx(n,{children:"First name"})}),e.jsx(r,{children:e.jsx(n,{children:"Last name"})}),e.jsx(r,{children:e.jsx(n,{children:"Street"})}),e.jsx(r,{children:e.jsx(n,{children:"House number"})})]}),e.jsx(l,{children:e.jsx(s,{children:'Gap size: "m"'})}),e.jsxs(i,{...t,gap:"m",children:[e.jsx(r,{children:e.jsx(n,{children:"First name"})}),e.jsx(r,{children:e.jsx(n,{children:"Last name"})}),e.jsx(r,{children:e.jsx(n,{children:"Street"})}),e.jsx(r,{children:e.jsx(n,{children:"House number"})})]}),e.jsx(l,{children:e.jsx(s,{children:'Gap size: "l"'})}),e.jsxs(i,{...t,gap:"l",children:[e.jsx(r,{children:e.jsx(n,{children:"First name"})}),e.jsx(r,{children:e.jsx(n,{children:"Last name"})}),e.jsx(r,{children:e.jsx(n,{children:"Street"})}),e.jsx(r,{children:e.jsx(n,{children:"House number"})})]})]})},c={render:t=>e.jsxs(i,{...t,l:[1,1],children:[e.jsx(r,{children:e.jsx(n,{children:"First name"})}),e.jsx(r,{children:e.jsx(n,{children:"Last name"})}),e.jsxs(i,{children:[e.jsx(r,{children:e.jsx(n,{children:"Street"})}),e.jsx(r,{children:e.jsx(n,{children:"House number"})})]})]})};var p,u,h;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(h=(u=a.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var j,L,b;o.parameters={...o.parameters,docs:{...(j=o.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Alert>
+        <Heading>s: [1] (default), m: [2, 1], l: [2, 1] (inherited)</Heading>
+      </Alert>
+      <ColumnLayout {...props} m={[2, 1]}>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(b=(L=o.parameters)==null?void 0:L.docs)==null?void 0:b.source}}};var F,T,S;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Alert>
+        <Heading>s: [2, 1], m: [3, 2, 1], l: [4, 3, 2, 1]</Heading>
+      </Alert>
+      <ColumnLayout {...props} s={[2, 1]} m={[3, 2, 1]} l={[4, 3, 2, 1]}>
+        <TextField>
+          <Label>First name</Label>
+        </TextField>
+        <TextField>
+          <Label>Last name</Label>
+        </TextField>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(S=(T=d.parameters)==null?void 0:T.docs)==null?void 0:S.source}}};var H,g,C;m.parameters={...m.parameters,docs:{...(H=m.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Alert>
+        <Heading>Gap size: "s"</Heading>
+      </Alert>
+      <ColumnLayout {...props} gap="s">
+        <TextField>
+          <Label>First name</Label>
+        </TextField>
+        <TextField>
+          <Label>Last name</Label>
+        </TextField>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+      <Alert>
+        <Heading>Gap size: "m"</Heading>
+      </Alert>
+      <ColumnLayout {...props} gap="m">
+        <TextField>
+          <Label>First name</Label>
+        </TextField>
+        <TextField>
+          <Label>Last name</Label>
+        </TextField>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+      <Alert>
+        <Heading>Gap size: "l"</Heading>
+      </Alert>
+      <ColumnLayout {...props} gap="l">
+        <TextField>
+          <Label>First name</Label>
+        </TextField>
+        <TextField>
+          <Label>Last name</Label>
+        </TextField>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </Section>
+}`,...(C=(g=m.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var y,f,A;c.parameters={...c.parameters,docs:{...(y=c.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <ColumnLayout {...props} l={[1, 1]}>
+      <TextField>
+        <Label>First name</Label>
+      </TextField>
+      <TextField>
+        <Label>Last name</Label>
+      </TextField>
+      <ColumnLayout>
+        <TextField>
+          <Label>Street</Label>
+        </TextField>
+        <TextField>
+          <Label>House number</Label>
+        </TextField>
+      </ColumnLayout>
+    </ColumnLayout>
+}`,...(A=(f=c.parameters)==null?void 0:f.docs)==null?void 0:A.source}}};const ve=["Default","CustomValueM","AllCustomValues","Gaps","Nested"];export{d as AllCustomValues,o as CustomValueM,a as Default,m as Gaps,c as Nested,ve as __namedExportsOrder,qe as default};
---
+++ storybook/support-uncontrolled-fields/assets/TextField.stories-Dym_EWCT.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as j}from"./ActionGroup-Ctzl3N1i.js";import{B as p}from"./Button-BLGNEMW7.js";import{L as o}from"./Label-bOVntHUy.js";import{S as f}from"./Section-D1p7ntAU.js";import{T as m}from"./TextField-DjHFfmzf.js";import{a as N,u as b,F,t as S}from"./Form-BL6CflcR.js";import{s as g}from"./Action-CXU1vJUm.js";import{a as y}from"./index-B-lxVbXh.js";import"./index-F2Fta7eo.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./PropsContextProvider-CGP4rIIM.js";import"./mergeRefs-12BfVTtv.js";import"./clsx-B-dksMZM.js";import"./index-GHXRqlzO.js";import"./index-FKzx4Va7.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-Cwkd5BAj.js";import"./useStatic-CeLwg7Tb.js";import"./IconWarning-JHNVKD2U.js";import"./Text-C0sWdp1H.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DovK0xj5.js";import"./Text-B6xPbVJ8.js";import"./utils-DCmqzjri.js";import"./LoadingSpinner-IrCiETzR.js";import"./useLocalizedStringFormatter-QhihLYa3.js";import"./Button-CEdwHtJW.js";import"./ProgressBar-BxwrPIRf.js";import"./Label-DKaTUQz-.js";import"./Hidden-aaDrlwOM.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DB0A2tZF.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-heCNvahi.js";import"./useFocus-CPhYqd-d.js";import"./useFocusRing-4oM8aI2c.js";import"./useFocusable-BlNB76NP.js";import"./ContextMenuSection-Cu2bLHsG.js";import"./Dialog-7H2cTJka.js";import"./RSPContexts-DhZDYkwS.js";import"./OverlayArrow-NIVDv4-Z.js";import"./Collection-CW2dxbCi.js";import"./CollectionBuilder-DuMx_c7J.js";import"./Separator-DdlpIzj5.js";import"./useOverlayTriggerState-D0DLvnhT.js";import"./useControlledState-15AFXVxl.js";import"./SelectionManager-1l7b-eWb.js";import"./useEvent-BL4Dc6Ef.js";import"./useCollator-4BrhQKp8.js";import"./FocusScope-BlLq5c4F.js";import"./VisuallyHidden-LoYkyM-T.js";import"./TextFieldBase-fQaefIQS.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-B7WE-msJ.js";import"./FieldError-BIIkw2ft.js";import"./FieldDescription-ujFz9pWL.js";import"./Form-YK_aY0K2.js";import"./Input-Cy89Xo-s.js";import"./useTextField-BbIZEqTu.js";import"./useFormReset-hDk0Ijlo.js";import"./useFormValidation-C9irvmUd.js";import"./ReactAriaControlledValueFix-2mXEZviY.js";import"./context-Df4xkzhD.js";import"./v4-CtRu48qb.js";const V=y("submit"),Ue={title:"Integrations/React Hook Form/TextField",component:N,render:()=>{const s=async n=>{await g(1500),V(n)},t=b({defaultValues:{name:"",nameDefaultValue:"John",nameRequired:"",nameMaxLength:"",controlledName:""}}),r=S();return e.jsx(F,{form:t,onSubmit:s,children:e.jsxs(f,{children:[e.jsx(r,{name:"name",children:e.jsx(m,{children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"nameDefaultValue",children:e.jsx(m,{children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"nameRequired",rules:{required:"Please enter your name"},children:e.jsx(m,{children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"nameMaxLength",children:e.jsx(m,{maxLength:10,showCharacterCount:!0,children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"controlledName",children:e.jsx(m,{value:t.watch("controlledName").toUpperCase(),children:e.jsx(o,{children:"Name"})})}),e.jsxs(j,{children:[e.jsx(p,{type:"reset",children:"Reset"}),e.jsx(p,{type:"submit",children:"Submit"})]})]})})}},i={},a={render:()=>{const s=n=>{V(n)},t=b({defaultValues:{name:""}}),r=S();return e.jsx(F,{form:t,onSubmit:s,children:e.jsxs(f,{children:[e.jsx(r,{name:"name",children:e.jsx(m,{value:t.watch("name"),onChange:n=>{t.setValue("name",n.toUpperCase())},children:e.jsx(o,{children:"Name"})})}),e.jsx(j,{children:e.jsx(p,{type:"submit",children:"Submit"})})]})})}};var l,c,d;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(d=(c=i.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,h,x;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => {
+    interface Values {
+      name: string;
+    }
+    const handleOnSubmit = (values: Values) => {
+      submitAction(values);
+    };
+    const form = useForm<Values>({
+      defaultValues: {
+        name: ""
+      }
+    });
+    const Field = typedField(form);
+    return <Form form={form} onSubmit={handleOnSubmit}>
+        <Section>
+          <Field name="name">
+            <TextField value={form.watch("name")} onChange={val => {
+            form.setValue("name", val.toUpperCase());
+          }}>
+              <Label>Name</Label>
+            </TextField>
+          </Field>
+          <ActionGroup>
+            <Button type="submit">Submit</Button>
+          </ActionGroup>
+        </Section>
+      </Form>;
+  }
+}`,...(x=(h=a.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const Ee=["Default","WithTransformedValue"];export{i as Default,a as WithTransformedValue,Ee as __namedExportsOrder,Ue as default};
