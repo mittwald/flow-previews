1f78563b336876f18be76b612b366bab02d1cc8a
mittwald <>
2025-05-20 14:05:55 +0000

    Cleanup merged Storybook previews

--- storybook/1364-addition-of-actiongroup-in-the-tile/assets/ButtonView-DDadKIF4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-CgfFrydU.js";import{B as n}from"./Button-C5D3ZbLG.js";import{v as s}from"./PropsContextProvider-Ijz6GgOq.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
