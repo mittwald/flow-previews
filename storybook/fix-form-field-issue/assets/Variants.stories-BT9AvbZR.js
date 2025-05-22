46d8d868d55741d739670d4d4e7c59ca2b68e961
mittwald <>
2025-05-22 07:18:18 +0000

    Update from https://github.com/mittwald/flow/commit/297203e4618f88b19f393968b4399b2b1ac12307

---
+++ storybook/fix-form-field-issue/assets/Variants.stories-BT9AvbZR.js
@@ -0,0 +1,19 @@
+import f from"./Default.stories-DArxal3z.js";import"./jsx-runtime-D_zvdyIk.js";import"./ProgressBar-BewxFgEc.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-yY6RzzUT.js";import"./mergeRefs-_uxeg_17.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";import"./ProgressBar-DuZjZRB2.js";import"./utils-DvYDvJ-W.js";import"./Label-XuXA-row.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DP9zv8XI.js";import"./useLocalizedStringFormatter-jDgQNJem.js";import"./NumberFormatter-DNR9MAW-.js";import"./Legend-C-qtt0LZ.js";import"./Text-9PUJLXl5.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-KnQ_Srvn.js";import"./Text-1CJ30n_Q.js";import"./Label-Bp6Pnofu.js";const H={...f,title:"Status/ProgressBar/Variants"},r={args:{status:"info"}},s={args:{status:"success"}},a={args:{status:"warning",value:800}},t={args:{status:"danger",value:950}};var o,e,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    status: "info"
+  }
+}`,...(n=(e=r.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};var m,p,i;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  }
+}`,...(i=(p=s.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var c,u,g;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    status: "warning",
+    value: 800
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,l,S;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "danger",
+    value: 950
+  }
+}`,...(S=(l=t.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};const J=["Info","Success","Warning","Danger"];export{t as Danger,r as Info,s as Success,a as Warning,J as __namedExportsOrder,H as default};
