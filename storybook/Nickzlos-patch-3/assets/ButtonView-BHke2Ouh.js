95af5d43a08315a6cd55d8ac26b1f4d647d7ff49
mittwald <>
2025-05-22 08:10:16 +0000

    Update from https://github.com/mittwald/flow/commit/c3d9b353ce244cbcd9f515ac72c62e23ae85d95e

---
+++ storybook/Nickzlos-patch-3/assets/ButtonView-BHke2Ouh.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-CgfFrydU.js";import{B as n}from"./Button-JVF-Pk0m.js";import{v as s}from"./PropsContextProvider-Ijz6GgOq.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
---
+++ storybook/Nickzlos-patch-3/assets/Variants.stories-CZFY2eC_.js
@@ -0,0 +1,31 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-CgfFrydU.js";import{H as a}from"./Heading-vWhd3mBG.js";import b from"./Default.stories-LRYzE-FW.js";import{N as n}from"./Notification-C0ybHRDC.js";import{T as p}from"./Text-CZf9tped.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Ijz6GgOq.js";import"./mergeRefs-78-678FT.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";import"./Heading-BtmHxYgZ.js";import"./RSPContexts-9nj2DFX_.js";import"./utils-CEW6KzsL.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./AlertIcon-DH9jLJc0.js";import"./IconWarning-BvNClYx6.js";import"./useLocalizedStringFormatter-P5-5MQWA.js";import"./Button-JVF-Pk0m.js";import"./LoadingSpinner-BQsipT6D.js";import"./Button-DbeMfWvm.js";import"./ProgressBar-BwuCIevz.js";import"./Label-i46snhnj.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-Bwhd-XGR.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-CcZGRUOg.js";import"./useFocus-DQVi-kHM.js";import"./useFocusRing-B_JVXUf7.js";import"./useFocusable-D2JRVW0s.js";import"./Link-CVILd8AC.js";import"./dynamic-DKDa4OpU.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-KnQ_Srvn.js";import"./Text-H2FrnJWx.js";const nr={...b,title:"Status/Notifications/Notification/Status"},t={},o={args:{status:"warning"},render:e=>r.jsxs(n,{...e,children:[r.jsx(a,{children:"Storage almost exceeded"}),r.jsxs(p,{children:["The storage in your project ",r.jsx("b",{children:"My Project"})," is over 80% utilized."]})]})},i={args:{status:"danger"},render:e=>r.jsxs(n,{...e,children:[r.jsx(a,{children:"No SSL certificate"}),r.jsxs(p,{children:["No SSL certificate could be issued for ",r.jsx("b",{children:"example.de"}),"."]})]})},s={args:{status:"success"},render:e=>r.jsxs(n,{...e,children:[r.jsx(a,{children:"App installation finished"}),r.jsxs(p,{children:["Your app ",r.jsx("b",{children:"My WordPress"})," has been installed."]})]})};var c,m,d;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(m=t.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var u,l,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    status: "warning"
+  },
+  render: props => <Notification {...props}>
+      <Heading>Storage almost exceeded</Heading>
+      <Text>
+        The storage in your project <b>My Project</b> is over 80% utilized.
+      </Text>
+    </Notification>
+}`,...(g=(l=o.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var x,f,h;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    status: "danger"
+  },
+  render: props => <Notification {...props}>
+      <Heading>No SSL certificate</Heading>
+      <Text>
+        No SSL certificate could be issued for <b>example.de</b>.
+      </Text>
+    </Notification>
+}`,...(h=(f=i.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var j,S,N;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  },
+  render: props => <Notification {...props}>
+      <Heading>App installation finished</Heading>
+      <Text>
+        Your app <b>My WordPress</b> has been installed.
+      </Text>
+    </Notification>
+}`,...(N=(S=s.parameters)==null?void 0:S.docs)==null?void 0:N.source}}};const pr=["Info","Warning","Danger","Success"];export{i as Danger,t as Info,s as Success,o as Warning,pr as __namedExportsOrder,nr as default};
