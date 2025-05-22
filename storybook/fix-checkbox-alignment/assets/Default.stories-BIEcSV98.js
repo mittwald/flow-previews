d459b16d437f4d6dfa256f2b8c30c9e13c52a0fd
mittwald <>
2025-05-22 11:17:33 +0000

    Update from https://github.com/mittwald/flow/commit/4557d3c3fb0246491e6652b41960191c4833f22c

---
+++ storybook/fix-checkbox-alignment/assets/Default.stories-BIEcSV98.js
@@ -0,0 +1,54 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as Q}from"./Button-CT6jKELJ.js";import{F as H}from"./FieldDescription-DiuoPF4X.js";import{F as X}from"./FieldError-00yIk5F2.js";import{L as o}from"./Label-Bp6Pnofu.js";import{u as Y,F as Z}from"./Form-BHGiHopX.js";import{s as $}from"./Action-BASfP4Hw.js";import{a as J}from"./index-B-lxVbXh.js";import{r as ee}from"./index-CgfFrydU.js";import{T as t}from"./TextField-BUX7IupV.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-yY6RzzUT.js";import"./mergeRefs-_uxeg_17.js";import"./index-sY8i7rw0.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-B2KKL2uI.js";import"./IconWarning-70jApbHx.js";import"./Text-9PUJLXl5.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-KnQ_Srvn.js";import"./Text-1CJ30n_Q.js";import"./utils-DvYDvJ-W.js";import"./LoadingSpinner-BHQSc97o.js";import"./useLocalizedStringFormatter-jDgQNJem.js";import"./Button-CY6niT9f.js";import"./ProgressBar-DuZjZRB2.js";import"./Label-XuXA-row.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DP9zv8XI.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-rEv_nV0O.js";import"./useFocus-bBgo0bKd.js";import"./useFocusRing-DUlPoSa4.js";import"./useFocusable-fU6dY-gF.js";import"./FieldError-DQg1rr6h.js";import"./dynamic-DKDa4OpU.js";import"./context-DnKpt6Zm.js";import"./useStatic-CLm-J0DW.js";import"./getActionGroupSlot-BoNoL21H.js";import"./v4-CtRu48qb.js";import"./TextFieldBase-GVMdq-tN.js";import"./FormField.module-B9AzUSUD.js";import"./Form-eodUc7lY.js";import"./Input-Bwq1TeOx.js";import"./useTextField-CfbNrntM.js";import"./useFormReset-BiMHJINb.js";import"./useControlledState-DjePt-lg.js";import"./useFormValidation-9a9nu2yD.js";import"./ReactAriaControlledValueFix-CzEct_qc.js";const Ze={title:"Form Controls/TextField",component:t,render:r=>e.jsx(t,{onChange:J("onChange"),...r,children:e.jsx(o,{children:"First name"})})},s={},a={args:{isDisabled:!0}},i={args:{isRequired:!0}},n={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"URL"}),e.jsx(H,{children:'Start with "https://"'})]})},p={render:r=>e.jsx(t,{...r,defaultValue:"https://mittwald.de",children:e.jsx(o,{children:"URL"})})},l={render:r=>{const[h,K]=ee.useState("");return e.jsx(t,{...r,value:h,onChange:N=>K(N.toUpperCase()),children:e.jsx(o,{children:"URL"})})}},m={render:r=>e.jsx(t,{...r,placeholder:"https://",children:e.jsx(o,{children:"URL"})})},d={render:r=>e.jsxs(t,{...r,isInvalid:!0,defaultValue:"hello",children:[e.jsx(o,{children:"URL"}),e.jsx(X,{children:"Invalid input"}),e.jsx(H,{children:'Start with "https://"'})]})},c={render:r=>{const h=Y();return e.jsxs(Z,{form:h,onSubmit:async()=>await $(2e3),children:[e.jsx(t,{...r,type:"email",inputMode:"email",isRequired:!0,children:e.jsx(o,{children:"Email"})}),e.jsx("br",{}),e.jsx(Q,{type:"submit",children:"Submit"})]})}},u={args:{showCharacterCount:!0,maxLength:10},render:r=>e.jsx(t,{onChange:J("onChange"),...r,children:e.jsx(o,{children:"User name"})})};var x,F,L;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:"{}",...(L=(F=s.parameters)==null?void 0:F.docs)==null?void 0:L.source}}};var b,g,j;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(j=(g=a.parameters)==null?void 0:g.docs)==null?void 0:j.source}}};var f,S,C;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(C=(S=i.parameters)==null?void 0:S.docs)==null?void 0:C.source}}};var R,T,D;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: props => <TextField {...props}>
+      <Label>URL</Label>
+      <FieldDescription>Start with "https://"</FieldDescription>
+    </TextField>
+}`,...(D=(T=n.parameters)==null?void 0:T.docs)==null?void 0:D.source}}};var U,w,W;p.parameters={...p.parameters,docs:{...(U=p.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: props => <TextField {...props} defaultValue="https://mittwald.de">
+      <Label>URL</Label>
+    </TextField>
+}`,...(W=(w=p.parameters)==null?void 0:w.docs)==null?void 0:W.source}}};var V,v,E;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: props => {
+    const [value, setValue] = useState("");
+    return <TextField {...props} value={value} onChange={val => setValue(val.toUpperCase())}>
+        <Label>URL</Label>
+      </TextField>;
+  }
+}`,...(E=(v=l.parameters)==null?void 0:v.docs)==null?void 0:E.source}}};var q,y,B;m.parameters={...m.parameters,docs:{...(q=m.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: props => <TextField {...props} placeholder="https://">
+      <Label>URL</Label>
+    </TextField>
+}`,...(B=(y=m.parameters)==null?void 0:y.docs)==null?void 0:B.source}}};var I,M,P;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <TextField {...props} isInvalid defaultValue="hello">
+      <Label>URL</Label>
+      <FieldError>Invalid input</FieldError>
+      <FieldDescription>Start with "https://"</FieldDescription>
+    </TextField>
+}`,...(P=(M=d.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var _,O,k;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Form form={form} onSubmit={async () => await sleep(2000)}>
+        <TextField {...props} type="email" inputMode="email" isRequired>
+          <Label>Email</Label>
+        </TextField>
+        <br />
+        <Button type="submit">Submit</Button>
+      </Form>;
+  }
+}`,...(k=(O=c.parameters)==null?void 0:O.docs)==null?void 0:k.source}}};var z,A,G;u.parameters={...u.parameters,docs:{...(z=u.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    showCharacterCount: true,
+    maxLength: 10
+  },
+  render: props => <TextField onChange={action("onChange")} {...props}>
+      <Label>User name</Label>
+    </TextField>
+}`,...(G=(A=u.parameters)==null?void 0:A.docs)==null?void 0:G.source}}};const $e=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithControlledValue","WithPlaceholder","WithFieldError","WithForm","ShowCharacterCount"];export{s as Default,a as Disabled,i as Required,u as ShowCharacterCount,l as WithControlledValue,p as WithDefaultValue,n as WithFieldDescription,d as WithFieldError,c as WithForm,m as WithPlaceholder,$e as __namedExportsOrder,Ze as default};
