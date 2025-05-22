2e7bbba5acaf151160f5574bf508fc42cc147219
mittwald <>
2025-05-22 11:24:31 +0000

    Update from https://github.com/mittwald/flow/commit/9654cc4bd6678448e4607e5520a4e690e30a05e9

---
+++ storybook-main/assets/Levels.stories-YTGUFKXc.js
@@ -0,0 +1,25 @@
+import{H as O}from"./Heading-bJ8ZWrvE.js";import $ from"./Default.stories-BM2Ak6Vz.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-yY6RzzUT.js";import"./mergeRefs-_uxeg_17.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";import"./Heading-BFHXViOZ.js";import"./RSPContexts-9nj2DFX_.js";import"./utils-DvYDvJ-W.js";import"./IconWarning-70jApbHx.js";import"./storyBackgrounds-GFSAi-dI.js";import"./Section-Coqp9vad.js";import"./ContextMenuSection-CNNj2Vix.js";import"./Action-BASfP4Hw.js";import"./context-DnKpt6Zm.js";import"./useStatic-CLm-J0DW.js";import"./browser-B1A6F7LW.js";import"./getActionGroupSlot-BoNoL21H.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-WcTWvLN6.js";import"./Button-CY6niT9f.js";import"./ProgressBar-DuZjZRB2.js";import"./Label-XuXA-row.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DP9zv8XI.js";import"./useLocalizedStringFormatter-jDgQNJem.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-rEv_nV0O.js";import"./useFocus-bBgo0bKd.js";import"./useFocusRing-DUlPoSa4.js";import"./useFocusable-fU6dY-gF.js";import"./OverlayArrow-C1erJO4A.js";import"./useControlledState-DjePt-lg.js";import"./Collection-i8EjBJSq.js";import"./CollectionBuilder-Dw9MTGx5.js";import"./Separator-CUf66PuO.js";import"./Text-1CJ30n_Q.js";import"./SelectionManager-CTcRB2Ps.js";import"./useEvent-DsDOr2ug.js";import"./useCollator-Cym9_BaK.js";import"./FocusScope-BCiwkv3e.js";import"./VisuallyHidden-B4AzoGqn.js";const _r={...$,title:"Content/Heading/Levels",component:O},r={args:{level:1}},o={args:{level:2}},e={args:{level:3}},t={args:{level:4}},m={args:{level:5}},s={args:{level:6}};var p,a,i;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    level: 1
+  }
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var c,n,l;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    level: 2
+  }
+}`,...(l=(n=o.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var d,g,u;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    level: 3
+  }
+}`,...(u=(g=e.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var H,v,S;t.parameters={...t.parameters,docs:{...(H=t.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    level: 4
+  }
+}`,...(S=(v=t.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var f,x,_;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    level: 5
+  }
+}`,...(_=(x=m.parameters)==null?void 0:x.docs)==null?void 0:_.source}}};var C,E,L;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    level: 6
+  }
+}`,...(L=(E=s.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};const Cr=["H1","H2","H3","H4","H5","H6"];export{r as H1,o as H2,e as H3,t as H4,m as H5,s as H6,Cr as __namedExportsOrder,_r as default};
