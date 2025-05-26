5d187c274ff49d056538fbe8f9b4928237ee9be0
mittwald <>
2025-05-26 06:35:40 +0000

    Cleanup merged Storybook previews

--- storybook/switch-index-caro/assets/useStatic-Dvh5S8aV.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./PropsContextProvider-Ijz6GgOq.js";import{r as e}from"./index-CgfFrydU.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
