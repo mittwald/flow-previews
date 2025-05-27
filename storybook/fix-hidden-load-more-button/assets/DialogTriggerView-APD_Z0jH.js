e81b856dc0164a7738e7f36cc56381a9daa8b723
mittwald <>
2025-05-27 09:42:37 +0000

    Cleanup merged Storybook previews

--- storybook/fix-hidden-load-more-button/assets/DialogTriggerView-APD_Z0jH.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r as i}from"./index-CgfFrydU.js";import{D as t}from"./OverlayTrigger-Bwrcht6a.js";import{v as s}from"./PropsContextProvider-yY6RzzUT.js";const g=o=>{const r=i.useContext(s).DialogTrigger??t;return e.jsx(r,{...o})};g.__docgenInfo={description:"",methods:[],displayName:"DialogTriggerView"};export{g as D};
