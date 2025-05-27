67e3dfb8cb249b9443d5adbac422b1808542b715
mittwald <>
2025-05-27 06:09:15 +0000

    Update from https://github.com/mittwald/flow/commit/4cf61b6f6368e7cc24f6e5254e14990b4bc54725

---
+++ storybook/update-switch-position-in-context-menu/assets/EdgeCases.stories-BFWXp8VA.js
@@ -0,0 +1,23 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{H as i}from"./Heading-bJ8ZWrvE.js";import B from"./Default.stories-BM2Ak6Vz.js";import"./index-CgfFrydU.js";import{s as H}from"./IconWarning-70jApbHx.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import{A as T}from"./AlertBadge-YesYMvOP.js";import{C as j,a as h}from"./ContextualHelpTrigger-B76NiStg.js";import{B as f}from"./Button-CT6jKELJ.js";import{T as C}from"./Text-9PUJLXl5.js";import{B as A}from"./Badge-2BUMdBfD.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-yY6RzzUT.js";import"./mergeRefs-_uxeg_17.js";import"./index-sY8i7rw0.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-B2KKL2uI.js";import"./Heading-BFHXViOZ.js";import"./RSPContexts-9nj2DFX_.js";import"./utils-DvYDvJ-W.js";import"./storyBackgrounds-GFSAi-dI.js";import"./Section-Coqp9vad.js";import"./ContextMenuSection-CNNj2Vix.js";import"./Action-BASfP4Hw.js";import"./context-DnKpt6Zm.js";import"./useStatic-CLm-J0DW.js";import"./browser-B1A6F7LW.js";import"./getActionGroupSlot-BoNoL21H.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-WcTWvLN6.js";import"./Button-CY6niT9f.js";import"./ProgressBar-DuZjZRB2.js";import"./Label-XuXA-row.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DP9zv8XI.js";import"./useLocalizedStringFormatter-jDgQNJem.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-rEv_nV0O.js";import"./useFocus-bBgo0bKd.js";import"./useFocusRing-DUlPoSa4.js";import"./useFocusable-fU6dY-gF.js";import"./OverlayArrow-C1erJO4A.js";import"./useControlledState-DjePt-lg.js";import"./Collection-i8EjBJSq.js";import"./CollectionBuilder-Dw9MTGx5.js";import"./Separator-CUf66PuO.js";import"./Text-1CJ30n_Q.js";import"./SelectionManager-CTcRB2Ps.js";import"./useEvent-DsDOr2ug.js";import"./useCollator-Cym9_BaK.js";import"./FocusScope-BCiwkv3e.js";import"./VisuallyHidden-B4AzoGqn.js";import"./AlertIcon-0Mrbq7jC.js";import"./Popover-Dr9LZuKh.js";import"./OverlayTrigger-Bwrcht6a.js";import"./DialogTriggerView-APD_Z0jH.js";import"./LoadingSpinner-BHQSc97o.js";import"./EmulatedBoldText-KnQ_Srvn.js";const Sr={...B,title:"Content/Heading/Edge Cases",component:i},e={render:t=>r.jsxs(i,{...t,children:[r.jsx(H,{}),p.medium]})},o={render:t=>r.jsxs(i,{...t,children:[r.jsx(H,{}),p.long,r.jsx(T,{children:"Alert"}),r.jsx(A,{children:"Badge"})]})},m={render:t=>r.jsxs(i,{...t,children:[p.long,r.jsxs(j,{children:[r.jsx(f,{}),r.jsx(h,{...t,children:r.jsx(C,{children:p.medium})})]})]})};var n,a,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      {dummyText.medium}
+    </Heading>
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var d,l,c;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      {dummyText.long}
+      <AlertBadge>Alert</AlertBadge>
+      <Badge>Badge</Badge>
+    </Heading>
+}`,...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var g,x,u;m.parameters={...m.parameters,docs:{...(g=m.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      {dummyText.long}
+      <ContextualHelpTrigger>
+        <Button />
+        <ContextualHelp {...props}>
+          <Text>{dummyText.medium}</Text>
+        </ContextualHelp>
+      </ContextualHelpTrigger>
+    </Heading>
+}`,...(u=(x=m.parameters)==null?void 0:x.docs)==null?void 0:u.source}}};const Lr=["LongText","WithAlertBadge","WithContextualHelp"];export{e as LongText,o as WithAlertBadge,m as WithContextualHelp,Lr as __namedExportsOrder,Sr as default};
