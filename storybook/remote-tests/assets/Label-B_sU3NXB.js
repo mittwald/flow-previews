581f5f4a87e448371ead184faec649dabf8c6928
mittwald <>
2025-05-28 11:04:40 +0000

    Cleanup merged Storybook previews

--- storybook/remote-tests/assets/Label-B_sU3NXB.js
+++
@@ -1,1 +0,0 @@
-import{$ as o}from"./utils-Bm37ngJk.js";import{$}from"./Hidden-DwT0szmK.js";import{r as b,R as f}from"./index-CgfFrydU.js";const l=b.createContext({}),p=$(function(e,a){[e,a]=o(e,a,l);let{elementType:t="label",...r}=e;return f.createElement(t,{className:"react-aria-Label",...r,ref:a})});export{p as $,l as a};
