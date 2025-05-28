85f258cb749227804a522d4df677fdc99d21f588
mittwald <>
2025-05-28 06:28:18 +0000

    Update from https://github.com/mittwald/flow/commit/ab9cd877a0b73f3fea45daaff977168acdaede25

---
+++ storybook-main/assets/Default.stories--P1MiC8I.js
@@ -0,0 +1,74 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{T as t}from"./Text-9PUJLXl5.js";import"./index-CgfFrydU.js";import{S as M}from"./Section-C3HeVScC.js";import{a as N,s as P}from"./storyBackgrounds-GFSAi-dI.js";import{I as u}from"./InlineCode-CcUXxW3h.js";import{I as h}from"./IconWarning-70jApbHx.js";import{d as x}from"./dummyText-CX_I_Wpl.js";import"./PropsContextProvider-yY6RzzUT.js";import"./mergeRefs-_uxeg_17.js";import"./clsx-B-dksMZM.js";import"./index-sY8i7rw0.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-B2KKL2uI.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-KnQ_Srvn.js";import"./Text-1CJ30n_Q.js";import"./utils-DvYDvJ-W.js";import"./ContextMenuSection-tX_qKic9.js";import"./Action-BASfP4Hw.js";import"./context-DnKpt6Zm.js";import"./useStatic-CLm-J0DW.js";import"./getActionGroupSlot-BoNoL21H.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-y7i2Ibzp.js";import"./Button-CY6niT9f.js";import"./ProgressBar-DuZjZRB2.js";import"./Label-XuXA-row.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DP9zv8XI.js";import"./useLocalizedStringFormatter-jDgQNJem.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-rEv_nV0O.js";import"./useFocus-bBgo0bKd.js";import"./useFocusRing-DUlPoSa4.js";import"./useFocusable-fU6dY-gF.js";import"./RSPContexts-9nj2DFX_.js";import"./OverlayArrow-C1erJO4A.js";import"./useControlledState-DjePt-lg.js";import"./Collection-i8EjBJSq.js";import"./CollectionBuilder-Dw9MTGx5.js";import"./Separator-CUf66PuO.js";import"./SelectionManager-CTcRB2Ps.js";import"./useEvent-DsDOr2ug.js";import"./useCollator-Cym9_BaK.js";import"./FocusScope-BCiwkv3e.js";import"./VisuallyHidden-B4AzoGqn.js";const Jr={title:"Content/Text",component:t,argTypes:{elementType:{control:"inline-radio",options:["span","p"],defaultValue:"span"}},args:{elementType:"span"},render:e=>r.jsx(M,{children:r.jsx(t,{...e,children:"Text is an unstyled component that can be used to display texts. By default it renders a span."})})},s={},n={render:e=>r.jsx(t,{...e,children:"Text without styling parent components"})},o={render:e=>r.jsxs(t,{...e,children:["Text with unordered list:",r.jsxs("ul",{children:[r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"})]})]})},a={render:e=>r.jsxs(t,{...e,children:["Text with ordered list:",r.jsxs("ol",{children:[r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"}),r.jsx("li",{children:"Item"})]})]})},i={render:e=>r.jsx(t,{...e,children:r.jsx("small",{children:"Small Text"})})},p={args:{color:"dark"},parameters:{backgrounds:N}},m={args:{color:"light"},parameters:{backgrounds:P}},c={args:{align:"end"}},l={render:e=>r.jsxs(M,{children:[r.jsx(t,{children:r.jsxs("strong",{children:["Without specified ",r.jsx(u,{children:"wrap"}),":"]})}),r.jsx(t,{...e,align:"center",children:"This is a centered text in a small container so the text is wrapped."}),r.jsx(t,{children:r.jsxs("strong",{children:["With ",r.jsx(u,{children:"wrap: balance"}),":"]})}),r.jsx(t,{...e,align:"center",wrap:"balance",children:"This is a centered text in a small container so the text is wrapped."})]}),parameters:{viewport:{defaultViewport:"mobile2"}}},d={render:e=>r.jsxs(t,{...e,children:[r.jsx(h,{})," ",x.medium,r.jsxs("small",{children:[x.short," ",r.jsx(h,{})," ",x.short]})]})};var g,T,j;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(j=(T=s.parameters)==null?void 0:T.docs)==null?void 0:j.source}}};var I,w,S;n.parameters={...n.parameters,docs:{...(I=n.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <Text {...props}>Text without styling parent components</Text>
+}`,...(S=(w=n.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};var f,y,k;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with unordered list:
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    </Text>
+}`,...(k=(y=o.parameters)==null?void 0:y.docs)==null?void 0:k.source}}};var b,L,W;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with ordered list:
+      <ol>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ol>
+    </Text>
+}`,...(W=(L=a.parameters)==null?void 0:L.docs)==null?void 0:W.source}}};var B,C,D;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <small>Small Text</small>
+    </Text>
+}`,...(D=(C=i.parameters)==null?void 0:C.docs)==null?void 0:D.source}}};var E,O,R;p.parameters={...p.parameters,docs:{...(E=p.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(R=(O=p.parameters)==null?void 0:O.docs)==null?void 0:R.source}}};var V,v,A;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(A=(v=m.parameters)==null?void 0:v.docs)==null?void 0:A.source}}};var U,_,q;c.parameters={...c.parameters,docs:{...(U=c.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    align: "end"
+  }
+}`,...(q=(_=c.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var z,F,G;l.parameters={...l.parameters,docs:{...(z=l.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        <strong>
+          Without specified <InlineCode>wrap</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+      <Text>
+        <strong>
+          With <InlineCode>wrap: balance</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center" wrap="balance">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2"
+    }
+  }
+}`,...(G=(F=l.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var H,J,K;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <IconStar /> {dummyText.medium}
+      <small>
+        {dummyText.short} <IconStar /> {dummyText.short}
+      </small>
+    </Text>
+}`,...(K=(J=d.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const Kr=["Default","RawText","UnorderedList","OrderedList","SmallText","Dark","Light","AlignEnd","WrapBalance","WithIcon"];export{c as AlignEnd,p as Dark,s as Default,m as Light,a as OrderedList,n as RawText,i as SmallText,o as UnorderedList,d as WithIcon,l as WrapBalance,Kr as __namedExportsOrder,Jr as default};
---
+++ storybook-main/assets/Default.stories-8QsiScIq.js
@@ -0,0 +1,32 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-CgfFrydU.js";import{B as o}from"./Button-CT6jKELJ.js";import{A as T}from"./ActionGroup-BOpkwUJI.js";import{L as s,a as y}from"./LightBox-BQ6tgtrS.js";import{I as m}from"./Image-DQ-87tny.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import{x as G,y as O}from"./IconWarning-70jApbHx.js";import{u as w}from"./DialogTriggerView-DPQAVvTg.js";import"./_commonjsHelpers-CqkleIqs.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-yY6RzzUT.js";import"./mergeRefs-_uxeg_17.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";import"./Text-9PUJLXl5.js";import"./browser-B1A6F7LW.js";import"./EmulatedBoldText-KnQ_Srvn.js";import"./Text-1CJ30n_Q.js";import"./utils-DvYDvJ-W.js";import"./LoadingSpinner-BHQSc97o.js";import"./useLocalizedStringFormatter-jDgQNJem.js";import"./Button-CY6niT9f.js";import"./ProgressBar-DuZjZRB2.js";import"./Label-XuXA-row.js";import"./Hidden-DwT0szmK.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DP9zv8XI.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-rEv_nV0O.js";import"./useFocus-bBgo0bKd.js";import"./useFocusRing-DUlPoSa4.js";import"./useFocusable-fU6dY-gF.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-BoNoL21H.js";import"./useStatic-CLm-J0DW.js";import"./Overlay-CdMVcq_M.js";import"./Dialog-y7i2Ibzp.js";import"./RSPContexts-9nj2DFX_.js";import"./OverlayArrow-C1erJO4A.js";import"./useControlledState-DjePt-lg.js";import"./Collection-i8EjBJSq.js";import"./CollectionBuilder-Dw9MTGx5.js";import"./Separator-CUf66PuO.js";import"./SelectionManager-CTcRB2Ps.js";import"./useEvent-DsDOr2ug.js";import"./useCollator-Cym9_BaK.js";import"./FocusScope-BCiwkv3e.js";import"./VisuallyHidden-B4AzoGqn.js";import"./DivView-DEFtJl5H.js";import"./ButtonView-SJ4ZBLK3.js";import"./context-DnKpt6Zm.js";const wr={title:"Overlays/LightBox",component:s,parameters:{controls:{exclude:["controller"]}},render:a=>r.jsxs(y,{children:[r.jsx(o,{children:"Open LightBox"}),r.jsxs(s,{...a,children:[r.jsx(m,{src:p.imageSrc}),r.jsxs(T,{children:[r.jsx(o,{children:r.jsx(G,{})}),r.jsx(o,{children:r.jsx(O,{})})]})]})]})},t={},e={args:{fitScreen:!1}},i={render:a=>{const c=w("LightBox");return r.jsxs(r.Fragment,{children:[r.jsx(o,{onPress:c.open,children:"Open LightBox"}),r.jsxs(s,{...a,controller:c,children:[r.jsx(m,{src:p.imageSrc}),r.jsxs(T,{children:[r.jsx(o,{children:r.jsx(G,{})}),r.jsx(o,{children:r.jsx(O,{})})]})]})]})}},n={render:()=>r.jsxs(y,{children:[r.jsx(o,{children:r.jsx(m,{width:100,withBorder:!0,alt:"Gopher",src:p.imageSrc})}),r.jsx(s,{children:r.jsx(m,{alt:"Gopher",src:p.imageSrc})})]})};var l,d,x;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(x=(d=t.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};var g,h,u;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    fitScreen: false
+  }
+}`,...(u=(h=e.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var B,j,f;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => {
+    const controller = useOverlayController("LightBox");
+    return <>
+        <Button onPress={controller.open}>Open LightBox</Button>
+        <LightBox {...props} controller={controller}>
+          <Image src={dummyText.imageSrc} />
+          <ActionGroup>
+            <Button>
+              <IconDownload />
+            </Button>
+            <Button>
+              <IconDelete />
+            </Button>
+          </ActionGroup>
+        </LightBox>
+      </>;
+  }
+}`,...(f=(j=i.parameters)==null?void 0:j.docs)==null?void 0:f.source}}};var L,S,I;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: () => <LightBoxTrigger>
+      <Button>
+        <Image width={100} withBorder alt="Gopher" src={dummyText.imageSrc} />
+      </Button>
+      <LightBox>
+        <Image alt="Gopher" src={dummyText.imageSrc} />
+      </LightBox>
+    </LightBoxTrigger>
+}`,...(I=(S=n.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};const Dr=["Default","WithoutFitScreen","WithController","WithImageTrigger"];export{t as Default,i as WithController,n as WithImageTrigger,e as WithoutFitScreen,Dr as __namedExportsOrder,wr as default};
