cf088dafc8198a9c3a459e0da7a8b2c4f9a3bc09
mittwald <>
2025-05-30 12:34:14 +0000

    Update from https://github.com/mittwald/flow/commit/fa27950bb0b5228283063ba36769f74d670cca5d

---
+++ storybook/kati-ux-tooltips-guidelines/assets/Default.stories-Df-9HPIG.js
@@ -0,0 +1,94 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{r as N}from"./index-BdpSHsi2.js";import{L as t}from"./Label-DVrAxHfr.js";import{O as n}from"./Option-Nki_Sk5_.js";import{F as Q}from"./FieldDescription-CxC_NqHp.js";import{F as A}from"./FieldError-BhXsQ0t6.js";import{C as r}from"./ComboBox-DLmJJs08.js";import{S as U}from"./Section-6pFHXLoF.js";import{C as X,a as Y}from"./ContextualHelpTrigger-BTcSK9hz.js";import{B as Z}from"./Button-CaA5mTK3.js";import{T as $}from"./Text-B3DcLpPX.js";import{d as oo}from"./dummyText-CX_I_Wpl.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DmYPEqdN.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-Djme4n5v.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Label-C5gSD-Mq.js";import"./utils-q_Aud5Js.js";import"./Hidden-RoZfe91Z.js";import"./Popover-B7Z6gFgD.js";import"./DialogTriggerView-ERZeYafD.js";import"./context-DrivVi2O.js";import"./useStatic-B48yfRVA.js";import"./Dialog-CLdEiJe6.js";import"./Button-BV9jeieD.js";import"./ProgressBar-DbWwTk60.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-BtcTDQEe.js";import"./ListBox-Da8mSHPi.js";import"./DragAndDrop-1oT9LeNd.js";import"./inertValue-Cx-SdfM1.js";import"./useListState-eKLbauDG.js";import"./IconWarning-CNa_Q51C.js";import"./FieldError-D2eBwE7I.js";import"./FormField.module-B9AzUSUD.js";import"./Form-CSvT4C5Q.js";import"./Group-wR-qYHNv.js";import"./Input-Dom3MqAg.js";import"./useFilter-ClmrtTI7.js";import"./useFormValidation-BP6joqgG.js";import"./useSingleSelectListState-L7tbABOK.js";import"./useUpdateEffect-CMGfMYOO.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./ContextMenuSection-Drgz47XP.js";import"./Action-B1hdkiYX.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-h1ROBgBP.js";import"./dynamic-DKDa4OpU.js";import"./LoadingSpinner-BaFzc2M0.js";import"./EmulatedBoldText-DOyoRrwd.js";const Dn={title:"Form Controls/ComboBox",component:r,render:e=>o.jsxs(r,{...e,children:[o.jsx(t,{children:"Domain"}),o.jsx(n,{children:"mydomain.de"}),o.jsx(n,{children:"shop.mydomain.de"}),o.jsx(n,{children:"anotherdomain.com"}),o.jsx(n,{children:"www.anotherdomain.com"}),o.jsx(n,{children:"anotherdomain.com/shop"}),o.jsx(n,{children:"anotherdomain.com/blog"}),o.jsx(n,{children:"onemoredomain.de"}),o.jsx(n,{children:"www.onemoredomain.de"})]})},a={},d={args:{isDisabled:!0}},s={args:{isRequired:!0}},p={render:e=>o.jsxs(r,{...e,children:[o.jsx(t,{children:"Domain"}),o.jsx(n,{children:"mydomain.de"}),o.jsx(n,{children:"shop.mydomain.de"}),o.jsx(n,{children:"anotherdomain.com"}),o.jsx(n,{children:"www.anotherdomain.com"}),o.jsx(n,{children:"anotherdomain.com/shop"}),o.jsx(n,{children:"anotherdomain.com/blog"}),o.jsx(n,{children:"onemoredomain.de"}),o.jsx(n,{children:"www.onemoredomain.de"}),o.jsx(Q,{children:"Select a domain"})]})},c={render:e=>o.jsxs(r,{...e,defaultSelectedKey:"mydomain.de",children:[o.jsx(t,{children:"Domain"}),o.jsx(n,{value:"mydomain.de",children:"mydomain.de"}),o.jsx(n,{children:"shop.mydomain.de"}),o.jsx(n,{children:"anotherdomain.com"}),o.jsx(n,{children:"www.anotherdomain.com"}),o.jsx(n,{children:"anotherdomain.com/shop"}),o.jsx(n,{children:"anotherdomain.com/blog"}),o.jsx(n,{children:"onemoredomain.de"}),o.jsx(n,{children:"www.onemoredomain.de"})]})},l={render:e=>o.jsxs(r,{...e,isInvalid:!0,isRequired:!0,children:[o.jsx(t,{children:"Domain"}),o.jsx(n,{children:"mydomain.de"}),o.jsx(n,{children:"shop.mydomain.de"}),o.jsx(n,{children:"anotherdomain.com"}),o.jsx(n,{children:"www.anotherdomain.com"}),o.jsx(n,{children:"anotherdomain.com/shop"}),o.jsx(n,{children:"anotherdomain.com/blog"}),o.jsx(n,{children:"onemoredomain.de"}),o.jsx(n,{children:"www.onemoredomain.de"}),o.jsx(A,{children:"Select a domain to continue"})]})},h={render:e=>{const[i,G]=N.useState(),J=["a.de","b.de","c.de"],M=i!=null&&i.includes("@")?J.map(m=>o.jsx(n,{value:(i==null?void 0:i.split("@")[0])+"@"+m,children:(i==null?void 0:i.split("@")[0])+"@"+m},m)):null;return o.jsx(U,{children:o.jsxs(r,{...e,allowsEmptyCollection:!0,isRequired:!0,onInputChange:m=>G(m),children:[o.jsx(t,{children:"Domain"}),M]})})}},x={render:e=>o.jsxs(r,{...e,placeholder:"Select a domain",isRequired:!0,children:[o.jsx(t,{children:"Domain"}),o.jsx(n,{children:"mydomain.de"}),o.jsx(n,{children:"shop.mydomain.de"}),o.jsx(n,{children:"anotherdomain.com"}),o.jsx(n,{children:"www.anotherdomain.com"}),o.jsx(n,{children:"anotherdomain.com/shop"}),o.jsx(n,{children:"anotherdomain.com/blog"}),o.jsx(n,{children:"onemoredomain.de"}),o.jsx(n,{children:"www.onemoredomain.de"}),o.jsx(A,{children:"Select a domain to continue"})]})},u={render:e=>o.jsxs(r,{...e,children:[o.jsxs(t,{children:["Domain",o.jsxs(X,{children:[o.jsx(Z,{}),o.jsx(Y,{children:o.jsx($,{children:oo.short})})]})]}),o.jsx(n,{children:"mydomain.de"}),o.jsx(n,{children:"shop.mydomain.de"}),o.jsx(n,{children:"anotherdomain.com"}),o.jsx(n,{children:"www.anotherdomain.com"}),o.jsx(n,{children:"anotherdomain.com/shop"}),o.jsx(n,{children:"anotherdomain.com/blog"}),o.jsx(n,{children:"onemoredomain.de"}),o.jsx(n,{children:"www.onemoredomain.de"})]})};var O,j,w;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:"{}",...(w=(j=a.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};var b,g,y;d.parameters={...d.parameters,docs:{...(b=d.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(y=(g=d.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var C,D,S;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(S=(D=s.parameters)==null?void 0:D.docs)==null?void 0:S.source}}};var f,B,F;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <ComboBox {...props}>
+      <Label>Domain</Label>
+      <Option>mydomain.de</Option>
+      <Option>shop.mydomain.de</Option>
+      <Option>anotherdomain.com</Option>
+      <Option>www.anotherdomain.com</Option>
+      <Option>anotherdomain.com/shop</Option>
+      <Option>anotherdomain.com/blog</Option>
+      <Option>onemoredomain.de</Option>
+      <Option>www.onemoredomain.de</Option>
+      <FieldDescription>Select a domain</FieldDescription>
+    </ComboBox>
+}`,...(F=(B=p.parameters)==null?void 0:B.docs)==null?void 0:F.source}}};var E,L,R;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: props => <ComboBox {...props} defaultSelectedKey="mydomain.de">
+      <Label>Domain</Label>
+      <Option value="mydomain.de">mydomain.de</Option>
+      <Option>shop.mydomain.de</Option>
+      <Option>anotherdomain.com</Option>
+      <Option>www.anotherdomain.com</Option>
+      <Option>anotherdomain.com/shop</Option>
+      <Option>anotherdomain.com/blog</Option>
+      <Option>onemoredomain.de</Option>
+      <Option>www.onemoredomain.de</Option>
+    </ComboBox>
+}`,...(R=(L=c.parameters)==null?void 0:L.docs)==null?void 0:R.source}}};var q,W,T;l.parameters={...l.parameters,docs:{...(q=l.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: props => <ComboBox {...props} isInvalid isRequired>
+      <Label>Domain</Label>
+      <Option>mydomain.de</Option>
+      <Option>shop.mydomain.de</Option>
+      <Option>anotherdomain.com</Option>
+      <Option>www.anotherdomain.com</Option>
+      <Option>anotherdomain.com/shop</Option>
+      <Option>anotherdomain.com/blog</Option>
+      <Option>onemoredomain.de</Option>
+      <Option>www.onemoredomain.de</Option>
+      <FieldError>Select a domain to continue</FieldError>
+    </ComboBox>
+}`,...(T=(W=l.parameters)==null?void 0:W.docs)==null?void 0:T.source}}};var H,v,V;h.parameters={...h.parameters,docs:{...(H=h.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: props => {
+    const [value, setValue] = useState<string>();
+    const domains = ["a.de", "b.de", "c.de"];
+    const options = value?.includes("@") ? domains.map(d => <Option key={d} value={value?.split("@")[0] + "@" + d}>
+            {value?.split("@")[0] + "@" + d}
+          </Option>) : null;
+    return <Section>
+        <ComboBox {...props} allowsEmptyCollection isRequired onInputChange={v => setValue(v)}>
+          <Label>Domain</Label>
+          {options}
+        </ComboBox>
+      </Section>;
+  }
+}`,...(V=(v=h.parameters)==null?void 0:v.docs)==null?void 0:V.source}}};var I,K,P;x.parameters={...x.parameters,docs:{...(I=x.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <ComboBox {...props} placeholder="Select a domain" isRequired>
+      <Label>Domain</Label>
+      <Option>mydomain.de</Option>
+      <Option>shop.mydomain.de</Option>
+      <Option>anotherdomain.com</Option>
+      <Option>www.anotherdomain.com</Option>
+      <Option>anotherdomain.com/shop</Option>
+      <Option>anotherdomain.com/blog</Option>
+      <Option>onemoredomain.de</Option>
+      <Option>www.onemoredomain.de</Option>
+      <FieldError>Select a domain to continue</FieldError>
+    </ComboBox>
+}`,...(P=(K=x.parameters)==null?void 0:K.docs)==null?void 0:P.source}}};var _,k,z;u.parameters={...u.parameters,docs:{...(_=u.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: props => <ComboBox {...props}>
+      <Label>
+        Domain
+        <ContextualHelpTrigger>
+          <Button />
+          <ContextualHelp>
+            <Text>{dummyText.short}</Text>
+          </ContextualHelp>
+        </ContextualHelpTrigger>
+      </Label>
+      <Option>mydomain.de</Option>
+      <Option>shop.mydomain.de</Option>
+      <Option>anotherdomain.com</Option>
+      <Option>www.anotherdomain.com</Option>
+      <Option>anotherdomain.com/shop</Option>
+      <Option>anotherdomain.com/blog</Option>
+      <Option>onemoredomain.de</Option>
+      <Option>www.onemoredomain.de</Option>
+    </ComboBox>
+}`,...(z=(k=u.parameters)==null?void 0:k.docs)==null?void 0:z.source}}};const Sn=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithFieldError","Emails","WithPlaceholder","WithContextualHelp"];export{a as Default,d as Disabled,h as Emails,s as Required,u as WithContextualHelp,c as WithDefaultValue,p as WithFieldDescription,l as WithFieldError,x as WithPlaceholder,Sn as __namedExportsOrder,Dn as default};
