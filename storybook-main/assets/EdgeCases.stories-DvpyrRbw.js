64b6159a8c7923f42c5cdfcb07832774668da85f
mittwald <>
2025-06-03 12:58:14 +0000

    Update from https://github.com/mittwald/flow/commit/7be3bd48dcdc4a24b6161fe860f855f866ff7fac

---
+++ storybook-main/assets/EdgeCases.stories-DvpyrRbw.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{T as l}from"./Text-DvpCWahg.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import f from"./Default.stories-Dk5a3Vlz.js";import{I as c}from"./IllustratedMessage-Ot6eWQFJ.js";import{H as u}from"./Heading-7ZuPWCoT.js";import{B as x}from"./Button-Bg5GyRsX.js";import{c as g}from"./IconWarning-l71w2QxE.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./PropsContextProvider-BHQ2INuZ.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-DZ7wHvlF.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ActionGroup-CnCIdT5J.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-BmbxRmec.js";import"./useStatic-CZxLTWti.js";import"./ProgressBar-RlG-NEsJ.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./Legend-DN9uT_O2.js";import"./Label-UDce9YwW.js";import"./Heading-PKR8zMP8.js";import"./RSPContexts-Cbm9ZJUe.js";import"./LoadingSpinner-Bd7rzN7_.js";import"./Button-BV9jeieD.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";const ar={...f,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>r.jsxs(c,{...o,children:[r.jsx(g,{}),r.jsx(u,{children:p.medium}),r.jsx(l,{children:p.long}),r.jsx(x,{children:"Create app"})]})},e={render:o=>r.jsxs(c,{...o,children:[r.jsx(g,{}),r.jsx(u,{children:"No apps installed"}),r.jsx(l,{children:"Create your first app to start working on your website."}),r.jsx(x,{children:"Create app"})]}),parameters:{viewport:{defaultViewport:"mobile1"}}};var s,m,i;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(i=(m=t.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var a,n,d;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(d=(n=e.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};const nr=["LongTexts","SmallSpace"];export{t as LongTexts,e as SmallSpace,nr as __namedExportsOrder,ar as default};
