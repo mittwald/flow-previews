9b89f7773b47600c74e53f772249623c1c920d7b
mittwald <>
2025-06-03 11:22:51 +0000

    Update from https://github.com/mittwald/flow/commit/492445623d9d20e63d5721d6f481401786a3d8a0

---
+++ storybook/allow-columns-for-radios-and-checkboxes/assets/ButtonView-CSd5eBcL.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-BdpSHsi2.js";import{B as n}from"./Button-a3aehxY2.js";import{v as s}from"./PropsContextProvider-C54X5Apg.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
