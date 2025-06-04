3fa404a3c23222b4e0d5ce9cbbcc55b4d45471a2
mittwald <>
2025-06-04 10:00:10 +0000

    Update from https://github.com/mittwald/flow/commit/2d3136bdf1a68a642444d5cab56e8ec90765781e

---
+++ storybook/AlertBadge-overview-Caro-1/assets/EdgeCases.stories-BC6jzQLr.js
@@ -0,0 +1,18 @@
+import c from"./Default.stories-whetpZ6S.js";import{F as d}from"./FileCard-CnhZUk4A.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import{a as s}from"./index-B-lxVbXh.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./ContextMenuContent-BmNso7be.js";import"./Action-D-sUUVQZ.js";import"./context-BvMovFkA.js";import"./PropsContextProvider-WdxBf-09.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-C6hfTCMX.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useStatic-Cp3d5BPs.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-BGkN04Q5.js";import"./dynamic-DKDa4OpU.js";import"./ContextMenuSection-B5-Bvy3J.js";import"./Dialog-CLdEiJe6.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-BtcTDQEe.js";import"./Popover-BxkN7LyH.js";import"./DialogTriggerView-BJdiUClr.js";import"./IconWarning-DZDqnTdB.js";import"./Switch-Dc4iC_ZN.js";import"./Label-BNSNqTMI.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./Avatar-Bse7dMDJ.js";import"./AlertIcon-C8nTc5Bg.js";import"./Image-Duv4tVWk.js";import"./Text-duAw7RQ5.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Link-A0AgyPWO.js";import"./Button-D7NjIbbc.js";import"./LoadingSpinner-DLrAYX4w.js";import"./OptionsButton-CiMf7jaE.js";import"./ButtonView-BiY0wTVM.js";import"./v4-CtRu48qb.js";const To={...c,title:"Upload/FileCard/Edge Cases",component:d},o={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")}}},r={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")},href:"#"}};var t,m,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(i=(m=o.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var e,p,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    },
+    href: "#"
+  }
+}`,...(n=(p=r.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const ho=["LongName","LongNameAndLink"];export{o as LongName,r as LongNameAndLink,ho as __namedExportsOrder,To as default};
