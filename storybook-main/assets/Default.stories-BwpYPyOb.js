3f50b207f6395044b33839a95ab27e7c3af9388f
mittwald <>
2025-06-04 10:34:54 +0000

    Update from https://github.com/mittwald/flow/commit/e7b00ac7df6b1396b17c2574d0b82479192d1174

---
+++ storybook-main/assets/Default.stories-BwpYPyOb.js
@@ -0,0 +1,28 @@
+import{j as g}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{M as i}from"./Markdown-C7upUv8u.js";import{a as k,s as b}from"./storyBackgrounds-GFSAi-dI.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./CodeBlock-CB-G5kyv.js";import"./clsx-B-dksMZM.js";import"./CopyButton-CZxMQSCR.js";import"./Button-CLFommIT.js";import"./PropsContextProvider-BFDwIaNf.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-DYouPS06.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./IconWarning-DmQ1-Hvu.js";import"./Text-W2o7sLxr.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./LoadingSpinner-Ckd3wDxm.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Button-BV9jeieD.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./Tooltip-0Z6fTLJr.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./react-children-utilities-DvaK_N8_.js";import"./Action-BAfrIQ8u.js";import"./context-BWqojjN5.js";import"./useStatic-DIY0jyg5.js";import"./getActionGroupSlot-Al_IZrjT.js";import"./dynamic-DKDa4OpU.js";import"./onlyText-C2raRYWa.js";import"./Heading-Cu42G17X.js";import"./Heading-PKR8zMP8.js";import"./RSPContexts-Cbm9ZJUe.js";import"./InlineCode-BkOC6JyQ.js";import"./Link-CGNvTDLm.js";import"./Separator-BrRg1FQa.js";import"./Separator-Dc9-x765.js";import"./CollectionBuilder-DkA--_MS.js";const br={title:"Content/Markdown",component:i,render:l=>g.jsx(i,{...l,children:`# Heading
+Lorem ipsum dolor sit amet **consectetur adipisicing** elit. Cumque eius \`quam quas vel voluptas\` ullam aliquid fugit.
+\`\`\`json
+{
+    "projectId": "b3a96db5-ba8f-40dd-9100-bab43ac1f698",
+    "name": "My Project"
+}
+\`\`\`
+[link](#)
+> Block quote
+ - Unordered list item 1 
+ - Unordered list item 2 
+ 1. Ordered list item 1 
+ 2. Ordered list item 2`})},r={},o={args:{color:"dark"},parameters:{backgrounds:k}},t={args:{color:"light"},parameters:{backgrounds:b}};var m,e,a;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var p,s,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var c,d,u;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const fr=["Default","Dark","Light"];export{o as Dark,r as Default,t as Light,fr as __namedExportsOrder,br as default};
---
+++ storybook-main/assets/Default.stories-JlVzm1r8.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{L as t}from"./LabeledValue-BEH4FYIe.js";import"./index-BdpSHsi2.js";import{L as o}from"./Label-Dx3HrOfD.js";import{C as I}from"./Content-BQDNOIm3.js";import{C as m}from"./CopyButton-CZxMQSCR.js";import{B as W}from"./Button-CLFommIT.js";import{I as g}from"./InlineCode-BkOC6JyQ.js";import{L as k}from"./Link-CGNvTDLm.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BFDwIaNf.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-DYouPS06.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./index-BAMY2Nnw.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Label-C5gSD-Mq.js";import"./utils-q_Aud5Js.js";import"./Hidden-RoZfe91Z.js";import"./IconWarning-DmQ1-Hvu.js";import"./Tooltip-0Z6fTLJr.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useFocus-4XTC7e6o.js";import"./ProgressBar-DbWwTk60.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-rjpdN6qd.js";import"./useFocusable-BXcKiciC.js";import"./useFocusRing-DY5AGIis.js";import"./react-children-utilities-DvaK_N8_.js";import"./Action-BAfrIQ8u.js";import"./context-BWqojjN5.js";import"./useStatic-DIY0jyg5.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-Al_IZrjT.js";import"./dynamic-DKDa4OpU.js";import"./onlyText-C2raRYWa.js";import"./Text-W2o7sLxr.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./LoadingSpinner-Ckd3wDxm.js";import"./Button-BV9jeieD.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";const Be={title:"Content/LabeledValue",component:t,parameters:{controls:{exclude:["className"]}},render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Project"}),e.jsx(I,{children:"My proSpace"})]})},p={},a={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Project"}),e.jsx(I,{children:"My proSpace"}),e.jsx(m,{text:"My proSpace"})]})},s={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"IP address"}),e.jsx(g,{children:"127.0.0.1"}),e.jsx(m,{text:"127.0.0.1"})]})},n={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Domain"}),e.jsx(k,{children:"https://mittwald.de"}),e.jsx(m,{text:"https://mittwald.de"})]})},i={render:r=>e.jsxs(t,{...r,children:[e.jsx(o,{children:"Support pin"}),e.jsx(W,{variant:"soft",color:"secondary",children:"Generate"})]})};var d,c,l;p.parameters={...p.parameters,docs:{...(d=p.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(l=(c=p.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,x,h;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Project</Label>
+      <Content>My proSpace</Content>
+      <CopyButton text="My proSpace" />
+    </LabeledValue>
+}`,...(h=(x=a.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var L,j,b;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>IP address</Label>
+      <InlineCode>127.0.0.1</InlineCode>
+      <CopyButton text="127.0.0.1" />
+    </LabeledValue>
+}`,...(b=(j=s.parameters)==null?void 0:j.docs)==null?void 0:b.source}}};var C,f,y;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Domain</Label>
+      <Link>https://mittwald.de</Link>
+      <CopyButton text="https://mittwald.de" />
+    </LabeledValue>
+}`,...(y=(f=n.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var B,S,V;i.parameters={...i.parameters,docs:{...(B=i.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Support pin</Label>
+      <Button variant="soft" color="secondary">
+        Generate
+      </Button>
+    </LabeledValue>
+}`,...(V=(S=i.parameters)==null?void 0:S.docs)==null?void 0:V.source}}};const Se=["Default","WithCopyButton","WithInlineCode","WithLink","WithButton"];export{p as Default,i as WithButton,a as WithCopyButton,s as WithInlineCode,n as WithLink,Se as __namedExportsOrder,Be as default};
