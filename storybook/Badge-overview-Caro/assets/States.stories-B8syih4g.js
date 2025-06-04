c99606391cb2df2e08e026e1e0c1455e327d400e
mittwald <>
2025-06-04 05:49:20 +0000

    Update from https://github.com/mittwald/flow/commit/7c10e6564619659c760cad0aca987a3db4ca1c09

---
+++ storybook/Badge-overview-Caro/assets/States.stories-B8syih4g.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{B as W}from"./Button-Bg5GyRsX.js";import _ from"./Default.stories-Bcco7-bF.js";import"./index-BdpSHsi2.js";import{e as w,d as C}from"./IconWarning-l71w2QxE.js";import{T as O}from"./Text-DvpCWahg.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHQ2INuZ.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-DZ7wHvlF.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./LoadingSpinner-Bd7rzN7_.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./Avatar-Cng_96Mj.js";import"./AlertIcon-B5Czb2Ni.js";import"./Tooltip-0Z6fTLJr.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./dummyText-CX_I_Wpl.js";import"./Image-7_TXtZIP.js";import"./index-BAMY2Nnw.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";const jr={..._,title:"Actions/Button/States",render:p=>r.jsxs(W,{...p,children:[r.jsx(O,{children:"Add email address"}),r.jsx(w,{})]})},e={args:{isDisabled:!0}},a={args:{isDisabled:!0,variant:"plain"}},i={args:{isPending:!0}},s={args:{isPending:!0,variant:"plain"}},o={args:{isPending:!0,variant:"plain"},render:p=>r.jsx(W,{...p,"aria-label":"Add to favorites",children:r.jsx(C,{})})},t={args:{isSucceeded:!0}},n={args:{isFailed:!0,variant:"plain"}};var m,d,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(c=(d=e.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var l,u,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true,
+    variant: "plain"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var P,S,v;i.parameters={...i.parameters,docs:{...(P=i.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    isPending: true
+  }
+}`,...(v=(S=i.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var b,x,D;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  }
+}`,...(D=(x=s.parameters)==null?void 0:x.docs)==null?void 0:D.source}}};var f,j,h;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    isPending: true,
+    variant: "plain"
+  },
+  render: props => <Button {...props} aria-label="Add to favorites">
+      <IconPlus />
+    </Button>
+}`,...(h=(j=o.parameters)==null?void 0:j.docs)==null?void 0:h.source}}};var B,I,A;t.parameters={...t.parameters,docs:{...(B=t.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    isSucceeded: true
+  }
+}`,...(A=(I=t.parameters)==null?void 0:I.docs)==null?void 0:A.source}}};var F,E,T;n.parameters={...n.parameters,docs:{...(F=n.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    isFailed: true,
+    variant: "plain"
+  }
+}`,...(T=(E=n.parameters)==null?void 0:E.docs)==null?void 0:T.source}}};const hr=["DisabledSolid","DisabledPlain","PendingSolid","PendingPlain","PendingPlainWithIcon","SucceededSolid","FailedPlain"];export{a as DisabledPlain,e as DisabledSolid,n as FailedPlain,s as PendingPlain,o as PendingPlainWithIcon,i as PendingSolid,t as SucceededSolid,hr as __namedExportsOrder,jr as default};
