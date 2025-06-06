4bcb6073099da91ba88c3450d0d52260a98f0e86
mittwald <>
2025-06-06 09:26:51 +0000

    Update from https://github.com/mittwald/flow/commit/a95c340a55e109df3b6c95eb8439f0d5c907e1db

---
+++ storybook/1551-progressbar-must-display-the-real-maxvalue/assets/Default.stories-DUsD2O76.js
@@ -0,0 +1,71 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r as h}from"./index-BdpSHsi2.js";import{F as t}from"./FileDropZone-eUjlslDh.js";import{S as c}from"./Section-_z-S7Ia0.js";import{F as d}from"./FileCardList-DcgEp5ev.js";import{F as u}from"./FileCard-0V2u1Hp2.js";import{u as b,F as I,t as T}from"./Form-v4R2sh80.js";import{a as U}from"./index-B-lxVbXh.js";import{B as s}from"./Button-Bwnf9MPP.js";import{A as w}from"./ActionGroup-V7mQsirO.js";import{k as x,l as W}from"./IconWarning-C38NVWlR.js";import{H as F}from"./Heading-Txxau-2G.js";import{F as f}from"./FileField-AIW_FUqZ.js";import{T as E}from"./Text-DnncCDWL.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./IllustratedMessage-DhhMiOqA.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CnP1l1jr.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-nPg91gjW.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./utils-q_Aud5Js.js";import"./intlStrings-ZMTSFH_M.js";import"./Text-DtGJyZhh.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useFocus-4XTC7e6o.js";import"./useCollator-DWYYoDxV.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Button-BV9jeieD.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./ContextMenuSection-Uk3RMmT2.js";import"./Action-C5onqV_3.js";import"./context-CEBt9HJC.js";import"./useStatic-LRqcoCvr.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-DjvG_HBT.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-HD9t1mUg.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./FocusScope-D_x47-q0.js";import"./ColumnLayout-CrtUKl2S.js";import"./Avatar-Cw8hYCiy.js";import"./AlertIcon-DX3onwQh.js";import"./Image-Brs1XSGd.js";import"./Link-BsFg4TSR.js";import"./OptionsButton-DWjIquFj.js";import"./ButtonView-CDWJNk9h.js";import"./ContextMenuContent-odff_4GW.js";import"./Popover-1laqsUPc.js";import"./DialogTriggerView-T9-0cZe3.js";import"./Switch-D16GZV5a.js";import"./Label-ixwaI_SB.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./FieldError-CPYg7Tc9.js";import"./FieldError-D2eBwE7I.js";import"./v4-CtRu48qb.js";import"./LoadingSpinner-B0G09yz1.js";import"./Heading-PKR8zMP8.js";import"./FormField.module-B9AzUSUD.js";import"./useFormValidation-BP6joqgG.js";import"./Input-Dom3MqAg.js";import"./EmulatedBoldText-DOyoRrwd.js";const cr={title:"Upload/FileDropZone",component:t,parameters:{controls:{exclude:["className","controller","onChange"]}},render:n=>{const[o,i]=h.useState(null);return e.jsxs(c,{children:[e.jsxs(t,{...n,onChange:i,children:[e.jsx(x,{}),e.jsx(F,{children:"Drop file"}),e.jsx(f,{name:"file",onChange:i,children:e.jsx(s,{children:"Select file"})})]}),e.jsx(d,{children:[...o??[]].map(r=>e.jsx(u,{name:r.name},r.name))})]})}},G=U("submit"),m={},a={args:{accept:"image/png"},render:n=>{const[o,i]=h.useState(null);return e.jsxs(c,{children:[e.jsxs(t,{...n,onChange:i,children:[e.jsx(W,{}),e.jsx(F,{children:"Drop image"}),e.jsx(E,{children:"Only image/png images are allowed."}),e.jsx(f,{name:"file",onChange:i,children:e.jsx(s,{children:"Select image"})})]}),e.jsx(d,{children:[...o??[]].map(r=>e.jsx(u,{name:r.name},r.name))})]})}},l={args:{multiple:!0},render:n=>{const[o,i]=h.useState(null);return e.jsxs(c,{children:[e.jsxs(t,{...n,onChange:i,children:[e.jsx(x,{}),e.jsx(F,{children:"Drop files"}),e.jsx(f,{name:"file",onChange:i,children:e.jsx(s,{children:"Select files"})})]}),e.jsx(d,{children:[...o??[]].map(r=>e.jsx(u,{name:r.name},r.name))})]})}},p={render:n=>{const o=b(),i=T();return e.jsxs(I,{form:o,onSubmit:G,children:[e.jsxs(c,{children:[e.jsxs(t,{...n,onChange:r=>o.setValue("file",r),children:[e.jsx(x,{}),e.jsx(F,{children:"Drop file"}),e.jsx(i,{name:"file",rules:{required:"Please choose a file"},children:e.jsx(f,{children:e.jsx(s,{variant:"outline",color:"dark",children:"Select file"})})})]}),e.jsx(d,{children:[...o.watch("file")??[]].map(r=>e.jsx(u,{name:r.name},r.name))})]}),e.jsx(w,{children:e.jsx(s,{type:"submit",children:"Upload"})})]})}};var g,j,S;m.parameters={...m.parameters,docs:{...(g=m.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(S=(j=m.parameters)==null?void 0:j.docs)==null?void 0:S.source}}};var C,D,y;a.parameters={...a.parameters,docs:{...(C=a.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    accept: "image/png"
+  },
+  render: props => {
+    const [files, setFiles] = useState<FileList | null>(null);
+    return <Section>
+        <FileDropZone {...props} onChange={setFiles}>
+          <IconImage />
+          <Heading>Drop image</Heading>
+          <Text>Only image/png images are allowed.</Text>
+          <FileField name="file" onChange={setFiles}>
+            <Button>Select image</Button>
+          </FileField>
+        </FileDropZone>
+        <FileCardList>
+          {[...(files ?? [])].map(f => <FileCard name={f.name} key={f.name} />)}
+        </FileCardList>
+      </Section>;
+  }
+}`,...(y=(D=a.parameters)==null?void 0:D.docs)==null?void 0:y.source}}};var B,H,L;l.parameters={...l.parameters,docs:{...(B=l.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    multiple: true
+  },
+  render: props => {
+    const [files, setFiles] = useState<FileList | null>(null);
+    return <Section>
+        <FileDropZone {...props} onChange={setFiles}>
+          <IconUpload />
+          <Heading>Drop files</Heading>
+          <FileField name="file" onChange={setFiles}>
+            <Button>Select files</Button>
+          </FileField>
+        </FileDropZone>
+        <FileCardList>
+          {[...(files ?? [])].map(f => <FileCard name={f.name} key={f.name} />)}
+        </FileCardList>
+      </Section>;
+  }
+}`,...(L=(H=l.parameters)==null?void 0:H.docs)==null?void 0:L.source}}};var k,A,Z;p.parameters={...p.parameters,docs:{...(k=p.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm<{
+      file: FileList | null;
+    }>();
+    const Field = typedField(form);
+    return <Form form={form} onSubmit={submitAction}>
+        <Section>
+          <FileDropZone {...props} onChange={f => form.setValue("file", f)}>
+            <IconUpload />
+            <Heading>Drop file</Heading>
+            <Field name="file" rules={{
+            required: "Please choose a file"
+          }}>
+              <FileField>
+                <Button variant="outline" color="dark">
+                  Select file
+                </Button>
+              </FileField>
+            </Field>
+          </FileDropZone>
+
+          <FileCardList>
+            {[...(form.watch("file") ?? [])].map(f => <FileCard name={f.name} key={f.name} />)}
+          </FileCardList>
+        </Section>
+        <ActionGroup>
+          <Button type="submit">Upload</Button>
+        </ActionGroup>
+      </Form>;
+  }
+}`,...(Z=(A=p.parameters)==null?void 0:A.docs)==null?void 0:Z.source}}};const dr=["Default","WithAcceptedTypes","Multiple","WithReactHookForm"];export{m as Default,l as Multiple,a as WithAcceptedTypes,p as WithReactHookForm,dr as __namedExportsOrder,cr as default};
---
+++ storybook/1551-progressbar-must-display-the-real-maxvalue/assets/EdgeCases.stories-DxVyrXxU.js
@@ -0,0 +1,63 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{M as u}from"./Modal-BNV2DGJY.js";import"./index-BdpSHsi2.js";import{B as m}from"./Button-Bwnf9MPP.js";import y from"./Default.stories-BQU4Oofu.js";import{C as h}from"./Content-QaM31tUL.js";import{S as r}from"./Section-_z-S7Ia0.js";import{T as n}from"./Text-DnncCDWL.js";import{A as j}from"./ActionGroup-V7mQsirO.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{H as T}from"./Heading-Txxau-2G.js";import{A as g}from"./Action-C5onqV_3.js";import{u as f}from"./DialogTriggerView-T9-0cZe3.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CnP1l1jr.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-nPg91gjW.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./Overlay-C-2Td9Wg.js";import"./utils-q_Aud5Js.js";import"./Dialog-HD9t1mUg.js";import"./Button-BV9jeieD.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./IconWarning-C38NVWlR.js";import"./Header-D3x_y1HT.js";import"./ButtonView-CDWJNk9h.js";import"./index-BAMY2Nnw.js";import"./LoadingSpinner-B0G09yz1.js";import"./TextField-C5M0lqk-.js";import"./TextFieldBase-DEwa6n7Y.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-CPYg7Tc9.js";import"./FieldError-D2eBwE7I.js";import"./FieldDescription-C8hOUlbl.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./Input-Dom3MqAg.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./useFormValidation-BP6joqgG.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./Label-ixwaI_SB.js";import"./lib-APWcJLcN.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./Form-v4R2sh80.js";import"./dynamic-DKDa4OpU.js";import"./browser-BJKb5O22.js";import"./Align-DcBwwto_.js";import"./ColumnLayout-CrtUKl2S.js";import"./AccentBox-V0utXW_g.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Heading-PKR8zMP8.js";import"./context-CEBt9HJC.js";import"./useStatic-LRqcoCvr.js";import"./getActionGroupSlot-DjvG_HBT.js";import"./ContextMenuSection-Uk3RMmT2.js";const Wo={...y,title:"Overlays/Modal/Edge Cases"},e={render:s=>o.jsxs(u,{...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})},i={render:s=>o.jsxs(u,{offCanvas:!0,...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})};var c,l,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(p=(l=e.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var a,d,x;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Modal offCanvas {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(x=(d=i.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const Xo=["LongContent","LongContentOffCanvas"];export{e as LongContent,i as LongContentOffCanvas,Xo as __namedExportsOrder,Wo as default};
