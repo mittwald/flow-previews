8787aed45367c0572686a75791a176b3e96ec41f
mittwald <>
2025-06-06 09:13:16 +0000

    Update from https://github.com/mittwald/flow/commit/012493943f643a70e52a615f4d0d4b13bf41612a

---
+++ storybook/fix-switch-in-menu-item/assets/Default.stories-CdB2zq4R.js
@@ -0,0 +1,43 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{P as p,a as W,s as o,c as R}from"./PasswordCreationField-DVEkMriK.js";import"./index-BdpSHsi2.js";import{L as l}from"./Label-CowEq1IA.js";import{a as M}from"./index-B-lxVbXh.js";import{u as k,F as v,a as E}from"./Form-l244iBHo.js";import{s as I}from"./Action-D1RKKqts.js";import{B as c}from"./Button-Bm9B9Bx6.js";import{w as V}from"./IconWarning-C8AvogJ0.js";import"./PropsContextProvider-DL6oyv_5.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-BwWt-Nck.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./dynamic-DKDa4OpU.js";import"./FieldError-D2eBwE7I.js";import"./utils-q_Aud5Js.js";import"./Text-DtGJyZhh.js";import"./filterDOMProps-CeZl_uWj.js";import"./Group-wR-qYHNv.js";import"./useFocusRing-DY5AGIis.js";import"./useFocus-4XTC7e6o.js";import"./Input-Dom3MqAg.js";import"./Hidden-RoZfe91Z.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./useLabel-DtWExE0a.js";import"./Label-C5gSD-Mq.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./useControlledState-rjpdN6qd.js";import"./useFocusable-BXcKiciC.js";import"./useFormValidation-BP6joqgG.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-lrT2U8YF.js";import"./FieldDescription-B3YVhdFu.js";import"./Text-DeaNheZ5.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./index-BAMY2Nnw.js";import"./Tooltip-0Z6fTLJr.js";import"./OverlayArrow-fUY3Kb5L.js";import"./ProgressBar-DbWwTk60.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./context-CLozQwQq.js";import"./useStatic-DjsEYe3l.js";import"./ContextualHelpTrigger-wi1aJULs.js";import"./Popover-JXY9RFrz.js";import"./DialogTriggerView-Dab8gus5.js";import"./Dialog-HD9t1mUg.js";import"./Button-BV9jeieD.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./RSPContexts-Cbm9ZJUe.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Heading-Cvv6Q0q9.js";import"./Heading-PKR8zMP8.js";import"./v4-CtRu48qb.js";import"./getActionGroupSlot-Brr8Fby7.js";import"./LoadingSpinner-W5NKtl5_.js";const _={minComplexity:3,rules:[{ruleType:o.length,min:8,max:12},{ruleType:o.sequence,sequences:[R.number],maxLength:2},{ruleType:o.regex,pattern:"[A-B]",min:1,max:2},{ruleType:o.hibp},{identifier:"special",ruleType:o.charPool,charPools:["special"],min:1,max:2},{identifier:"numbers",ruleType:o.charPool,charPools:["numbers"],min:1,max:2},{ruleType:o.blocklist,blocklist:["foo","bar"],substringMatch:!0}]},to={title:"Form Controls/PasswordCreationField",component:p,render:e=>r.jsx(p,{onChange:M("onChange"),...e,children:r.jsx(l,{children:"Password"})})},t={},s={args:{isDisabled:!0}},i={args:{isRequired:!0}},a={args:{placeholder:"helloMoto"}},m={render:e=>r.jsxs(p,{...e,children:[r.jsx(l,{children:"Password"}),r.jsx(c,{children:r.jsx(V,{})})]})},n={render:()=>{const e=W.fromDeclaration(_),T=k({defaultValues:{password:""}});return r.jsxs(v,{form:T,onSubmit:async()=>await I(2e3),children:[r.jsx(E,{rules:{required:!0},name:"password",children:r.jsxs(p,{validationPolicy:e,children:[r.jsx(l,{children:"Password"}),r.jsx(c,{children:"asd"})]})}),r.jsx("br",{}),r.jsx(c,{type:"submit",children:"Submit"})]})}};var d,u,h;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(h=(u=t.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var b,x,f;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(f=(x=s.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};var P,g,y;i.parameters={...i.parameters,docs:{...(P=i.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(y=(g=i.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var w,F,j;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    placeholder: "helloMoto"
+  }
+}`,...(j=(F=a.parameters)==null?void 0:F.docs)==null?void 0:j.source}}};var C,D,B;m.parameters={...m.parameters,docs:{...(C=m.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => {
+    return <PasswordCreationField {...props}>
+        <Label>Password</Label>
+        <Button>
+          <IconDanger />
+        </Button>
+      </PasswordCreationField>;
+  }
+}`,...(B=(D=m.parameters)==null?void 0:D.docs)==null?void 0:B.source}}};var S,q,L;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: () => {
+    const policy = Policy.fromDeclaration(policyDecl);
+    const form = useForm({
+      defaultValues: {
+        password: ""
+      }
+    });
+    return <Form form={form} onSubmit={async () => await sleep(2000)}>
+        <Field rules={{
+        required: true
+      }} name="password">
+          <PasswordCreationField validationPolicy={policy}>
+            <Label>Password</Label>
+            <Button>asd</Button>
+          </PasswordCreationField>
+        </Field>
+        <br />
+        <Button type="submit">Submit</Button>
+      </Form>;
+  }
+}`,...(L=(q=n.parameters)==null?void 0:q.docs)==null?void 0:L.source}}};const so=["Default","Disabled","Required","WithPlaceholder","WithCustomButtons","WithForm"];export{t as Default,s as Disabled,i as Required,m as WithCustomButtons,n as WithForm,a as WithPlaceholder,so as __namedExportsOrder,to as default};
