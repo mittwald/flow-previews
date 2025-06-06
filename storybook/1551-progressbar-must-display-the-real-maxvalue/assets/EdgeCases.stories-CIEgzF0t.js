306a206ad8c05d1d7114d763cf9fd773e6b1602f
mittwald <>
2025-06-06 09:19:59 +0000

    Update from https://github.com/mittwald/flow/commit/aa4b166b943b46fc03db24e1e8a4a81539355adc

---
+++ storybook/1551-progressbar-must-display-the-real-maxvalue/assets/EdgeCases.stories-CIEgzF0t.js
@@ -0,0 +1,30 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{S as d}from"./Section-C1A6oshC.js";import"./index-BdpSHsi2.js";import{H as c}from"./Heading-B03Eq3Ad.js";import{T as u}from"./Text-tKX_BTQa.js";import{S as b}from"./Switch-LkYqaFNB.js";import S from"./Default.stories-BVkW08Qa.js";import{d as h}from"./dummyText-CX_I_Wpl.js";import{H as l}from"./Header-DH7DEqgK.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BHrENk1P.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-CMQsawDv.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./ContextMenuSection-DR11-L6h.js";import"./Action-Buc2j9bj.js";import"./context-B4MBEaY7.js";import"./useStatic-Bxlyqhmp.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-kjwYtmr_.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-HD9t1mUg.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./index-BAMY2Nnw.js";import"./Heading-PKR8zMP8.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./IconWarning-D5jaS_bU.js";import"./Label-BC8IxyiV.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./TextField-DQQON5CW.js";import"./TextFieldBase-CrJ6Ujba.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-Bi4fGWa7.js";import"./FieldError-D2eBwE7I.js";import"./FieldDescription-DM4LA8lv.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./Input-Dom3MqAg.js";import"./useTextField-xVyqyefx.js";import"./useFormValidation-BP6joqgG.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./Link-VQDEMaeu.js";import"./AlertBadge-B4wC1hOS.js";import"./AlertIcon-BjqAuw02.js";import"./Button-BjmsTBEw.js";import"./LoadingSpinner-CzNUOGve.js";import"./Modal-C31jYMh2.js";import"./DialogTriggerView-Ctd_lp0Q.js";import"./Overlay-DH2Dd8LG.js";import"./ButtonView-dlQgrw2e.js";import"./ContextualHelpTrigger-DLq8Q5h4.js";import"./Popover-B5n1qPeB.js";import"./ContextMenuContent-BaPgvwHd.js";import"./FileField-BFUwmrHS.js";import"./Form-DtMRHIMM.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";const Mt={...S,title:"Structure/Section/Edge Cases"},r={render:i=>t.jsxs(d,{...i,children:[t.jsxs(l,{children:[t.jsx(c,{children:h.medium}),t.jsx(b,{children:"Subscribed"})]}),t.jsx(u,{children:"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."})]})},o={render:i=>t.jsxs(d,{...i,children:[t.jsxs(l,{children:[t.jsx(c,{children:"Newsletter Subscription"}),t.jsx(b,{children:"Subscribed"})]}),t.jsx(u,{children:"Upcoming releases, new features and tips about your hosting - we bring the most important information to inbox. Subscribe to our newsletter and stay up to date."})]}),parameters:{viewport:{defaultViewport:"mobile1"}}};var e,m,p;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>{dummyText.medium}</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,s,a;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: props => <Section {...props}>
+      <Header>
+        <Heading>Newsletter Subscription</Heading>
+        <Switch>Subscribed</Switch>
+      </Header>
+      <Text>
+        Upcoming releases, new features and tips about your hosting - we bring
+        the most important information to inbox. Subscribe to our newsletter and
+        stay up to date.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(a=(s=o.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const Pt=["WithLongHeading","SmallSpace"];export{o as SmallSpace,r as WithLongHeading,Pt as __namedExportsOrder,Mt as default};
---
+++ storybook/1551-progressbar-must-display-the-real-maxvalue/assets/Variants.stories-Cmkn18Ww.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{A as n}from"./AlertBadge-B4wC1hOS.js";import f from"./Default.stories-CdSghvaU.js";import"./index-BdpSHsi2.js";import"./clsx-B-dksMZM.js";import"./AlertIcon-BjqAuw02.js";import"./IconWarning-D5jaS_bU.js";import"./PropsContextProvider-BHrENk1P.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-CMQsawDv.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Text-tKX_BTQa.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./index-BAMY2Nnw.js";const H={...f,title:"Status/AlertBadge/Status"},e={},s={render:r=>o.jsx(n,{...r,children:"Success"}),args:{status:"success"}},a={render:r=>o.jsx(n,{...r,children:"Warning"}),args:{status:"warning"}},t={render:r=>o.jsx(n,{...r,children:"Danger"}),args:{status:"danger"}};var p,c,m;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var i,d,g;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Success</AlertBadge>,
+  args: {
+    status: "success"
+  }
+}`,...(g=(d=s.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,l,S;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Warning</AlertBadge>,
+  args: {
+    status: "warning"
+  }
+}`,...(S=(l=a.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};var A,B,x;t.parameters={...t.parameters,docs:{...(A=t.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => <AlertBadge {...props}>Danger</AlertBadge>,
+  args: {
+    status: "danger"
+  }
+}`,...(x=(B=t.parameters)==null?void 0:B.docs)==null?void 0:x.source}}};const J=["Info","Success","Warning","Danger"];export{t as Danger,e as Info,s as Success,a as Warning,J as __namedExportsOrder,H as default};
