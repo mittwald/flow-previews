3cd2a7344f9d6ac1c80e0c8702174d43d47103a4
mittwald <>
2025-06-10 06:05:42 +0000

    Update from https://github.com/mittwald/flow/commit/203b83db89259ef931bc8e6ccd2a395ac925b267

---
+++ storybook-main/assets/EdgeCases.stories-DQtOS7Q7.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{a as g,C as c,M as m}from"./ContextMenuContent-HBBfijZJ.js";import"./ContextMenuSection-BiCz9vtm.js";import{B as l}from"./Button-Bm9B9Bx6.js";import M from"./Default.stories-Cht5LgHC.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./Action-D1RKKqts.js";import"./context-CLozQwQq.js";import"./PropsContextProvider-DL6oyv_5.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-BwWt-Nck.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useStatic-DjsEYe3l.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-Brr8Fby7.js";import"./dynamic-DKDa4OpU.js";import"./Popover-JXY9RFrz.js";import"./DialogTriggerView-Dab8gus5.js";import"./Dialog-HD9t1mUg.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./IconWarning-C8AvogJ0.js";import"./Switch-MbkYAMav.js";import"./Label-CowEq1IA.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./Text-DeaNheZ5.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./LoadingSpinner-W5NKtl5_.js";import"./Separator-BrRg1FQa.js";import"./Heading-Cvv6Q0q9.js";import"./Heading-PKR8zMP8.js";import"./Avatar-DATpe7LS.js";import"./AlertIcon-BcGOXbuX.js";import"./Initials-uzs-orCf.js";import"./react-children-utilities-DvaK_N8_.js";import"./index-SBtdq8jY.js";import"./onlyText-C2raRYWa.js";const _r={...M,title:"Actions/ContextMenu/Edge Cases"},t={args:{width:400},render:e=>r.jsxs(g,{children:[r.jsx(l,{children:"Trigger"}),r.jsxs(c,{...e,children:[r.jsx(m,{children:p.medium}),r.jsx(m,{children:p.medium})]})]})},o={render:e=>r.jsxs(g,{children:[r.jsx(l,{children:"Trigger"}),r.jsx(c,{...e,children:Array(20).fill("").map((T,i)=>r.jsxs(m,{children:["Item ",i+1]},i))})]})};var n,s,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    width: 400
+  },
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu {...props}>
+        <MenuItem>{dummyText.medium}</MenuItem>
+        <MenuItem>{dummyText.medium}</MenuItem>
+      </ContextMenu>
+    </ContextMenuTrigger>
+}`,...(u=(s=t.parameters)==null?void 0:s.docs)==null?void 0:u.source}}};var a,d,x;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu {...props}>
+        {Array(20).fill("").map((value, index) => <MenuItem key={index}>Item {index + 1}</MenuItem>)}
+      </ContextMenu>
+    </ContextMenuTrigger>
+}`,...(x=(d=o.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const kr=["LongTexts","LongList"];export{o as LongList,t as LongTexts,kr as __namedExportsOrder,_r as default};
---
+++ storybook-main/assets/FileCardList-S1eFC86b.js
@@ -0,0 +1,2 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{C as i}from"./ColumnLayout-BayVsf4Q.js";import{f as l,P as p}from"./PropsContextProvider-DL6oyv_5.js";const a=l("FileCardList",o=>{const{className:r,ref:t,...s}=o,n={FileCard:{elementType:"li"}};return e.jsx(p,{props:n,children:e.jsx(i,{ref:t,elementType:"ul",className:r,...s})})});a.__docgenInfo={description:`@flr-generate all
+@flr-clear-props-context`,methods:[],displayName:"FileCardList"};export{a as F};
