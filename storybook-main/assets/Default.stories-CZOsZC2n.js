ee1131cfd66036406851cd4707d58a3890e6963c
mittwald <>
2025-06-11 08:10:47 +0000

    Update from https://github.com/mittwald/flow/commit/da96aa6bb802e53aa40d5f919fb152eab7d52bb4

--- storybook-main/assets/Default.stories-CZOsZC2n.js
+++
@@ -1,5 +0,0 @@
-import{j as c}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{R as t}from"./Rating-BBNUZ1B6.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./IconWarning-DQz9hrro.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-nWMkLFdX.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-9dukh4eN.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-QO03R6A5.js";const _={title:"Content/Rating",component:t,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:n=>c.jsx(t,{...n})},r={},o={args:{size:"s"}};var e,s,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var m,i,p;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...(p=(i=o.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const y=["Default","Small"];export{r as Default,o as Small,y as __namedExportsOrder,_ as default};
