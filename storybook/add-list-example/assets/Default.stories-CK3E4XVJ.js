c90e6f4e5c575fcf2440602396bc3319fc969834
mittwald <>
2025-06-11 12:04:30 +0000

    Update from https://github.com/mittwald/flow/commit/a39f9129176dff4e7222021051060ca0cfe3f3a9

---
+++ storybook/add-list-example/assets/Default.stories-CK3E4XVJ.js
@@ -0,0 +1,17 @@
+import{A as S}from"./AlertIcon-DAcnqGtq.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./IconWarning-Di4PiSmu.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-O3IJWEjd.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-BVcpeqcI.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-QO03R6A5.js";const h={title:"Status/AlertIcon",component:S,parameters:{controls:{exclude:["render","tunnelId"]}},argTypes:{size:{control:"inline-radio",options:["s","m","l"]},status:{control:"inline-radio",options:["info","success","warning","danger"]}},args:{size:"m",status:"info"}},r={args:{status:"info"}},s={args:{status:"success"}},a={args:{status:"warning"}},e={args:{status:"danger"}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    status: "info"
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,i,m;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  }
+}`,...(m=(i=s.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,u,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    status: "warning"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,l,f;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "danger"
+  }
+}`,...(f=(l=e.parameters)==null?void 0:l.docs)==null?void 0:f.source}}};const j=["Info","Success","Warning","Danger"];export{e as Danger,r as Info,s as Success,a as Warning,j as __namedExportsOrder,h as default};
---
+++ storybook/add-list-example/assets/TextField.stories-DcPV0xT5.js
@@ -0,0 +1,30 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as j}from"./ActionGroup-BBRoi1Tq.js";import{B as p}from"./Button-BDCNWm9A.js";import{L as o}from"./Label-x24uvOko.js";import{S as f}from"./Section-B8GXMGGD.js";import{T as m}from"./TextField-DxjSvhBj.js";import{a as N,u as b,F,t as S}from"./Form-DGoGDSBD.js";import{s as g}from"./Action-BjNfAQ6U.js";import{a as y}from"./index-B-lxVbXh.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./PropsContextProvider-O3IJWEjd.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-BVcpeqcI.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-CWrB4Cta.js";import"./useStatic-DqBnXmq8.js";import"./IconWarning-Di4PiSmu.js";import"./Text-DBNV-VLM.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./LoadingSpinner-B54VIL4z.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Button-BV9jeieD.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./ContextMenuSection-Bwsgw7oA.js";import"./Dialog-HD9t1mUg.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./TextFieldBase-AKRSQqNx.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-BJS5eaZV.js";import"./FieldError-D2eBwE7I.js";import"./FieldDescription-D09iCrb0.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./Input-Dom3MqAg.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./useFormValidation-BP6joqgG.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./context-D3g-bGgj.js";import"./v4-CtRu48qb.js";const V=y("submit"),Me={title:"Integrations/React Hook Form/TextField",component:N,render:()=>{const s=async n=>{await g(1500),V(n)},t=b({defaultValues:{name:"",nameDefaultValue:"John",nameRequired:"",nameMaxLength:"",controlledName:""}}),r=S();return e.jsx(F,{form:t,onSubmit:s,children:e.jsxs(f,{children:[e.jsx(r,{name:"name",children:e.jsx(m,{children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"nameDefaultValue",children:e.jsx(m,{children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"nameRequired",rules:{required:"Please enter your name"},children:e.jsx(m,{children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"nameMaxLength",children:e.jsx(m,{maxLength:10,showCharacterCount:!0,children:e.jsx(o,{children:"Name"})})}),e.jsx(r,{name:"controlledName",children:e.jsx(m,{value:t.watch("controlledName").toUpperCase(),children:e.jsx(o,{children:"Name"})})}),e.jsxs(j,{children:[e.jsx(p,{type:"reset",children:"Reset"}),e.jsx(p,{type:"submit",children:"Submit"})]})]})})}},i={},a={render:()=>{const s=n=>{V(n)},t=b({defaultValues:{name:""}}),r=S();return e.jsx(F,{form:t,onSubmit:s,children:e.jsxs(f,{children:[e.jsx(r,{name:"name",children:e.jsx(m,{value:t.watch("name"),onChange:n=>{t.setValue("name",n.toUpperCase())},children:e.jsx(o,{children:"Name"})})}),e.jsx(j,{children:e.jsx(p,{type:"submit",children:"Submit"})})]})})}};var l,c,d;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(d=(c=i.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,h,x;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => {
+    interface Values {
+      name: string;
+    }
+    const handleOnSubmit = (values: Values) => {
+      submitAction(values);
+    };
+    const form = useForm<Values>({
+      defaultValues: {
+        name: ""
+      }
+    });
+    const Field = typedField(form);
+    return <Form form={form} onSubmit={handleOnSubmit}>
+        <Section>
+          <Field name="name">
+            <TextField value={form.watch("name")} onChange={val => {
+            form.setValue("name", val.toUpperCase());
+          }}>
+              <Label>Name</Label>
+            </TextField>
+          </Field>
+          <ActionGroup>
+            <Button type="submit">Submit</Button>
+          </ActionGroup>
+        </Section>
+      </Form>;
+  }
+}`,...(x=(h=a.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const We=["Default","WithTransformedValue"];export{i as Default,a as WithTransformedValue,We as __namedExportsOrder,Me as default};
