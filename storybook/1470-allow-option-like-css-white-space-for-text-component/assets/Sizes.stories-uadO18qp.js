78f1181d024ae637ad50c6f4183b1edffd3fdc01
mittwald <>
2025-06-11 11:36:46 +0000

    Update from https://github.com/mittwald/flow/commit/ba87c4e8b8392c3c58afa3aed5606e960496596b

---
+++ storybook/1470-allow-option-like-css-white-space-for-text-component/assets/Sizes.stories-uadO18qp.js
@@ -0,0 +1,13 @@
+import{j as u}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import g from"./Default.stories-BEXhQa5B.js";import{v as z}from"./IconWarning-Di4PiSmu.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-O3IJWEjd.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-BVcpeqcI.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";const C={...g,title:"Content/Icon/Sizes",args:{"aria-label":"home"},render:l=>u.jsx(z,{...l})},r={args:{size:"s"}},e={args:{size:"m"}},s={args:{size:"l"}};var o,a,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: "s"
+  }
+}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var m,i,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: "m"
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var p,c,d;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: "l"
+  }
+}`,...(d=(c=s.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const H=["Small","Medium","Large"];export{s as Large,e as Medium,r as Small,H as __namedExportsOrder,C as default};
