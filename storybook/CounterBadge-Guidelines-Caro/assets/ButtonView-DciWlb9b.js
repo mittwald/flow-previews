722a3a6748351f7e283a21683d675c2c2e8fef49
mittwald <>
2025-06-12 12:43:16 +0000

    Update from https://github.com/mittwald/flow/commit/1679c83bece5323e08460dd93b4e61386d04b3aa

---
+++ storybook/CounterBadge-Guidelines-Caro/assets/ButtonView-DciWlb9b.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-BdpSHsi2.js";import{B as n}from"./Button-816gfjTU.js";import{v as s}from"./PropsContextProvider-AVUEe6bt.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
