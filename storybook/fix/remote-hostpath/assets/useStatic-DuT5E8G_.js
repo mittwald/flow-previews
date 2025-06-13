07b55d7458b0e0f5daec20d5e65c4968e8f47080
mittwald <>
2025-06-13 09:05:49 +0000

    Update from https://github.com/mittwald/flow/commit/46140699c377cb9d4ccc8335142b084ddc333c89

---
+++ storybook/fix/remote-hostpath/assets/useStatic-DuT5E8G_.js
@@ -0,0 +1,1 @@
+import{b as a}from"./PropsContextProvider-AVUEe6bt.js";import{r as e}from"./index-BdpSHsi2.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
