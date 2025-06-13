a7c0ddba12b9a287d4348f5355d421243a8d3764
mittwald <>
2025-06-13 12:29:21 +0000

    Cleanup merged Storybook previews

--- storybook/fix/remote-hostpath/assets/Default.stories-CORFCZdQ.js
+++
@@ -1,7 +0,0 @@
-import{j as l}from"./jsx-runtime-D_zvdyIk.js";import{S as s}from"./SkeletonText-C6D8Hpjs.js";import"./index-BdpSHsi2.js";import{s as x}from"./storyBackgrounds-GFSAi-dI.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";const B={title:"Content/SkeletonText",component:s,render:a=>l.jsx(s,{...a}),parameters:{controls:{exclude:["className"]}}},r={},e={parameters:{backgrounds:x}},o={render:a=>l.jsx(s,{...a,width:"200px"})};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var p,m,d;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
-  parameters: {
-    backgrounds: storyBackgroundDark
-  }
-}`,...(d=(m=e.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var i,u,k;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  render: props => <SkeletonText {...props} width="200px" />
-}`,...(k=(u=o.parameters)==null?void 0:u.docs)==null?void 0:k.source}}};const W=["Default","OnDarkBackground","WithWidth"];export{r as Default,e as OnDarkBackground,o as WithWidth,W as __namedExportsOrder,B as default};
