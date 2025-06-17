fcc77f8bc359cf87eae05dbff93e61820ea40abd
mittwald <>
2025-06-17 12:08:40 +0000

    Update from https://github.com/mittwald/flow/commit/84699fdeaf473c9ce93aa0bd46987a835fdfa46e

---
+++ storybook-main/assets/Default.stories-CtC8v0a-.js
@@ -0,0 +1,61 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{M as s}from"./Message-b_I8lnfC.js";import{H as a}from"./Header-d9IPb-2O.js";import{B as m}from"./Button-uCpB0yle.js";import{A as p}from"./Align-DKEWaDUN.js";import{A as l}from"./Avatar-DTUu0cke.js";import{I as c}from"./Initials-DUn1yVRX.js";import{C as d}from"./Content-F9kNgdPX.js";import{T as e}from"./Text-D0fme2yZ.js";import{M as j}from"./MessageThread-ZpVqDwlf.js";import{C as x}from"./Chat-KaUeJfAd.js";import{d as h}from"./dummyText-CX_I_Wpl.js";import{T as u}from"./TextArea-CXhF4Yt5.js";import{g as B}from"./IconWarning-DQNrQpss.js";import{F as I}from"./FileCardList-G5bhJ7YI.js";import{F as M}from"./FileCard-Bu1OjOvj.js";import{u as v,F as y,a as H}from"./Form-w03Zju3c.js";import{a as U}from"./index-B-lxVbXh.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CVOihS9S.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-Ba4Kk3IG.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./LoadingSpinner-BfTav99-.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./AlertIcon-Txehx7oo.js";import"./react-children-utilities-DvaK_N8_.js";import"./index-SBtdq8jY.js";import"./onlyText-C2raRYWa.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./dynamic-DKDa4OpU.js";import"./TextFieldBase-B6s2Stu4.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-D_MiNKAS.js";import"./FieldError-D2eBwE7I.js";import"./FieldDescription-BukBgDJo.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./Input-Dom3MqAg.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./useControlledState-rjpdN6qd.js";import"./useFormValidation-BP6joqgG.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./ColumnLayout-Dkb2orsM.js";import"./Image-BlnGovO1.js";import"./Link-IADpNv8g.js";import"./OptionsButton-CpGsa3DX.js";import"./ButtonView-Db7BaMQ7.js";import"./ContextMenuContent-DLUIBTIb.js";import"./Action-cmnit0Vn.js";import"./context-BQxsMDuL.js";import"./useStatic-C2-dvEZN.js";import"./getActionGroupSlot-CDf0B89L.js";import"./ContextMenuSection-CiKvi5Te.js";import"./Dialog-HD9t1mUg.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Popover-ClV6Tp61.js";import"./DialogTriggerView-B3-XKKh2.js";import"./Switch-BWVDI9f8.js";import"./Label-fDnNtSZR.js";import"./useToggleState-C8sObcIl.js";import"./v4-CtRu48qb.js";const Me={title:"Chat/Chat",component:x,parameters:{controls:{exclude:["className"]}},args:{height:400},render:t=>r.jsxs(x,{...t,children:[r.jsxs(j,{children:[r.jsxs(s,{children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]}),r.jsxs(s,{type:"sender",children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"John Doe"})}),r.jsx(e,{children:r.jsx("b",{children:"John Doe"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})]}),r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:4}),r.jsx(m,{color:"accent",children:"Submit"})]})},o={},n={render:t=>r.jsxs(x,{...t,children:[r.jsx(j,{children:r.jsxs(s,{children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})}),r.jsx(m,{color:"secondary",variant:"plain","aria-label":"file attachment",children:r.jsx(B,{})}),r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:4}),r.jsx(m,{color:"accent",children:"Submit"}),r.jsxs(I,{children:[r.jsx(M,{name:"Uploaded File 1"}),r.jsx(M,{name:"Uploaded File 2"})]})]})},i={render:t=>{const S=v();return r.jsxs(x,{...t,children:[r.jsx(j,{children:r.jsxs(s,{children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})}),r.jsxs(y,{form:S,onSubmit:()=>{U("submitted")},children:[r.jsx(H,{name:"message",children:r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:4})}),r.jsx(m,{color:"accent",children:"Submit"})]})]})}};var g,f,b;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(b=(f=o.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var F,T,C;n.parameters={...n.parameters,docs:{...(F=n.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: props => <Chat {...props}>
+      <MessageThread>
+        <Message>
+          <Header>
+            <Align>
+              <Avatar>
+                <Initials>Max Mustermann</Initials>
+              </Avatar>
+              <Text>
+                <b>Max Mustermann</b>
+              </Text>
+            </Align>
+          </Header>
+          <Content>
+            <Text>{dummyText.long}</Text>
+          </Content>
+        </Message>
+      </MessageThread>
+      <Button color="secondary" variant="plain" aria-label="file attachment">
+        <IconAttachment />
+      </Button>
+      <TextArea aria-label="message" rows={1} autoResizeMaxRows={4} />
+      <Button color="accent">Submit</Button>
+      <FileCardList>
+        <FileCard name="Uploaded File 1" />
+        <FileCard name="Uploaded File 2" />
+      </FileCardList>
+    </Chat>
+}`,...(C=(T=n.parameters)==null?void 0:T.docs)==null?void 0:C.source}}};var A,R,w;i.parameters={...i.parameters,docs:{...(A=i.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Chat {...props}>
+        <MessageThread>
+          <Message>
+            <Header>
+              <Align>
+                <Avatar>
+                  <Initials>Max Mustermann</Initials>
+                </Avatar>
+                <Text>
+                  <b>Max Mustermann</b>
+                </Text>
+              </Align>
+            </Header>
+            <Content>
+              <Text>{dummyText.long}</Text>
+            </Content>
+          </Message>
+        </MessageThread>
+        <Form form={form} onSubmit={() => {
+        action("submitted");
+      }}>
+          <Field name="message">
+            <TextArea aria-label="message" rows={1} autoResizeMaxRows={4} />
+          </Field>
+          <Button color="accent">Submit</Button>
+        </Form>
+      </Chat>;
+  }
+}`,...(w=(R=i.parameters)==null?void 0:R.docs)==null?void 0:w.source}}};const ge=["Default","WithUpload","WithForm"];export{o as Default,i as WithForm,n as WithUpload,ge as __namedExportsOrder,Me as default};
---
+++ storybook-main/assets/EdgeCases.stories-Cmwj0K72.js
@@ -0,0 +1,176 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{A as W}from"./AlertBadge-Dvjyh5uM.js";import{A as N}from"./Avatar-DTUu0cke.js";import{C as B}from"./Content-F9kNgdPX.js";import{C as P,M as S}from"./ContextMenuContent-DLUIBTIb.js";import"./ContextMenuSection-CiKvi5Te.js";import{H as L}from"./Heading-DWO6DuZV.js";import{I as Z}from"./Initials-DUn1yVRX.js";import{i as _}from"./browser-BJKb5O22.js";import{D as p}from"./NotificationProvider-D1ZLXtyS.js";import{R as x,L as $}from"./List-D1B6Cnay.js";import"./index-BdpSHsi2.js";import{t as d}from"./typedList-B8IQFfUG.js";import{S as O}from"./Section-BzK2qmZ9.js";import{T as s}from"./Text-D0fme2yZ.js";import{a as U,K}from"./IconWarning-DQNrQpss.js";import"./AccentBox-DWKCYSmA.js";import"./Accordion-BVAdx6Bw.js";import"./Action-cmnit0Vn.js";import"./ActionGroup-iSr2V8YW.js";import"./Activity-Crl60Rue.js";import"./Alert-yFNc5Aj8.js";import"./AlertIcon-Txehx7oo.js";import"./Align-DKEWaDUN.js";import"./ChartLegend-CZUtF3UB.js";import"./AvatarStack-C0Y93ok-.js";import"./Badge-CVBxggCa.js";import"./BigNumber-B67DMJaU.js";import"./Breadcrumb-CplnwTwg.js";import"./context-BQxsMDuL.js";import"./PropsContextProvider-CVOihS9S.js";import"./Button-uCpB0yle.js";import"./Chat-KaUeJfAd.js";import{C as q}from"./Checkbox-CTTdOmUF.js";import"./CheckboxButton-Bq4V1McQ.js";import"./CheckboxGroup-TsIFfZPY.js";import"./CodeBlock-BeX8ZTUv.js";import"./Color-Bo2dSxzi.js";import"./ColumnLayout-Dkb2orsM.js";import"./ComboBox-Dm9Yj6xU.js";import"./ContextualHelpTrigger-BZM2fneb.js";import"./CopyButton-DWwrYowA.js";import"./CounterBadge-BEAMQpl3.js";import"./DatePicker-DB5BG0og.js";import"./DateRangePicker-Chs-PjL1.js";import"./DivView-BLwcm6Gu.js";import"./FieldDescription-BukBgDJo.js";import"./FieldError-D_MiNKAS.js";import"./FileCard-Bu1OjOvj.js";import"./FileCardList-G5bhJ7YI.js";import"./FileDropZone-fJc3-RSq.js";import"./FileField-BkQE_Ww5.js";import"./Flex-GWFKGCBE.js";import"./Header-d9IPb-2O.js";import"./HeaderNavigation-iOtpNWQy.js";import"./IllustratedMessage-DjX18e0M.js";import"./Image-BlnGovO1.js";import"./InlineCode-CEoZCPUU.js";import"./Label-fDnNtSZR.js";import"./LabeledValue-BCp2WzCk.js";import"./LayoutCard-DW8tbF-b.js";import"./Legend-BZKYDVoc.js";import"./LightBox-Ct6CqlEE.js";import"./Link-IADpNv8g.js";import"./LoadingSpinner-BfTav99-.js";import"./Markdown-C42MI0la.js";import"./Message-b_I8lnfC.js";import"./MessageSeparator-B7p6F_Xa.js";import"./MessageThread-ZpVqDwlf.js";import"./Modal-B1t4NESB.js";import"./NavigationGroup-CnjnJob7.js";import"./Notification-B8fGCUVR.js";import"./NumberField-Dl-V0wHB.js";import"./Option-6XafxChS.js";import"./Overlay-5M6Z3xOC.js";import"./DialogTriggerView-B3-XKKh2.js";import"./PasswordCreationField-Xm5qALP-.js";import"./Popover-ClV6Tp61.js";import"./ProgressBar-LTRyX2BS.js";import"./RadioButton-DsY6q8IW.js";import"./Rating-CEbCRr9E.js";import"./SearchField-B4B0nKSu.js";import"./Segment-D4CdPF-Z.js";import"./CountryOptions-COuIcItj.js";import"./Separator-BrRg1FQa.js";import"./Skeleton-ZxV5AzDo.js";import"./SkeletonText-C6D8Hpjs.js";import"./Slider-D9NBEVsa.js";import"./Switch-BWVDI9f8.js";import"./Table-DmvQQYH0.js";import"./Tab-C0lSe3SZ.js";import"./TextArea-CXhF4Yt5.js";import"./TextField-BcxZnQCi.js";import"./TimeField-CiBEy-Ma.js";import"./Tooltip-0Z6fTLJr.js";import{m as G,u as b,d as Q}from"./Default.stories-DxRFBzHT.js";import"./clsx-B-dksMZM.js";import"./Dialog-HD9t1mUg.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./mergeRefs-COuk0-v5.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Heading-PKR8zMP8.js";import"./react-children-utilities-DvaK_N8_.js";import"./index-SBtdq8jY.js";import"./onlyText-C2raRYWa.js";import"./useStatic-C2-dvEZN.js";import"./OptionsButton-CpGsa3DX.js";import"./ButtonView-Db7BaMQ7.js";import"./DragAndDrop-1oT9LeNd.js";import"./ListBox-2j2qa_ow.js";import"./inertValue-Cx-SdfM1.js";import"./useListState-eKLbauDG.js";import"./useHasTabbableChild-BIQJ9OEZ.js";import"./dynamic-DKDa4OpU.js";import"./index-BAMY2Nnw.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./getActionGroupSlot-CDf0B89L.js";import"./iframe-Ba4Kk3IG.js";import"./FieldError-D2eBwE7I.js";import"./Form-CSvT4C5Q.js";import"./useFormValidation-BP6joqgG.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./FormField.module-B9AzUSUD.js";import"./Group-wR-qYHNv.js";import"./Input-Dom3MqAg.js";import"./useFilter-ClmrtTI7.js";import"./useSingleSelectListState-L7tbABOK.js";import"./useUpdateEffect-CMGfMYOO.js";import"./useTextField-xVyqyefx.js";import"./DateField-CbnNDRXl.js";import"./useSpinButton-PhnS8dt7.js";import"./RangeCalendar-4C8z4HE4.js";import"./intlStrings-ZMTSFH_M.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./TextField-BqNO7Lq4.js";import"./RadioGroup-CCpKxIOR.js";import"./TextFieldBase-B6s2Stu4.js";const I={bigInt:(i,e,r)=>{const n=i.getValue(r),o=e.getValue(r);if(n==null)return o==null?0:-1;if(o==null)return 1;try{return _(typeof n=="bigint"&&typeof o=="bigint",`Expected BigInt values, got ${typeof n} and ${typeof o}`),n<o?-1:n>o?1:0}catch(a){console.error(`Error in BigInt comparison: ${a}`)}},dateTime:(i,e,r)=>{const n=i.getValue(r),o=e.getValue(r);if(n==null)return o==null?0:-1;if(o==null)return 1;let a=null,m=null;return n instanceof p?a=n:typeof n=="string"&&(a=p.fromISO(n)),o instanceof p?m=o:typeof o=="string"&&(m=p.fromISO(o)),a!=null&&a.isValid&&(m!=null&&m.isValid)?a.toMillis()-m.toMillis():a!=null&&a.isValid?-1:m!=null&&m.isValid?1:(console.warn("Invalid DateTime values for sorting."),0)}},oo={...G,title:"Structure/List/Edge Cases"},X=()=>new Promise(i=>window.setTimeout(i,2e3)),h=async i=>(await X(),`${i}@info.de`),c={render:()=>{const i=d();return t.jsxs(i.List,{batchSize:5,"aria-label":"List",children:[t.jsx(i.StaticData,{data:[{name:"John"},{name:"Max"}]}),t.jsx(i.Item,{textValue:e=>e.name,children:e=>t.jsx(x,{children:()=>{const r=b(h,[e.name]);return t.jsxs($,{children:[t.jsx(L,{children:e.name}),t.jsx(s,{children:r})]})}})})]})}},g={render:()=>{const i=d();return t.jsxs(i.List,{defaultViewMode:"tiles",batchSize:5,"aria-label":"List",children:[t.jsx(i.StaticData,{data:[{name:"John"},{name:"Max"}]}),t.jsx(i.Item,{showTiles:!0,showList:!1,textValue:e=>e.name,children:e=>t.jsx(x,{children:()=>{const r=b(h,[e.name]);return t.jsxs($,{children:[t.jsx(L,{children:e.name}),t.jsx(s,{children:r})]})}})})]})}},u={render:()=>{const i=d();return t.jsxs(i.List,{batchSize:5,"aria-label":"List",defaultViewMode:"table",children:[t.jsx(i.StaticData,{data:[{name:"John"},{name:"Max"}]}),t.jsxs(i.Table,{children:[t.jsxs(i.TableHeader,{children:[t.jsx(i.TableColumn,{children:"Name"}),t.jsx(i.TableColumn,{children:"Email"})]}),t.jsx(i.TableBody,{children:t.jsxs(i.TableRow,{children:[t.jsx(i.TableCell,{children:e=>e.name}),t.jsx(i.TableCell,{children:e=>t.jsx(x,{children:()=>b(h,[e.name])})})]})})]})]})}},T={render:()=>{const i=d();return t.jsxs(i.List,{children:[t.jsx(i.StaticData,{data:[{name:"John Doe"}]}),t.jsx(i.Item,{showTiles:!0,textValue:e=>e.name,children:e=>t.jsxs(i.ItemView,{children:[t.jsx(q,{"aria-label":"select user"}),t.jsx(N,{children:t.jsx(Z,{children:e.name})}),t.jsxs(L,{children:["HeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeading",t.jsx(W,{status:"warning",children:"AlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadge"})]}),t.jsx(s,{children:"SubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitle"}),t.jsx(B,{slot:"top",children:"TopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTop"}),t.jsx(B,{slot:"bottom",children:"BottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottom"}),t.jsx(P,{children:t.jsx(S,{children:"Show details"})})]})})]})}},j=(i,e,r)=>{const n=String(i.getValue(r)||""),o=String(e.getValue(r)||"");return n.length-o.length},Y=(i,e,r)=>{const n=i.getValue(r),o=e.getValue(r);return n==="Domain"&&o==="Subdomain"?-1:n==="Subdomain"&&o==="Domain"?1:String(n).localeCompare(String(o))},l=()=>{const i=Q.map((o,a)=>{const m=a*3+Math.floor(Math.random()*5),k=BigInt(1e12+a);return{...o,id:k,createdAt:p.now().minus({days:m})}}),e=d(),r=I.bigInt,n=I.dateTime;return t.jsx(O,{children:t.jsxs(e.List,{batchSize:10,children:[t.jsx(e.StaticData,{data:i}),t.jsx(e.Sorting,{property:"hostname",name:"Name A bis Z",direction:"asc"}),t.jsx(e.Sorting,{property:"hostname",name:"Name Z bis A",direction:"desc"}),t.jsx(e.Sorting,{property:"id",name:"ID (aufsteigend)",direction:"asc",customSortingFn:r}),t.jsx(e.Sorting,{property:"id",name:"ID (absteigend)",direction:"desc",customSortingFn:r,defaultEnabled:!0}),t.jsx(e.Sorting,{property:"createdAt",name:"Erstelldatum (älteste zuerst)",direction:"asc",customSortingFn:n}),t.jsx(e.Sorting,{property:"createdAt",name:"Erstelldatum (neueste zuerst)",direction:"desc",customSortingFn:n,defaultEnabled:!0}),t.jsx(e.Sorting,{property:"tld",name:"TLD-Länge (kürzeste zuerst)",direction:"asc",customSortingFn:j}),t.jsx(e.Sorting,{property:"tld",name:"TLD-Länge (längste zuerst)",direction:"desc",customSortingFn:j}),t.jsx(e.Sorting,{property:"type",name:"Typ (Domains zuerst)",direction:"asc",customSortingFn:Y}),t.jsx(e.Item,{children:o=>{var a;return t.jsxs(e.ItemView,{children:[t.jsx(N,{color:o.type==="Domain"?"blue":"teal",children:o.type==="Domain"?t.jsx(U,{}):t.jsx(K,{})}),t.jsxs(L,{children:[o.hostname,!o.verified&&t.jsx(W,{status:"warning",children:"Unverifiziert"})]}),t.jsx(s,{children:o.type}),t.jsxs(s,{children:["ID: ",o.id]}),t.jsxs(s,{children:["TLD: ",o.tld]}),t.jsxs(s,{children:["Erstellt am: ",(a=o.createdAt)==null?void 0:a.toLocaleString()]}),t.jsxs(P,{children:[t.jsx(S,{children:"Details anzeigen"}),t.jsx(S,{children:"Löschen"})]})]})}})]})})};l.__docgenInfo={description:"",methods:[],displayName:"CustomSortingList"};var D,f,y;c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List batchSize={5} aria-label="List">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Item textValue={item => item.name}>
+          {item => <Render>
+              {() => {
+            const email = usePromise(getEmail, [item.name]);
+            return <ListItemView>
+                    <Heading>{item.name}</Heading>
+                    <Text>{email}</Text>
+                  </ListItemView>;
+          }}
+            </Render>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(y=(f=c.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var A,H,w;g.parameters={...g.parameters,docs:{...(A=g.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List defaultViewMode="tiles" batchSize={5} aria-label="List">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Item showTiles showList={false} textValue={item => item.name}>
+          {item => <Render>
+              {() => {
+            const email = usePromise(getEmail, [item.name]);
+            return <ListItemView>
+                    <Heading>{item.name}</Heading>
+                    <Text>{email}</Text>
+                  </ListItemView>;
+          }}
+            </Render>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(w=(H=g.parameters)==null?void 0:H.docs)==null?void 0:w.source}}};var V,C,M;u.parameters={...u.parameters,docs:{...(V=u.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List batchSize={5} aria-label="List" defaultViewMode="table">
+        <List.StaticData data={[{
+        name: "John"
+      }, {
+        name: "Max"
+      }]} />
+
+        <List.Table>
+          <List.TableHeader>
+            <List.TableColumn>Name</List.TableColumn>
+            <List.TableColumn>Email</List.TableColumn>
+          </List.TableHeader>
+
+          <List.TableBody>
+            <List.TableRow>
+              <List.TableCell>{item => item.name}</List.TableCell>
+              <List.TableCell>
+                {item => <Render>
+                    {() => {
+                  return usePromise(getEmail, [item.name]);
+                }}
+                  </Render>}
+              </List.TableCell>
+            </List.TableRow>
+          </List.TableBody>
+        </List.Table>
+      </List.List>;
+  }
+}`,...(M=(C=u.parameters)==null?void 0:C.docs)==null?void 0:M.source}}};var z,F,E;T.parameters={...T.parameters,docs:{...(z=T.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: () => {
+    const List = typedList<{
+      name: string;
+    }>();
+    return <List.List>
+        <List.StaticData data={[{
+        name: "John Doe"
+      }]} />
+        <List.Item showTiles textValue={user => user.name}>
+          {user => <List.ItemView>
+              <Checkbox aria-label="select user" />
+              <Avatar>
+                <Initials>{user.name}</Initials>
+              </Avatar>
+              <Heading>
+                HeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeadingHeading
+                <AlertBadge status="warning">
+                  AlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadgeAlertBadge
+                </AlertBadge>
+              </Heading>
+              <Text>
+                SubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitleSubtitle
+              </Text>
+              <Content slot="top">
+                TopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTopTop
+              </Content>
+              <Content slot="bottom">
+                BottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottomBottom
+              </Content>
+              <ContextMenu>
+                <MenuItem>Show details</MenuItem>
+              </ContextMenu>
+            </List.ItemView>}
+        </List.Item>
+      </List.List>;
+  }
+}`,...(E=(F=T.parameters)==null?void 0:F.docs)==null?void 0:E.source}}};var v,R,J;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`() => {
+  const domainsWithDateTime = domains.map((domain, index) => {
+    const daysAgo = index * 3 + Math.floor(Math.random() * 5);
+    const bigIntId = BigInt(1000000000000 + index);
+    return {
+      ...domain,
+      id: bigIntId,
+      createdAt: DateTime.now().minus({
+        days: daysAgo
+      })
+    };
+  });
+  const DomainList = typedList<DomainWithBigIntId>();
+  const bigIntSorting = SortingFunctions.bigInt as SortingFn<DomainWithBigIntId>;
+  const dateTimeSorting = SortingFunctions.dateTime as SortingFn<DomainWithBigIntId>;
+  return <Section>
+      <DomainList.List batchSize={10}>
+        <DomainList.StaticData data={domainsWithDateTime} />
+
+        <DomainList.Sorting property="hostname" name="Name A bis Z" direction="asc" />
+        <DomainList.Sorting property="hostname" name="Name Z bis A" direction="desc" />
+
+        <DomainList.Sorting property="id" name="ID (aufsteigend)" direction="asc" customSortingFn={bigIntSorting} />
+        <DomainList.Sorting property="id" name="ID (absteigend)" direction="desc" customSortingFn={bigIntSorting} defaultEnabled />
+
+        <DomainList.Sorting property="createdAt" name="Erstelldatum (älteste zuerst)" direction="asc" customSortingFn={dateTimeSorting} />
+
+        <DomainList.Sorting property="createdAt" name="Erstelldatum (neueste zuerst)" direction="desc" customSortingFn={dateTimeSorting} defaultEnabled />
+
+        <DomainList.Sorting property="tld" name="TLD-Länge (kürzeste zuerst)" direction="asc" customSortingFn={tldLengthSortingFn} />
+        <DomainList.Sorting property="tld" name="TLD-Länge (längste zuerst)" direction="desc" customSortingFn={tldLengthSortingFn} />
+
+        <DomainList.Sorting property="type" name="Typ (Domains zuerst)" direction="asc" customSortingFn={domainTypeSortingFn} />
+
+        <DomainList.Item>
+          {domain => <DomainList.ItemView>
+              <Avatar color={domain.type === "Domain" ? "blue" : "teal"}>
+                {domain.type === "Domain" ? <IconDomain /> : <IconSubdomain />}
+              </Avatar>
+              <Heading>
+                {domain.hostname}
+                {!domain.verified && <AlertBadge status="warning">Unverifiziert</AlertBadge>}
+              </Heading>
+              <Text>{domain.type}</Text>
+              <Text>ID: {domain.id}</Text>
+              <Text>TLD: {domain.tld}</Text>
+              <Text>Erstellt am: {domain.createdAt?.toLocaleString()}</Text>
+              <ContextMenu>
+                <MenuItem>Details anzeigen</MenuItem>
+                <MenuItem>Löschen</MenuItem>
+              </ContextMenu>
+            </DomainList.ItemView>}
+        </DomainList.Item>
+      </DomainList.List>
+    </Section>;
+}`,...(J=(R=l.parameters)==null?void 0:R.docs)==null?void 0:J.source}}};const no=["LoadingListItem","LoadingTile","LoadingTableCell","VeryLongWords","CustomSortingList"];export{l as CustomSortingList,c as LoadingListItem,u as LoadingTableCell,g as LoadingTile,T as VeryLongWords,no as __namedExportsOrder,oo as default};
