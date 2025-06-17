bdd8d097435917d87131012e2cfce0a798be8be2
mittwald <>
2025-06-17 13:59:44 +0000

    Update from https://github.com/mittwald/flow/commit/d447a88b41d9ced070b94256676d78d07a90b9de

---
+++ storybook-main/assets/Default.stories-_MtYyTiV.js
@@ -0,0 +1,52 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{A as i,C as m,a as r,X as s,Y as n,b as d,c as y}from"./ChartLegend-DD1djNRK.js";import{I as C}from"./IllustratedMessage-hSSqek32.js";import"./AccentBox-DLrhsbu1.js";import"./Accordion-BuE-DDQT.js";import"./Action-Dh7Im5JG.js";import"./ActionGroup-CfVfeeGS.js";import"./Activity-Crl60Rue.js";import"./Alert-CHFXmRny.js";import"./AlertBadge-BykYSRAi.js";import"./AlertIcon-DeD6EX5U.js";import"./Align-Dvm-xDWF.js";import"./Avatar-CMYElceY.js";import"./AvatarStack-CHT0a9Fr.js";import"./Badge-C5AeQxEC.js";import"./BigNumber-BMFFodN2.js";import"./Breadcrumb-BFpCHJ_4.js";import"./index-BdpSHsi2.js";import"./context-CBT0xkbu.js";import"./PropsContextProvider-8e6_V-we.js";import"./Button-BaLxRHou.js";import"./Chat-BEXeGjnA.js";import"./Checkbox-DHisugKw.js";import"./CheckboxButton-dn4WwIpv.js";import"./CheckboxGroup-COVL91WR.js";import"./CodeBlock-BXnugd8d.js";import"./Color-Bo2dSxzi.js";import"./ColumnLayout-DN4NXfac.js";import"./ComboBox-133Qm4jJ.js";import"./Content-BEglEj7s.js";import"./ContextMenuContent-CeB9QaOT.js";import"./ContextMenuSection-D3ioSDSj.js";import"./ContextualHelpTrigger-Cu7EYleJ.js";import"./CopyButton-cti1KIua.js";import"./CounterBadge-EklUnLTp.js";import"./DatePicker-Cma0Aa7G.js";import"./DateRangePicker-JJyj3d8X.js";import"./DivView-yChHcg7R.js";import"./FieldDescription-B5U3ehLm.js";import"./FieldError-BFmIIc0-.js";import"./FileCard-NXd8Z6JE.js";import"./FileCardList-B9fCYkgc.js";import"./FileDropZone-DNNR8M8I.js";import"./FileField-KOpw3yc6.js";import"./Flex-GWFKGCBE.js";import"./Header-DOhAzRfX.js";import"./HeaderNavigation-Cjh3nW6h.js";import{H as f}from"./Heading-IeaK_UGJ.js";import{b as S}from"./IconWarning-Bmni-YAQ.js";import"./Image-DQqGJjVO.js";import"./Initials-C03DBOny.js";import"./InlineCode-CL3_YtTH.js";import"./Label-ThqHcUi-.js";import"./LabeledValue-_HGNnInh.js";import"./LayoutCard-BFr747rG.js";import"./Legend-CQdSIv4H.js";import"./LightBox-BHF8L1L5.js";import"./Link-Bl8j2Cht.js";import"./browser-BJKb5O22.js";import"./List-DoCq2PUM.js";import"./LoadingSpinner-BHkd9yXV.js";import"./Markdown-DiQCqACl.js";import"./Message-ZaQziAys.js";import"./MessageSeparator-BLXGGetY.js";import"./MessageThread-BH-W9ibG.js";import"./Modal-BGgrpFje.js";import"./NavigationGroup-1iu8HYEq.js";import"./Notification-eRCzv1g9.js";import"./NotificationProvider-BzVkq6Ob.js";import"./NumberField-GYUVbnOC.js";import"./Option-DRHWMN9g.js";import"./Overlay-BDuz8DfV.js";import"./DialogTriggerView-Cb0RhJK_.js";import"./PasswordCreationField-DSiaX9aB.js";import"./Popover-DIzfrEmb.js";import"./ProgressBar-sCgZD-lv.js";import"./RadioButton-XSCGxnm9.js";import"./Rating-rvvoAG1r.js";import"./SearchField-DXMX-ceq.js";import"./Section-dBivT6zE.js";import"./Segment-CLvDO74G.js";import"./CountryOptions-CVAb_C_2.js";import"./Separator-BrRg1FQa.js";import"./Skeleton-ZxV5AzDo.js";import"./SkeletonText-C6D8Hpjs.js";import"./Slider-C4HeEiyY.js";import"./Switch-DkM4w1UO.js";import"./Table-CEVbPoug.js";import"./Tab-Drf3p-aD.js";import"./Text-BfrxO2um.js";import"./TextArea-BEzZdbOC.js";import"./TextField-BcLgMEit.js";import"./TimeField-DfiiJUq8.js";import"./Tooltip-0Z6fTLJr.js";import"./clsx-B-dksMZM.js";import"./OverlayArrow-fUY3Kb5L.js";import"./mergeRefs-COuk0-v5.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./useFocus-4XTC7e6o.js";import"./ProgressBar-DbWwTk60.js";import"./utils-q_Aud5Js.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-rjpdN6qd.js";import"./index-BAMY2Nnw.js";import"./dynamic-DKDa4OpU.js";import"./useStatic-wEStvoUF.js";import"./getActionGroupSlot-CV6IWFNw.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./useFocusable-BXcKiciC.js";import"./iframe-CKPxcNbh.js";import"./Button-BV9jeieD.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocusRing-DY5AGIis.js";import"./RSPContexts-Cbm9ZJUe.js";import"./FieldError-D2eBwE7I.js";import"./Text-DtGJyZhh.js";import"./Form-CSvT4C5Q.js";import"./useFormValidation-BP6joqgG.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./FormField.module-B9AzUSUD.js";import"./Group-wR-qYHNv.js";import"./Input-Dom3MqAg.js";import"./ListBox-2j2qa_ow.js";import"./DragAndDrop-1oT9LeNd.js";import"./useCollator-DWYYoDxV.js";import"./Dialog-HD9t1mUg.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./FocusScope-D_x47-q0.js";import"./inertValue-Cx-SdfM1.js";import"./useListState-eKLbauDG.js";import"./useFilter-ClmrtTI7.js";import"./useSingleSelectListState-L7tbABOK.js";import"./useUpdateEffect-CMGfMYOO.js";import"./useTextField-xVyqyefx.js";import"./react-children-utilities-DvaK_N8_.js";import"./onlyText-C2raRYWa.js";import"./DateField-CbnNDRXl.js";import"./useSpinButton-PhnS8dt7.js";import"./RangeCalendar-JR4l8cfV.js";import"./Heading-PKR8zMP8.js";import"./OptionsButton-BLH4BL52.js";import"./ButtonView-D8mPE87d.js";import"./intlStrings-ZMTSFH_M.js";import"./index-SBtdq8jY.js";import"./useHasTabbableChild-BIQJ9OEZ.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./TextField-BqNO7Lq4.js";import"./RadioGroup-CCpKxIOR.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./TextFieldBase-CFqPw3rJ.js";const v=[{name:"Stat 1",firstKey:4e3,secondKey:2400,thirdKey:2400},{name:"Stat 2",firstKey:3e3,secondKey:1398,thirdKey:2210},{name:"Stat 3",firstKey:2e3,secondKey:9800,thirdKey:2290},{name:"Stat 4",firstKey:2780,secondKey:3908,thirdKey:2e3},{name:"Stat 5",firstKey:1890,secondKey:4800,thirdKey:2181},{name:"Stat 6",firstKey:2390,secondKey:3800,thirdKey:2500},{name:"Stat 7",firstKey:3490,secondKey:4300,thirdKey:2100}],Ie={title:"Data Visualisation/AreaChart",component:i,parameters:{controls:{exclude:["className"]}},args:{data:v},argTypes:{}},o={render:e=>t.jsxs(i,{...e,height:"70vh",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey"}),t.jsx(r,{dataKey:"secondKey",color:"palatinate-blue"}),t.jsx(r,{dataKey:"thirdKey",color:"tangerine"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{interval:"equidistantPreserveStart"}),t.jsx(d,{}),t.jsx(y,{})]})},a={render:e=>t.jsxs(t.Fragment,{children:[t.jsxs(i,{...e,height:"250px",syncId:"syncedAreaCharts",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey"}),t.jsx(r,{dataKey:"secondKey",color:"palatinate-blue"}),t.jsx(r,{dataKey:"thirdKey",color:"tangerine"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{}),t.jsx(d,{}),t.jsx(y,{})]}),t.jsxs(i,{...e,height:"250px",syncId:"syncedAreaCharts",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey",color:"magenta"}),t.jsx(r,{dataKey:"secondKey",color:"tropical-indigo"}),t.jsx(r,{dataKey:"thirdKey",color:"malachite"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{}),t.jsx(d,{}),t.jsx(y,{})]})]})},p={render:()=>{const e=()=>t.jsxs(C,{children:[t.jsx(S,{}),t.jsx(f,{children:"Keine Daten verfügbar"})]});return t.jsxs(i,{emptyView:e,height:"70vh",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey"}),t.jsx(r,{dataKey:"secondKey",color:"palatinate-blue"}),t.jsx(r,{dataKey:"thirdKey",color:"tangerine"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{interval:"equidistantPreserveStart"}),t.jsx(d,{}),t.jsx(y,{})]})}};var c,K,h;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <AreaChart {...props} height="70vh">
+      <CartesianGrid />
+      <Area dataKey="firstKey" />
+      <Area dataKey="secondKey" color="palatinate-blue" />
+      <Area dataKey="thirdKey" color="tangerine" />
+      <XAxis dataKey="name" />
+      <YAxis interval="equidistantPreserveStart" />
+      <ChartTooltip />
+      <ChartLegend />
+    </AreaChart>
+}`,...(h=(K=o.parameters)==null?void 0:K.docs)==null?void 0:h.source}}};var l,x,j;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: props => <>
+      <AreaChart {...props} height="250px" syncId="syncedAreaCharts">
+        <CartesianGrid />
+        <Area dataKey="firstKey" />
+        <Area dataKey="secondKey" color="palatinate-blue" />
+        <Area dataKey="thirdKey" color="tangerine" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <ChartTooltip />
+        <ChartLegend />
+      </AreaChart>
+      <AreaChart {...props} height="250px" syncId="syncedAreaCharts">
+        <CartesianGrid />
+        <Area dataKey="firstKey" color="magenta" />
+        <Area dataKey="secondKey" color="tropical-indigo" />
+        <Area dataKey="thirdKey" color="malachite" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <ChartTooltip />
+        <ChartLegend />
+      </AreaChart>
+    </>
+}`,...(j=(x=a.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var g,A,u;p.parameters={...p.parameters,docs:{...(g=p.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => {
+    const emptyView = () => <IllustratedMessage>
+        <IconMonitoring />
+        <Heading>Keine Daten verfügbar</Heading>
+      </IllustratedMessage>;
+    return <AreaChart emptyView={emptyView} height="70vh">
+        <CartesianGrid />
+        <Area dataKey="firstKey" />
+        <Area dataKey="secondKey" color="palatinate-blue" />
+        <Area dataKey="thirdKey" color="tangerine" />
+        <XAxis dataKey="name" />
+        <YAxis interval="equidistantPreserveStart" />
+        <ChartTooltip />
+        <ChartLegend />
+      </AreaChart>;
+  }
+}`,...(u=(A=p.parameters)==null?void 0:A.docs)==null?void 0:u.source}}};const Me=["Default","MultipleSynced","WithEmptyView"];export{o as Default,a as MultipleSynced,p as WithEmptyView,Me as __namedExportsOrder,Ie as default};
---
+++ storybook-main/assets/EdgeCases.stories-C4msgT6g.js
@@ -0,0 +1,18 @@
+import c from"./Default.stories-B0sB7FlU.js";import{F as d}from"./FileCard-NXd8Z6JE.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import{a as s}from"./index-B-lxVbXh.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./ContextMenuContent-CeB9QaOT.js";import"./Action-Dh7Im5JG.js";import"./context-CBT0xkbu.js";import"./PropsContextProvider-8e6_V-we.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-CKPxcNbh.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useStatic-wEStvoUF.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-CV6IWFNw.js";import"./dynamic-DKDa4OpU.js";import"./ContextMenuSection-D3ioSDSj.js";import"./Dialog-HD9t1mUg.js";import"./Button-BV9jeieD.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-DbWwTk60.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-fUY3Kb5L.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-DweNvqjD.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-DWYYoDxV.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Popover-DIzfrEmb.js";import"./DialogTriggerView-Cb0RhJK_.js";import"./IconWarning-Bmni-YAQ.js";import"./Switch-DkM4w1UO.js";import"./Label-ThqHcUi-.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./Avatar-CMYElceY.js";import"./AlertIcon-DeD6EX5U.js";import"./Image-DQqGJjVO.js";import"./Text-BfrxO2um.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Link-Bl8j2Cht.js";import"./Button-BaLxRHou.js";import"./LoadingSpinner-BHkd9yXV.js";import"./OptionsButton-BLH4BL52.js";import"./ButtonView-D8mPE87d.js";import"./v4-CtRu48qb.js";const To={...c,title:"Upload/FileCard/Edge Cases",component:d},o={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")}}},r={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")},href:"#"}};var t,m,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(i=(m=o.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var e,p,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    },
+    href: "#"
+  }
+}`,...(n=(p=r.parameters)==null?void 0:p.docs)==null?void 0:n.source}}};const ho=["LongName","LongNameAndLink"];export{o as LongName,r as LongNameAndLink,ho as __namedExportsOrder,To as default};
