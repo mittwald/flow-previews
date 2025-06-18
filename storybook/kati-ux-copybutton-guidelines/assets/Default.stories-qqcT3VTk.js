d04ab9d7d381f3662085fdedf57cb579ca2868ec
mittwald <>
2025-06-18 10:10:28 +0000

    Update from https://github.com/mittwald/flow/commit/b4cb4a162f03eccd2c1fb62ff866671b71725c88

---
+++ storybook/kati-ux-copybutton-guidelines/assets/Default.stories-qqcT3VTk.js
@@ -0,0 +1,17 @@
+import{A as S}from"./AlertIcon-BYe4Qt7t.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./IconWarning-B7Gv673I.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-4QVksy1R.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-CQymS3fL.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./context-C-2WHVw-.js";const j={title:"Status/AlertIcon",component:S,parameters:{controls:{exclude:["render","tunnelId"]}},argTypes:{size:{control:"inline-radio",options:["s","m","l"]},status:{control:"inline-radio",options:["info","success","warning","danger"]}},args:{size:"m",status:"info"}},r={args:{status:"info"}},s={args:{status:"success"}},a={args:{status:"warning"}},e={args:{status:"danger"}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    status: "info"
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,i,m;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  }
+}`,...(m=(i=s.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,u,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    status: "warning"
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,l,f;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "danger"
+  }
+}`,...(f=(l=e.parameters)==null?void 0:l.docs)==null?void 0:f.source}}};const k=["Info","Success","Warning","Danger"];export{e as Danger,r as Info,s as Success,a as Warning,k as __namedExportsOrder,j as default};
