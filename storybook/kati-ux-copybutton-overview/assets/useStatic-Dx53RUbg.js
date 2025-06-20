eee36206d020efb1470493e5022260aaaf5ed7a0
mittwald <>
2025-06-20 07:45:12 +0000

    Cleanup merged Storybook previews

--- storybook/kati-ux-copybutton-overview/assets/useStatic-Dx53RUbg.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./PropsContextProvider-4QVksy1R.js";import{r as e}from"./index-BdpSHsi2.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
