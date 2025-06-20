9be8908a4504b9fd2fa191d513eea7dbf7d597f2
mittwald <>
2025-06-20 05:53:14 +0000

    Update from https://github.com/mittwald/flow/commit/ae72e32e420b4899b4931c95eaf1fd0b9ba870b0

---
+++ storybook/kati-ux-avatar-index/assets/Default.stories-C3iyCMMq.js
@@ -0,0 +1,36 @@
+import{j as p}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{F as c}from"./FileCard-C2EI_Ezz.js";import{d as R}from"./dummyText-CX_I_Wpl.js";import{a as _}from"./index-B-lxVbXh.js";import{C as U,M as b}from"./ContextMenuContent-DXm6NDNA.js";import"./ContextMenuSection-DVClNGav.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./IconWarning-B7Gv673I.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-4QVksy1R.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-CQymS3fL.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./Avatar-Dr08Kukf.js";import"./AlertIcon-BYe4Qt7t.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./context-C-2WHVw-.js";import"./Image-Cfx3lvvh.js";import"./Text-DumWlxAm.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./Link-Cr1SVHBX.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./Button-CyqFMui6.js";import"./LoadingSpinner-CUXyA16Z.js";import"./Button-B8HIYCal.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./OptionsButton-D8aLOfzs.js";import"./ButtonView-W76iR0f1.js";import"./v4-CtRu48qb.js";import"./Action-DBWRTYmU.js";import"./context-CAkx8u38.js";import"./useStatic-Dx53RUbg.js";import"./getActionGroupSlot-FSOuscEu.js";import"./dynamic-DKDa4OpU.js";import"./Popover-9YPE1cAR.js";import"./DialogTriggerView-BF0VQuc2.js";import"./Dialog-CnnCEkMG.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Switch-tIFPrO-o.js";import"./Label-DiicyWtl.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";const Zr={title:"Upload/FileCard",component:c,args:{name:"image.jpg"},render:n=>p.jsx(c,{...n})},r={},e={args:{type:"image/png"}},t={args:{sizeInBytes:47500}},o={args:{onDelete:()=>{_("onDelete")}}},m={args:{href:"#"}},i={args:{href:"#",onDelete:()=>{_("onDelete")}}},s={render:n=>p.jsx(c,{...n,children:p.jsx(U,{children:p.jsx(b,{children:"Löschen"})})})},a={args:{imageSrc:R.imageSrc}};var d,u,g;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(g=(u=r.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var l,h,x;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    type: "image/png"
+  }
+}`,...(x=(h=e.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var D,S,W;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    sizeInBytes: 47500
+  }
+}`,...(W=(S=t.parameters)==null?void 0:S.docs)==null?void 0:W.source}}};var f,C,M;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(M=(C=o.parameters)==null?void 0:C.docs)==null?void 0:M.source}}};var y,j,I;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    href: "#"
+  }
+}`,...(I=(j=m.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var L,F,O;i.parameters={...i.parameters,docs:{...(L=i.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    href: "#",
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(O=(F=i.parameters)==null?void 0:F.docs)==null?void 0:O.source}}};var k,z,T;s.parameters={...s.parameters,docs:{...(k=s.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: props => <FileCard {...props}>
+      <ContextMenu>
+        <MenuItem>Löschen</MenuItem>
+      </ContextMenu>
+    </FileCard>
+}`,...(T=(z=s.parameters)==null?void 0:z.docs)==null?void 0:T.source}}};var A,B,E;a.parameters={...a.parameters,docs:{...(A=a.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    imageSrc: dummyText.imageSrc
+  }
+}`,...(E=(B=a.parameters)==null?void 0:B.docs)==null?void 0:E.source}}};const $r=["Default","WithType","WithSize","WithOnDelete","WithLink","WithLinkAndOnDelete","WithContextMenu","WithImage"];export{r as Default,s as WithContextMenu,a as WithImage,m as WithLink,i as WithLinkAndOnDelete,o as WithOnDelete,t as WithSize,e as WithType,$r as __namedExportsOrder,Zr as default};
---
+++ storybook/kati-ux-avatar-index/assets/EdgeCases.stories-kVF8fK_H.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{T as l}from"./Text-DumWlxAm.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import f from"./Default.stories-DOIpUP4a.js";import{I as c}from"./IllustratedMessage-Qf5QyC8f.js";import{H as u}from"./Heading-DGz_X3l7.js";import{B as x}from"./Button-CyqFMui6.js";import{c as g}from"./IconWarning-B7Gv673I.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./PropsContextProvider-4QVksy1R.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-CQymS3fL.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ActionGroup-DecytC9x.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-FSOuscEu.js";import"./useStatic-Dx53RUbg.js";import"./ProgressBar-DxZqcT1F.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Legend-BVI7141r.js";import"./Label-DiicyWtl.js";import"./Heading-PKR8zMP8.js";import"./RSPContexts-Cbm9ZJUe.js";import"./LoadingSpinner-CUXyA16Z.js";import"./Button-B8HIYCal.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";const dr={...f,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>r.jsxs(c,{...o,children:[r.jsx(g,{}),r.jsx(u,{children:p.medium}),r.jsx(l,{children:p.long}),r.jsx(x,{children:"Create app"})]})},e={render:o=>r.jsxs(c,{...o,children:[r.jsx(g,{}),r.jsx(u,{children:"No apps installed"}),r.jsx(l,{children:"Create your first app to start working on your website."}),r.jsx(x,{children:"Create app"})]}),parameters:{viewport:{defaultViewport:"mobile1"}}};var m,s,i;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(i=(s=t.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var a,n,d;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(d=(n=e.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};const lr=["LongTexts","SmallSpace"];export{t as LongTexts,e as SmallSpace,lr as __namedExportsOrder,dr as default};
