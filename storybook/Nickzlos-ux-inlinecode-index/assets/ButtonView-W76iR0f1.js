8222fd480186a5709148e7270d5a094ed51a0153
mittwald <>
2025-06-20 11:56:04 +0000

    Update from https://github.com/mittwald/flow/commit/f7efc45dad777ede67adae655c4eae7df3f307b2

---
+++ storybook/Nickzlos-ux-inlinecode-index/assets/ButtonView-W76iR0f1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-BdpSHsi2.js";import{B as n}from"./Button-CyqFMui6.js";import{v as s}from"./PropsContextProvider-4QVksy1R.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
