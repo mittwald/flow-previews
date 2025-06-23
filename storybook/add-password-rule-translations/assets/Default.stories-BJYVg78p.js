fa9f35af4b1ab8cda56f9b62ac096567196efe08
mittwald <>
2025-06-23 05:05:20 +0000

    Update from https://github.com/mittwald/flow/commit/074481de9d73d110722de985445ca4fd7c8e645b

---
+++ storybook/add-password-rule-translations/assets/Default.stories-BJYVg78p.js
@@ -0,0 +1,49 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{o as B,p as H,j as S,q as v,r as C}from"./IconWarning-CvDXxX1P.js";import{L as m}from"./Link-BXMBEpXY.js";import{H as s}from"./HeaderNavigation-BMDqutSo.js";import{B as o}from"./Button-DOHio3Wm.js";import{T as p}from"./Text-DIVAyxxL.js";import{I as L}from"./Image-B2L5p-qU.js";import{d as N}from"./dummyText-CX_I_Wpl.js";import{A as y}from"./Avatar-CYKZGH9w.js";import{a as b,C as D,M as c}from"./ContextMenuContent-B8AqtzYD.js";import"./ContextMenuSection-0uOj3poF.js";import{a as A,s as G}from"./storyBackgrounds-GFSAi-dI.js";import{a as E,M as P}from"./Modal-Cx0zyBRJ.js";import{H as W}from"./Heading-HvDOP0w5.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Bm7vqKWx.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-7-ygt9WY.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./context-C-2WHVw-.js";import"./utils-q_Aud5Js.js";import"./filterDOMProps-CeZl_uWj.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./dynamic-DKDa4OpU.js";import"./LoadingSpinner-BXqgTi6N.js";import"./Button-B8HIYCal.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./AlertIcon-BQdK0TKZ.js";import"./Action-a1z3kKhd.js";import"./context-BrtULD8u.js";import"./useStatic-NZrXO1in.js";import"./getActionGroupSlot-CNTzqHFz.js";import"./Popover-DXJefAnk.js";import"./DialogTriggerView--Dpp_Da-.js";import"./Dialog-CnnCEkMG.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Switch-CLNGT55n.js";import"./Label-B4Pxv214.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./Overlay-fsG-0cz1.js";import"./Header-DsAnZr3f.js";import"./ButtonView-DoWzu9_Q.js";import"./Heading-PKR8zMP8.js";const ro={title:"Navigation/HeaderNavigation",component:s,parameters:{controls:{exclude:["className"]}},render:n=>r.jsxs(s,{"aria-label":"Header navigation",...n,children:[r.jsx(m,{href:"#",children:"Getting startet"}),r.jsx(m,{href:"#","aria-current":"page",children:"Components"}),r.jsx(o,{children:r.jsx(B,{})})]}),argTypes:{color:{control:"inline-radio",options:["primary","dark","light"]}}},t={},e={render:n=>r.jsxs(s,{"aria-label":"Header navigation",...n,children:[r.jsx(o,{children:r.jsx(B,{})}),r.jsx(o,{children:r.jsx(H,{})}),r.jsxs(E,{children:[r.jsx(o,{children:r.jsx(S,{})}),r.jsx(P,{children:r.jsx(W,{children:"Notifications"})})]}),r.jsxs(b,{children:[r.jsx(o,{children:r.jsx(y,{children:r.jsx(L,{alt:"Gopher",src:N.imageSrc})})}),r.jsxs(D,{children:[r.jsxs(c,{children:[r.jsx(v,{}),r.jsx(p,{children:"Profile"})]}),r.jsxs(c,{children:[r.jsx(C,{}),r.jsx(p,{children:"Logout"})]})]})]})]})},a={args:{color:"dark"},parameters:{backgrounds:A}},i={args:{color:"light"},parameters:{backgrounds:G}};var d,g,l;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:"{}",...(l=(g=t.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var u,x,h;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: props => <HeaderNavigation aria-label="Header navigation" {...props}>
+      <Button>
+        <IconSearch />
+      </Button>
+      <Button>
+        <IconSupport />
+      </Button>
+      <ModalTrigger>
+        <Button>
+          <IconNotification />
+        </Button>
+        <Modal>
+          <Heading>Notifications</Heading>
+        </Modal>
+      </ModalTrigger>
+      <ContextMenuTrigger>
+        <Button>
+          <Avatar>
+            <Image alt="Gopher" src={dummyText.imageSrc} />
+          </Avatar>
+        </Button>
+        <ContextMenu>
+          <MenuItem>
+            <IconSettings />
+            <Text>Profile</Text>
+          </MenuItem>
+          <MenuItem>
+            <IconLogout />
+            <Text>Logout</Text>
+          </MenuItem>
+        </ContextMenu>
+      </ContextMenuTrigger>
+    </HeaderNavigation>
+}`,...(h=(x=e.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var j,f,M;a.parameters={...a.parameters,docs:{...(j=a.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(M=(f=a.parameters)==null?void 0:f.docs)==null?void 0:M.source}}};var I,k,T;i.parameters={...i.parameters,docs:{...(I=i.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(T=(k=i.parameters)==null?void 0:k.docs)==null?void 0:T.source}}};const oo=["Default","WithContextMenu","Dark","Light"];export{a as Dark,t as Default,i as Light,e as WithContextMenu,oo as __namedExportsOrder,ro as default};
---
+++ storybook/add-password-rule-translations/assets/Default.stories-Bw31Hbep.js
@@ -0,0 +1,45 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{L as o}from"./Label-B4Pxv214.js";import{B as l}from"./Button-DOHio3Wm.js";import{C as y,a as k}from"./ContextualHelpTrigger-CZeH1dLY.js";import{H as S}from"./Heading-HvDOP0w5.js";import{T as t}from"./Text-DIVAyxxL.js";import{L as B}from"./Link-BXMBEpXY.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Bm7vqKWx.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-7-ygt9WY.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./context-C-2WHVw-.js";import"./Label-C5gSD-Mq.js";import"./utils-q_Aud5Js.js";import"./Hidden-RoZfe91Z.js";import"./IconWarning-CvDXxX1P.js";import"./LoadingSpinner-BXqgTi6N.js";import"./Button-B8HIYCal.js";import"./ProgressBar-Dwuqfbjf.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./Popover-DXJefAnk.js";import"./DialogTriggerView--Dpp_Da-.js";import"./context-BrtULD8u.js";import"./useStatic-NZrXO1in.js";import"./Dialog-CnnCEkMG.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Heading-PKR8zMP8.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";const Br={title:"Content/Label",component:o,render:e=>r.jsx(o,{...e,children:"Label"})},n={},a={args:{optional:!0}},i={render:e=>r.jsxs(o,{...e,children:[r.jsx(t,{children:"Label"}),r.jsx(l,{children:"Do stuff"})]})},s={render:e=>r.jsxs(o,{...e,children:[r.jsx(t,{children:"Label"}),r.jsxs(y,{children:[r.jsx(l,{}),r.jsxs(k,{children:[r.jsx(S,{children:"Rights & roles"}),r.jsx(t,{children:"Each user profile is assigned a role in mStudio for each project and/or organization. This allows you to work in a completely new and modern way."}),r.jsx(B,{children:"Learn more"})]})]})]})},p={args:{optional:!0},render:e=>r.jsxs(o,{...e,children:[r.jsx(t,{children:"Label"}),r.jsxs(y,{children:[r.jsx(l,{}),r.jsxs(k,{children:[r.jsx(S,{children:"Rights & roles"}),r.jsx(t,{children:"Each user profile is assigned a role in mStudio for each project and/or organization. This allows you to work in a completely new and modern way."}),r.jsx(B,{children:"Learn more"})]})]})]})};var m,c,d;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(d=(c=n.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,x,h;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    optional: true
+  }
+}`,...(h=(x=a.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var g,j,L;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Label {...props}>
+      <Text>Label</Text>
+      <Button>Do stuff</Button>
+    </Label>
+}`,...(L=(j=i.parameters)==null?void 0:j.docs)==null?void 0:L.source}}};var f,T,H;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <Label {...props}>
+      <Text>Label</Text>
+      <ContextualHelpTrigger>
+        <Button />
+        <ContextualHelp>
+          <Heading>Rights & roles</Heading>
+          <Text>
+            Each user profile is assigned a role in mStudio for each project
+            and/or organization. This allows you to work in a completely new and
+            modern way.
+          </Text>
+          <Link>Learn more</Link>
+        </ContextualHelp>
+      </ContextualHelpTrigger>
+    </Label>
+}`,...(H=(T=s.parameters)==null?void 0:T.docs)==null?void 0:H.source}}};var w,C,b;p.parameters={...p.parameters,docs:{...(w=p.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    optional: true
+  },
+  render: props => <Label {...props}>
+      <Text>Label</Text>
+      <ContextualHelpTrigger>
+        <Button />
+        <ContextualHelp>
+          <Heading>Rights & roles</Heading>
+          <Text>
+            Each user profile is assigned a role in mStudio for each project
+            and/or organization. This allows you to work in a completely new and
+            modern way.
+          </Text>
+          <Link>Learn more</Link>
+        </ContextualHelp>
+      </ContextualHelpTrigger>
+    </Label>
+}`,...(b=(C=p.parameters)==null?void 0:C.docs)==null?void 0:b.source}}};const Er=["Default","Optional","WithButton","WithContextualHelp","OptionalWithContextualHelp"];export{n as Default,a as Optional,p as OptionalWithContextualHelp,i as WithButton,s as WithContextualHelp,Er as __namedExportsOrder,Br as default};
