35cd9f6ce2b26dc777a20d3057b3a25d303788dd
mittwald <>
2025-06-23 08:56:30 +0000

    Update from https://github.com/mittwald/flow/commit/8c41b24aad795fb2c17d3b5782063c1ca8834eb7

---
+++ storybook/SearchField-Guideline-Caro/assets/EdgeCases.stories-CUJRW2rT.js
@@ -0,0 +1,63 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{M as u}from"./Modal-Cwa3ph7X.js";import"./index-BdpSHsi2.js";import{B as m}from"./Button-Cla0UN75.js";import y from"./Default.stories-C6SATEYT.js";import{C as h}from"./Content-CMmpIkFz.js";import{S as r}from"./Section-SXcP01si.js";import{T as n}from"./Text-Do0xD7Eu.js";import{A as j}from"./ActionGroup-Cti0O0GK.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{H as T}from"./Heading-BkEauBJW.js";import{A as g}from"./Action-C-BgmOTv.js";import{u as f}from"./DialogTriggerView-Kf6yMsw8.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BTxoOpDa.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-DjmEL4rd.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./Overlay-09Kb-9Vr.js";import"./utils-q_Aud5Js.js";import"./Dialog-CnnCEkMG.js";import"./Button-B8HIYCal.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./IconWarning-CAyw9v1O.js";import"./Header-BIEB4SOJ.js";import"./ButtonView-hMvRbMES.js";import"./index-BAMY2Nnw.js";import"./LoadingSpinner-DRmwP3Vw.js";import"./TextField-Cngqgkmw.js";import"./TextFieldBase-B6lO6qrp.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-e52kgctm.js";import"./FieldError-D2eBwE7I.js";import"./FieldDescription-DOY8eFlE.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./Input-Dom3MqAg.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./useFormValidation-BP6joqgG.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./Label-BY4u1vBV.js";import"./lib-eZEWgZ4_.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./Form-DCrJowlG.js";import"./dynamic-DKDa4OpU.js";import"./browser-BJKb5O22.js";import"./Align-B3jVFW8z.js";import"./ColumnLayout-g_ofgCo0.js";import"./AccentBox-DwtfsIAP.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Heading-PKR8zMP8.js";import"./context-BDFzdp4e.js";import"./useStatic-CBjrLflN.js";import"./getActionGroupSlot-DQ6_PhKI.js";import"./ContextMenuSection-C_VOltZn.js";const Xo={...y,title:"Overlays/Modal/Edge Cases"},e={render:s=>o.jsxs(u,{...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})},i={render:s=>o.jsxs(u,{offCanvas:!0,...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})};var c,p,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var a,d,x;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Modal offCanvas {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(x=(d=i.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const Yo=["LongContent","LongContentOffCanvas"];export{e as LongContent,i as LongContentOffCanvas,Yo as __namedExportsOrder,Xo as default};
---
+++ storybook/SearchField-Guideline-Caro/assets/EdgeCases.stories-Cpue3WAq.js
@@ -0,0 +1,18 @@
+import c from"./Default.stories-ChGJyQeQ.js";import{F as d}from"./FileCard-nHbVLFOv.js";import{d as a}from"./dummyText-CX_I_Wpl.js";import{a as s}from"./index-B-lxVbXh.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./ContextMenuContent-CHwo4b5T.js";import"./Action-C-BgmOTv.js";import"./context-BDFzdp4e.js";import"./PropsContextProvider-BTxoOpDa.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-DjmEL4rd.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useStatic-CBjrLflN.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-DQ6_PhKI.js";import"./dynamic-DKDa4OpU.js";import"./ContextMenuSection-C_VOltZn.js";import"./Dialog-CnnCEkMG.js";import"./Button-B8HIYCal.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Popover-BbMYYETs.js";import"./DialogTriggerView-Kf6yMsw8.js";import"./IconWarning-CAyw9v1O.js";import"./Switch-DffI4-MT.js";import"./Label-BY4u1vBV.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./Avatar-CMZRKNdc.js";import"./AlertIcon-BzPd4nkg.js";import"./Image-B4xg-guO.js";import"./Text-Do0xD7Eu.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Link-AwX2b6Rz.js";import"./Button-Cla0UN75.js";import"./LoadingSpinner-DRmwP3Vw.js";import"./OptionsButton-BPIhZCGt.js";import"./ButtonView-hMvRbMES.js";import"./v4-CtRu48qb.js";const ho={...c,title:"Upload/FileCard/Edge Cases",component:d},o={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")}}},r={args:{name:a.long,sizeInBytes:47500,onDelete:()=>{s("onDelete")},href:"#"}};var t,m,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    }
+  }
+}`,...(i=(m=o.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var p,e,n;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    name: dummyText.long,
+    sizeInBytes: 47500,
+    onDelete: () => {
+      action("onDelete");
+    },
+    href: "#"
+  }
+}`,...(n=(e=r.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const ko=["LongName","LongNameAndLink"];export{o as LongName,r as LongNameAndLink,ko as __namedExportsOrder,ho as default};
