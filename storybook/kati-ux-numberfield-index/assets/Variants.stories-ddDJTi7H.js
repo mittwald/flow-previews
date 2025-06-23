2715f520f5f8aa09a58b24617664ee5e8e50864e
mittwald <>
2025-06-23 08:46:17 +0000

    Update from https://github.com/mittwald/flow/commit/ecfe9fe4a14d6ddf384b496b1888db24764e7f39

---
+++ storybook/kati-ux-numberfield-index/assets/Variants.stories-ddDJTi7H.js
@@ -0,0 +1,19 @@
+import f from"./Default.stories-BeQxWLcB.js";import"./jsx-runtime-D_zvdyIk.js";import"./ProgressBar-CZw8bknH.js";import"./index-BdpSHsi2.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BTxoOpDa.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-DjmEL4rd.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./ProgressBar-Dwuqfbjf.js";import"./utils-q_Aud5Js.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Legend-Bpbi01GV.js";import"./Text-Do0xD7Eu.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./Label-BY4u1vBV.js";const M={...f,title:"Status/ProgressBar/Variants"},r={args:{status:"info"}},s={args:{status:"success"}},t={args:{status:"warning",value:800}},a={args:{status:"danger",value:950}};var o,e,m;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    status: "info"
+  }
+}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};var n,p,i;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    status: "success"
+  }
+}`,...(i=(p=s.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var c,u,g;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    status: "warning",
+    value: 800
+  }
+}`,...(g=(u=t.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var d,l,S;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    status: "danger",
+    value: 950
+  }
+}`,...(S=(l=a.parameters)==null?void 0:l.docs)==null?void 0:S.source}}};const N=["Info","Success","Warning","Danger"];export{a as Danger,r as Info,s as Success,t as Warning,N as __namedExportsOrder,M as default};
