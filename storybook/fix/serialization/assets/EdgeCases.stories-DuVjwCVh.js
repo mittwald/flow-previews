d926585e703cd17fae0c6919dcb0355867d5d1e2
mittwald <>
2025-06-25 09:09:03 +0000

    Update from https://github.com/mittwald/flow/commit/4d2889e4a327ffab0b1cdcc155adc332cf956682

--- storybook/fix/serialization/assets/EdgeCases.stories-DuVjwCVh.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{B as s}from"./Breadcrumb-4hXsKLkj.js";import"./index-BdpSHsi2.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import d from"./Default.stories-CTy8WNaW.js";import{L as t}from"./Link-BuyNOW_Z.js";import"./clsx-B-dksMZM.js";import"./IconWarning-DFrsh5CB.js";import"./PropsContextProvider-DGDN6nze.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-DxVCVr6-.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Hidden-RoZfe91Z.js";import"./useFocusable-BXcKiciC.js";import"./useFocus-4XTC7e6o.js";import"./utils-q_Aud5Js.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLocalizedStringFormatter-QO03R6A5.js";import"./index-BAMY2Nnw.js";import"./storyBackgrounds-GFSAi-dI.js";import"./usePress-mDFtgnbt.js";import"./useFocusRing-DY5AGIis.js";const w={...d,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>r.jsxs(s,{children:[r.jsx(t,{children:i.medium}),r.jsx(t,{children:i.medium}),r.jsx(t,{children:i.medium})]})};var o,e,p;m.parameters={...m.parameters,docs:{...(o=m.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...(p=(e=m.parameters)==null?void 0:e.docs)==null?void 0:p.source}}};const z=["LongTexts"];export{m as LongTexts,z as __namedExportsOrder,w as default};
