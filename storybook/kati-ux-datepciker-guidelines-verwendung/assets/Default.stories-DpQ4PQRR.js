67be5e740855a92ebdb1a19ac1f40c774db39a4f
mittwald <>
2025-07-01 06:08:36 +0000

    Update from https://github.com/mittwald/flow/commit/5bdebc7c09e45102362afe8d49ac85963bb41f57

---
+++ storybook/kati-ux-datepciker-guidelines-verwendung/assets/Default.stories-DpQ4PQRR.js
@@ -0,0 +1,52 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{A as i,C as m,a as r,X as s,Y as n,b as d,c as y}from"./ChartLegend-B8jfBpF-.js";import{I as C}from"./IllustratedMessage-DbzMk0hu.js";import"./AccentBox-yYvAWVll.js";import"./Accordion-fPCr9NEP.js";import"./Action-BbVuO0KL.js";import"./ActionGroup-BBPBXyQd.js";import"./Activity-Crl60Rue.js";import"./Alert-LTXwVaRG.js";import"./AlertBadge-DSjOLoTU.js";import"./AlertIcon-B0lbwaOd.js";import"./Align-BOPK2CZN.js";import"./Avatar-biRoBbPn.js";import"./AvatarStack-CxyrsYrV.js";import"./Badge-JGM3l7ko.js";import"./BigNumber-BXWnCJRm.js";import"./Breadcrumb-CKOdc289.js";import"./index-BdpSHsi2.js";import"./context-CcaOJZ7_.js";import"./PropsContextProvider-DnxO5avC.js";import"./Button-B4A2XH58.js";import"./Chat-OeJeePUR.js";import"./Checkbox-C3w87NZb.js";import"./CheckboxButton-DFPh3QmH.js";import"./CheckboxGroup-CgxwQ-oQ.js";import"./CodeBlock-CbJTj_T9.js";import"./Color-Bo2dSxzi.js";import"./ColumnLayout-jRZfT7iT.js";import"./ComboBox-D14SDO2u.js";import"./Content-C2ZvRMDm.js";import"./ContextMenuContent-C3E0GdBO.js";import"./ContextMenuSection-CTUDXKoE.js";import"./ContextualHelpTrigger-0Ih5aJhf.js";import"./CopyButton-DdjqIOu_.js";import"./CounterBadge-RbF0Z1Ed.js";import"./DatePicker-3vf3LKuA.js";import"./DateRangePicker-DXzsC-V0.js";import"./DivView-BSQ8Z5lX.js";import"./DonutChart-DNDCVODz.js";import"./FieldDescription-DU-_pxqW.js";import"./FieldError-BXwenTDh.js";import"./FileCard-3pgiteYz.js";import"./FileCardList-BiH4AVAG.js";import"./FileDropZone-Dv1w3Jng.js";import"./FileField-CdBUElaa.js";import"./Flex-GWFKGCBE.js";import"./Header-By7mEiUF.js";import"./HeaderNavigation-4kKCM9Mv.js";import{H as f}from"./Heading-CncqC9cj.js";import{b as S}from"./IconWarning-DCPm6Thg.js";import"./Image-BsLpl9gJ.js";import"./Initials-CY520IxP.js";import"./InlineCode-D8j5CV28.js";import"./Label-DjJU0gqA.js";import"./LabeledValue-BJH75rb6.js";import"./LayoutCard-CkBXkGbW.js";import"./Legend-CsaaHC8C.js";import"./LightBox-BsxTlkZP.js";import"./Link-CB6XdanB.js";import"./browser-BJKb5O22.js";import"./List-CESKOPe2.js";import"./LoadingSpinner-gGsyPL53.js";import"./Markdown-D5ozBKEX.js";import"./Message-C2mP8tSW.js";import"./MessageSeparator-CH937tzf.js";import"./MessageThread-Cfbx3ZXb.js";import"./Modal-dDIoLWKL.js";import"./NavigationGroup-Bd2b9pOS.js";import"./Notification-CpFWrUQU.js";import"./NotificationProvider-DFl2QWCI.js";import"./NumberField-B26KepjA.js";import"./Option-CrpliWZ4.js";import"./Overlay-DbSm9f5t.js";import"./DialogTriggerView-D9ZaZwFu.js";import"./PasswordCreationField-Dky5VO2e.js";import"./Popover-BsqzIVzV.js";import"./ProgressBar-CdFfGnJn.js";import"./RadioButton-CplWAk2K.js";import"./Rating-CAaanEAO.js";import"./SearchField-CO_XbWTL.js";import"./Section-BoNwc34H.js";import"./Segment-BKgS1sfm.js";import"./CountryOptions-CGK8PvUd.js";import"./Separator-BrRg1FQa.js";import"./Skeleton-ZxV5AzDo.js";import"./SkeletonText-C6D8Hpjs.js";import"./Slider-mbDiF3Ws.js";import"./Switch-D4DISUnv.js";import"./Table-CGO8vcn5.js";import"./Tab-DUDXIVQS.js";import"./Text-C_2ARRZ-.js";import"./TextArea-BVyAJRX8.js";import"./TextField-D61n0Ktl.js";import"./TimeField-EeysMwgq.js";import"./Tooltip-CVZ3fbfG.js";import"./clsx-B-dksMZM.js";import"./OverlayArrow-Czdks72e.js";import"./mergeRefs-COuk0-v5.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./useFocus-4XTC7e6o.js";import"./ProgressBar-Dwuqfbjf.js";import"./utils-q_Aud5Js.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-rjpdN6qd.js";import"./index-BAMY2Nnw.js";import"./dynamic-DKDa4OpU.js";import"./useStatic-NzMx8TMJ.js";import"./getActionGroupSlot-DPDmJdd_.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./useFocusable-BXcKiciC.js";import"./iframe-2XJ608vZ.js";import"./Button-B8HIYCal.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocusRing-DY5AGIis.js";import"./RSPContexts-Cbm9ZJUe.js";import"./FieldError-D2eBwE7I.js";import"./Text-DtGJyZhh.js";import"./Form-CSvT4C5Q.js";import"./useFormValidation-BP6joqgG.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./FormField.module-B9AzUSUD.js";import"./Group-wR-qYHNv.js";import"./Input-Dom3MqAg.js";import"./ListBox-CbR1SQdX.js";import"./DragAndDrop-C6jssBmw.js";import"./useCollator-CmZhdF0V.js";import"./Dialog-CnnCEkMG.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./FocusScope-D_x47-q0.js";import"./inertValue-Cx-SdfM1.js";import"./useListState-F73dCXfi.js";import"./useFilter-C2pFErnT.js";import"./useSingleSelectListState-nrZ49i5x.js";import"./useUpdateEffect-CMGfMYOO.js";import"./useTextField-xVyqyefx.js";import"./react-children-utilities-DvaK_N8_.js";import"./onlyText-C2raRYWa.js";import"./DateField-rJie6Pb8.js";import"./useSpinButton-C2klML3i.js";import"./RangeCalendar-ClJuSNay.js";import"./Heading-PKR8zMP8.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./OptionsButton-u_b_X5O_.js";import"./ButtonView-Cd8Af6fz.js";import"./intlStrings-ZMTSFH_M.js";import"./index-SBtdq8jY.js";import"./useHasTabbableChild-BIQJ9OEZ.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./TextField-BqNO7Lq4.js";import"./RadioGroup-CAaRh8Mz.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./TextFieldBase-QAAhUMD2.js";const v=[{name:"Stat 1",firstKey:4e3,secondKey:2400,thirdKey:2400},{name:"Stat 2",firstKey:3e3,secondKey:1398,thirdKey:2210},{name:"Stat 3",firstKey:2e3,secondKey:9800,thirdKey:2290},{name:"Stat 4",firstKey:2780,secondKey:3908,thirdKey:2e3},{name:"Stat 5",firstKey:1890,secondKey:4800,thirdKey:2181},{name:"Stat 6",firstKey:2390,secondKey:3800,thirdKey:2500},{name:"Stat 7",firstKey:3490,secondKey:4300,thirdKey:2100}],we={title:"Data Visualisation/AreaChart",component:i,parameters:{controls:{exclude:["className"]}},args:{data:v},argTypes:{}},o={render:e=>t.jsxs(i,{...e,height:"70vh",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey"}),t.jsx(r,{dataKey:"secondKey",color:"palatinate-blue"}),t.jsx(r,{dataKey:"thirdKey",color:"tangerine"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{interval:"equidistantPreserveStart"}),t.jsx(d,{}),t.jsx(y,{})]})},a={render:e=>t.jsxs(t.Fragment,{children:[t.jsxs(i,{...e,height:"250px",syncId:"syncedAreaCharts",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey"}),t.jsx(r,{dataKey:"secondKey",color:"palatinate-blue"}),t.jsx(r,{dataKey:"thirdKey",color:"tangerine"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{}),t.jsx(d,{}),t.jsx(y,{})]}),t.jsxs(i,{...e,height:"250px",syncId:"syncedAreaCharts",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey",color:"magenta"}),t.jsx(r,{dataKey:"secondKey",color:"tropical-indigo"}),t.jsx(r,{dataKey:"thirdKey",color:"malachite"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{}),t.jsx(d,{}),t.jsx(y,{})]})]})},p={render:()=>{const e=()=>t.jsxs(C,{children:[t.jsx(S,{}),t.jsx(f,{children:"Keine Daten verfügbar"})]});return t.jsxs(i,{emptyView:e,height:"70vh",children:[t.jsx(m,{}),t.jsx(r,{dataKey:"firstKey"}),t.jsx(r,{dataKey:"secondKey",color:"palatinate-blue"}),t.jsx(r,{dataKey:"thirdKey",color:"tangerine"}),t.jsx(s,{dataKey:"name"}),t.jsx(n,{interval:"equidistantPreserveStart"}),t.jsx(d,{}),t.jsx(y,{})]})}};var c,K,h;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <AreaChart {...props} height="70vh">
+      <CartesianGrid />
+      <Area dataKey="firstKey" />
+      <Area dataKey="secondKey" color="palatinate-blue" />
+      <Area dataKey="thirdKey" color="tangerine" />
+      <XAxis dataKey="name" />
+      <YAxis interval="equidistantPreserveStart" />
+      <ChartTooltip />
+      <ChartLegend />
+    </AreaChart>
+}`,...(h=(K=o.parameters)==null?void 0:K.docs)==null?void 0:h.source}}};var l,x,j;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: props => <>
+      <AreaChart {...props} height="250px" syncId="syncedAreaCharts">
+        <CartesianGrid />
+        <Area dataKey="firstKey" />
+        <Area dataKey="secondKey" color="palatinate-blue" />
+        <Area dataKey="thirdKey" color="tangerine" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <ChartTooltip />
+        <ChartLegend />
+      </AreaChart>
+      <AreaChart {...props} height="250px" syncId="syncedAreaCharts">
+        <CartesianGrid />
+        <Area dataKey="firstKey" color="magenta" />
+        <Area dataKey="secondKey" color="tropical-indigo" />
+        <Area dataKey="thirdKey" color="malachite" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <ChartTooltip />
+        <ChartLegend />
+      </AreaChart>
+    </>
+}`,...(j=(x=a.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var g,A,u;p.parameters={...p.parameters,docs:{...(g=p.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => {
+    const emptyView = () => <IllustratedMessage>
+        <IconMonitoring />
+        <Heading>Keine Daten verfügbar</Heading>
+      </IllustratedMessage>;
+    return <AreaChart emptyView={emptyView} height="70vh">
+        <CartesianGrid />
+        <Area dataKey="firstKey" />
+        <Area dataKey="secondKey" color="palatinate-blue" />
+        <Area dataKey="thirdKey" color="tangerine" />
+        <XAxis dataKey="name" />
+        <YAxis interval="equidistantPreserveStart" />
+        <ChartTooltip />
+        <ChartLegend />
+      </AreaChart>;
+  }
+}`,...(u=(A=p.parameters)==null?void 0:A.docs)==null?void 0:u.source}}};const De=["Default","MultipleSynced","WithEmptyView"];export{o as Default,a as MultipleSynced,p as WithEmptyView,De as __namedExportsOrder,we as default};
