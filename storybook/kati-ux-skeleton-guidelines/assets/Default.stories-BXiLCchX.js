d15abeb792e056c47dfd3e20cfa5c5373cbe41a9
mittwald <>
2025-07-01 09:33:10 +0000

    Update from https://github.com/mittwald/flow/commit/82d6544146829e1c35641eab8114bf2162e2881b

---
+++ storybook/kati-ux-skeleton-guidelines/assets/Default.stories-BXiLCchX.js
@@ -0,0 +1,61 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{M as s}from"./Message-C2mP8tSW.js";import{H as a}from"./Header-By7mEiUF.js";import{B as m}from"./Button-B4A2XH58.js";import{A as p}from"./Align-BOPK2CZN.js";import{A as l}from"./Avatar-biRoBbPn.js";import{I as c}from"./Initials-CY520IxP.js";import{C as d}from"./Content-C2ZvRMDm.js";import{T as e}from"./Text-C_2ARRZ-.js";import{M as j}from"./MessageThread-Cfbx3ZXb.js";import{C as x}from"./Chat-OeJeePUR.js";import{d as h}from"./dummyText-CX_I_Wpl.js";import{T as u}from"./TextArea-BVyAJRX8.js";import{g as B}from"./IconWarning-DCPm6Thg.js";import{F as I}from"./FileCardList-BiH4AVAG.js";import{F as M}from"./FileCard-3pgiteYz.js";import{u as v,F as y,a as H}from"./Form-0j_d8wul.js";import{a as U}from"./index-B-lxVbXh.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DnxO5avC.js";import"./mergeRefs-COuk0-v5.js";import"./iframe-2XJ608vZ.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./LoadingSpinner-gGsyPL53.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./context-C-2WHVw-.js";import"./Button-B8HIYCal.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./AlertIcon-B0lbwaOd.js";import"./react-children-utilities-DvaK_N8_.js";import"./index-SBtdq8jY.js";import"./onlyText-C2raRYWa.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./dynamic-DKDa4OpU.js";import"./TextFieldBase-QAAhUMD2.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-BXwenTDh.js";import"./FieldError-D2eBwE7I.js";import"./FieldDescription-DU-_pxqW.js";import"./TextField-BqNO7Lq4.js";import"./Form-CSvT4C5Q.js";import"./Input-Dom3MqAg.js";import"./useTextField-xVyqyefx.js";import"./useFormReset-B9LhY6oC.js";import"./useControlledState-rjpdN6qd.js";import"./useFormValidation-BP6joqgG.js";import"./ReactAriaControlledValueFix-SEnjHoLp.js";import"./ColumnLayout-jRZfT7iT.js";import"./Image-BsLpl9gJ.js";import"./Link-CB6XdanB.js";import"./OptionsButton-u_b_X5O_.js";import"./ButtonView-Cd8Af6fz.js";import"./ContextMenuContent-C3E0GdBO.js";import"./Action-BbVuO0KL.js";import"./context-CcaOJZ7_.js";import"./useStatic-NzMx8TMJ.js";import"./getActionGroupSlot-DPDmJdd_.js";import"./ContextMenuSection-CTUDXKoE.js";import"./Dialog-CnnCEkMG.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./Popover-BsqzIVzV.js";import"./DialogTriggerView-D9ZaZwFu.js";import"./Switch-D4DISUnv.js";import"./Label-DjJU0gqA.js";import"./useToggleState-C8sObcIl.js";import"./v4-CtRu48qb.js";const ge={title:"Chat/Chat",component:x,parameters:{controls:{exclude:["className"]}},args:{height:400},render:t=>r.jsxs(x,{...t,children:[r.jsxs(j,{children:[r.jsxs(s,{children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]}),r.jsxs(s,{type:"sender",children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"John Doe"})}),r.jsx(e,{children:r.jsx("b",{children:"John Doe"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})]}),r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:4}),r.jsx(m,{color:"accent",children:"Submit"})]})},o={},i={render:t=>r.jsxs(x,{...t,children:[r.jsx(j,{children:r.jsxs(s,{children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})}),r.jsx(m,{color:"secondary",variant:"plain","aria-label":"file attachment",children:r.jsx(B,{})}),r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:4}),r.jsx(m,{color:"accent",children:"Submit"}),r.jsxs(I,{children:[r.jsx(M,{name:"Uploaded File 1"}),r.jsx(M,{name:"Uploaded File 2"})]})]})},n={render:t=>{const S=v();return r.jsxs(x,{...t,children:[r.jsx(j,{children:r.jsxs(s,{children:[r.jsx(a,{children:r.jsxs(p,{children:[r.jsx(l,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})}),r.jsxs(y,{form:S,onSubmit:()=>{U("submitted")},children:[r.jsx(H,{name:"message",children:r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:4})}),r.jsx(m,{color:"accent",children:"Submit"})]})]})}};var g,f,b;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(b=(f=o.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var F,T,C;i.parameters={...i.parameters,docs:{...(F=i.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: props => <Chat {...props}>
+      <MessageThread>
+        <Message>
+          <Header>
+            <Align>
+              <Avatar>
+                <Initials>Max Mustermann</Initials>
+              </Avatar>
+              <Text>
+                <b>Max Mustermann</b>
+              </Text>
+            </Align>
+          </Header>
+          <Content>
+            <Text>{dummyText.long}</Text>
+          </Content>
+        </Message>
+      </MessageThread>
+      <Button color="secondary" variant="plain" aria-label="file attachment">
+        <IconAttachment />
+      </Button>
+      <TextArea aria-label="message" rows={1} autoResizeMaxRows={4} />
+      <Button color="accent">Submit</Button>
+      <FileCardList>
+        <FileCard name="Uploaded File 1" />
+        <FileCard name="Uploaded File 2" />
+      </FileCardList>
+    </Chat>
+}`,...(C=(T=i.parameters)==null?void 0:T.docs)==null?void 0:C.source}}};var A,R,w;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Chat {...props}>
+        <MessageThread>
+          <Message>
+            <Header>
+              <Align>
+                <Avatar>
+                  <Initials>Max Mustermann</Initials>
+                </Avatar>
+                <Text>
+                  <b>Max Mustermann</b>
+                </Text>
+              </Align>
+            </Header>
+            <Content>
+              <Text>{dummyText.long}</Text>
+            </Content>
+          </Message>
+        </MessageThread>
+        <Form form={form} onSubmit={() => {
+        action("submitted");
+      }}>
+          <Field name="message">
+            <TextArea aria-label="message" rows={1} autoResizeMaxRows={4} />
+          </Field>
+          <Button color="accent">Submit</Button>
+        </Form>
+      </Chat>;
+  }
+}`,...(w=(R=n.parameters)==null?void 0:R.docs)==null?void 0:w.source}}};const fe=["Default","WithUpload","WithForm"];export{o as Default,n as WithForm,i as WithUpload,fe as __namedExportsOrder,ge as default};
---
+++ storybook/kati-ux-skeleton-guidelines/assets/EdgeCases.stories-CGec--ii.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{T as l}from"./Text-C_2ARRZ-.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import f from"./Default.stories-C37keyXC.js";import{I as c}from"./IllustratedMessage-DbzMk0hu.js";import{H as u}from"./Heading-CncqC9cj.js";import{B as x}from"./Button-B4A2XH58.js";import{c as g}from"./IconWarning-DCPm6Thg.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./PropsContextProvider-DnxO5avC.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-2XJ608vZ.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Text-DtGJyZhh.js";import"./utils-q_Aud5Js.js";import"./storyBackgrounds-GFSAi-dI.js";import"./ActionGroup-BBPBXyQd.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-DPDmJdd_.js";import"./useStatic-NzMx8TMJ.js";import"./ProgressBar-CdFfGnJn.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Legend-CsaaHC8C.js";import"./Label-DjJU0gqA.js";import"./Heading-PKR8zMP8.js";import"./RSPContexts-Cbm9ZJUe.js";import"./LoadingSpinner-gGsyPL53.js";import"./Button-B8HIYCal.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";const dr={...f,title:"Content/Illustrated Message/Edge Cases"},t={render:o=>r.jsxs(c,{...o,children:[r.jsx(g,{}),r.jsx(u,{children:p.medium}),r.jsx(l,{children:p.long}),r.jsx(x,{children:"Create app"})]})},e={render:o=>r.jsxs(c,{...o,children:[r.jsx(g,{}),r.jsx(u,{children:"No apps installed"}),r.jsx(l,{children:"Create your first app to start working on your website."}),r.jsx(x,{children:"Create app"})]}),parameters:{viewport:{defaultViewport:"mobile1"}}};var m,s,i;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>{dummyText.medium}</Heading>
+      <Text>{dummyText.long}</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>
+}`,...(i=(s=t.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var a,n,d;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <IllustratedMessage {...props}>
+      <IconApp />
+      <Heading>No apps installed</Heading>
+      <Text>Create your first app to start working on your website.</Text>
+      <Button>Create app</Button>
+    </IllustratedMessage>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(d=(n=e.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};const lr=["LongTexts","SmallSpace"];export{t as LongTexts,e as SmallSpace,lr as __namedExportsOrder,dr as default};
