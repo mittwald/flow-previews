3c2df563db6253e0963ce5b0a9ca847f1500714f
mittwald <>
2025-07-02 05:35:15 +0000

    Update from https://github.com/mittwald/flow/commit/47ba1be4e1880553125cb59f2f74d9c31d8ef433

---
+++ storybook/ContextMenu-Index-Caro/assets/EdgeCases.stories-DsZAJVXe.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{a as g,C as c,M as m}from"./ContextMenuContent-COGtxD5r.js";import"./ContextMenuSection-DDmhWfbV.js";import{B as l}from"./Button-Cs5B0OUq.js";import M from"./Default.stories-C-QfQoUR.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./Action-C9WSAPzP.js";import"./context-m-8f7_tp.js";import"./PropsContextProvider-Dd_19RHv.js";import"./mergeRefs-COuk0-v5.js";import"./clsx-B-dksMZM.js";import"./iframe-DvWMbf41.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./useStatic-D-zA8DDB.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-GRjE8TPo.js";import"./dynamic-DKDa4OpU.js";import"./Popover-Dli3CM_o.js";import"./DialogTriggerView-DSud8MaU.js";import"./Dialog-CnnCEkMG.js";import"./Button-B8HIYCal.js";import"./utils-q_Aud5Js.js";import"./ProgressBar-Dwuqfbjf.js";import"./Label-C5gSD-Mq.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-DtWExE0a.js";import"./context-C-2WHVw-.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-mDFtgnbt.js";import"./useFocus-4XTC7e6o.js";import"./useFocusRing-DY5AGIis.js";import"./useFocusable-BXcKiciC.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-Czdks72e.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-CgAMQp3e.js";import"./CollectionBuilder-DkA--_MS.js";import"./Separator-Dc9-x765.js";import"./Text-DtGJyZhh.js";import"./SelectionManager-ClU3Y9Vn.js";import"./useEvent-C_OiV_rL.js";import"./useCollator-CmZhdF0V.js";import"./FocusScope-D_x47-q0.js";import"./useLocalizedStringFormatter-Bs3jJydz.js";import"./VisuallyHidden-D-Ys6Nju.js";import"./IconWarning-Cp0yxZuF.js";import"./Switch-DiJlOLIy.js";import"./Label-and1Fbvh.js";import"./useToggleState-C8sObcIl.js";import"./useFormReset-B9LhY6oC.js";import"./Text-D77Eefk7.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./LoadingSpinner-f_Py74Lv.js";import"./Separator-BrRg1FQa.js";import"./Heading-CW1dJWaB.js";import"./Heading-PKR8zMP8.js";import"./Avatar-ByKMC7Y6.js";import"./AlertIcon-BR7OSjrZ.js";import"./Initials-CHwzDCYS.js";import"./react-children-utilities-DvaK_N8_.js";import"./index-SBtdq8jY.js";import"./onlyText-C2raRYWa.js";const kr={...M,title:"Actions/ContextMenu/Edge Cases"},t={args:{width:400},render:e=>r.jsxs(g,{children:[r.jsx(l,{children:"Trigger"}),r.jsxs(c,{...e,children:[r.jsx(m,{children:p.medium}),r.jsx(m,{children:p.medium})]})]})},o={render:e=>r.jsxs(g,{children:[r.jsx(l,{children:"Trigger"}),r.jsx(c,{...e,children:Array(20).fill("").map((T,i)=>r.jsxs(m,{children:["Item ",i+1]},i))})]})};var n,s,u;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    width: 400
+  },
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu {...props}>
+        <MenuItem>{dummyText.medium}</MenuItem>
+        <MenuItem>{dummyText.medium}</MenuItem>
+      </ContextMenu>
+    </ContextMenuTrigger>
+}`,...(u=(s=t.parameters)==null?void 0:s.docs)==null?void 0:u.source}}};var a,d,x;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <ContextMenuTrigger>
+      <Button>Trigger</Button>
+      <ContextMenu {...props}>
+        {Array(20).fill("").map((value, index) => <MenuItem key={index}>Item {index + 1}</MenuItem>)}
+      </ContextMenu>
+    </ContextMenuTrigger>
+}`,...(x=(d=o.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const Or=["LongTexts","LongList"];export{o as LongList,t as LongTexts,Or as __namedExportsOrder,kr as default};
