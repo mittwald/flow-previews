3428ee45a4f9339e2e5fe2438d8e4781e479479f
mittwald <>
2025-07-11 08:49:21 +0000

    Cleanup merged Storybook previews

--- storybook/fix/autcomplete/assets/ButtonView-Lt4ftcPk.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-BdpSHsi2.js";import{B as n}from"./Button-f5sKTz4U.js";import{v as s}from"./PropsContextProvider-XvJCvUyN.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
