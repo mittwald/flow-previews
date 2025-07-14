ddb3591479ab65e5b736ae7300c8b61011bf4f86
mittwald <>
2025-07-14 09:29:44 +0000

    Update from https://github.com/mittwald/flow/commit/11165920529bb1fbec7e99cbcb769c8ac89341a8

---
+++ storybook/1742-contextualhelp-action-area-spans-full-width/assets/Default.stories-jqnWIceH.js
@@ -0,0 +1,41 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{H as e}from"./Heading-CpmBnXxh.js";import"./index-BdpSHsi2.js";import{s as w}from"./IconWarning-AciE9vmB.js";import{a as y,s as D}from"./storyBackgrounds-GFSAi-dI.js";import{S as W}from"./Section-C5cOK4rd.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DddmxMHn.js";import"./mergeRefs-f9wmNE8z.js";import"./iframe-PZQV_-qh.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./Heading-glOOglw2.js";import"./RSPContexts-Cbm9ZJUe.js";import"./utils-CNUJJGcD.js";import"./index-BAMY2Nnw.js";import"./ContextMenuSection-DK70pp4R.js";import"./Action-Deg5pXPH.js";import"./context-CzehN3AA.js";import"./useStatic-B8zZLCUK.js";import"./browser-BJKb5O22.js";import"./getActionGroupSlot-Wlljn7Dc.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-BhYWsRnA.js";import"./Button-BTfgqu_0.js";import"./ProgressBar-BDeVoaCo.js";import"./Label-DsLtIi5U.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-XC8v3O7U.js";import"./context-B2NjtFkN.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-DWaDT6O_.js";import"./useFocus-Ck8e1iB7.js";import"./useFocusRing-BmK03SYd.js";import"./useFocusable-CxpMFfxV.js";import"./OverlayArrow-D1_DfAqv.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-DM8z4Ehe.js";import"./CollectionBuilder-CnXQkgAs.js";import"./Separator-COTp_Dm9.js";import"./Text-BJ9sUP_l.js";import"./Input--iZvazXr.js";import"./SearchField-_B0N1wQq.js";import"./FieldError-Dhym38A1.js";import"./Form-CXVBffIi.js";import"./Group-ryYih4mY.js";import"./useLocalizedStringFormatter-De8CUlme.js";import"./useTextField-fBojglfp.js";import"./useFormReset-Tdit5Ane.js";import"./TextField-DnxvqTg5.js";import"./SelectionManager-CgSqbwt8.js";import"./useEvent-Bvgrsh8W.js";import"./useCollator-C6lmXHHJ.js";import"./FocusScope-BN4baDIG.js";import"./VisuallyHidden-QAya5EjI.js";const Er={title:"Content/Heading",component:e,argTypes:{level:{control:"inline-radio",options:[1,2,3,4,5,6]},size:{control:"inline-radio",options:["xs","s","m","l","xl","xxl"]},color:{control:"inline-radio",options:["primary","dark","light"]}},args:{level:2},render:r=>o.jsxs(e,{...r,children:["I am a H",r.level," Heading"]})},t={},a={render:r=>o.jsxs(e,{...r,children:[o.jsx(w,{}),"Personal Information"]})},n={args:{level:1,size:"xxl"},render:r=>o.jsxs(e,{level:1,size:"xxl",...r,children:[o.jsx(w,{}),"Personal Information"]})},i={args:{color:"dark"},parameters:{backgrounds:y}},s={args:{color:"light"},parameters:{backgrounds:D}},m={render:r=>o.jsxs(W,{children:[o.jsx(e,{...r,children:"Personal Information of the current user"}),o.jsx(e,{...r,wrap:"balance",children:"Personal Information of the current user"})]}),parameters:{viewport:{defaultViewport:"mobile2"}}};var p,c,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var d,u,g;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      Personal Information
+    </Heading>
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var x,f,h;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    level: 1,
+    size: "xxl"
+  },
+  render: props => <Heading level={1} size="xxl" {...props}>
+      <IconMember />
+      Personal Information
+    </Heading>
+}`,...(h=(f=n.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var k,I,H;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  parameters: {
+    backgrounds: storyBackgroundLight
+  }
+}`,...(H=(I=i.parameters)==null?void 0:I.docs)==null?void 0:H.source}}};var S,b,j;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  parameters: {
+    backgrounds: storyBackgroundDark
+  }
+}`,...(j=(b=s.parameters)==null?void 0:b.docs)==null?void 0:j.source}}};var v,P,z;m.parameters={...m.parameters,docs:{...(v=m.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Heading {...props}>Personal Information of the current user</Heading>
+      <Heading {...props} wrap="balance">
+        Personal Information of the current user
+      </Heading>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2"
+    }
+  }
+}`,...(z=(P=m.parameters)==null?void 0:P.docs)==null?void 0:z.source}}};const Vr=["Default","WithIcon","WithCustomSize","Dark","Light","Wrap"];export{i as Dark,t as Default,s as Light,n as WithCustomSize,a as WithIcon,m as Wrap,Vr as __namedExportsOrder,Er as default};
---
+++ storybook/1742-contextualhelp-action-area-spans-full-width/assets/EdgeCases.stories-BUh1WWUR.js
@@ -0,0 +1,63 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{M as u}from"./Modal-CW31E7sg.js";import"./index-BdpSHsi2.js";import{B as m}from"./Button-A3J-uSYR.js";import y from"./Default.stories-tb4Y_cG9.js";import{C as h}from"./Content-Blz7cW7M.js";import{S as r}from"./Section-C5cOK4rd.js";import{T as n}from"./Text-NoXUlxas.js";import{A as j}from"./ActionGroup-DzH8ZpZj.js";import{d as t}from"./dummyText-CX_I_Wpl.js";import{H as T}from"./Heading-CpmBnXxh.js";import{A as g}from"./Action-Deg5pXPH.js";import{u as f}from"./DialogTriggerView-CQ22ljU6.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DddmxMHn.js";import"./mergeRefs-f9wmNE8z.js";import"./iframe-PZQV_-qh.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./Overlay-L6enDC0f.js";import"./utils-CNUJJGcD.js";import"./Dialog-BhYWsRnA.js";import"./Button-BTfgqu_0.js";import"./ProgressBar-BDeVoaCo.js";import"./Label-DsLtIi5U.js";import"./Hidden-RoZfe91Z.js";import"./filterDOMProps-CeZl_uWj.js";import"./useLabel-XC8v3O7U.js";import"./context-B2NjtFkN.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-DWaDT6O_.js";import"./useFocus-Ck8e1iB7.js";import"./useFocusRing-BmK03SYd.js";import"./useFocusable-CxpMFfxV.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-D1_DfAqv.js";import"./useControlledState-rjpdN6qd.js";import"./Collection-DM8z4Ehe.js";import"./CollectionBuilder-CnXQkgAs.js";import"./Separator-COTp_Dm9.js";import"./Text-BJ9sUP_l.js";import"./Input--iZvazXr.js";import"./SearchField-_B0N1wQq.js";import"./FieldError-Dhym38A1.js";import"./Form-CXVBffIi.js";import"./Group-ryYih4mY.js";import"./useLocalizedStringFormatter-De8CUlme.js";import"./useTextField-fBojglfp.js";import"./useFormReset-Tdit5Ane.js";import"./TextField-DnxvqTg5.js";import"./SelectionManager-CgSqbwt8.js";import"./useEvent-Bvgrsh8W.js";import"./useCollator-C6lmXHHJ.js";import"./FocusScope-BN4baDIG.js";import"./VisuallyHidden-QAya5EjI.js";import"./IconWarning-AciE9vmB.js";import"./Header-CtUF_Xbg.js";import"./ButtonView-DQzjoUbV.js";import"./index-BAMY2Nnw.js";import"./LoadingSpinner-DtwyiX9E.js";import"./TextField-CxTuqBrD.js";import"./TextFieldBase-DopnL7H6.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-IsY6Decp.js";import"./FieldDescription-CEna2Kcc.js";import"./ReactAriaControlledValueFix-BLJm2WOQ.js";import"./Label-BucbQZFx.js";import"./lib-BjGyzjx8.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./Form-C96Px6Wo.js";import"./dynamic-DKDa4OpU.js";import"./browser-BJKb5O22.js";import"./Align-DkE2TkHJ.js";import"./ColumnLayout-BFP83pCZ.js";import"./AccentBox-DQKnpRsM.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./Heading-glOOglw2.js";import"./context-CzehN3AA.js";import"./useStatic-B8zZLCUK.js";import"./getActionGroupSlot-Wlljn7Dc.js";import"./ContextMenuSection-DK70pp4R.js";const Yo={...y,title:"Overlays/Modal/Edge Cases"},e={render:s=>o.jsxs(u,{...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})},i={render:s=>o.jsxs(u,{offCanvas:!0,...s,controller:f("Modal",{isDefaultOpen:!0}),children:[o.jsx(T,{children:t.short}),o.jsxs(h,{children:[o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})}),o.jsx(r,{children:o.jsx(n,{children:t.long})})]}),o.jsx(j,{children:o.jsxs(g,{closeOverlay:"Modal",children:[o.jsx(m,{color:"accent",children:"Create customer"}),o.jsx(m,{variant:"soft",color:"secondary",children:"Abort"})]})})]})};var c,p,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: props => <Modal {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var a,d,x;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Modal offCanvas {...props} controller={useOverlayController("Modal", {
+    isDefaultOpen: true
+  })}>
+      <Heading>{dummyText.short}</Heading>
+      <Content>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+        <Section>
+          <Text>{dummyText.long}</Text>
+        </Section>
+      </Content>
+      <ActionGroup>
+        <Action closeOverlay="Modal">
+          <Button color="accent">Create customer</Button>
+          <Button variant="soft" color="secondary">
+            Abort
+          </Button>
+        </Action>
+      </ActionGroup>
+    </Modal>
+}`,...(x=(d=i.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};const Zo=["LongContent","LongContentOffCanvas"];export{e as LongContent,i as LongContentOffCanvas,Zo as __namedExportsOrder,Yo as default};
