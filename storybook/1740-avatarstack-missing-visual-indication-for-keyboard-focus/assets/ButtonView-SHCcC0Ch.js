ea0d1ecedf99088b4be548e5dda34f63b1ed676a
mittwald <>
2025-07-14 10:52:56 +0000

    Cleanup merged Storybook previews

--- storybook/1740-avatarstack-missing-visual-indication-for-keyboard-focus/assets/ButtonView-SHCcC0Ch.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r}from"./index-BdpSHsi2.js";import{B as n}from"./Button-Dt9YTY6z.js";import{v as s}from"./PropsContextProvider-o8Mtwa1S.js";const i=t=>{const o=r.useContext(s).Button??n;return e.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
