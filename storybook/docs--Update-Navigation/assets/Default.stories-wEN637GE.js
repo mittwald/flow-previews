c0e2412747fd4a51741a5bb67a5ec98181d2ab4f
mittwald <>
2025-07-14 11:08:54 +0000

    Update from https://github.com/mittwald/flow/commit/d0d20d20ba2377e8236e52e2d9d9788db2e34b7e

---
+++ storybook/docs--Update-Navigation/assets/Default.stories-wEN637GE.js
@@ -0,0 +1,37 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{T as s}from"./TextArea-Bd5kGh91.js";import"./index-BdpSHsi2.js";import{L as a}from"./Label-BZEv17pm.js";import{a as k}from"./index-B-lxVbXh.js";import{F as _}from"./FieldDescription-DzC1bqRm.js";import{F as O}from"./FieldError-BnfZ8IAn.js";import"./TextFieldBase-KOQxU2ii.js";import"./FormField.module-B9AzUSUD.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BEoBLpOc.js";import"./mergeRefs-f9wmNE8z.js";import"./iframe-DiQSYI78.js";import"./index-DU8gk_nc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-lEnnwJps.js";import"./useLocalizedStringFormatter-De8CUlme.js";import"./context-B2NjtFkN.js";import"./TextField-DnxvqTg5.js";import"./utils-CNUJJGcD.js";import"./FieldError-Dhym38A1.js";import"./Text-BJ9sUP_l.js";import"./filterDOMProps-CeZl_uWj.js";import"./Form-CXVBffIi.js";import"./useFocus-Ck8e1iB7.js";import"./useLabel-XC8v3O7U.js";import"./Input--iZvazXr.js";import"./Hidden-RoZfe91Z.js";import"./useFocusRing-BmK03SYd.js";import"./Label-DsLtIi5U.js";import"./useTextField-fBojglfp.js";import"./useFormReset-Tdit5Ane.js";import"./useControlledState-rjpdN6qd.js";import"./useFocusable-CxpMFfxV.js";import"./ReactAriaControlledValueFix-BLJm2WOQ.js";import"./index-BAMY2Nnw.js";import"./v4-CtRu48qb.js";import"./Text-Dwi5o-9q.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./IconWarning-Dz6oXSxd.js";const Kr={title:"Form Controls/TextArea",component:s,render:e=>r.jsx(s,{onChange:k("onChange"),...e,children:r.jsx(a,{children:"Message"})})},o={},t={args:{isDisabled:!0}},i={args:{isRequired:!0}},p={render:e=>r.jsxs(s,{...e,children:[r.jsx(a,{children:"Public Key"}),r.jsx(_,{children:"Starts with ssh-rsa"})]})},n={render:e=>r.jsx(s,{...e,defaultValue:"ssh-rsa asdfkehbahrgvszuagfdashdfgsaghdfiuadsf",children:r.jsx(a,{children:"Public Key"})})},c={render:e=>r.jsx(s,{...e,placeholder:"ssh-rsa...",children:r.jsx(a,{children:"Public Key"})})},d={render:e=>r.jsxs(s,{...e,isInvalid:!0,defaultValue:"hello",children:[r.jsx(a,{children:"Public Key"}),r.jsx(O,{children:"Invalid Key"})]})},m={args:{showCharacterCount:!0,maxLength:100}},l={args:{rows:1,autoResizeMaxRows:5}};var u,h,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(g=(h=o.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var x,b,f;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(f=(b=t.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var j,D,F;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    isRequired: true
+  }
+}`,...(F=(D=i.parameters)==null?void 0:D.docs)==null?void 0:F.source}}};var S,L,C;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <TextArea {...props}>
+      <Label>Public Key</Label>
+      <FieldDescription>Starts with ssh-rsa</FieldDescription>
+    </TextArea>
+}`,...(C=(L=p.parameters)==null?void 0:L.docs)==null?void 0:C.source}}};var R,T,w;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} defaultValue="ssh-rsa asdfkehbahrgvszuagfdashdfgsaghdfiuadsf">
+      <Label>Public Key</Label>
+    </TextArea>
+}`,...(w=(T=n.parameters)==null?void 0:T.docs)==null?void 0:w.source}}};var y,A,K;c.parameters={...c.parameters,docs:{...(y=c.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} placeholder="ssh-rsa...">
+      <Label>Public Key</Label>
+    </TextArea>
+}`,...(K=(A=c.parameters)==null?void 0:A.docs)==null?void 0:K.source}}};var P,W,E;d.parameters={...d.parameters,docs:{...(P=d.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: props => <TextArea {...props} isInvalid defaultValue="hello">
+      <Label>Public Key</Label>
+      <FieldError>Invalid Key</FieldError>
+    </TextArea>
+}`,...(E=(W=d.parameters)==null?void 0:W.docs)==null?void 0:E.source}}};var v,z,V;m.parameters={...m.parameters,docs:{...(v=m.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    showCharacterCount: true,
+    maxLength: 100
+  }
+}`,...(V=(z=m.parameters)==null?void 0:z.docs)==null?void 0:V.source}}};var q,I,M;l.parameters={...l.parameters,docs:{...(q=l.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    rows: 1,
+    autoResizeMaxRows: 5
+  }
+}`,...(M=(I=l.parameters)==null?void 0:I.docs)==null?void 0:M.source}}};const Pr=["Default","Disabled","Required","WithFieldDescription","WithDefaultValue","WithPlaceholder","WithFieldError","ShowCharacterCount","Resizeable"];export{o as Default,t as Disabled,i as Required,l as Resizeable,m as ShowCharacterCount,n as WithDefaultValue,p as WithFieldDescription,d as WithFieldError,c as WithPlaceholder,Pr as __namedExportsOrder,Kr as default};
---
+++ storybook/docs--Update-Navigation/assets/InputButton.stories-CCZsKsR0.js
@@ -0,0 +1,41 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-BdpSHsi2.js";import{A as e}from"./Align-f-Wg6rmJ.js";import{T as l}from"./TextField-dy2FmMlp.js";import{L as i}from"./Label-BZEv17pm.js";import{B as o}from"./Button-ICKftcKq.js";import y from"./Default.stories-CA7eIGk7.js";import{N as w}from"./NumberField-Bc8P7UTn.js";import{T as D}from"./TextArea-Bd5kGh91.js";import{S as E}from"./CountryOptions-RsWAgNWh.js";import{O as d}from"./Option-Byab9EuV.js";import{u as _,F as I}from"./Form-Dct_wQCK.js";import{s as R}from"./Action-CT57mrJz.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BAMY2Nnw.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BEoBLpOc.js";import"./mergeRefs-f9wmNE8z.js";import"./iframe-DiQSYI78.js";import"./index-DU8gk_nc.js";import"./index-lEnnwJps.js";import"./TextFieldBase-KOQxU2ii.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-BnfZ8IAn.js";import"./IconWarning-Dz6oXSxd.js";import"./FieldError-Dhym38A1.js";import"./utils-CNUJJGcD.js";import"./Text-BJ9sUP_l.js";import"./filterDOMProps-CeZl_uWj.js";import"./FieldDescription-DzC1bqRm.js";import"./Text-Dwi5o-9q.js";import"./browser-BJKb5O22.js";import"./EmulatedBoldText-DOyoRrwd.js";import"./useLocalizedStringFormatter-De8CUlme.js";import"./context-B2NjtFkN.js";import"./TextField-DnxvqTg5.js";import"./Form-CXVBffIi.js";import"./useFocus-Ck8e1iB7.js";import"./useLabel-XC8v3O7U.js";import"./Input--iZvazXr.js";import"./Hidden-RoZfe91Z.js";import"./useFocusRing-BmK03SYd.js";import"./Label-DsLtIi5U.js";import"./useTextField-fBojglfp.js";import"./useFormReset-Tdit5Ane.js";import"./useControlledState-rjpdN6qd.js";import"./useFocusable-CxpMFfxV.js";import"./ReactAriaControlledValueFix-BLJm2WOQ.js";import"./LoadingSpinner-C-lYsDox.js";import"./Button-BTfgqu_0.js";import"./ProgressBar-BDeVoaCo.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-DWaDT6O_.js";import"./Avatar-n8V6DUYt.js";import"./AlertIcon-BBwYCwxR.js";import"./Initials-DvkxTd6K.js";import"./react-children-utilities-DvaK_N8_.js";import"./index-SBtdq8jY.js";import"./onlyText-C2raRYWa.js";import"./Group-ryYih4mY.js";import"./useSpinButton-CAAnmCPV.js";import"./useEvent-Bvgrsh8W.js";import"./DialogTriggerView-B6VxoeKR.js";import"./context-DSPerFXk.js";import"./useStatic-Cv27Ti44.js";import"./Dialog-BhYWsRnA.js";import"./RSPContexts-Cbm9ZJUe.js";import"./OverlayArrow-D1_DfAqv.js";import"./Collection-DM8z4Ehe.js";import"./CollectionBuilder-CnXQkgAs.js";import"./Separator-COTp_Dm9.js";import"./SearchField-_B0N1wQq.js";import"./SelectionManager-CgSqbwt8.js";import"./useCollator-C6lmXHHJ.js";import"./FocusScope-BN4baDIG.js";import"./VisuallyHidden-QAya5EjI.js";import"./intlStrings-ZMTSFH_M.js";import"./ListBox-D6P1GheJ.js";import"./DragAndDrop-Ck-jnxWO.js";import"./inertValue-Cx-SdfM1.js";import"./useListState-DO8QABPY.js";import"./useSingleSelectListState-B_c5sxxN.js";import"./Popover-OFOvyfED.js";import"./dynamic-DKDa4OpU.js";import"./getActionGroupSlot-CpSYDWSy.js";const ft={...y,title:"Structure/Align/Input + Button",component:e,render:t=>r.jsxs(e,{...t,children:[r.jsx(l,{children:r.jsx(i,{children:"Mail address"})}),r.jsx(o,{children:"Hinzufügen"})]})},n={},p={render:t=>r.jsxs(e,{...t,children:[r.jsx(w,{children:r.jsx(i,{children:"Number"})}),r.jsx(o,{children:"Hinzufügen"})]})},m={render:t=>r.jsxs(e,{...t,children:[r.jsx(D,{children:r.jsx(i,{children:"Message"})}),r.jsx(o,{children:"Hinzufügen"})]})},s={render:t=>r.jsxs(e,{...t,children:[r.jsxs(E,{children:[r.jsx(i,{children:"Options"}),r.jsx(d,{children:"Option 1"}),r.jsx(d,{children:"Option 2"})]}),r.jsx(o,{children:"Hinzufügen"})]})},a={render:t=>r.jsxs(e,{...t,children:[r.jsx(l,{}),r.jsx(o,{children:"Hinzufügen"})]})},c={render:t=>{const M=_();return r.jsx(I,{form:M,onSubmit:async()=>await R(2e3),children:r.jsxs(e,{...t,children:[r.jsx(l,{children:r.jsx(i,{children:"Mail address"})}),r.jsx(o,{type:"submit",children:"Hinzufügen"})]})})}};var u,x,h;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:"{}",...(h=(x=n.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var g,f,j;p.parameters={...p.parameters,docs:{...(g=p.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <NumberField>
+        <Label>Number</Label>
+      </NumberField>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(j=(f=p.parameters)==null?void 0:f.docs)==null?void 0:j.source}}};var b,A,F;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextArea>
+        <Label>Message</Label>
+      </TextArea>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(F=(A=m.parameters)==null?void 0:A.docs)==null?void 0:F.source}}};var S,B,O;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <Select>
+        <Label>Options</Label>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+      </Select>
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(O=(B=s.parameters)==null?void 0:B.docs)==null?void 0:O.source}}};var L,z,H;a.parameters={...a.parameters,docs:{...(L=a.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: props => <Align {...props}>
+      <TextField />
+      <Button>Hinzufügen</Button>
+    </Align>
+}`,...(H=(z=a.parameters)==null?void 0:z.docs)==null?void 0:H.source}}};var T,W,N;c.parameters={...c.parameters,docs:{...(T=c.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Form form={form} onSubmit={async () => await sleep(2000)}>
+        <Align {...props}>
+          <TextField>
+            <Label>Mail address</Label>
+          </TextField>
+          <Button type="submit">Hinzufügen</Button>
+        </Align>
+      </Form>;
+  }
+}`,...(N=(W=c.parameters)==null?void 0:W.docs)==null?void 0:N.source}}};const jt=["Default","WithNumberField","WithTextArea","WithSelect","WithoutLabel","WithForm"];export{n as Default,c as WithForm,p as WithNumberField,s as WithSelect,m as WithTextArea,a as WithoutLabel,jt as __namedExportsOrder,ft as default};
