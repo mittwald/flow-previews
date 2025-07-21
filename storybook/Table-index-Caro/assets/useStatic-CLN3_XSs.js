98c0d5a8fa494dcdd6c63d006de95e9e907e3ae8
mittwald <>
2025-07-21 12:24:05 +0000

    Cleanup merged Storybook previews

--- storybook/Table-index-Caro/assets/useStatic-CLN3_XSs.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./PropsContextProvider-BCJdYZuQ.js";import{r as e}from"./iframe-D5s8HkwO.js";const n=(t,r=[])=>{const[s,o]=e.useState(t());return e.useEffect(()=>a(()=>{o(t())}),r),s},S=t=>e.useState(()=>t())[0];export{n as a,S as u};
