b972718607c5eaf0265cff0555d8d23439b50f8d
mittwald <>
2025-07-22 09:54:19 +0000

    Cleanup merged Storybook previews

--- storybook/Nickzlos-align-index/assets/Default.stories-DNc5gHw9.js
+++
@@ -1,5 +0,0 @@
-import{j as c}from"./iframe-B5D6Jy2e.js";import{R as o}from"./Rating-BMNuO8dG.js";import"./IconWarning-DjYRPplk.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CnEOAU2e.js";import"./mergeRefs-C6bCzzs4.js";import"./index-98juzA4z.js";import"./useLocalizedStringFormatter-BQI7yYKW.js";import"./context-Bj-w4NoV.js";const v={title:"Content/Rating",component:o,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:p=>c.jsx(o,{...p})},r={},e={args:{size:"s"}};var s,t,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var m,i,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const z=["Default","Small"];export{r as Default,e as Small,z as __namedExportsOrder,v as default};
--- storybook/Nickzlos-align-index/assets/useEvent-DdUUCx0h.js
+++
@@ -1,1 +0,0 @@
-import{c as l}from"./mergeRefs-C6bCzzs4.js";import{r as s}from"./iframe-B5D6Jy2e.js";function i(e,r,c,t){let a=l(c),f=c==null;s.useEffect(()=>{if(f||!e.current)return;let n=e.current;return n.addEventListener(r,a,t),()=>{n.removeEventListener(r,a,t)}},[e,r,t,f,a])}export{i as $};
