09241f3d6d7fd89128dd2f2975894cc3ef4c9cf5
mittwald <>
2025-07-28 08:29:19 +0000

    Update from https://github.com/mittwald/flow/commit/2710076c8adf5ac7818d8364b80a2f780d9d8a4c

---
+++ storybook/Nickzlos-patch-1/assets/Default.stories-hsk2gtNs.js
@@ -0,0 +1,5 @@
+import{j as c}from"./iframe-vEDY1jTX.js";import{R as o}from"./Rating-Dy_dWhbb.js";import"./IconWarning-Br_z56yn.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-D1FW-2uj.js";import"./mergeRefs-CKsG1c36.js";import"./index-C6zHk2IC.js";import"./useLocalizedStringFormatter-BqEHuk_G.js";import"./context-DTpTEKQR.js";const v={title:"Content/Rating",component:o,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:p=>c.jsx(o,{...p})},r={},e={args:{size:"s"}};var s,t,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var m,i,n;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: "s"
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const z=["Default","Small"];export{r as Default,e as Small,z as __namedExportsOrder,v as default};
