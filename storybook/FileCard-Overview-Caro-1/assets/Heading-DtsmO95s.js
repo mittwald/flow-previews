68cc8f34866c70e92fd7e260ad957ff0792316a0
mittwald <>
2025-07-29 07:37:36 +0000

    Cleanup merged Storybook previews

--- storybook/FileCard-Overview-Caro-1/assets/Heading-DtsmO95s.js
+++
@@ -1,1 +0,0 @@
-import{$ as c}from"./RSPContexts-DWSs-nZQ.js";import{$ as f}from"./utils-gfhUS_KB.js";import{r as m,R as s}from"./iframe-vEDY1jTX.js";const x=m.forwardRef(function(e,a){[e,a]=f(e,a,c);let{children:t,level:l=3,className:r,...o}=e,$=`h${l}`;return s.createElement($,{...o,ref:a,className:r??"react-aria-Heading"},t)});export{x as $};
