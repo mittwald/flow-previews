79a1d99be0b1e7bb6cc871643a568f94c32c28fa
mittwald <>
2025-07-30 14:35:16 +0000

    Cleanup merged Storybook previews

--- storybook/1801-remove-datetime-sorting-function/assets/Default.stories-D7siGYwT.js
+++
@@ -1,9 +0,0 @@
-import{j as u}from"./iframe-Cf8YTZ8I.js";import{S as t}from"./Switch-CfWdaY-y.js";import"./clsx-B-dksMZM.js";import"./IconWarning-qh9NwBsS.js";import"./PropsContextProvider-DTkA4BUB.js";import"./mergeRefs-D3ggMSJr.js";import"./index-B8aDKTw0.js";import"./Label-D1xaprCz.js";import"./useLocalizedStringFormatter-BO4NN1ph.js";import"./context-D-u3TSjV.js";import"./Label-CYpSb4aj.js";import"./utils-mApyKjAg.js";import"./Hidden-CFe2gnmC.js";import"./filterDOMProps-CghfNOdR.js";import"./useToggleState-D9vno5XO.js";import"./useFormReset-Ctmxo4kM.js";import"./usePress-CjpkCDJv.js";import"./useFocus-_Kohxctg.js";import"./useFocusRing-WtizUsSm.js";import"./useFocusable-n963r6Th.js";import"./useControlledState-J34_Aqnk.js";import"./VisuallyHidden-CDGiYjfo.js";const{action:b}=__STORYBOOK_MODULE_ACTIONS__,N={title:"Form Controls/Switch",component:t,args:{onChange:b("onChange"),labelPosition:"trailing"},parameters:{controls:{exclude:["onChange"]}},argTypes:{labelPosition:{control:"inline-radio",options:["trailing","leading"]}},render:g=>u.jsx(t,{...g,children:"Autoresponder"})},r={},o={args:{labelPosition:"leading"}},e={args:{isDisabled:!0}};var a,i,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(s=(i=r.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var n,m,p;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  args: {
-    labelPosition: "leading"
-  }
-}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var c,l,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    isDisabled: true
-  }
-}`,...(d=(l=e.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const U=["Default","LabelLeading","Disabled"];export{r as Default,e as Disabled,o as LabelLeading,U as __namedExportsOrder,N as default};
