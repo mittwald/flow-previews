2529735fcbfe3abf4aee0f67caca73c16d6a563b
mittwald <>
2025-07-30 12:50:10 +0000

    Cleanup merged Storybook previews

--- storybook/1736-lightbox-improvement-better-visibility-for-icon-buttons/assets/Default.stories-Cu1jGd_q.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-5e2kPeup.js";import{B as s}from"./BigNumber-CIFYTOQl.js";import{T as a}from"./Text-CEKkIKYs.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-DEH0t7hl.js";import"./mergeRefs-uY3blBsM.js";import"./index-lLG5KW4n.js";import"./browser-D35eJ3ID.js";import"./EmulatedBoldText-DjgxRs5N.js";import"./Text-DiRiFDT_.js";import"./utils-Bj-e-2L9.js";const S={title:"Data Visualisation/BigNumber",component:s,render:o=>t.jsx(s,{...o,children:"69%"})},r={},e={render:o=>t.jsxs(s,{...o,children:[t.jsx(a,{children:"69%"}),t.jsx(a,{children:"Performance"})]})};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,c,u;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render: props => <BigNumber {...props}>
-      <Text>69%</Text>
-      <Text>Performance</Text>
-    </BigNumber>
-}`,...(u=(c=e.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const D=["Default","WithSubtitle"];export{r as Default,e as WithSubtitle,D as __namedExportsOrder,S as default};
--- storybook/1736-lightbox-improvement-better-visibility-for-icon-buttons/assets/Heading-0mE3T3ep.js
+++
@@ -1,1 +0,0 @@
-import{$ as c}from"./RSPContexts-DWx74BAe.js";import{$ as f}from"./utils-Bj-e-2L9.js";import{r as m,R as s}from"./iframe-5e2kPeup.js";const x=m.forwardRef(function(e,a){[e,a]=f(e,a,c);let{children:t,level:l=3,className:r,...o}=e,$=`h${l}`;return s.createElement($,{...o,ref:a,className:r??"react-aria-Heading"},t)});export{x as $};
