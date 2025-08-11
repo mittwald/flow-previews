2fbd5e2bb08430845e0f78f52447a000e29d9494
mittwald <>
2025-08-11 05:43:13 +0000

    Update from https://github.com/mittwald/flow/commit/a027459a81609924ed9fca2c006936d0d395468d

---
+++ storybook/fix-combobox/assets/ButtonView-BFvxxGTy.js
@@ -0,0 +1,1 @@
+import{r as e,j as n}from"./iframe-CRCn_1k8.js";import{B as s}from"./Button-BVbQUaZI.js";import{v as r}from"./PropsContextProvider-BubU3Kz3.js";const i=t=>{const o=e.useContext(r).Button??s;return n.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
---
+++ storybook/fix-combobox/assets/Default.stories-BKABoDmu.js
@@ -0,0 +1,78 @@
+import{j as e}from"./iframe-CRCn_1k8.js";import{T as t}from"./Text-DcU8-VYF.js";import{S as X}from"./Section-CL7D0Pyy.js";import{I as h}from"./InlineCode-DB1AXSai.js";import{I as g}from"./IconWarning-DfyJ3u1O.js";import{d as u}from"./dummyText-CX_I_Wpl.js";import"./PropsContextProvider-BubU3Kz3.js";import"./mergeRefs-Gw2OcT6Q.js";import"./index-7hVsDhMa.js";import"./clsx-B-dksMZM.js";import"./browser-B1ctrq8_.js";import"./EmulatedBoldText-YLFM-K80.js";import"./Text-CnPg80ig.js";import"./utils-BI2kd8b-.js";import"./ContextMenuSection-DZ1YudgL.js";import"./Action-DUoIh7jH.js";import"./context-BrbDjT5a.js";import"./useStatic-coyUivo9.js";import"./getActionGroupSlot-QXW-CFJF.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-1-lFH5mz.js";import"./Button-DC_FOGfE.js";import"./ProgressBar-CqTE0rEm.js";import"./Label-DuoAXWP0.js";import"./Hidden-860WeEVo.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-76mwt2Hf.js";import"./context-fzWZoyLv.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-CPRYRx8p.js";import"./useFocus-B3vm1A24.js";import"./useFocusRing-CAd7IlbP.js";import"./useFocusable-BJ6918Tp.js";import"./RSPContexts-C8n8IA5U.js";import"./OverlayArrow-CtMzTmFy.js";import"./useControlledState-gjMGbt2_.js";import"./Collection-BXQIElN8.js";import"./CollectionBuilder-DKleWLkr.js";import"./Separator-C4pDfZY6.js";import"./Group-D1x-PT5o.js";import"./SearchField-sAGmdzL3.js";import"./FieldError-Get8RxYf.js";import"./Form-BUDb6yDR.js";import"./useLocalizedStringFormatter-DT9CXOan.js";import"./useTextField-BRTDaWoE.js";import"./useFormReset-DT5J9yBd.js";import"./TextField-BUKK7PKt.js";import"./useEvent-BjeIZ3GR.js";import"./SelectionManager-gdTsuSpq.js";import"./useCollator-CSlC2lAk.js";import"./FocusScope-B7tfNpBC.js";import"./VisuallyHidden-4OPl2Xdl.js";const Xe={title:"Content/Text",component:t,argTypes:{elementType:{control:"inline-radio",options:["span","p"],defaultValue:"span"}},args:{elementType:"span"},render:r=>e.jsx(X,{children:e.jsx(t,{...r,children:"Text is an unstyled component that can be used to display texts. By default it renders a span."})})},o={},s={render:r=>e.jsx(t,{...r,children:"Text without styling parent components"})},n={render:r=>e.jsxs(t,{...r,children:["Text with unordered list:",e.jsxs("ul",{children:[e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"})]})]})},a={render:r=>e.jsxs(t,{...r,children:["Text with ordered list:",e.jsxs("ol",{children:[e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"})]})]})},i={render:r=>e.jsx(t,{...r,children:e.jsx("small",{children:"Small Text"})})},p={args:{color:"dark"},globals:{backgrounds:"light"}},m={args:{color:"light"},globals:{backgrounds:"dark"}},l={args:{align:"end"}},c={render:r=>e.jsxs(X,{children:[e.jsx(t,{children:e.jsxs("strong",{children:["Without specified ",e.jsx(h,{children:"wrap"}),":"]})}),e.jsx(t,{...r,align:"center",children:"This is a centered text in a small container so the text is wrapped."}),e.jsx(t,{children:e.jsxs("strong",{children:["With ",e.jsx(h,{children:"wrap: balance"}),":"]})}),e.jsx(t,{...r,align:"center",wrap:"balance",children:"This is a centered text in a small container so the text is wrapped."})]}),parameters:{viewport:{defaultViewport:"mobile2"}}},d={render:r=>e.jsxs(t,{...r,children:[e.jsx(g,{})," ",u.medium,e.jsxs("small",{children:[u.short," ",e.jsx(g,{})," ",u.short]})]})},x={render:r=>e.jsx(t,{...r,wordBreak:"break-word",children:u.long.replace(/[^\p{L}\p{N}]/gu,"")})};var T,j,w;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:"{}",...(w=(j=o.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};var I,S,b;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <Text {...props}>Text without styling parent components</Text>
+}`,...(b=(S=s.parameters)==null?void 0:S.docs)==null?void 0:b.source}}};var k,f,y;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with unordered list:
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    </Text>
+}`,...(y=(f=n.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var W,L,B;a.parameters={...a.parameters,docs:{...(W=a.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with ordered list:
+      <ol>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ol>
+    </Text>
+}`,...(B=(L=a.parameters)==null?void 0:L.docs)==null?void 0:B.source}}};var C,D,E;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <small>Small Text</small>
+    </Text>
+}`,...(E=(D=i.parameters)==null?void 0:D.docs)==null?void 0:E.source}}};var O,R,V;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  globals: {
+    backgrounds: "light"
+  }
+}`,...(V=(R=p.parameters)==null?void 0:R.docs)==null?void 0:V.source}}};var v,A,N;m.parameters={...m.parameters,docs:{...(v=m.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  globals: {
+    backgrounds: "dark"
+  }
+}`,...(N=(A=m.parameters)==null?void 0:A.docs)==null?void 0:N.source}}};var U,_,q;l.parameters={...l.parameters,docs:{...(U=l.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    align: "end"
+  }
+}`,...(q=(_=l.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var z,F,G;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        <strong>
+          Without specified <InlineCode>wrap</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+      <Text>
+        <strong>
+          With <InlineCode>wrap: balance</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center" wrap="balance">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2"
+    }
+  }
+}`,...(G=(F=c.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var H,J,K;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <IconStar /> {dummyText.medium}
+      <small>
+        {dummyText.short} <IconStar /> {dummyText.short}
+      </small>
+    </Text>
+}`,...(K=(J=d.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var M,P,Q;x.parameters={...x.parameters,docs:{...(M=x.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: props => <Text {...props} wordBreak="break-word">
+      {dummyText.long.replace(/[^\\p{L}\\p{N}]/gu, "")}
+    </Text>
+}`,...(Q=(P=x.parameters)==null?void 0:P.docs)==null?void 0:Q.source}}};const Ye=["Default","RawText","UnorderedList","OrderedList","SmallText","Dark","Light","AlignEnd","WrapBalance","WithIcon","WithWordBreak"];export{l as AlignEnd,p as Dark,o as Default,m as Light,a as OrderedList,s as RawText,i as SmallText,n as UnorderedList,d as WithIcon,x as WithWordBreak,c as WrapBalance,Ye as __namedExportsOrder,Xe as default};
