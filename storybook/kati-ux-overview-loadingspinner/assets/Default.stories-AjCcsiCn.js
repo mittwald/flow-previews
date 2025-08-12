a3fff1686df4a005313d5ab0b82410d4741d91f1
mittwald <>
2025-08-12 08:52:23 +0000

    Cleanup merged Storybook previews

--- storybook/kati-ux-overview-loadingspinner/assets/Default.stories-AjCcsiCn.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-BBgxZ9_Z.js";import{B as s}from"./BigNumber-BbFMesG8.js";import{T as a}from"./Text-DGcNKUf7.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-IdqCMJT1.js";import"./mergeRefs-BLmxAuNF.js";import"./index-BhvP9-i6.js";import"./browser-XdNXsdMD.js";import"./EmulatedBoldText-Bf9Yi8oc.js";import"./Text-D0NxCgiz.js";import"./utils-MwuRBPAU.js";const S={title:"Data Visualisation/BigNumber",component:s,render:o=>t.jsx(s,{...o,children:"69%"})},r={},e={render:o=>t.jsxs(s,{...o,children:[t.jsx(a,{children:"69%"}),t.jsx(a,{children:"Performance"})]})};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,c,u;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render: props => <BigNumber {...props}>
-      <Text>69%</Text>
-      <Text>Performance</Text>
-    </BigNumber>
-}`,...(u=(c=e.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const D=["Default","WithSubtitle"];export{r as Default,e as WithSubtitle,D as __namedExportsOrder,S as default};
--- storybook/kati-ux-overview-loadingspinner/assets/Heading-B8IOml0A.js
+++
@@ -1,1 +0,0 @@
-import{$ as c}from"./RSPContexts-3v3yZrnH.js";import{$ as f}from"./utils-MwuRBPAU.js";import{r as m,R as s}from"./iframe-BBgxZ9_Z.js";const x=m.forwardRef(function(e,a){[e,a]=f(e,a,c);let{children:t,level:l=3,className:r,...o}=e,$=`h${l}`;return s.createElement($,{...o,ref:a,className:r??"react-aria-Heading"},t)});export{x as $};
