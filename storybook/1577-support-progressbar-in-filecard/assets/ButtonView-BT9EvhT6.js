36d62164f8a706a24919bd7c254b377dec809c4b
mittwald <>
2025-08-12 09:04:49 +0000

    Cleanup merged Storybook previews

--- storybook/1577-support-progressbar-in-filecard/assets/ButtonView-BT9EvhT6.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./iframe-DdApAMdB.js";import{B as s}from"./Button-BxqrW572.js";import{v as r}from"./PropsContextProvider-CEuVt76S.js";const i=t=>{const o=e.useContext(r).Button??s;return n.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
