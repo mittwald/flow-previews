af6320dc13e1502f37d565f2f9f997670cfe6cf7
mittwald <>
2025-08-13 12:05:07 +0000

    Cleanup merged Storybook previews

--- storybook/Nickzlos-markdown-guidelines/assets/EdgeCases.stories-C5L-q4i2.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-CPpiztA9.js";import{L as n}from"./Link-Cwf-Yfzv.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import a from"./Default.stories-C9gjk9eK.js";import{g as s}from"./IconWarning-DGFg2xah.js";import"./PropsContextProvider-CgD2VZvZ.js";import"./mergeRefs-CY9oKSjb.js";import"./index-CXr5LMa8.js";import"./clsx-B-dksMZM.js";import"./useLocalizedStringFormatter-CcOUbIQp.js";import"./context-Dtd4A8DJ.js";import"./utils-DXg4ziAt.js";import"./filterDOMProps-CghfNOdR.js";import"./usePress-Bn1QFLmx.js";import"./useFocus-f0syXR1u.js";import"./useFocusRing-ryN7bem9.js";import"./useFocusable-BdzC7gBh.js";import"./Text-B9GDlRIl.js";import"./browser-CCSZxPxO.js";import"./EmulatedBoldText-49ZPQjyJ.js";import"./Text-D73bWKdR.js";const R={...a,title:"Navigation/Link/Edge Cases"},r={render:m=>o.jsxs(n,{...m,children:[p.long,o.jsx(s,{"aria-label":"external link"})]})};var t,e,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...(i=(e=r.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const S=["LongText"];export{r as LongText,S as __namedExportsOrder,R as default};
