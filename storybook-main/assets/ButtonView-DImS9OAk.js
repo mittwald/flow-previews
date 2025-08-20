ed68afad916ddae1cf394bd24f3216dc8d5b2d4d
mittwald <>
2025-08-20 13:18:20 +0000

    Update from https://github.com/mittwald/flow/commit/ad9577d52b614be9053d408ac9e1f39c669f6769

--- storybook-main/assets/ButtonView-DImS9OAk.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./iframe-_AHHYLe8.js";import{B as s}from"./Button-DpBb7dXP.js";import{v as r}from"./PropsContextProvider-CHGKeBFG.js";const i=t=>{const o=e.useContext(r).Button??s;return n.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
