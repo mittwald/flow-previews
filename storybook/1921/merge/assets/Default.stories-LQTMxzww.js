0e09d1eed57bfb6024d13222a3430033b45c8a9e
mittwald <>
2025-08-26 07:27:00 +0000

    Update from https://github.com/mittwald/flow/commit/a4a95e5bd213be169f019c756368368b295f284b

---
+++ storybook/1921/merge/assets/Default.stories-LQTMxzww.js
@@ -0,0 +1,61 @@
+import{j as r}from"./iframe-CbGYfYhl.js";import{M as i}from"./Message-C6WTW_at.js";import{H as a}from"./Header-Dc3143w-.js";import{B as m}from"./Button-Gcjkh2he.js";import{A as l}from"./Align-BE28ipAt.js";import{A as p}from"./Avatar-BqaP-FlA.js";import{I as c}from"./Initials-_U6PZIw8.js";import{C as d}from"./Content-DTD-acN4.js";import{T as e}from"./Text-OS1-aGym.js";import{M as j}from"./MessageSeparator-oaNaPI4O.js";import{C as x}from"./Chat-D-88_5WV.js";import{d as h}from"./dummyText-CX_I_Wpl.js";import{T as u}from"./TextArea-B_oRii7X.js";import{U as B}from"./IconWarning-DbjMP3z5.js";import{F as I}from"./FileCardList-DzrGMEdt.js";import{F as M}from"./FileCard-Bs_WkRPp.js";import{u as U,F as _,a as v}from"./Form-TtiWG0JA.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-jqIHveb4.js";import"./mergeRefs-B7E9CKB8.js";import"./index-DUvIA6jy.js";import"./LoadingSpinner-BIdUmOnp.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useLocalizedStringFormatter-CO6Ocor8.js";import"./context-DrN7EfSG.js";import"./Button-dGVmYdyu.js";import"./utils-Bta_km8V.js";import"./ProgressBar-C2FOdX6j.js";import"./Label-CQ-8Wai_.js";import"./Hidden-CH7YYats.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-D6vayTQl.js";import"./NumberFormatter-DNR9MAW-.js";import"./usePress-B6TDm-Ku.js";import"./useFocus-CgyMmhEU.js";import"./useFocusRing-D3ulEQrZ.js";import"./useFocusable-CdHr2TzL.js";import"./AlertIcon-OBS66cIc.js";import"./react-children-utilities-CALHcDe7.js";import"./browser-CJ8QzV59.js";import"./EmulatedBoldText-CQSaLX8r.js";import"./Text-B56mlhY7.js";import"./dynamic-DKDa4OpU.js";import"./Separator-DJRg0bD9.js";import"./Separator-BL4Cqy6R.js";import"./CollectionBuilder-zI7nOiFC.js";import"./TextFieldBase-DWb3SUpl.js";import"./FormField.module-B9AzUSUD.js";import"./FieldError-DIOhoIE9.js";import"./FieldError-CCN5oAXI.js";import"./FieldDescription-D4tY6X_s.js";import"./TextField-DGs8gg6j.js";import"./Form-dHMqMIPi.js";import"./Group-G140fFme.js";import"./useTextField-ONw8H3cW.js";import"./useFormReset-knQsHZrP.js";import"./useControlledState-Ckf81dN5.js";import"./ReactAriaControlledValueFix-B7ow68aT.js";import"./ColumnLayout-D8G1VvNk.js";import"./Image-zEqh1WpH.js";import"./Link-Cl1AMoZr.js";import"./ButtonView-DrD4LYA7.js";import"./ContextMenuContent-41w8T_sr.js";import"./Action-QD7EalEG.js";import"./context-J2lB5nq9.js";import"./useStatic-CDZ8mZeU.js";import"./getActionGroupSlot-DTlXxCo0.js";import"./ContextMenuSection-COmeM-5Q.js";import"./Dialog-DWI3Gb4X.js";import"./RSPContexts-CWauLjE1.js";import"./OverlayArrow-CAzhQBDT.js";import"./Collection-CnCkxRBF.js";import"./SearchField-0rw3SXHS.js";import"./useEvent-BrHW1E5Z.js";import"./SelectionManager-C3iuS2jK.js";import"./useCollator-B95zPweT.js";import"./FocusScope-CXp1mWOp.js";import"./VisuallyHidden-BDzAgjFk.js";import"./Popover-AInzHW1-.js";import"./DialogTriggerView-BAlepQJB.js";import"./Switch-CYU4Cetv.js";import"./Label-ChSLebPR.js";import"./useToggleState-CKEjHtck.js";const{action:y}=__STORYBOOK_MODULE_ACTIONS__,le={title:"Chat/Chat",component:x,parameters:{controls:{exclude:["className"]}},args:{height:400},render:t=>r.jsxs(x,{...t,children:[r.jsxs(j,{children:[r.jsxs(i,{children:[r.jsx(a,{children:r.jsxs(l,{children:[r.jsx(p,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]}),r.jsxs(i,{type:"sender",children:[r.jsx(a,{children:r.jsxs(l,{children:[r.jsx(p,{children:r.jsx(c,{children:"John Doe"})}),r.jsx(e,{children:r.jsx("b",{children:"John Doe"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})]}),r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:10}),r.jsx(m,{color:"accent",children:"Submit"})]})},o={},n={render:t=>r.jsxs(x,{...t,children:[r.jsx(j,{children:r.jsxs(i,{children:[r.jsx(a,{children:r.jsxs(l,{children:[r.jsx(p,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})}),r.jsx(m,{color:"secondary",variant:"plain","aria-label":"file attachment",children:r.jsx(B,{})}),r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:10}),r.jsx(m,{color:"accent",children:"Submit"}),r.jsxs(I,{children:[r.jsx(M,{name:"Uploaded File 1"}),r.jsx(M,{name:"Uploaded File 2"})]})]})},s={render:t=>{const w=U();return r.jsxs(x,{...t,children:[r.jsx(j,{children:r.jsxs(i,{children:[r.jsx(a,{children:r.jsxs(l,{children:[r.jsx(p,{children:r.jsx(c,{children:"Max Mustermann"})}),r.jsx(e,{children:r.jsx("b",{children:"Max Mustermann"})})]})}),r.jsx(d,{children:r.jsx(e,{children:h.long})})]})}),r.jsxs(_,{form:w,onSubmit:()=>{y("submitted")},children:[r.jsx(v,{name:"message",children:r.jsx(u,{"aria-label":"message",rows:1,autoResizeMaxRows:10})}),r.jsx(m,{color:"accent",children:"Submit"})]})]})}};var g,f,b;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:"{}",...(b=(f=o.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var T,F,C;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: props => <Chat {...props}>
+      <MessageThread>
+        <Message>
+          <Header>
+            <Align>
+              <Avatar>
+                <Initials>Max Mustermann</Initials>
+              </Avatar>
+              <Text>
+                <b>Max Mustermann</b>
+              </Text>
+            </Align>
+          </Header>
+          <Content>
+            <Text>{dummyText.long}</Text>
+          </Content>
+        </Message>
+      </MessageThread>
+      <Button color="secondary" variant="plain" aria-label="file attachment">
+        <IconAttachment />
+      </Button>
+      <TextArea aria-label="message" rows={1} autoResizeMaxRows={10} />
+      <Button color="accent">Submit</Button>
+      <FileCardList>
+        <FileCard name="Uploaded File 1" />
+        <FileCard name="Uploaded File 2" />
+      </FileCardList>
+    </Chat>
+}`,...(C=(F=n.parameters)==null?void 0:F.docs)==null?void 0:C.source}}};var A,R,S;s.parameters={...s.parameters,docs:{...(A=s.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Chat {...props}>
+        <MessageThread>
+          <Message>
+            <Header>
+              <Align>
+                <Avatar>
+                  <Initials>Max Mustermann</Initials>
+                </Avatar>
+                <Text>
+                  <b>Max Mustermann</b>
+                </Text>
+              </Align>
+            </Header>
+            <Content>
+              <Text>{dummyText.long}</Text>
+            </Content>
+          </Message>
+        </MessageThread>
+        <Form form={form} onSubmit={() => {
+        action("submitted");
+      }}>
+          <Field name="message">
+            <TextArea aria-label="message" rows={1} autoResizeMaxRows={10} />
+          </Field>
+          <Button color="accent">Submit</Button>
+        </Form>
+      </Chat>;
+  }
+}`,...(S=(R=s.parameters)==null?void 0:R.docs)==null?void 0:S.source}}};const pe=["Default","WithUpload","WithForm"];export{o as Default,s as WithForm,n as WithUpload,pe as __namedExportsOrder,le as default};
