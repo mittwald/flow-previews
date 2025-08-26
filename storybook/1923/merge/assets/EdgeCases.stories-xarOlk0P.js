956f4cb0b2d6feb864325a5568a8b33a06212a09
mittwald <>
2025-08-26 10:06:58 +0000

    Update from https://github.com/mittwald/flow/commit/adb0fea15f9543b6556eaedba8e2d32e79cf7901

---
+++ storybook/1923/merge/assets/EdgeCases.stories-xarOlk0P.js
@@ -0,0 +1,6 @@
+import{j as o}from"./iframe-CbGYfYhl.js";import{L as n}from"./Link-Cl1AMoZr.js";import{d as p}from"./dummyText-CX_I_Wpl.js";import a from"./Default.stories-KDMYRHIy.js";import{g as s}from"./IconWarning-DbjMP3z5.js";import"./PropsContextProvider-jqIHveb4.js";import"./mergeRefs-B7E9CKB8.js";import"./index-DUvIA6jy.js";import"./clsx-B-dksMZM.js";import"./useLocalizedStringFormatter-CO6Ocor8.js";import"./context-DrN7EfSG.js";import"./utils-Bta_km8V.js";import"./filterDOMProps-CghfNOdR.js";import"./usePress-B6TDm-Ku.js";import"./useFocus-CgyMmhEU.js";import"./useFocusRing-D3ulEQrZ.js";import"./useFocusable-CdHr2TzL.js";import"./Text-OS1-aGym.js";import"./browser-CJ8QzV59.js";import"./EmulatedBoldText-CQSaLX8r.js";import"./Text-B56mlhY7.js";const R={...a,title:"Navigation/Link/Edge Cases"},r={render:m=>o.jsxs(n,{...m,children:[p.long,o.jsx(s,{"aria-label":"external link"})]})};var t,e,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: props => <Link {...props}>
+      {dummyText.long}
+      <IconExternalLink aria-label="external link" />
+    </Link>
+}`,...(i=(e=r.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const S=["LongText"];export{r as LongText,S as __namedExportsOrder,R as default};
---
+++ storybook/1923/merge/assets/Levels.stories-C7kptCDP.js
@@ -0,0 +1,25 @@
+import{H as O}from"./Heading-CwkEWkPk.js";import $ from"./Default.stories-BMOmL9rK.js";import"./iframe-CbGYfYhl.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-jqIHveb4.js";import"./mergeRefs-B7E9CKB8.js";import"./index-DUvIA6jy.js";import"./Heading-Cd40tbYh.js";import"./RSPContexts-CWauLjE1.js";import"./utils-Bta_km8V.js";import"./IconWarning-DbjMP3z5.js";import"./Section-BbmZZsTL.js";import"./ContextMenuSection-COmeM-5Q.js";import"./Action-QD7EalEG.js";import"./context-J2lB5nq9.js";import"./useStatic-CDZ8mZeU.js";import"./browser-CJ8QzV59.js";import"./getActionGroupSlot-DTlXxCo0.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-DWI3Gb4X.js";import"./Button-dGVmYdyu.js";import"./ProgressBar-C2FOdX6j.js";import"./Label-CQ-8Wai_.js";import"./Hidden-CH7YYats.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-D6vayTQl.js";import"./context-DrN7EfSG.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-B6TDm-Ku.js";import"./useFocus-CgyMmhEU.js";import"./useFocusRing-D3ulEQrZ.js";import"./useFocusable-CdHr2TzL.js";import"./OverlayArrow-CAzhQBDT.js";import"./useControlledState-Ckf81dN5.js";import"./Collection-CnCkxRBF.js";import"./CollectionBuilder-zI7nOiFC.js";import"./Separator-BL4Cqy6R.js";import"./Text-B56mlhY7.js";import"./Group-G140fFme.js";import"./SearchField-0rw3SXHS.js";import"./FieldError-CCN5oAXI.js";import"./Form-dHMqMIPi.js";import"./useLocalizedStringFormatter-CO6Ocor8.js";import"./useTextField-ONw8H3cW.js";import"./useFormReset-knQsHZrP.js";import"./TextField-DGs8gg6j.js";import"./useEvent-BrHW1E5Z.js";import"./SelectionManager-C3iuS2jK.js";import"./useCollator-B95zPweT.js";import"./FocusScope-CXp1mWOp.js";import"./VisuallyHidden-BDzAgjFk.js";const Or={...$,title:"Content/Heading/Levels",component:O},r={args:{level:1}},o={args:{level:2}},e={args:{level:3}},t={args:{level:4}},m={args:{level:5}},p={args:{level:6}};var s,a,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    level: 1
+  }
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var c,n,l;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    level: 2
+  }
+}`,...(l=(n=o.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var d,g,u;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    level: 3
+  }
+}`,...(u=(g=e.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var H,v,S;t.parameters={...t.parameters,docs:{...(H=t.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    level: 4
+  }
+}`,...(S=(v=t.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var f,x,_;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    level: 5
+  }
+}`,...(_=(x=m.parameters)==null?void 0:x.docs)==null?void 0:_.source}}};var C,E,L;p.parameters={...p.parameters,docs:{...(C=p.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    level: 6
+  }
+}`,...(L=(E=p.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};const $r=["H1","H2","H3","H4","H5","H6"];export{r as H1,o as H2,e as H3,t as H4,m as H5,p as H6,$r as __namedExportsOrder,Or as default};
