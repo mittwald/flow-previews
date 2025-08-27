3bf87c4c6f56676b34c63b19d1811394262bb430
mittwald <>
2025-08-27 11:00:41 +0000

    Update from https://github.com/mittwald/flow/commit/ee1612c7eb13588356732bdcf974610e64366f9b

---
+++ storybook/1928/merge/assets/Default.stories-COyg_rt8.js
@@ -0,0 +1,24 @@
+import{j as e}from"./iframe-CbGYfYhl.js";import{D as t}from"./DatePicker-DtSjIk57.js";import{L as m}from"./Label-ChSLebPR.js";import{F as R}from"./FieldDescription-D4tY6X_s.js";import{h as $,J as q,L as k}from"./DateField-BuvTmWto.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-jqIHveb4.js";import"./mergeRefs-B7E9CKB8.js";import"./index-DUvIA6jy.js";import"./Button-Gcjkh2he.js";import"./IconWarning-DbjMP3z5.js";import"./Text-OS1-aGym.js";import"./browser-CJ8QzV59.js";import"./EmulatedBoldText-CQSaLX8r.js";import"./Text-B56mlhY7.js";import"./utils-Bta_km8V.js";import"./LoadingSpinner-BIdUmOnp.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./useLocalizedStringFormatter-CO6Ocor8.js";import"./context-DrN7EfSG.js";import"./Button-dGVmYdyu.js";import"./ProgressBar-C2FOdX6j.js";import"./Label-CQ-8Wai_.js";import"./Hidden-CH7YYats.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-D6vayTQl.js";import"./NumberFormatter-DNR9MAW-.js";import"./usePress-B6TDm-Ku.js";import"./useFocus-CgyMmhEU.js";import"./useFocusRing-D3ulEQrZ.js";import"./useFocusable-CdHr2TzL.js";import"./Group-G140fFme.js";import"./FieldError-DIOhoIE9.js";import"./FieldError-CCN5oAXI.js";import"./FormField.module-B9AzUSUD.js";import"./Popover-AInzHW1-.js";import"./DialogTriggerView-BAlepQJB.js";import"./context-J2lB5nq9.js";import"./useStatic-CDZ8mZeU.js";import"./Dialog-DWI3Gb4X.js";import"./RSPContexts-CWauLjE1.js";import"./OverlayArrow-CAzhQBDT.js";import"./useControlledState-Ckf81dN5.js";import"./Collection-CnCkxRBF.js";import"./CollectionBuilder-zI7nOiFC.js";import"./Separator-BL4Cqy6R.js";import"./SearchField-0rw3SXHS.js";import"./Form-dHMqMIPi.js";import"./useTextField-ONw8H3cW.js";import"./useFormReset-knQsHZrP.js";import"./TextField-DGs8gg6j.js";import"./useEvent-BrHW1E5Z.js";import"./SelectionManager-C3iuS2jK.js";import"./useCollator-B95zPweT.js";import"./FocusScope-CXp1mWOp.js";import"./VisuallyHidden-BDzAgjFk.js";import"./RangeCalendar-BPsDQHL0.js";import"./Heading-Cd40tbYh.js";import"./useUpdateEffect-BJsf15T_.js";import"./useSpinButton-BO2mhGZh.js";import"./useFilter-CjTp5x30.js";const Te={title:"Form Controls/DatePicker",component:t,render:r=>e.jsx(t,{...r,isRequired:!0,children:e.jsx(m,{children:"Date"})}),parameters:{controls:{exclude:["errorMessage"]}}},a={},o={args:{isDisabled:!0}},i={args:{minValue:$(k())},render:r=>e.jsx(t,{isRequired:!0,defaultValue:q("2012-07-03"),...r,isInvalid:!0,errorMessage:"Date is in the past",children:e.jsx(m,{children:"Future Date"})})},s={args:{minValue:$(k())},render:r=>e.jsxs(t,{isRequired:!0,...r,children:[e.jsx(m,{children:"Future Date"}),e.jsx(R,{children:"Select a future date"})]})},p={render:r=>e.jsx(t,{isRequired:!0,isDateUnavailable:P=>P.day!==1,...r,children:e.jsx(m,{children:"Future Date"})})};var n,c,d;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,l,D;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(D=(l=o.parameters)==null?void 0:l.docs)==null?void 0:D.source}}};var f,b,x;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DatePicker isRequired defaultValue={parseDate("2012-07-03")} {...props} isInvalid errorMessage="Date is in the past">
+      <Label>Future Date</Label>
+    </DatePicker>
+}`,...(x=(b=i.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var g,h,F;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    minValue: today(getLocalTimeZone())
+  },
+  render: props => <DatePicker isRequired {...props}>
+      <Label>Future Date</Label>
+      <FieldDescription>Select a future date</FieldDescription>
+    </DatePicker>
+}`,...(F=(h=s.parameters)==null?void 0:h.docs)==null?void 0:F.source}}};var j,L,y;p.parameters={...p.parameters,docs:{...(j=p.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: props => <DatePicker isRequired isDateUnavailable={(date: DateValue) => date.day !== 1} {...props}>
+      <Label>Future Date</Label>
+    </DatePicker>
+}`,...(y=(L=p.parameters)==null?void 0:L.docs)==null?void 0:y.source}}};const Ue=["Default","Disabled","Invalid","FutureDatesOnly","OnlyOneDayInMonth"];export{a as Default,o as Disabled,s as FutureDatesOnly,i as Invalid,p as OnlyOneDayInMonth,Ue as __namedExportsOrder,Te as default};
