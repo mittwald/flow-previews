7f5d5f60ce1ddbcac5a57c9aae079e92ac4f84d6
mittwald <>
2025-08-27 06:54:33 +0000

    Update from https://github.com/mittwald/flow/commit/13b2a0fd90d29418539b2f7fdb354015acb79dbb

---
+++ storybook/1925/merge/assets/Default.stories-C5dmS-qB.js
@@ -0,0 +1,78 @@
+import{j as e}from"./iframe-CbGYfYhl.js";import{T as t}from"./Text-OS1-aGym.js";import{S as X}from"./Section-BbmZZsTL.js";import{I as h}from"./InlineCode-CCad4sTH.js";import{I as g}from"./IconWarning-DbjMP3z5.js";import{d as u}from"./dummyText-CX_I_Wpl.js";import"./PropsContextProvider-jqIHveb4.js";import"./mergeRefs-B7E9CKB8.js";import"./index-DUvIA6jy.js";import"./clsx-B-dksMZM.js";import"./browser-CJ8QzV59.js";import"./EmulatedBoldText-CQSaLX8r.js";import"./Text-B56mlhY7.js";import"./utils-Bta_km8V.js";import"./ContextMenuSection-COmeM-5Q.js";import"./Action-QD7EalEG.js";import"./context-J2lB5nq9.js";import"./useStatic-CDZ8mZeU.js";import"./getActionGroupSlot-DTlXxCo0.js";import"./dynamic-DKDa4OpU.js";import"./Dialog-DWI3Gb4X.js";import"./Button-dGVmYdyu.js";import"./ProgressBar-C2FOdX6j.js";import"./Label-CQ-8Wai_.js";import"./Hidden-CH7YYats.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-D6vayTQl.js";import"./context-DrN7EfSG.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-B6TDm-Ku.js";import"./useFocus-CgyMmhEU.js";import"./useFocusRing-D3ulEQrZ.js";import"./useFocusable-CdHr2TzL.js";import"./RSPContexts-CWauLjE1.js";import"./OverlayArrow-CAzhQBDT.js";import"./useControlledState-Ckf81dN5.js";import"./Collection-CnCkxRBF.js";import"./CollectionBuilder-zI7nOiFC.js";import"./Separator-BL4Cqy6R.js";import"./Group-G140fFme.js";import"./SearchField-0rw3SXHS.js";import"./FieldError-CCN5oAXI.js";import"./Form-dHMqMIPi.js";import"./useLocalizedStringFormatter-CO6Ocor8.js";import"./useTextField-ONw8H3cW.js";import"./useFormReset-knQsHZrP.js";import"./TextField-DGs8gg6j.js";import"./useEvent-BrHW1E5Z.js";import"./SelectionManager-C3iuS2jK.js";import"./useCollator-B95zPweT.js";import"./FocusScope-CXp1mWOp.js";import"./VisuallyHidden-BDzAgjFk.js";const Xe={title:"Content/Text",component:t,argTypes:{elementType:{control:"inline-radio",options:["span","p"],defaultValue:"span"}},args:{elementType:"span"},render:r=>e.jsx(X,{children:e.jsx(t,{...r,children:"Text is an unstyled component that can be used to display texts. By default it renders a span."})})},o={},s={render:r=>e.jsx(t,{...r,children:"Text without styling parent components"})},n={render:r=>e.jsxs(t,{...r,children:["Text with unordered list:",e.jsxs("ul",{children:[e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"})]})]})},a={render:r=>e.jsxs(t,{...r,children:["Text with ordered list:",e.jsxs("ol",{children:[e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"}),e.jsx("li",{children:"Item"})]})]})},i={render:r=>e.jsx(t,{...r,children:e.jsx("small",{children:"Small Text"})})},p={args:{color:"dark"},globals:{backgrounds:"light"}},m={args:{color:"light"},globals:{backgrounds:"dark"}},l={args:{align:"end"}},c={render:r=>e.jsxs(X,{children:[e.jsx(t,{children:e.jsxs("strong",{children:["Without specified ",e.jsx(h,{children:"wrap"}),":"]})}),e.jsx(t,{...r,align:"center",children:"This is a centered text in a small container so the text is wrapped."}),e.jsx(t,{children:e.jsxs("strong",{children:["With ",e.jsx(h,{children:"wrap: balance"}),":"]})}),e.jsx(t,{...r,align:"center",wrap:"balance",children:"This is a centered text in a small container so the text is wrapped."})]}),parameters:{viewport:{defaultViewport:"mobile2"}}},d={render:r=>e.jsxs(t,{...r,children:[e.jsx(g,{})," ",u.medium,e.jsxs("small",{children:[u.short," ",e.jsx(g,{})," ",u.short]})]})},x={render:r=>e.jsx(t,{...r,wordBreak:"break-word",children:u.long.replace(/[^\p{L}\p{N}]/gu,"")})};var T,j,w;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:"{}",...(w=(j=o.parameters)==null?void 0:j.docs)==null?void 0:w.source}}};var I,S,b;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: props => <Text {...props}>Text without styling parent components</Text>
+}`,...(b=(S=s.parameters)==null?void 0:S.docs)==null?void 0:b.source}}};var k,f,y;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with unordered list:
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    </Text>
+}`,...(y=(f=n.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var W,L,B;a.parameters={...a.parameters,docs:{...(W=a.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      Text with ordered list:
+      <ol>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ol>
+    </Text>
+}`,...(B=(L=a.parameters)==null?void 0:L.docs)==null?void 0:B.source}}};var C,D,E;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <small>Small Text</small>
+    </Text>
+}`,...(E=(D=i.parameters)==null?void 0:D.docs)==null?void 0:E.source}}};var O,R,V;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  globals: {
+    backgrounds: "light"
+  }
+}`,...(V=(R=p.parameters)==null?void 0:R.docs)==null?void 0:V.source}}};var v,A,N;m.parameters={...m.parameters,docs:{...(v=m.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  globals: {
+    backgrounds: "dark"
+  }
+}`,...(N=(A=m.parameters)==null?void 0:A.docs)==null?void 0:N.source}}};var U,_,q;l.parameters={...l.parameters,docs:{...(U=l.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    align: "end"
+  }
+}`,...(q=(_=l.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var z,F,G;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Text>
+        <strong>
+          Without specified <InlineCode>wrap</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+      <Text>
+        <strong>
+          With <InlineCode>wrap: balance</InlineCode>:
+        </strong>
+      </Text>
+      <Text {...props} align="center" wrap="balance">
+        This is a centered text in a small container so the text is wrapped.
+      </Text>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2"
+    }
+  }
+}`,...(G=(F=c.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var H,J,K;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: props => <Text {...props}>
+      <IconStar /> {dummyText.medium}
+      <small>
+        {dummyText.short} <IconStar /> {dummyText.short}
+      </small>
+    </Text>
+}`,...(K=(J=d.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var M,P,Q;x.parameters={...x.parameters,docs:{...(M=x.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: props => <Text {...props} wordBreak="break-word">
+      {dummyText.long.replace(/[^\\p{L}\\p{N}]/gu, "")}
+    </Text>
+}`,...(Q=(P=x.parameters)==null?void 0:P.docs)==null?void 0:Q.source}}};const Ye=["Default","RawText","UnorderedList","OrderedList","SmallText","Dark","Light","AlignEnd","WrapBalance","WithIcon","WithWordBreak"];export{l as AlignEnd,p as Dark,o as Default,m as Light,a as OrderedList,s as RawText,i as SmallText,n as UnorderedList,d as WithIcon,x as WithWordBreak,c as WrapBalance,Ye as __namedExportsOrder,Xe as default};
