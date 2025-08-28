5343f89f7f24ad42dc36cdd3b35e3fa031930ca6
mittwald <>
2025-08-28 06:11:33 +0000

    Update from https://github.com/mittwald/flow/commit/1e49d67ad158069cda45f1c04744448bae4177fd

---
+++ storybook/1929/merge/assets/ButtonView-DrD4LYA7.js
@@ -0,0 +1,1 @@
+import{r as e,j as n}from"./iframe-CbGYfYhl.js";import{B as s}from"./Button-Gcjkh2he.js";import{v as r}from"./PropsContextProvider-jqIHveb4.js";const i=t=>{const o=e.useContext(r).Button??s;return n.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
