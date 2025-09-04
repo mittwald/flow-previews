89ac081650a64cbaef064e26716a1825b796298e
mittwald <>
2025-09-04 11:00:58 +0000

    Cleanup merged Storybook previews

--- storybook/1953/merge/assets/FileCardList-qWPsGLvr.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./iframe-Coh3lrZj.js";import{C as i}from"./ColumnLayout-CCp9bR-F.js";import{f as a,P as p}from"./PropsContextProvider-CKV7yByX.js";const m=a("FileCardList",r=>{const{className:o,ref:t,children:s,...n}=r,l={FileCard:{elementType:"li"}};return e.jsx(p,{props:l,children:e.jsx(i,{ref:t,elementType:"ul",className:o,...n,children:s})})});m.__docgenInfo={description:`@flr-generate all
-@flr-clear-props-context`,methods:[],displayName:"FileCardList"};export{m as F};
