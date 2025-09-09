582ea3be136462555b0a34ab22d53e78ab48ab9d
mittwald <>
2025-09-09 12:06:34 +0000

    Cleanup merged Storybook previews

--- storybook/1964/merge/assets/ButtonView-bLk6_MFs.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./iframe-CW4eA9k4.js";import{B as s}from"./Button-BjlFMLDb.js";import{v as r}from"./PropsContextProvider-Bql0h4A2.js";const i=t=>{const o=e.useContext(r).Button??s;return n.jsx(o,{...t})};i.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{i as B};
