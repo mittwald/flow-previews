1e7006a01976914c50d55616d0a3cc7414c7de9f
mittwald <>
2025-09-25 11:33:50 +0000

    Cleanup merged Storybook previews

--- storybook/2009/merge/assets/Default.stories-DXwjJaj-.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-RErHS549.js";import{B as s}from"./BigNumber-Lq14Ybhs.js";import{T as a}from"./Text-CPoMhumh.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-U9W9I2uQ.js";import"./mergeRefs-tFWMCLRY.js";import"./index-CKO7ssoe.js";import"./browser-32b-cNga.js";import"./EmulatedBoldText-JQA-sSfV.js";import"./Text-CT26T-mx.js";import"./utils-CdmgfGZ7.js";const S={title:"Data Visualisation/BigNumber",component:s,render:o=>t.jsx(s,{...o,children:"69%"})},r={},e={render:o=>t.jsxs(s,{...o,children:[t.jsx(a,{children:"69%"}),t.jsx(a,{children:"Performance"})]})};var i,m,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,c,u;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render: props => <BigNumber {...props}>
-      <Text>69%</Text>
-      <Text>Performance</Text>
-    </BigNumber>
-}`,...(u=(c=e.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const D=["Default","WithSubtitle"];export{r as Default,e as WithSubtitle,D as __namedExportsOrder,S as default};
