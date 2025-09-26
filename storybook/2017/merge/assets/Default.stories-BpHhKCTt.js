886f17d96ae86ea8efa95c2425a2d8f2153faae3
mittwald <>
2025-09-26 12:53:36 +0000

    Update from https://github.com/mittwald/flow/commit/fe0ecc960428449a7d85c3023a59603eea3a37bd

---
+++ storybook/2017/merge/assets/Default.stories-BpHhKCTt.js
@@ -0,0 +1,13 @@
+import{j as _}from"./iframe-JrijvI0G.js";import{S as t}from"./Switch-BhTGOwor.js";import"./index-Cun1SEai.js";import"./clsx-B-dksMZM.js";import"./IconWarning-B3U3_U2s.js";import"./PropsContextProvider-Bc6JGim9.js";import"./mergeRefs-CbRd_Ras.js";import"./index-BPCP2ZZE.js";import"./Label-BWdEIo8l.js";import"./useLocalizedStringFormatter-M08v5G9w.js";import"./context-CnkMg4nA.js";import"./Label-1jXHzd37.js";import"./utils-DhZkrhHH.js";import"./Hidden-CHpGmdBq.js";import"./filterDOMProps-CghfNOdR.js";import"./useToggleState-CJGhq3Gn.js";import"./useFormReset-Bl0dawgu.js";import"./usePress-JaoXEhrO.js";import"./useFocus-DA61-eEI.js";import"./useFocusRing-DCbTuOGO.js";import"./useFocusable-DIk21gkV.js";import"./useControlledState-DQPwS-UF.js";import"./VisuallyHidden-B4SYhrOy.js";const{action:D}=__STORYBOOK_MODULE_ACTIONS__,v={title:"Form Controls/Switch",component:t,args:{onChange:D("onChange"),labelPosition:"trailing"},parameters:{controls:{exclude:["onChange"]}},argTypes:{labelPosition:{control:"inline-radio",options:["trailing","leading"]}},render:S=>_.jsx(t,{...S,children:"Autoresponder"})},r={},e={args:{labelPosition:"leading"}},o={args:{isDisabled:!0}},a={args:{isReadOnly:!0}};var s,i,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var m,p,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    labelPosition: "leading"
+  }
+}`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var l,d,g;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    isDisabled: true
+  }
+}`,...(g=(d=o.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,b,O;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isReadOnly: true
+  }
+}`,...(O=(b=a.parameters)==null?void 0:b.docs)==null?void 0:O.source}}};const z=["Default","LabelLeading","Disabled","ReadOnly"];export{r as Default,o as Disabled,e as LabelLeading,a as ReadOnly,z as __namedExportsOrder,v as default};
---
+++ storybook/2017/merge/assets/Default.stories-CK2OAHCT.js
@@ -0,0 +1,41 @@
+import{j as o}from"./iframe-JrijvI0G.js";import{H as e}from"./Heading-C_XHAp4M.js";import{a2 as w}from"./IconWarning-B3U3_U2s.js";import{S as W}from"./Section-_sZpzwSW.js";import"./index-Cun1SEai.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Bc6JGim9.js";import"./mergeRefs-CbRd_Ras.js";import"./index-BPCP2ZZE.js";import"./Heading-BW0daRbe.js";import"./RSPContexts-bEimFc0k.js";import"./utils-DhZkrhHH.js";import"./ContextMenuSection-B_Wuc4T7.js";import"./Action-CunTtwFt.js";import"./context-CS_K62PD.js";import"./useStatic-9jHtrOTK.js";import"./browser-eHAjn9uV.js";import"./getActionGroupSlot-BuUDRoIK.js";import"./dynamic-BUc0KwfX.js";import"./Popover-irUcUI-O.js";import"./DialogTriggerView-DOh3nc4_.js";import"./Button-Bp13GR3E.js";import"./ProgressBar-D10mUZdz.js";import"./Label-1jXHzd37.js";import"./Hidden-CHpGmdBq.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-D5nQ4vXC.js";import"./context-CnkMg4nA.js";import"./NumberFormatter-DNR9MAW-.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./usePress-JaoXEhrO.js";import"./useFocus-DA61-eEI.js";import"./useFocusRing-DCbTuOGO.js";import"./useFocusable-DIk21gkV.js";import"./ClearPropsContextView-Dl_oHLSA.js";import"./useControlledState-DQPwS-UF.js";import"./FocusScope-Bwbpfbbo.js";import"./useCollator-ChdaOTfc.js";import"./useLocalizedStringFormatter-M08v5G9w.js";import"./VisuallyHidden-B4SYhrOy.js";import"./Collection-yP15xrib.js";import"./CollectionBuilder-CnCpguDd.js";import"./context-SLDg-fw2.js";import"./Separator-D6-9qD1D.js";import"./Text-Bm8Zb_oa.js";import"./SelectionManager-C2kTlReE.js";import"./useEvent-BTKtfW_h.js";import"./Switch-BhTGOwor.js";import"./Label-BWdEIo8l.js";import"./useToggleState-CJGhq3Gn.js";import"./useFormReset-Bl0dawgu.js";const zr={title:"Content/Heading",component:e,argTypes:{level:{control:"inline-radio",options:[1,2,3,4,5,6]},size:{control:"inline-radio",options:["xs","s","m","l","xl","xxl"]},color:{control:"inline-radio",options:["primary","dark","light"]}},args:{level:2},render:r=>o.jsxs(e,{...r,children:["I am a H",r.level," Heading"]})},t={},n={render:r=>o.jsxs(e,{...r,children:[o.jsx(w,{}),"Personal Information"]})},a={args:{level:1,size:"xxl"},render:r=>o.jsxs(e,{level:1,size:"xxl",...r,children:[o.jsx(w,{}),"Personal Information"]})},i={args:{color:"dark"},globals:{backgrounds:"light"}},s={args:{color:"light"},globals:{backgrounds:"dark"}},p={render:r=>o.jsxs(W,{children:[o.jsx(e,{...r,children:"Personal Information of the current user"}),o.jsx(e,{...r,wrap:"balance",children:"Personal Information of the current user"})]}),parameters:{viewport:{defaultViewport:"mobile2"}}};var m,c,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var d,g,u;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: props => <Heading {...props}>
+      <IconMember />
+      Personal Information
+    </Heading>
+}`,...(u=(g=n.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var x,f,h;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    level: 1,
+    size: "xxl"
+  },
+  render: props => <Heading level={1} size="xxl" {...props}>
+      <IconMember />
+      Personal Information
+    </Heading>
+}`,...(h=(f=a.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var b,I,H;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    color: "dark"
+  },
+  globals: {
+    backgrounds: "light"
+  }
+}`,...(H=(I=i.parameters)==null?void 0:I.docs)==null?void 0:H.source}}};var S,k,j;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    color: "light"
+  },
+  globals: {
+    backgrounds: "dark"
+  }
+}`,...(j=(k=s.parameters)==null?void 0:k.docs)==null?void 0:j.source}}};var v,P,z;p.parameters={...p.parameters,docs:{...(v=p.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: props => <Section>
+      <Heading {...props}>Personal Information of the current user</Heading>
+      <Heading {...props} wrap="balance">
+        Personal Information of the current user
+      </Heading>
+    </Section>,
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile2"
+    }
+  }
+}`,...(z=(P=p.parameters)==null?void 0:P.docs)==null?void 0:z.source}}};const wr=["Default","WithIcon","WithCustomSize","Dark","Light","Wrap"];export{i as Dark,t as Default,s as Light,a as WithCustomSize,n as WithIcon,p as Wrap,wr as __namedExportsOrder,zr as default};
