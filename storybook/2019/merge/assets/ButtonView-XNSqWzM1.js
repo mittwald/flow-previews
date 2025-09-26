4f28f713d0f77376c7d3e604790b73d28fe74fee
mittwald <>
2025-09-26 15:08:06 +0000

    Update from https://github.com/mittwald/flow/commit/82d65e05864ad411c3caf264dec0a82de874ab5e

---
+++ storybook/2019/merge/assets/ButtonView-XNSqWzM1.js
@@ -0,0 +1,1 @@
+import{r as t,j as s}from"./iframe-JrijvI0G.js";import{B as r}from"./Button-nOLp9HyP.js";import{v as i}from"./PropsContextProvider-Bc6JGim9.js";const o=t.memo(e=>{const n=t.useContext(i).Button??r;return s.jsx(n,{...e})});o.displayName="ButtonView";o.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{o as B};
---
+++ storybook/2019/merge/assets/Default.stories-Cyy1-zfi.js
@@ -0,0 +1,45 @@
+import{j as e}from"./iframe-JrijvI0G.js";import{L as o}from"./LabeledValue-6Du6D22r.js";import{L as t}from"./Label-BWdEIo8l.js";import{C as k}from"./Content-BxeQqMi9.js";import{C as l}from"./CopyButton-yFxpCMxK.js";import{B as I}from"./Button-nOLp9HyP.js";import{I as P}from"./InlineCode-DZMJuouN.js";import{L as M}from"./Link-3TLUsjGm.js";import{C as R,a as D}from"./ContextualHelpTrigger-1-THRkGe.js";import{H as E}from"./Heading-C_XHAp4M.js";import{T as d}from"./Text-9s7Vdafx.js";import"./index-Cun1SEai.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Bc6JGim9.js";import"./mergeRefs-CbRd_Ras.js";import"./index-BPCP2ZZE.js";import"./useLocalizedStringFormatter-M08v5G9w.js";import"./context-CnkMg4nA.js";import"./Label-1jXHzd37.js";import"./utils-DhZkrhHH.js";import"./Hidden-CHpGmdBq.js";import"./IconWarning-B3U3_U2s.js";import"./Tooltip-C5LUaqFr.js";import"./ClearPropsContextView-Dl_oHLSA.js";import"./useFocus-DA61-eEI.js";import"./ProgressBar-D10mUZdz.js";import"./filterDOMProps-CghfNOdR.js";import"./useLabel-D5nQ4vXC.js";import"./NumberFormatter-DNR9MAW-.js";import"./useControlledState-DQPwS-UF.js";import"./useFocusable-DIk21gkV.js";import"./useFocusRing-DCbTuOGO.js";import"./react-children-utilities-C5pwjcTQ.js";import"./Action-CunTtwFt.js";import"./context-CS_K62PD.js";import"./useStatic-9jHtrOTK.js";import"./browser-eHAjn9uV.js";import"./getActionGroupSlot-BuUDRoIK.js";import"./dynamic-BUc0KwfX.js";import"./LoadingSpinner-BpDSxo2f.js";import"./LiveAnnouncer-CeCcBDbP.js";import"./Button-Bp13GR3E.js";import"./usePress-JaoXEhrO.js";import"./Popover-irUcUI-O.js";import"./DialogTriggerView-DOh3nc4_.js";import"./RSPContexts-bEimFc0k.js";import"./FocusScope-Bwbpfbbo.js";import"./useCollator-ChdaOTfc.js";import"./VisuallyHidden-B4SYhrOy.js";import"./Collection-yP15xrib.js";import"./CollectionBuilder-CnCpguDd.js";import"./context-SLDg-fw2.js";import"./Separator-D6-9qD1D.js";import"./Text-Bm8Zb_oa.js";import"./SelectionManager-C2kTlReE.js";import"./useEvent-BTKtfW_h.js";import"./Heading-BW0daRbe.js";import"./EmulatedBoldText-BVZ71ko2.js";const _e={title:"Content/LabeledValue",component:o,parameters:{controls:{exclude:["className"]}},render:r=>e.jsxs(o,{...r,children:[e.jsx(t,{children:"Project"}),e.jsx(k,{children:"My proSpace"})]})},n={},a={render:r=>e.jsxs(o,{...r,children:[e.jsx(t,{children:"Project"}),e.jsx(k,{children:"My proSpace"}),e.jsx(l,{text:"My proSpace"})]})},s={render:r=>e.jsxs(o,{...r,children:[e.jsx(t,{children:"IP address"}),e.jsx(P,{children:"127.0.0.1"}),e.jsx(l,{text:"127.0.0.1"})]})},i={render:r=>e.jsxs(o,{...r,children:[e.jsx(t,{children:"Domain"}),e.jsx(M,{children:"https://mittwald.de"}),e.jsx(l,{text:"https://mittwald.de"})]})},p={render:r=>e.jsxs(o,{...r,children:[e.jsx(t,{children:"Support pin"}),e.jsx(I,{variant:"soft",color:"secondary",children:"Generate"})]})},m={render:r=>e.jsxs(o,{children:[e.jsxs(t,{children:["Role",e.jsxs(R,{children:[e.jsx(I,{}),e.jsxs(D,{...r,children:[e.jsx(E,{children:"Rights & roles"}),e.jsx(d,{children:"Each user profile is assigned a role in mStudio for each project and/or organization. This allows you to work in a completely new and modern way."}),e.jsx(M,{children:"Learn more"})]})]})]}),e.jsx(d,{children:"Admin"})]})};var c,u,x;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(x=(u=n.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};var h,L,j;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Project</Label>
+      <Content>My proSpace</Content>
+      <CopyButton text="My proSpace" />
+    </LabeledValue>
+}`,...(j=(L=a.parameters)==null?void 0:L.docs)==null?void 0:j.source}}};var C,b,g;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>IP address</Label>
+      <InlineCode>127.0.0.1</InlineCode>
+      <CopyButton text="127.0.0.1" />
+    </LabeledValue>
+}`,...(g=(b=s.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var f,y,S;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Domain</Label>
+      <Link>https://mittwald.de</Link>
+      <CopyButton text="https://mittwald.de" />
+    </LabeledValue>
+}`,...(S=(y=i.parameters)==null?void 0:y.docs)==null?void 0:S.source}}};var B,w,H;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: props => <LabeledValue {...props}>
+      <Label>Support pin</Label>
+      <Button variant="soft" color="secondary">
+        Generate
+      </Button>
+    </LabeledValue>
+}`,...(H=(w=p.parameters)==null?void 0:w.docs)==null?void 0:H.source}}};var V,T,W;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: props => <LabeledValue>
+      <Label>
+        Role
+        <ContextualHelpTrigger>
+          <Button />
+          <ContextualHelp {...props}>
+            <Heading>Rights & roles</Heading>
+            <Text>
+              Each user profile is assigned a role in mStudio for each project
+              and/or organization. This allows you to work in a completely new
+              and modern way.
+            </Text>
+            <Link>Learn more</Link>
+          </ContextualHelp>
+        </ContextualHelpTrigger>
+      </Label>
+      <Text>Admin</Text>
+    </LabeledValue>
+}`,...(W=(T=m.parameters)==null?void 0:T.docs)==null?void 0:W.source}}};const Ne=["Default","WithCopyButton","WithInlineCode","WithLink","WithButton","WithContextualHelp"];export{n as Default,p as WithButton,m as WithContextualHelp,a as WithCopyButton,s as WithInlineCode,i as WithLink,Ne as __namedExportsOrder,_e as default};
