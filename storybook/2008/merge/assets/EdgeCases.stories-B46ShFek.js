bca146196c62e29fd484d31e32c0f91659b425e2
mittwald <>
2025-09-30 06:06:54 +0000

    Cleanup merged Storybook previews

--- storybook/2008/merge/assets/EdgeCases.stories-B46ShFek.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./iframe-BkQEXHdV.js";import{B as s}from"./Breadcrumb--X0Uov65.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import d from"./Default.stories-gmHMs6i-.js";import{L as i}from"./Link-C1KcylOI.js";import"./index-Cun1SEai.js";import"./clsx-B-dksMZM.js";import"./IconWarning-Bhg2W068.js";import"./PropsContextProvider-DGcDvaD0.js";import"./mergeRefs-CrNR4Adf.js";import"./index-NrWjSaDr.js";import"./Collection-41evgXm1.js";import"./CollectionBuilder-CMEP5N_o.js";import"./Hidden-B12OlcMW.js";import"./useFocusable-E_oKRPWi.js";import"./useFocus-DvrdYcoe.js";import"./utils-vh8nJaNh.js";import"./filterDOMProps-CghfNOdR.js";import"./useLocalizedStringFormatter-C-gsK0Kk.js";import"./context-DYNAGC-2.js";import"./usePress-CS8Sp9_K.js";import"./useFocusRing-DgjuUHXy.js";const R={...d,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>r.jsxs(s,{children:[r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium})]})};var t,o,p;m.parameters={...m.parameters,docs:{...(t=m.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...(p=(o=m.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const S=["LongTexts"];export{m as LongTexts,S as __namedExportsOrder,R as default};
