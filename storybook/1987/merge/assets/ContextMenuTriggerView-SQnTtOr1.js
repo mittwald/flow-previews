4fcdeaac555e6154e289b4e3ffe79333339bcf25
mittwald <>
2025-09-30 09:19:34 +0000

    Cleanup merged Storybook previews

--- storybook/1987/merge/assets/ContextMenuTriggerView-SQnTtOr1.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./iframe-JrijvI0G.js";import{b as i}from"./ContextMenuSection-B_Wuc4T7.js";import{v as s}from"./PropsContextProvider-Bc6JGim9.js";const t=e.memo(o=>{const r=e.useContext(s).ContextMenuTrigger??i;return n.jsx(r,{...o})});t.displayName="ContextMenuTriggerView";t.__docgenInfo={description:"",methods:[],displayName:"ContextMenuTriggerView"};export{t as C};
