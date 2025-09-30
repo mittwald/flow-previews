bcc49f339b356f56cb7d8f1c37810cdc5b9ee367
mittwald <>
2025-09-30 12:06:40 +0000

    Cleanup merged Storybook previews

--- storybook/1845/merge/assets/useEvent-2yiTmDJR.js
+++
@@ -1,1 +0,0 @@
-import{d as l}from"./index-DeoRyUxP.js";import{r as s}from"./iframe-B7R3Cp0m.js";function d(e,r,c,t){let a=l(c),f=c==null;s.useEffect(()=>{if(f||!e.current)return;let n=e.current;return n.addEventListener(r,a,t),()=>{n.removeEventListener(r,a,t)}},[e,r,t,f,a])}export{d as $};
