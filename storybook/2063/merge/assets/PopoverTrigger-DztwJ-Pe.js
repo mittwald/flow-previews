35764fa66bab55f72d3f3288e40f3fe977c013dd
mittwald <>
2025-10-09 09:59:49 +0000

    Cleanup merged Storybook previews

--- storybook/2063/merge/assets/PopoverTrigger-DztwJ-Pe.js
+++
@@ -1,1 +0,0 @@
-import{j as p}from"./iframe-CwjVVvSu.js";import{O as i}from"./OverlayTrigger-BpES57uV.js";import{f as t}from"./flowComponent-CL0EVs0o.js";import{D as g}from"./DialogTriggerView-CFk6lPET.js";const f=t("PopoverTrigger",r=>{const{children:o,...e}=r;return p.jsx(i,{overlayType:"Popover",...e,component:g,children:o})},{type:"provider"});export{f as P};
