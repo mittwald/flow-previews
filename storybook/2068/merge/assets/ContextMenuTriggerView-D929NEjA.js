e5102a27c326adf5e93fedb722463a39c85f9933
mittwald <>
2025-10-10 08:24:21 +0000

    Cleanup merged Storybook previews

--- storybook/2068/merge/assets/ContextMenuTriggerView-D929NEjA.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as n}from"./iframe-BsWE3_7o.js";import{C as i}from"./ContextMenuTrigger-8lrQpNpV.js";import{v as s}from"./flowComponent-Cm7zYSgo.js";const t=e.memo(o=>{const r=e.useContext(s).ContextMenuTrigger??i;return n.jsx(r,{...o})});t.displayName="ContextMenuTriggerView";t.__docgenInfo={description:"",methods:[],displayName:"ContextMenuTriggerView"};export{t as C};
--- storybook/2068/merge/assets/PopoverTrigger-UdIbNYx_.js
+++
@@ -1,1 +0,0 @@
-import{j as p}from"./iframe-BsWE3_7o.js";import{O as i}from"./OverlayTrigger-SkKoiEws.js";import{f as t}from"./flowComponent-Cm7zYSgo.js";import{D as g}from"./DialogTriggerView-CTYJ0CEZ.js";const f=t("PopoverTrigger",r=>{const{children:o,...e}=r;return p.jsx(i,{overlayType:"Popover",...e,component:g,children:o})},{type:"provider"});export{f as P};
