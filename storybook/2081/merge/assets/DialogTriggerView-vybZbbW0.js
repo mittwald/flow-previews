64b139cb6152ae5695849b07592c96172cc25d59
mittwald <>
2025-10-21 07:58:37 +0000

    Cleanup merged Storybook previews

--- storybook/2081/merge/assets/DialogTriggerView-vybZbbW0.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as t}from"./iframe-Cxe2R7_6.js";import{D as g}from"./OverlayTrigger-Bx33MvQ_.js";import{v as s}from"./flowComponent-atuhteL1.js";const e=o.memo(r=>{const i=o.useContext(s).DialogTrigger??g;return t.jsx(i,{...r})});e.displayName="DialogTriggerView";e.__docgenInfo={description:"",methods:[],displayName:"DialogTriggerView"};export{e as D};
