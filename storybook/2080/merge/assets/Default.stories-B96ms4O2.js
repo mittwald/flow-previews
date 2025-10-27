711b740b5cfa7d3391e9c8a6f72f31f38981a1eb
mittwald <>
2025-10-27 07:04:09 +0000

    Cleanup merged Storybook previews

--- storybook/2080/merge/assets/Default.stories-B96ms4O2.js
+++
@@ -1,5 +0,0 @@
-import{j as c}from"./iframe-CTxtjXon.js";import{R as o}from"./Rating-CWgtACoL.js";import"./index-Cun1SEai.js";import"./IconWarning-BCagwmXL.js";import"./clsx-B-dksMZM.js";import"./flowComponent-Cceoz8ap.js";import"./index-BzGnAInb.js";import"./index-BCkzqzEU.js";import"./useLocalizedStringFormatter-B51ecTCL.js";import"./context-OuuPOLWB.js";const z={title:"Content/Rating",component:o,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:n=>c.jsx(o,{...n})},r={},e={args:{size:"s"}};var t,s,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var m,i,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const D=["Default","Small"];export{r as Default,e as Small,D as __namedExportsOrder,z as default};
