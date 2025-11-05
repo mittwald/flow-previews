b9d8b00e54a2fef47a676b2abd50de21594ca327
mittwald <>
2025-11-05 05:53:39 +0000

    Cleanup merged Storybook previews

--- storybook/2114/merge/assets/EdgeCases.stories-DoR0GnpC.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./iframe-BnYuPSwa.js";import{B as t}from"./Breadcrumb-30UhIvGz.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import o from"./Default.stories-0Fj9uq1Q.js";import{L as i}from"./Link-CQXfsiwR.js";import"./index-nuYtCEEu.js";import"./clsx-B-dksMZM.js";import"./IconWarning-BustMFuX.js";import"./flowComponent-DiACFF0p.js";import"./index-BcFNTWxO.js";import"./index-CqvtBndu.js";import"./Collection-BD1KYYyZ.js";import"./CollectionBuilder-DbTeSjLO.js";import"./Hidden-C5Dr68xZ.js";import"./useFocusable-bs-iuI5B.js";import"./utils-DoWPBSSq.js";import"./filterDOMProps-i7L6S0l1.js";import"./useLocalizedStringFormatter-Cnytr8Uw.js";import"./context-DQlYt1c9.js";import"./useFocusRing-CPn0GoCO.js";const _={...o,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>r.jsxs(t,{children:[r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium})]})};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...m.parameters?.docs?.source}}};const v=["LongTexts"];export{m as LongTexts,v as __namedExportsOrder,_ as default};
