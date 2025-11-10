5741e51948c8e60e1b62be767c66ea8b8524093d
mittwald <>
2025-11-10 06:57:15 +0000

    Cleanup merged Storybook previews

--- storybook/2123/merge/assets/EdgeCases.stories-W09a4Ds4.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-Bb2Dt4gO.js";import{L as e}from"./Link-BMpobd2R.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import m from"./Default.stories-C9SSGkx2.js";import{f as n}from"./IconWarning-ncqjKY0i.js";import"./index-nuYtCEEu.js";import"./flowComponent-CJrXYk4c.js";import"./index-DhaS9Xnk.js";import"./clsx-B-dksMZM.js";import"./index-P4MnPsl7.js";import"./useLocalizedStringFormatter-C4xJr-3b.js";import"./context-OBblbX9f.js";import"./utils-BRWgyZeC.js";import"./filterDOMProps-i7L6S0l1.js";import"./useFocusRing-BqqueoK_.js";import"./useFocusable-BsZWsXG_.js";import"./Text-DlHja6rG.js";import"./browser-C45ETDbe.js";import"./EmulatedBoldText-BkxAkbDy.js";const h={...m,title:"Navigation/Link/Edge Cases"},r={render:t=>o.jsxs(e,{...t,children:[i.long,o.jsx(n,{"aria-label":"external link"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...r.parameters?.docs?.source}}};const v=["LongText"];export{r as LongText,v as __namedExportsOrder,h as default};
