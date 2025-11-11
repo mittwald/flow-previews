0fa77fc3f4cd672f0157917dcf5a9c14b06ee0fe
mittwald <>
2025-11-11 14:12:02 +0000

    Cleanup merged Storybook previews

--- storybook/2125/merge/assets/EdgeCases.stories-ChQR_OBa.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./iframe-sw6YBGs2.js";import a from"./Default.stories-BRKZ2eKM.js";import{D as o}from"./DonutChart-Behnkh0T.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import"./index-nuYtCEEu.js";import"./clsx-B-dksMZM.js";import"./ProgressBar-LVLLuOf0.js";import"./utils-DUnEZ_OM.js";import"./index-CQ8O5V0p.js";import"./index-DXtHeKIw.js";import"./Hidden-CsbjM4Ab.js";import"./filterDOMProps-i7L6S0l1.js";import"./context-C55dwVSC.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Text-BcvoGUvL.js";import"./flowComponent-9kUQeyVn.js";import"./browser-CM4mIXp1.js";import"./EmulatedBoldText-2hgsjV1R.js";import"./Legend-VgLIiYH1.js";const y={title:"Data Visualisation/DonutChart/Edge Cases",...a},r={render:()=>e.jsx(o,{value:1e12,maxValue:11e11})},t={render:()=>e.jsx(o,{value:10,size:"l",children:m.short})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={1000000000000} maxValue={1100000000000} />
-}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={10} size="l">
-      {dummyText.short}
-    </DonutChart>
-}`,...t.parameters?.docs?.source}}};const z=["LongValue","LongChildrenContent"];export{t as LongChildrenContent,r as LongValue,z as __namedExportsOrder,y as default};
