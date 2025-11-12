b4aec01da597fd81b86ac255aae43715866b2453
mittwald <>
2025-11-12 08:42:32 +0000

    Cleanup merged Storybook previews

--- storybook/2120/merge/assets/EdgeCases.stories-CQXaupEJ.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-Ye-9ROja.js";import{L as e}from"./Link-IU6OuunG.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import m from"./Default.stories-CimgdWbS.js";import{f as n}from"./IconWarning-C5ScV98r.js";import"./index-nuYtCEEu.js";import"./flowComponent-DVMSJKid.js";import"./index-BD2FLLtY.js";import"./clsx-B-dksMZM.js";import"./index-MGNRFzGq.js";import"./useLocalizedStringFormatter-8EdZld9N.js";import"./context-BMOJ7e9i.js";import"./utils-CF6YsxS1.js";import"./filterDOMProps-i7L6S0l1.js";import"./useFocusRing-CJrPyvnd.js";import"./useFocusable-CkKnxemF.js";import"./Text-Bsxj-TZS.js";import"./browser-BcPcK0wn.js";import"./EmulatedBoldText-Bx43jLQW.js";const h={...m,title:"Navigation/Link/Edge Cases"},r={render:t=>o.jsxs(e,{...t,children:[i.long,o.jsx(n,{"aria-label":"external link"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...r.parameters?.docs?.source}}};const v=["LongText"];export{r as LongText,v as __namedExportsOrder,h as default};
--- storybook/2120/merge/assets/EdgeCases.stories-DQbmLDLO.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./iframe-Ye-9ROja.js";import a from"./Default.stories-Dzb7MXgf.js";import{D as o}from"./DonutChart-CX5CdKNp.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import"./index-nuYtCEEu.js";import"./clsx-B-dksMZM.js";import"./ProgressBar-Dc-dQlrj.js";import"./utils-CF6YsxS1.js";import"./index-BD2FLLtY.js";import"./index-MGNRFzGq.js";import"./Hidden-CYSIeRK5.js";import"./filterDOMProps-i7L6S0l1.js";import"./context-BMOJ7e9i.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Text-Bsxj-TZS.js";import"./flowComponent-DVMSJKid.js";import"./browser-BcPcK0wn.js";import"./EmulatedBoldText-Bx43jLQW.js";import"./Legend-CZBMwtAS.js";const y={title:"Data Visualisation/DonutChart/Edge Cases",...a},r={render:()=>e.jsx(o,{value:1e12,maxValue:11e11})},t={render:()=>e.jsx(o,{value:10,size:"l",children:m.short})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={1000000000000} maxValue={1100000000000} />
-}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={10} size="l">
-      {dummyText.short}
-    </DonutChart>
-}`,...t.parameters?.docs?.source}}};const z=["LongValue","LongChildrenContent"];export{t as LongChildrenContent,r as LongValue,z as __namedExportsOrder,y as default};
