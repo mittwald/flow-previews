d0e1cca81b4fd9cbe374f5056d79226e4162156a
mittwald <>
2025-11-12 09:21:03 +0000

    Cleanup merged Storybook previews

--- storybook/2126/merge/assets/Default.stories-8I4YWk2W.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./iframe-Ye-9ROja.js";import{R as o}from"./Rating-DADKnEjj.js";import"./index-nuYtCEEu.js";import"./IconWarning-C5ScV98r.js";import"./clsx-B-dksMZM.js";import"./flowComponent-DVMSJKid.js";import"./index-BD2FLLtY.js";import"./index-MGNRFzGq.js";import"./useLocalizedStringFormatter-8EdZld9N.js";import"./context-BMOJ7e9i.js";const f={title:"Content/Rating",component:o,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:t=>s.jsx(o,{...t})},r={},e={args:{size:"s"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...e.parameters?.docs?.source}}};const x=["Default","Small"];export{r as Default,e as Small,x as __namedExportsOrder,f as default};
