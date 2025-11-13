60333124297b0f047c5e33afd11b424091e4046d
mittwald <>
2025-11-13 12:34:29 +0000

    Cleanup merged Storybook previews

--- storybook/2130/merge/assets/Default.stories-CzY9XzI_.js
+++
@@ -1,9 +0,0 @@
-import{j as s}from"./iframe-D9rlzAqJ.js";import{I as o}from"./Image-znh9VsXc.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import"./index-nuYtCEEu.js";import"./flowComponent-DpPOKAlU.js";import"./index-Cnuf1N7w.js";import"./clsx-B-dksMZM.js";import"./index-BRV6ez6J.js";const h={title:"Content/Image",component:o,render:a=>s.jsx(o,{...a,alt:"Gopher",src:m.imageSrc})},r={},e={args:{withBorder:!0}},t={args:{aspectRatio:16/9}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    withBorder: true
-  }
-}`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    aspectRatio: 16 / 9
-  }
-}`,...t.parameters?.docs?.source}}};const f=["Default","WithBorder","WithAspectRation"];export{r as Default,t as WithAspectRation,e as WithBorder,f as __namedExportsOrder,h as default};
--- storybook/2130/merge/assets/EdgeCases.stories-CR8mA-rm.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./iframe-D9rlzAqJ.js";import{B as t}from"./Breadcrumb-0HuKzEPm.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import o from"./Default.stories-ClWi1m-5.js";import{L as i}from"./Link-CiWldIOq.js";import"./index-nuYtCEEu.js";import"./clsx-B-dksMZM.js";import"./IconWarning-DYHE_-m1.js";import"./flowComponent-DpPOKAlU.js";import"./index-Cnuf1N7w.js";import"./index-BRV6ez6J.js";import"./Collection-CHEizjOQ.js";import"./CollectionBuilder-CmYRnc-a.js";import"./Hidden-BT89mVsv.js";import"./useFocusable-DWITSf8K.js";import"./utils-7I1ZD9XH.js";import"./filterDOMProps-i7L6S0l1.js";import"./useLocalizedStringFormatter-sycgthOl.js";import"./context-SsG6f9B0.js";import"./useFocusRing-Bm-SRwoH.js";const _={...o,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>r.jsxs(t,{children:[r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium})]})};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...m.parameters?.docs?.source}}};const v=["LongTexts"];export{m as LongTexts,v as __namedExportsOrder,_ as default};
