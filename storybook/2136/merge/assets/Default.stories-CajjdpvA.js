80d6ff57b94871a684cd420d98d79dde02942c74
mittwald <>
2025-11-17 08:24:05 +0000

    Cleanup merged Storybook previews

--- storybook/2136/merge/assets/Default.stories-CajjdpvA.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./iframe-DBiC_ZnR.js";import{R as o}from"./Rating-DLMdLSIQ.js";import"./index-nuYtCEEu.js";import"./IconWarning-CK2ZGKNK.js";import"./clsx-B-dksMZM.js";import"./flowComponent-D0XIYrNX.js";import"./index-Bp9tFnUt.js";import"./index-CVV0Ew5-.js";import"./useLocalizedStringFormatter-D7iDpTHP.js";import"./context-7Lcog2P0.js";const f={title:"Content/Rating",component:o,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:t=>s.jsx(o,{...t})},r={},e={args:{size:"s"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...e.parameters?.docs?.source}}};const x=["Default","Small"];export{r as Default,e as Small,x as __namedExportsOrder,f as default};
--- storybook/2136/merge/assets/EdgeCases.stories-DzTCC-wj.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-DBiC_ZnR.js";import{L as e}from"./Link-D0ngLqvb.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import m from"./Default.stories-FUnzQHvQ.js";import{f as n}from"./IconWarning-CK2ZGKNK.js";import"./index-nuYtCEEu.js";import"./flowComponent-D0XIYrNX.js";import"./index-Bp9tFnUt.js";import"./clsx-B-dksMZM.js";import"./index-CVV0Ew5-.js";import"./useLocalizedStringFormatter-D7iDpTHP.js";import"./context-7Lcog2P0.js";import"./utils-DtstTdJR.js";import"./filterDOMProps-i7L6S0l1.js";import"./useFocusRing-DtYok8JN.js";import"./useFocusable-CIN_gk5Z.js";import"./Text-BcLTITKQ.js";import"./browser-DU6w9x_W.js";import"./EmulatedBoldText-ClmR1Uhh.js";const h={...m,title:"Navigation/Link/Edge Cases"},r={render:t=>o.jsxs(e,{...t,children:[i.long,o.jsx(n,{"aria-label":"external link"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...r.parameters?.docs?.source}}};const v=["LongText"];export{r as LongText,v as __namedExportsOrder,h as default};
