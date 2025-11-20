e5b94701276eb243976a534463ac4a637d4624db
mittwald <>
2025-11-20 14:07:26 +0000

    Update from https://github.com/mittwald/flow/commit/708fae00962ff3957aaf5720ca17bde59169c8e2

---
+++ storybook/2142/merge/assets/Default.stories-JhvxW0Uf.js
@@ -0,0 +1,66 @@
+import{j as r}from"./iframe-ChXgWPPC.js";import{A as e}from"./ActionGroup-BMrZFOui.js";import{B as t}from"./Button-DdSGnULn.js";import{A as u,s as h}from"./Section-EbTwCLIg.js";import{S as y}from"./Modal-CZyndeHi.js";import{L as x}from"./Link-B12snMlw.js";import{u as j,F as f}from"./Form-LmaSyp--.js";import"./ResetButton-C7qHWWdg.js";import"./index-nuYtCEEu.js";import"./dynamic-nfc0SAk9.js";import"./flowComponent-DLnOhNG7.js";import"./index-BEhDMpqQ.js";import"./clsx-B-dksMZM.js";import"./index-CUhsAGa6.js";import"./IconWarning-BNdWvzeJ.js";import"./Text-B34O2ICL.js";import"./browser-DjrPe_WT.js";import"./utils-DiB9MypO.js";import"./EmulatedBoldText-BEs_gMjx.js";import"./LoadingSpinner-K2z7KZMw.js";import"./Button-DACNP5IV.js";import"./ProgressBar-Cav1e1WF.js";import"./Hidden-DouN_7bs.js";import"./filterDOMProps-i7L6S0l1.js";import"./context-29WSV5fn.js";import"./useFocusRing-CVyMmbQQ.js";import"./useFocusable-CRhE58ZF.js";import"./useLocalizedStringFormatter-6uEz2p-j.js";import"./context-7RM43Tkb.js";import"./RSPContexts-T2aOxEw5.js";import"./Collection-TEOU7cw3.js";import"./CollectionBuilder-CTYVMSyP.js";import"./SelectionIndicator-C4aBFtUw.js";import"./Separator-Dg3iBkBi.js";import"./useStatic-DmmsCa0X.js";import"./FileCardList-C4eHcIg8.js";import"./Avatar-ghljliNz.js";import"./AlertIcon-DTTgmXrL.js";import"./Image-DALWDs5z.js";import"./ControlledNotification-vSaFkeEz.js";import"./Flex-BwGFoWCl.js";import"./Accordion-BSAUPkyO.js";import"./Alert-BVyhkX9T.js";import"./AlertBadge-B7nfqq50.js";import"./Align-CLYk2yCP.js";import"./AvatarStack-36KK9ktO.js";import"./BigNumber-CFSbrkgt.js";import"./Breadcrumb-cvLbQ11O.js";import"./Heading-DMHDZ_u6.js";import"./Legend-DYla4PjT.js";import"./Color-BIUpZZos.js";import"./TableFooterRow-Dnf9rhvN.js";import"./SkeletonText-9oOatj3m.js";import"./Content-BASnjacW.js";import"./Label-BPAVwVMs.js";import"./CounterBadge-BfruJEd0.js";import"./DonutChart-CVlfJdd5.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./IllustratedMessage-Clt45Y1H.js";import"./Header-Dthqz58G.js";import"./Initials-BRgog33-.js";import"./InlineCode-D5O-E-M9.js";import"./LayoutCard-CyxN8E7I.js";import"./Separator-DHHhirb8.js";import"./MessageSeparator-DBolDfwP.js";import"./NavigationGroup-BwXeVraB.js";import"./Notification-BADXrVSo.js";import"./NotificationProvider-v-2Uz-uV.js";import"./ProgressBar-BUhrPkyu.js";import"./Rating-Bz_0i4Bq.js";import"./Skeleton-DepZBCMx.js";const Mr={title:"Actions/ActionGroup",component:e,render:o=>r.jsxs(e,{...o,children:[r.jsx(u,{action:()=>h(1500),children:r.jsx(t,{color:"accent",children:"Create customer"})}),r.jsx(u,{children:r.jsx(t,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})})]})},n={},s={render:o=>r.jsxs(e,{...o,children:[r.jsx(t,{color:"danger",children:"Delete project"}),r.jsx(t,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})]})},a={render:o=>r.jsx(e,{...o,children:r.jsx(t,{children:"Ok"})})},i={render:o=>r.jsxs(e,{...o,children:[r.jsx(t,{slot:"primary",color:"accent",children:"Add email address"}),r.jsx(t,{slot:"secondary",variant:"soft",color:"secondary",children:"Save and add more"}),r.jsx(t,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})]})},c={render:o=>r.jsxs(e,{...o,children:[r.jsx(t,{slot:"primary",color:"accent",children:"Add email address"}),r.jsx(t,{slot:"secondary",variant:"soft",color:"secondary",children:"Save and add more"}),r.jsx(t,{slot:"secondary",variant:"soft",color:"secondary",children:"Save as new"}),r.jsx(t,{slot:"abort",variant:"soft",color:"secondary",children:"Abort"})]})},p={parameters:{viewport:{defaultViewport:"mobile1"}}},m={render:o=>r.jsxs(e,{...o,children:[r.jsx(t,{slot:"secondary",children:"Edit"}),r.jsx(y,{slot:"primary",children:"Activate"})]})},d={render:o=>r.jsxs(e,{...o,children:[r.jsx(x,{slot:"abort",children:"Forgot password?"}),r.jsx(t,{color:"accent",children:"Login"})]})},l={render:o=>{const A=j();return r.jsx(f,{form:A,onSubmit:async()=>await h(2e3),children:r.jsx(e,{...o,children:r.jsx(t,{color:"accent",type:"submit",children:"Login"})})})}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:"{}",...n.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button color="danger">Delete project</Button>
+      <Button slot="abort" variant="soft" color="secondary">
+        Abort
+      </Button>
+    </ActionGroup>
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button>Ok</Button>
+    </ActionGroup>
+}`,...a.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button slot="primary" color="accent">
+        Add email address
+      </Button>
+      <Button slot="secondary" variant="soft" color="secondary">
+        Save and add more
+      </Button>
+      <Button slot="abort" variant="soft" color="secondary">
+        Abort
+      </Button>
+    </ActionGroup>
+}`,...i.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button slot="primary" color="accent">
+        Add email address
+      </Button>
+      <Button slot="secondary" variant="soft" color="secondary">
+        Save and add more
+      </Button>
+      <Button slot="secondary" variant="soft" color="secondary">
+        Save as new
+      </Button>
+      <Button slot="abort" variant="soft" color="secondary">
+        Abort
+      </Button>
+    </ActionGroup>
+}`,...c.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...p.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Button slot="secondary">Edit</Button>
+      <Switch slot="primary">Activate</Switch>
+    </ActionGroup>
+}`,...m.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  render: props => <ActionGroup {...props}>
+      <Link slot="abort">Forgot password?</Link>
+      <Button color="accent">Login</Button>
+    </ActionGroup>
+}`,...d.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: props => {
+    const form = useForm();
+    return <Form form={form} onSubmit={async () => await sleep(2000)}>
+        <ActionGroup {...props}>
+          <Button color="accent" type="submit">
+            Login
+          </Button>
+        </ActionGroup>
+      </Form>;
+  }
+}`,...l.parameters?.docs?.source}}};const Vr=["Default","Danger","Info","WithSecondaryAction","WithExtraSecondaryAction","Mobile","WithSwitch","WithLink","WithReactHookForm"];export{s as Danger,n as Default,a as Info,p as Mobile,c as WithExtraSecondaryAction,d as WithLink,l as WithReactHookForm,i as WithSecondaryAction,m as WithSwitch,Vr as __namedExportsOrder,Mr as default};
