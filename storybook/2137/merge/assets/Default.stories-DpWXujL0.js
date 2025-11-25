1045c8eb1f91cbfb728e4a465a9065450cf6c136
mittwald <>
2025-11-25 14:39:47 +0000

    Cleanup merged Storybook previews

--- storybook/2137/merge/assets/Default.stories-DpWXujL0.js
+++
@@ -1,9 +0,0 @@
-import{j as s}from"./iframe-Du_qouD_.js";import{I as o}from"./Image-B0yvlgyM.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import"./index-nuYtCEEu.js";import"./flowComponent-rDAUUIs0.js";import"./index-D82te6aO.js";import"./clsx-B-dksMZM.js";import"./index-Cna9wk2l.js";const h={title:"Content/Image",component:o,render:a=>s.jsx(o,{...a,alt:"Gopher",src:m.imageSrc})},r={},e={args:{withBorder:!0}},t={args:{aspectRatio:16/9}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    withBorder: true
-  }
-}`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    aspectRatio: 16 / 9
-  }
-}`,...t.parameters?.docs?.source}}};const f=["Default","WithBorder","WithAspectRation"];export{r as Default,t as WithAspectRation,e as WithBorder,f as __namedExportsOrder,h as default};
