54fa7de11c8f804f80115e14097814a8dd2ce4e0
mittwald <>
2025-11-26 13:45:14 +0000

    Cleanup merged Storybook previews

--- storybook/2132/merge/assets/Default.stories-NRDRTo56.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-DTBy15H7.js";import{B as s}from"./BigNumber-BCtBSLB-.js";import{T as a}from"./Text-Bj8vPlc0.js";import"./index-nuYtCEEu.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-Fi7tnu-J.js";import"./mergeRefs-CWVC3DZq.js";import"./index-C9i3V5R3.js";import"./browser-MW83o6M0.js";import"./utils-Dl3W8pzT.js";import"./EmulatedBoldText-Dvqrfp28.js";const g={title:"Data Visualisation/BigNumber",component:s,render:o=>t.jsx(s,{...o,children:"69%"})},r={},e={render:o=>t.jsxs(s,{...o,children:[t.jsx(a,{children:"69%"}),t.jsx(a,{children:"Performance"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: props => <BigNumber {...props}>
-      <Text>69%</Text>
-      <Text>Performance</Text>
-    </BigNumber>
-}`,...e.parameters?.docs?.source}}};const h=["Default","WithSubtitle"];export{r as Default,e as WithSubtitle,h as __namedExportsOrder,g as default};
--- storybook/2132/merge/assets/EdgeCases.stories-pYn_lVw_.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./iframe-DTBy15H7.js";import{L as e}from"./Link-CxbSfmHB.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import m from"./Default.stories-NXLSC14P.js";import{f as n}from"./IconWarning-Djz7NATM.js";import"./index-nuYtCEEu.js";import"./PropsContextProvider-Fi7tnu-J.js";import"./mergeRefs-CWVC3DZq.js";import"./index-C9i3V5R3.js";import"./clsx-B-dksMZM.js";import"./useLocalizedStringFormatter-C6mCvBeu.js";import"./context-DDV21Rg9.js";import"./utils-Dl3W8pzT.js";import"./filterDOMProps-i7L6S0l1.js";import"./useFocusRing-DZNjyW13.js";import"./useFocusable-C5o0miI6.js";import"./Text-Bj8vPlc0.js";import"./browser-MW83o6M0.js";import"./EmulatedBoldText-Dvqrfp28.js";const h={...m,title:"Navigation/Link/Edge Cases"},r={render:t=>o.jsxs(e,{...t,children:[i.long,o.jsx(n,{"aria-label":"external link"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: props => <Link {...props}>
-      {dummyText.long}
-      <IconExternalLink aria-label="external link" />
-    </Link>
-}`,...r.parameters?.docs?.source}}};const v=["LongText"];export{r as LongText,v as __namedExportsOrder,h as default};
