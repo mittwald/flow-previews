d49fdca842556a184976556f44bb03f666a1e177
mittwald <>
2025-12-05 10:58:54 +0000

    Cleanup merged Storybook previews

--- storybook/2175/merge/assets/Default.stories-CBeqBnoY.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./iframe-C5kwxwrP.js";import{B as s}from"./BigNumber-BUKpylgi.js";import{T as a}from"./Text-D7zEodsr.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-CTsx1ICl.js";import"./mergeRefs-BjhR6ukN.js";import"./index-D0rH5M4c.js";import"./browser-CdNWfkH8.js";import"./utils-ClkSWM7u.js";import"./EmulatedBoldText-B3gCae9O.js";const g={title:"Data Visualisation/BigNumber",component:s,render:o=>t.jsx(s,{...o,children:"69%"})},r={},e={render:o=>t.jsxs(s,{...o,children:[t.jsx(a,{children:"69%"}),t.jsx(a,{children:"Performance"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  render: props => <BigNumber {...props}>
-      <Text>69%</Text>
-      <Text>Performance</Text>
-    </BigNumber>
-}`,...e.parameters?.docs?.source}}};const h=["Default","WithSubtitle"];export{r as Default,e as WithSubtitle,h as __namedExportsOrder,g as default};
