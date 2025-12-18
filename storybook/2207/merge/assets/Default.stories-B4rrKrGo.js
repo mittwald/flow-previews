b7d2abfffe1a3cdc75263ba7e60834779f29a52b
mittwald <>
2025-12-18 08:37:22 +0000

    Cleanup merged Storybook previews

--- storybook/2207/merge/assets/Default.stories-B4rrKrGo.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./iframe-BA9q8iRx.js";import{R as o}from"./Rating-TF40lZ1V.js";import"./preload-helper-PPVm8Dsz.js";import"./IconWarning-Ddu6VgyQ.js";import"./clsx-B-dksMZM.js";import"./PropsContextProvider-BVBE-WhE.js";import"./mergeRefs-D8u8JUrl.js";import"./index-DpY8H-Wc.js";import"./useLocalizedStringFormatter-w9QjgZdm.js";import"./context-Cn4rMbpf.js";const f={title:"Content/Rating",component:o,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:t=>s.jsx(o,{...t})},r={},e={args:{size:"s"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...e.parameters?.docs?.source}}};const x=["Default","Small"];export{r as Default,e as Small,x as __namedExportsOrder,f as default};
--- storybook/2207/merge/assets/EdgeCases.stories-D6Scijmz.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./iframe-BA9q8iRx.js";import{B as t}from"./Breadcrumb-BFLF3mqz.js";import{d as e}from"./dummyText-CX_I_Wpl.js";import o from"./Default.stories-BPAxtXQ-.js";import{L as i}from"./Link-VeHt-BOj.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-B-dksMZM.js";import"./IconWarning-Ddu6VgyQ.js";import"./PropsContextProvider-BVBE-WhE.js";import"./mergeRefs-D8u8JUrl.js";import"./index-DpY8H-Wc.js";import"./Collection-DW_VGPX_.js";import"./CollectionBuilder-VfTqomJV.js";import"./Hidden-CwA9YFci.js";import"./useFocusable-CyfzywkV.js";import"./utils-QcIDpANH.js";import"./filterDOMProps-i7L6S0l1.js";import"./useLocalizedStringFormatter-w9QjgZdm.js";import"./context-Cn4rMbpf.js";import"./useFocusRing--2IgGnS-.js";const _={...o,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>r.jsxs(t,{children:[r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium}),r.jsx(i,{children:e.medium})]})};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...m.parameters?.docs?.source}}};const v=["LongTexts"];export{m as LongTexts,v as __namedExportsOrder,_ as default};
