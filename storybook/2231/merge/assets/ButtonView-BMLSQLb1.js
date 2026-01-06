c304d74773a87a9639249d05ad6248a3373e1105
mittwald <>
2026-01-06 12:59:09 +0000

    Cleanup merged Storybook previews

--- storybook/2231/merge/assets/ButtonView-BMLSQLb1.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as s}from"./iframe-Dnm846PF.js";import{B as r}from"./Button-D3IXyzzj.js";import{v as i}from"./flowComponent-S63Cq1sy.js";const o=t.memo(e=>{const n=t.useContext(i).Button??r;return s.jsx(n,{...e})});o.displayName="ButtonView";o.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{o as B};
--- storybook/2231/merge/assets/EdgeCases.stories-CGDBg4uW.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./iframe-Dnm846PF.js";import a from"./Default.stories-BwDMSO98.js";import{D as e}from"./DonutChart-BA2hvE0U.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-B-dksMZM.js";import"./ProgressBar-BIFWcEy_.js";import"./utils-VDIU0BnD.js";import"./index-Db1dbbsL.js";import"./index-sBHKlQZD.js";import"./Label-B2apKb0w.js";import"./Hidden-1UUWpuD7.js";import"./filterDOMProps-i7L6S0l1.js";import"./useLabel-pLNL7Cvk.js";import"./context-CoOH5oCy.js";import"./NumberFormatter-DNR9MAW-.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Legend-B9PSMXCO.js";import"./Text-DtZxu6g8.js";import"./flowComponent-S63Cq1sy.js";import"./browser-DEiAhc6G.js";import"./EmulatedBoldText-DH6NWkOG.js";import"./Text-1v5GZWlz.js";const _={title:"Data Visualisation/DonutChart/Edge Cases",...a},r={render:()=>o.jsx(e,{value:1e12,maxValue:11e11})},t={render:()=>o.jsx(e,{value:10,size:"l",children:m.short})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={1000000000000} maxValue={1100000000000} />
-}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={10} size="l">
-      {dummyText.short}
-    </DonutChart>
-}`,...t.parameters?.docs?.source}}};const O=["LongValue","LongChildrenContent"];export{t as LongChildrenContent,r as LongValue,O as __namedExportsOrder,_ as default};
