cf919f088814486d9230c5aacfe1c5bb10f7bf83
mittwald <>
2026-01-07 08:55:16 +0000

    Cleanup merged Storybook previews

--- storybook/2217/merge/assets/ContextMenuView-C787-Exy.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as s}from"./iframe-DITmTDde.js";import{C as r}from"./MenuItem-CTC75U-2.js";import{v as i}from"./flowComponent-BIGGpSqP.js";const t=e.memo(o=>{const n=e.useContext(i).ContextMenu??r;return s.jsx(n,{...o})});t.displayName="ContextMenuView";t.__docgenInfo={description:"",methods:[],displayName:"ContextMenuView"};export{t as C};
