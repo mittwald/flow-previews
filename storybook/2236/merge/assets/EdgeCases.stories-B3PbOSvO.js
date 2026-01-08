f1bf3997947fc6ff3ac69a8f300c1adf1c63248d
mittwald <>
2026-01-08 11:47:15 +0000

    Cleanup merged Storybook previews

--- storybook/2236/merge/assets/EdgeCases.stories-B3PbOSvO.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./iframe-Bt_Ape3b.js";import a from"./Default.stories-CLyeuvin.js";import{D as e}from"./DonutChart-D1kwz-sk.js";import{d as m}from"./dummyText-CX_I_Wpl.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-B-dksMZM.js";import"./ProgressBar-DgNpOae2.js";import"./utils-DGGum7sG.js";import"./index-DJTzXtHe.js";import"./index-gN6AAF3-.js";import"./Label-Bfb_thp3.js";import"./Hidden-CJIrU941.js";import"./filterDOMProps-i7L6S0l1.js";import"./useLabel-BAWBHbvC.js";import"./context-BGqy3a6E.js";import"./NumberFormatter-DNR9MAW-.js";import"./getCategoricalColorByIndex-BIvTpnTO.js";import"./Legend-DgfzaVpi.js";import"./Text-Bs2hbxJA.js";import"./flowComponent-CoHw3WqD.js";import"./browser-BI8hAkYP.js";import"./EmulatedBoldText-213nH2nf.js";import"./Text-DwU1R_Cs.js";const _={title:"Data Visualisation/DonutChart/Edge Cases",...a},r={render:()=>o.jsx(e,{value:1e12,maxValue:11e11})},t={render:()=>o.jsx(e,{value:10,size:"l",children:m.short})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={1000000000000} maxValue={1100000000000} />
-}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  render: () => <DonutChart value={10} size="l">
-      {dummyText.short}
-    </DonutChart>
-}`,...t.parameters?.docs?.source}}};const O=["LongValue","LongChildrenContent"];export{t as LongChildrenContent,r as LongValue,O as __namedExportsOrder,_ as default};
--- storybook/2236/merge/assets/dynamic-D3ysZU61.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./flowComponent-CoHw3WqD.js";const m=o=>({[a]:o});export{m as d};
