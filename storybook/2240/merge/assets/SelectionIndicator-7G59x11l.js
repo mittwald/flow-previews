e0c5bde0f246c5f8d308f41ae5b40f7658d04b39
mittwald <>
2026-01-12 08:45:39 +0000

    Cleanup merged Storybook previews

--- storybook/2240/merge/assets/SelectionIndicator-7G59x11l.js
+++
@@ -1,1 +0,0 @@
-import"./index-DJTzXtHe.js";import{r as e,R as a}from"./iframe-Bt_Ape3b.js";const c=e.createContext(null);function n(t){let r=e.useRef({});return a.createElement(c.Provider,{value:r},t.children)}const s=e.createContext({isSelected:!1});export{s as $,n as a};
