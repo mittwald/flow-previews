cd781cde3ad92e7b9ebb94532ab68973c07d40ae
mittwald <>
2026-01-12 09:04:53 +0000

    Cleanup merged Storybook previews

--- storybook/2238/merge/assets/EdgeCases.stories-BDMBtaNL.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./iframe-4vGjmvQN.js";import{B as e}from"./Breadcrumb-CyXZYWpO.js";import{d as i}from"./dummyText-CX_I_Wpl.js";import o from"./Default.stories-ERvSGDk3.js";import{L as t}from"./Link-BJK7mn5B.js";import"./preload-helper-PPVm8Dsz.js";import"./clsx-B-dksMZM.js";import"./IconWarning-CMo0ZXSb.js";import"./flowComponent-BTnwUX2e.js";import"./index-DFF4TtWB.js";import"./index--XKY9bK1.js";import"./remote-BMVFgzCt.js";import"./Collection-JwP5-obH.js";import"./CollectionBuilder-BFcqcbLB.js";import"./Hidden-DKOWGOBv.js";import"./useFocusable-BvBcbeR1.js";import"./useFocus-BGpJf3EA.js";import"./utils-Cb3SeWCs.js";import"./filterDOMProps-i7L6S0l1.js";import"./useLocalizedStringFormatter-Ci5i2g9p.js";import"./context-Cugfl_gO.js";import"./usePress-SP29s-a6.js";import"./useFocusRing-ctZhab72.js";const N={...o,title:"Navigation/Breadcrumb/Edge Cases"},m={render:()=>r.jsxs(e,{children:[r.jsx(t,{children:i.medium}),r.jsx(t,{children:i.medium}),r.jsx(t,{children:i.medium})]})};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  render: () => <Breadcrumb>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-      <Link>{dummyText.medium}</Link>
-    </Breadcrumb>
-}`,...m.parameters?.docs?.source}}};const O=["LongTexts"];export{m as LongTexts,O as __namedExportsOrder,N as default};
--- storybook/2238/merge/assets/Label-DRFrGBxg.js
+++
@@ -1,1 +0,0 @@
-import{$ as o}from"./utils-Cb3SeWCs.js";import{$}from"./Hidden-DKOWGOBv.js";import{r as b,R as f}from"./iframe-4vGjmvQN.js";const l=b.createContext({}),p=$(function(e,a){[e,a]=o(e,a,l);let{elementType:t="label",...r}=e;return f.createElement(t,{className:"react-aria-Label",...r,ref:a})});export{l as $,p as a};
