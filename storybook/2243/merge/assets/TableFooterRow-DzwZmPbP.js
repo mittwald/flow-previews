51385e556af525d87d513632f37edda8e981ba8d
mittwald <>
2026-01-12 13:36:39 +0000

    Cleanup merged Storybook previews

--- storybook/2243/merge/assets/TableFooterRow-DzwZmPbP.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./iframe-Ca6Y6Qbd.js";import{e as r}from"./TableCell-9iT4_CYZ.js";const t=e=>o.jsx(r,{...e,footer:!0});t.__docgenInfo={description:"@flr-generate all",methods:[],displayName:"TableFooterRow",props:{footer:{required:!1,tsType:{name:"boolean"},description:"@internal"}}};export{t as T};
