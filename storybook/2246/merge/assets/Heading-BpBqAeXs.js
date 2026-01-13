391d0546f88ed2d4979aee7cf44736fff781ac7a
mittwald <>
2026-01-13 07:22:12 +0000

    Cleanup merged Storybook previews

--- storybook/2246/merge/assets/Heading-BpBqAeXs.js
+++
@@ -1,1 +0,0 @@
-import{$ as c}from"./RSPContexts-BzWalm_3.js";import{$ as f}from"./utils-B2fStg81.js";import{r as m,R as s}from"./iframe-D5OSMSYj.js";const x=m.forwardRef(function(e,a){[e,a]=f(e,a,c);let{children:t,level:l=3,className:r,...o}=e,$=`h${l}`;return s.createElement($,{...o,ref:a,className:r??"react-aria-Heading"},t)});export{x as $};
