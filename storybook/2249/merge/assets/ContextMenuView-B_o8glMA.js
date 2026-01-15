6e956fe1056c7d0e4c87e4908f3e58f83fe2a1fb
mittwald <>
2026-01-15 12:50:39 +0000

    Cleanup merged Storybook previews

--- storybook/2249/merge/assets/ContextMenuView-B_o8glMA.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as s}from"./iframe-pcvtdEGM.js";import{C as r}from"./ContextMenu-DJR2fZHZ.js";import{v as i}from"./flowComponent-BsMfbHRX.js";const t=e.memo(o=>{const n=e.useContext(i).ContextMenu??r;return s.jsx(n,{...o})});t.displayName="ContextMenuView";t.__docgenInfo={description:"",methods:[],displayName:"ContextMenuView"};export{t as C};
--- storybook/2249/merge/assets/Default.stories-CB5ZG3OV.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./iframe-pcvtdEGM.js";import{R as e}from"./Rating-tI85R1gX.js";import"./preload-helper-PPVm8Dsz.js";import"./IconWarning-BUxMadyi.js";import"./clsx-B-dksMZM.js";import"./flowComponent-BsMfbHRX.js";import"./index-qOUwp4oz.js";import"./index-YznY-g7A.js";import"./remote-O2EONUZN.js";import"./useLocalizedStringFormatter-vFyNiPiP.js";import"./context-We2COa_S.js";const x={title:"Content/Rating",component:e,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:t=>s.jsx(e,{...t})},r={},o={args:{size:"s"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...o.parameters?.docs?.source}}};const R=["Default","Small"];export{r as Default,o as Small,R as __namedExportsOrder,x as default};
