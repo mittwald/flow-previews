3c1a1dde41b6919b5bd9e48126039437dda26aa4
mittwald <>
2026-01-20 07:26:51 +0000

    Cleanup merged Storybook previews

--- storybook/2253/merge/assets/DialogTriggerView-CPaNcdTJ.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as t}from"./iframe-C5hcdLE6.js";import{D as g}from"./OverlayTrigger-DT2_gJPw.js";import{v as s}from"./flowComponent-DfDzLOdb.js";const e=o.memo(r=>{const i=o.useContext(s).DialogTrigger??g;return t.jsx(i,{...r})});e.displayName="DialogTriggerView";e.__docgenInfo={description:"",methods:[],displayName:"DialogTriggerView"};export{e as D};
