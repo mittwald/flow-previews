0a7bcb225c8e2daaefbe19c94ceed47b711607c0
mittwald <>
2026-01-23 13:09:42 +0000

    Cleanup merged Storybook previews

--- storybook/2263/merge/assets/DialogTriggerView-GLdM8JEW.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as t}from"./iframe-B_VLmVOf.js";import{D as g}from"./OverlayTrigger-r-EuZvmq.js";import{v as s}from"./flowComponent-Bc5-OX5Z.js";const e=o.memo(r=>{const i=o.useContext(s).DialogTrigger??g;return t.jsx(i,{...r})});e.displayName="DialogTriggerView";e.__docgenInfo={description:"",methods:[],displayName:"DialogTriggerView"};export{e as D};
