e59860b0c9d1ca6402f24a64b03bbd7d42bdecd6
mittwald <>
2026-01-27 08:59:37 +0000

    Cleanup merged Storybook previews

--- storybook/2272/merge/assets/ButtonView-BaOHg3N5.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as s}from"./iframe-COrLnxQ5.js";import{B as r}from"./Button-B5T-9t49.js";import{v as i}from"./flowComponent-Whsy6gdm.js";const o=t.memo(e=>{const n=t.useContext(i).Button??r;return s.jsx(n,{...e})});o.displayName="ButtonView";o.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{o as B};
--- storybook/2272/merge/assets/ContextMenuView-BlYlTfe6.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as s}from"./iframe-COrLnxQ5.js";import{C as r}from"./ContextMenu-DVAA2gTD.js";import{v as i}from"./flowComponent-Whsy6gdm.js";const t=e.memo(o=>{const n=e.useContext(i).ContextMenu??r;return s.jsx(n,{...o})});t.displayName="ContextMenuView";t.__docgenInfo={description:"",methods:[],displayName:"ContextMenuView"};export{t as C};
