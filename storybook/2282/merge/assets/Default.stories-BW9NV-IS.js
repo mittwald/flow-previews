0fd7d1d87d1a9f1acd74fb9650a48253b3a4d127
mittwald <>
2026-01-28 06:35:18 +0000

    Cleanup merged Storybook previews

--- storybook/2282/merge/assets/Default.stories-BW9NV-IS.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./iframe-BbrP11p-.js";import{R as e}from"./Rating-BkpSYUTA.js";import"./preload-helper-PPVm8Dsz.js";import"./IconWarning-BeRGE0lU.js";import"./clsx-B-dksMZM.js";import"./flowComponent-Bjf10jjx.js";import"./index-l4GzuhKJ.js";import"./index-CvmJvI2t.js";import"./remote-BUE50-N_.js";import"./useLocalizedStringFormatter-au7M1iG_.js";import"./context-Cg0av1qu.js";const x={title:"Content/Rating",component:e,argTypes:{value:{control:"inline-radio",options:[0,1,2,3,4,5]}},args:{value:2},render:t=>s.jsx(e,{...t})},r={},o={args:{size:"s"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: "s"
-  }
-}`,...o.parameters?.docs?.source}}};const R=["Default","Small"];export{r as Default,o as Small,R as __namedExportsOrder,x as default};
