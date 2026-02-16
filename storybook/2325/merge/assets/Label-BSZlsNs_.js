d09980bb9d24ad253993defc3c83c4417ec355b4
mittwald <>
2026-02-16 13:53:27 +0000

    Cleanup merged Storybook previews

--- storybook/2325/merge/assets/Label-BSZlsNs_.js
+++
@@ -1,1 +0,0 @@
-import{$ as o}from"./utils-BuDA3Ujk.js";import{$}from"./Hidden-U4jsQoYY.js";import{r as b,R as f}from"./iframe-C5ZTNo7B.js";const l=b.createContext({}),p=$(function(e,a){[e,a]=o(e,a,l);let{elementType:t="label",...r}=e;return f.createElement(t,{className:"react-aria-Label",...r,ref:a})});export{l as $,p as a};
