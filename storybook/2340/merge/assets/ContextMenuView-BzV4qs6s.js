88b77d6753c6a6f066a4548d5ec7337ee9bd11aa
mittwald <>
2026-02-23 07:06:25 +0000

    Cleanup merged Storybook previews

--- storybook/2340/merge/assets/ContextMenuView-BzV4qs6s.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as s}from"./iframe-BoiTaxGN.js";import{C as r}from"./MenuItem-CDDy6p-6.js";import{v as i}from"./flowComponent-5SfKf5nB.js";const t=e.memo(o=>{const n=e.useContext(i).ContextMenu??r;return s.jsx(n,{...o})});t.displayName="ContextMenuView";t.__docgenInfo={description:"",methods:[],displayName:"ContextMenuView"};export{t as C};
