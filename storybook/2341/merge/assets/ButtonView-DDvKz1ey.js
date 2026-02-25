3d3c1cd24780815224870e40c839d2f859dcc624
mittwald <>
2026-02-25 13:02:17 +0000

    Cleanup merged Storybook previews

--- storybook/2341/merge/assets/ButtonView-DDvKz1ey.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as s}from"./iframe-Bh3xJiHl.js";import{B as r}from"./Button-C1GDS2T5.js";import{v as i}from"./flowComponent-C3sfiT_Y.js";const o=t.memo(e=>{const n=t.useContext(i).Button??r;return s.jsx(n,{...e})});o.displayName="ButtonView";o.__docgenInfo={description:"",methods:[],displayName:"ButtonView"};export{o as B};
